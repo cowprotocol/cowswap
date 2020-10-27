416ed9ff6a1db9dfff8d0abef1721c46bca5e8a9
Anxo Rodriguez <anxolin@gmail.com>
2020-10-27 15:02:31 +0100

    Add padding

--- src/custom/components/Header.tsx
+++ src/custom/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const HeaderFrame = styled.div`
+  padding-left: 2rem;
+`
 
 export default function Header() {
   return (
-    <h2><span role="img" aria-label="logo">🦐</span> OBA Swap</h2>
+    <HeaderFrame>
+      <h2><span role="img" aria-label="logo">🦐</span> OBA Swap</h2>
+
+    </HeaderFrame>
   )
 }
