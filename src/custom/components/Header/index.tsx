3e044e20b5c85fb91f8d29e8e905aba0ae11fe73
Anxo Rodriguez <anxolin@gmail.com>
2020-10-29 12:24:14 +0100

    Custom header (#5)
    * Add modified header
    
    * Add header modification
    
    * Override logos
    
    * Leave header placeholder
    
    * Create type WithClasss
    
    * Make header styled component compatible
    
    * Remove relative path

---
+++ src/custom/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import HeaderMod from './HeaderMod'
+import styled from 'styled-components'
+
+export const Header = styled(HeaderMod)`
+  // CSS Override
+`
+
+export default Header
---
+++ src/custom/types.ts
@@ -0,0 +1,3 @@
+export interface WithClassName {
+  className?: string
+}
