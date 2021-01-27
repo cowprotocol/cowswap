a5c35e51b3e4966b567197fe4036383fb8fbc373
Anxo Rodriguez <anxolin@gmail.com>
2021-01-27 10:28:22 +0100

    Make e2e independent (#108)
    * Create custom e2e
    
    * Just re-use test implementation
    
    * Add landing test
    
    * Reuse config ts
    
    * Reuse commands
    
    * Add list test
    
    * Add send tests
    
    * Add TODO
    
    * Reenable integration tests
    
    * Fix test integration attempt
    
    * Cache cypress
    
    * Run cypress
    
    * Run local server
    
    * Simplify integration tests
    
    * Rename integration ci
    
    * Rename file
    
    * Fix issue with quote
    
    * Remove node version
    
    * Use 2.2.7
    
    * Use 2.3.6
    
    * Set CI true
    
    * Revert original integration actions
    
    * Check custom key
    
    * Revert back

---
+++ cypress-custom/integration/lists.test.ts
@@ -0,0 +1,2 @@
+// TODO: Fix broken test https://github.com/gnosis/gp-swap-ui/issues/104
+// import '../../cypress/integration/lists.test'
---
+++ cypress-custom/support/index.js
@@ -0,0 +1,12 @@
+// ***********************************************************
+// This file is processed and loaded automatically before your test files.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Uniswap original commands
+import '../../cypress/support/commands'
+
+// Import commands.ts using ES2015 syntax:
+import './commands'
