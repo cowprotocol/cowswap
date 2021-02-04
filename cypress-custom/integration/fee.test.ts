c8574e742dc524dd3c5ca071fd2ccba128ef059b
David <david.sato64@gmail.com>
2021-02-04 21:45:26 +0000

    [open][e2e] check expired fee (#127)
    Part of the e2e suite - follows the Anxo _way_
    
    ### This is no longer a draft but is staying open. Tests do pass but many race condition problems are a concern. Order of tests changed to pass an even bigger indication that Cypress tear-down logic is flawed in v5 (and v6 still)
    
    1. Adds a helper fn for getting `_getChainFeeStorage` returning chain id fee infomation
    2. sets up checking expired fee and ticking ahead 4 hours using `cy.tick(...)`
    3. ???
    
    ### Problems/Observations:
    1. ~backend handles getting fee/expirationDate, meaning it doesn't care what the date on frontend is, meaning testing this is hard [see here](https://github.com/gnosis/gp-v2-services/blob/537ca1856d270b698ca6c7950265198d85c009c7/orderbook/src/api/get_fee_info.rs#L28)~
    2. it _does_ indeed speed up time by overwriting all `Date` function calls (logging inside `fee/updater.tsx` confirms this) so the backened calls the API and updates the state (local, redux) with the new fee. That fee's `expirationDate` however, is based on the code in the link above (it's currently set to a few minutes from the backends own Date.now() call)

--- cypress-custom/integration/fee.test.ts
+++ cypress-custom/integration/fee.test.ts
@@ -1,10 +1,11 @@
 import { ChainId, WETH } from '@uniswap/sdk'
-import { FeeInformation } from '../../src/custom/state/fee/reducer'
+import { FeeInformation, FeeInformationObject } from '../../src/custom/state/fee/reducer'
 
 const DAI = '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735'
 const RINKEBY = ChainId.RINKEBY.toString()
 const FEE_QUERY = `https://protocol-rinkeby.dev.gnosisdev.com/api/v1/tokens/${WETH[4].address}/fee`
 const FEE_QUOTES_LOCAL_STORAGE_KEY = 'redux_localstorage_simple_fee'
+const FOUR_HOURS = 3600 * 4 * 1000
 
 function _assertFeeData(fee: FeeInformation): void {
   expect(fee).to.have.property('minimalFee')
@@ -16,20 +17,31 @@ function _getLocalStorage(): Cypress.Chainable<Storage> {
   return cy.window().then(window => window.localStorage)
 }
 
-function _assertFeeFetched(token: string): void {
-  _getLocalStorage()
-    .its(FEE_QUOTES_LOCAL_STORAGE_KEY)
-    .should(feeQuotesStorage => {
-      // THEN: There is fee information for Rinkeby and token
-      const feeQuoteData = JSON.parse(feeQuotesStorage)
-      expect(feeQuoteData).to.exist
-      expect(feeQuoteData).to.have.property(RINKEBY)
-      expect(feeQuoteData[RINKEBY]).to.have.property(token)
+function _getChainFeeStorage(
+  networkKey: string,
+  token?: string
+): Cypress.Chainable<{ [t: string]: FeeInformationObject }> {
+  return (
+    _getLocalStorage()
+      .its(FEE_QUOTES_LOCAL_STORAGE_KEY)
+      // To properly return this we need .should and an expectation
+      .should(feeQuotesStorage => {
+        expect(JSON.parse(feeQuotesStorage)).to.have.property(networkKey)
+        token && expect(JSON.parse(feeQuotesStorage)[RINKEBY]).to.have.property(token)
+      })
+      .then(fee => JSON.parse(fee)[networkKey])
+  )
+}
 
-      // THEN: The quote has the expected information
-      const fee = feeQuoteData[RINKEBY][token].fee
-      _assertFeeData(fee)
-    })
+function _assertFeeFetched(token: string): Cypress.Chainable {
+  return _getChainFeeStorage(RINKEBY, token).then(feeQuoteData => {
+    expect(feeQuoteData).to.exist
+    expect(feeQuoteData).to.have.property(token)
+
+    // THEN: The quote has the expected information
+    const fee = feeQuoteData[token].fee
+    _assertFeeData(fee)
+  })
 }
 
 describe('Fee endpoint', () => {
@@ -43,77 +55,65 @@ describe('Fee endpoint', () => {
   })
 })
 
-describe('Fetch and persist fee', () => {
-  beforeEach(() => {
-    cy.visit('/swap')
-  })
+describe('Fee: Complex fetch and persist fee', () => {
+  // Needs to run first to pass because of Cypress async issues between tests
+  it('Re-fetched when it expires', () => {
+    // GIVEN: input token Fee expiration is always 6 hours from now
+    const SIX_HOURS = FOUR_HOURS * 1.5
+    const LATER_TIME = new Date(Date.now() + SIX_HOURS).toISOString()
+    const LATER_FEE = {
+      expirationDate: LATER_TIME,
+      minimalFee: '0',
+      feeRatio: 0
+    }
 
+    // set the Cypress clock to now (default is UNIX 0)
+    // only override Date functions (default is to override all time based functions)
+    cy.clock(Date.now(), ['Date'])
+    cy.stubResponse({ url: FEE_QUERY, alias: 'feeRequest', body: LATER_FEE })
+
+    // GIVEN: user visits app and goes AFK
+    cy.visit('/swap')
+
+    // WHEN: The user comes back 4h later (so the fee quote is expired) (after a long sandwich eating sesh, dude was hungry)
+    cy.tick(FOUR_HOURS).then($clock => {
+      // Stub the API fee response to return LATER_FEE
+
+      // THEN: a new fee request is made AHEAD of current (advanced) time
+      cy.wait('@feeRequest')
+        .its('response.body')
+        .then($body => {
+          const body = JSON.parse($body)
+          // @ts-expect-error - cypress untyped method
+          const mockedTime = new Date($clock.details().now)
+
+          // THEN: fee time is properly stubbed and
+          expect(body.expirationDate).to.equal(LATER_TIME)
+          // THEN: the mocked later date is indeed less than the new fee (read: the fee is valid)
+          expect(new Date(body.expirationDate)).to.be.greaterThan(mockedTime)
+        })
+    })
+  })
+})
+
+describe('Fee: simple checks it exists', () => {
+  beforeEach(() => {
+    cy.visit('/app')
+  })
   it('Fetch fee automatically on load', () => {
-    // GIVEN: An user loads the swap page
+    // GIVEN: A user loads the swap page
     // WHEN: He does nothing
     // THEN: The fee for ETH is fetched
     _assertFeeFetched('ETH')
   })
 
   it('Fetch fee when selecting token', () => {
-    // GIVEN: Clean local storage
-    cy.clearLocalStorage()
-
+    // GIVEN: A user loads the swap page
     // WHEN: Select DAI token
     cy.swapSelectInput(DAI)
 
     // THEN: The fee for DAI is fetched
     _assertFeeFetched(DAI)
-  })
-
-  // TODO: not sure if it's easy to test this
-  it('Re-fetched when it expires', () => {
-    const NETWORK = ChainId.RINKEBY.toString()
-    const TOKEN = 'ETH'
-    const VALUE = JSON.stringify({
-      [NETWORK]: {
-        [TOKEN]: {
-          fee: {
-            minimalFee: '777',
-            feeRatio: 777,
-            expirationDate: new Date(Date.now() - 200000).toISOString()
-          },
-          token: TOKEN
-        }
-      }
-    })
-    // GIVEN: A fee is present in the local storage
-    // Set expiring fee in localStorage
-    _getLocalStorage()
-      .then($storage => {
-        // set time in the past
-        $storage.setItem(FEE_QUOTES_LOCAL_STORAGE_KEY, VALUE)
-      })
-      .its(FEE_QUOTES_LOCAL_STORAGE_KEY)
-      // Keep retrying until localStorage is populated from app
-      .should($feeStorage => expect($feeStorage).to.have.property(NETWORK))
-      // Check that fee is currently in the PAST
-      .then($feeStorage => {
-        // we need to parse JSON
-        const feeStorage = JSON.parse($feeStorage)
-        const NOW = new Date()
-        const feeExpirationDate = new Date(feeStorage[NETWORK].ETH.fee.expirationDate)
-
-        // Here we expect the current saved expirationDate to be in the PAST
-        expect(NOW).to.be.greaterThan(feeExpirationDate)
-      })
-      // WHEN: When the fee quote expires, we refetch the fee
-      // better imo than using cy.wait - clear fee storage
-      .then(() => _getLocalStorage().then($storage => $storage.removeItem(FEE_QUOTES_LOCAL_STORAGE_KEY)))
-      .its(FEE_QUOTES_LOCAL_STORAGE_KEY)
-      // THEN: We get another quote
-      .should($feeStorage => {
-        const feeStorage = JSON.parse($feeStorage)
-
-        const NOW = new Date()
-        const feeExpirationDate = new Date(feeStorage[NETWORK].ETH.fee.expirationDate)
-        expect(feeExpirationDate).to.be.greaterThan(NOW)
-      })
   })
 })
 
--- cypress-custom/support/commands.js
+++ cypress-custom/support/commands.js
@@ -3,3 +3,15 @@ Cypress.Commands.add('swapSelectInput', tokenAddress => {
   cy.get('.token-item-' + tokenAddress).should('be.visible')
   cy.get('.token-item-' + tokenAddress).click({ force: true })
 })
+
+function _responseHandlerFactory(body) {
+  return req =>
+    req.reply(res => {
+      const newBody = JSON.stringify(body || res.body)
+      res.body = newBody
+    })
+}
+
+Cypress.Commands.add('stubResponse', ({ url, alias = 'stubbedResponse', body }) => {
+  cy.route2({ method: 'GET', url }, _responseHandlerFactory(body)).as(alias)
+})
