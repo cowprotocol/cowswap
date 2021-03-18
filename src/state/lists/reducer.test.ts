86c7c34f4bd950f8f9a331ebde0f9e678e6230a3
David Sato <david.sato64@gmail.com>
2021-03-18 17:51:13 +0000

    fix import in base list reducer test

--- src/state/lists/reducer.test.ts
+++ src/state/lists/reducer.test.ts
@@ -1,6 +1,6 @@
 import { DEFAULT_ACTIVE_LIST_URLS } from './../../constants/lists'
 import { createStore, Store } from 'redux'
-import { DEFAULT_LIST_OF_LISTS } from 'constants/lists'
+import { DEFAULT_LIST_OF_LISTS } from '@src/constants/lists'
 import { updateVersion } from '../global/actions'
 import { fetchTokenList, acceptListUpdate, addList, removeList, enableList } from './actions'
 import reducer, { ListsState } from './reducer'
