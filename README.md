466138921c5a1f27ca2b23cedbbe59782dba2166
Anxo Rodriguez <anxolin@gmail.com>
2021-04-16 19:53:05 +0200

    Add multi-network support (#435)
    * Add multi-network support
    
    * --amend
    
    * Remove duplicated var
    
    * Move env to the body
    
    * Simplify checks
    
    * Cow swap for waller link
    
    * Revert original test

--- README.md
+++ README.md
@@ -68,9 +68,15 @@ yarn cypress
 To have the interface default to a different network when a wallet is not connected:
 
 1. Make a copy of `.env` named `.env.local`
-2. Change `REACT_APP_NETWORK_ID` to `"{YOUR_NETWORK_ID}"`
-3. Change `REACT_APP_NETWORK_URL` to e.g. `"https://{YOUR_NETWORK_ID}.infura.io/v3/{YOUR_INFURA_KEY}"`
-4. Change `REACT_APP_ID` Ask for your id in [chat.gnosis.io](https://chat.gnosis.io)
+2. Change `REACT_APP_NETWORK_ID` to `"{YOUR_NETWORK_ID}"`. This will be your default network id
+3. Define your own list of supported networks:
+```ini
+REACT_APP_SUPPORTED_CHAIN_IDS="1,4,100"
+REACT_APP_NETWORK_URL_1=https://mainnet.infura.io/v3/{YOUR_INFURA_KEY}
+REACT_APP_NETWORK_URL_4=https://rinkeby.infura.io/v3/{YOUR_INFURA_KEY}
+REACT_APP_NETWORK_URL_100=https://rpc.xdaichain.com
+``` 
+4. Change `REACT_APP_ID` Ask for your id at [chat.gnosis.io](https://chat.gnosis.io)
 5. Change `REACT_APP_API_BASE_URL_{XDAI|RINKEBY|MAINNET}` to e.g. `"http://localhost:8080/api/v1"` when running the services locally.
 
 For production:
--- src/custom/connectors/index.ts
+++ src/custom/connectors/index.ts
@@ -1,16 +1,103 @@
-import { ChainId } from '@uniswap/sdk'
+import { Web3Provider } from '@ethersproject/providers'
 import { InjectedConnector } from '@web3-react/injected-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import { PortisConnector } from '@web3-react/portis-connector'
 
-export {
-  NETWORK_CHAIN_ID,
-  fortmatic,
-  portis,
-  network,
-  walletconnect,
-  walletlink,
-  getNetworkLibrary
-} from '@src/connectors'
+import { FortmaticConnector } from 'connectors/Fortmatic'
+import { NetworkConnector } from 'connectors/NetworkConnector'
 
-export const injected = new InjectedConnector({
-  supportedChainIds: [ChainId.MAINNET, ChainId.ROPSTEN, ChainId.RINKEBY, ChainId.GÖRLI, ChainId.KOVAN, ChainId.XDAI]
+type RpcNetworks = { [chainId: number]: string }
+
+function getRpcNetworks(): [RpcNetworks, number[]] {
+  const supportedChainIdsEnv = process.env.REACT_APP_SUPPORTED_CHAIN_IDS
+  const defaultChainId = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1')
+
+  // Make sure the mandatory envs are present
+  if (!supportedChainIdsEnv) {
+    throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
+  }
+
+  // Get list of supported chains
+  const chainIds = supportedChainIdsEnv.split(',').map(chainId => Number(chainId.trim()))
+  if (chainIds.length === 0) {
+    throw new Error(`At least one network should be supported. REACT_APP_CHAIN_ID`)
+  }
+
+  // Make sure the default chain is in the list of supported chains
+  if (!chainIds.includes(defaultChainId)) {
+    throw new Error(
+      `The default chain id (${defaultChainId}) must be part of the list of supported networks: ${chainIds.join(', ')}`
+    )
+  }
+
+  // Return rpc urls per network
+  const rpcNetworks = chainIds.reduce<RpcNetworks>((acc, chainId) => {
+    const url = process.env['REACT_APP_NETWORK_URL_' + chainId]
+
+    if (!url) {
+      throw new Error(
+        `Network ${chainId} is supported, however 'REACT_APP_NETWORK_URL_${chainId} environment variable was not defined`
+      )
+    }
+
+    acc[chainId] = url
+
+    return acc
+  }, {})
+
+  // Get chainIds (excluding the NETWORK_CHAIN_ID)
+  // Reason: By convention we will return NETWORK_CHAIN_ID as the first element in the supported networks
+  const otherChainIds = Object.keys(rpcNetworks)
+    .map(Number)
+    .filter(networkId => networkId !== defaultChainId)
+  const supportedChainIds = [defaultChainId, ...otherChainIds]
+
+  return [rpcNetworks, supportedChainIds]
+}
+
+const [rpcNetworks, supportedChainIds] = getRpcNetworks()
+export const NETWORK_CHAIN_ID = supportedChainIds[0]
+
+export const network = new NetworkConnector({
+  urls: rpcNetworks,
+  defaultChainId: NETWORK_CHAIN_ID
+})
+
+let networkLibrary: Web3Provider | undefined
+export function getNetworkLibrary(): Web3Provider {
+  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
+}
+
+export const injected = new InjectedConnector({ supportedChainIds })
+
+// mainnet only
+export const walletconnect = new WalletConnectConnector({
+  // TODO: Use any network when this PR is merged https://github.com/NoahZinsmeister/web3-react/pull/185
+  // rpc: rpcNetworks,
+  rpc: { 1: rpcNetworks[NETWORK_CHAIN_ID] },
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+  pollingInterval: 15000
+})
+
+// mainnet only
+export const fortmatic = new FortmaticConnector({
+  apiKey: process.env.REACT_APP_FORTMATIC_KEY ?? '',
+  chainId: NETWORK_CHAIN_ID
+})
+
+// mainnet only
+export const portis = new PortisConnector({
+  dAppId: process.env.REACT_APP_PORTIS_ID ?? '',
+  // TODO: Allow to configure multiple networks in portis
+  // networks: supportedChainIds
+  networks: [NETWORK_CHAIN_ID]
+})
+
+// mainnet only
+export const walletlink = new WalletLinkConnector({
+  url: rpcNetworks[NETWORK_CHAIN_ID],
+  appName: 'Cow Swap',
+  appLogoUrl: 'https://raw.githubusercontent.com/gnosis/gp-swap-ui/develop/public/images/logo-square-512.png'
 })
