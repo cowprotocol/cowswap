ac263d58f0ab14b676bede725829e01af0724186
David <david.sato64@gmail.com>
2021-04-23 11:26:10 +0100

    Fix cypress swap with new ETH flow (#501)
    * fix cypress swap with new ETH flow
    
    * add biggger delay on buy token selection

--- cypress-custom/integration/swap.test.ts
+++ cypress-custom/integration/swap.test.ts
@@ -22,8 +22,8 @@ describe('Swap (custom)', () => {
     cy.get('#confirm-swap-or-send').should('contain', 'Confirm Swap')
   })
 
-  // ETH should NOT be tradable
-  it('cannot swap ETH for DAI', () => {
+  // ETH should be tradable but show Switch to Weth
+  it('Swap ETH for DAI - shows Switch to WETH ', () => {
     // select ETH
     cy.get('#swap-currency-input .open-currency-select-button').click()
     cy.get('.token-item-ETHER').should('be.visible')
@@ -34,10 +34,8 @@ describe('Swap (custom)', () => {
     cy.get('.token-item-0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735').click({ force: true })
     // input amounts
     cy.get('#swap-currency-input .token-amount-input').should('be.visible')
-    cy.get('#swap-currency-input .token-amount-input').type('0.001', { force: true, delay: 200 })
+    cy.get('#swap-currency-input .token-amount-input').type('0.001', { force: true, delay: 400 })
     cy.get('#swap-currency-output .token-amount-input').should('not.equal', '')
-    cy.get('#swap-button')
-      .should('be.disabled')
-      .should('contain', 'ETH cannot be traded. Use WETH')
+    cy.get('#swap-button').should('contain', 'Switch to WETH')
   })
 })
