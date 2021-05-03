553990a42729bcd635a82a4bb1844c5844ac772d
Anxo Rodriguez <anxolin@gmail.com>
2021-05-03 17:26:20 +0200

    Disable coingecko

--- src/custom/constants/lists.ts
+++ src/custom/constants/lists.ts
@@ -13,7 +13,7 @@ const WRAPPED_LIST = 'wrapped.tokensoft.eth'
 const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
 const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
-const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
+// const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const CMC_ALL_LIST = 'defi.cmc.eth'
 const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const KLEROS_LIST = 't2crtokens.eth'
@@ -56,7 +56,7 @@ export const DEFAULT_LIST_OF_LISTS_BY_NETWORK: NetworkLists = {
       SET_LIST,
       OPYN_LIST,
       ROLL_LIST,
-      COINGECKO_LIST,
+      // COINGECKO_LIST,
       CMC_ALL_LIST,
       CMC_STABLECOIN,
       KLEROS_LIST,
