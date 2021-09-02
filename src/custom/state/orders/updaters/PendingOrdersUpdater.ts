d6321c958a78c425e24d11e52dcfc38923748de7
Anxo Rodriguez <anxolin@gmail.com>
2021-09-02 20:07:21 +0200

    Update updaters

--- src/custom/state/orders/updaters/PendingOrdersUpdater.ts
+++ src/custom/state/orders/updaters/PendingOrdersUpdater.ts
@@ -2,9 +2,15 @@ import { useCallback, useEffect, useRef } from 'react'
 
 import { useActiveWeb3React } from 'hooks/web3'
 
-import { useCancelOrdersBatch, useExpireOrdersBatch, useFulfillOrdersBatch, usePendingOrders } from 'state/orders/hooks'
-import { ApiOrderStatus } from 'state/orders/utils'
-import { OrderFulfillmentData } from 'state/orders/actions'
+import {
+  useCancelOrdersBatch,
+  useExpireOrdersBatch,
+  useFulfillOrdersBatch,
+  useMarkOrdersAsPresigned,
+  usePendingOrders,
+} from 'state/orders/hooks'
+import { OrderTransitionStatus } from 'state/orders/utils'
+import { Order, OrderFulfillmentData, OrderStatus } from 'state/orders/actions'
 import { OPERATOR_API_POLL_INTERVAL } from 'state/orders/consts'
 
 import { SupportedChainId as ChainId } from 'constants/chains'
@@ -12,6 +18,24 @@ import { SupportedChainId as ChainId } from 'constants/chains'
 import { OrderID } from 'api/gnosisProtocol'
 
 import { fetchOrderPopupData, OrderLogPopupMixData } from 'state/orders/updaters/utils'
+
+/**
+ * Return the ids od the orders that we are not yet aware that are signed.
+ * This is, pre-sign orders, in state of "PRESIGNATURE_PENDING", for which we now know they are signed
+ *
+ * Used as an auxiliar method to detect which orders we should mark as pre-signed, so we change their state
+ *
+ * @param allPendingOrders All pending orders
+ * @param signedOrdersIds ids of orders we know are already pre-signed
+ * @returns ids of the pending orders that were pending for pre-sign, and we now know are pre-signed
+ */
+function _getPresignedOrdersStillWaiting(allPendingOrders: Order[], signedOrdersIds: OrderID[]) {
+  const presignaturePendingIds = allPendingOrders
+    .filter((order) => order.status === OrderStatus.PRESIGNATURE_PENDING)
+    .map((order) => order.id)
+
+  return signedOrdersIds.filter((order: OrderID) => presignaturePendingIds.includes(order))
+}
 
 export function PendingOrdersUpdater(): null {
   const { chainId } = useActiveWeb3React()
@@ -25,6 +49,7 @@ export function PendingOrdersUpdater(): null {
   const fulfillOrdersBatch = useFulfillOrdersBatch()
   const expireOrdersBatch = useExpireOrdersBatch()
   const cancelOrdersBatch = useCancelOrdersBatch()
+  const markOrdersAsPresigned = useMarkOrdersAsPresigned()
 
   const updateOrders = useCallback(
     async (chainId: ChainId) => {
@@ -33,42 +58,59 @@ export function PendingOrdersUpdater(): null {
         return
       }
 
-      // Iterate over pending orders fetching operator order data, async
+      // Iterate over pending orders fetching API data
       const unfilteredOrdersData = await Promise.all(
         pendingRef.current.map(async (orderFromStore) => fetchOrderPopupData(orderFromStore, chainId))
       )
 
       // Group resolved promises by status
       // Only pick the status that are final
-      const { fulfilled, expired, cancelled } = unfilteredOrdersData.reduce<
-        Record<ApiOrderStatus, OrderLogPopupMixData[]>
+      const { fulfilled, expired, cancelled, presigned } = unfilteredOrdersData.reduce<
+        Record<OrderTransitionStatus, OrderLogPopupMixData[]>
       >(
         (acc, { status, popupData }) => {
           popupData && acc[status].push(popupData)
           return acc
         },
-        { fulfilled: [], expired: [], cancelled: [], unknown: [], pending: [] }
+        { fulfilled: [], expired: [], cancelled: [], unknown: [], presigned: [], pending: [] }
       )
 
-      // Bach state update per group, if any
+      if (presigned.length > 0) {
+        // Only mark as presigned the orders we were not aware of their new state
+        const presignedOrderIds = presigned as OrderID[]
+        const ordersPresignaturePendingSigned = _getPresignedOrdersStillWaiting(pendingRef.current, presignedOrderIds)
+        console.log({ presignedOrderIds, ordersPresignaturePendingSigned })
 
-      fulfilled.length > 0 &&
-        fulfillOrdersBatch({
-          ordersData: fulfilled as OrderFulfillmentData[],
-          chainId,
-        })
-      expired.length > 0 &&
+        if (ordersPresignaturePendingSigned.length > 0) {
+          markOrdersAsPresigned({
+            ids: ordersPresignaturePendingSigned,
+            chainId,
+          })
+        }
+      }
+
+      if (expired.length > 0) {
         expireOrdersBatch({
           ids: expired as OrderID[],
           chainId,
         })
-      cancelled.length > 0 &&
+      }
+
+      if (cancelled.length > 0) {
         cancelOrdersBatch({
           ids: cancelled as OrderID[],
           chainId,
         })
+      }
+
+      if (fulfilled.length > 0) {
+        fulfillOrdersBatch({
+          ordersData: fulfilled as OrderFulfillmentData[],
+          chainId,
+        })
+      }
     },
-    [cancelOrdersBatch, expireOrdersBatch, fulfillOrdersBatch]
+    [cancelOrdersBatch, expireOrdersBatch, fulfillOrdersBatch, markOrdersAsPresigned]
   )
 
   useEffect(() => {
