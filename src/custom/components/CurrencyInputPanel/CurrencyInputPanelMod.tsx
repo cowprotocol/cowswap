18943ad11b041806b56dc85809a6007b3cfda128
Michel <31534717+biocom@users.noreply.github.com>
2021-09-06 17:04:35 +0200

    Default cursor for USD est./price impact. (#1361)
    * Default cursor for USD est./price impact.
    
    * Fix mouse cursor/max button state.
    
    * Remove unused type.

--- src/custom/components/CurrencyInputPanel/CurrencyInputPanelMod.tsx
+++ src/custom/components/CurrencyInputPanel/CurrencyInputPanelMod.tsx
@@ -270,7 +270,11 @@ export default function CurrencyInputPanel({
                       color={theme.text1}
                       fontWeight={400}
                       fontSize={14}
-                      style={{ display: 'inline', cursor: 'pointer' }}
+                      style={{
+                        display: 'inline',
+                        // cursor: 'pointer'
+                        cursor: showMaxButton ? 'pointer' : 'initial', // mod
+                      }}
                       title={`${formatMax(selectedCurrencyBalance, currency?.decimals) || '-'} ${
                         currency?.symbol || ''
                       }`}
