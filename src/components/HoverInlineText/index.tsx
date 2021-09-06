30b582b11e6e6d0a452d6156dc01cbe69e0f6615
Michel <31534717+biocom@users.noreply.github.com>
2021-09-06 17:04:35 +0200

    Default cursor for USD est./price impact. (#1361)
    * Default cursor for USD est./price impact.
    
    * Fix mouse cursor/max button state.
    
    * Remove unused type.

--- src/components/HoverInlineText/index.tsx
+++ src/components/HoverInlineText/index.tsx
@@ -2,7 +2,7 @@ import Tooltip from '@src/components/Tooltip'
 import React, { useState } from 'react'
 import styled from 'styled-components/macro'
 
-const TextWrapper = styled.span<{ margin: boolean; link?: boolean; fontSize?: string; adjustSize?: boolean }>`
+export const TextWrapper = styled.span<{ margin: boolean; link?: boolean; fontSize?: string; adjustSize?: boolean }>`
   margin-left: ${({ margin }) => margin && '4px'};
   color: ${({ theme, link }) => (link ? theme.blue1 : theme.text1)};
   font-size: ${({ fontSize }) => fontSize ?? 'inherit'};
