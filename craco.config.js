c9bd4687e56c05db56d9335989c34c4274cf6e3e
Nenad Vracar <34926005+nenadV91@users.noreply.github.com>
2021-09-08 07:25:08 +0200

    added sentry-webpack plugin for source maps (#1371)
    # Summary
    
    This will provide source maps support in sentry console which will make easier to debug errors in production.
    
    ![Screenshot from 2021-09-01 13-56-37](https://user-images.githubusercontent.com/34926005/131670516-1d3644ea-8ce5-4a55-9dfd-7b4e127ad6c5.png)
    
    
     # To Test
    
    - source maps support should be visible in the sentry console after the deployment of source code with this valid sentry authToken
    
    
     # Background
    
    - the sentry-webpack-plugin will only be added if the REACT_APP_SENTRY_AUTH_TOKEN is defined as a env variable. The value of this variable is the authToken provided by sentry.

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,30 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+const { version } = require('./package.json')
 
 // see https://github.com/gsoft-inc/craco/blob/master/packages/craco/README.md#configuration-overview
+
+const plugins = []
+const SENTRY_AUTH_TOKEN = process.env.SENTRY_AUTH_TOKEN
+const SENTRY_RELEASE_VERSION = 'CowSwap@' + version
+
+if (SENTRY_AUTH_TOKEN) {
+  plugins.push(
+    new SentryWebpackPlugin({
+      // sentry-cli configuration - can also be done directly through sentry-cli
+      // see https://docs.sentry.io/product/cli/configuration/ for details
+      authToken: SENTRY_AUTH_TOKEN,
+      org: 'gnosis-protocol',
+      project: 'cowswap',
+      release: SENTRY_RELEASE_VERSION,
+
+      // other SentryWebpackPlugin configuration
+      include: '.',
+      ignore: ['node_modules', 'webpack.config.js'],
+    })
+  )
+}
 
 module.exports = function () {
   return {
@@ -9,11 +32,10 @@ module.exports = function () {
       plugins: ['@babel/plugin-proposal-nullish-coalescing-operator'],
     },
     webpack: {
+      plugins,
       alias: {
         '@src': path.resolve(__dirname, 'src'),
       },
-      plugins: [],
-
       // https://webpack.js.org/configuration
       configure: (webpackConfig) => ({
         ...webpackConfig,
