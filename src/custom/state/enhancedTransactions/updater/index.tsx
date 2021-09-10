da7ac4e69aadda8411c35929a62d6da4bf56d549
Anxo Rodriguez <anxolin@gmail.com>
2021-09-10 16:32:41 +0200

    Add

--- src/custom/state/enhancedTransactions/updater/index.tsx
+++ src/custom/state/enhancedTransactions/updater/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import FinalizeTxUpdater from './FinalizeTxUpdater'
+import GnosisSafeTxUpdater from './GnosisSafeTxUpdater'
 import CancelReplaceTxUpdater from './CancelReplaceTxUpdater'
 
 export default function Updater() {
   return (
     <>
       <FinalizeTxUpdater />
+      <GnosisSafeTxUpdater />
       <CancelReplaceTxUpdater />
     </>
   )
