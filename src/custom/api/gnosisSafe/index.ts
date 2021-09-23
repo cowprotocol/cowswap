0522c24aaa191e04451930024490b5e2099a68a6
Anxo Rodriguez <anxolin@gmail.com>
2021-09-23 11:17:47 +0200

    [presign] Detect safe wallet, and load its data (#1418)
    * Add safe client
    
    * Add API
    
    * Detect gnosis safe wallet, fetch info
    
    * Show wallet info in cowgame

---
+++ src/custom/api/gnosisSafe/index.ts
@@ -0,0 +1,52 @@
+import SafeServiceClient, { SafeInfoResponse, SafeMultisigTransactionResponse } from '@gnosis.pm/safe-service-client'
+import { registerOnWindow } from '@src/custom/utils/misc'
+import { ChainId } from '@uniswap/sdk'
+
+const SAFE_TRANSACTION_SERVICE_URL: Partial<Record<ChainId, string>> = {
+  [ChainId.MAINNET]: 'https://safe-transaction.gnosis.io',
+  [ChainId.RINKEBY]: 'https://safe-transaction.rinkeby.gnosis.io',
+  [ChainId.XDAI]: 'https://safe-transaction.xdai.gnosis.io',
+}
+
+const SAFE_TRANSACTION_SERVICE_CACHE: Partial<Record<ChainId, SafeServiceClient | null>> = {}
+
+function _getClient(chainId: ChainId): SafeServiceClient | null {
+  let client = SAFE_TRANSACTION_SERVICE_CACHE[chainId]
+  if (client === undefined) {
+    const url = SAFE_TRANSACTION_SERVICE_URL[chainId]
+    if (!url) {
+      client = null
+    } else {
+      client = new SafeServiceClient(url)
+    }
+
+    // Add client to cache (or null if unknonw network)
+    SAFE_TRANSACTION_SERVICE_CACHE[chainId] = client
+  }
+
+  return client
+}
+
+function _getClientOrThrow(chainId: ChainId): SafeServiceClient {
+  const client = _getClient(chainId)
+  if (!client) {
+    throw new Error('Unsupported network for Gnosis Safe Transaction Service: ' + chainId)
+  }
+
+  return client
+}
+
+export function getSafeTransaction(chainId: ChainId, safeTxHash: string): Promise<SafeMultisigTransactionResponse> {
+  console.log('[api/gnosisSafe] getSafeTransaction', chainId, safeTxHash)
+  const client = _getClientOrThrow(chainId)
+  return client.getTransaction(safeTxHash)
+}
+
+export function getSafeInfo(chainId: ChainId, safeAddress: string): Promise<SafeInfoResponse> {
+  console.log('[api/gnosisSafe] getSafeInfo', chainId, safeAddress)
+  const client = _getClientOrThrow(chainId)
+
+  return client.getSafeInfo(safeAddress)
+}
+
+registerOnWindow({ getSafeTransaction, getSafeInfo })
