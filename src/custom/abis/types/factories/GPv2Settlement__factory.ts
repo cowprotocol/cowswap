8922b4040d9d9f350ab501c2cc0dd9db58570d17
Lint Action <lint-action@samuelmeuli.com>
2021-10-07 18:25:19 +0000

    Fix code style issues with Prettier

--- src/custom/abis/types/factories/GPv2Settlement__factory.ts
+++ src/custom/abis/types/factories/GPv2Settlement__factory.ts
@@ -2,12 +2,9 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import { Contract, Signer, utils } from "ethers";
-import { Provider } from "@ethersproject/providers";
-import type {
-  GPv2Settlement,
-  GPv2SettlementInterface,
-} from "../GPv2Settlement";
+import { Contract, Signer, utils } from 'ethers'
+import { Provider } from '@ethersproject/providers'
+import type { GPv2Settlement, GPv2SettlementInterface } from '../GPv2Settlement'
 
 const _abi = [
   {
@@ -15,79 +12,76 @@ const _abi = [
     inputs: [
       {
         indexed: true,
-        internalType: "address",
-        name: "owner",
-        type: "address",
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
       },
       {
         indexed: false,
-        internalType: "contract IERC20",
-        name: "sellToken",
-        type: "address",
+        internalType: 'contract IERC20',
+        name: 'sellToken',
+        type: 'address',
       },
       {
         indexed: false,
-        internalType: "contract IERC20",
-        name: "buyToken",
-        type: "address",
+        internalType: 'contract IERC20',
+        name: 'buyToken',
+        type: 'address',
       },
       {
         indexed: false,
-        internalType: "uint256",
-        name: "sellAmount",
-        type: "uint256",
+        internalType: 'uint256',
+        name: 'sellAmount',
+        type: 'uint256',
       },
       {
         indexed: false,
-        internalType: "uint256",
-        name: "buyAmount",
-        type: "uint256",
+        internalType: 'uint256',
+        name: 'buyAmount',
+        type: 'uint256',
       },
       {
         indexed: false,
-        internalType: "uint256",
-        name: "feeAmount",
-        type: "uint256",
+        internalType: 'uint256',
+        name: 'feeAmount',
+        type: 'uint256',
       },
       {
         indexed: false,
-        internalType: "bytes",
-        name: "orderUid",
-        type: "bytes",
+        internalType: 'bytes',
+        name: 'orderUid',
+        type: 'bytes',
       },
     ],
-    name: "Trade",
-    type: "event",
+    name: 'Trade',
+    type: 'event',
   },
   {
     inputs: [
       {
-        internalType: "bytes",
-        name: "orderUid",
-        type: "bytes",
+        internalType: 'bytes',
+        name: 'orderUid',
+        type: 'bytes',
       },
       {
-        internalType: "bool",
-        name: "signed",
-        type: "bool",
+        internalType: 'bool',
+        name: 'signed',
+        type: 'bool',
       },
     ],
-    name: "setPreSignature",
+    name: 'setPreSignature',
     outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    stateMutability: 'nonpayable',
+    type: 'function',
   },
-];
+]
 
 export class GPv2Settlement__factory {
-  static readonly abi = _abi;
+  static readonly abi = _abi
   static createInterface(): GPv2SettlementInterface {
-    return new utils.Interface(_abi) as GPv2SettlementInterface;
+    return new utils.Interface(_abi) as GPv2SettlementInterface
   }
-  static connect(
-    address: string,
-    signerOrProvider: Signer | Provider
-  ): GPv2Settlement {
-    return new Contract(address, _abi, signerOrProvider) as GPv2Settlement;
+  static connect(address: string, signerOrProvider: Signer | Provider): GPv2Settlement {
+    return new Contract(address, _abi, signerOrProvider) as GPv2Settlement
   }
 }
