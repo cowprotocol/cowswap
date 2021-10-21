b5574468d6a72ba6c4ef9f31ee6b66cb21e536af
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-10-21 15:51:52 -0700

    Merge branch 'do-not-update-orders-other-accounts' into updater-for-current-account

--- src/custom/state/orders/updaters/PendingOrdersUpdater.ts
+++ src/custom/state/orders/updaters/PendingOrdersUpdater.ts
@@ -86,6 +86,7 @@ async function _updatePresignGnosisSafeTx(
 }
 
 interface UpdateOrdersParams {
+  account: string
   chainId: ChainId
   orders: Order[]
 
@@ -99,6 +100,7 @@ interface UpdateOrdersParams {
 }
 
 async function _updateOrders({
+  account,
   chainId,
   orders,
 
@@ -110,14 +112,17 @@ async function _updateOrders({
   updatePresignGnosisSafeTx,
   getSafeInfo,
 }: UpdateOrdersParams): Promise<void> {
+  // Only check pending orders of current connected account
+  const pending = orders.filter((order) => order.owner === account)
+
   // Exit early when there are no pending orders
-  if (orders.length === 0) {
+  if (pending.length === 0) {
     return
   }
 
-  // Iterate over pending orders fetching API data
+  // Iterate over pending pending fetching API data
   const unfilteredOrdersData = await Promise.all(
-    orders.map(async (orderFromStore) => fetchOrderPopupData(orderFromStore, chainId))
+    pending.map(async (orderFromStore) => fetchOrderPopupData(orderFromStore, chainId))
   )
 
   // Group resolved promises by status
@@ -171,7 +176,7 @@ async function _updateOrders({
 }
 
 export function PendingOrdersUpdater(): null {
-  const { chainId } = useActiveWeb3React()
+  const { chainId, account } = useActiveWeb3React()
 
   const pending = usePendingOrders({ chainId })
 
@@ -187,8 +192,9 @@ export function PendingOrdersUpdater(): null {
   const getSafeInfo = useGetSafeInfo()
 
   const updateOrders = useCallback(
-    async (chainId: ChainId) =>
+    async (chainId: ChainId, account: string) =>
       _updateOrders({
+        account,
         chainId,
         orders: pendingRef.current,
         fulfillOrdersBatch,
@@ -202,14 +208,14 @@ export function PendingOrdersUpdater(): null {
   )
 
   useEffect(() => {
-    if (!chainId) {
+    if (!chainId || !account) {
       return
     }
 
-    const interval = setInterval(() => updateOrders(chainId), OPERATOR_API_POLL_INTERVAL)
+    const interval = setInterval(() => updateOrders(chainId, account), OPERATOR_API_POLL_INTERVAL)
 
     return () => clearInterval(interval)
-  }, [chainId, updateOrders])
+  }, [account, chainId, updateOrders])
 
   return null
 }
