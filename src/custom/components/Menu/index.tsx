c1921aaca03ca6db4701062a0f10da166085ddbc
Agustín Longoni <agustin.longoni@altoros.com>
2021-10-28 07:10:32 -0300

    [Affiliate] Add Profile page link (#1706)
    # Summary
    Fixes #1701
    Add Profile page link in mobile and desktop. Move About link to the secondary nav.
    Desktop:
    ![image](https://user-images.githubusercontent.com/622217/139112131-35df33e9-09cf-4b95-bfcb-b8f67782818e.png)
    Mobile: 
    ![image](https://user-images.githubusercontent.com/622217/139149209-239433b5-e853-4698-b331-ddba7f768a7f.png)
      # To Test
    
    1.  Open the [page `swap`](https://pr1706--gpswapui.review.gnosisdev.com/) (check if Profile link appears and works fine on the menu and sub menu)

--- src/custom/components/Menu/index.tsx
+++ src/custom/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import { Code, HelpCircle, BookOpen, PieChart, Moon, Sun, Repeat, Star } from 'react-feather'
+import { Code, HelpCircle, BookOpen, PieChart, Moon, Sun, Repeat, Star, User } from 'react-feather'
 
 import MenuMod, {
   MenuItem,
@@ -224,9 +224,14 @@ export function Menu({ darkMode, toggleDarkMode }: MenuProps) {
         <ResponsiveInternalMenuItem to="/" onClick={close}>
           <Repeat size={14} /> Swap
         </ResponsiveInternalMenuItem>
-        <ResponsiveInternalMenuItem to="/about" onClick={close}>
-          <Star size={14} /> About
+        <ResponsiveInternalMenuItem to="/profile" onClick={close}>
+          <User size={14} /> Profile
         </ResponsiveInternalMenuItem>
+
+        <InternalMenuItem to="/about" onClick={close}>
+          <Star size={14} />
+          About
+        </InternalMenuItem>
 
         <InternalMenuItem to="/faq" onClick={close}>
           <HelpCircle size={14} />
