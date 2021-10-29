a03bca907d804e239bb481f0c1763a11f4c23358
David W3stside <david.sato64@gmail.com>
2021-10-29 10:12:54 +0100

    useGetGpApiStatus hook (mocked)
    1. checks api to use
    2. mocked right now

---
+++ src/custom/hooks/useGetGpApiStatus.ts
@@ -0,0 +1,39 @@
+import ms from 'ms.macro'
+import { useState, useEffect } from 'react'
+
+type GpQuoteStatus = 'COWSWAP' | 'LEGACY'
+// TODO: use actual API call
+export async function checkGpQuoteApiStatus(): Promise<GpQuoteStatus> {
+  return new Promise((accept) => setTimeout(() => accept('COWSWAP'), 500))
+}
+const GP_QUOTE_STATUS_INTERVAL_TIME = ms`2 hours`
+
+export default function useCheckGpQuoteStatus(defaultApiToUse: GpQuoteStatus): GpQuoteStatus {
+  const [gpQuoteApiStatus, setGpQuoteApiStatus] = useState<GpQuoteStatus>(defaultApiToUse)
+
+  useEffect(() => {
+    console.debug('[useGetQuoteCallback::GP API Status]::', gpQuoteApiStatus)
+
+    const checkStatus = () => {
+      checkGpQuoteApiStatus()
+        .then(setGpQuoteApiStatus)
+        .catch((err: Error) => {
+          console.error('[useGetQuoteCallback::useEffect] Error getting GP quote status::', err)
+          // Fallback to LEGACY
+          setGpQuoteApiStatus('LEGACY')
+        })
+    }
+
+    // Create initial call on mount
+    checkStatus()
+
+    // set interval for GP_QUOTE_STATUS_INTERVAL_TIME (2 hours)
+    const intervalId = setInterval(() => {
+      checkStatus()
+    }, GP_QUOTE_STATUS_INTERVAL_TIME)
+
+    return () => clearInterval(intervalId)
+  }, [gpQuoteApiStatus])
+
+  return gpQuoteApiStatus
+}
