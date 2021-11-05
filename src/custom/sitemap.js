287c0768aaffd8f5609d1a317b0772cdc2ddca11
Anxo Rodriguez <anxolin@gmail.com>
2021-11-05 18:45:59 +0100

    Sitemap2 (#1796)
    * Add sitemap
    
    * Add sitemap instructions
    
    * Fix json
    
    * Fix code style issues with Prettier
    
    * Remove module
    
    * Fix code style issues with Prettier
    
    * Remove cookie policy
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

---
+++ src/custom/sitemap.js
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { renderSitemap } = require('simple-sitemap-renderer')
+const fs = require('fs')
+
+const OUTPUT_FILE = 'public/sitemap.xml'
+const PRODUCTION_URL = 'https://cowswap.exchange'
+
+const PAGES = [
+  'swap',
+  'about',
+  // 'profile',
+  'faq',
+  'play/cow-runner',
+  'play/mev-slicer',
+  'privacy-policy',
+  // 'cookie-policy',
+  'terms-and-conditions',
+]
+
+const entries = PAGES.map((name) => ({ url: `${PRODUCTION_URL}/#/${name}` }))
+
+const sitemap = renderSitemap(entries, {})
+
+console.log('Sitemap file generated in:', OUTPUT_FILE)
+fs.writeFileSync(OUTPUT_FILE, sitemap)
