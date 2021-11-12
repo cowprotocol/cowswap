c99877f1a73b52cba546df25a869d43c50a06343
Leandro Boscariol <alfetopito@users.noreply.github.com>
2021-11-12 03:20:27 -0800

    Do not update txs other accounts (#1797)
    * 1.4.4-rc.0
    
    * Cache issues (#1814)
    
    * Fix cache issues
    
    * Fix yarn command
    
    * Add worker messages
    
    * Fix code style issues with Prettier
    
    * Fix code style issues with Prettier
    
    * 1.4.4-cache.0
    
    * 1.4.4-cache.1
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    
    * 1.4.4
    
    * Only check for mined txs for the current connected account
    
    * Only check for tx speed ups/cancellations for current connected account
    
    * Update also `transactionHash` when replacing tx
    
    Co-authored-by: Anxo Rodriguez <anxolin@gmail.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: Leandro Boscariol <leandro.boscariol@gnosis.io>

--- src/custom/state/enhancedTransactions/updater/CancelReplaceTxUpdater.tsx
+++ src/custom/state/enhancedTransactions/updater/CancelReplaceTxUpdater.tsx
@@ -55,9 +55,10 @@ function unwatchTxChanges(pendingHashes: string[], chainId: number) {
 }
 
 export default function CancelReplaceTxUpdater(): null {
-  const { chainId, library } = useActiveWeb3React()
+  const { chainId, library, account } = useActiveWeb3React()
   const dispatch = useAppDispatch()
-  const pendingHashes = useAllTransactionHashes((tx) => !tx.receipt)
+  const accountLowerCase = account?.toLowerCase() || ''
+  const pendingHashes = useAllTransactionHashes((tx) => !tx.receipt && tx.from.toLowerCase() === accountLowerCase)
 
   useEffect(() => {
     if (!chainId || !library) return
