7f6716b5f329fa16d9e301d0d8026d93c926df31
Anxo Rodriguez <anxolin@gmail.com>
2021-11-15 10:00:48 +0000

    Render posts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Alert from '../components/Alerts'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.scss'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,6 +18,34 @@ export default function Home() {
           hi
         </Alert>
       </section>
+
+       {/* Add this <section> tag below the existing <section> tag */}
+       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps(){
+  // Get external data
+  const allPostsData = getSortedPostsData()
+
+  // 
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
