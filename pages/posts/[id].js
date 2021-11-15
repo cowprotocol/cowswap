5b1d0509caf4d56469d106b3d8ccf8b4f87a82c1
Anxo Rodriguez <anxolin@gmail.com>
2021-11-15 13:15:12 +0000

    Format markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,19 @@
+import Head from 'next/head'
+
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
+  const { id, title, date, contentHtml } = postData
   return (
     <Layout>
-      <pre>{ JSON.stringify(postData, null, 2) }</pre>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>CoW - { title }</title>
+      </Head>
+      <h2>{ title }</h2>
+      <em>{ date }</em>
+      {/* <pre>{ JSON.stringify(postData, null, 2) }</pre> */}
+      <div data-post-id={id} dangerouslySetInnerHTML={{ __html: contentHtml }} />
     </Layout>
   )
 }
@@ -23,7 +27,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
