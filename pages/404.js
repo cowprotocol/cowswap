5bb6a8ef462362957e9164c29e67893b95a8d43d
Anxo Rodriguez <anxolin@gmail.com>
2021-11-15 13:53:16 +0000

    Add 404

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout'
+
+// pages/404.js
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1>404 - Page Not Found</h1>
+    </Layout>
+  )
+}
