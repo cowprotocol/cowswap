85535a767c3bd557f602a01d0c8c7410eba19663
nenadV91 <nenadwebdevelop@gmail.com>
2021-12-15 20:17:15 +0100

    fix for xDai wrap/unwrap

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -36,7 +36,7 @@ import { V3Migrator } from 'types/v3/V3Migrator'
 import { getContract } from 'utils'
 
 import { ArgentWalletDetector, EnsPublicResolver, EnsRegistrar, Erc20, Weth } from '../abis/types'
-import { UNI, WETH9_EXTENDED } from '../constants/tokens'
+import { UNI, WETH9_EXTENDED } from 'constants/tokens'
 import { useActiveWeb3React } from './web3'
 
 // returns null on errors
