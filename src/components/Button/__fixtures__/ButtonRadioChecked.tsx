202a45d7a773cd197b07d53e9c9b4ce2ea2fcee0
nenadV91 <nenadwebdevelop@gmail.com>
2021-12-17 14:02:57 +0100

    added react-cosmos implementation and some examples

---
+++ src/components/Button/__fixtures__/ButtonRadioChecked.tsx
@@ -0,0 +1,7 @@
+// ButtonRadioChecked.fixture.jsx
+import { ButtonRadioChecked } from '../index'
+
+export default {
+  active: <ButtonRadioChecked active={true}>Active</ButtonRadioChecked>,
+  notActive: <ButtonRadioChecked active={false}>Not active</ButtonRadioChecked>,
+}
