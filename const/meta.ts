6c7a5c1be675e999d49244588d4735b49472e3da
Anxo Rodriguez <anxolin@gmail.com>
2022-01-07 13:22:18 +0100

    Last changes urls icon (#14)
    * Add metatags and logo
    
    * Update logo
    
    * Update API

--- const/meta.ts
+++ const/meta.ts
@@ -21,27 +21,3 @@ export const siteConfig = {
     forum: { label: 'Forum', url: 'https://forum.gnosis.io/' },
   }
 }
-
-export const meta = `
-  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
-
-  <meta name="description" content=${siteConfig.description}/>
-  <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
-  <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#163861" />
-
-  <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-  <link rel="apple-touch-icon" sizes="192x192" href="/images/logo-square-192.png" />
-  <link rel="apple-touch-icon" sizes="512x512" href="/images/logo-square-512.png" />
-
-  <meta property="og:type" content="website" />
-  <meta property="og:title" content=${siteConfig.title} />
-
-  <meta property="og:description" content=${siteConfig.description} />
-  <meta property="og:image" content=${siteConfig.url.root + "/images/og-meta-cowswap.png"} />
-  <meta property="og:url" content=${siteConfig.url.root} /> {/* TODO: Add URL */}
-
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content=${siteConfig.social.twitter.account} />
-  <meta name="twitter:title" content=${siteConfig.title} />
-  <meta name="twitter:image" content=${siteConfig.url.root + "/images/og-meta-cowswap.png"} />
-`
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppProps } from 'next/app'
 import GlobalStyles from 'const/styles/global'
 import Head from 'next/head'
-import { meta } from 'const/meta'
+
+import { siteConfig } from '@/const/meta'
 
 // import { i18n } from '@lingui/core'
 // import { I18nProvider } from "@lingui/react";
@@ -38,7 +39,27 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        {meta}
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
+
+        <meta name="description" content={siteConfig.description} />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#163861" />
+
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/images/logo-square-192.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/images/logo-square-512.png" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={siteConfig.title} />
+
+        <meta property="og:description" content={siteConfig.description} />
+        <meta property="og:image" content={siteConfig.url.root + "/images/og-meta-cowswap.png"} />
+        <meta property="og:url" content={siteConfig.url.root} /> {/* TODO: Add URL */}
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={siteConfig.social.twitter.account} />
+        <meta name="twitter:title" content={siteConfig.title} />
+        <meta name="twitter:image" content={siteConfig.url.root + "/images/og-meta-cowswap.png"} />
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"></meta>
       </Head>
       {/* <I18nProvider i18n={i18n}> */}
