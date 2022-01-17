767c86d6112c3bd9dd0fc467a6c853225c03125f
David W3stside <david.sato64@gmail.com>
2022-01-17 18:54:12 +0000

    edit the modded trade and claim approve hooks

--- src/custom/hooks/useApproveCallback/index.ts
+++ src/custom/hooks/useApproveCallback/index.ts
@@ -1,22 +1,30 @@
-import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
+import { Percent } from '@uniswap/sdk-core'
 import { useActiveWeb3React } from '@src/hooks/web3'
 import { Field } from '@src/state/swap/actions'
 import { computeSlippageAdjustedAmounts } from 'utils/prices'
 import { useMemo } from 'react'
 import { GP_VAULT_RELAYER, V_COW_CONTRACT_ADDRESS } from 'constants/index'
 import TradeGp from 'state/swap/TradeGp'
-import { ZERO_PERCENT } from 'constants/misc'
 
-import { useApproveCallback } from './useApproveCallbackMod'
+import { ApproveCallbackParams, useApproveCallback } from './useApproveCallbackMod'
 export { ApprovalState, useApproveCallback } from './useApproveCallbackMod'
 
+type ApproveCallbackFromTradeParams = Pick<
+  ApproveCallbackParams,
+  'openTransactionConfirmationModal' | 'closeModals' | 'amountToCheckAgainstAllowance'
+> & {
+  trade: TradeGp | undefined
+  allowedSlippage: Percent
+}
+
 // export function useApproveCallbackFromTrade(trade?: Trade, allowedSlippage = 0) {
-export function useApproveCallbackFromTrade(
-  openTransactionConfirmationModal: (message: string) => void,
-  closeModals: () => void,
-  trade?: TradeGp,
-  allowedSlippage = ZERO_PERCENT
-) {
+export function useApproveCallbackFromTrade({
+  openTransactionConfirmationModal,
+  closeModals,
+  trade,
+  allowedSlippage,
+  amountToCheckAgainstAllowance,
+}: ApproveCallbackFromTradeParams) {
   const { chainId } = useActiveWeb3React()
 
   const amountToApprove = useMemo(() => {
@@ -29,22 +37,37 @@ export function useApproveCallbackFromTrade(
 
   const vaultRelayer = chainId ? GP_VAULT_RELAYER[chainId] : undefined
 
-  return useApproveCallback(openTransactionConfirmationModal, closeModals, amountToApprove, vaultRelayer)
+  return useApproveCallback({
+    openTransactionConfirmationModal,
+    closeModals,
+    amountToApprove,
+    spender: vaultRelayer,
+    amountToCheckAgainstAllowance,
+  })
 }
 
 export type OptionalApproveCallbackParams = {
   transactionSummary: string
 }
 
-export function useApproveCallbackFromClaim(
-  openTransactionConfirmationModal: (message: string) => void,
-  closeModals: () => void,
-  amountToApprove: CurrencyAmount<Currency> | undefined
-) {
+type ApproveCallbackFromClaimParams = Omit<ApproveCallbackParams, 'spender'>
+
+export function useApproveCallbackFromClaim({
+  openTransactionConfirmationModal,
+  closeModals,
+  amountToApprove,
+  amountToCheckAgainstAllowance,
+}: ApproveCallbackFromClaimParams) {
   const { chainId } = useActiveWeb3React()
 
   const vCowContract = chainId ? V_COW_CONTRACT_ADDRESS[chainId] : undefined
 
   // Params: modal cbs, amountToApprove: token user is investing e.g, spender: vcow token contract
-  return useApproveCallback(openTransactionConfirmationModal, closeModals, amountToApprove, vCowContract)
+  return useApproveCallback({
+    openTransactionConfirmationModal,
+    closeModals,
+    amountToApprove,
+    spender: vCowContract,
+    amountToCheckAgainstAllowance,
+  })
 }
