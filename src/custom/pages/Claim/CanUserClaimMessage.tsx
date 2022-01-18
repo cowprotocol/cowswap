ba6ff22f1eee57bd3dcd58c59757977ba63c5ede
Anxo Rodriguez <anxolin@gmail.com>
2022-01-18 12:35:09 +0100

    Fix bad merge (#2184)

--- src/custom/pages/Claim/CanUserClaimMessage.tsx
+++ src/custom/pages/Claim/CanUserClaimMessage.tsx
@@ -3,7 +3,7 @@ import { ButtonSecondary } from 'components/Button'
 import { ExternalLink } from 'theme'
 import { IntroDescription } from './styled'
 import { ClaimCommonTypes } from './types'
-import { useAirdropDeadline, useClaimDispatchers, useClaimState } from 'state/claim/hooks'
+import { useClaimDispatchers, useClaimState, useClaimTimeInfo } from 'state/claim/hooks'
 import { ClaimStatus } from 'state/claim/actions'
 
 type ClaimIntroductionProps = Pick<ClaimCommonTypes, 'hasClaims'> & {
@@ -14,7 +14,16 @@ export default function CanUserClaimMessage({ hasClaims, isAirdropOnly }: ClaimI
   const { activeClaimAccount, claimStatus } = useClaimState()
   const { setActiveClaimAccount } = useClaimDispatchers()
 
-  const end = useAirdropDeadline()
+  const { airdropDeadline, deployment, investmentDeadline, isAirdropWindowOpen, isInvestmentWindowOpen } =
+    useClaimTimeInfo()
+  // console.log('Deadlines', {
+  //   airdropDeadline: airdropDeadline && new Date(airdropDeadline).toISOString(),
+  //   deployment: deployment && new Date(deployment).toISOString(),
+  //   investmentDeadline: investmentDeadline && new Date(investmentDeadline).toISOString(),
+
+  //   isAirdropWindowOpen,
+  //   isInvestmentWindowOpen,
+  // })
 
   // only show when active claim account
   if (!activeClaimAccount || claimStatus !== ClaimStatus.DEFAULT) return null
@@ -26,7 +35,7 @@ export default function CanUserClaimMessage({ hasClaims, isAirdropOnly }: ClaimI
           <Trans>
             Thank you for being a supporter of CowSwap and the CoW protocol. As an important member of the CowSwap
             Community you may claim vCOW to be used for voting and governance. You can claim your tokens until{' '}
-            <i>{end && new Date(end).toLocaleString()}</i>
+            <i>{airdropDeadline && new Date(airdropDeadline).toLocaleString()}</i>
             <ExternalLink href="https://cow.fi/">Read more about vCOW</ExternalLink>
           </Trans>
         </p>
