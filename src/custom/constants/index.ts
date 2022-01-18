50a0be11b5ceebc9c5981e495012b722648149f2
Anxo Rodriguez <anxolin@gmail.com>
2022-01-18 10:57:55 +0000

    Revert "Update address"
    This reverts commit 4a095a47df5a8042dd688278150f535393177ba8.

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -57,7 +57,7 @@ export const V_COW_CONTRACT_ADDRESS: Partial<Record<number, string>> = {
   // [ChainId.MAINNET]: GPv2Settlement[ChainId.MAINNET].address,
   // [ChainId.RINKEBY]: GPv2Settlement[ChainId.RINKEBY].address,
   // [ChainId.XDAI]: GPv2Settlement[ChainId.XDAI].address,
-  [ChainId.RINKEBY]: '0x135d7eaC6f3e1e66A92105b70705F82FA8f4736E',
+  [ChainId.RINKEBY]: '0x71A377EC0026A8E35AfD082d54E2f62d39AF075c',
 }
 
 // See https://github.com/gnosis/gp-v2-contracts/commit/821b5a8da213297b0f7f1d8b17c893c5627020af#diff-12bbbe13cd5cf42d639e34a39d8795021ba40d3ee1e1a8282df652eb161a11d6R13
