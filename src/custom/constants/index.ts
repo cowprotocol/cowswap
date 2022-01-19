9d57cf0e6a4668809ecab0247b80299e558a2137
Anxo Rodriguez <anxolin@gmail.com>
2022-01-19 19:47:14 +0000

    Redeploy

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -58,7 +58,7 @@ export const V_COW_CONTRACT_ADDRESS: Partial<Record<number, string>> = {
   // [ChainId.MAINNET]: GPv2Settlement[ChainId.MAINNET].address,
   // [ChainId.RINKEBY]: GPv2Settlement[ChainId.RINKEBY].address,
   // [ChainId.XDAI]: GPv2Settlement[ChainId.XDAI].address,
-  [ChainId.RINKEBY]: '0x4a75F89002b0F17Ee363E0805bc6F6cFf5b7E7f1',
+  [ChainId.RINKEBY]: '0x19Ab4728203C8315394447599BdE8fb0a03BEd36',
 }
 
 // See https://github.com/gnosis/gp-v2-contracts/commit/821b5a8da213297b0f7f1d8b17c893c5627020af#diff-12bbbe13cd5cf42d639e34a39d8795021ba40d3ee1e1a8282df652eb161a11d6R13
