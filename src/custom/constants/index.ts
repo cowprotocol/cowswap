861c588b6026273c45dea24cae7bf4c8e8ac2860
Anxo Rodriguez <anxolin@gmail.com>
2022-01-27 21:48:38 +0000

    Upload checksummed

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -51,9 +51,9 @@ export const GP_VAULT_RELAYER: Partial<Record<number, string>> = {
 }
 
 export const V_COW_CONTRACT_ADDRESS: Partial<Record<number, string>> = {
-  [ChainId.MAINNET]: '0x53877cd4111b4c041abea77cc501ed68c22630bd ',
-  [ChainId.XDAI]: '0x7a005ebd31f7c6ee13bf44b936952c3b308d53e0 ',
-  [ChainId.RINKEBY]: '0xa9ad5792a458a212b9e2f004e5663ef77aba6c11',
+  [ChainId.MAINNET]: '0x53877cD4111B4C041AbEa77CC501ed68C22630Bd',
+  [ChainId.XDAI]: '0x7a005eBd31f7c6Ee13Bf44b936952C3b308D53E0',
+  [ChainId.RINKEBY]: '0xA9aD5792A458A212B9e2F004e5663EF77ABA6c11',
 }
 
 // See https://github.com/gnosis/gp-v2-contracts/commit/821b5a8da213297b0f7f1d8b17c893c5627020af#diff-12bbbe13cd5cf42d639e34a39d8795021ba40d3ee1e1a8282df652eb161a11d6R13
