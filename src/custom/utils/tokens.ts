00b100dcb5ba2d3ad0f336789ed3ea052113c585
w3stside <david.sato64@gmail.com>
2022-02-24 09:10:02 +0000

    refactor

--- src/custom/utils/tokens.ts
+++ src/custom/utils/tokens.ts
@@ -2,9 +2,17 @@ import { SupportedChainId as ChainId } from 'constants/chains'
 import { GpEther as ETHER, WETH9_EXTENDED as WETH } from 'constants/tokens'
 import { NATIVE_CURRENCY_BUY_ADDRESS } from '../constants'
 
+export function isNativeAddress(tokenAddress: string, chainId: ChainId) {
+  if (tokenAddress === 'ETH' || tokenAddress === ETHER.onChain(chainId).symbol) {
+    return true
+  }
+
+  return false
+}
+
 export function toErc20Address(tokenAddress: string, chainId: ChainId): string {
   let checkedAddress = tokenAddress
-  if (tokenAddress === 'ETH' || tokenAddress === ETHER.onChain(chainId).symbol) {
+  if (isNativeAddress(tokenAddress, chainId)) {
     checkedAddress = WETH[chainId].address
   }
 
@@ -13,7 +21,7 @@ export function toErc20Address(tokenAddress: string, chainId: ChainId): string {
 
 export function toNativeBuyAddress(tokenAddress: string, chainId: ChainId): string {
   let checkedAddress = tokenAddress
-  if (tokenAddress === 'ETH' || tokenAddress === ETHER.onChain(chainId).symbol) {
+  if (isNativeAddress(tokenAddress, chainId)) {
     checkedAddress = NATIVE_CURRENCY_BUY_ADDRESS
   }
 
