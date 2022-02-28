670ecf383f6393d0b75d63abcad53fb753ec7b3c
biocom <michel@gnosis.pm>
2022-02-28 18:04:20 +0000

    Revert "Swap page layout (#2488)"
    This reverts commit 67d6294c7c6273abad530399ccf38d9b3c7ce94c.

--- src/custom/pages/App/index.tsx
+++ src/custom/pages/App/index.tsx
@@ -43,12 +43,11 @@ export const Wrapper = styled(AppMod)``
 
 export const BodyWrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   width: 100%;
-  padding-top: 10vh;
+  padding-top: 120px;
   align-items: center;
-  justify-content: center;
-  flex: auto;
+  flex: 1;
   overflow-y: auto;
   overflow-x: hidden;
   z-index: 1;
