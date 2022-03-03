3ab0f03c162d08a26addf48d6b6fd394aee0d13a
Agustín Longoni <agustin.longoni@altoros.com>
2022-03-03 16:09:15 -0300

    change layout css

--- src/custom/components/Footer/index.tsx
+++ src/custom/components/Footer/index.tsx
@@ -18,12 +18,8 @@ const Wrapper = styled.div`
   ${({ theme }) => theme.flexRowNoWrap}
   align-items: center;
   justify-content: space-evenly;
-  margin: auto 96px 0 32px;
+  margin: auto 16px;
   width: 100%;
-
-  ${({ theme }) => theme.mediaWidth.upToMedium`
-    margin: 0 auto 65px;
-  `}
 `
 
 const FooterWrapper = styled.div`
