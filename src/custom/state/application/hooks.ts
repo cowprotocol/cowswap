91a4fafefc00b8cb806ce2e4f742b5b9c94a4071
Leandro <leandro.boscariol@gnosis.io>
2022-03-04 14:51:25 -0800

    Re-adding on mod file state/application/hooks removed from original

--- src/custom/state/application/hooks.ts
+++ src/custom/state/application/hooks.ts
@@ -1,11 +1,24 @@
 import { createAction } from '@reduxjs/toolkit'
-
-import { ApplicationModal } from 'state/application/reducer'
 import { useToggleModal } from '@src/state/application/hooks'
+import { useCallback } from 'react'
+import { ApplicationModal } from 'state/application/reducer'
+import { useAppDispatch } from 'state/hooks'
+
 export * from '@src/state/application/hooks'
 
 export const setOpenModal = createAction<ApplicationModal | null>('application/setOpenModal')
 
 export function useToggleTransactionConfirmation(): () => void {
   return useToggleModal(ApplicationModal.TRANSACTION_CONFIRMATION)
+}
+
+// TODO: These two seem to be gone from original. Check whether they have been replaced
+export function useOpenModal(modal: ApplicationModal): () => void {
+  const dispatch = useAppDispatch()
+  return useCallback(() => dispatch(setOpenModal(modal)), [dispatch, modal])
+}
+
+export function useCloseModals(): () => void {
+  const dispatch = useAppDispatch()
+  return useCallback(() => dispatch(setOpenModal(null)), [dispatch])
 }
