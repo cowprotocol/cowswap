f535a5b1cb5f1d1382fdc1277c8af4cc75b22be6
Leandro <leandro.boscariol@gnosis.io>
2022-03-09 17:32:37 -0800

    Fixed bug loading IPFS NFT profile image on original files

--- src/lib/utils/uriToHttp.ts
+++ src/lib/utils/uriToHttp.ts
@@ -12,7 +12,7 @@ export default function uriToHttp(uri: string): string[] {
     case 'http':
       return ['https' + uri.substr(4), uri]
     case 'ipfs':
-      const hash = uri.match(/^ipfs:(\/\/)?(.*)$/i)?.[2]
+      const hash = uri.match(/^ipfs:(\/\/)?(ipfs\/)?(.*)$/i)?.[3] // TODO: probably a bug on original code
       return [`https://cloudflare-ipfs.com/ipfs/${hash}/`, `https://ipfs.io/ipfs/${hash}/`]
     case 'ipns':
       const name = uri.match(/^ipns:(\/\/)?(.*)$/i)?.[2]
