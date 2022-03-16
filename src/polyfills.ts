c01d30acc97ab99a2e2a3483ee277fb69f56ed23
dave <david.sato64@gmail.com>
2022-03-16 09:39:47 +0000

    [Technical Debt] top level console override (replacement for 2532) (#2537)
    * top level console override
    
    * use proxy and force // types

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -5,3 +5,18 @@ import flatMap from 'array.prototype.flatmap'
 
 flat.shim()
 flatMap.shim()
+
+const originalConsole = window.console
+// define a new console
+const proxiedConsole = new Proxy(window.console, {
+  get(obj, prop: keyof Console) {
+    if (process.env.NODE_ENV !== 'production') {
+      return obj[prop]
+    } else {
+      return () => undefined
+    }
+  },
+})
+
+window.console = Object.assign({}, proxiedConsole, { force: originalConsole })
+console = window.console
