1dc3ad74561b4cbcde55e4f0365ec4a4ca740c70
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-03-30 10:38:20 +0200

    chunk up the merkle proof data

--- src/custom/pages/Profile/LockedGnoVesting/claimData/index.ts
+++ src/custom/pages/Profile/LockedGnoVesting/claimData/index.ts
@@ -1,6 +1,9 @@
-import mainnetAddresses from './mainnetAddresses.json'
-import rinkebyAddresses from './rinkebyAddresses.json'
-import gnosisChainAddresses from './gnosischainAddresses.json'
+import mainnetIndex from './mainnet/index.json'
+import mainnetAddresses from './mainnet/addresses.json'
+import rinkebyIndex from './rinkeby/index.json'
+import rinkebyAddresses from './rinkeby/addresses.json'
+import gnosisChainIndex from './gnosisChain/index.json'
+import gnosisChainAddresses from './gnosisChain/addresses.json'
 
 export const hasAllocation = (address: string, chainId: number) => {
   const addressHead = address.substring(2, 8).toLowerCase()
@@ -21,18 +24,42 @@ interface Claim {
   proof: string[]
 }
 
+// The merkle proof data has been chunked up using the following function:
+// const chunk = (claims) => {
+//   const addresses = Object.keys(claims).sort()
+//   return addresses.reduce((chunks, address, index) => {
+//     const chunkIndex = Math.floor(index / 64)
+//     if (!chunks[chunkIndex]) chunks[chunkIndex] = {}
+//     chunks[chunkIndex][address] = claims[address]
+//     return chunks
+//   }, [])
+// }
 export const fetchClaim = async (address: string, chainId: number): Promise<Claim> => {
+  const lowerCaseAddress = address.toLowerCase()
   let json
-  switch (chainId) {
-    case 1:
-      json = await import('./mainnet.json')
-      break
-    case 4:
-      json = await import('./rinkeby.json')
-      break
-    case 100:
-      json = await import('./gnosischain.json')
+  try {
+    switch (chainId) {
+      case 1:
+        json = await import(`./mainnet/chunk_${lookupChunkIndex(mainnetIndex, lowerCaseAddress)}.json`)
+        break
+      case 4:
+        json = await import(`./rinkeby/chunk_${lookupChunkIndex(rinkebyIndex, lowerCaseAddress)}.json`)
+        break
+      case 100:
+        json = await import(`./gnosisChain/chunk_${lookupChunkIndex(gnosisChainIndex, lowerCaseAddress)}.json`)
+    }
+  } catch (e) {
+    console.error(e)
   }
-  if (!json) throw new Error(`Could not fetch claim data for chain #${chainId}`)
-  return json.claims[address.toLowerCase() as keyof typeof json.claims]
+  if (!json) throw new Error(`Could not fetch claim data for chain #${chainId} and address ${address}`)
+  return json[lowerCaseAddress]
+}
+
+const lookupChunkIndex = (
+  chunkIndexJson: typeof mainnetIndex | typeof rinkebyIndex | typeof gnosisChainIndex,
+  address: string
+) => {
+  let nextChunkIndex = chunkIndexJson.findIndex((a) => address < a)
+  if (nextChunkIndex === -1) nextChunkIndex = chunkIndexJson.length
+  return nextChunkIndex - 1
 }
