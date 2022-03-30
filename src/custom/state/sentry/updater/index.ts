e4304f0cd396286af31b1b2dbc4443bb1350de82
w3stside <david.sato64@gmail.com>
2022-03-30 15:34:47 +0100

    sentry updater

---
+++ src/custom/state/sentry/updater/index.ts
@@ -0,0 +1,53 @@
+import { useEffect } from 'react'
+import * as Sentry from '@sentry/browser'
+
+import useIsWindowVisible from 'hooks/useIsWindowVisible'
+import { useSwapState } from 'state/swap/hooks'
+import { SupportedChainId } from 'constants/chains'
+import { useCurrency } from 'hooks/Tokens'
+import { useWalletInfo } from 'hooks/useWalletInfo'
+
+export default function Updater(): null {
+  const { account, chainId, walletName, isSupportedWallet } = useWalletInfo()
+  const windowVisible = useIsWindowVisible()
+
+  const {
+    INPUT: { currencyId: sellTokenAddress },
+    OUTPUT: { currencyId: buyTokenAddress },
+  } = useSwapState()
+
+  const sellCurrency = useCurrency(sellTokenAddress)
+  const buyCurrency = useCurrency(buyTokenAddress)
+
+  // create sentry context based on "main" parameters
+  useEffect(() => {
+    if (windowVisible) {
+      Sentry.setContext('user', {
+        userAddress: account || 'DISCONNECTED',
+        wallet: walletName,
+        isWalletSupported: isSupportedWallet,
+        network:
+          account && chainId
+            ? SupportedChainId[chainId]
+            : `${chainId ? SupportedChainId[chainId] : 'MAINNET'} - DISCONNECTED`,
+        sellToken: `${sellTokenAddress} <${sellCurrency?.symbol}>`,
+        buyToken: `${buyTokenAddress} <${buyCurrency?.symbol}>`,
+      })
+    }
+  }, [
+    // user
+    account,
+    chainId,
+    walletName,
+    isSupportedWallet,
+    // tokens
+    sellTokenAddress,
+    buyTokenAddress,
+    buyCurrency?.symbol,
+    sellCurrency?.symbol,
+    // window visibility check
+    windowVisible,
+  ])
+
+  return null
+}
