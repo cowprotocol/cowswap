3837a52d0edb97ba8fcb397824239ab47bddc016
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-04-01 15:33:33 -0300

    parse ens name from qs

--- src/custom/hooks/useParseReferralQueryParam.ts
+++ src/custom/hooks/useParseReferralQueryParam.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react'
-import { isAddress } from '@ethersproject/address'
 import useParsedQueryString from 'hooks/useParsedQueryString'
 import { REFERRAL_QUERY_PARAM } from 'hooks/useReferralLink'
+import useENS from 'hooks/useENS'
 
 type ReferralQueryValue = {
   value: string
@@ -13,20 +13,21 @@ type ReferralQueryValue = {
  */
 export default function useParseReferralQueryParam(): ReferralQueryValue {
   const parsedQs = useParsedQueryString()
+  const referralAddress = parsedQs[REFERRAL_QUERY_PARAM] as string
+  const result = useENS(referralAddress)
 
   const referral = useMemo(() => {
-    const referralAddress = parsedQs[REFERRAL_QUERY_PARAM]
-    if (typeof referralAddress === 'string' && isAddress(referralAddress)) {
-      return { value: referralAddress, isValid: true }
+    if (result.loading || !referralAddress) {
+      return null
     }
 
-    if (referralAddress) {
-      console.warn('Invalid referral address')
-      return { value: '', isValid: false }
+    if (result.address) {
+      return { value: result.address, isValid: true }
     }
 
-    return null
-  }, [parsedQs])
+    console.warn('Invalid referral address')
+    return { value: '', isValid: false }
+  }, [result.loading, result.address, referralAddress])
 
   return referral
 }
