4aab98b37e5c5bd5bcb3b6d6123e16c34b26e97c
nenadV91 <nenadwebdevelop@gmail.com>
2022-04-05 13:23:39 +0200

    Update COW balance button

--- src/custom/components/CowBalanceButton/index.tsx
+++ src/custom/components/CowBalanceButton/index.tsx
@@ -1,5 +1,5 @@
 import { Trans } from '@lingui/macro'
-import styled, { css } from 'styled-components/macro'
+import styled from 'styled-components/macro'
 import CowProtocolLogo from 'components/CowProtocolLogo'
 import { useTokenBalance } from 'state/wallet/hooks'
 import { V_COW } from 'constants/tokens'
@@ -7,7 +7,7 @@ import { ChainId } from 'state/lists/actions/actionsMod'
 import { formatMax, formatSmartLocaleAware } from 'utils/format'
 import { AMOUNT_PRECISION } from 'constants/index'
 
-export const Wrapper = styled.div<{ isClaimPage?: boolean | null }>`
+export const Wrapper = styled.div`
   ${({ theme }) => theme.card.boxShadow};
   color: ${({ theme }) => theme.text1};
   padding: 0 12px;
@@ -36,65 +36,16 @@ export const Wrapper = styled.div<{ isClaimPage?: boolean | null }>`
       overflow: visible;
       max-width: initial;
     `};
-
-    &::before,
-    &::after {
-      content: '';
-      position: absolute;
-      left: -1px;
-      top: -1px;
-      background: ${({ theme }) =>
-        `linear-gradient(45deg, ${theme.primary1}, ${theme.primary2}, ${theme.primary3}, ${theme.bg4}, ${theme.primary1}, ${theme.primary2})`};
-      background-size: 800%;
-      width: calc(100% + 2px);
-      height: calc(100% + 2px);
-      z-index: -1;
-      animation: glow 50s linear infinite;
-      transition: background-position 0.3s ease-in-out;
-      border-radius: 12px;
-    }
-
-    &::after {
-      filter: blur(8px);
-    }
-
-    &:hover::before,
-    &:hover::after {
-      animation: glow 12s linear infinite;
-    }
-
-    // Stop glowing effect on claim page
-    ${({ isClaimPage }) =>
-      isClaimPage &&
-      css`
-        &::before,
-        &::after {
-          content: none;
-        }
-      `};
-  }
-
-  @keyframes glow {
-    0% {
-      background-position: 0 0;
-    }
-    50% {
-      background-position: 300% 0;
-    }
-    100% {
-      background-position: 0 0;
-    }
   }
 `
 
-interface CowClaimButtonProps {
-  isClaimPage?: boolean | null | undefined
+interface CowBalanceButtonProps {
   account?: string | null | undefined
   chainId: ChainId | undefined
-  handleOnClickClaim?: () => void
+  onClick?: () => void
 }
 
-export default function CowClaimButton({ isClaimPage, account, chainId, handleOnClickClaim }: CowClaimButtonProps) {
+export default function CowBalanceButton({ account, chainId, onClick }: CowBalanceButtonProps) {
   const vCowToken = chainId ? V_COW[chainId] : undefined
   const vCowBalance = useTokenBalance(account || undefined, vCowToken)
 
@@ -102,7 +53,7 @@ export default function CowClaimButton({ isClaimPage, account, chainId, handleOn
   const formattedMaxVCowBalance = formatMax(vCowBalance, vCowToken?.decimals)
 
   return (
-    <Wrapper isClaimPage={isClaimPage} onClick={handleOnClickClaim}>
+    <Wrapper onClick={onClick}>
       <CowProtocolLogo />
       <b title={formattedMaxVCowBalance && `${formattedMaxVCowBalance} vCOW`}>
         <Trans>{formattedVCowBalance} vCOW</Trans>
