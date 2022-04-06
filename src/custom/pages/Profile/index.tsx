cac897c67a8690cf49f5463d285dfbc50482dbce
fairlight <31534717+fairlighteth@users.noreply.github.com>
2022-04-06 19:36:47 +0100

    Profile: Orange link hover and shorter Add to Metamask label: Waterfall PR[5] (#381)
    * Orange link on hover.
    
    * Add shorter label option.
    
    * Profile: Conditional 'Add Token' vs 'Copy contract address': Waterfall PR[6] (#383)
    
    * Detect MetaMask: conditional AddToMetaMask button
    
    * Click to copy token contract address.
    
    * Profile: Make elements responsive: Waterfall PR[7] (#384)
    
    * Mobile responsive.
    
    * Mobile responsive fix + add copy contract vCOW
    
    * Use string directly
    
    * Replace 1 by ChainId
    
    * Add check
    
    * Set default mainnet chain when deconstructing.
    
    * Update src/custom/pages/Profile/index.tsx
    
    Co-authored-by: dave <david.sato64@gmail.com>
    
    * Remove conditional check
    
    Co-authored-by: dave <david.sato64@gmail.com>

--- src/custom/pages/Profile/index.tsx
+++ src/custom/pages/Profile/index.tsx
@@ -1,5 +1,4 @@
 import { useCallback } from 'react'
-import { Currency } from '@uniswap/sdk-core'
 import { Txt } from 'assets/styles/styled'
 import {
   FlexCol,
@@ -54,12 +53,13 @@ import useTransactionConfirmationModal from 'hooks/useTransactionConfirmationMod
 import { SwapVCowStatus } from 'state/cowToken/actions'
 import AddToMetamask from 'components/AddToMetamask'
 import { Link } from 'react-router-dom'
+import CopyHelper from 'components/Copy'
 
 const COW_DECIMALS = COW[ChainId.MAINNET].decimals
 
 export default function Profile() {
   const referralLink = useReferralLink()
-  const { account, chainId } = useActiveWeb3React()
+  const { account, chainId = ChainId.MAINNET, library } = useActiveWeb3React()
   const { profileData, isLoading, error } = useFetchProfile()
   const lastUpdated = useTimeAgo(profileData?.lastUpdated)
   const isTradesTooltipVisible = account && chainId == 1 && !!profileData?.totalTrades
@@ -162,6 +162,8 @@ export default function Profile() {
     </>
   )
 
+  const currencyCOW = COW[chainId]
+
   return (
     <Container>
       <TransactionConfirmationModal />
@@ -207,9 +209,12 @@ export default function Profile() {
             </ConvertWrapper>
 
             <CardActions>
-              <ExtLink href={getBlockExplorerUrl(chainId || 1, V_COW_CONTRACT_ADDRESS[chainId || 1], 'address')}>
+              <ExtLink href={getBlockExplorerUrl(chainId, V_COW_CONTRACT_ADDRESS[chainId], 'address')}>
                 Contract ↗
               </ExtLink>
+              <CopyHelper toCopy={V_COW_CONTRACT_ADDRESS[chainId]}>
+                <div title="Click to copy token contract address">Copy contract</div>
+              </CopyHelper>
             </CardActions>
           </Card>
         )}
@@ -223,13 +228,22 @@ export default function Profile() {
             </span>
           </BalanceDisplay>
           <CardActions>
-            <ExtLink href={getBlockExplorerUrl(chainId || 1, COW_CONTRACT_ADDRESS[chainId || 1], 'address')}>
+            <ExtLink
+              title="View contract"
+              href={getBlockExplorerUrl(chainId, COW_CONTRACT_ADDRESS[chainId], 'address')}
+            >
               Contract ↗
             </ExtLink>
 
-            <AddToMetamask currency={COW[chainId || 1] as Currency | undefined} />
+            {library?.provider?.isMetaMask && <AddToMetamask shortLabel currency={currencyCOW} />}
 
-            <Link to={`/swap?outputCurrency=${COW_CONTRACT_ADDRESS[chainId || 1]}`}>Buy COW ↗</Link>
+            {!library?.provider?.isMetaMask && (
+              <CopyHelper toCopy={COW_CONTRACT_ADDRESS[chainId]}>
+                <div title="Click to copy token contract address">Copy contract</div>
+              </CopyHelper>
+            )}
+
+            <Link to={`/swap?outputCurrency=${COW_CONTRACT_ADDRESS[chainId]}`}>Buy COW</Link>
           </CardActions>
         </Card>
 
@@ -273,7 +287,7 @@ export default function Profile() {
                     )}
                   </Txt>
                   {hasOrders && (
-                    <ExtLink href={getExplorerAddressLink(chainId || 1, account)}>
+                    <ExtLink href={getExplorerAddressLink(chainId, account)}>
                       <Txt secondary>View all orders ↗</Txt>
                     </ExtLink>
                   )}
