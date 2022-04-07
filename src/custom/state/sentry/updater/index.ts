2028ef342e227f6367bbae233eebd2e599592e9e
dave <david.sato64@gmail.com>
2022-04-07 15:59:22 +0100

    [SENTRY] Context and error description cleanup (#361)
    * sentry updater
    
    * simplify error description
    
    * simpler networl
    
    * remove user address sentry logging

---
+++ src/custom/state/sentry/updater/index.ts
@@ -0,0 +1,49 @@
+import { useEffect } from 'react'
+import * as Sentry from '@sentry/browser'
+
+import useIsWindowVisible from 'hooks/useIsWindowVisible'
+import { useSwapState } from 'state/swap/hooks'
+import { SupportedChainId } from 'constants/chains'
+import { useCurrency } from 'hooks/Tokens'
+import { useWalletInfo } from 'hooks/useWalletInfo'
+
+export default function Updater(): null {
+  const { account, chainId, walletName, isSupportedWallet } = useWalletInfo()
+  const windowVisible = useIsWindowVisible()
+
+  const {
+    INPUT: { currencyId: sellTokenAddress },
+    OUTPUT: { currencyId: buyTokenAddress },
+  } = useSwapState()
+
+  const sellCurrency = useCurrency(sellTokenAddress)
+  const buyCurrency = useCurrency(buyTokenAddress)
+
+  // create sentry context based on "main" parameters
+  useEffect(() => {
+    if (windowVisible) {
+      Sentry.setContext('user', {
+        // userAddress: account || 'DISCONNECTED', // TODO: validate with legal
+        wallet: walletName,
+        network: chainId ? SupportedChainId[chainId] : chainId,
+        sellToken: `${sellTokenAddress} <${sellCurrency?.symbol}>`,
+        buyToken: `${buyTokenAddress} <${buyCurrency?.symbol}>`,
+      })
+    }
+  }, [
+    // user
+    account,
+    chainId,
+    walletName,
+    isSupportedWallet,
+    // tokens
+    sellTokenAddress,
+    buyTokenAddress,
+    buyCurrency?.symbol,
+    sellCurrency?.symbol,
+    // window visibility check
+    windowVisible,
+  ])
+
+  return null
+}
