71d99f2e95b83473629c25db42494948f846a000
Leandro Boscariol <alfetopito@users.noreply.github.com>
2022-04-19 09:14:54 -0700

    411/widget functionality (#413)
    * Modded lib/components/BrandedFooter
    
    * Overwritting lib/assets/svg/logo with CoW Protocol's logo

--- src/custom/lib/components/BrandedFooter/BrandedFooterMod.tsx
+++ src/custom/lib/components/BrandedFooter/BrandedFooterMod.tsx
@@ -1,11 +1,14 @@
 import { Trans } from '@lingui/macro'
 import Row from 'lib/components/Row'
 import { Logo } from 'lib/icons'
-import styled, { brand, ThemedText } from 'lib/theme'
+import { /*styled,*/ /*brand,*/ ThemedText } from 'lib/theme'
 
-import ExternalLink from './ExternalLink'
+// MOD imports
+import { UniswapA } from '@src/lib/components/BrandedFooter'
 
-export const UniswapA = styled(ExternalLink)`
+/* import ExternalLink from './ExternalLink'
+
+const UniswapA = styled(ExternalLink)`
   color: ${({ theme }) => theme.secondary};
   cursor: pointer;
   text-decoration: none;
@@ -22,16 +25,16 @@ export const UniswapA = styled(ExternalLink)`
     fill: ${brand};
     transform: rotate(-5deg);
   }
-`
+` */
 
 export default function BrandedFooter() {
   return (
     <Row justify="center">
-      <UniswapA href={`https://uniswap.org/`}>
+      <UniswapA href={`https://cow.fi/`}>
         <Row gap={0.25}>
           <Logo />
           <ThemedText.Caption>
-            <Trans>Powered by the Uniswap protocol</Trans>
+            <Trans>Powered by the CoW Protocol</Trans>
           </ThemedText.Caption>
         </Row>
       </UniswapA>
