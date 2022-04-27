aac28ce9134354af1e0e964ece32a029ae586a93
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2022-04-27 17:55:42 +0100

    store as constant

--- src/custom/pages/App/index.tsx
+++ src/custom/pages/App/index.tsx
@@ -14,7 +14,7 @@ import RedirectAnySwapAffectedUsers from 'pages/error/AnySwapAffectedUsers/Redir
 import { SENTRY_IGNORED_GP_QUOTE_ERRORS } from 'api/gnosisProtocol/errors/QuoteError'
 
 import SideBanner from 'components/SideBanner'
-const SIDE_BANNER_VISIBLE_KEY = 'isSideBannerVisible'
+import { IS_SIDE_BANNER_VISIBLE_KEY } from '@src/constants/misc'
 
 const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN
 const SENTRY_TRACES_SAMPLE_RATE = process.env.REACT_APP_SENTRY_TRACES_SAMPLE_RATE
@@ -99,7 +99,7 @@ export default function App() {
   // Dealing with empty URL queryParameters
   useFilterEmptyQueryParams()
 
-  const isVisible = useMemo(() => getShowBannerState(SIDE_BANNER_VISIBLE_KEY), []) // empty array only calls on mount
+  const isVisible = useMemo(() => getShowBannerState(IS_SIDE_BANNER_VISIBLE_KEY), []) // empty array only calls on mount
 
   return (
     <>
