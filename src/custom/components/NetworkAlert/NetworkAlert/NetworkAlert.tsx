f0a4a432115f21445345dccb58f097ee6b8ec359
Alfetopito <alfetopito@users.noreply.github.com>
2022-05-04 13:48:32 +0100

    Underline only when text is hovered and changing text color to primary1

--- src/custom/components/NetworkAlert/NetworkAlert/NetworkAlert.tsx
+++ src/custom/components/NetworkAlert/NetworkAlert/NetworkAlert.tsx
@@ -33,6 +33,12 @@ const BodyText = styled.div`
   justify-content: flex-start;
   margin: 8px;
   font-size: 14px;
+
+  //  mod
+  :hover {
+    text-decoration: underline;
+    color: ${({ theme }) => theme.primary1};
+  }
 `
 const RootWrapper = styled.div`
   position: relative;
@@ -108,7 +114,6 @@ const ContentWrapper = styled.div<{ chainId: NetworkAlertChains; darkMode: boole
 
   // mod
   :hover {
-    text-decoration: underline;
     background-color: ${({ theme }) => theme.bg7};
   }
 `
