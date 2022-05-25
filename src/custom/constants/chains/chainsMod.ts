d32d68ec71015e411c7f9a834ecbab94e768d39a
w3stside <david.sato64@gmail.com>
2022-05-25 13:36:13 +0100

    leave commented out mod

--- src/custom/constants/chains/chainsMod.ts
+++ src/custom/constants/chains/chainsMod.ts
@@ -8,6 +8,20 @@ import { SupportedChainId } from '@cowprotocol/cow-sdk'
 export * from '@src/constants/chains'
 export { SupportedChainId } from '@cowprotocol/cow-sdk'
 
+/* export enum SupportedChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GOERLI = 5,
+  KOVAN = 42,
+  ARBITRUM_ONE = 42161,
+  ARBITRUM_RINKEBY = 421611,
+  OPTIMISM = 10,
+  OPTIMISTIC_KOVAN = 69,
+  POLYGON = 137,
+  POLYGON_MUMBAI = 80001,
+}*/
+
 export const CHAIN_IDS_TO_NAMES = {
   ...UNI_CHAIN_IDS_TO_NAMES,
   [SupportedChainId.GNOSIS_CHAIN]: 'gnosis_chain',
