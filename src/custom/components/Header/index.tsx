cd55b0516d0306c83aa3ff5be8cc6ff751f8f596
Anxo Rodriguez <anxolin@gmail.com>
2022-06-21 10:57:07 +0100

    Refactor Menu (#715)
    * Break down menu logic
    
    * Rename function
    
    * items falsy
    
    * Refactor
    
    * Break down
    
    * Cleanup and finish refactor
    
    * Remove duplicate
    
    Co-authored-by: fairlighteth <31534717+fairlighteth@users.noreply.github.com>

--- src/custom/components/Header/index.tsx
+++ src/custom/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useMemo } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { SupportedChainId as ChainId } from 'constants/chains'
 import { Routes } from 'constants/routes'
 import { useHistory } from 'react-router-dom'
@@ -8,39 +8,30 @@ import { useNativeCurrencyBalances } from 'state/wallet/hooks'
 import { useDarkModeManager } from 'state/user/hooks'
 import { useMediaQuery, upToSmall, upToMedium, upToLarge, LargeAndUp } from 'hooks/useMediaQuery'
 import { AMOUNT_PRECISION } from 'constants/index'
-import { MAIN_MENU, MAIN_MENU_TYPE } from 'constants/mainMenu'
+
 import { supportedChainId } from 'utils/supportedChainId'
 import { formatSmart } from 'utils/format'
 import { addBodyClass, removeBodyClass } from 'utils/toggleBodyClass'
-import SVG from 'react-inlinesvg'
 
 // Components
-import { ExternalLink } from 'theme/components'
 import { HeaderRow } from './HeaderMod'
 import {
   Wrapper,
   Title,
   LogoImage,
-  HeaderLinks,
   HeaderModWrapper,
   UniIcon,
-  StyledNavLink,
   AccountElement,
   BalanceText,
   HeaderControls,
   HeaderElement,
 } from './styled'
+import { MenuTree } from './MenuTree'
 import MobileMenuIcon from './MobileMenuIcon'
-import MenuDropdown from 'components/MenuDropdown'
-import { MenuTitle, MenuSection } from 'components/MenuDropdown/styled'
 import Web3Status from 'components/Web3Status'
 import OrdersPanel from 'components/OrdersPanel'
 import NetworkSelector from 'components/Header/NetworkSelector'
 import CowBalanceButton from 'components/CowBalanceButton'
-
-// Assets
-import IMAGE_MOON from 'assets/cow-swap/moon.svg'
-import IMAGE_SUN from 'assets/cow-swap/sun.svg'
 
 export const NETWORK_LABELS: { [chainId in ChainId]?: string } = {
   [ChainId.RINKEBY]: 'Rinkeby',
@@ -107,67 +98,6 @@ export default function Header() {
     setIsTouch('ontouchstart' in document.documentElement)
   }, [isOrdersPanelOpen, isMobileMenuOpen, isUpToLarge, isUpToMedium, isUpToSmall, isLargeAndUp])
 
-  const getMainMenu = useMemo(
-    () =>
-      MAIN_MENU.map(({ title, url, externalURL, items }: MAIN_MENU_TYPE, index) =>
-        !items && !externalURL && url ? (
-          <StyledNavLink key={index} exact to={url} onClick={handleMobileMenuOnClick}>
-            {title}
-          </StyledNavLink>
-        ) : !items && externalURL && url ? (
-          <ExternalLink key={index} href={url} onClickOptional={handleMobileMenuOnClick}>
-            {title}
-          </ExternalLink>
-        ) : items ? (
-          <MenuDropdown key={index} title={title}>
-            {items.map(({ sectionTitle, links }, index) => {
-              return (
-                <MenuSection key={index}>
-                  {sectionTitle && <MenuTitle>{sectionTitle}</MenuTitle>}
-                  {links.map(({ title, url, externalURL, icon, iconSVG, action }, index) => {
-                    return action && action === 'setColorMode' ? (
-                      <button
-                        key={index}
-                        onClick={() => {
-                          handleMobileMenuOnClick()
-                          toggleDarkMode()
-                        }}
-                      >
-                        <SVG
-                          src={darkMode ? IMAGE_SUN : IMAGE_MOON}
-                          description={`${darkMode ? 'Sun/light' : 'Moon/dark'} mode icon`}
-                        />{' '}
-                        {darkMode ? 'Light' : 'Dark'} Mode
-                      </button>
-                    ) : !externalURL && url ? (
-                      <StyledNavLink key={index} exact to={url} onClick={handleMobileMenuOnClick}>
-                        {iconSVG ? (
-                          <SVG src={iconSVG} description={`${title} icon`} />
-                        ) : icon ? (
-                          <img src={icon} alt={`${title} icon`} />
-                        ) : null}{' '}
-                        {title}
-                      </StyledNavLink>
-                    ) : url ? (
-                      <ExternalLink key={index} href={url} onClickOptional={handleMobileMenuOnClick}>
-                        {iconSVG ? (
-                          <SVG src={iconSVG} description={`${title} icon`} />
-                        ) : icon ? (
-                          <img src={icon} alt={`${title} icon`} />
-                        ) : null}{' '}
-                        {title}
-                      </ExternalLink>
-                    ) : null
-                  })}
-                </MenuSection>
-              )
-            })}
-          </MenuDropdown>
-        ) : null
-      ),
-    [darkMode, handleMobileMenuOnClick, toggleDarkMode]
-  )
-
   return (
     <Wrapper isMobileMenuOpen={isMobileMenuOpen}>
       <HeaderModWrapper>
@@ -177,7 +107,12 @@ export default function Header() {
               <LogoImage isMobileMenuOpen={isMobileMenuOpen} />
             </UniIcon>
           </Title>
-          <HeaderLinks isMobileMenuOpen={isMobileMenuOpen}>{getMainMenu}</HeaderLinks>
+          <MenuTree
+            isMobileMenuOpen={isMobileMenuOpen}
+            darkMode={darkMode}
+            toggleDarkMode={toggleDarkMode}
+            handleMobileMenuOnClick={handleMobileMenuOnClick}
+          />
         </HeaderRow>
 
         <HeaderControls>
