8ea8b507bb0415f50583220ccca470338a55f414
nenadV91 <nenadwebdevelop@gmail.com>
2022-07-05 18:15:05 +0200

    Added mod comments

--- src/custom/hooks/useAddTokenToMetamask.ts
+++ src/custom/hooks/useAddTokenToMetamask.ts
@@ -2,7 +2,7 @@ import { Currency, Token } from '@uniswap/sdk-core'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import useCurrencyLogoURIs from 'lib/hooks/useCurrencyLogoURIs'
 import { useCallback, useState } from 'react'
-import { addTokenToMetamaskAnalytics } from 'utils/analytics'
+import { addTokenToMetamaskAnalytics } from 'utils/analytics' // MOD
 
 export default function useAddTokenToMetamask(currencyToAdd: Currency | undefined): {
   addToken: () => void
@@ -32,10 +32,12 @@ export default function useAddTokenToMetamask(currencyToAdd: Currency | undefine
           },
         })
         .then((success) => {
+          // MOD: google analytics
           addTokenToMetamaskAnalytics('Succeeded', token.symbol)
           setSuccess(success)
         })
         .catch(() => {
+          // MOD: google analytics
           addTokenToMetamaskAnalytics('Failed', token.symbol)
           setSuccess(false)
         })
