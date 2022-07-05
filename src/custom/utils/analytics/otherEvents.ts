ee34975ead82525d121ed38b836988b5f0afa7f1
nenadV91 <nenadwebdevelop@gmail.com>
2022-07-05 15:56:08 +0200

    Added GA event for external link

--- src/custom/utils/analytics/otherEvents.ts
+++ src/custom/utils/analytics/otherEvents.ts
@@ -7,3 +7,25 @@ export function gameAnalytics(gameType: GameType) {
     action: `Playing ${gameType} game`,
   })
 }
+
+function _detectExplorer(href: string) {
+  if (href.includes('explorer')) {
+    return 'Explorer'
+  } else if (href.includes('blockscout')) {
+    return 'Blockscout'
+  } else {
+    return undefined
+  }
+}
+
+export function externalLinkAnalytics(href: string) {
+  const explorer = _detectExplorer(href)
+
+  if (explorer) {
+    reportEvent({
+      category: Category.EXTERNAL_LINK,
+      action: `View on ${_detectExplorer(href)}`,
+      label: href,
+    })
+  }
+}
