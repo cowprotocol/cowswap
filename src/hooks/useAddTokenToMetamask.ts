aa13222006514f427102b6296810970a5945fbbc
nenadV91 <nenadwebdevelop@gmail.com>
2022-07-05 15:03:44 +0200

    Add token to metamask GA event

--- src/hooks/useAddTokenToMetamask.ts
+++ src/hooks/useAddTokenToMetamask.ts
@@ -2,6 +2,7 @@ import { Currency, Token } from '@uniswap/sdk-core'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import useCurrencyLogoURIs from 'lib/hooks/useCurrencyLogoURIs'
 import { useCallback, useState } from 'react'
+import { addTokenToMetamaskAnalytics } from 'utils/analytics'
 
 export default function useAddTokenToMetamask(currencyToAdd: Currency | undefined): {
   addToken: () => void
@@ -31,9 +32,13 @@ export default function useAddTokenToMetamask(currencyToAdd: Currency | undefine
           },
         })
         .then((success) => {
+          addTokenToMetamaskAnalytics('Success', token.symbol)
           setSuccess(success)
         })
-        .catch(() => setSuccess(false))
+        .catch(() => {
+          addTokenToMetamaskAnalytics('Fail', token.symbol)
+          setSuccess(false)
+        })
     } else {
       setSuccess(false)
     }
