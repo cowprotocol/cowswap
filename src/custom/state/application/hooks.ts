31ba467362672395eb967571b6bdb6238040d551
Henry Palacios <henrydpalacios@gmail.com>
2022-07-19 15:31:39 -0300

    Reusing showFollowTxPopupAtom

--- src/custom/state/application/hooks.ts
+++ src/custom/state/application/hooks.ts
@@ -4,7 +4,7 @@ import { useToggleModal } from '@src/state/application/hooks'
 import { useCallback, useEffect } from 'react'
 import { addPopup, ApplicationModal, PopupContent } from 'state/application/reducer'
 import { useAppDispatch } from 'state/hooks'
-import { followPendingTxPopupAtom, handleFollowPendingTxPopupAtom, selectAtom, useUpdateAtom } from './atoms'
+import { handleFollowPendingTxPopupAtom, showFollowTxPopupAtom, useUpdateAtom, useAtomValue } from './atoms'
 
 export * from '@src/state/application/hooks'
 
@@ -47,7 +47,7 @@ export function useAddPopup(): (content: PopupContent, key?: string, removeAfter
 // Set pop up closed if it has not been closed and not fulfill a condition such as not pending tx
 export function useCloseFollowTxPopupIfNot(fulfillsCondition: boolean) {
   const setShowFollowTxPopup = useUpdateAtom(handleFollowPendingTxPopupAtom)
-  const showingPopup = selectAtom(followPendingTxPopupAtom, ({ showPopup }) => showPopup)
+  const showingPopup = useAtomValue(showFollowTxPopupAtom)
 
   const closeIfNotFulfillsCondition = useCallback(
     (_fulfillsCondition) => {
