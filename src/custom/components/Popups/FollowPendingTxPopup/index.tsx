4dac983b5502edc32b3ed1d2b9ef8f799aad9176
Henry Palacios <henrydpalacios@gmail.com>
2022-07-21 05:59:01 -0300

    Adding FollowPendingTxPopup when TxSubmittedModal is close (#783)
    * Adding FollowPendingTxPopup
    
    * Add handle close modal
    
    * Fix logic show popup
    
    * Show when OrderProgressBar is Hide
    
    * Hide popup
    
    * Fix pass props on FollowPendingTxPopup
    
    * Adding close icon to popup
    
    * Move hook to hook file
    
    * Remove hook, move popover behind tooltip
    
    * Fix popup showing incorrect bahavior
    
    * Use selectAtom to read jotai state
    
    * do not show popup when no tx is pending
    
    * Show popup even when onDismiss TxConfirmationModal
    
    * To extract logic popup to another component
    
    * Show also in expert mode
    
    * Move popup to left side
    
    * Improve the tooltip width in mobile
    
    * Remove unnecessary css style
    
    * Fix overlapping of popup to menu in mobile mode
    
    * Fix overlap on 960px
    
    * Clean remaining comments
    
    * Use a large tooltip on devices > 700px
    
    * Reusing showFollowTxPopupAtom
    
    * OnCheck function renaming

---
+++ src/custom/components/Popups/FollowPendingTxPopup/index.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useMemo } from 'react'
+
+import { useIsExpertMode } from '@src/state/user/hooks'
+import useRecentActivity, { TransactionAndOrder } from 'hooks/useRecentActivity'
+import { Order } from 'state/orders/actions'
+import {
+  useUpdateAtom,
+  useAtomValue,
+  handleFollowPendingTxPopupAtom,
+  handleHidePopupPermanentlyAtom,
+  showFollowTxPopupAtom,
+} from 'state/application/atoms'
+import FollowPendingTxPopupUI from './FollowPendingTxPopupUI'
+import { isPending } from 'components/Web3Status'
+
+function isAnOrder(element: TransactionAndOrder): element is Order & { addedTime: number } {
+  return 'inputToken' in element && 'outputToken' in element
+}
+
+const FollowPendingTxPopup: React.FC = ({ children }): JSX.Element => {
+  const setShowFollowPendingTxPopup = useUpdateAtom(handleFollowPendingTxPopupAtom)
+  const setHidePendingTxPopupPermanently = useUpdateAtom(handleHidePopupPermanentlyAtom)
+  const showFollowPendingTxPopup = useAtomValue(showFollowTxPopupAtom)
+  const isExpertMode = useIsExpertMode()
+  const allRecentActivity = useRecentActivity()
+  const isTherePendingOrder = useMemo(() => {
+    if (!isExpertMode) return
+    const pendings = allRecentActivity.filter(isPending)
+
+    return isAnOrder(pendings[pendings.length - 1])
+  }, [allRecentActivity, isExpertMode])
+
+  useEffect(() => {
+    if (isExpertMode && isTherePendingOrder) {
+      setShowFollowPendingTxPopup(true)
+    }
+  }, [isExpertMode, isTherePendingOrder, setShowFollowPendingTxPopup])
+
+  return (
+    <FollowPendingTxPopupUI
+      show={showFollowPendingTxPopup}
+      onCheck={() => setHidePendingTxPopupPermanently(true)}
+      onClose={() => setShowFollowPendingTxPopup(false)}
+    >
+      {children}
+    </FollowPendingTxPopupUI>
+  )
+}
+
+export default FollowPendingTxPopup
