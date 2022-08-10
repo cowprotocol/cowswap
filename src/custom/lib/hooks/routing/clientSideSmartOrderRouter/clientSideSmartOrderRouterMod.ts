85df3e4abbbfbe2059b0e9e6dd1a682f02af53cd
Alexandr Kazachenko <shoom3301@gmail.com>
2022-08-10 17:06:30 +0600

    Performance optimizations (#886)
    * Perf enhancement: remove usage of @uniswap/smart-order-router
    
    * Perf enhancement: remove unused Uni ABIs
    
    * Make `Swap` route sync
    
    * Replace cow-load from gif to webp to save 18kb (#890)

---
+++ src/custom/lib/hooks/routing/clientSideSmartOrderRouter/clientSideSmartOrderRouterMod.ts
@@ -0,0 +1,12 @@
+import type { ChainId } from '@uniswap/smart-order-router'
+import { SupportedChainId } from 'constants/chains'
+
+export function toSupportedChainId(chainId: ChainId): SupportedChainId | undefined {
+  const numericChainId: number = chainId
+  if (SupportedChainId[numericChainId]) return numericChainId
+  return undefined
+}
+export function isSupportedChainId(chainId: ChainId | undefined): boolean {
+  if (chainId === undefined) return false
+  return toSupportedChainId(chainId) !== undefined
+}
