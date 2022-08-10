7f4cff695909c6af5c7fd5f90248ccef87cb0c10
w3stside <david@cow.fi>
2022-08-10 13:00:08 +0100

    use swapstate in eth flow hook

--- src/custom/state/ethFlow/hooks.ts
+++ src/custom/state/ethFlow/hooks.ts
@@ -1,6 +1,18 @@
+import { useCurrency } from '@src/hooks/Tokens'
+import { Field } from '@src/state/swap/actions'
 import { useAtomValue } from 'jotai/utils'
+import { useDetectNativeToken, useSwapState } from '../swap/hooks'
 import { isUserNativeEthFlow } from './atoms'
 
 export function useIsUserNativeEthFlow() {
-  return useAtomValue(isUserNativeEthFlow)
+  const {
+    [Field.INPUT]: { currencyId },
+  } = useSwapState()
+  const currency = useCurrency(currencyId)
+  // we check if user is selling native currency (not pure wrapping either)
+  const { isNativeIn } = useDetectNativeToken({ currency })
+  // get the user set native flow option
+  const isUserNativeEthFlowSet = useAtomValue(isUserNativeEthFlow)
+
+  return isNativeIn && isUserNativeEthFlowSet
 }
