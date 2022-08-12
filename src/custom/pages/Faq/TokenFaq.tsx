3490cda71bc3ed69a1274e71a23bd65fa098a6db
Alex V <80408352+avsavsavs@users.noreply.github.com>
2022-08-12 05:36:58 -0400

    Update TokenFaq.tsx

--- src/custom/pages/Faq/TokenFaq.tsx
+++ src/custom/pages/Faq/TokenFaq.tsx
@@ -18,7 +18,7 @@ export default function TokenFaq() {
         <ToC toc={toc} name="Token FAQ" />
         <Content>
           <h2 id="Token">Token</h2>
-          <h3 id="does-cowswap-have-a-token">Does CowSwap have a token?</h3>
+          <h3 id="does-cowswap-have-a-token">Does CoW Swap have a token?</h3>
           <p>
             Yes,{' '}
             <ExternalLinkFaq href="https://etherscan.io/address/0xd057b63f5e69cf1b929b356b579cba08d7688048">
@@ -28,7 +28,7 @@ export default function TokenFaq() {
           </p>
           <p>
             At launch the token will be a pure virtual token for governance. This implies that the token will not be
-            transferable. The token entitles its holders with voting rights in the CowDAO. It is up to CowDAO to vote on
+            transferable. The token entitles its holders with voting rights in the CoW DAO. It is up to CowDAO to vote on
             whether making the token transferable complies with applicable laws.
           </p>
           <p>
@@ -37,14 +37,14 @@ export default function TokenFaq() {
           </p>
           <p>Be cautious, some people may create fake COW tokens, that are not affiliated with this project.</p>
           <p>
-            Follow <ExternalLinkFaq href="https://twitter.com/mevprotection">@MEVProtection</ExternalLinkFaq> on Twitter
+            Follow <ExternalLinkFaq href="https://twitter.com/CoWSwap">@CoWSwapn</ExternalLinkFaq> on Twitter
             to be up to date!
           </p>
           <h3 id="is-there-a-proposal-to-make-vcow-transferable">Is there a proposal to make vCOW transferrable?</h3>
           <p>
             Yes! A CoWmunity member created a{' '}
             <ExternalLinkFaq href="https://forum.cow.fi/t/cip-draft-enable-swapping-of-vcow-to-cow/91/22">
-              CoWDAO Proposal
+              CoW DAO Proposal
             </ExternalLinkFaq>{' '}
             on the forum.
           </p>
@@ -53,7 +53,7 @@ export default function TokenFaq() {
           </h3>
           <p>
             {' '}
-            Directly in the CowSwap UI. Simply click on the <Link to={Routes.ACCOUNT}>account</Link> menu item at the
+            Directly in the CoW Swap UI. Simply click on the <Link to={Routes.ACCOUNT}>account</Link> menu item at the
             top left of the page (desktop) or in the mobile menu. You then will be redirected to the{' '}
             <Link to={Routes.ACCOUNT}>account</Link> page where you can see your total COW and/or vCOW balance. You will
             then be able to convert your vCOW to COW (if applicable).
@@ -61,13 +61,13 @@ export default function TokenFaq() {
 
           <h3 id="what-is-the-purpose-of-cow-token">What is the purpose of COW Token?</h3>
           <p>
-            The COW token will entitle its holders to voting rights within the CowDAO. The main purpose of CowDAO is to
+            The COW token will entitle its holders to voting rights within the CoW DAO. The main purpose of CoW DAO is to
             govern and curate essential infrastructure components of the CoW Protocol ecosystem, including:
           </p>
           <p>
             <ul>
               <li>
-                <strong>System Parameters</strong>: CowDAO will be in charge of all system parameters such as the
+                <strong>System Parameters</strong>: CoW DAO will be in charge of all system parameters such as the
                 objective function for solution scoring and solver slashing conditions.
               </li>
               <li>
@@ -95,9 +95,9 @@ export default function TokenFaq() {
           <p>
             <ul>
               <li>
-                <strong>CoW POAP</strong>: Awarded POAPs from the testing sessions, Batch challenges, CowStars or user
+                <strong>CoW POAP</strong>: Awarded POAPs from the testing sessions, Batch challenges, CoWStars or user
                 research sessions. Not all are equally valuable. Some that are most special are the ones for our
-                CowStars and User Test Participants. Note that we say “awarded” and not “owns”, since we know that there
+                CoWStars and User Test Participants. Note that we say “awarded” and not “owns”, since we know that there
                 have been secondary markets for these POAPS. We only took awardees into consideration.
               </li>
               <li>
