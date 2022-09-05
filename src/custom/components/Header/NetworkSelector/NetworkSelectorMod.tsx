0b497f53c700a11634b773d502a53835a47be74e
Alfetopito <alfetopito@users.noreply.github.com>
2022-09-05 14:43:52 +0100

    Replaced blockscout with gnosisscan for gnosischain network

--- src/custom/components/Header/NetworkSelector/NetworkSelectorMod.tsx
+++ src/custom/components/Header/NetworkSelector/NetworkSelectorMod.tsx
@@ -244,7 +244,7 @@ const ExplorerLabel = ({ chainId }: { chainId: SupportedChainId }) => {
     // case SupportedChainId.CELO_ALFAJORES:
     //   return <Trans>Blockscout</Trans>
     case SupportedChainId.GNOSIS_CHAIN:
-      return <Trans>Blockscout</Trans>
+      return <Trans>Gnosisscan</Trans>
     default:
       return <Trans>Etherscan</Trans>
   }
