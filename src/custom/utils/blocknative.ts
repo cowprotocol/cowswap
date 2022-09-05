6c91d6306c9529875d68fe277de0be36ff8275e7
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-09-05 08:56:27 -0300

    capture sentry errors for blocknative issues

--- src/custom/utils/blocknative.ts
+++ src/custom/utils/blocknative.ts
@@ -1,4 +1,5 @@
 import BlocknativeSdk from 'bnc-sdk'
+import * as Sentry from '@sentry/browser'
 import { getSupportedChainIds } from 'connection'
 
 const BLOCKNATIVE_API_KEY = process.env.REACT_APP_BLOCKNATIVE_API_KEY
@@ -12,11 +13,18 @@ interface SDKError {
 
 if (!BLOCKNATIVE_API_KEY) {
   console.warn('[blocknative] Missing BLOCKNATIVE_API_KEY')
+  Sentry.captureException(new Error('Blocknative API key not set'), {
+    tags: { errorType: 'blocknative' },
+  })
 }
 
 export const sdk = !BLOCKNATIVE_API_KEY
   ? {}
   : getSupportedChainIds().reduce<Record<number, BlocknativeSdk | null>>((acc, networkId) => {
+      const params = {
+        apiKey: BLOCKNATIVE_API_KEY,
+        networkId,
+      }
       try {
         acc[networkId] = new BlocknativeSdk({
           dappId: BLOCKNATIVE_API_KEY,
@@ -24,10 +32,18 @@ export const sdk = !BLOCKNATIVE_API_KEY
           name: 'bnc_' + networkId,
           onerror: (error: SDKError) => {
             console.log('[blocknative]', error)
+            Sentry.captureException(error, {
+              tags: { errorType: 'blocknative' },
+              contexts: { params },
+            })
           },
         })
       } catch (error) {
         console.error('[blocknative] Instantiating BlocknativeSdk failed', error)
+        Sentry.captureException(error, {
+          tags: { errorType: 'blocknative' },
+          contexts: { params },
+        })
       }
 
       console.info(`[blocknative] BlocknativeSdk initialized on chain ${networkId}`)
