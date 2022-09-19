18c8e06cb556a484c62f2763adaecbb6d70b5d60
shoom3301 <shoom3301@gmail.com>
2022-09-19 14:10:41 +0600

    fix: show currencies amounts in the tokens list

--- src/custom/pages/NewSwap/pureComponents/CurrencyInputPanel/index.tsx
+++ src/custom/pages/NewSwap/pureComponents/CurrencyInputPanel/index.tsx
@@ -105,7 +105,7 @@ export function CurrencyInputPanel(props: CurrencyInputPanelProps) {
         selectedCurrency={currency}
         otherSelectedCurrency={currency}
         showCommonBases={true}
-        showCurrencyAmount={false}
+        showCurrencyAmount={true}
         disableNonToken={false}
       />
     </>
