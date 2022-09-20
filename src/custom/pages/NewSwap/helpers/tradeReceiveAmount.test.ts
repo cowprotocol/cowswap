6443b66802c4cbf4277b4b4c9e43362837de4778
shoom3301 <shoom3301@gmail.com>
2022-09-20 15:36:46 +0600

    Fix tradeReceiveAmount tests

--- src/custom/pages/NewSwap/helpers/tradeReceiveAmount.test.ts
+++ src/custom/pages/NewSwap/helpers/tradeReceiveAmount.test.ts
@@ -2,32 +2,41 @@ import { getInputReceiveAmountInfo, getOutputReceiveAmountInfo } from './tradeRe
 import TradeGp from 'state/swap/TradeGp'
 import { COW, GNO } from 'constants/tokens'
 import { SupportedChainId } from 'constants/chains'
-import { CurrencyAmount, Price, TradeType } from '@uniswap/sdk-core'
+import { CurrencyAmount, Price, Token, TradeType } from '@uniswap/sdk-core'
 
 const currency = COW[SupportedChainId.MAINNET]
 const currencyOut = GNO[SupportedChainId.MAINNET]
 const amount = 250
 const output = 250
 
+function currencyAmount18e(currency: Token, amount: number): CurrencyAmount<Token> {
+  return CurrencyAmount.fromRawAmount(currency, amount * 10 ** 18)
+}
+
 describe('Helpers to build ReceiveAmountInfo', () => {
   describe('getInputReceiveAmountInfo()', () => {
-    describe('When tradeType is EXACT_INPUT and inputAmountWithFee less than trade fee amount', () => {
+    describe('When selling, if the fee is bigger than the traded amount', () => {
       it('Then amountBeforeFees should be zero', () => {
         const value = getInputReceiveAmountInfo(
           new TradeGp({
-            inputAmount: CurrencyAmount.fromRawAmount(currency, amount * 10 ** 18),
-            inputAmountWithFee: CurrencyAmount.fromRawAmount(currency, 0),
-            inputAmountWithoutFee: CurrencyAmount.fromRawAmount(currency, amount * 10 ** 18),
-            outputAmount: CurrencyAmount.fromRawAmount(currency, output * 10 ** 18),
-            outputAmountWithoutFee: CurrencyAmount.fromRawAmount(currency, (output - 3) * 10 ** 18),
-            fee: { feeAsCurrency: CurrencyAmount.fromRawAmount(currency, 3 * 10 ** 18), amount: '5000' },
+            inputAmount: currencyAmount18e(currency, amount),
+            inputAmountWithFee: currencyAmount18e(currency, 0),
+            inputAmountWithoutFee: currencyAmount18e(currency, amount),
+            outputAmount: currencyAmount18e(currency, output),
+            outputAmountWithoutFee: currencyAmount18e(currency, output - 3),
+            fee: { feeAsCurrency: currencyAmount18e(currency, 3), amount: '5000' },
             executionPrice: new Price(currency, currencyOut, 1, 4),
             tradeType: TradeType.EXACT_INPUT,
             quoteId: 10000,
           })
         )
 
-        expect(value).toMatchSnapshot()
+        expect(value).toEqual({
+          amountAfterFees: '0',
+          amountBeforeFees: '0',
+          feeAmount: '3',
+          type: 'from',
+        })
         expect(value.amountBeforeFees).toBe('0')
       })
     })
@@ -37,19 +46,24 @@ describe('Helpers to build ReceiveAmountInfo', () => {
     it('Should match a snapshot', () => {
       const value = getOutputReceiveAmountInfo(
         new TradeGp({
-          inputAmount: CurrencyAmount.fromRawAmount(currency, amount * 10 ** 18),
-          inputAmountWithFee: CurrencyAmount.fromRawAmount(currency, 0),
-          inputAmountWithoutFee: CurrencyAmount.fromRawAmount(currency, amount * 10 ** 18),
-          outputAmount: CurrencyAmount.fromRawAmount(currency, output * 10 ** 18),
-          outputAmountWithoutFee: CurrencyAmount.fromRawAmount(currency, (output - 3) * 10 ** 18),
-          fee: { feeAsCurrency: CurrencyAmount.fromRawAmount(currency, 3 * 10 ** 18), amount: '5000' },
+          inputAmount: currencyAmount18e(currency, amount),
+          inputAmountWithFee: currencyAmount18e(currency, 0),
+          inputAmountWithoutFee: currencyAmount18e(currency, amount),
+          outputAmount: currencyAmount18e(currency, output),
+          outputAmountWithoutFee: currencyAmount18e(currency, output - 3),
+          fee: { feeAsCurrency: currencyAmount18e(currency, 3), amount: '5000' },
           executionPrice: new Price(currency, currencyOut, 1, 4),
           tradeType: TradeType.EXACT_INPUT,
           quoteId: 10000,
         })
       )
 
-      expect(value).toMatchSnapshot()
+      expect(value).toEqual({
+        amountAfterFees: '250',
+        amountBeforeFees: '247',
+        feeAmount: '-3',
+        type: 'to',
+      })
     })
   })
 })
