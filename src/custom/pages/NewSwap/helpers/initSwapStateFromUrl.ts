253d87eae467d5bdb5ed11912cf3bef904b17222
shoom3301 <shoom3301@gmail.com>
2022-09-21 14:43:11 +0600

    Don't set 1 as default value for source token amount

--- src/custom/pages/NewSwap/helpers/initSwapStateFromUrl.ts
+++ src/custom/pages/NewSwap/helpers/initSwapStateFromUrl.ts
@@ -15,7 +15,7 @@ export function initSwapStateFromUrl(
   const typedValue =
     (tradeStateFromUrl.amount && !isNaN(parseFloat(tradeStateFromUrl.amount)) ? tradeStateFromUrl.amount : '') ||
     persistedSwapState?.typedValue ||
-    '1'
+    ''
 
   const independentField =
     tradeStateFromUrl.independentField === 'output' ? Field.OUTPUT : persistedSwapState?.independentField || Field.INPUT
