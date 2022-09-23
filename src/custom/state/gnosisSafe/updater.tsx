b4b48eb2b2d40155213595bbc51d3d446a4f78f4
Henry Palacios <henrydpalacios@gmail.com>
2022-09-23 00:04:18 -0300

    Adding useGnosisSafe wallet to listen execution event

--- src/custom/state/gnosisSafe/updater.tsx
+++ src/custom/state/gnosisSafe/updater.tsx
@@ -4,6 +4,7 @@ import { getSafeInfo } from '@cow/api/gnosisSafe'
 import { useWeb3React } from '@web3-react/core'
 import { gnosisSafeAtom } from 'state/gnosisSafe/atoms'
 import { useUpdateAtom } from 'jotai/utils'
+import { useSafeWalletContract } from 'hooks/useContract'
 
 export default function Updater(): null {
   const { account, chainId, provider } = useWeb3React()
@@ -17,6 +18,23 @@ export default function Updater(): null {
       setGnosisSafeInfo(undefined)
     }
   }, [setGnosisSafeInfo, chainId, account, isGnosisSafeConnected, provider])
+
+  return null
+}
+
+export function UpdaterExecutionDate(): null {
+  const isSafeWallet = useIsGnosisSafeWallet()
+  const { account } = useWeb3React()
+  const safeWalletContract = useSafeWalletContract(account)
+
+  useEffect(() => {
+    if (!isSafeWallet || !safeWalletContract) return
+
+    const updateMock = (txHash: string, payment: number) => console.log('__executionSuccess', txHash, payment)
+    safeWalletContract.on('ExecutionSuccess', (txHash, payment) => updateMock)
+
+    // return () => safeWalletContract.removeListener('ExecutionSuccess', updateMock)
+  }, [account, isSafeWallet, safeWalletContract])
 
   return null
 }
