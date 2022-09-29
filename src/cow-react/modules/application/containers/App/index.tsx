1c0ad682c01be4dba4c121b31c0eb8953af669ed
Anxo Rodriguez <anxolin@gmail.com>
2022-09-29 20:21:20 +0100

    Move sentry to its own util

--- src/cow-react/modules/application/containers/App/index.tsx
+++ src/cow-react/modules/application/containers/App/index.tsx
@@ -1,3 +1,5 @@
+import 'cow-react/utils/sentry'
+
 import { initializeAnalytics } from 'components/AmplitudeAnalytics'
 import TopLevelModals from 'components/TopLevelModals'
 import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
@@ -6,8 +8,6 @@ import styled from 'styled-components/macro'
 import ErrorBoundary from 'components/ErrorBoundary'
 import Header from 'components/Header'
 import Polling from 'components/Header/Polling'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
 
 import { Routes } from 'constants/routes'
 
@@ -18,29 +18,8 @@ import URLWarning from 'components/Header/URLWarning'
 import Footer from 'components/Footer'
 import * as CSS from 'csstype'
 
-import { version } from '@src/../package.json'
-import { environmentName } from 'utils/environments'
-import { SENTRY_IGNORED_GP_QUOTE_ERRORS } from 'cow-react/api/gnosisProtocol/errors/QuoteError'
 import RedirectAnySwapAffectedUsers from 'cow-react/pages/error/AnySwapAffectedUsers/RedirectAnySwapAffectedUsers'
 import { RoutesApp } from './RoutesApp'
-
-const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN
-const SENTRY_TRACES_SAMPLE_RATE = process.env.REACT_APP_SENTRY_TRACES_SAMPLE_RATE
-
-if (SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.REACT_APP_SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
-    release: 'CowSwap@v' + version,
-    environment: environmentName,
-    ignoreErrors: [...SENTRY_IGNORED_GP_QUOTE_ERRORS],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE ? Number(SENTRY_TRACES_SAMPLE_RATE) : 1.0,
-  })
-}
 
 const AppWrapper = styled.div<Partial<CSS.Properties>>`
   display: flex;
