c6f107d0d48bb29aa7b04639513367650adb5e40
Anxo Rodriguez <anxolin@gmail.com>
2022-09-29 19:59:08 +0100

    Move updaters

--- src/cow-react/index.tsx
+++ src/cow-react/index.tsx
@@ -4,7 +4,6 @@ import 'polyfills'
 import 'components/analytics'
 
 import { BlockNumberProvider } from 'lib/hooks/useBlockNumber'
-import { MulticallUpdater } from 'lib/state/multicall'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -16,34 +15,15 @@ import { LanguageProvider } from 'i18n'
 import App from 'pages/App'
 import * as serviceWorkerRegistration from 'serviceWorkerRegistration'
 import store from 'state'
-import ApplicationUpdater from 'state/application/updater'
-import ListsUpdater from 'state/lists/updater'
-import LogsUpdater from 'state/logs/updater'
-import TransactionUpdater from 'state/transactions/updater'
-import UserUpdater from 'state/user/updater'
-import GnosisSafeUpdater from 'state/gnosisSafe/updater'
 import ThemeProvider, { FixedGlobalStyle, ThemedGlobalStyle } from 'theme'
-import RadialGradientByChainUpdater from 'theme/RadialGradientByChainUpdater'
 
-import EnhancedTransactionUpdater from 'state/enhancedTransactions/updater'
-import FeesUpdater from 'state/price/updater'
-import GasUpdater from 'state/gas/updater'
-import SentryUpdater from 'state/sentry/updater'
-
-import {
-  GpOrdersUpdater,
-  CancelledOrdersUpdater,
-  PendingOrdersUpdater,
-  UnfillableOrdersUpdater,
-} from 'state/orders/updaters'
 // import { EventUpdater } from 'state/orders/mocks'
 import AppziButton from 'components/AppziButton'
 import { nodeRemoveChildFix } from 'utils/node'
 import { Provider as AtomProvider } from 'jotai'
 
 import Popups from 'components/Popups'
-import { UploadToIpfsUpdater } from 'state/appData/updater'
-import { GasPriceStrategyUpdater } from 'state/gas/gas-price-strategy-updater'
+import { Updaters } from 'cow-react/modules/application/containers/App/Updaters'
 
 // Node removeChild hackaround
 // based on: https://github.com/facebook/react/issues/11538#issuecomment-417504600
@@ -51,31 +31,6 @@ nodeRemoveChildFix()
 
 if (!!window.ethereum) {
   window.ethereum.autoRefreshOnNetworkChange = false
-}
-
-function Updaters() {
-  return (
-    <>
-      <RadialGradientByChainUpdater />
-      <ListsUpdater />
-      <UserUpdater />
-      <ApplicationUpdater />
-      <TransactionUpdater />
-      <EnhancedTransactionUpdater />
-      <MulticallUpdater />
-      <PendingOrdersUpdater />
-      <CancelledOrdersUpdater />
-      <FeesUpdater />
-      <UnfillableOrdersUpdater />
-      <GpOrdersUpdater />
-      <GasUpdater />
-      <LogsUpdater />
-      <SentryUpdater />
-      <UploadToIpfsUpdater />
-      <GnosisSafeUpdater />
-      <GasPriceStrategyUpdater />
-    </>
-  )
 }
 
 ReactDOM.render(
