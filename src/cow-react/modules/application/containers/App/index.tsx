030fc0475eb9f4e70f4f826839cf996eeb2bfc73
Anxo Rodriguez <anxolin@gmail.com>
2022-09-29 20:31:04 +0100

    Move styled components out

--- src/cow-react/modules/application/containers/App/index.tsx
+++ src/cow-react/modules/application/containers/App/index.tsx
@@ -2,64 +2,19 @@ import { initializeAnalytics } from 'components/AmplitudeAnalytics'
 import TopLevelModals from 'components/TopLevelModals'
 import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
 import { Route, useLocation } from 'react-router-dom'
-import styled from 'styled-components/macro'
 import ErrorBoundary from 'components/ErrorBoundary'
 import Header from 'components/Header'
 import Polling from 'components/Header/Polling'
-
-import { Routes } from 'constants/routes'
 
 import DarkModeQueryParamReader from 'theme'
 
 import ReferralLinkUpdater from 'state/affiliate/updater'
 import URLWarning from 'components/Header/URLWarning'
 import Footer from 'components/Footer'
-import * as CSS from 'csstype'
 
 import RedirectAnySwapAffectedUsers from 'cow-react/pages/error/AnySwapAffectedUsers/RedirectAnySwapAffectedUsers'
 import { RoutesApp } from './RoutesApp'
-
-const AppWrapper = styled.div<Partial<CSS.Properties>>`
-  display: flex;
-  flex-flow: column;
-  align-items: flex-start;
-  min-height: 100vh;
-`
-
-const HeaderWrapper = styled.div`
-  ${({ theme }) => theme.flexRowNoWrap}
-  width: 100%;
-  justify-content: space-between;
-`
-
-const FooterWrapper = styled(HeaderWrapper)`
-  z-index: 1;
-  width: auto;
-`
-
-const Marginer = styled.div`
-  margin-top: 5rem;
-`
-
-const BodyWrapper = styled.div<{ location: { pathname: string } }>`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  padding-top: 10vh;
-  align-items: center;
-  justify-content: center;
-  flex: auto;
-  z-index: 1;
-
-  ${({ theme }) => theme.mediaWidth.upToExtraLarge`
-    padding-top: 5vh;
-    align-items: flex-start;
-  `}
-
-  ${({ theme, location }) => theme.mediaWidth.upToMedium`
-    padding: ${[Routes.SWAP].includes(location.pathname as Routes) ? '0 0 16px' : '0 16px 16px'};
-  `}
-`
+import * as styledEl from './styled'
 
 export function App() {
   const location = useLocation()
@@ -71,22 +26,22 @@ export function App() {
       <RedirectAnySwapAffectedUsers />
       <Route component={DarkModeQueryParamReader} />
       <Route component={ApeModeQueryParamReader} />
-      <AppWrapper>
+      <styledEl.AppWrapper>
         <URLWarning />
-        <HeaderWrapper>
+        <styledEl.HeaderWrapper>
           <Header />
-        </HeaderWrapper>
-        <BodyWrapper location={location}>
+        </styledEl.HeaderWrapper>
+        <styledEl.BodyWrapper location={location}>
           <Polling />
           <TopLevelModals />
           <ReferralLinkUpdater />
           <RoutesApp />
-          <Marginer />
-        </BodyWrapper>
-        <FooterWrapper>
+          <styledEl.Marginer />
+        </styledEl.BodyWrapper>
+        <styledEl.FooterWrapper>
           <Footer />
-        </FooterWrapper>
-      </AppWrapper>
+        </styledEl.FooterWrapper>
+      </styledEl.AppWrapper>
     </ErrorBoundary>
   )
 }
