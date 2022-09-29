bbdacab61d0c3d7be5b9f58f28f4d24b155ba148
nenadV91 <nenadwebdevelop@gmail.com>
2022-09-29 13:58:17 +0200

    Initial layout for limit orders page

---
+++ src/cow-react/modules/limitOrders/pure/InfoPopup/index.tsx
@@ -0,0 +1,13 @@
+import * as styledEl from './styled'
+
+export function InfoPopup() {
+  return (
+    <styledEl.InfoPopup>
+      <b>How limit orders work</b>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora placeat praesentium ad laborum dolor, eveniet
+        consectetur unde sint a mollitia!
+      </p>
+    </styledEl.InfoPopup>
+  )
+}
--- src/cow-react/pages/LimitOrders/index.tsx
+++ src/cow-react/pages/LimitOrders/index.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import * as styledEl from './styled'
+
 import { LimitOrdersWidget } from 'cow-react/modules/limitOrders/containers/LimitOrdersWidget'
+import { Chart } from 'cow-react/modules/limitOrders/containers/Chart'
+import { Orders } from 'cow-react/modules/limitOrders/containers/Orders'
+import { InfoPopup } from 'cow-react/modules/limitOrders/pure/InfoPopup'
 
 export default function LimitOrderPage() {
-  return <LimitOrdersWidget />
+  return (
+    <styledEl.PageWrapper>
+      <styledEl.Column>
+        <LimitOrdersWidget />
+        <InfoPopup />
+      </styledEl.Column>
+
+      <styledEl.Column>
+        <Chart />
+        <Orders />
+      </styledEl.Column>
+    </styledEl.PageWrapper>
+  )
 }
