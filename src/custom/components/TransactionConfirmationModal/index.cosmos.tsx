443762cb63b5c0668354f652b3e4899ac2d2fcde
shoom3301 <shoom3301@gmail.com>
2022-10-24 15:54:57 +0600

    L2Content cosmos fixture

--- src/custom/components/TransactionConfirmationModal/index.cosmos.tsx
+++ src/custom/components/TransactionConfirmationModal/index.cosmos.tsx
@@ -1,4 +1,4 @@
-import { ConfirmationPendingContent, OperationType } from 'components/TransactionConfirmationModal'
+import { ConfirmationPendingContent, L2Content, OperationType } from 'components/TransactionConfirmationModal'
 import { COW, GNO } from 'constants/tokens'
 import { SupportedChainId } from 'constants/chains'
 import store from 'state'
@@ -23,9 +23,18 @@ const defaultProps = {
 const Wrapper = styled.div`
   background: #d5e9f0;
   border-radius: 16px;
+
+  > div {
+    box-sizing: border-box;
+  }
 `
 
 const Fixtures = {
+  L2Content: (
+    <Wrapper>
+      <L2Content {...defaultProps} chainId={100} />
+    </Wrapper>
+  ),
   TransactionSubmittedContent: (
     <Wrapper>
       <TransactionSubmittedContent {...defaultProps} />
