4109056039c3f79d9ca8fc83ff78fadd8ad7320f
w3stside <david@cow.fi>
2022-10-25 16:58:44 +0100

    storage for prev deadline amt

--- src/cow-react/modules/swap/state/EthFlow/updaters/deadlineUpdater.tsx
+++ src/cow-react/modules/swap/state/EthFlow/updaters/deadlineUpdater.tsx
@@ -1,14 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useUserTransactionTTL } from 'state/user/hooks'
 import { useIsEthFlow } from '@cow/modules/swap/hooks/useIsEthFlow'
 
+const STORAGE_KEY = 'UserPreviousDeadline'
 // 10 minutes in SECONDS
 export const DEADLINE_LOWER_THRESHOLD_SECONDS = 600
 
 export function EthFlowDeadlineUpdater() {
+  const [mounted, setMounted] = useState(false)
+
   // USER DEADLINE (IN SECONDS)
   const [userDeadline, setUserDeadline] = useUserTransactionTTL()
   const isEthFlow = useIsEthFlow()
+
+  // on updater mount
+  useEffect(() => {
+    const previousDeadline = _parsePreviousDeadlineFromStorage()
+
+    // only update if some storage amt exists
+    if (previousDeadline) {
+      setUserDeadline(previousDeadline)
+    }
+
+    setMounted(true)
+    // we only want this on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     if (isEthFlow) {
@@ -20,11 +37,30 @@ export function EthFlowDeadlineUpdater() {
           userDeadline,
           deadlineLessThanThreshold
         )
+        _saveDeadlineToStorage(userDeadline)
         setUserDeadline(DEADLINE_LOWER_THRESHOLD_SECONDS)
       }
+    } else if (mounted) {
+      _parsePreviousDeadlineFromStorageSet(setUserDeadline)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isEthFlow, userDeadline])
+  }, [isEthFlow])
 
   return null
+}
+
+function _saveDeadlineToStorage(currentUserDeadline: number) {
+  return localStorage.setItem(STORAGE_KEY, JSON.stringify(currentUserDeadline))
+}
+
+function _parsePreviousDeadlineFromStorage(): number | null {
+  const previousDeadlineStorage = localStorage.getItem(STORAGE_KEY)
+
+  return previousDeadlineStorage ? +JSON.parse(previousDeadlineStorage) : null
+}
+
+function _parsePreviousDeadlineFromStorageSet(setUserDeadline: (slippage: number) => void) {
+  const parsedDeadline = _parsePreviousDeadlineFromStorage()
+  // user switched back to non-native swap, set deadline back to previous value
+  parsedDeadline && setUserDeadline(parsedDeadline)
 }
