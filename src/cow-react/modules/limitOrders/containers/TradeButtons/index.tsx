007d6446bd32cc61e8cbfa2e298b75b8eb59fca7
shoom3301 <shoom3301@gmail.com>
2022-10-27 18:55:27 +0600

    Merge changes

--- src/cow-react/modules/limitOrders/containers/TradeButtons/index.tsx
+++ src/cow-react/modules/limitOrders/containers/TradeButtons/index.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback } from 'react'
 import { Trans } from '@lingui/macro'
 import { useAtomValue } from 'jotai/utils'
+import { useSetAtom } from 'jotai'
 import { tradeFlow, TradeFlowContext } from '@cow/modules/limitOrders/services/tradeFlow'
 import { limitOrdersSettingsAtom } from '@cow/modules/limitOrders/state/limitOrdersSettingsAtom'
 import { useLimitOrdersTradeState } from '@cow/modules/limitOrders/hooks/useLimitOrdersTradeState'
 import { useLimitOrdersFormState } from '../../hooks/useLimitOrdersFormState'
 import { limitOrdersTradeButtonsMap, SwapButton } from './limitOrdersTradeButtonsMap'
+import { limitOrdersConfirmState } from '../LimitOrdersConfirmModal/state'
 
 export interface TradeButtonsProps {
   tradeContext: TradeFlowContext | null
@@ -17,14 +19,16 @@ export function TradeButtons(props: TradeButtonsProps) {
   const { expertMode } = useAtomValue(limitOrdersSettingsAtom)
   const formState = useLimitOrdersFormState()
   const tradeState = useLimitOrdersTradeState()
+  const setConfirmationState = useSetAtom(limitOrdersConfirmState)
 
   const doTrade = useCallback(() => {
-    if (expertMode) {
-      tradeContext && tradeFlow(tradeContext)
+    if (expertMode && tradeContext) {
+      setConfirmationState({ isPending: true, orderHash: null })
+      tradeFlow(tradeContext)
     } else {
       openConfirmScreen()
     }
-  }, [expertMode, tradeContext, openConfirmScreen])
+  }, [expertMode, tradeContext, openConfirmScreen, setConfirmationState])
 
   const button = limitOrdersTradeButtonsMap[formState]
 
