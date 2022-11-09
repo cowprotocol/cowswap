1714d9dd30b83ca76d99df0364c9bdd6b88103ff
Nenad Vracar <34926005+nenadV91@users.noreply.github.com>
2022-11-09 11:36:42 +0100

    Fix some smaller bugs with limit orders (#1288)

--- src/cow-react/modules/limitOrders/updaters/ActiveRateUpdater/index.ts
+++ src/cow-react/modules/limitOrders/updaters/ActiveRateUpdater/index.ts
@@ -6,36 +6,50 @@ import usePrevious from 'hooks/usePrevious'
 import { useUpdateCurrencyAmount } from '@cow/modules/limitOrders/hooks/useUpdateCurrencyAmount'
 import { limitRateAtom, updateLimitRateAtom } from '@cow/modules/limitOrders/state/limitRateAtom'
 import { useLimitOrdersTradeState } from '@cow/modules/limitOrders/hooks/useLimitOrdersTradeState'
+import { updateLimitOrdersAtom } from '@cow/modules/limitOrders/state/limitOrdersAtom'
 
 // Observe the activeRate value changes
 // Re-trigger the input field to apply the new rate to output
 export function ActiveRateUpdater() {
   const { chainId } = useWeb3React()
   const { inputCurrencyAmount } = useLimitOrdersTradeState()
-  const { isInversed, activeRate } = useAtomValue(limitRateAtom)
+  const { isInversed, activeRate, isLoading } = useAtomValue(limitRateAtom)
 
+  const updateLimitOrdersState = useUpdateAtom(updateLimitOrdersAtom)
   const updateLimitRateState = useUpdateAtom(updateLimitRateAtom)
   const updateCurrencyAmount = useUpdateCurrencyAmount()
   const prevIsInversed = usePrevious(isInversed)
   const prevChainId = usePrevious(chainId)
 
-  // Handle activeRate changes
   useEffect(() => {
+    // Handle active rate change
     if (isInversed === prevIsInversed && activeRate && inputCurrencyAmount) {
       updateCurrencyAmount({
         inputCurrencyAmount: inputCurrencyAmount?.toExact(),
         keepOrderKind: true,
       })
     }
+
+    // Clear output amount when there is no active rate
+    if (!activeRate) {
+      updateLimitOrdersState({
+        outputCurrencyAmount: null,
+      })
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeRate])
 
-  // Clear active rate on network change
   useEffect(() => {
+    // Clear active rate on network change
     if (prevChainId && prevChainId !== chainId) {
       updateLimitRateState({ activeRate: null })
     }
-  }, [chainId, prevChainId, updateLimitRateState])
+
+    // Clear active rate when its loading (currency changed)
+    if (isLoading) {
+      updateLimitRateState({ activeRate: null })
+    }
+  }, [chainId, isLoading, prevChainId, updateLimitRateState])
 
   return null
 }
