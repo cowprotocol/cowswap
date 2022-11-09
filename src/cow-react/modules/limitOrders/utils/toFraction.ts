3dc248e64163fadd35b291288c8e66f3d65f2393
shoom3301 <shoom3301@gmail.com>
2022-11-09 15:21:36 +0600

    Simplify toFraction

--- src/cow-react/modules/limitOrders/utils/toFraction.ts
+++ src/cow-react/modules/limitOrders/utils/toFraction.ts
@@ -1,16 +1,12 @@
 import { Fraction } from '@uniswap/sdk-core'
 import F from 'fraction.js'
 
-export function toFraction(value: string, isInversed = false) {
+export function toFraction(value: string, isInversed = false): Fraction {
   if (!value || !Number(value)) return new Fraction(0)
 
-  const f = new F(value)
-
-  const { n: numerator, d: denominator } = f
+  const { n: numerator, d: denominator } = new F(value)
 
   const params: [number, number] = !isInversed ? [numerator, denominator] : [denominator, numerator]
 
-  const fraction = new Fraction(...params)
-
-  return fraction
+  return new Fraction(...params)
 }
