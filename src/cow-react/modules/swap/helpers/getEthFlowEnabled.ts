5331f6459c275185cc97a119c22a680432e2fb04
Alfetopito <alfetopito@users.noreply.github.com>
2022-11-10 17:58:42 +0000

    Force-prevent it from being enabled when on Prod

--- src/cow-react/modules/swap/helpers/getEthFlowEnabled.ts
+++ src/cow-react/modules/swap/helpers/getEthFlowEnabled.ts
@@ -1,5 +1,7 @@
+import { isProd } from 'utils/environments'
+
 const ETH_FLOW_ENABLED_LOCAL_STORAGE_KEY = 'enableEthFlow'
 
 export function getEthFlowEnabled(): boolean {
-  return localStorage.getItem(ETH_FLOW_ENABLED_LOCAL_STORAGE_KEY) === '1'
+  return !isProd && localStorage.getItem(ETH_FLOW_ENABLED_LOCAL_STORAGE_KEY) === '1'
 }
