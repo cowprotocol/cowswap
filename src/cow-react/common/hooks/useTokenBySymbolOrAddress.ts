c35a0bb37faa6c505549c633659b203173077645
shoom3301 <shoom3301@gmail.com>
2022-11-10 12:57:52 +0600

    Search token by symbol or address with taking into account favourite tokens

--- src/cow-react/common/hooks/useTokenBySymbolOrAddress.ts
+++ src/cow-react/common/hooks/useTokenBySymbolOrAddress.ts
@@ -2,10 +2,15 @@ import { NativeCurrency, Token } from '@uniswap/sdk-core'
 import { useMemo } from 'react'
 import { useAllTokensList } from 'cow-react/common/hooks/useAllTokensList'
 import useNativeCurrency from 'lib/hooks/useNativeCurrency'
+import { useFavouriteTokens } from 'state/user/hooks'
 
 export function useTokenBySymbolOrAddress(symbolOrAddress?: string | null): Token | NativeCurrency | null {
   const tokens = useAllTokensList()
   const nativeCurrency = useNativeCurrency()
+  const favouriteTokens = useFavouriteTokens()
+  const checkBySymbolAndAddress = (token: Token, symbolOrAddress: string) =>
+    token.address.toLowerCase() === symbolOrAddress.toLowerCase() ||
+    token.symbol?.toLowerCase() === symbolOrAddress.toLowerCase()
 
   return useMemo(() => {
     if (!symbolOrAddress) {
@@ -17,9 +22,9 @@ export function useTokenBySymbolOrAddress(symbolOrAddress?: string | null): Toke
     }
 
     return (
-      tokens.find(
-        (item) => item.address.toLowerCase() === symbolOrAddress.toLowerCase() || item.symbol === symbolOrAddress
-      ) || null
+      tokens.find((item) => checkBySymbolAndAddress(item, symbolOrAddress)) ||
+      favouriteTokens.find((item) => checkBySymbolAndAddress(item, symbolOrAddress)) ||
+      null
     )
-  }, [symbolOrAddress, nativeCurrency, tokens])
+  }, [symbolOrAddress, nativeCurrency, tokens, favouriteTokens])
 }
