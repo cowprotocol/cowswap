0d31a0a8425891d23b1a9f1fa4bd188c68444a88
shoom3301 <shoom3301@gmail.com>
2022-11-10 12:16:25 +0600

    [Swap] Redirect from the old URL format to a new one

--- src/cow-react/pages/NewSwap/index.tsx
+++ src/cow-react/pages/NewSwap/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 import { NewSwapWidget } from '@cow/modules/swap/containers/NewSwapWidget'
+import { Redirect, RouteComponentProps } from 'react-router-dom'
+import { SupportedChainId } from 'constants/chains'
+import { WRAPPED_NATIVE_CURRENCY as WETH } from 'constants/tokens'
+import { parameterizeTradeRoute } from '@cow/modules/trade/utils/parameterizeTradeRoute'
+import { Routes } from '@cow/constants/routes'
 
 export function NewSwapPage() {
   return <NewSwapWidget />
+}
+
+export function NewSwapPageRedirect({ location }: RouteComponentProps) {
+  const chainId = SupportedChainId.MAINNET
+  const inputCurrencyId = WETH[chainId].symbol
+
+  const pathname = parameterizeTradeRoute(
+    { chainId: String(chainId), inputCurrencyId, outputCurrencyId: undefined },
+    Routes.SWAP
+  )
+
+  return <Redirect to={{ ...location, pathname }} />
 }
