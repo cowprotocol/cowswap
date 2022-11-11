eb557d38b623e26c3913d4bd28eb5ad67048cae0
Alfetopito <alfetopito@users.noreply.github.com>
2022-11-11 17:54:53 +0000

    Refactor constant to use ms.macro for a ms value

--- src/custom/state/orders/consts.ts
+++ src/custom/state/orders/consts.ts
@@ -1,5 +1,6 @@
 import { Percent } from '@uniswap/sdk-core'
 import { SupportedChainId as ChainId } from 'constants/chains'
+import ms from 'ms.macro'
 
 export const ContractDeploymentBlocks: Partial<Record<ChainId, number>> = {
   [ChainId.MAINNET]: 11469934,
@@ -7,7 +8,7 @@ export const ContractDeploymentBlocks: Partial<Record<ChainId, number>> = {
   [ChainId.GNOSIS_CHAIN]: 13566914,
 }
 
-export const OPERATOR_API_POLL_INTERVAL = 2000 // in ms
-export const PENDING_ORDERS_PRICE_CHECK_POLL_INTERVAL = 15000 // in ms
+export const OPERATOR_API_POLL_INTERVAL = ms`2s` // in ms
+export const PENDING_ORDERS_PRICE_CHECK_POLL_INTERVAL = ms`15s` // in ms
 
 export const OUT_OF_MARKET_PRICE_DELTA_PERCENTAGE = new Percent(1, 100) // 1/100 => 0.01 => 1%
