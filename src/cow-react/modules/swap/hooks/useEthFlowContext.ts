4c326bca013cb8883ffbc60993fb44100b073166
Alfetopito <alfetopito@users.noreply.github.com>
2022-11-15 10:42:51 +0000

    We actually need to have WETH in EthFlow orders for the contract signature to match

--- src/cow-react/modules/swap/hooks/useEthFlowContext.ts
+++ src/cow-react/modules/swap/hooks/useEthFlowContext.ts
@@ -2,7 +2,7 @@ import { OrderKind } from '@cowprotocol/contracts'
 import { useEthFlowContract } from 'hooks/useContract'
 import { useBaseFlowContextSetup, getFlowContext } from '@cow/modules/swap/hooks/useFlowContext'
 import { EthFlowContext } from '@cow/modules/swap/services/common/types'
-import { NATIVE_CURRENCY_BUY_TOKEN } from 'constants/index'
+import { WRAPPED_NATIVE_CURRENCY } from 'constants/tokens'
 
 export function useEthFlowContext(): EthFlowContext | null {
   const contract = useEthFlowContract()
@@ -10,7 +10,7 @@ export function useEthFlowContext(): EthFlowContext | null {
 
   const baseContext = getFlowContext({
     baseProps,
-    sellToken: baseProps.chainId ? NATIVE_CURRENCY_BUY_TOKEN[baseProps.chainId] : undefined,
+    sellToken: baseProps.chainId ? WRAPPED_NATIVE_CURRENCY[baseProps.chainId] : undefined,
     kind: OrderKind.SELL,
   })
 
