bd92032934364fe00c1914075e0bf0a7c5aa84b3
shoom3301 <shoom3301@gmail.com>
2022-11-17 20:23:44 +0600

    Set proper kind for limit-order

--- src/cow-react/modules/limitOrders/hooks/useTradeFlowContext.ts
+++ src/cow-react/modules/limitOrders/hooks/useTradeFlowContext.ts
@@ -1,6 +1,5 @@
 import { TradeFlowContext } from '@cow/modules/limitOrders/services/tradeFlow'
 import { useWeb3React } from '@web3-react/core'
-import { OrderKind } from '@cowprotocol/contracts'
 import { CurrencyAmount, Token } from '@uniswap/sdk-core'
 import { useWalletInfo } from 'hooks/useWalletInfo'
 import { useGP2SettlementContract } from 'hooks/useContract'
@@ -50,7 +49,7 @@ export function useTradeFlowContext(): TradeFlowContext | null {
     isGnosisSafeWallet,
     dispatch,
     postOrderParams: {
-      kind: OrderKind.SELL,
+      kind: state.orderKind,
       account,
       chainId,
       sellToken,
