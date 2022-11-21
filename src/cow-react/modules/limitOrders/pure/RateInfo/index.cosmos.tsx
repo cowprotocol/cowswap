4b771b74f5b700f8c8cba6efa04df0ff688fb29e
shoom3301 <shoom3301@gmail.com>
2022-11-21 20:15:13 +0600

    Improve SmartQuoteSelection cosmos

--- src/cow-react/modules/limitOrders/pure/RateInfo/index.cosmos.tsx
+++ src/cow-react/modules/limitOrders/pure/RateInfo/index.cosmos.tsx
@@ -47,35 +47,64 @@ const Box = styled.div`
 
   p {
     margin-top: 0;
+    font-size: 13px;
+    color: #888888;
   }
 `
 
 function SmartQuoteSelection() {
   const rates = [
-    buildActiveRateDisplay(DAI_GOERLI, GNO_GOERLI, 2000, 100),
-    buildActiveRateDisplay(GNO_GOERLI, DAI_GOERLI, 2000, 100),
-    buildActiveRateDisplay(USDT_GOERLI, COW_GOERLI, 6704, 12),
-    buildActiveRateDisplay(COW_GOERLI, USDT_GOERLI, 100, 6433),
-    buildActiveRateDisplay(GNO_GOERLI, WETH_GOERLI, 12, 600),
-    buildActiveRateDisplay(WETH_GOERLI, GNO_GOERLI, 600, 12),
-    buildActiveRateDisplay(WBTC_GOERLI, GNO_GOERLI, 5, 2500),
-    buildActiveRateDisplay(GNO_GOERLI, WBTC_GOERLI, 2500, 5),
+    {
+      title: 'When one of tokens is stable-coin, then another token is anchor',
+      examples: [
+        buildActiveRateDisplay(DAI_GOERLI, GNO_GOERLI, 2000, 100),
+        buildActiveRateDisplay(GNO_GOERLI, DAI_GOERLI, 2000, 100),
+        buildActiveRateDisplay(USDT_GOERLI, WETH_GOERLI, 6704, 12),
+        buildActiveRateDisplay(WETH_GOERLI, USDT_GOERLI, 100, 6433),
+      ],
+    },
+    {
+      title: `When one of tokens is BTC or wrapped native, then it's anchor`,
+      examples: [
+        buildActiveRateDisplay(GNO_GOERLI, WETH_GOERLI, 12, 600),
+        buildActiveRateDisplay(WETH_GOERLI, GNO_GOERLI, 600, 12),
+        buildActiveRateDisplay(WBTC_GOERLI, COW_GOERLI, 5, 2500),
+        buildActiveRateDisplay(COW_GOERLI, WBTC_GOERLI, 2500, 5),
+      ],
+    },
+    {
+      title: 'For other cases the anchor is a token that has the smallest amount',
+      examples: [
+        buildActiveRateDisplay(COW_GOERLI, GNO_GOERLI, 12, 652),
+        buildActiveRateDisplay(GNO_GOERLI, COW_GOERLI, 2, 4220),
+      ],
+    },
   ]
 
   return (
     <div>
-      <h3>Smart quote selection:</h3>
-      {rates.map((rate, i) => {
-        const { inputCurrencyAmount, outputCurrencyAmount } = rate
+      <h3>Smart quote selection</h3>
+      <p>Anchor token it&apos;s a token that always = 1. For example: 1 WETH = 0.333 DAI, WETH is anchor</p>
+      <br />
+      {rates.map(({ title, examples }, j) => {
         return (
-          <Box key={i}>
-            <p>
-              {inputCurrencyAmount?.toExact()} {inputCurrencyAmount?.currency.symbol}
-              {'->'}
-              {outputCurrencyAmount?.toExact()} {outputCurrencyAmount?.currency.symbol}{' '}
-            </p>
-            <RateInfo noLabel={true} activeRateDisplay={rate} />
-          </Box>
+          <div key={j}>
+            <p>{title}</p>
+            {examples.map((rate, i) => {
+              const { inputCurrencyAmount, outputCurrencyAmount } = rate
+
+              return (
+                <Box key={i}>
+                  <p>
+                    {inputCurrencyAmount?.toExact()} {inputCurrencyAmount?.currency.symbol}
+                    {' -> '}
+                    {outputCurrencyAmount?.toExact()} {outputCurrencyAmount?.currency.symbol}{' '}
+                  </p>
+                  <RateInfo noLabel={true} activeRateDisplay={rate} />
+                </Box>
+              )
+            })}
+          </div>
         )
       })}
     </div>
