ae7a6d70963e771904f5e70322a6f169f2600a24
Alexandr Kazachenko <shoom3301@gmail.com>
2022-11-22 13:03:41 +0600

    Limit orders price impact warning (#1414)
    * Price impact warning for Limit orders
    
    * Remove unused code
    
    * TradeWarning component
    
    * Display Price Impact Warning n Limit orders page
    
    * Confirm price impact before trade
    
    * Display price impact percent on confirmation screen
    
    * Fix rate info icon color
    
    * Comments
    
    * Fix price impact displaying conditions
    
    * Use ms instead of numbers
    
    * Fix PriceImpact unknown banner displaying
    
    * Fix Price Impact request validTo value
    
    * Fix expert mode button id

--- src/cow-react/modules/limitOrders/hooks/useRateImpact.ts
+++ src/cow-react/modules/limitOrders/hooks/useRateImpact.ts
@@ -6,7 +6,7 @@ export function useRateImpact(): number {
   const { activeRate, executionRate, isLoading, isLoadingExecutionRate } = useAtomValue(limitRateAtom)
 
   return useMemo(() => {
-    if (!activeRate || !executionRate || isLoading || isLoadingExecutionRate) return 0
+    if (!activeRate || activeRate.equalTo(0) || !executionRate || isLoading || isLoadingExecutionRate) return 0
 
     const ratePercent = +activeRate.divide(executionRate).multiply(100).subtract(100).toFixed(1)
 
