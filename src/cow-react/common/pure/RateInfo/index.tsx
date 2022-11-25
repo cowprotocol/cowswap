6dfb99bb96e86951eb55d665268d0b6523169aa9
nenadV91 <nenadwebdevelop@gmail.com>
2022-11-25 16:53:08 +0100

    Fix rate title

--- src/cow-react/common/pure/RateInfo/index.tsx
+++ src/cow-react/common/pure/RateInfo/index.tsx
@@ -147,7 +147,7 @@ export function RateInfo({
       )}
       <div>
         <RateWrapper onClick={() => setCurrentIsInversed((state) => !state)}>
-          <span title={currentActiveRate.toSignificant(18) + ' ' + rateInputCurrency.symbol}>
+          <span title={currentActiveRate.toSignificant(18) + ' ' + rateOutputCurrency.symbol}>
             <LightText>1 {rateInputCurrency.symbol}</LightText> = {formatSmart(currentActiveRate)}{' '}
             {rateOutputCurrency.symbol}
           </span>{' '}
