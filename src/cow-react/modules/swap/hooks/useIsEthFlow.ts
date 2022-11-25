5b7032b36c6e57d2418b71b5870352fbffbfb431
Alfetopito <alfetopito@users.noreply.github.com>
2022-11-25 11:08:50 +0000

    Created hook to identify when it's an ethflow buy order

--- src/cow-react/modules/swap/hooks/useIsEthFlow.ts
+++ src/cow-react/modules/swap/hooks/useIsEthFlow.ts
@@ -1,6 +1,8 @@
 import { useDetectNativeToken } from '@cow/modules/swap/hooks/useDetectNativeToken'
 import { getEthFlowEnabled } from '@cow/modules/swap/helpers/getEthFlowEnabled'
 import { useIsSmartContractWallet } from '@cow/common/hooks/useIsSmartContractWallet'
+import { useSwapState } from 'state/swap/hooks'
+import { Field } from 'state/swap/actions'
 
 export function useIsEthFlow(): boolean {
   const { isNativeIn, isWrapOrUnwrap } = useDetectNativeToken()
@@ -8,4 +10,12 @@ export function useIsEthFlow(): boolean {
   const isEnabled = getEthFlowEnabled(isSmartContractWallet)
 
   return isEnabled && isNativeIn && !isWrapOrUnwrap
+}
+
+export function useIsEthFlowBuyOrder(): boolean {
+  const isEthFlow = useIsEthFlow()
+  const { isNativeIn } = useDetectNativeToken()
+  const { independentField } = useSwapState()
+
+  return isEthFlow && isNativeIn && independentField === Field.OUTPUT
 }
