2c86562c3bb4cf9d9cf9aad431f52134d034864b
Alfetopito <alfetopito@users.noreply.github.com>
2022-11-25 16:48:46 +0000

    Removed hook useIsEthFlowBuyOrder no longer in use

--- src/cow-react/modules/swap/hooks/useIsEthFlow.ts
+++ src/cow-react/modules/swap/hooks/useIsEthFlow.ts
@@ -1,8 +1,6 @@
 import { useDetectNativeToken } from '@cow/modules/swap/hooks/useDetectNativeToken'
 import { getEthFlowEnabled } from '@cow/modules/swap/helpers/getEthFlowEnabled'
 import { useIsSmartContractWallet } from '@cow/common/hooks/useIsSmartContractWallet'
-import { useSwapState } from 'state/swap/hooks'
-import { Field } from 'state/swap/actions'
 
 export function useIsEthFlow(): boolean {
   const { isNativeIn, isWrapOrUnwrap } = useDetectNativeToken()
@@ -10,12 +8,4 @@ export function useIsEthFlow(): boolean {
   const isEnabled = getEthFlowEnabled(isSmartContractWallet)
 
   return isEnabled && isNativeIn && !isWrapOrUnwrap
-}
-
-export function useIsEthFlowBuyOrder(): boolean {
-  const isEthFlow = useIsEthFlow()
-  const { isNativeIn } = useDetectNativeToken()
-  const { independentField } = useSwapState()
-
-  return isEthFlow && isNativeIn && independentField === Field.OUTPUT
 }
