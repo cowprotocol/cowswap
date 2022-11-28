eabc9fd170cf7a0f27cc22c7121f37a7f16f88cf
Alfetopito <alfetopito@users.noreply.github.com>
2022-11-28 15:44:03 +0000

    Enabled ETH flow without the flag for testing

--- src/cow-react/modules/swap/helpers/getEthFlowEnabled.ts
+++ src/cow-react/modules/swap/helpers/getEthFlowEnabled.ts
@@ -1,7 +1,8 @@
 import { isProd } from 'utils/environments'
 
-const ETH_FLOW_ENABLED_LOCAL_STORAGE_KEY = 'enableEthFlow'
+// const ETH_FLOW_ENABLED_LOCAL_STORAGE_KEY = 'enableEthFlow'
 
+// TODO: disabled only for testing, DO NOT MERGE!!!
 export function getEthFlowEnabled(isSmartContractWallet: boolean): boolean {
-  return !isProd && !isSmartContractWallet && localStorage.getItem(ETH_FLOW_ENABLED_LOCAL_STORAGE_KEY) === '1'
+  return !isProd && !isSmartContractWallet
 }
