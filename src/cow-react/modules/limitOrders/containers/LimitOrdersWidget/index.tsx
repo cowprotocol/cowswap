49dd5475cb04ffca3c2b0f1509fe53057e108cc3
shoom3301 <shoom3301@gmail.com>
2022-11-29 19:23:31 +0600

    Hide "Add a recipient" after clicking on it

--- src/cow-react/modules/limitOrders/containers/LimitOrdersWidget/index.tsx
+++ src/cow-react/modules/limitOrders/containers/LimitOrdersWidget/index.tsx
@@ -160,7 +160,7 @@ export function LimitOrdersWidget() {
                   isLoading={isTradePriceUpdating}
                   hasSeparatorLine={true}
                 />
-                {showRecipient && <AddRecipient onChangeRecipient={onChangeRecipient} />}
+                {showRecipient && recipient === null && <AddRecipient onChangeRecipient={onChangeRecipient} />}
               </styledEl.CurrencySeparatorBox>
               <CurrencyInputPanel
                 id="swap-currency-output"
