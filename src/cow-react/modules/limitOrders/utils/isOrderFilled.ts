16b9d0021c8a3be0a6e1ed2b8f45b4bd5bf24984
Nenad Vracar <34926005+nenadV91@users.noreply.github.com>
2022-11-30 11:42:19 +0100

    Limit orders receipt filled field (#1537)
    * Add isOrderFilled util function
    
    * Fix filled and from amounts
    
    * PR comments

---
+++ src/cow-react/modules/limitOrders/utils/isOrderFilled.ts
@@ -0,0 +1,26 @@
+import { Order } from 'state/orders/actions'
+import { BigNumber } from 'bignumber.js'
+
+const ONE_BIG_NUMBER = new BigNumber(1)
+const FILLED_ORDER_EPSILON = new BigNumber('0.0001')
+
+export function isOrderFilled(order: Order): boolean {
+  let amount, executedAmount
+  const { apiAdditionalInfo } = order
+
+  if (!apiAdditionalInfo) {
+    return false
+  }
+
+  if (order.kind === 'buy') {
+    amount = new BigNumber(order.buyAmount.toString())
+    executedAmount = new BigNumber(apiAdditionalInfo.executedBuyAmount)
+  } else {
+    amount = new BigNumber(order.sellAmount.toString())
+    executedAmount = new BigNumber(apiAdditionalInfo.executedSellAmount).minus(apiAdditionalInfo.executedFeeAmount)
+  }
+
+  const minimumAmount = amount.multipliedBy(ONE_BIG_NUMBER.minus(FILLED_ORDER_EPSILON))
+
+  return executedAmount.gte(minimumAmount)
+}
