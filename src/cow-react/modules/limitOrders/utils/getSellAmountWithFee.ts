2e4c8ab8c02629565a2c4b9c15b0fadd4ed97b4c
nenadV91 <nenadwebdevelop@gmail.com>
2022-11-30 13:01:29 +0100

    Add return type

--- src/cow-react/modules/limitOrders/utils/getSellAmountWithFee.ts
+++ src/cow-react/modules/limitOrders/utils/getSellAmountWithFee.ts
@@ -1,7 +1,7 @@
 import { Order } from '@src/custom/state/orders/actions'
-import { CurrencyAmount } from '@uniswap/sdk-core'
+import { CurrencyAmount, Token } from '@uniswap/sdk-core'
 
-export function getSellAmountWithFee(order: Order) {
+export function getSellAmountWithFee(order: Order): CurrencyAmount<Token> {
   const feeAmountParsed = CurrencyAmount.fromRawAmount(order.inputToken, order.feeAmount.toString())
   const sellAmountParsed = CurrencyAmount.fromRawAmount(order.inputToken, order.sellAmount.toString())
   return sellAmountParsed.add(feeAmountParsed)
