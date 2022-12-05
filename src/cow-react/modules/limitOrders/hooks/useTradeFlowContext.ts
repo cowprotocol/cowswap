cb83e448c3b9e71ca7aac9587954934bd2b91399
shoom3301 <shoom3301@gmail.com>
2022-12-05 14:42:50 +0600

    Reduce limit orders widget re-renders

--- src/cow-react/modules/limitOrders/hooks/useTradeFlowContext.ts
+++ src/cow-react/modules/limitOrders/hooks/useTradeFlowContext.ts
@@ -9,13 +9,11 @@ import { useAppData } from 'hooks/useAppData'
 import { LIMIT_ORDER_SLIPPAGE } from '@cow/modules/limitOrders/const/trade'
 import useENSAddress from 'hooks/useENSAddress'
 import { useLimitOrdersTradeState } from './useLimitOrdersTradeState'
-import { useLimitOrdersDeadline } from './useLimitOrdersDeadline'
 import { OrderClass } from '@src/custom/state/orders/actions'
 
 export function useTradeFlowContext(): TradeFlowContext | null {
   const { chainId, account, provider } = useWeb3React()
   const state = useLimitOrdersTradeState()
-  const deadlineTimestamp = useLimitOrdersDeadline()
   const { allowsOffchainSigning, gnosisSafeInfo } = useWalletInfo()
   const settlementContract = useGP2SettlementContract()
   const dispatch = useDispatch<AppDispatch>()
@@ -49,6 +47,7 @@ export function useTradeFlowContext(): TradeFlowContext | null {
     allowsOffchainSigning,
     isGnosisSafeWallet,
     dispatch,
+    provider,
     postOrderParams: {
       class: OrderClass.LIMIT,
       kind: state.orderKind,
@@ -56,12 +55,10 @@ export function useTradeFlowContext(): TradeFlowContext | null {
       chainId,
       sellToken,
       buyToken,
-      validTo: deadlineTimestamp,
       recipient,
       recipientAddressOrName,
       allowsOffchainSigning,
       feeAmount,
-      signer: provider.getSigner(),
       inputAmount: state.inputCurrencyAmount,
       outputAmount: state.outputCurrencyAmount,
       sellAmountBeforeFee: state.inputCurrencyAmount,
