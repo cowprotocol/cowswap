02ecbbac566eb2c30e451c2595f8881a598d3707
Anxo Rodriguez <anxolin@gmail.com>
2022-12-06 15:37:04 +0000

    Reduce deadline options

--- src/cow-react/modules/limitOrders/pure/DeadlineSelector/deadlines.ts
+++ src/cow-react/modules/limitOrders/pure/DeadlineSelector/deadlines.ts
@@ -14,7 +14,4 @@ export const limitOrdersDeadlines: LimitOrderDeadline[] = [
   { title: '30 Minutes', value: ms`30m` },
   defaultLimitOrderDeadline,
   { title: '1 Day', value: ms`1d` },
-  { title: '3 Days', value: ms`3d` },
-  { title: '7 Days', value: ms`7d` },
-  { title: '1 Month', value: ms`30d` },
 ]
