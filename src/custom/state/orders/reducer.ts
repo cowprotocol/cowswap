bc1dbe8fb10b08e9adfec40c6fe83ff2105bb14e
Alfetopito <alfetopito@users.noreply.github.com>
2022-12-06 15:53:56 +0000

    Added new order reducer action setOrderCancellationHash

--- src/custom/state/orders/reducer.ts
+++ src/custom/state/orders/reducer.ts
@@ -14,6 +14,7 @@ import {
   requestOrderCancellation,
   SerializedOrder,
   setIsOrderUnfillable,
+  setOrderCancellationHash,
   updateLastCheckedBlock,
   updatePresignGnosisSafeTx,
 } from './actions'
@@ -316,6 +317,16 @@ export default createReducer(initialState, (builder) =>
           addOrderToState(state, chainId, id, 'expired', orderObject.order)
         }
       })
+    })
+    .addCase(setOrderCancellationHash, (state, action) => {
+      prefillState(state, action)
+      const { id, chainId, hash } = action.payload
+
+      const orderObject = getOrderById(state, chainId, id)
+
+      if (orderObject) {
+        orderObject.order.cancellationHash = hash
+      }
     })
     .addCase(requestOrderCancellation, (state, action) => {
       prefillState(state, action)
