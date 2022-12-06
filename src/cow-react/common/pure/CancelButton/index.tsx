1a73337aba7f34ed542702ee735a61ed59bda3bf
Alfetopito <alfetopito@users.noreply.github.com>
2022-12-06 16:20:30 +0000

    Simplified pure CancelButton

--- src/cow-react/common/pure/CancelButton/index.tsx
+++ src/cow-react/common/pure/CancelButton/index.tsx
@@ -1,33 +1,9 @@
 import { LinkStyledButton } from 'theme'
-import { CancellationModal } from './CancellationModal'
-
-// TODO: add for each type when implemented
-export type CancelButtonType = 'soft'
 
 export type CancelButtonProps = {
-  orderId: string
-  summary: string | undefined
-  chainId: number
-  error: string | null
-  showCancelModal: boolean
-  type: CancelButtonType
-  softCancellationContext?: SoftCancellationContext
-  onCancelButtonClick: () => void
-  onDismiss: () => void
+  onClick: () => void
 }
 
-export type SoftCancellationContext = {
-  cancelOrder: () => void
-  isWaitingSignature: boolean
-}
-
-export function CancelButton(props: CancelButtonProps) {
-  const { showCancelModal, onCancelButtonClick, ...rest } = props
-
-  return (
-    <>
-      <LinkStyledButton onClick={onCancelButtonClick}>Cancel order</LinkStyledButton>{' '}
-      {showCancelModal && <CancellationModal {...rest} />}
-    </>
-  )
+export function CancelButton({ onClick }: CancelButtonProps) {
+  return <LinkStyledButton onClick={onClick}>Cancel order</LinkStyledButton>
 }
