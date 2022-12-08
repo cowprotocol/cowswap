99964b2307cf184247b974e39956b543bfa51115
shoom3301 <shoom3301@gmail.com>
2022-12-08 15:10:48 +0600

    Use adjustDecimals for useApplyLimitRate()

--- src/cow-react/modules/limitOrders/hooks/useApplyLimitRate.ts
+++ src/cow-react/modules/limitOrders/hooks/useApplyLimitRate.ts
@@ -6,7 +6,7 @@ import { limitRateAtom } from '@cow/modules/limitOrders/state/limitRateAtom'
 import { Fraction } from '@uniswap/sdk-core'
 import { toFraction } from '@cow/modules/limitOrders/utils/toFraction'
 import { useLimitOrdersTradeState } from '@cow/modules/limitOrders/hooks/useLimitOrdersTradeState'
-import JSBI from 'jsbi'
+import { adjustDecimals } from '@cow/modules/limitOrders/utils/adjustDecimals'
 
 // Applies rate to provided value which can be INPUT or OUTPUT
 export function useApplyLimitRate() {
@@ -22,27 +22,11 @@ export function useApplyLimitRate() {
       const { decimals: inputDecimals } = inputCurrency
       const { decimals: outputDecimals } = outputCurrency
 
-      /**
-       * Consider a trade DAI (decimals 18) <-> USDC (decimals 6)
-       * 1. When input is 9 DAI, the value equals 9000000000000000000
-       * Then we should divide it to get value for USDC = 9000000 (6 decimals)
-       *
-       * 2. When input is 9 USDC, the value equals 9000000
-       * Then we should multiply to get value for DAI = 9000000000000000000 (18 decimals)
-       */
-      const shouldMultiplyInput = field === Field.INPUT && inputDecimals < outputDecimals
-      const shouldMultiplyOutput = field === Field.OUTPUT && outputDecimals < inputDecimals
-      const decimalsShift = JSBI.BigInt(10 ** Math.abs(inputDecimals - outputDecimals))
-
-      const parsedValue = ((fraction) => {
-        if (inputDecimals === outputDecimals) return fraction
-
-        if (shouldMultiplyInput || shouldMultiplyOutput) {
-          return fraction.multiply(decimalsShift)
-        }
-
-        return fraction.divide(decimalsShift)
-      })(toFraction(value))
+      const parsedValue = adjustDecimals(
+        toFraction(value),
+        field === Field.INPUT ? inputDecimals : outputDecimals,
+        field === Field.INPUT ? outputDecimals : inputDecimals
+      )
 
       if (field === Field.INPUT) {
         return parsedValue.multiply(activeRate)
