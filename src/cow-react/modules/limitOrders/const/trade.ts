84dc4af5061a62e726efdb1d83dd8b267b4a7477
nenadV91 <nenadwebdevelop@gmail.com>
2022-12-08 12:30:59 +0100

    Address comments from previous PR

--- src/cow-react/modules/limitOrders/const/trade.ts
+++ src/cow-react/modules/limitOrders/const/trade.ts
@@ -1,4 +1,6 @@
-import { Percent } from '@uniswap/sdk-core'
+import { Percent, Fraction } from '@uniswap/sdk-core'
 
 export const LIMIT_ORDER_SLIPPAGE = new Percent(0)
 export const LOW_RATE_THRESHOLD_PERCENT = -5
+export const MARKET_RATE_ADJUSTMENT = new Fraction(1, 1000) // 0.1%
+export const ONE = new Fraction(1)
