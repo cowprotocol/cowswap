11ff552a66f5443064e687fb17de3f03f5356581
nenadV91 <nenadwebdevelop@gmail.com>
2022-12-09 13:01:52 +0100

    Update faq links

--- src/cow-react/modules/limitOrders/pure/InfoPopup/index.tsx
+++ src/cow-react/modules/limitOrders/pure/InfoPopup/index.tsx
@@ -2,6 +2,7 @@ import * as styledEl from './styled'
 import { useState } from 'react'
 import AlertIcon from 'assets/cow-swap/alert-circle.svg'
 import SVG from 'react-inlinesvg'
+import { HashLink } from 'react-router-hash-link'
 
 const localStorageKey = 'LimitOrdersInfoPopupShown'
 
@@ -22,7 +23,7 @@ export function InfoPopup() {
       <div>
         CoW Swap will cover your fees by executing your order at a slightly better price than the limit price you set.
         For this reason, your order may not be filled exactly when the market price reaches your limit price.{' '}
-        {/*TODO HIDDEN: <a href="TODO">Learn more</a>*/}
+        <HashLink to="/faq/limit-order#how-do-fees-work">Learn more</HashLink>
       </div>
       <styledEl.CloseIcon onClick={closePopup} size={16} />
     </styledEl.InfoPopup>
