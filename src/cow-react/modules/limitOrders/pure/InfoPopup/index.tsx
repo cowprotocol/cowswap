0f51283c8bfd5ab4d1beb46fae8bfd91edf05a62
nenadV91 <nenadwebdevelop@gmail.com>
2022-12-13 12:25:34 +0100

    Update limit-order links

--- src/cow-react/modules/limitOrders/pure/InfoPopup/index.tsx
+++ src/cow-react/modules/limitOrders/pure/InfoPopup/index.tsx
@@ -1,7 +1,7 @@
 import * as styledEl from './styled'
 import AlertIcon from 'assets/cow-swap/alert-circle.svg'
 import SVG from 'react-inlinesvg'
-import { HashLink } from 'react-router-hash-link'
+import { ExternalLink } from 'theme'
 
 export function InfoPopup() {
   return (
@@ -13,9 +13,9 @@ export function InfoPopup() {
         CoW Swap will cover your gas fee by executing your order at a slightly better price than the limit price you
         set. For this reason, your order may not be filled exactly when the market price reaches your limit price.{' '}
         <br />
-        <HashLink to="/faq/limit-order#how-do-fees-work">
+        <ExternalLink href="https://cow-protocol.medium.com/the-cow-has-no-limits-342e7eae8794">
           <strong>Learn more</strong>
-        </HashLink>
+        </ExternalLink>
       </div>
     </styledEl.InfoPopup>
   )
