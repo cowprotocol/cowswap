e7ed8b788465c68acad0ddcf915a87e743ca9ac1
josojo <josojo@hotmail.de>
2022-12-15 11:20:10 +0100

    also updating the minimum ethflow deadline

--- src/custom/constants/index.ts
+++ src/custom/constants/index.ts
@@ -103,7 +103,7 @@ export const PRICE_API_TIMEOUT_MS = 10000 // 10s
 export const GP_ORDER_UPDATE_INTERVAL = 30 * 1000 // 30s
 export const MINIMUM_ORDER_VALID_TO_TIME_SECONDS = 120
 // Minimum deadline for EthFlow orders. Like the default deadline, anything smaller will be replaced by this
-export const MINIMUM_ETH_FLOW_DEADLINE_SECONDS = 600 // 10 minutes in SECONDS
+export const MINIMUM_ETH_FLOW_DEADLINE_SECONDS = 60 // 10 minutes in SECONDS
 
 export const WETH_LOGO_URI =
   'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png'
