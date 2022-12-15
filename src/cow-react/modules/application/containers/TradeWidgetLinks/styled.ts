c7e56cdd2d23bdedd40e3936c5021b647dd51baf
Alexandr Kazachenko <shoom3301@gmail.com>
2022-12-15 17:35:50 +0600

    Remove hover effect from the Beta label (#1712)
    * Remove hover effect from the Beta label
    
    * Fix styles
    
    * Fix styles

--- src/cow-react/modules/application/containers/TradeWidgetLinks/styled.ts
+++ src/cow-react/modules/application/containers/TradeWidgetLinks/styled.ts
@@ -2,20 +2,22 @@ import styled from 'styled-components/macro'
 import { NavLink } from 'react-router-dom'
 
 export const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
   font-size: 1rem;
   font-weight: 500;
   margin-right: 6px;
-  color: ${({ theme }) => (theme.darkMode ? theme.text1 : theme.text2)}};
 `
 
 export const Link = styled(NavLink)<{ isActive?: boolean }>`
   text-decoration: none;
   padding: 0 5px;
   opacity: 0.5;
-  display: flex;
-  align-items: center;
+  color: ${({ theme }) => (theme.darkMode ? theme.text1 : theme.text2)};
+  text-decoration-color: ${({ theme }) => theme.blue1}!important;
 
-  &.active {
+  &.active,
+  &.active + div {
     opacity: 1;
   }
 `
@@ -23,6 +25,8 @@ export const Link = styled(NavLink)<{ isActive?: boolean }>`
 export const Badge = styled.div`
   background-color: ${({ theme }) => theme.bg2};
   color: ${({ theme }) => theme.white};
+  pointer-events: none;
+  opacity: 0.5;
   border: none;
   cursor: pointer;
   border-radius: 16px;
