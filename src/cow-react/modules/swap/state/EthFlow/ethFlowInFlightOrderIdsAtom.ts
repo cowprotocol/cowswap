76352dd17340200dbb8b058e155a9895487cf446
Anxo Rodriguez <anxolin@gmail.com>
2022-12-16 19:36:18 +0000

    Change to v0

--- src/cow-react/modules/swap/state/EthFlow/ethFlowInFlightOrderIdsAtom.ts
+++ src/cow-react/modules/swap/state/EthFlow/ethFlowInFlightOrderIdsAtom.ts
@@ -1,7 +1,7 @@
 import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
-export const ethFlowInFlightOrderIdsAtom = atomWithStorage<string[]>('eth-flow-in-flight-order-ids:v1', [])
+export const ethFlowInFlightOrderIdsAtom = atomWithStorage<string[]>('eth-flow-in-flight-order-ids:v0', [])
 
 export const addInFlightOrderIdAtom = atom(null, (get, set, orderId: string) => {
   const orderIds = get(ethFlowInFlightOrderIdsAtom)
