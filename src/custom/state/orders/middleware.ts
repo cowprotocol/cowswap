25b583ae19effd9ae08e84e920b9564f1df2c6ea
Alfetopito <alfetopito@users.noreply.github.com>
2022-12-27 12:44:28 +0000

    Actually triggering sounds on failed txs

--- src/custom/state/orders/middleware.ts
+++ src/custom/state/orders/middleware.ts
@@ -217,6 +217,8 @@ export const soundMiddleware: Middleware<Record<string, unknown>, AppState> = (s
     cowSound = getCowSoundError()
   } else if (isCancelOrderAction(action)) {
     cowSound = getCowSoundError()
+  } else if (isFailedTxAction(action)) {
+    cowSound = getCowSoundError()
   }
 
   if (cowSound) {
@@ -226,6 +228,15 @@ export const soundMiddleware: Middleware<Record<string, unknown>, AppState> = (s
   }
 
   return result
+}
+
+const isAddPopup = isAnyOf(addPopup)
+
+/**
+ * Checks whether the action is `addPopup` for a `txn` which failed
+ */
+function isFailedTxAction(action: unknown): boolean {
+  return isAddPopup(action) && action.payload?.content?.txn?.success === false
 }
 
 export const appziMiddleware: Middleware<Record<string, unknown>, AppState> = (store) => (next) => (action) => {
