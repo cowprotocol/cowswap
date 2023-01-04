96e0e90bc86f1d89849e9aa8d5a15b9b3ec9c2b2
Alfetopito <alfetopito@users.noreply.github.com>
2023-01-04 14:39:58 +0000

    Revert "move winter edition into module (#1770)"
    This reverts commit 2776a6d51445c02b4928b7e0c21856f3b7a52c53.

--- src/cow-react/modules/application/containers/App/index.tsx
+++ src/cow-react/modules/application/containers/App/index.tsx
@@ -18,10 +18,17 @@ import RedirectAnySwapAffectedUsers from '@cow/pages/error/AnySwapAffectedUsers/
 import { RoutesApp } from './RoutesApp'
 import * as styledEl from './styled'
 
-import { WinterFooter } from '@cow/modules/winterEdition' // Winter Theme
+// Winter Theme
+import { useContext } from 'react'
+import { ThemeContext } from 'styled-components/macro'
+import Snowfall from 'react-snowfall'
+import { transparentize } from 'polished'
 
 export function App() {
   // const location = useLocation()
+
+  // Winter Theme
+  const theme = useContext(ThemeContext)
 
   initializeAnalytics()
 
@@ -45,7 +52,9 @@ export function App() {
         </styledEl.BodyWrapper>
         <styledEl.FooterWrapper>
           <Footer />
-          <WinterFooter /> {/* Winter Themed Footer */}
+
+          {/* Winter Theme */}
+          <Snowfall color={transparentize(0.7, theme.text1)} snowflakeCount={50} />
         </styledEl.FooterWrapper>
       </styledEl.AppWrapper>
     </ErrorBoundary>
