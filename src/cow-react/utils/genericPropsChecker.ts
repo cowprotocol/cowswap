cb1848867ebc0694ba43061586d5c2974a0ccb24
shoom3301 <shoom3301@gmail.com>
2023-01-05 16:12:12 +0600

    Use recursion to check props in genericPropsChecker()

--- src/cow-react/utils/genericPropsChecker.ts
+++ src/cow-react/utils/genericPropsChecker.ts
@@ -13,6 +13,24 @@ export function genericPropsChecker(prev: any, next: any): boolean {
       const prevValue = prev[key]
       const nextValue = next[key]
 
+      /* In case when value is object we go into recursion.
+      For example:
+
+      props = {
+        id: 10,
+        options: {
+          option1: { foo: 'bar' },
+          keys: [5, 6],
+        },
+      }
+
+      When we get `options` key we detect that it's an object and check it through genericPropsChecker again recursively
+      And we do it until we met not object value (primitive, array, function)
+      */
+      if (typeof nextValue === 'object' && !Array.isArray(nextValue) && nextValue !== null) {
+        return genericPropsChecker(prevValue, nextValue)
+      }
+
       // We shouldn't check functions using JSON.stringify because it always returns true
       if (typeof prevValue === 'function' || typeof nextValue === 'function') {
         return prevValue === nextValue
