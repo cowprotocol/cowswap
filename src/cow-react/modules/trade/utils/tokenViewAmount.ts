0d14cdf271eb4497d2ad0f946422bbfc193a4c72
shoom3301 <shoom3301@gmail.com>
2023-02-01 15:01:49 +0600

    Fix tokenViewAmount

--- src/cow-react/modules/trade/utils/tokenViewAmount.ts
+++ src/cow-react/modules/trade/utils/tokenViewAmount.ts
@@ -1,7 +1,13 @@
-import { Fraction } from '@uniswap/sdk-core'
-import { AMOUNT_PRECISION } from 'constants/index'
+import { Currency, CurrencyAmount, Fraction } from '@uniswap/sdk-core'
 import { Nullish } from '@cow/types'
+import { FULL_PRICE_PRECISION } from 'constants/index'
 
-export function tokenViewAmount(amount: Nullish<Fraction>): string {
-  return amount?.toSignificant(AMOUNT_PRECISION) || ''
+export function tokenViewAmount(amount: Nullish<CurrencyAmount<Currency> | Fraction>): string {
+  if (!amount) return ''
+
+  if (amount instanceof CurrencyAmount) {
+    return amount.toSignificant(amount.currency.decimals) || ''
+  }
+
+  return amount.toFixed(FULL_PRICE_PRECISION) || ''
 }
