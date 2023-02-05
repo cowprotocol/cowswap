55e94069e5590102da16f2ed55b3986ae91de060
shoom3301 <shoom3301@gmail.com>
2023-02-05 21:50:29 +0600

    Fix trimTrailingZeros

--- src/cow-react/utils/trimTrailingZeros.ts
+++ src/cow-react/utils/trimTrailingZeros.ts
@@ -1,5 +1,5 @@
 const trailingZerosRegex = /(\.\d*?[1-9])0*$/
 
 export function trimTrailingZeros(value: string): string {
-  return value.replace(trailingZerosRegex, '$1')
+  return value.replace(trailingZerosRegex, '$1').replace(/\.0*$/, '')
 }
