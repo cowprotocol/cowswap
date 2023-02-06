4e180ffd001abafba101d3fc1dadbaa3d138340c
shoom3301 <shoom3301@gmail.com>
2023-02-06 14:14:26 +0600

    Fix tests

--- src/cow-react/utils/trimTrailingZeros.ts
+++ src/cow-react/utils/trimTrailingZeros.ts
@@ -1,5 +1,20 @@
-const trailingZerosRegex = /(\.\d*?[1-9])0*$/
+const DOT = '.'
+const ZERO = '0'
 
 export function trimTrailingZeros(value: string): string {
-  return value.replace(trailingZerosRegex, '$1').replace(/\.0*$/, '')
+  if (!value.includes(DOT)) return value
+
+  const trimmed = value.slice(0, getFirstTrailingZeroIndex(value))
+
+  if (trimmed[trimmed.length - 1] === DOT) return trimmed.slice(0, -1)
+
+  return trimmed
+}
+
+function getFirstTrailingZeroIndex(value: string): number {
+  for (let i = value.length - 1; i > 0; i--) {
+    if (value[i] !== ZERO) return i + 1
+  }
+
+  return value.length
 }
