95f2ba528f80606be203ef13c122dafc538a7c27
shoom3301 <shoom3301@gmail.com>
2023-02-08 16:19:49 +0600

    Fix ethereumjs pathing script

--- scripts/fix-ethereumjs-bundle.js
+++ scripts/fix-ethereumjs-bundle.js
@@ -6,14 +6,23 @@ These files are included in the main bundle, but we don't need them.
 Because of it, we just override the content of the files by {}.
  */
 
-fs.writeFileSync(
-  path.resolve(
-    __dirname,
-    '../node_modules/@ethereumjs/tx/node_modules/@ethereumjs/common/dist.browser/genesisStates/mainnet.json'
-  ),
-  '{}'
-)
-fs.writeFileSync(
-  path.resolve(__dirname, '../node_modules/@ethereumjs/common/dist.browser/genesisStates/mainnet.json'),
-  '{}'
-)
+const sources = ['dist.browser', 'dist', 'src']
+const chains = ['mainnet', 'goerli', 'rinkeby', 'ropsten']
+
+const filesToReplace = [
+  (source, chain) => `../node_modules/@ethereumjs/common/${source}/genesisStates/${chain}.json`,
+  (source, chain) =>
+    `../node_modules/@web3-onboard/hw-common/node_modules/@ethereumjs/common/${source}/genesisStates/${chain}.json`,
+  (source, chain) =>
+    `../node_modules/web3-eth-accounts/node_modules/@ethereumjs/common/${source}/genesisStates/${chain}.json`,
+  (source, chain) =>
+    `../node_modules/web3-eth-accounts/node_modules/@ethereumjs/tx/node_modules/@ethereumjs/common/${source}/genesisStates/${chain}.json`,
+]
+
+filesToReplace.forEach((filePath) => {
+  sources.forEach((source) => {
+    chains.forEach((chain) => {
+      fs.writeFileSync(path.resolve(__dirname, filePath(source, chain)), '{}')
+    })
+  })
+})
