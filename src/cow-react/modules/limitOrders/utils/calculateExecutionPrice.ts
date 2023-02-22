cef6cb6ea5be91f5396ffde8d718e6e14a97d81a
shoom3301 <shoom3301@gmail.com>
2023-02-22 16:10:40 +0600

    Remove marketPrice restr

--- src/cow-react/modules/limitOrders/utils/calculateExecutionPrice.ts
+++ src/cow-react/modules/limitOrders/utils/calculateExecutionPrice.ts
@@ -70,5 +70,5 @@ export function calculateExecutionPrice(params: ExecutionPriceParams): Price<Cur
     quoteAmount: outputCurrencyAmount,
   })
 
-  return currentPrice.greaterThan(marketPrice) ? marketPrice : currentPrice
+  return currentPrice
 }
