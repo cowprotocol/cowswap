deec06da5ef18fb41de30564543d000504d21a5e
Anxo Rodriguez <anxolin@gmail.com>
2023-02-24 23:55:39 +0000

    Move web3status

--- src/cow-react/modules/wallet/index.ts
+++ src/cow-react/modules/wallet/index.ts
@@ -2,7 +2,7 @@
 export * from './web3-react/hooks/useWalletInfo'
 export * from './web3-react/hooks/useDisconnectWallet'
 
-export * from './web3-react/Web3Status'
+export * from './web3-react/containers/Web3Status'
 export * from './web3-react/containers/WalletModal'
 export * from './web3-react/containers/Identicon'
 export * from './web3-react/containers/AddToMetamask'
