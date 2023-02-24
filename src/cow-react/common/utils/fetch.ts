a214863e36fba8915a9f6bb5ea377de7017308fb
Lint Action <lint-action@samuelmeuli.com>
2023-02-24 10:59:07 +0000

    Fix code style issues with Prettier

--- src/cow-react/common/utils/fetch.ts
+++ src/cow-react/common/utils/fetch.ts
@@ -1,5 +1,4 @@
-import { backOff, BackoffOptions } from "exponential-backoff"
-
+import { backOff, BackoffOptions } from 'exponential-backoff'
 
 // See config in https://www.npmjs.com/package/@insertish/exponential-backoff
 const DEFAULT_BACKOFF_OPTIONS: BackoffOptions = {
@@ -9,19 +8,21 @@ const DEFAULT_BACKOFF_OPTIONS: BackoffOptions = {
 }
 
 /**
- * 
+ *
  * @param input Request info (as in the fetch method)
  * @param init Request options (as in fetch method)
  * @param backoffOptions Backoff parameters. By default It will wait a maximum of ~102s maximun wait time. Wait time is 100ms * 2**(attenmpt)
  * @returns A promise of the request
  */
 // By default, it will wait
-export function fetchWithBackoff(input: RequestInfo | URL, init?: RequestInit, backoffOptions?: BackoffOptions): Promise<Response> {
-
+export function fetchWithBackoff(
+  input: RequestInfo | URL,
+  init?: RequestInit,
+  backoffOptions?: BackoffOptions
+): Promise<Response> {
   if (backoffOptions) {
-    return backOff(() => fetch(input, init), {...DEFAULT_BACKOFF_OPTIONS, ...backoffOptions})
+    return backOff(() => fetch(input, init), { ...DEFAULT_BACKOFF_OPTIONS, ...backoffOptions })
   }
 
   return fetch(input, init)
 }
-
