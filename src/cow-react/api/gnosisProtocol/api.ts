be679640f60a17ae1c157d5fb6568bfdc15f4f36
Lint Action <lint-action@samuelmeuli.com>
2023-02-24 13:10:06 +0000

    Fix code style issues with Prettier

--- src/cow-react/api/gnosisProtocol/api.ts
+++ src/cow-react/api/gnosisProtocol/api.ts
@@ -54,8 +54,7 @@ function getGnosisProtocolUrl(): Partial<Record<ChainId, string>> {
 function getProfileUrl(): Partial<Record<ChainId, string>> {
   if (isLocal || isDev || isPr || isBarn) {
     return {
-      [ChainId.MAINNET]:
-        process.env.REACT_APP_PROFILE_API_URL_STAGING_MAINNET || 'https://dev.cow.fi/affiliate/api',
+      [ChainId.MAINNET]: process.env.REACT_APP_PROFILE_API_URL_STAGING_MAINNET || 'https://dev.cow.fi/affiliate/api',
     }
   }
 
