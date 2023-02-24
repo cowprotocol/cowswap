4c9dcf3461eb1ea6e4694022e95495420bf0c78f
Anxo Rodriguez <anxolin@gmail.com>
2023-02-24 10:54:24 +0000

    Add documentation

--- src/cow-react/common/utils/fetch.ts
+++ src/cow-react/common/utils/fetch.ts
@@ -8,6 +8,14 @@ const DEFAULT_BACKOFF_OPTIONS: BackoffOptions = {
   jitter: 'none',
 }
 
+/**
+ * 
+ * @param input Request info (as in the fetch method)
+ * @param init Request options (as in fetch method)
+ * @param backoffOptions Backoff parameters. By default It will wait a maximum of ~102s maximun wait time. Wait time is 100ms * 2**(attenmpt)
+ * @returns A promise of the request
+ */
+// By default, it will wait
 export function fetchWithBackoff(input: RequestInfo | URL, init?: RequestInit, backoffOptions?: BackoffOptions): Promise<Response> {
 
   if (backoffOptions) {
@@ -16,3 +24,4 @@ export function fetchWithBackoff(input: RequestInfo | URL, init?: RequestInit, b
 
   return fetch(input, init)
 }
+
