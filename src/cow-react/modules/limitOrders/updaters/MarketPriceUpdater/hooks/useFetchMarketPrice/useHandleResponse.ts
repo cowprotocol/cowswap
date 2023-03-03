2a4ad77299706e9f9758a7cc6386ba29eaba80a1
shoom3301 <shoom3301@gmail.com>
2023-03-03 15:39:26 +0600

    Tests for handleLimitOrderQuoteResponse

--- src/cow-react/modules/limitOrders/updaters/MarketPriceUpdater/hooks/useFetchMarketPrice/useHandleResponse.ts
+++ src/cow-react/modules/limitOrders/updaters/MarketPriceUpdater/hooks/useFetchMarketPrice/useHandleResponse.ts
@@ -1,16 +1,46 @@
 import { useSetAtom } from 'jotai'
 import { useCallback } from 'react'
 import { useUpdateAtom } from 'jotai/utils'
-import { CurrencyAmount, Percent, Price } from '@uniswap/sdk-core'
+import { Currency, CurrencyAmount, Percent, Price } from '@uniswap/sdk-core'
 
 import { SimpleGetQuoteResponse } from '@cowprotocol/cow-sdk'
 import { useLimitOrdersTradeState } from '@cow/modules/limitOrders/hooks/useLimitOrdersTradeState'
-import { updateLimitRateAtom } from '@cow/modules/limitOrders/state/limitRateAtom'
+import { LimitRateState, updateLimitRateAtom } from '@cow/modules/limitOrders/state/limitRateAtom'
 import { limitOrdersQuoteAtom } from '@cow/modules/limitOrders/state/limitOrdersQuoteAtom'
 import { CancelableResult } from 'utils/async'
 import { FractionUtils } from '@cow/utils/fractionUtils'
 
-const PRICE_SLIPPAGE = new Percent(1, 10) // 0.1%
+export const LIMIT_ORDERS_PRICE_SLIPPAGE = new Percent(1, 10) // 0.1%
+
+export function handleLimitOrderQuoteResponse(
+  inputCurrency: Currency | null,
+  outputCurrency: Currency | null,
+  response: CancelableResult<SimpleGetQuoteResponse>
+): { rateState: Partial<LimitRateState>; quote: SimpleGetQuoteResponse } | undefined {
+  const { cancelled, data } = response
+
+  if (cancelled) {
+    return
+  }
+
+  const { buyAmount: buyAmountRaw, sellAmount: sellAmountRaw, feeAmount: feeAmountRaw } = data.quote
+
+  if (!outputCurrency || !inputCurrency) {
+    return
+  }
+
+  const feeAmount = CurrencyAmount.fromRawAmount(inputCurrency, feeAmountRaw)
+  const sellAmount = CurrencyAmount.fromRawAmount(inputCurrency, sellAmountRaw)
+  const buyAmount = CurrencyAmount.fromRawAmount(outputCurrency, buyAmountRaw)
+
+  const price = FractionUtils.fractionLikeToFraction(new Price({ baseAmount: sellAmount, quoteAmount: buyAmount }))
+  const marketRateWithSlippage = price.subtract(price.multiply(LIMIT_ORDERS_PRICE_SLIPPAGE.divide(100)))
+
+  return {
+    rateState: { marketRate: marketRateWithSlippage, feeAmount },
+    quote: data,
+  }
+}
 
 export function useHandleResponse() {
   const updateLimitRateState = useUpdateAtom(updateLimitRateAtom)
@@ -20,30 +50,15 @@ export function useHandleResponse() {
 
   return useCallback(
     (response: CancelableResult<SimpleGetQuoteResponse>) => {
-      const { cancelled, data } = response
-
       try {
-        if (cancelled) {
-          return
-        }
+        const result = handleLimitOrderQuoteResponse(inputCurrency, outputCurrency, response)
 
-        const { buyAmount: buyAmountRaw, sellAmount: sellAmountRaw, feeAmount: feeAmountRaw } = data.quote
+        if (!result) return
 
-        if (!outputCurrency || !inputCurrency) {
-          return
-        }
+        const { rateState, quote } = result
 
-        const feeAmount = CurrencyAmount.fromRawAmount(inputCurrency, feeAmountRaw)
-        const sellAmount = CurrencyAmount.fromRawAmount(inputCurrency, sellAmountRaw)
-        const buyAmount = CurrencyAmount.fromRawAmount(outputCurrency, buyAmountRaw)
-
-        const price = FractionUtils.fractionLikeToFraction(
-          new Price({ baseAmount: sellAmount, quoteAmount: buyAmount })
-        )
-        const marketRateWithSlippage = price.subtract(price.multiply(PRICE_SLIPPAGE.divide(100)))
-
-        updateLimitRateState({ marketRate: marketRateWithSlippage, feeAmount })
-        setLimitOrdersQuote({ response: data })
+        updateLimitRateState(rateState)
+        setLimitOrdersQuote({ response: quote })
       } catch (error: any) {
         console.debug('[useFetchMarketPrice] Failed to fetch exection price', error)
         updateLimitRateState({ marketRate: null })
