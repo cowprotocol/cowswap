1f27fdc57d42c4da3c8e7c45c44905428d36f261
Alfetopito <alfetopito@users.noreply.github.com>
2023-03-03 13:12:20 +0000

    Extracted function for building price instances

--- src/cow-react/common/hooks/usePrice.ts
+++ src/cow-react/common/hooks/usePrice.ts
@@ -1,21 +1,13 @@
 import { useSafeMemo } from '@cow/common/hooks/useSafeMemo'
 import { Currency, CurrencyAmount, Price } from '@uniswap/sdk-core'
-import { isFractionFalsy } from '@cow/utils/isFractionFalsy'
+import { buildPriceFromCurrencyAmounts } from '@cow/modules/limitOrders/utils/buildPriceFromCurrencyAmounts'
 
 export function usePrice(
   inputCurrencyAmount: CurrencyAmount<Currency> | null,
   outputCurrencyAmount: CurrencyAmount<Currency> | null
 ): Price<Currency, Currency> | null {
-  return useSafeMemo(() => {
-    if (
-      !inputCurrencyAmount ||
-      !outputCurrencyAmount ||
-      isFractionFalsy(inputCurrencyAmount) ||
-      isFractionFalsy(outputCurrencyAmount)
-    ) {
-      return null
-    }
-
-    return new Price({ baseAmount: inputCurrencyAmount, quoteAmount: outputCurrencyAmount })
-  }, [inputCurrencyAmount, outputCurrencyAmount])
+  return useSafeMemo(
+    () => buildPriceFromCurrencyAmounts(inputCurrencyAmount, outputCurrencyAmount),
+    [inputCurrencyAmount, outputCurrencyAmount]
+  )
 }
