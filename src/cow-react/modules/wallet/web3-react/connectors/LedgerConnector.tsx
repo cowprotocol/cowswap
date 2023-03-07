fcb8a911915aeef192813e30ae9e8a99c9db954d
shoom3301 <shoom3301@gmail.com>
2023-03-07 20:01:53 +0600

    Ledger HID

--- src/cow-react/modules/wallet/web3-react/connectors/LedgerConnector.tsx
+++ src/cow-react/modules/wallet/web3-react/connectors/LedgerConnector.tsx
@@ -1,19 +1,32 @@
-import {
-  Actions,
-  // AddEthereumChainParameter,
-  Connector,
-  Provider,
-  // ProviderConnectInfo,
-  // ProviderRpcError,
-  RequestArguments,
-} from '@web3-react/types'
+import { Actions, Connector, Provider } from '@web3-react/types'
 import { Web3Provider, ExternalProvider } from '@ethersproject/providers'
+import type Transport from '@ledgerhq/hw-transport'
+import ProviderEngine from 'web3-provider-engine'
 
-import { loadConnectKit, LedgerConnectKit, SupportedProviders } from '@ledgerhq/connect-kit-loader'
+global.process = { browser: true } as any
+const RpcSubprovider = require('web3-provider-engine/subproviders/rpc')
 
-type LedgerProvider = Provider & {
-  connected: () => boolean
-  request<T>(args: RequestArguments): Promise<T>
+const derivationPaths = ["44'/60'/x'/0/0", "44'/60'/0'/x"]
+
+export function generateDerivationPaths(context: { accountsOffset: number; accountsLength: number }): string[] {
+  const { accountsOffset, accountsLength } = context
+  const pathList: string[] = []
+
+  for (let i = accountsOffset; i < accountsOffset + accountsLength; i++) {
+    const x = Math.floor(i / derivationPaths.length)
+    const pathIndex = i - derivationPaths.length * x
+    const path = derivationPaths[pathIndex].replace('x', String(x))
+
+    pathList.push(path)
+  }
+
+  return pathList
+}
+
+export interface EcdsaSignature {
+  r: string
+  s: string
+  v: number
 }
 
 interface LedgerConstructorArgs {
@@ -33,25 +46,73 @@ function parseChainId(chainId: number | string) {
   return Number.parseInt(String(chainId), 16)
 }
 
+interface LedgerModules {
+  transportFactory: {
+    create(): Promise<Transport>
+  }
+  createLedgerSubprovider: (factory: () => Promise<Transport>, options: unknown) => any
+  LedgerEth: new (transport: Transport) => any
+}
+
 export class Ledger extends Connector {
-  public provider?: LedgerProvider
+  static getModule(): Promise<LedgerModules> {
+    return (
+      Promise.all([
+        // @ts-ignore
+        import('@ledgerhq/hw-transport-u2f'),
+        // @ts-ignore
+        import('@ledgerhq/hw-transport-webhid'),
+        // @ts-ignore
+        import('@ledgerhq/web3-subprovider'),
+        // @ts-ignore
+        import('@ledgerhq/hw-app-eth'),
+      ])
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        .then(([TransportU2F, TransportWebHID, createLedgerSubprovider, LedgerEth]) => {
+          return {
+            transportFactory: 'hid' in navigator ? TransportWebHID.default : TransportU2F.default,
+            createLedgerSubprovider: createLedgerSubprovider.default,
+            LedgerEth: LedgerEth.default,
+          }
+        })
+    )
+  }
+
+  public provider?: Provider
   private readonly options: LedgerOptions
-  private eagerConnection?: Promise<void>
-  private connectKitPromise: Promise<LedgerConnectKit>
+
+  private isActivated = false
+
+  private providerRequests: ((provider: Provider) => void)[] = []
 
   constructor({ actions, onError, options = {} }: LedgerConstructorArgs) {
     super(actions, onError)
 
     this.options = options
-    this.connectKitPromise = loadConnectKit()
+  }
+
+  async getProvider(
+    { forceCreate }: { chainId?: number; forceCreate?: boolean } = {
+      forceCreate: false,
+    }
+  ): Promise<Provider> {
+    if (!this.provider) {
+      if (forceCreate) {
+        this.activate()
+      }
+
+      return new Promise((resolve) => {
+        this.providerRequests.push(resolve)
+      })
+    }
+
+    return Promise.resolve(this.provider)
   }
 
   async getAccounts() {
-    console.log('getAccount')
-
     const provider = await this.getProvider()
     const accounts = (await provider.request({
-      method: 'eth_requestAccounts',
+      method: 'eth_accounts',
     })) as string[]
 
     return accounts
@@ -60,62 +121,88 @@ export class Ledger extends Connector {
   async getChainId() {
     console.log('getChainId')
 
-    const provider = await this.getProvider()
-    const chainId = (await provider.request({
-      method: 'eth_chainId',
-    })) as string
-
-    return parseChainId(chainId)
-  }
-
-  async getProvider(
-    { forceCreate }: { chainId?: number; forceCreate?: boolean } = {
-      forceCreate: false,
-    }
-  ) {
-    console.log('getProvider')
-
-    if (!this.provider || forceCreate) {
-      console.log('getting provider from Connect Kit')
-      const connectKit = await this.connectKitPromise
-      this.provider = (await connectKit.getProvider()) as LedgerProvider
-    }
-
-    return this.provider
+    return 1
+    // const provider = await this.getProvider()
+    // const chainId = (await provider.request({
+    //   method: 'eth_networkId',
+    // })) as string
+    //
+    // return +chainId
   }
 
   async getSigner() {
     console.log('getSigner')
 
     const [provider, account] = await Promise.all([this.getProvider(), this.getAccounts()])
-    return new Web3Provider(provider as ExternalProvider).getSigner(account[0])
+    const signer = new Web3Provider(provider as ExternalProvider).getSigner(account[0])
+
+    // TODO: implement this
+    // signer._signTypedData = async (domain: TypedDataDomain, types: Record<string, Array<TypedDataField>>, value: Record<string, any>) => {
+    //   const module = await Ledger.getModule();
+    //   const transport = await module.transportFactory.create();
+    //
+    //   try {
+    //     const ledgerEth = new module.LedgerEth(transport);
+    //
+    //     const pathList = generateDerivationPaths({accountsLength: 1, accountsOffset: 0});
+    //
+    //     const signature: EcdsaSignature = await ledgerEth.signEIP712HashedMessage(
+    //       pathList[0],
+    //       domainSeparator,
+    //       dataHash
+    //     );
+    //
+    //     return '0x' + signature.r + signature.s + signature.v.toString(16);
+    //   }
+    //   finally {
+    //     transport.close().catch((error) => {
+    //       console.error(error);
+    //     });
+    //   }
+    // }
+
+    return signer
   }
 
-  public async activate() {
-    console.log('debug activate')
+  async activate() {
+    if (this.isActivated) return
 
-    const connectKit = await this.connectKitPromise
+    this.isActivated = true
 
-    connectKit.enableDebugLogs()
+    const { transportFactory, createLedgerSubprovider } = await Ledger.getModule()
 
-    connectKit.checkSupport({
-      providerType: SupportedProviders.Ethereum,
-      chainId: this.options.chainId,
-      infuraId: this.options.infuraId,
-      rpc: this.options.rpc,
+    const engine = new ProviderEngine()
+
+    const ledger = createLedgerSubprovider(() => transportFactory.create(), {
+      networkId: 1,
+      accountsOffset: 0,
+      accountsLength: 1,
     })
 
-    const provider = await this.getProvider({ forceCreate: true })
+    ;(engine as any)['request'] = function (request: any) {
+      return new Promise((resolve, reject) => {
+        engine.sendAsync(request, (error, result) => {
+          const resultError = error
 
-    console.log('debug provider', this.provider)
-
-    if (provider.on) {
-      console.log('debug assigning event handlers')
-      provider.on('accountsChanged', this.onAccountsChanged)
-      provider.on('chainChanged', this.onChainChanged)
-      provider.on('disconnect', this.onDisconnect)
-      provider.on('close', this.onDisconnect)
+          if (resultError) {
+            console.log('ERROR REQUEST: ', request)
+            reject(resultError)
+          } else {
+            resolve(result.result)
+          }
+        })
+      })
     }
+
+    engine.addProvider(ledger)
+    engine.addProvider(new RpcSubprovider({ rpcUrl: this.options.rpc?.[1] }))
+    engine.start()
+
+    this.provider = engine as any
+    this.providerRequests.forEach((callback) => {
+      callback(engine as any)
+    })
+    this.providerRequests = []
 
     const accounts = await this.getAccounts()
     const chainId = await this.getChainId()
@@ -123,26 +210,13 @@ export class Ledger extends Connector {
     return this.actions.update({ chainId, accounts })
   }
 
-  public async connectEagerly(): Promise<void> {}
-
-  protected onAccountsChanged = (accounts: string[]): void => {
-    console.log('debug accounts changed', accounts)
-
-    if (accounts.length === 0) {
-      this.actions.resetState()
-    } else {
-      this.actions.update({ accounts })
-    }
+  async connectEagerly(): Promise<void> {
+    return undefined
   }
 
-  protected onChainChanged = (chainId: number | string): void => {
-    console.log('debug chain changed')
+  deactivate(...args: unknown[]): Promise<void> | void {
+    this.isActivated = false
 
-    this.actions.update({ chainId: parseChainId(chainId) })
-  }
-
-  protected onDisconnect = (error: any): void => {
-    this.actions.resetState()
-    this.onError?.(error)
+    return super.deactivate?.(...args)
   }
 }
