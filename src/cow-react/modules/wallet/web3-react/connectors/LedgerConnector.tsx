60f0ff824de4f21e0edb736eab5cc87ee9ffd5b1
shoom3301 <shoom3301@gmail.com>
2023-03-07 21:42:13 +0600

    Sign typed data

--- src/cow-react/modules/wallet/web3-react/connectors/LedgerConnector.tsx
+++ src/cow-react/modules/wallet/web3-react/connectors/LedgerConnector.tsx
@@ -1,11 +1,14 @@
 import { Actions, Connector, Provider } from '@web3-react/types'
-import { Web3Provider, ExternalProvider } from '@ethersproject/providers'
 import type Transport from '@ledgerhq/hw-transport'
 import ProviderEngine from 'web3-provider-engine'
+import { TypedDataUtils, SignTypedDataVersion } from '@metamask/eth-sig-util'
+import { TypedDataDomain, TypedDataField } from '@ethersproject/abstract-signer'
 
 global.process = { browser: true } as any
 const RpcSubprovider = require('web3-provider-engine/subproviders/rpc')
 
+// TODO: use current chain Id
+const currentChainId = 1
 const derivationPaths = ["44'/60'/x'/0/0", "44'/60'/0'/x"]
 
 export function generateDerivationPaths(context: { accountsOffset: number; accountsLength: number }): string[] {
@@ -42,16 +45,40 @@ interface LedgerOptions {
   rpc?: { [chainId: number]: string }
 }
 
-function parseChainId(chainId: number | string) {
-  return Number.parseInt(String(chainId), 16)
-}
-
 interface LedgerModules {
   transportFactory: {
     create(): Promise<Transport>
   }
   createLedgerSubprovider: (factory: () => Promise<Transport>, options: unknown) => any
   LedgerEth: new (transport: Transport) => any
+}
+
+const signTypedData = async (
+  _domain: TypedDataDomain,
+  types: Record<string, Array<TypedDataField>>,
+  value: Record<string, any>,
+  primaryType: string
+): Promise<string> => {
+  const module = await Ledger.getModule()
+  const transport = await module.transportFactory.create()
+
+  // TODO: https://etherscan.io/address/0x9008d19f58aabd9ed0d60971565aa8510560ab41#readContract
+  const domainSeparator = '0xc078f884a2676e1345748b1feace7b0abee5d00ecadb6e574dcdd109a63e8943'
+  const dataHash = TypedDataUtils.hashStruct(primaryType, value, types, SignTypedDataVersion.V4).toString('hex')
+
+  try {
+    const ledgerEth = new module.LedgerEth(transport)
+
+    const pathList = generateDerivationPaths({ accountsLength: 1, accountsOffset: 0 })
+
+    const signature: EcdsaSignature = await ledgerEth.signEIP712HashedMessage(pathList[0], domainSeparator, dataHash)
+
+    return '0x' + signature.r + signature.s + signature.v.toString(16)
+  } finally {
+    transport.close().catch((error) => {
+      console.error(error)
+    })
+  }
 }
 
 export class Ledger extends Connector {
@@ -109,59 +136,22 @@ export class Ledger extends Connector {
     return Promise.resolve(this.provider)
   }
 
-  async getAccounts() {
+  async getAccounts(): Promise<string[]> {
     const provider = await this.getProvider()
-    const accounts = (await provider.request({
+    return (await provider.request({
       method: 'eth_accounts',
     })) as string[]
-
-    return accounts
   }
 
-  async getChainId() {
-    console.log('getChainId')
-
-    return 1
+  async getChainId(): Promise<number> {
+    return currentChainId
+    // TODO: fix me
     // const provider = await this.getProvider()
     // const chainId = (await provider.request({
     //   method: 'eth_networkId',
     // })) as string
     //
     // return +chainId
-  }
-
-  async getSigner() {
-    console.log('getSigner')
-
-    const [provider, account] = await Promise.all([this.getProvider(), this.getAccounts()])
-    const signer = new Web3Provider(provider as ExternalProvider).getSigner(account[0])
-
-    // TODO: implement this
-    // signer._signTypedData = async (domain: TypedDataDomain, types: Record<string, Array<TypedDataField>>, value: Record<string, any>) => {
-    //   const module = await Ledger.getModule();
-    //   const transport = await module.transportFactory.create();
-    //
-    //   try {
-    //     const ledgerEth = new module.LedgerEth(transport);
-    //
-    //     const pathList = generateDerivationPaths({accountsLength: 1, accountsOffset: 0});
-    //
-    //     const signature: EcdsaSignature = await ledgerEth.signEIP712HashedMessage(
-    //       pathList[0],
-    //       domainSeparator,
-    //       dataHash
-    //     );
-    //
-    //     return '0x' + signature.r + signature.s + signature.v.toString(16);
-    //   }
-    //   finally {
-    //     transport.close().catch((error) => {
-    //       console.error(error);
-    //     });
-    //   }
-    // }
-
-    return signer
   }
 
   async activate() {
@@ -174,18 +164,22 @@ export class Ledger extends Connector {
     const engine = new ProviderEngine()
 
     const ledger = createLedgerSubprovider(() => transportFactory.create(), {
-      networkId: 1,
+      networkId: currentChainId,
       accountsOffset: 0,
       accountsLength: 1,
     })
 
     ;(engine as any)['request'] = function (request: any) {
+      if (request.method === 'eth_signTypedData_v4') {
+        const { domain, message, primaryType, types } = JSON.parse(request.params[1])
+        return signTypedData(domain, types, message, primaryType)
+      }
+
       return new Promise((resolve, reject) => {
-        engine.sendAsync(request, (error, result) => {
+        engine.sendAsync({ id: 1, jsonrpc: '2.0', ...request }, (error, result) => {
           const resultError = error
 
           if (resultError) {
-            console.log('ERROR REQUEST: ', request)
             reject(resultError)
           } else {
             resolve(result.result)
@@ -195,7 +189,7 @@ export class Ledger extends Connector {
     }
 
     engine.addProvider(ledger)
-    engine.addProvider(new RpcSubprovider({ rpcUrl: this.options.rpc?.[1] }))
+    engine.addProvider(new RpcSubprovider({ rpcUrl: this.options.rpc?.[currentChainId] }))
     engine.start()
 
     this.provider = engine as any
@@ -211,7 +205,7 @@ export class Ledger extends Connector {
   }
 
   async connectEagerly(): Promise<void> {
-    return undefined
+    return this.activate()
   }
 
   deactivate(...args: unknown[]): Promise<void> | void {
