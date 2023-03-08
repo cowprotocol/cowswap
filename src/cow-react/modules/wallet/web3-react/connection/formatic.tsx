bbff7efb924d844ce66815a53e76b0dfa296e9c4
shoom3301 <shoom3301@gmail.com>
2023-03-08 19:00:55 +0600

    Move back AsyncConnector

--- src/cow-react/modules/wallet/web3-react/connection/formatic.tsx
+++ src/cow-react/modules/wallet/web3-react/connection/formatic.tsx
@@ -1,21 +1,28 @@
 import { ConnectionType } from '@cow/modules/wallet'
 
-import Fortmatic from 'fortmatic'
 import { getConnectionName } from '@cow/modules/wallet/api/utils/connection'
 
 import { useIsActiveWallet } from 'hooks/useIsActiveWallet'
 import { ConnectWalletOption } from '@cow/modules/wallet/api/pure/ConnectWalletOption'
 
 import { initializeConnector } from '@web3-react/core'
-import { EIP1193 } from '@web3-react/eip1193'
 
 import { TryActivation } from '.'
 import { Web3ReactConnection } from '../types'
 
 import { default as FormaticImage } from '@cow/modules/wallet/api/assets/formatic.png'
+import { AsyncConnector } from './asyncConnector'
 
-const [web3Fortmatic, web3FortmaticHooks] = initializeConnector<EIP1193>(
-  (actions) => new EIP1193({ actions, provider: new Fortmatic(process.env.REACT_APP_FORTMATIC_KEY).getProvider() })
+const [web3Fortmatic, web3FortmaticHooks] = initializeConnector<AsyncConnector>(
+  (actions) =>
+    new AsyncConnector(
+      () =>
+        Promise.all([import('@web3-react/eip1193'), import('fortmatic')]).then(([{ EIP1193 }, m]) => {
+          const Fortmatic = m.default
+          return new EIP1193({ actions, provider: new Fortmatic(process.env.REACT_APP_FORTMATIC_KEY).getProvider() })
+        }),
+      actions
+    )
 )
 export const fortmaticConnection: Web3ReactConnection = {
   connector: web3Fortmatic,
