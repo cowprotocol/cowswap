62d1be4dd822067499988ead7648ec23c5b667dd
Alfetopito <alfetopito@users.noreply.github.com>
2023-03-29 10:49:15 +0100

    Commenting out token search

--- src/custom/components/SearchModal/CurrencySearch/CurrencySearchMod.tsx
+++ src/custom/components/SearchModal/CurrencySearch/CurrencySearchMod.tsx
@@ -34,7 +34,7 @@ import useNetworkName from 'hooks/useNetworkName'
 import { ContentWrapper } from '.'
 import { searchByAddressAnalytics } from 'components/analytics'
 import { useWalletInfo } from '@cow/modules/wallet'
-import { useTokenSearch } from './useTokenSearch'
+// import { useTokenSearch } from './useTokenSearch'
 
 /* const ContentWrapper = styled(Column)`
   width: 100%;
@@ -202,16 +202,17 @@ export function CurrencySearch({
     return () => clearTimeout(tokenLoaderTimer)
   }, [])
 
-  const existingTokens = useMemo(
-    () =>
-      new Map(
-        [...filteredSortedTokens, ...(filteredInactiveTokens ?? [])]
-          .filter((currency: Currency): currency is Token => currency.isToken)
-          .map(({ address }) => [address, true])
-      ),
-    [filteredSortedTokens, filteredInactiveTokens]
-  )
-  const additionalTokens = useTokenSearch(searchQuery, existingTokens)
+  // TODO: uncomment when we get a new source of tokens to query from
+  // const existingTokens = useMemo(
+  //   () =>
+  //     new Map(
+  //       [...filteredSortedTokens, ...(filteredInactiveTokens ?? [])]
+  //         .filter((currency: Currency): currency is Token => currency.isToken)
+  //         .map(({ address }) => [address, true])
+  //     ),
+  //   [filteredSortedTokens, filteredInactiveTokens]
+  // )
+  const additionalTokens: Token[] = [] // useTokenSearch(searchQuery, existingTokens)
 
   return (
     <Trace name={EventName.TOKEN_SELECTOR_OPENED} modal={ModalName.TOKEN_SELECTOR} shouldLogImpression={true}>
