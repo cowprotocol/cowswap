abdcbed121317fa8d641e709b69c32497fcb79de
Alexandr Kazachenko <shoom3301@gmail.com>
2023-03-30 17:47:17 +0600

    OrderSigningUtils for order and order cancellation signing (#2187)

--- src/custom/utils/trade.ts
+++ src/custom/utils/trade.ts
@@ -3,13 +3,19 @@ import { isAddress, shortenAddress } from 'utils'
 import { ChangeOrderStatusParams, Order, OrderStatus } from 'state/orders/actions'
 import { AddUnserialisedPendingOrderParams } from 'state/orders/hooks'
 
-import { signOrder, signOrderCancellation, UnsignedOrder } from 'utils/signatures'
 import { OrderID } from '@cow/api/gnosisProtocol'
 import { Signer } from '@ethersproject/abstract-signer'
 import { RADIX_DECIMAL, NATIVE_CURRENCY_BUY_ADDRESS } from 'constants/index'
 import { SupportedChainId as ChainId } from '@cowprotocol/cow-sdk'
 import { formatSymbol } from '@cow/utils/format'
-import { OrderClass, OrderKind, SigningScheme, EcdsaSigningScheme } from '@cowprotocol/cow-sdk'
+import {
+  EcdsaSigningScheme,
+  OrderClass,
+  OrderKind,
+  UnsignedOrder,
+  SigningScheme,
+  OrderSigningUtils,
+} from '@cowprotocol/cow-sdk'
 import { getProfileData } from '@cow/api/gnosisProtocol/api'
 import { formatTokenAmount } from '@cow/utils/amountFormat'
 import { orderBookApi } from '@cow/cowSdk'
@@ -188,8 +194,9 @@ export async function signAndPostOrder(params: PostOrderParams): Promise<AddUnse
 
   let signingScheme: SigningScheme
   let signature: string | undefined
+
   if (allowsOffchainSigning) {
-    const signedOrderInfo = await signOrder(unsignedOrder, chainId, signer)
+    const signedOrderInfo = await OrderSigningUtils.signOrder(unsignedOrder, chainId, signer)
     signingScheme =
       signedOrderInfo.signingScheme === EcdsaSigningScheme.ETHSIGN ? SigningScheme.ETHSIGN : SigningScheme.EIP712
     signature = signedOrderInfo.signature
@@ -197,6 +204,8 @@ export async function signAndPostOrder(params: PostOrderParams): Promise<AddUnse
     signingScheme = SigningScheme.PRESIGN
     signature = account
   }
+
+  if (!signature) throw new Error('Signature is undefined!')
 
   // Call API
   const orderId = await orderBookApi.sendOrder(
@@ -235,11 +244,13 @@ type OrderCancellationParams = {
 export async function sendOrderCancellation(params: OrderCancellationParams): Promise<void> {
   const { orderId, chainId, signer, cancelPendingOrder } = params
 
-  const { signature, signingScheme } = await signOrderCancellation(orderId, chainId, signer)
+  const { signature, signingScheme } = await OrderSigningUtils.signOrderCancellation(orderId, chainId, signer)
 
-  await orderBookApi.sendSignedOrderCancellation(
-    orderId,
+  if (!signature) throw new Error('Signature is undefined!')
+
+  await orderBookApi.sendSignedOrderCancellations(
     {
+      orderUids: [orderId],
       signature,
       signingScheme,
     },
