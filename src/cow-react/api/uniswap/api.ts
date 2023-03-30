7f978a8c8d08a8162617a06d5902fbc2e139647d
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-03-30 10:36:43 +0200

    Remove unnecessary variable

--- src/cow-react/api/uniswap/api.ts
+++ src/cow-react/api/uniswap/api.ts
@@ -6,9 +6,7 @@ const BASE_URL = 'https://cow-web-services.vercel.app/api/serverless/proxy'
 const GQL_CLIENT = new GraphQLClient(BASE_URL)
 
 export async function getTokens(searchQuery: string): Promise<FetchTokensApiResult> {
-  const data = await GQL_CLIENT.request<FetchTokensApiResult>(SEARCH_TOKENS, {
+  return await GQL_CLIENT.request<FetchTokensApiResult>(SEARCH_TOKENS, {
     searchQuery,
   })
-
-  return data
 }
