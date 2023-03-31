9bd557ccc5cb701995182b2e4f4b5df677b9e1de
Anxo Rodriguez <anxolin@gmail.com>
2023-03-31 12:26:44 +0100

    Show recipient in one step (#2221)
    * Show recipient in one step
    
    * Remove add recipient
    
    * Make toggles independent
    
    * Make expert mode and Toggle Reipient
    
    * Change label from "Toggle recipient" to "Custom recipient"

--- src/cow-react/modules/swap/containers/SetRecipient/index.tsx
+++ src/cow-react/modules/swap/containers/SetRecipient/index.tsx
@@ -1,31 +1,25 @@
 import { AutoRow } from 'components/Row'
 import { ArrowWrapper } from 'components/swap/styleds'
 import { ArrowDown } from 'react-feather'
-import { LinkStyledButton } from 'theme'
-import { Trans } from '@lingui/macro'
 import AddressInputPanel from 'components/AddressInputPanel'
 import { useContext } from 'react'
 import { ThemeContext } from 'styled-components/macro'
-
-export interface RemoveRecipientProps {
+export interface SetRecipientProps {
   recipient: string
   onChangeRecipient(recipient: string | null): void
   className?: string
 }
 
-export function RemoveRecipient(props: RemoveRecipientProps) {
+export function SetRecipient(props: SetRecipientProps) {
   const { recipient, onChangeRecipient, className } = props
   const theme = useContext(ThemeContext)
 
   return (
     <>
-      <AutoRow className={className} justify="space-between" style={{ padding: '0 1rem' }}>
+      <AutoRow className={className} justify="space-between" style={{ padding: '0 rem', margin: '1rem 0' }}>
         <ArrowWrapper clickable={false}>
           <ArrowDown size="16" color={theme.text2} />
         </ArrowWrapper>
-        <LinkStyledButton id="remove-recipient-button" onClick={() => onChangeRecipient(null)}>
-          <Trans>- Remove recipient</Trans>
-        </LinkStyledButton>
       </AutoRow>
       <AddressInputPanel id="recipient" value={recipient} onChange={onChangeRecipient} />
     </>
