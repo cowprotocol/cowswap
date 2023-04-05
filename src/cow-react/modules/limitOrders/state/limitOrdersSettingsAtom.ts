a70f48c6b7c8fbd5b34b4feafff585dbf9b20b84
Alfetopito <alfetopito@users.noreply.github.com>
2023-04-05 17:18:58 +0100

    Added setting to control partiallyFillable flag

--- src/cow-react/modules/limitOrders/state/limitOrdersSettingsAtom.ts
+++ src/cow-react/modules/limitOrders/state/limitOrdersSettingsAtom.ts
@@ -6,6 +6,7 @@ import { Milliseconds, Timestamp } from '@cow/types'
 export interface LimitOrdersSettingsState {
   readonly expertMode: boolean
   readonly showRecipient: boolean
+  readonly partialFillsEnabled: boolean
   readonly deadlineMilliseconds: Milliseconds
   readonly customDeadlineTimestamp: Timestamp | null
 }
@@ -13,12 +14,13 @@ export interface LimitOrdersSettingsState {
 export const defaultLimitOrdersSettings: LimitOrdersSettingsState = {
   expertMode: false,
   showRecipient: false,
+  partialFillsEnabled: true,
   deadlineMilliseconds: defaultLimitOrderDeadline.value,
   customDeadlineTimestamp: null,
 }
 
 export const limitOrdersSettingsAtom = atomWithStorage<LimitOrdersSettingsState>(
-  'limit-orders-settings-atom:v1',
+  'limit-orders-settings-atom:v2',
   defaultLimitOrdersSettings
 )
 
