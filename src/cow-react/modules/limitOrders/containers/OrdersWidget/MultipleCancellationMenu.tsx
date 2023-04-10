cbb6c548b5ec1772ecf13de04383b5be7e35f6e7
Alexandr Kazachenko <shoom3301@gmail.com>
2023-04-10 21:10:03 +0600

    Styles and layout for MultipleCancellationMenu

--- src/cow-react/modules/limitOrders/containers/OrdersWidget/MultipleCancellationMenu.tsx
+++ src/cow-react/modules/limitOrders/containers/OrdersWidget/MultipleCancellationMenu.tsx
@@ -3,36 +3,80 @@ import { useAtom } from 'jotai'
 import { ordersToCancelAtom } from '@cow/common/hooks/useMultipleOrdersCancellation/state'
 import { useMultipleOrdersCancellation } from '@cow/common/hooks/useMultipleOrdersCancellation'
 import { ParsedOrder } from '@cow/modules/limitOrders/containers/OrdersWidget/hooks/useLimitOrdersList'
-import { shortenOrderId } from 'utils'
+import styled from 'styled-components/macro'
+import { transparentize } from 'polished'
+import { CloseIcon } from 'theme'
 
 interface Props {
   pendingOrders: ParsedOrder[]
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  margin-bottom: 15px;
+  gap: 10px;
+`
+
+const ActionButton = styled.button`
+  display: inline-block;
+  background: ${({ theme }) => transparentize(0.88, theme.text3)};
+  color: ${({ theme }) => theme.text1};
+  font-weight: 600;
+  text-decoration: none;
+  font-size: 13px;
+  padding: 10px 24px;
+  border: 0;
+  outline: none;
+  cursor: pointer;
+  transition: background 0.15s ease-in-out, color 0.2s ease-in-out;
+  border-radius: 4px;
+
+  &:hover:not([disabled]) {
+    background: ${({ theme }) => theme.bg1};
+    color: ${({ theme }) => theme.text1};
+  }
+
+  &[disabled] {
+    background: transparent;
+    outline: 1px solid ${({ theme }) => transparentize(0.88, theme.text3)};
+  }
+`
+
 export function MultipleCancellationMenu({ pendingOrders }: Props) {
   const [ordersToCancel, setOrdersToCancel] = useAtom(ordersToCancelAtom)
   const multipleCancellation = useMultipleOrdersCancellation()
 
+  const isMultipleCancelEnabled = ordersToCancel !== null
+
   const toggleMultipleCancellation = useCallback(() => {
-    setOrdersToCancel(ordersToCancel !== null ? null : [])
-  }, [setOrdersToCancel, ordersToCancel])
+    setOrdersToCancel(isMultipleCancelEnabled ? null : [])
+  }, [setOrdersToCancel, isMultipleCancelEnabled])
 
   const cancelAllPendingOrders = useCallback(() => {
     multipleCancellation(pendingOrders)
   }, [multipleCancellation, pendingOrders])
 
+  const toggleOrCancel = useCallback(() => {
+    if (isMultipleCancelEnabled) {
+      if (!ordersToCancel?.length) return
+
+      multipleCancellation(ordersToCancel)
+    } else {
+      toggleMultipleCancellation()
+    }
+  }, [isMultipleCancelEnabled, ordersToCancel, multipleCancellation, toggleMultipleCancellation])
+
+  if (pendingOrders.length === 0) return null
+
   return (
-    <div>
-      {ordersToCancel && (
-        <div>Selected orders: {ordersToCancel.map((order) => shortenOrderId(order.id)).join(',')}</div>
-      )}
-
-      <button onClick={toggleMultipleCancellation}>Multiple cancellation</button>
-      <button onClick={cancelAllPendingOrders}>Cancel all pending orders</button>
-
-      {ordersToCancel && (
-        <button onClick={() => multipleCancellation(ordersToCancel)}>Cancel ({ordersToCancel.length})</button>
-      )}
-    </div>
+    <Wrapper>
+      {isMultipleCancelEnabled && <CloseIcon onClick={toggleMultipleCancellation}></CloseIcon>}
+      <ActionButton disabled={ordersToCancel?.length === 0} onClick={toggleOrCancel}>
+        {isMultipleCancelEnabled ? `Cancel (${ordersToCancel.length})` : 'Multiple cancellation'}
+      </ActionButton>
+      <ActionButton onClick={cancelAllPendingOrders}>Cancel all pending orders</ActionButton>
+    </Wrapper>
   )
 }
