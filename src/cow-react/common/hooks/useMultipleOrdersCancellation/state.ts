8b840ccf837859b3e23bcee6d7bf34b39c4d9368
Alexandr Kazachenko <shoom3301@gmail.com>
2023-04-12 19:58:23 +0600

    Removed irrelevant comment

--- src/cow-react/common/hooks/useMultipleOrdersCancellation/state.ts
+++ src/cow-react/common/hooks/useMultipleOrdersCancellation/state.ts
@@ -2,8 +2,6 @@ import { atom } from 'jotai'
 import { Order } from 'state/orders/actions'
 import { isOrderOffChainCancellable } from '@cow/common/utils/isOrderOffChainCancellable'
 
-// null - when orders cancellation is not enabled
-// [] - when orders cancellation is enabled
 export const ordersToCancelAtom = atom<Order[]>([])
 
 export const updateOrdersToCancelAtom = atom(null, (get, set, nextState: Order[]) => {
