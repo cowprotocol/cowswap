c69494acf80d2a348b22a1ee1f860c56cf827394
Alexandr Kazachenko <shoom3301@gmail.com>
2023-04-19 13:55:09 +0600

    Fix typo

--- src/cow-react/modules/fortune/state/fortunesListAtom.ts
+++ src/cow-react/modules/fortune/state/fortunesListAtom.ts
@@ -3,7 +3,7 @@ import { RAW_CODE_LINK } from 'constants/index'
 import { loadable } from 'jotai/utils'
 import { FortuneItem } from '@cow/modules/fortune/types'
 
-const FILE_PATH = '/configuration/config/furtunes.json'
+const FILE_PATH = '/configuration/config/fortunes.json'
 
 const fortuneFetchAtom = atom(async () => {
   const fortunes: FortuneItem[] = await fetch(RAW_CODE_LINK + FILE_PATH).then((res) => res.json())
