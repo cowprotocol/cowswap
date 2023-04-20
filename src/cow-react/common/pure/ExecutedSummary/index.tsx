997afb6ab5a9fe02185e46a26b000556b606be71
Nenad Vracar <34926005+nenadV91@users.noreply.github.com>
2023-04-20 17:39:34 +0200

    Surplus/usd value (#2338)
    * Added USD value to executed order popup
    
    * Add USD value to confirmation modal and use coingecko for USD
    
    * Refactor
    
    * PR comments
    
    * PR comments 2
    
    * PR comments 3

--- src/cow-react/common/pure/ExecutedSummary/index.tsx
+++ src/cow-react/common/pure/ExecutedSummary/index.tsx
@@ -1,52 +1,39 @@
-import styled from 'styled-components/macro'
-
-import { TokenAmount } from '@cow/common/pure/TokenAmount'
 import { getExecutedSummaryData } from '@cow/utils/getExecutedSummaryData'
 import { Order } from '@src/custom/state/orders/actions'
+import { useCoingeckoUsdValue } from '@src/custom/hooks/useStablecoinPrice'
+import * as styledEl from './styled'
+import { MIN_FIAT_SURPLUS_VALUE } from '@src/custom/constants'
 
-const SummaryWrapper = styled.div`
-  font-size: 1rem;
-
-  > div {
-    margin-bottom: 1rem;
-
-    &:last-child {
-      margin-bottom: 0.6rem;
-    }
-  }
-`
-
-const Strong = styled.strong`
-  font-size: 0.9rem;
-  white-space: nowrap;
-`
-
-export function getExecutedSummary(order: Order): JSX.Element | string | null {
-  if (!order) return null
-
+export function ExecutedSummary({ order }: { order: Order }) {
   const { formattedFilledAmount, formattedSwappedAmount, surplusAmount, surplusToken } = getExecutedSummaryData(order)
 
+  const fiatValue = useCoingeckoUsdValue(surplusAmount)
+  // I think its fine here to use Number because its always USD value
+  const showFiatValue = Number(fiatValue?.toExact()) > MIN_FIAT_SURPLUS_VALUE
+
   return (
-    <SummaryWrapper>
+    <styledEl.SummaryWrapper>
       <div>
         Traded{' '}
-        <Strong>
-          <TokenAmount amount={formattedFilledAmount} tokenSymbol={formattedFilledAmount.currency} />
-        </Strong>{' '}
-        for a total of{' '}
-        <Strong>
-          <TokenAmount amount={formattedSwappedAmount} tokenSymbol={formattedSwappedAmount.currency} />
-        </Strong>
+        <styledEl.StyledTokenAmount amount={formattedFilledAmount} tokenSymbol={formattedFilledAmount.currency} /> for a
+        total of{' '}
+        <styledEl.StyledTokenAmount amount={formattedSwappedAmount} tokenSymbol={formattedSwappedAmount.currency} />
       </div>
 
       {!!surplusAmount && (
-        <div>
+        <styledEl.SurplusWrapper>
           <span>Order surplus: </span>
-          <Strong>
-            <TokenAmount amount={surplusAmount} tokenSymbol={surplusToken} />
-          </Strong>
-        </div>
+
+          <styledEl.SurplusAmount>
+            <styledEl.StyledTokenAmount amount={surplusAmount} tokenSymbol={surplusToken} />
+            {showFiatValue && (
+              <styledEl.FiatWrapper>
+                (<styledEl.StyledFiatAmount amount={fiatValue} />)
+              </styledEl.FiatWrapper>
+            )}
+          </styledEl.SurplusAmount>
+        </styledEl.SurplusWrapper>
       )}
-    </SummaryWrapper>
+    </styledEl.SummaryWrapper>
   )
 }
