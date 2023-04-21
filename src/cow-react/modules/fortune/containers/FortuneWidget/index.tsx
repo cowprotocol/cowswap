502cb4997ab4c4df70ebdcbc17271e10e6424fbf
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2023-04-21 17:48:45 +0100

    cow fortune styles

--- src/cow-react/modules/fortune/containers/FortuneWidget/index.tsx
+++ src/cow-react/modules/fortune/containers/FortuneWidget/index.tsx
@@ -13,97 +13,194 @@ import { SuccessBanner } from '@cow/pages/Claim/styled'
 import { Trans } from '@lingui/macro'
 import SVG from 'react-inlinesvg'
 import twitterImage from 'assets/cow-swap/twitter.svg'
+import fortuneCookieImage from 'assets/cow-swap/fortune-cookie.png'
 import { ExternalLink } from 'theme'
-import { ButtonPrimary } from 'components/Button'
+import { X } from 'react-feather'
+import { transparentize } from 'polished'
 
 const FortuneButton = styled.div`
-  @keyframes glowing {
-    from {
-      box-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #f0f, 0 0 40px #0ff, 0 0 50px #e60073, 0 0 60px #e60073,
-        0 0 70px #e60073;
-    }
-    to {
-      box-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6,
-        0 0 70px #ff4da6, 0 0 80px #ff4da6;
-    }
-  }
-
-  @keyframes swinging {
-    0% {
-      transform: rotate(10deg) translateY(0px);
-    }
-    50% {
-      transform: rotate(-5deg) translateY(30px);
-    }
-    100% {
-      transform: rotate(10deg) translateY(0px);
-    }
-  }
-
+  --size: 75px;
   display: inline-block;
   position: fixed;
   z-index: 500;
-  right: 100px;
-  bottom: 100px;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
+  right: 10px;
+  bottom: 94px;
+  width: var(--size);
+  height: var(--size);
+  border-radius: var(--size);
   text-align: center;
-  animation: glowing 1s ease-in-out infinite alternate, swinging 3.5s ease-in-out forwards infinite;
+  animation: floating 3.5s ease-in-out forwards infinite;
   cursor: pointer;
+  transform: scale(1);
 
-  &:before {
-    content: '🎉';
-    font-size: 76px;
-    position: relative;
-    top: -27px;
-    left: -7px;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    --size: 52px;
+    left: 65px;
+    right: initial;
+    bottom: 0;
+  `}
+
+  &::after {
+    --size: 90%;
+    content: "";
+    display: block;
+    background: url(${fortuneCookieImage}) no-repeat center 100%/contain;
+    width: var(--size);
+    height: var(--size);
+    transition: transform 0.3s ease-in-out;
   }
+
+  &:hover::after {
+    transform: scale(1.4);
+  }
+
+  @keyframes floating {
+    0% {
+      transform: scale(1) rotate(10deg)
+    }
+    50% {
+      transform: scale(1) rotate(-5deg)
+    }
+    52% {
+      transform: scale(1.2) rotate(-5deg)
+    }
+    54% {
+      transform: scale(1) rotate(-5deg)
+    }
+    56% {
+      transform: scale(1.2) rotate(-5deg)
+    }
+    58% {
+      transform: scale(1) rotate(-5deg)
+    }
+    100% {
+      transform: scale(1) rotate(10deg)
+    }
+  }
+}
 `
 
 const FortuneBanner = styled.div`
   position: fixed;
-  right: 50px;
-  bottom: 20%;
+  width: 100vw;
+  height: 100vh;
+  right: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  margin: auto;
   z-index: 501;
-  background: #fff;
-  padding: 20px;
-  box-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6,
-    0 0 80px #ff4da6;
-  border-radius: 12px;
+  background: ${({ theme }) => transparentize(0.95, theme.text3)};
+  backdrop-filter: blur(45px);
+  padding: 32px;
+  animation: open 0.3s ease-in-out forwards;
+  overflow-y: auto;
+
+  @keyframes open {
+    from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+  }
 `
 
 const FortuneBannerActions = styled.div`
+  display: flex;
+  background: ${({ theme }) => theme.grey1};
+  padding: 0;
+  border-radius: 24px;
+
+  // Tweet button
+  > a > div {
+    margin: 0;
+    gap: 12px;
+  }
+`
+
+const FortuneTitle = styled.h2`
   display: block;
+  width: 100%;
+  font-size: 21px;
+  text-align: center;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text2};
+
+  > i {
+    font-size: 16px;
+    text-transform: uppercase;
+    display: block;
+    width: 100%;
+    font-weight: 300;
+    letter-spacing: 2px;
+  }
 `
 
 const FortuneText = styled.h3`
-  margin-top: 0;
-  padding: 20px;
-  font-size: 25px;
-  max-width: 400px;
-  border-radius: 40px;
+  padding: 24px;
+  width: 100%;
+  font-size: 34px;
+  border-radius: 42px;
+  word-break: break-word;
+  margin: 34px auto 70px;
+  font-weight: 700;
+  text-align: center;
   position: relative;
-  background: rgba(255, 77, 166, 0.09);
+  background: ${({ theme }) => theme.grey1};
 
   &:before {
     content: '“';
-    left: -5px;
-    top: 30px;
+    top: -70px;
+    left: 0;
   }
 
   &:after {
     content: '”';
-    right: -5px;
-    bottom: -15px;
+    bottom: -120px;
+    right: 0;
   }
 
   &:before,
   &:after {
-    font-size: 82px;
+    font-size: 100px;
     position: absolute;
     z-index: 1;
-    line-height: 0;
+    opacity: 0.3;
+  }
+`
+
+const FortuneContent = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 500px;
+`
+
+const StyledCloseIcon = styled(X)`
+  --size: 56px;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  height: var(--size);
+  width: var(--size);
+  opacity: 0.4;
+  transition: opacity 0.3s ease-in-out;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    --size: 42px;
+  `}
+
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+
+  > line {
+    stroke: ${({ theme }) => theme.text1};
   }
 `
 
@@ -136,18 +233,21 @@ export function FortuneWidget() {
     <>
       {openFortune && (
         <FortuneBanner>
-          <FortuneText>{openFortune.text}</FortuneText>
-          <FortuneBannerActions>
-            <ExternalLink href={`https://twitter.com/intent/tweet?text=${twitterText}`}>
-              <SuccessBanner type={'Twitter'}>
-                <span>
-                  <Trans>Share on Twitter</Trans>
-                </span>
-                <SVG src={twitterImage} description="Twitter" />
-              </SuccessBanner>
-            </ExternalLink>
-            <ButtonPrimary onClick={() => updateOpenFortune({ openFortune: null })}>Close</ButtonPrimary>
-          </FortuneBannerActions>
+          <StyledCloseIcon onClick={() => updateOpenFortune({ openFortune: null })}>Close</StyledCloseIcon>
+          <FortuneTitle>CoW Fortune <i>of the day</i></FortuneTitle>
+          <FortuneContent>
+            <FortuneText>{openFortune.text}</FortuneText>
+            <FortuneBannerActions>
+              <ExternalLink href={`https://twitter.com/intent/tweet?text=${twitterText}`}>
+                <SuccessBanner type={'Twitter'}>
+                  <span>
+                    <Trans>Share on Twitter</Trans>
+                  </span>
+                  <SVG src={twitterImage} description="Twitter" />
+                </SuccessBanner>
+              </ExternalLink>
+            </FortuneBannerActions>
+          </FortuneContent>
         </FortuneBanner>
       )}
       {isFortuneButtonVisible && <FortuneButton onClick={openRandomFortune}></FortuneButton>}
