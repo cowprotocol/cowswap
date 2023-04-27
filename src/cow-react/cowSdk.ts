3b69053b6cd9076eb350f72227756cc42e4c4670
Alexandr Kazachenko <shoom3301@gmail.com>
2023-04-27 22:17:11 +0600

    chore: pulled changes from develop

--- src/cow-react/cowSdk.ts
+++ src/cow-react/cowSdk.ts
@@ -1,6 +1,17 @@
 import { MetadataApi } from '@cowprotocol/app-data'
-import { OrderBookApi } from '@cowprotocol/cow-sdk'
+import { OrderBookApi, SupportedChainId, ApiBaseUrls, ORDER_BOOK_PROD_CONFIG } from '@cowprotocol/cow-sdk'
 import { isBarn, isDev, isLocal, isPr } from 'utils/environments'
 
+const ORDER_BOOK_DEV_CONFIG: ApiBaseUrls = {
+  [SupportedChainId.MAINNET]: 'https://dev.cow.fi/mainnet',
+  [SupportedChainId.GNOSIS_CHAIN]: 'https://dev.cow.fi/xdai',
+  [SupportedChainId.GOERLI]: 'https://dev.cow.fi/goerli',
+}
+
+const isNotProd = isLocal || isDev || isPr || isBarn
+
 export const metadataApiSDK = new MetadataApi()
-export const orderBookApi = new OrderBookApi({ env: isLocal || isDev || isPr || isBarn ? 'staging' : 'prod' })
+export const orderBookApi = new OrderBookApi({
+  env: isNotProd ? 'staging' : 'prod',
+  baseUrls: isNotProd ? ORDER_BOOK_DEV_CONFIG : ORDER_BOOK_PROD_CONFIG,
+})
