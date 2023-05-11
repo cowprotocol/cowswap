8d5ae01eb89384ee1f4a5c8b2e7118cc345d258a
Alfetopito <alfetopito@users.noreply.github.com>
2023-05-11 17:39:39 +0100

    test: add urls params cypress tests for limit orders

--- cypress-custom/integration/limit-orders.test.ts
+++ cypress-custom/integration/limit-orders.test.ts
@@ -22,4 +22,47 @@ describe('Limit orders', () => {
     )
     cy.get('#limit-orders-confirm #output-currency-preview .token-amount-input').should('contain.text', '200B')
   })
+
+  describe('url params', () => {
+    const SELL_TOKEN = 'WETH'
+    const BUY_TOKEN = 'DAI'
+
+    it('should accept sellAmount url param', () => {
+      cy.visit(`/#/${CHAIN_ID}/limit-orders/${SELL_TOKEN}?sellAmount=0.1`)
+      cy.get('#unlock-limit-orders-btn').click()
+      cy.get('#input-currency-input .token-amount-input').should('have.value', '0.1')
+    })
+
+    it('should not accept sellAmount when there is no sell token', () => {
+      cy.visit(`/#/${CHAIN_ID}/limit-orders/_/${BUY_TOKEN}?sellAmount=0.1`)
+      cy.get('#unlock-limit-orders-btn').click()
+      cy.get('#input-currency-input .token-amount-input').should('have.value', '')
+    })
+
+    it('should accept buyAmount url param', () => {
+      cy.visit(`/#/${CHAIN_ID}/limit-orders/_/${BUY_TOKEN}?buyAmount=0.1`)
+      cy.get('#unlock-limit-orders-btn').click()
+      cy.get('#output-currency-input .token-amount-input').should('have.value', '0.1')
+    })
+
+    it('should not accept buyAmount when there is no buy token', () => {
+      cy.visit(`/#/${CHAIN_ID}/limit-orders?buyAmount=0.1`)
+      cy.get('#unlock-limit-orders-btn').click()
+      cy.get('#output-currency-input .token-amount-input').should('have.value', '')
+    })
+
+    it('should accept both sellAmount and buyAmount url params', () => {
+      cy.visit(`/#/${CHAIN_ID}/limit-orders/${SELL_TOKEN}/${BUY_TOKEN}?sellAmount=0.1&buyAmount=0.2`)
+      cy.get('#unlock-limit-orders-btn').click()
+      cy.get('#input-currency-input .token-amount-input').should('have.value', '0.1')
+      cy.get('#output-currency-input .token-amount-input').should('have.value', '0.2')
+    })
+
+    it('should ignore invalid sellAmount and buyAmount url params', () => {
+      cy.visit(`/#/${CHAIN_ID}/limit-orders/${SELL_TOKEN}/${BUY_TOKEN}?sellAmount=rwe&buyAmount=aaa`)
+      cy.get('#unlock-limit-orders-btn').click()
+      cy.get('#input-currency-input .token-amount-input').should('have.value', '')
+      cy.get('#output-currency-input .token-amount-input').should('have.value', '')
+    })
+  })
 })
