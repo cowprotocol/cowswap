a22bf2cc1c5e92103d86b2279cbd5ba399219d3b
Anxo Rodriguez <anxolin@gmail.com>
2023-05-12 14:04:55 +0100

    chore: alllow state to be undefined

--- src/cow-react/modules/utm/state.ts
+++ src/cow-react/modules/utm/state.ts
@@ -1,4 +1,4 @@
 import { UtmParams } from './types'
 import { atomWithStorage } from 'jotai/utils'
 
-export const utmAtom = atomWithStorage<UtmParams>('utm-atom:v1', {})
+export const utmAtom = atomWithStorage<UtmParams | undefined>('utm-atom:v1', undefined)
