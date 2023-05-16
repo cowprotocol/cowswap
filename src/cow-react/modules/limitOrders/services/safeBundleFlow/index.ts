4560f777c3392103a9d81bd7fae2c163ffc6f42a
Alfetopito <alfetopito@users.noreply.github.com>
2023-05-16 10:10:54 +0100

    feat: after order creation, add it hidden to store. Unhide if wallet returns

--- src/cow-react/modules/limitOrders/services/safeBundleFlow/index.ts
+++ src/cow-react/modules/limitOrders/services/safeBundleFlow/index.ts
@@ -10,6 +10,7 @@ import { buildApproveTx } from '@cow/modules/operations/bundle/buildApproveTx'
 import { buildPresignTx } from '@cow/modules/operations/bundle/buildPresignTx'
 import { MetaTransactionData } from '@safe-global/safe-core-sdk-types'
 import { PriceImpactDeclineError, SafeBundleFlowContext } from '@cow/modules/limitOrders/services/types'
+import { partialOrderUpdate } from '@src/state/orders/utils'
 
 const LOG_PREFIX = 'LIMIT ORDER SAFE BUNDLE FLOW'
 
@@ -62,11 +63,23 @@ export async function safeBundleFlow(
       signer: provider.getSigner(),
       validTo,
     })
+    logTradeFlow(LOG_PREFIX, 'STEP 4: add order, but hidden')
+    addPendingOrderStep(
+      {
+        id: orderId,
+        chainId: chainId,
+        order: {
+          ...order,
+          isHidden: true,
+        },
+      },
+      dispatch
+    )
 
-    logTradeFlow(LOG_PREFIX, 'STEP 4: build presign tx')
+    logTradeFlow(LOG_PREFIX, 'STEP 5: build presign tx')
     const presignTx = await buildPresignTx({ settlementContract, orderId })
 
-    logTradeFlow(LOG_PREFIX, 'STEP 5: send safe tx')
+    logTradeFlow(LOG_PREFIX, 'STEP 6: send safe tx')
     const safeTransactionData: MetaTransactionData[] = [
       { to: approveTx.to!, data: approveTx.data!, value: '0', operation: 0 },
       { to: presignTx.to!, data: presignTx.data!, value: '0', operation: 0 },
@@ -74,27 +87,27 @@ export async function safeBundleFlow(
 
     const safeTx = await safeAppsSdk.txs.send({ txs: safeTransactionData })
 
-    logTradeFlow(LOG_PREFIX, 'STEP 6: add pending order step')
-    addPendingOrderStep(
+    logTradeFlow(LOG_PREFIX, 'STEP 7: add safe tx hash and unhide order')
+    partialOrderUpdate(
       {
-        id: orderId,
         chainId: chainId,
         order: {
-          ...order,
+          id: order.id,
           presignGnosisSafeTxHash: safeTx.safeTxHash,
+          isHidden: false,
         },
       },
       dispatch
     )
 
-    logTradeFlow(LOG_PREFIX, 'STEP 6: add app data to upload queue')
+    logTradeFlow(LOG_PREFIX, 'STEP 8: add app data to upload queue')
     addAppDataToUploadQueue({ chainId, orderId, appData })
 
     // tradeFlowAnalytics.sign(swapFlowAnalyticsContext)
 
     return orderId
   } catch (error: any) {
-    logTradeFlow(LOG_PREFIX, 'STEP 7: ERROR: ', error)
+    logTradeFlow(LOG_PREFIX, 'STEP 9: ERROR: ', error)
     // const swapErrorMessage = getSwapErrorMessage(error)
 
     // TODO: handle analytics stuff
