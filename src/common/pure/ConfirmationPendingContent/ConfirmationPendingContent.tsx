f1fea92ad802118f531a0b020f0732599bbd1a40
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-05-22 04:02:32 +0200

    feat(usdt-approval): :sparkles: add confirmation pending content component

---
+++ src/common/pure/ConfirmationPendingContent/ConfirmationPendingContent.tsx
@@ -0,0 +1,67 @@
+import { Trans } from '@lingui/macro'
+import { CheckCircle, UserCheck } from 'react-feather'
+import { ReactNode } from 'react'
+import { Wrapper } from './styled'
+import { UpperSection } from './styled'
+import { CloseIconWrapper } from './styled'
+import { WalletIcon } from './styled'
+import { LowerSection } from './styled'
+import { StepsWrapper } from './styled'
+import { StepsIconWrapper } from './styled'
+
+interface ConfirmationPendingContentProps {
+  onDismiss: () => void
+  statusIcon: ReactNode
+  title: string | ReactNode
+  description: string | ReactNode
+  operationSubmittedMessage: string
+  operationLabel: string
+  walletNameLabel?: string
+  walletAddress?: string
+}
+
+export function ConfirmationPendingContent({
+  onDismiss,
+  statusIcon,
+  title,
+  description,
+  operationSubmittedMessage,
+  operationLabel,
+  walletNameLabel = 'wallet',
+  walletAddress,
+}: ConfirmationPendingContentProps) {
+  return (
+    <Wrapper>
+      <UpperSection>
+        <CloseIconWrapper onClick={onDismiss} />
+        <WalletIcon>{statusIcon}</WalletIcon>
+        <span>{title}</span>
+      </UpperSection>
+      <LowerSection>
+        <h3>
+          <span>{description}</span>
+        </h3>
+
+        <StepsWrapper>
+          <div>
+            <StepsIconWrapper>
+              <UserCheck />
+            </StepsIconWrapper>
+            <p>
+              <Trans>
+                Sign the {operationLabel} with your {walletNameLabel}. {walletAddress && <span>{walletAddress}</span>}
+              </Trans>
+            </p>
+          </div>
+          <hr />
+          <div>
+            <StepsIconWrapper>
+              <CheckCircle />
+            </StepsIconWrapper>
+            <p>{operationSubmittedMessage}</p>
+          </div>
+        </StepsWrapper>
+      </LowerSection>
+    </Wrapper>
+  )
+}
