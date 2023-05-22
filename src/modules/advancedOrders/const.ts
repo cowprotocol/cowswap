631e928ac593d5cb33e567103dcc9c4e055903b4
shoom3301 <shoom3301@gmail.com>
2023-05-22 13:32:07 +0600

    feat(twap): twap order creation services

---
+++ src/modules/advancedOrders/const.ts
@@ -0,0 +1,7 @@
+import { SupportedChainId } from '@cowprotocol/cow-sdk'
+
+export const COMPOSABLE_COW_ADDRESS: Record<SupportedChainId, string> = {
+  1: 'TODO',
+  100: 'TODO',
+  5: '0xa31b99bd44528c7bae9e1f675d810ae13b0e29aa',
+}
---
+++ src/modules/twap/types.ts
@@ -0,0 +1,23 @@
+import { CurrencyAmount, Token } from '@uniswap/sdk-core'
+
+export interface TWAPOrder {
+  sellAmount: CurrencyAmount<Token>
+  buyAmount: CurrencyAmount<Token>
+  receiver: string
+  numOfParts: number
+  startTime: number
+  timeInterval: number
+  span: number
+}
+
+export interface TWAPOrderStruct {
+  sellToken: string
+  buyToken: string
+  receiver: string
+  partSellAmount: string
+  minPartLimit: string
+  t0: number
+  n: number
+  t: number
+  span: number
+}
