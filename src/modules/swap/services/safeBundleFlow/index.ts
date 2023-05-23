91c1761cd9b22cd80320986c37a12fc4008c9dff
Alfetopito <alfetopito@users.noreply.github.com>
2023-05-23 16:20:31 +0100

    feat: hide Safe bundling on Swap before signature

--- src/modules/swap/services/safeBundleFlow/index.ts
+++ src/modules/swap/services/safeBundleFlow/index.ts
@@ -8,6 +8,7 @@ import { getSwapErrorMessage } from 'modules/trade/utils/swapErrorHelper'
 import { addPendingOrderStep } from 'modules/trade/utils/addPendingOrderStep'
 import { PriceImpact } from 'legacy/hooks/usePriceImpact'
 import { signAndPostOrder } from 'legacy/utils/trade'
+import { partialOrderUpdate } from 'legacy/state/orders/utils'
 import { tradeFlowAnalytics } from 'modules/trade/utils/analytics'
 
 const LOG_PREFIX = 'SAFE BUNDLE FLOW'
@@ -54,6 +55,18 @@ export async function safeBundleFlow(
       callbacks.closeModals()
     })
 
+    addPendingOrderStep(
+      {
+        id: orderId,
+        chainId: context.chainId,
+        order: {
+          ...order,
+          isHidden: true,
+        },
+      },
+      dispatch
+    )
+
     logTradeFlow(LOG_PREFIX, 'STEP 4: build presign tx')
     const presignTx = await buildPresignTx({ settlementContract, orderId })
 
@@ -65,14 +78,14 @@ export async function safeBundleFlow(
 
     const safeTx = await safeAppsSdk.txs.send({ txs: safeTransactionData })
 
-    logTradeFlow(LOG_PREFIX, 'STEP 6: add tx to store')
-    addPendingOrderStep(
+    logTradeFlow(LOG_PREFIX, 'STEP 6: add safe tx hash and unhide order')
+    partialOrderUpdate(
       {
-        id: orderId,
         chainId: context.chainId,
         order: {
-          ...order,
+          id: order.id,
           presignGnosisSafeTxHash: safeTx.safeTxHash,
+          isHidden: false,
         },
       },
       dispatch
