64cdc3f8dcde780310f42f1d5240f1c92368ee36
shoom3301 <shoom3301@gmail.com>
2023-05-24 13:47:06 +0600

    test: ts-mockito test

--- src/legacy/state/orders/middlewares/pendingOrderActionMiddleware.test.tsx
+++ src/legacy/state/orders/middlewares/pendingOrderActionMiddleware.test.tsx
@@ -1,42 +1,42 @@
 import { pendingOrderActionMiddleware } from './pendingOrderActionMiddleware'
 import { MiddlewareAPI } from '@reduxjs/toolkit'
-import { Dispatch } from 'redux'
+import { AnyAction, Dispatch } from 'redux'
 import { AppState } from '../../index'
 import { AddPendingOrderParams } from '../actions'
 import { OrderClass } from '@cowprotocol/cow-sdk'
+import { capture, instance, mock, when } from 'ts-mockito'
+
+const ordersMockData = {
+  1: {
+    pending: {
+      '0x001': {
+        id: '0x001',
+        order: {
+          summary: 'summary',
+          orderClass: OrderClass.LIMIT,
+          orderCreationHash: '0xhash',
+        },
+      },
+    },
+  },
+}
+
 describe('pendingOrderActionMiddleware', () => {
   it('When some conditions, then should dispatch addPopup event', () => {
-    const dispatchFn = jest.fn()
-    const store = {
-      dispatch: dispatchFn,
-      getState: () => ({
-        orders: {
-          1: {
-            pending: {
-              '0x001': {
-                id: '0x001',
-                order: {
-                  summary: 'summary',
-                  orderClass: OrderClass.LIMIT,
-                  orderCreationHash: '0xhash',
-                },
-              },
-            },
-          },
-        },
-      }),
-    } as any as MiddlewareAPI<Dispatch, AppState>
-
-    const payload = {
-      id: '0x001',
-      chainId: 1,
-    } as any as AddPendingOrderParams
-
     const result = {}
 
-    pendingOrderActionMiddleware(store, payload, result)
+    const storeMock = mock<MiddlewareAPI<Dispatch, AppState>>()
+    const payloadMock = mock<AddPendingOrderParams>()
 
-    expect(dispatchFn).toHaveBeenCalledTimes(1)
-    expect(dispatchFn.mock.calls[0]).toMatchSnapshot()
+    when(storeMock.getState()).thenReturn({ orders: ordersMockData } as any)
+
+    when(payloadMock.chainId).thenReturn(1)
+    when(payloadMock.id).thenReturn('0x001')
+
+    pendingOrderActionMiddleware(instance(storeMock), instance(payloadMock), result)
+
+    const [addPopupAction] = capture(storeMock.dispatch<AnyAction>).first()
+
+    expect(addPopupAction).toMatchSnapshot()
   })
 })
