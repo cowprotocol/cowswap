9d9f6741abbe6d8e1cba095e2336eec47e4554d3
nenadV91 <nenadwebdevelop@gmail.com>
2023-05-26 09:51:02 +0200

    fix: fix some PR issues

--- src/modules/limitOrders/utils/isUnsupportedTokenInQuote.ts
+++ src/modules/limitOrders/utils/isUnsupportedTokenInQuote.ts
@@ -1,6 +1,6 @@
-import { LimitOrdersQuoteState } from 'modules/limitOrders/state/limitOrdersQuoteAtom'
 import { GpQuoteErrorCodes } from 'api/gnosisProtocol/errors/QuoteError'
+import { TradeQuoteState } from 'modules/tradeQuote'
 
-export function isUnsupportedTokenInQuote(state: LimitOrdersQuoteState): boolean {
+export function isUnsupportedTokenInQuote(state: TradeQuoteState): boolean {
   return state.error?.type === GpQuoteErrorCodes.UnsupportedToken
 }
