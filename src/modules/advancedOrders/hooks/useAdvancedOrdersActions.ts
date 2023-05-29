50525edef076318155fe7e15fc1bc53eed253e49
nenadV91 <nenadwebdevelop@gmail.com>
2023-05-29 16:28:58 +0200

    feat: swap SELL and BUY fields

--- src/modules/advancedOrders/hooks/useAdvancedOrdersActions.ts
+++ src/modules/advancedOrders/hooks/useAdvancedOrdersActions.ts
@@ -1,3 +1,4 @@
+import { useSetAtom } from 'jotai'
 import { useCallback } from 'react'
 import { Currency } from '@uniswap/sdk-core'
 import { useNavigateOnCurrencySelection } from 'modules/trade/hooks/useNavigateOnCurrencySelection'
@@ -5,15 +6,25 @@ import { useUpdateCurrencyAmount } from 'modules/trade/hooks/useUpdateCurrencyAm
 import { Field } from 'legacy/state/swap/actions'
 import { useAdvancedOrdersDerivedState } from './useAdvancedOrdersDerivedState'
 import { updateTradeQuoteAtom } from 'modules/tradeQuote'
-import { useSetAtom } from 'jotai'
+import { useTradeNavigate } from 'modules/trade/hooks/useTradeNavigate'
+import { useWalletInfo } from 'modules/wallet'
+import { useAdvancedOrdersRawState } from './useAdvancedOrdersRawState'
+import { useIsWrapOrUnwrap } from 'modules/trade/hooks/useIsWrapOrUnwrap'
+import { useUpdateAdvancedOrdersRawState } from './useAdvancedOrdersRawState'
+import { FractionUtils } from 'utils/fractionUtils'
 
 // TODO: this should be also unified for each trade widget (swap, limit, advanced)
 export function useAdvancedOrdersActions() {
-  const { inputCurrency } = useAdvancedOrdersDerivedState()
+  const { chainId } = useWalletInfo()
+  const { inputCurrency, outputCurrencyAmount } = useAdvancedOrdersDerivedState()
+  const { inputCurrencyId, outputCurrencyId } = useAdvancedOrdersRawState()
 
   const naviageOnCurrencySelection = useNavigateOnCurrencySelection()
   const updateCurrencyAmount = useUpdateCurrencyAmount()
   const updateQuoteState = useSetAtom(updateTradeQuoteAtom)
+  const updateAdvancedOrderRawState = useUpdateAdvancedOrdersRawState()
+  const tradeNavigate = useTradeNavigate()
+  const isWrapOrUnwrap = useIsWrapOrUnwrap()
 
   const onCurrencySelection = useCallback(
     (field: Field, currency: Currency | null) => {
@@ -46,10 +57,31 @@ export function useAdvancedOrdersActions() {
     console.log('On change recipient')
   }, [])
 
-  // TODO: implement this one
   const onSwitchTokens = useCallback(() => {
-    console.log('On switch tokens')
-  }, [])
+    if (!isWrapOrUnwrap) {
+      updateQuoteState({ response: null })
+      updateAdvancedOrderRawState({
+        inputCurrencyId: outputCurrencyId,
+        outputCurrencyId: inputCurrencyId,
+        inputCurrencyAmount: FractionUtils.serializeFractionToJSON(outputCurrencyAmount),
+        outputCurrencyAmount: null,
+      })
+    }
+
+    tradeNavigate(chainId, {
+      inputCurrencyId: outputCurrencyId,
+      outputCurrencyId: inputCurrencyId,
+    })
+  }, [
+    chainId,
+    tradeNavigate,
+    updateQuoteState,
+    updateAdvancedOrderRawState,
+    inputCurrencyId,
+    outputCurrencyId,
+    outputCurrencyAmount,
+    isWrapOrUnwrap,
+  ])
 
   return {
     onCurrencySelection,
