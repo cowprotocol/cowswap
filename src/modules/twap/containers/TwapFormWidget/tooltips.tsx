ff4593d379e37f274d69c19202101eedb79518cc
Lint Action <lint-action@samuelmeuli.com>
2023-06-06 09:14:56 +0000

    Fix code style issues with Prettier

--- src/modules/twap/containers/TwapFormWidget/tooltips.tsx
+++ src/modules/twap/containers/TwapFormWidget/tooltips.tsx
@@ -16,9 +16,9 @@ export const LABELS_TOOLTIPS: LabelTooltipItems = {
     label: 'Total duration',
     tooltip: (
       <>
-        The "Total duration" is the duration it takes to execute all parts of your TWAP order. For instance, your
-        order consists of 2 parts placed every 30 minutes, the total time to complete the order is 1 hour. Each limit
-        order remains open for 30 minutes until the next part becomes active.
+        The "Total duration" is the duration it takes to execute all parts of your TWAP order. For instance, your order
+        consists of 2 parts placed every 30 minutes, the total time to complete the order is 1 hour. Each limit order
+        remains open for 30 minutes until the next part becomes active.
       </>
     ),
   },
@@ -26,9 +26,9 @@ export const LABELS_TOOLTIPS: LabelTooltipItems = {
     label: 'Part duration',
     tooltip: (
       <>
-        The "Part duration" refers to the duration between each part of your TWAP order. Choosing a shorter time
-        allows for faster execution of each part, potentially reducing price fluctuations. Striking the right balance
-        is crucial for optimal execution.
+        The "Part duration" refers to the duration between each part of your TWAP order. Choosing a shorter time allows
+        for faster execution of each part, potentially reducing price fluctuations. Striking the right balance is
+        crucial for optimal execution.
       </>
     ),
   },
@@ -37,8 +37,8 @@ export const LABELS_TOOLTIPS: LabelTooltipItems = {
     tooltip: (
       <>
         This slippage will apply to each part of your order. Since a TWAP order executes over a longer period of time,
-        your slippage should take into account possible price fluctuations over that time. If your slippage is too
-        low, you risk some parts of your order failing to execute.
+        your slippage should take into account possible price fluctuations over that time. If your slippage is too low,
+        you risk some parts of your order failing to execute.
       </>
     ),
   },
