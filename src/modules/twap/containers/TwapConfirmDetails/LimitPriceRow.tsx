f943323751f349c64c98a2117a20eba39b4d5ee9
Alfetopito <alfetopito@users.noreply.github.com>
2023-06-08 14:36:06 +0100

    refactor: hide horrible button style

--- src/modules/twap/containers/TwapConfirmDetails/LimitPriceRow.tsx
+++ src/modules/twap/containers/TwapConfirmDetails/LimitPriceRow.tsx
@@ -7,6 +7,7 @@ import { Nullish } from 'types'
 import { ConfirmDetailsItem } from 'modules/twap/pure/ConfirmDetailsItem'
 
 import { ExecutionPrice } from 'common/pure/ExecutionPrice'
+import { RateWrapper } from 'common/pure/RateInfo'
 
 type Props = {
   price: Nullish<Price<Currency, Currency>>
@@ -18,8 +19,7 @@ export function LimitPriceRow(props: Props) {
   const [isInverted, setIsInverted] = isInvertedState
 
   return (
-    // TODO: style button
-    <button onClick={() => setIsInverted((curr) => !curr)}>
+    <RateWrapper onClick={() => setIsInverted((curr) => !curr)}>
       <ConfirmDetailsItem tooltip="TODO: limit price tooltip text" label="Limit price (incl fee/slippage)">
         {price ? (
           <ExecutionPrice executionPrice={price} isInverted={isInverted} showBaseCurrency separatorSymbol="=" />
@@ -27,6 +27,6 @@ export function LimitPriceRow(props: Props) {
           '-'
         )}
       </ConfirmDetailsItem>
-    </button>
+    </RateWrapper>
   )
 }
