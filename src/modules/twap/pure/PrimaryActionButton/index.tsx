41870cf78ce65580ebd45a5804305a3830b947cf
shoom3301 <shoom3301@gmail.com>
2023-06-15 13:21:39 +0600

    chore: remove TODDO

--- src/modules/twap/pure/PrimaryActionButton/index.tsx
+++ src/modules/twap/pure/PrimaryActionButton/index.tsx
@@ -9,7 +9,6 @@ export interface PrimaryActionButtonContext {
   confirmTrade(): void
 }
 
-// TODO: set correct buttons text
 const buttonsMap: Record<TwapFormState, (context: PrimaryActionButtonContext) => JSX.Element> = {
   [TwapFormState.LOADING_SAFE_INFO]: () => (
     <ButtonPrimary disabled={true} buttonSize={ButtonSize.BIG}>
