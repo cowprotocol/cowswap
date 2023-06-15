2e113da1579bb92864a958b3dcf1f9dddc65b578
nenadV91 <nenadwebdevelop@gmail.com>
2023-06-15 11:28:20 +0200

    fix: optimize icon and small code change

--- src/modules/wallet/web3-react/connection/walletConnectV2.tsx
+++ src/modules/wallet/web3-react/connection/walletConnectV2.tsx
@@ -4,18 +4,10 @@ import { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
 import { RPC_URLS } from 'legacy/constants/networks'
 import { useIsActiveWallet } from 'legacy/hooks/useIsActiveWallet'
 
-import { useWalletMetaData } from 'modules/wallet'
 import { ConnectionType } from 'modules/wallet'
 import { default as WalletConnectV2Image } from 'modules/wallet/api/assets/wallet-connect-v2.png'
 import { ConnectWalletOption } from 'modules/wallet/api/pure/ConnectWalletOption'
-import {
-  getConnectionName,
-  getIsZengoWallet,
-  getIsAmbireWallet,
-  getIsAlphaWallet,
-  getIsTrustWallet,
-} from 'modules/wallet/api/utils/connection'
-import { WC_DISABLED_TEXT } from 'modules/wallet/constants'
+import { getConnectionName } from 'modules/wallet/api/utils/connection'
 
 import { TryActivation, onError } from '.'
 
@@ -23,6 +15,7 @@ import { Web3ReactConnection } from '../types'
 
 const WC_PROJECT_ID = process.env.REACT_APP_WC_PROJECT_ID
 const WC_DEFAULT_PROJECT_ID = 'a6cc11517a10f6f12953fd67b1eb67e7'
+const TOOLTIP_TEXT = 'Under development and unsupported by most wallets'
 
 export const walletConnectV2Option = {
   color: '#4196FC',
@@ -53,23 +46,14 @@ export const walletConnectConnectionV2: Web3ReactConnection = {
 }
 
 export function WalletConnectV2Option({ tryActivation }: { tryActivation: TryActivation }) {
-  const { walletName } = useWalletMetaData()
-
-  const isWalletConnect = useIsActiveWallet(walletConnectConnectionV2)
-  const isActive =
-    isWalletConnect &&
-    !getIsZengoWallet(walletName) &&
-    !getIsAmbireWallet(walletName) &&
-    !getIsAlphaWallet(walletName) &&
-    !getIsTrustWallet(null, walletName)
-  const tooltipText = !isActive && isWalletConnect ? WC_DISABLED_TEXT : null
+  const isActive = useIsActiveWallet(walletConnectConnectionV2)
 
   return (
     <ConnectWalletOption
       {...walletConnectV2Option}
       isActive={isActive}
-      tooltipText={tooltipText}
-      clickable={!isWalletConnect}
+      tooltipText={TOOLTIP_TEXT}
+      clickable={!isActive}
       onClick={() => tryActivation(walletConnectConnectionV2.connector)}
       header={getConnectionName(ConnectionType.WALLET_CONNECT_V2)}
     />
