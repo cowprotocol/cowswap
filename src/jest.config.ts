f7688512b6a8ecfcab309e59490ca725b07c0bf6
Anxo Rodriguez <anxolin@gmail.com>
2023-06-20 17:36:23 +0100

    fix: make test to work

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,11 +1,13 @@
+// this is not used for now. we use "craco test", but eventually we will
+
 /* eslint-disable */
 export default {
-  displayName: 'ui',
-  preset: '../../../jest.preset.js',
+  displayName: 'cowswap',
+  preset: '../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/ui',
+  coverageDirectory: '../../coverage/cowswap',
 }
