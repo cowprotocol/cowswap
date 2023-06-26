013f7ef07146c01adbd32a1013ba94c0e8f67b5f
Alexandr Kazachenko <shoom3301@gmail.com>
2023-06-26 15:25:32 +0600

    docs(injected-widget): documentation (#2719)

--- src/modules/injectedWidget/updaters/InjectedWidgetUpdater.ts
+++ src/modules/injectedWidget/updaters/InjectedWidgetUpdater.ts
@@ -1,7 +1,9 @@
 import { useUpdateAtom } from 'jotai/utils'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { useNavigate } from 'react-router-dom'
+
+import { deepEqual } from 'utils/deepEqual'
 
 import { injectedWidgetParamsAtom } from '../state/injectedWidgetParamsAtom'
 
@@ -10,6 +12,7 @@ const COW_SWAP_WIDGET_EVENT_KEY = 'cowSwapWidget'
 export function InjectedWidgetUpdater() {
   const updateParams = useUpdateAtom(injectedWidgetParamsAtom)
   const navigate = useNavigate()
+  const prevData = useRef(null)
 
   useEffect(() => {
     window.addEventListener('message', (event) => {
@@ -18,6 +21,9 @@ export function InjectedWidgetUpdater() {
       if (event.data.key !== COW_SWAP_WIDGET_EVENT_KEY || !method) return
 
       if (method === 'update') {
+        if (prevData.current && deepEqual(prevData.current, event.data)) return
+
+        prevData.current = event.data
         updateParams(event.data.appParams)
         navigate(event.data.urlParams)
       }
