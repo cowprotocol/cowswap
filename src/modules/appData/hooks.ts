b7a1cc9b1be9fb5d143eb01b837d221bc75f4c74
Alexandr Kazachenko <shoom3301@gmail.com>
2023-06-26 17:32:20 +0600

    feat(injected-widget): set app-data appCode from integrator (#2721)

--- src/modules/appData/hooks.ts
+++ src/modules/appData/hooks.ts
@@ -1,11 +1,16 @@
 import { useAtomValue, useUpdateAtom } from 'jotai/utils'
+import { useMemo } from 'react'
 
 import { DEFAULT_APP_CODE, SAFE_APP_CODE } from 'legacy/constants'
 
 import { useIsSafeApp } from 'modules/wallet'
 
+import { isInjectedWidget } from 'common/utils/isInjectedWidget'
+
 import { addAppDataToUploadQueueAtom, appDataInfoAtom } from './state/atoms'
 import { AppDataInfo } from './types'
+
+import { injectedWidgetMetaDataAtom } from '../injectedWidget/state/injectedWidgetMetaDataAtom'
 
 const APP_CODE = process.env.REACT_APP_APP_CODE
 
@@ -17,13 +22,20 @@ export function useAppData(): AppDataInfo | null {
   return useAtomValue(appDataInfoAtom)
 }
 
-export function useAppCode(): string {
+export function useAppCode(): string | null {
+  const injectedWidgetMetaData = useAtomValue(injectedWidgetMetaDataAtom)
   const isSafeApp = useIsSafeApp()
 
-  if (APP_CODE) {
-    // appCode coming from env var has priority
-    return APP_CODE
-  }
+  return useMemo(() => {
+    if (isInjectedWidget()) {
+      return injectedWidgetMetaData?.appKey || null
+    }
 
-  return isSafeApp ? SAFE_APP_CODE : DEFAULT_APP_CODE
+    if (APP_CODE) {
+      // appCode coming from env var has priority
+      return APP_CODE
+    }
+
+    return isSafeApp ? SAFE_APP_CODE : DEFAULT_APP_CODE
+  }, [isSafeApp, injectedWidgetMetaData])
 }
