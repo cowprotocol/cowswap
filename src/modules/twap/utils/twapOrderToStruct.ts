c57b29f2f819e51f145e7c16b4a35ee289e68e05
shoom3301 <shoom3301@gmail.com>
2023-06-27 15:57:43 +0600

    fix(twap): set correct token address when buy asset is native

--- src/modules/twap/utils/twapOrderToStruct.ts
+++ src/modules/twap/utils/twapOrderToStruct.ts
@@ -1,9 +1,13 @@
+import { NATIVE_CURRENCY_BUY_ADDRESS } from 'legacy/constants'
+
 import { TWAPOrder, TWAPOrderStruct } from '../types'
 
 export function twapOrderToStruct(order: TWAPOrder): TWAPOrderStruct {
+  const buyToken = order.buyAmount.currency
+
   return {
     sellToken: order.sellAmount.currency.address,
-    buyToken: order.buyAmount.currency.address,
+    buyToken: buyToken.isNative ? NATIVE_CURRENCY_BUY_ADDRESS : buyToken.address,
     receiver: order.receiver,
     partSellAmount: order.sellAmount.divide(order.numOfParts).quotient.toString(),
     minPartLimit: order.buyAmount.divide(order.numOfParts).quotient.toString(),
