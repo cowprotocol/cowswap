90cd415ef555f906544256e8cc7632742946762d
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2023-07-04 16:44:46 +0100

    feat: container links

--- src/modules/application/containers/TradeWidgetLinks/index.tsx
+++ src/modules/application/containers/TradeWidgetLinks/index.tsx
@@ -1,42 +1,41 @@
 import { Trans } from '@lingui/macro'
+import { matchPath, useLocation } from 'react-router-dom'
 
 import { useTradeRouteContext } from 'modules/trade/hooks/useTradeRouteContext'
 import { parameterizeTradeRoute } from 'modules/trade/utils/parameterizeTradeRoute'
 
-import { Routes } from 'common/constants/routes'
+import { Routes, RoutesValues } from 'common/constants/routes'
 import { FeatureGuard } from 'common/containers/FeatureGuard'
 
 import * as styledEl from './styled'
 
 export function TradeWidgetLinks() {
   const tradeContext = useTradeRouteContext()
+  const location = useLocation()
+
+  const isActiveRoute = (route: RoutesValues) => {
+    const routePath = parameterizeTradeRoute(tradeContext, route)
+    const match = matchPath(location.pathname, routePath)
+    return !!match
+  }
 
   return (
     <styledEl.Wrapper>
-      <styledEl.MenuItem>
-        <styledEl.Link
-          className={({ isActive }) => (isActive ? 'active' : undefined)}
-          to={parameterizeTradeRoute(tradeContext, Routes.SWAP)}
-        >
+      <styledEl.MenuItem isActive={isActiveRoute(Routes.SWAP)}>
+        <styledEl.Link to={parameterizeTradeRoute(tradeContext, Routes.SWAP)}>
           <Trans>Swap</Trans>
         </styledEl.Link>
       </styledEl.MenuItem>
 
-      <styledEl.MenuItem>
-        <styledEl.Link
-          className={({ isActive }) => (isActive ? 'active' : undefined)}
-          to={parameterizeTradeRoute(tradeContext, Routes.LIMIT_ORDER)}
-        >
+      <styledEl.MenuItem isActive={isActiveRoute(Routes.LIMIT_ORDER)}>
+        <styledEl.Link to={parameterizeTradeRoute(tradeContext, Routes.LIMIT_ORDER)}>
           <Trans>Limit</Trans>
         </styledEl.Link>
       </styledEl.MenuItem>
 
       <FeatureGuard featureFlag="advancedOrdersEnabled">
-        <styledEl.MenuItem>
-          <styledEl.Link
-            className={({ isActive }) => (isActive ? 'active' : undefined)}
-            to={parameterizeTradeRoute(tradeContext, Routes.ADVANCED_ORDERS)}
-          >
+        <styledEl.MenuItem isActive={isActiveRoute(Routes.ADVANCED_ORDERS)}>
+          <styledEl.Link to={parameterizeTradeRoute(tradeContext, Routes.ADVANCED_ORDERS)}>
             <Trans>Advanced</Trans>
             <styledEl.Badge>
               <Trans>Beta</Trans>
