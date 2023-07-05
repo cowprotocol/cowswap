ae31bd9f50c73c3e7b7cdf04a7a108f0ee88019c
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-05 20:41:01 +0600

    fix(twap): don't request singleOrders data for just mined orders (#2761)

--- src/modules/twap/updaters/TwapOrdersUpdater.tsx
+++ src/modules/twap/updaters/TwapOrdersUpdater.tsx
@@ -4,14 +4,12 @@ import { useEffect, useMemo } from 'react'
 import { ComposableCoW } from '@cowprotocol/abis'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
-import { isTruthy } from 'legacy/utils/misc'
-
 import { TWAP_PENDING_STATUSES } from '../const'
 import { useFetchTwapOrdersFromSafe } from '../hooks/useFetchTwapOrdersFromSafe'
 import { useTwapDiscreteOrders } from '../hooks/useTwapDiscreteOrders'
 import { useTwapOrdersAuthMulticall } from '../hooks/useTwapOrdersAuthMulticall'
 import { twapOrdersListAtom, updateTwapOrdersListAtom } from '../state/twapOrdersListAtom'
-import { TwapOrderInfo } from '../types'
+import { TwapOrderInfo, TwapOrderItem, TwapOrdersSafeData } from '../types'
 import { buildTwapOrdersItems } from '../utils/buildTwapOrdersItems'
 import { getConditionalOrderId } from '../utils/getConditionalOrderId'
 import { isTwapOrderExpired } from '../utils/getTwapOrderStatus'
@@ -29,38 +27,11 @@ export function TwapOrdersUpdater(props: {
   const updateTwapOrders = useUpdateAtom(updateTwapOrdersListAtom)
   const ordersSafeData = useFetchTwapOrdersFromSafe(props)
 
-  const allOrdersInfo: TwapOrderInfo[] = useMemo(() => {
-    return ordersSafeData
-      .map((data) => {
-        try {
-          const id = getConditionalOrderId(data.conditionalOrderParams)
-          const order = parseTwapOrderStruct(data.conditionalOrderParams.staticInput)
-          const { executionDate } = data.safeTxParams
-
-          return {
-            id,
-            orderStruct: order,
-            safeData: data,
-            isExpired: isTwapOrderExpired(order, executionDate ? new Date(executionDate) : null),
-          }
-        } catch (e) {
-          return null
-        }
-      })
-      .filter(isTruthy)
-  }, [ordersSafeData])
+  const allOrdersInfo = useMemo(() => parseOrdersSafeData(ordersSafeData), [ordersSafeData])
 
   // Here we can split all orders in two groups: 1. Not signed + expired, 2. Open + cancelled
   const pendingOrCancelledOrders = useMemo(() => {
-    return allOrdersInfo.filter((info) => {
-      const existingOrder = twapOrdersList[info.id]
-
-      if (existingOrder) {
-        return TWAP_PENDING_STATUSES.includes(existingOrder.status)
-      }
-
-      return !info.isExpired && info.safeData.safeTxParams.isExecuted
-    })
+    return allOrdersInfo.filter((info) => shouldCheckOrderAuth(info, twapOrdersList[info.id]))
   }, [allOrdersInfo, twapOrdersList])
 
   // Here we know which orders are cancelled: if it's auth === false, then it's cancelled
@@ -74,4 +45,40 @@ export function TwapOrdersUpdater(props: {
   }, [chainId, safeAddress, allOrdersInfo, ordersAuthResult, twapDiscreteOrders, updateTwapOrders])
 
   return null
+}
+
+function parseOrdersSafeData(ordersSafeData: TwapOrdersSafeData[]): TwapOrderInfo[] {
+  return ordersSafeData.reduce<TwapOrderInfo[]>((acc, data) => {
+    try {
+      const info = {
+        id: getConditionalOrderId(data.conditionalOrderParams),
+        orderStruct: parseTwapOrderStruct(data.conditionalOrderParams.staticInput),
+        safeData: data,
+      }
+
+      acc.push(info)
+    } catch (e) {}
+
+    return acc
+  }, [])
+}
+
+function shouldCheckOrderAuth(info: TwapOrderInfo, existingOrder: TwapOrderItem | undefined): boolean {
+  const { isExecuted, confirmations, executionDate: _executionDate } = info.safeData.safeTxParams
+  const executionDate = _executionDate ? new Date(_executionDate) : null
+
+  // Skip expired orders
+  if (isTwapOrderExpired(info.orderStruct, executionDate)) return false
+
+  const isTxMined = isExecuted && confirmations > 0
+
+  // Skip not mined orders
+  if (!isTxMined) return false
+
+  // Skip NOT pending orders
+  if (existingOrder) {
+    return TWAP_PENDING_STATUSES.includes(existingOrder.status)
+  }
+
+  return true
 }
