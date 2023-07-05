10ab4c0dbf8d65ee8e18a3f10bcfd0103a65ce37
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-05 20:44:04 +0600

    feat(twap): display all TWAP parts in orders table (#2795)

--- src/api/gnosisProtocol/hooks.ts
+++ src/api/gnosisProtocol/hooks.ts
@@ -10,6 +10,7 @@ import { isBarnBackendEnv } from 'legacy/utils/environments'
 import { isTruthy } from 'legacy/utils/misc'
 import { supportedChainId } from 'legacy/utils/supportedChainId'
 
+import { emulatedPartOrdersAtom } from 'modules/twap/state/emulatedPartOrdersAtom'
 import { emulatedTwapOrdersAtom } from 'modules/twap/state/twapOrdersListAtom'
 import { TwapPartOrderItem, twapPartOrdersListAtom } from 'modules/twap/state/twapPartOrdersAtom'
 import { useWalletInfo } from 'modules/wallet'
@@ -17,6 +18,8 @@ import { useWalletInfo } from 'modules/wallet'
 import { OrderWithComposableCowInfo } from 'common/types'
 
 import { getOrders } from './api'
+
+type TwapPartOrdersMap = { [twapOrderHash: string]: TwapPartOrderItem }
 
 /**
  * TODO: refactor this hook
@@ -34,6 +37,7 @@ export function useGpOrders(account?: string | null, refreshInterval?: number): 
   const { chainId: _chainId } = useWalletInfo()
   const chainId = supportedChainId(_chainId)
   const emulatedTwapOrders = useAtomValue(emulatedTwapOrdersAtom)
+  const twapParticleOrders = useAtomValue(twapPartOrdersListAtom)
 
   const requestParams = useMemo(() => {
     return account ? { owner: account, limit: AMOUNT_OF_ORDERS_TO_FETCH } : null
@@ -67,13 +71,21 @@ export function useGpOrders(account?: string | null, refreshInterval?: number): 
     return isBarnBackendEnv ? loadedProdOrders : currentEnvOrders
   }, [currentEnvOrders, loadedProdOrders])
 
-  const twapChildOrders = useTwapChildOrders(prodOrders)
+  const twapPartOrdersMap: TwapPartOrdersMap = useMemo(() => {
+    return twapParticleOrders.reduce<TwapPartOrdersMap>((acc, val) => {
+      acc[val.uid] = val
+
+      return acc
+    }, {})
+  }, [twapParticleOrders])
+
+  const twapChildOrders = useTwapChildOrders(prodOrders, twapPartOrdersMap)
 
   const regularOrders: OrderWithComposableCowInfo[] = useMemo(() => {
     if (!currentEnvOrders) return []
 
-    return currentEnvOrders.map((order) => ({ order }))
-  }, [currentEnvOrders])
+    return currentEnvOrders.filter((order) => !twapPartOrdersMap[order.uid]).map((order) => ({ order }))
+  }, [currentEnvOrders, twapPartOrdersMap])
 
   return useMemo(() => {
     return [...regularOrders, ...emulatedTwapOrders, ...twapChildOrders]
@@ -81,21 +93,27 @@ export function useGpOrders(account?: string | null, refreshInterval?: number): 
 }
 
 // Take only orders are connected to TWAP orders
-function useTwapChildOrders(prodOrders: EnrichedOrder[] | undefined): OrderWithComposableCowInfo[] {
-  const twapParticleOrders = useAtomValue(twapPartOrdersListAtom)
+function useTwapChildOrders(
+  prodOrders: EnrichedOrder[] | undefined,
+  twapPartOrdersMap: TwapPartOrdersMap
+): OrderWithComposableCowInfo[] {
+  const emulatedPartOrders = useAtomValue(emulatedPartOrdersAtom)
 
-  return useMemo(() => {
-    if (!prodOrders) return []
-
-    const particleOrdersMap = twapParticleOrders.reduce<{ [uid: string]: TwapPartOrderItem }>((acc, val) => {
-      acc[val.uid] = val
-
+  const filteredEmulatedPartOrders = useMemo(() => {
+    const prodOrdersMap = (prodOrders || []).reduce<{ [key: string]: true }>((acc, val) => {
+      acc[val.uid] = true
       return acc
     }, {})
 
+    return emulatedPartOrders.filter((order) => !prodOrdersMap[order.order.uid])
+  }, [emulatedPartOrders, prodOrders])
+
+  const partOrdersFromProd = useMemo(() => {
+    if (!prodOrders) return []
+
     const orderWithComposableCowInfo: OrderWithComposableCowInfo[] = prodOrders
       .map((order) => {
-        const particleOrder = particleOrdersMap[order.uid]
+        const particleOrder = twapPartOrdersMap[order.uid]
 
         if (!particleOrder) return null
 
@@ -109,5 +127,9 @@ function useTwapChildOrders(prodOrders: EnrichedOrder[] | undefined): OrderWithC
       .filter(isTruthy)
 
     return orderWithComposableCowInfo
-  }, [twapParticleOrders, prodOrders])
+  }, [prodOrders, twapPartOrdersMap])
+
+  return useMemo(() => {
+    return [...partOrdersFromProd, ...filteredEmulatedPartOrders]
+  }, [partOrdersFromProd, filteredEmulatedPartOrders])
 }
---
+++ src/modules/twap/state/emulatedPartOrdersAtom.ts
@@ -0,0 +1,55 @@
+import { atom } from 'jotai'
+
+import { OrderClass, OrderStatus, SigningScheme } from '@cowprotocol/cow-sdk'
+
+import { OrderStatus as OrderStatusInApp } from 'legacy/state/orders/actions'
+
+import { OrderWithComposableCowInfo } from 'common/types'
+
+import { twapOrdersListAtom } from './twapOrdersListAtom'
+import { twapPartOrdersListAtom } from './twapPartOrdersAtom'
+
+import { TwapOrderItem, TwapOrderStatus } from '../types'
+
+export const emulatedPartOrdersAtom = atom<OrderWithComposableCowInfo[]>((get) => {
+  const twapOrders = get(twapOrdersListAtom)
+  const twapParticleOrders = get(twapPartOrdersListAtom)
+
+  return twapParticleOrders.map<OrderWithComposableCowInfo>((order) => {
+    const parent = twapOrders[order.twapOrderId]
+
+    const creationDate = new Date(1 + (order.order.validTo - parent.order.t) * 1000)
+
+    return {
+      order: {
+        ...order.order,
+        creationDate: creationDate.toISOString(),
+        class: OrderClass.LIMIT,
+        status: OrderStatus.OPEN,
+        owner: parent.safeAddress.toLowerCase(),
+        uid: order.uid,
+        signingScheme: SigningScheme.EIP1271,
+        signature: '',
+        appData: parent.order.appData,
+        totalFee: '0',
+        feeAmount: '0',
+        executedSellAmount: '0',
+        executedSellAmountBeforeFees: '0',
+        executedBuyAmount: '0',
+        executedFeeAmount: '0',
+        invalidated: false,
+      },
+      composableCowInfo: {
+        parentId: order.twapOrderId,
+        status: getPartOrderStatus(parent),
+      },
+    }
+  })
+})
+
+function getPartOrderStatus(parent: TwapOrderItem): OrderStatusInApp {
+  if (parent.status === TwapOrderStatus.Expired) return OrderStatusInApp.EXPIRED
+  if (parent.status === TwapOrderStatus.Cancelled) return OrderStatusInApp.CANCELLED
+
+  return OrderStatusInApp.SCHEDULED
+}
