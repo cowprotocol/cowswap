7901e1ac0fa6df6410be78382949675ac94be8a5
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-05 13:13:12 +0600

    feat(twap): generate all part orders (#2759)

--- src/modules/twap/containers/TwapFormWidget/index.tsx
+++ src/modules/twap/containers/TwapFormWidget/index.tsx
@@ -28,6 +28,7 @@ import { partsStateAtom } from '../../state/partsStateAtom'
 import { twapTimeIntervalAtom } from '../../state/twapOrderAtom'
 import { twapOrdersSettingsAtom, updateTwapOrdersSettingsAtom } from '../../state/twapOrdersSettingsAtom'
 import { FallbackHandlerVerificationUpdater } from '../../updaters/FallbackHandlerVerificationUpdater'
+import { PartOrdersUpdater } from '../../updaters/PartOrdersUpdater'
 import { TwapOrdersUpdater } from '../../updaters/TwapOrdersUpdater'
 import { deadlinePartsDisplay } from '../../utils/deadlinePartsDisplay'
 import { ActionButtons } from '../ActionButtons'
@@ -82,6 +83,7 @@ export function TwapFormWidget() {
     <>
       <QuoteObserverUpdater />
       <FallbackHandlerVerificationUpdater />
+      <PartOrdersUpdater />
       {shouldLoadTwapOrders && (
         <TwapOrdersUpdater composableCowContract={composableCowContract} safeAddress={account} chainId={chainId} />
       )}
