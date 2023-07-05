01ef65d7ce3f2034b8a624f4c82a316b0dba3131
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-05 13:43:21 +0600

    refactor: optimize useTwapOrdersExecutions

--- src/modules/twap/hooks/useTwapOrdersExecutions.ts
+++ src/modules/twap/hooks/useTwapOrdersExecutions.ts
@@ -2,7 +2,7 @@ import { useAtomValue } from 'jotai'
 import { useMemo } from 'react'
 
 import { Order, OrderInfoApi } from 'legacy/state/orders/actions'
-import { useOrders } from 'legacy/state/orders/hooks'
+import { useOrdersById } from 'legacy/state/orders/hooks'
 
 import { useWalletInfo } from 'modules/wallet'
 
@@ -12,40 +12,58 @@ import { TwapOrderExecutionInfo } from '../types'
 
 export type TwapOrdersExecutionMap = { [id: string]: TwapOrderExecutionInfo }
 
+type PartsIdsInfo = {
+  sets: { [id: string]: Set<string> }
+  ids: string[]
+}
+
 export function useTwapOrdersExecutions(ids: string[]): TwapOrdersExecutionMap {
   const { chainId } = useWalletInfo()
   const twapPartOrders = useAtomValue(twapPartOrdersAtom)
 
-  const allOrders = useOrders({ chainId })
+  const { sets: partSets, ids: allPartIds } = useMemo(() => {
+    return Object.keys(twapPartOrders).reduce<PartsIdsInfo>(
+      (acc, val) => {
+        const { sets, ids } = acc
 
-  const childrenIdSets = useMemo(() => {
-    return Object.keys(twapPartOrders).reduce<{ [id: string]: Set<string> }>((acc, val) => {
-      acc[val] = new Set<string>((twapPartOrders[val] || []).map((item) => item.uid))
-      return acc
-    }, {})
+        const partsIds = (twapPartOrders[val] || []).map((item) => item.uid)
+
+        sets[val] = new Set<string>(partsIds)
+        ids.push(...partsIds)
+
+        return acc
+      },
+      { sets: {}, ids: [] }
+    )
   }, [twapPartOrders])
+
+  const allPartOrdersMap = useOrdersById({ chainId, ids: allPartIds })
+
+  const allPartOrders = useMemo(() => {
+    if (!allPartOrdersMap) return []
+
+    return Object.values(allPartOrdersMap)
+  }, [allPartOrdersMap])
 
   return useMemo(() => {
     return ids.reduce<TwapOrdersExecutionMap>((acc, id) => {
-      const childrenIds = childrenIdSets[id]
+      const childrenIds = partSets[id]
 
       if (childrenIds?.size > 0) {
-        const children = allOrders.filter((order) => childrenIds.has(order.id))
+        const children = allPartOrders.filter((order) => childrenIds.has(order.id))
 
         const executedBuyAmount = sumChildrenAmount(children, 'executedBuyAmount').toString()
         const executedSellAmount = sumChildrenAmount(children, 'executedSellAmount').toString()
         const executedFeeAmount = sumChildrenAmount(children, 'executedFeeAmount').toString()
 
         acc[id] = { executedSellAmount, executedFeeAmount, executedBuyAmount }
-      }
-
-      if (!acc[id]) {
+      } else {
         acc[id] = DEFAULT_TWAP_EXECUTION_INFO
       }
 
       return acc
     }, {})
-  }, [ids, childrenIdSets, allOrders])
+  }, [ids, partSets, allPartOrders])
 }
 
 function sumChildrenAmount(children: Order[], key: keyof OrderInfoApi): BigInt {
