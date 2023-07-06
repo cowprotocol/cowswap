b683402e9e035458c2d9dcfa2e79a0ebb683a5f2
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-06 13:07:56 +0600

    fix: display relevant link in Order Submitted modal (#2788)

--- src/legacy/components/EnhancedTransactionLink/index.tsx
+++ src/legacy/components/EnhancedTransactionLink/index.tsx
@@ -1,12 +1,17 @@
-import { ExplorerLink } from 'legacy/components/ExplorerLink'
-import { EnhancedTransactionDetails, HashType } from 'legacy/state/enhancedTransactions/reducer'
+import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
-import { useGnosisSafeInfo, useWalletInfo } from 'modules/wallet'
+import { ExplorerLink } from 'legacy/components/ExplorerLink'
+import { HashType } from 'legacy/state/enhancedTransactions/reducer'
 
 import { SafeWalletLink } from 'common/pure/SafeWalletLink'
 
 interface Props {
-  tx: EnhancedTransactionDetails
+  chainId: SupportedChainId
+  tx: {
+    hash: string
+    hashType: HashType
+    safeTransaction?: { safe: string; safeTxHash: string }
+  }
 }
 
 /**
@@ -14,16 +19,12 @@ interface Props {
  * @param props
  */
 export function EnhancedTransactionLink(props: Props) {
-  const { tx } = props
-  const { chainId } = useWalletInfo()
-  const gnosisSafeInfo = useGnosisSafeInfo()
+  const { tx, chainId } = props
 
   if (tx.hashType === HashType.GNOSIS_SAFE_TX) {
     const safeTx = tx.safeTransaction
 
-    if (!chainId || !safeTx || !gnosisSafeInfo) {
-      return null
-    }
+    if (!safeTx) return null
 
     return <SafeWalletLink chainId={chainId} safeTransaction={safeTx} />
   } else {
