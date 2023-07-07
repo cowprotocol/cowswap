82d1c678b5463ff2976ec0e0589d67b622f2e48e
Leandro <alfetopito@users.noreply.github.com>
2023-07-07 06:46:03 -0700

    feat(twap): do not show est. exec. price for twap parent orders (#2804)
    * feat: do not show est. exec. price for twap parent orders
    
    * refactor: use getIsComposableCowParentOrder

--- src/modules/ordersTable/pure/OrdersTableContainer/OrderRow/index.tsx
+++ src/modules/ordersTable/pure/OrdersTableContainer/OrderRow/index.tsx
@@ -253,7 +253,7 @@ export function OrderRow({
       {isOpenOrdersTab && (
         <styledEl.PriceElement hasBackground onClick={toggleIsInverted}>
           {/*// TODO: gray out the price when it was updated too long ago*/}
-          {prices ? (
+          {prices && estimatedExecutionPrice ? (
             <styledEl.ExecuteCellWrapper>
               <EstimatedExecutionPrice
                 amount={executionPriceInverted}
@@ -268,7 +268,7 @@ export function OrderRow({
                 isUnfillable={isUnfillable}
               />
             </styledEl.ExecuteCellWrapper>
-          ) : prices === null || isOrderCreating ? (
+          ) : prices === null || !estimatedExecutionPrice || isOrderCreating ? (
             '-'
           ) : (
             <Loader size="14px" style={{ margin: '0 0 -2px 7px' }} />
