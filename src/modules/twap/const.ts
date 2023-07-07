0a1dc79a5877a87fcd1e51c69388f098f7fa00f3
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-07 20:04:12 +0600

    feat(twap): mock MINIMUM_PART_SELL_AMOUNT_FIAT to 5$

--- src/modules/twap/const.ts
+++ src/modules/twap/const.ts
@@ -39,7 +39,7 @@ export const TWAP_CANCELLED_STATUSES = [TwapOrderStatus.Cancelling, TwapOrderSta
 export const TWAP_FINAL_STATUSES = [TwapOrderStatus.Fulfilled, TwapOrderStatus.Expired, TwapOrderStatus.Cancelled]
 
 export const MINIMUM_PART_SELL_AMOUNT_FIAT: Record<SupportedChainId, CurrencyAmount<Currency>> = {
-  [SupportedChainId.MAINNET]: CurrencyAmount.fromRawAmount(USDC[SupportedChainId.MAINNET], 5_000e6), // 5k
+  [SupportedChainId.MAINNET]: CurrencyAmount.fromRawAmount(USDC[SupportedChainId.MAINNET], 5e6), // 5
   [SupportedChainId.GOERLI]: CurrencyAmount.fromRawAmount(USDC[SupportedChainId.GOERLI], 100e6), // 100
   [SupportedChainId.GNOSIS_CHAIN]: CurrencyAmount.fromRawAmount(USDC[SupportedChainId.GNOSIS_CHAIN], 5e6), // 5
 }
