ab0dcfdfa8730f03534b46d3721d0f4d8aa5c3c6
Alfetopito <alfetopito@users.noreply.github.com>
2023-07-07 17:08:44 +0100

    feat: add useNeedsZeroApproval

---
+++ src/common/hooks/useNeedsZeroApproval.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+import { Erc20 } from '@cowprotocol/abis'
+import { CurrencyAmount, Token } from '@uniswap/sdk-core'
+
+import { Nullish } from 'types'
+
+import { shouldZeroApprove as shouldZeroApproveFn } from './useShouldZeroApprove/shouldZeroApprove'
+
+export function useNeedsZeroApproval(
+  erc20Contract: Nullish<Erc20>,
+  spender: Nullish<string>,
+  sellAmount: Nullish<CurrencyAmount<Token>>
+): boolean {
+  const [shouldZeroApprove, setShouldZeroApprove] = useState(false)
+
+  useEffect(() => {
+    if (!erc20Contract || !spender || !sellAmount) return
+
+    shouldZeroApproveFn({
+      tokenContract: erc20Contract,
+      spender: spender,
+      amountToApprove: sellAmount,
+      isBundle: true,
+    }).then(setShouldZeroApprove)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [erc20Contract?.address, sellAmount?.quotient?.toString(), spender])
+
+  return shouldZeroApprove
+}
