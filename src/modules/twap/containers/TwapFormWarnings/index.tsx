e2297875e161cd8f68c3a83cabd4371c65c74dea
Leandro <alfetopito@users.noreply.github.com>
2023-07-10 07:48:18 -0700

    feat(twap): reset USDT allowance (#2819)
    * feat: add useNeedsZeroApproval
    
    * feat: add needsZeroApproval flag to TwapOrderCreationContext
    
    * feat: create zero approval tx if needed for TWAP
    
    * test: add unit test for zeroApproval tx
    
    * refactor: use `0` instead of CurrencyAmount instance
    
    * feat: show zero approval and bundle warnings on twap form
    
    * refactor: extract showFallbackHandlerWarning boolean
    
    * fix: do not show bundle & zero approval warnings on review modal

--- src/modules/twap/containers/TwapFormWarnings/index.tsx
+++ src/modules/twap/containers/TwapFormWarnings/index.tsx
@@ -7,6 +7,10 @@ import { modifySafeHandlerAnalytics } from 'legacy/components/analytics/events/t
 import { NoImpactWarning } from 'modules/trade/pure/NoImpactWarning'
 import { useIsSafeViaWc, useWalletInfo } from 'modules/wallet'
 
+import { useShouldZeroApprove } from 'common/hooks/useShouldZeroApprove'
+import { BundleTxApprovalBanner } from 'common/pure/InlineBanner/banners'
+import { ZeroApprovalWarning } from 'common/pure/ZeroApprovalWarning'
+
 import {
   FallbackHandlerWarning,
   SmallPartTimeWarning,
@@ -14,10 +18,15 @@ import {
   UnsupportedWalletWarning,
 } from './warnings'
 
+import { useAdvancedOrdersDerivedState } from '../../../advancedOrders'
+import { TradeFormValidation, useGetTradeFormValidation } from '../../../tradeFormValidation'
 import { useIsFallbackHandlerRequired } from '../../hooks/useFallbackHandlerVerification'
 import { useTwapWarningsContext } from '../../hooks/useTwapWarningsContext'
 import { TwapFormState } from '../../pure/PrimaryActionButton/getTwapFormState'
+import { twapOrderAtom } from '../../state/twapOrderAtom'
 import { twapOrdersSettingsAtom, updateTwapOrdersSettingsAtom } from '../../state/twapOrdersSettingsAtom'
+
+const BUNDLE_APPROVAL_STATES = [TradeFormValidation.ApproveAndSwap, TradeFormValidation.ExpertApproveAndSwap]
 
 interface TwapFormWarningsProps {
   localFormValidation: TwapFormState | null
@@ -27,6 +36,9 @@ interface TwapFormWarningsProps {
 export function TwapFormWarnings({ localFormValidation, isConfirmationModal }: TwapFormWarningsProps) {
   const { isFallbackHandlerSetupAccepted, isPriceImpactAccepted } = useAtomValue(twapOrdersSettingsAtom)
   const updateTwapOrdersSettings = useUpdateAtom(updateTwapOrdersSettingsAtom)
+  const twapOrder = useAtomValue(twapOrderAtom)
+  const { outputCurrencyAmount } = useAdvancedOrdersDerivedState()
+  const primaryFormValidation = useGetTradeFormValidation()
 
   const { chainId } = useWalletInfo()
   const isFallbackHandlerRequired = useIsFallbackHandlerRequired()
@@ -41,6 +53,12 @@ export function TwapFormWarnings({ localFormValidation, isConfirmationModal }: T
     [updateTwapOrdersSettings]
   )
 
+  const shouldZeroApprove = useShouldZeroApprove(twapOrder?.sellAmount)
+  const showZeroApprovalWarning = !isConfirmationModal && shouldZeroApprove && outputCurrencyAmount !== null
+  const showApprovalBundlingBanner =
+    !isConfirmationModal && primaryFormValidation && BUNDLE_APPROVAL_STATES.includes(primaryFormValidation)
+  const showFallbackHandlerWarning = !isConfirmationModal && canTrade && isFallbackHandlerRequired
+
   const setIsPriceImpactAccepted = useCallback(() => {
     updateTwapOrdersSettings({ isPriceImpactAccepted: !isPriceImpactAccepted })
   }, [updateTwapOrdersSettings, isPriceImpactAccepted])
@@ -50,6 +68,9 @@ export function TwapFormWarnings({ localFormValidation, isConfirmationModal }: T
 
   return (
     <>
+      {showZeroApprovalWarning && <ZeroApprovalWarning currency={twapOrder?.sellAmount?.currency} />}
+      {showApprovalBundlingBanner && <BundleTxApprovalBanner />}
+
       {!isConfirmationModal && showPriceImpactWarning && (
         <NoImpactWarning
           withoutAccepting={false}
@@ -71,7 +92,7 @@ export function TwapFormWarnings({ localFormValidation, isConfirmationModal }: T
           return <SmallPartTimeWarning />
         }
 
-        if (!isConfirmationModal && canTrade && isFallbackHandlerRequired) {
+        if (showFallbackHandlerWarning) {
           return (
             <FallbackHandlerWarning
               isFallbackHandlerSetupAccepted={isFallbackHandlerSetupAccepted}
