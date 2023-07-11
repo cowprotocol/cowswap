1dbb1f94fdb63f05bb8783da29a7b7cdd2e403c9
Leandro <alfetopito@users.noreply.github.com>
2023-07-11 01:52:51 -0700

    feat(hackathon-surplus): add tooltip for activity modal surplus (#2831)
    * feat(hackathon-surplus): add tooltip for activity modal surplus
    
    * feat(hackathon-surplus): do not show 0 surplus (#2833)

--- src/modules/account/containers/AccountDetails/SurplusCard.tsx
+++ src/modules/account/containers/AccountDetails/SurplusCard.tsx
@@ -8,13 +8,16 @@ import { useWalletInfo } from 'modules/wallet'
 import { FiatAmount } from 'common/pure/FiatAmount'
 import { TokenAmount } from 'common/pure/TokenAmount'
 import { useTotalSurplus } from 'common/state/totalSurplusState'
+import useNativeCurrency from 'lib/hooks/useNativeCurrency'
 
 import { InfoCard, SurplusCardWrapper } from './styled'
 
 export function SurplusCard() {
   const { surplusAmount, isLoading } = useTotalSurplus()
 
-  const surplusUsdAmount = useHigherUSDValue(surplusAmount)
+  const showSurplusAmount = surplusAmount && surplusAmount.greaterThan(0)
+  const surplusUsdAmount = useHigherUSDValue(showSurplusAmount ? surplusAmount : undefined)
+  const nativeSymbol = useNativeCurrency()?.symbol || 'ETH'
 
   // TODO: Remove these 2 lines once merged in DEVELOP (this change was cherry-picked from it, and it still needs these two lines because it doesn't have sasha refactor for supportedChainId)
   const { chainId } = useWalletInfo()
@@ -27,25 +30,26 @@ export function SurplusCard() {
         <div>
           <span>
             <i>
-              Your total surplus <QuestionHelper text={'TODO: insert tooltip'} />
+              Your total surplus{' '}
+              <QuestionHelper
+                text={`The total surplus CoW Swap has generated for you in ${nativeSymbol} across all your trades since March 2023`}
+              />
             </i>
           </span>
           <span>
             {isLoading ? (
               <p>Loading...</p>
+            ) : showSurplusAmount ? (
+              <b>
+                +<TokenAmount amount={surplusAmount} tokenSymbol={surplusAmount?.currency} />
+              </b>
             ) : (
-              surplusAmount && (
-                <b>
-                  +<TokenAmount amount={surplusAmount} tokenSymbol={surplusAmount?.currency} />
-                </b>
-              )
+              <p>No surplus for the given time period</p>
             )}
-            {!surplusAmount && <p>No surplus for the given time period</p>}
           </span>
           <small>{surplusUsdAmount && <FiatAmount amount={surplusUsdAmount} accurate={false} />}</small>
         </div>
         <div>
-          {/* TODO: add correct link */}
           <ExternalLink href={'https://blog.cow.fi/announcing-cow-swap-surplus-notifications-f679c77702ea'}>
             Learn about surplus on CoW Swap ↗
           </ExternalLink>
