dd484cc18ffa79a9dd97e5a18f6eec5682e10dee
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-20 16:57:45 +0600

    chore: fix getPartOrderStatus

--- src/modules/twap/utils/getPartOrderStatus.ts
+++ src/modules/twap/utils/getPartOrderStatus.ts
@@ -13,11 +13,12 @@ export function getPartOrderStatus(
   if (parent.status === TwapOrderStatus.Fulfilled) return OrderStatus.FULFILLED
   if (isOrderFulfilled(enrichedOrder)) return OrderStatus.FULFILLED
 
-  if (parent.status === TwapOrderStatus.Cancelled) return OrderStatus.CANCELLED
   if (isOrderCancelled(enrichedOrder) || enrichedOrder.status === SdkOrderStatus.CANCELLED) return OrderStatus.CANCELLED
 
-  if (parent.status === TwapOrderStatus.Expired) return OrderStatus.EXPIRED
   if (isOrderExpired(enrichedOrder)) return OrderStatus.EXPIRED
+
+  if (parent.status === TwapOrderStatus.Cancelled) return OrderStatus.CANCELLED
+  if (parent.status === TwapOrderStatus.Expired) return OrderStatus.EXPIRED
 
   if (isVirtualPart) return OrderStatus.SCHEDULED
 
