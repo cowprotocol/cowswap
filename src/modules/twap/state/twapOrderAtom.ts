e91b1e114ffdead0ddb5929ba52338f28d091c59
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-24 14:39:50 +0600

    feat(twap): small price protection warning (#2920)

--- src/modules/twap/state/twapOrderAtom.ts
+++ src/modules/twap/state/twapOrderAtom.ts
@@ -12,9 +12,15 @@ import { twapOrderSlippageAtom, twapOrdersSettingsAtom } from './twapOrdersSetti
 import { TWAPOrder } from '../types'
 import { customDeadlineToSeconds } from '../utils/deadlinePartsDisplay'
 
+export const twapDeadlineAtom = atom<number>((get) => {
+  const { isCustomDeadline, customDeadline, deadline } = get(twapOrdersSettingsAtom)
+
+  return isCustomDeadline ? customDeadlineToSeconds(customDeadline) : deadline / 1000
+})
+
 export const twapTimeIntervalAtom = atom<number>((get) => {
-  const { numberOfPartsValue, isCustomDeadline, customDeadline, deadline } = get(twapOrdersSettingsAtom)
-  const seconds = isCustomDeadline ? customDeadlineToSeconds(customDeadline) : deadline / 1000
+  const { numberOfPartsValue } = get(twapOrdersSettingsAtom)
+  const seconds = get(twapDeadlineAtom)
 
   return Math.ceil(seconds / numberOfPartsValue)
 })
