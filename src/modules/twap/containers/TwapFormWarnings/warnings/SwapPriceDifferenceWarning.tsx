fbb5e58f219741ddbe10961f781b8323bb1c1a25
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-26 13:38:30 +0600

    chore: styles for SwapPriceDifferenceWarning

--- src/modules/twap/containers/TwapFormWarnings/warnings/SwapPriceDifferenceWarning.tsx
+++ src/modules/twap/containers/TwapFormWarnings/warnings/SwapPriceDifferenceWarning.tsx
@@ -1,19 +1,39 @@
 import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 
+import styled from 'styled-components/macro'
+
 import { InlineBanner } from 'common/pure/InlineBanner'
 import { TokenAmount } from 'common/pure/TokenAmount'
+
+import { InfoIcon } from '../../../../../legacy/components/InfoIcon'
 
 export type SwapPriceDifferenceWarningProps = {
   amount: CurrencyAmount<Currency>
 }
 
+const Wrapper = styled.p`
+  display: flex;
+  gap: 12px;
+  text-align: left !important;
+`
+
+const EmojiBox = styled.span`
+  font-size: 18px;
+`
+
 export function SwapPriceDifferenceWarning({ amount }: SwapPriceDifferenceWarningProps) {
   return (
-    <InlineBanner type="success">
-      <p>
-        You are expected to receive <TokenAmount amount={amount} tokenSymbol={amount.currency} /> more compared to the
-        same SWAP order
-      </p>
+    <InlineBanner type="success" hideIcon={true}>
+      <Wrapper>
+        <EmojiBox>🤑</EmojiBox>
+        <div>
+          You are expected to receive <TokenAmount amount={amount} tokenSymbol={amount.currency} /> more compared to the
+          same SWAP order
+        </div>
+        <div>
+          <InfoIcon content="TODO: set the tooltip content"></InfoIcon>
+        </div>
+      </Wrapper>
     </InlineBanner>
   )
 }
