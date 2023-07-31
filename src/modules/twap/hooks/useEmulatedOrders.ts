bb65e9f02e8da88a0bda40e37a162d36f2f54138
Alexandr Kazachenko <shoom3301@gmail.com>
2023-07-31 17:03:31 +0600

    fix: don't display any TWAP orders when is not Safe app

--- src/modules/twap/hooks/useEmulatedOrders.ts
+++ src/modules/twap/hooks/useEmulatedOrders.ts
@@ -6,7 +6,7 @@ import { OrderClass } from '@cowprotocol/cow-sdk'
 import { Order } from 'legacy/state/orders/actions'
 import { useOrders } from 'legacy/state/orders/hooks'
 
-import { useWalletInfo } from 'modules/wallet'
+import { useIsSafeApp, useWalletInfo } from 'modules/wallet'
 
 import { emulatedPartOrdersAtom } from '../state/emulatedPartOrdersAtom'
 import { emulatedTwapOrdersAtom } from '../state/emulatedTwapOrdersAtom'
@@ -15,6 +15,7 @@ export function useEmulatedOrders(): Order[] {
   const { chainId, account } = useWalletInfo()
   const emulatedTwapOrders = useAtomValue(emulatedTwapOrdersAtom)
   const emulatedPartOrders = useAtomValue(emulatedPartOrdersAtom)
+  const isSafeApp = useIsSafeApp()
 
   const limitOrders = useOrders(chainId, account, OrderClass.LIMIT)
   const discreteTwapOrders = useMemo(() => {
@@ -22,8 +23,10 @@ export function useEmulatedOrders(): Order[] {
   }, [limitOrders])
 
   const allEmulatedOrders = useMemo(() => {
+    if (!isSafeApp) return []
+
     return emulatedTwapOrders.concat(emulatedPartOrders).concat(discreteTwapOrders)
-  }, [emulatedTwapOrders, emulatedPartOrders, discreteTwapOrders])
+  }, [emulatedTwapOrders, emulatedPartOrders, discreteTwapOrders, isSafeApp])
 
   return allEmulatedOrders
 }
