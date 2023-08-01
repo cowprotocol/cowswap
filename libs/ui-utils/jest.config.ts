4948652402c053be9613015615537bc9bd6ce19a
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-01 09:28:19 +0600

    chore: fix jest preset

--- libs/ui-utils/jest.config.ts
+++ libs/ui-utils/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'ui-utils',
-  preset: '../../../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
--- libs/widget-lib/jest.config.ts
+++ libs/widget-lib/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'widget-lib',
-  preset: '../../../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
