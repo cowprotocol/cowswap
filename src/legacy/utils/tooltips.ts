e519a8f95ebb8f19e0624b6157458e0c9aef5dde
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-02 21:17:39 +0600

    fix(swap): display correct value of slippage in min. price tooltip (#2982)

--- src/legacy/utils/tooltips.ts
+++ src/legacy/utils/tooltips.ts
@@ -1,16 +1,11 @@
 import { Percent } from '@uniswap/sdk-core'
 
-import { PERCENTAGE_PRECISION } from 'legacy/constants'
-
-import { formatAmountWithPrecision } from 'utils/amountFormat'
+import { formatPercent } from 'utils/amountFormat'
 
 export function getMinimumReceivedTooltip(allowedSlippage: Percent, isExactIn: boolean): string {
-  const slippagePercent = allowedSlippage
-
   return `${
     isExactIn ? "Minimum tokens you'll receive." : "Maximum tokens you'll sell."
-  } This accounts for the current price and your chosen slippage (${formatAmountWithPrecision(
-    slippagePercent,
-    PERCENTAGE_PRECISION
+  } This accounts for the current price and your chosen slippage (${formatPercent(
+    allowedSlippage
   )}%). If the price moves beyond your slippage, you won't trade but also you won't pay any fees or gas.`
 }
