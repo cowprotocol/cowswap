105705b566c21535b33204679dd3746dfe1dde67
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-03 14:22:46 +0600

    fix(twap): don't redirect to swap when feature-flag value is not defined (#2981)

--- src/modules/mainMenu/hooks.ts
+++ src/modules/mainMenu/hooks.ts
@@ -6,7 +6,7 @@ import { MenuTreeItem } from './types'
 import { buildMainMenuTreeItems } from './utils'
 
 export function useMenuItems(): MenuTreeItem[] {
-  const isAdvancedOrdersEnabled = useIsAdvancedOrdersEnabled()
+  const isAdvancedOrdersEnabled = !!useIsAdvancedOrdersEnabled()
 
   return useMemo(() => buildMainMenuTreeItems({ isAdvancedOrdersEnabled }), [isAdvancedOrdersEnabled])
 }
