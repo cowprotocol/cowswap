561d972c0e8784b84312fcb72a458f748f4aa33f
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-04 17:01:47 +0600

    chore: merge develop

--- apps/cowswap-frontend/src/common/pure/CurrencyAmountPreview/index.tsx
+++ apps/cowswap-frontend/src/common/pure/CurrencyAmountPreview/index.tsx
@@ -35,38 +35,42 @@ export function CurrencyAmountPreview(props: CurrencyPreviewProps) {
   const currency = amount?.currency
 
   return (
-    <styledEl.Wrapper
-      id={id}
-      className={className}
-      withReceiveAmountInfo={false}
-      disabled={false}
-      inputDisabled={false}
-    >
-      {topLabel && <styledEl.CurrencyTopLabel>{topLabel}</styledEl.CurrencyTopLabel>}
+    <>
+      <styledEl.Wrapper
+        id={id}
+        className={className}
+        withReceiveAmountInfo={false}
+        pointerDisabled={false}
+        readOnly={false}
+      >
+        {topLabel && <styledEl.CurrencyTopLabel>{topLabel}</styledEl.CurrencyTopLabel>}
 
-      <styledEl.CurrencyInputBox>
-        <div>
-          <CurrencySelectButton currency={currency} loading={false} readonlyMode={true} />
-        </div>
-        <div>
-          <styledEl.TokenAmountStyled className="token-amount-input" amount={amount} />
-        </div>
-      </styledEl.CurrencyInputBox>
+        <styledEl.CurrencyInputBox>
+          <div>
+            <CurrencySelectButton currency={currency} loading={false} readonlyMode={true} />
+          </div>
+          <div>
+            <styledEl.TokenAmountStyled className="token-amount-input" amount={amount} />
+          </div>
+        </styledEl.CurrencyInputBox>
 
-      <styledEl.CurrencyInputBox>
-        <div>
-          {balance && (
-            <styledEl.BalanceText>
-              <Trans>Balance</Trans>: <TokenAmount amount={balance} defaultValue="0" tokenSymbol={currency} />
-            </styledEl.BalanceText>
-          )}
-        </div>
-        <div>
-          <styledEl.FiatAmountText>
-            <FiatValue fiatValue={fiatAmount} priceImpactParams={priceImpactParams} />
-          </styledEl.FiatAmountText>
-        </div>
-      </styledEl.CurrencyInputBox>
-    </styledEl.Wrapper>
+        <styledEl.CurrencyInputBox>
+          <div>
+            {balance && (
+              <>
+                <styledEl.BalanceText>
+                  <Trans>Balance</Trans>: <TokenAmount amount={balance} defaultValue="0" tokenSymbol={currency} />
+                </styledEl.BalanceText>
+              </>
+            )}
+          </div>
+          <div>
+            <styledEl.FiatAmountText>
+              <FiatValue fiatValue={fiatAmount} priceImpactParams={priceImpactParams} />
+            </styledEl.FiatAmountText>
+          </div>
+        </styledEl.CurrencyInputBox>
+      </styledEl.Wrapper>
+    </>
   )
 }
---
+++ apps/cowswap-frontend/src/modules/appData/updater/UploadToIpfsUpdater.ts
@@ -0,0 +1,106 @@
+import { useAtomValue, useSetAtom } from 'jotai'
+import { useEffect, useRef } from 'react'
+
+import ms from 'ms.macro'
+
+import { uploadAppDataDocOrderbookApi } from '../services'
+import {
+  appDataUploadQueueAtom,
+  removeAppDataFromUploadQueueAtom,
+  updateAppDataOnUploadQueueAtom,
+} from '../state/atoms'
+import { AppDataKeyParams, AppDataRecord, UpdateAppDataOnUploadQueueParams } from '../types'
+
+const UPLOAD_CHECK_INTERVAL = ms`1 minute`
+const BASE_FOR_EXPONENTIAL_BACKOFF = 2 // in seconds, converted to milliseconds later
+const ONE_SECOND = ms`1s`
+const MAX_TIME_TO_WAIT = ms`5 minutes`
+
+export function UploadToIpfsUpdater(): null {
+  const toUpload = useAtomValue(appDataUploadQueueAtom)
+  const removePending = useSetAtom(removeAppDataFromUploadQueueAtom)
+  const updatePending = useSetAtom(updateAppDataOnUploadQueueAtom)
+
+  // Storing a reference to avoid re-render on every update
+  const refToUpload = useRef(toUpload)
+  refToUpload.current = toUpload
+
+  // Filtering only newly created and not yet attempted to upload docs
+  const newlyAdded = toUpload.filter(({ uploading, lastAttempt }) => !uploading && !lastAttempt)
+
+  useEffect(() => {
+    // Try to upload new docs as soon as they are added
+    newlyAdded.forEach((appDataRecord) => _uploadToIpfs(appDataRecord, updatePending, removePending))
+  }, [newlyAdded, removePending, updatePending])
+
+  useEffect(() => {
+    async function uploadPendingAppData() {
+      console.debug(`[UploadToIpfsUpdater] Iterating over ${refToUpload.current.length} appData on upload queue`)
+      refToUpload.current.forEach((appDataRecord) => _uploadToIpfs(appDataRecord, updatePending, removePending))
+    }
+
+    const intervalId = setInterval(uploadPendingAppData, UPLOAD_CHECK_INTERVAL)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [removePending, updatePending])
+
+  return null
+}
+
+async function _uploadToIpfs(
+  appDataRecord: AppDataRecord,
+  updatePending: (params: UpdateAppDataOnUploadQueueParams) => void,
+  removePending: (params: AppDataKeyParams) => void
+) {
+  const { fullAppData, appDataKeccak256, chainId, orderId, uploading, failedAttempts, lastAttempt } = appDataRecord
+
+  if (!fullAppData || !appDataKeccak256) {
+    // No actual doc to upload, nothing to do here
+    removePending({ chainId, orderId })
+  } else if (_canUpload(uploading, failedAttempts, lastAttempt)) {
+    await _actuallyUploadToIpfs(appDataRecord, updatePending, removePending)
+  } else {
+    console.log(`[UploadToIpfsUpdater] Criteria not met, skipping ${chainId}-${orderId}`)
+  }
+}
+
+function _canUpload(uploading: boolean, attempts: number, lastAttempt?: number): boolean {
+  if (uploading) {
+    return false
+  }
+
+  if (lastAttempt) {
+    // Every attempt takes BASE_FOR_EXPONENTIAL_BACKOFF ˆ failedAttempts
+    const timeToWait = BASE_FOR_EXPONENTIAL_BACKOFF ** attempts * ONE_SECOND
+    // Don't wait more than MAX_TIME_TO_WAIT.
+    // Both are in milliseconds
+    const timeDelta = Math.min(timeToWait, MAX_TIME_TO_WAIT)
+
+    return lastAttempt + timeDelta <= Date.now()
+  }
+
+  return true
+}
+
+async function _actuallyUploadToIpfs(
+  appDataRecord: AppDataRecord,
+  updatePending: (params: UpdateAppDataOnUploadQueueParams) => void,
+  removePending: (params: AppDataKeyParams) => void
+) {
+  const { fullAppData, appDataKeccak256, chainId, orderId, failedAttempts, env } = appDataRecord
+
+  if (!fullAppData || !appDataKeccak256) return
+
+  // Update state to prevent it to be uploaded by another process in the meantime
+  updatePending({ chainId, orderId, uploading: true })
+
+  try {
+    await uploadAppDataDocOrderbookApi({ appDataKeccak256, fullAppData, chainId, env })
+    removePending({ chainId, orderId })
+  } catch (e: any) {
+    console.error(`[UploadToIpfsUpdater] Failed to upload doc, will try again. Reason: ${e.message}`, e, fullAppData)
+    updatePending({ chainId, orderId, uploading: false, failedAttempts: failedAttempts + 1, lastAttempt: Date.now() })
+  }
+}
