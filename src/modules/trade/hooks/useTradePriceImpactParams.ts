5d511d7194578cea61b32fb131786c064b0dc5f7
Alfetopito <alfetopito@users.noreply.github.com>
2023-08-04 15:38:30 +0100

    chore: use useSafeMemo on useTradePriceImpactParams

--- src/modules/trade/hooks/useTradePriceImpactParams.ts
+++ src/modules/trade/hooks/useTradePriceImpactParams.ts
@@ -1,6 +1,6 @@
-import { useMemo } from 'react'
-
 import type { PriceImpactParams } from 'legacy/hooks/usePriceImpact'
+
+import { useSafeMemo } from 'common/hooks/useSafeMemo'
 
 import { useDerivedTradeState } from './useDerivedTradeState'
 import { useIsWrapOrUnwrap } from './useIsWrapOrUnwrap'
@@ -15,7 +15,7 @@ export function useTradePriceImpactParams(): PriceImpactParams {
   const isWrapOrUnwrap = useIsWrapOrUnwrap()
   const { inputCurrencyAmount, outputCurrencyAmount } = state || emptyState
 
-  return useMemo(() => {
+  return useSafeMemo(() => {
     const abTrade = {
       inputAmount: inputCurrencyAmount,
       outputAmount: outputCurrencyAmount,
@@ -34,6 +34,5 @@ export function useTradePriceImpactParams(): PriceImpactParams {
       parsedAmounts,
       isWrapping: isWrapOrUnwrap,
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [inputCurrencyAmount?.quotient.toString(), outputCurrencyAmount?.quotient.toString(), isWrapOrUnwrap])
+  }, [inputCurrencyAmount, outputCurrencyAmount, isWrapOrUnwrap])
 }
