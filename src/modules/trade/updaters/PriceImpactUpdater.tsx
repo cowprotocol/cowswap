c57e0bb74494c81d5d7d4880cdaea0d9ec892c55
Alfetopito <alfetopito@users.noreply.github.com>
2023-08-04 15:40:34 +0100

    feat: use useSafeEffect on PriceImpactUpdater

--- src/modules/trade/updaters/PriceImpactUpdater.tsx
+++ src/modules/trade/updaters/PriceImpactUpdater.tsx
@@ -1,7 +1,8 @@
 import { useSetAtom } from 'jotai'
-import { useEffect } from 'react'
 
 import { usePriceImpact } from 'legacy/hooks/usePriceImpact'
+
+import { useSafeEffect } from 'common/hooks/useSafeMemo'
 
 import { useTradePriceImpactParams } from '../hooks/useTradePriceImpactParams'
 import { priceImpactAtom } from '../state/priceImpactAtom'
@@ -9,13 +10,11 @@ import { priceImpactAtom } from '../state/priceImpactAtom'
 export function PriceImpactUpdater() {
   const updatePriceImpact = useSetAtom(priceImpactAtom)
   const params = useTradePriceImpactParams()
-  const priceImpact = usePriceImpact(params)
-  const priceImpactValue = priceImpact.priceImpact?.quotient.toString()
+  const { error, loading, priceImpact } = usePriceImpact(params)
 
-  useEffect(() => {
-    updatePriceImpact(priceImpact)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [priceImpactValue, priceImpact.loading, priceImpact.error, updatePriceImpact])
+  useSafeEffect(() => {
+    updatePriceImpact({ error, loading, priceImpact })
+  }, [error, loading, updatePriceImpact, priceImpact])
 
   return null
 }
