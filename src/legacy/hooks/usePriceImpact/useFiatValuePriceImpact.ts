3cbc27a3bb9cdbbf3cdfad2f84b40da745aeeeaf
Alfetopito <alfetopito@users.noreply.github.com>
2023-08-07 11:34:08 +0100

    chore: do not calculate fiat price impact unless both values are present

--- src/legacy/hooks/usePriceImpact/useFiatValuePriceImpact.ts
+++ src/legacy/hooks/usePriceImpact/useFiatValuePriceImpact.ts
@@ -1,13 +1,19 @@
 import { useHigherUSDValue } from 'legacy/hooks/useStablecoinPrice'
-import { Field } from 'legacy/state/swap/actions'
 import { computeFiatValuePriceImpact } from 'legacy/utils/computeFiatValuePriceImpact'
 
 import { ParsedAmounts } from './types'
 
-export default function useFiatValuePriceImpact(parsedAmounts: ParsedAmounts) {
-  const { value: fiatValueInput, isLoading: inputIsLoading } = useHigherUSDValue(parsedAmounts[Field.INPUT])
-  const { value: fiatValueOutput, isLoading: outputIsLoading } = useHigherUSDValue(parsedAmounts[Field.OUTPUT])
+export default function useFiatValuePriceImpact({ INPUT, OUTPUT }: ParsedAmounts) {
+  const areBothValuesPresent = !!INPUT && !!OUTPUT
+  // prevent querying any fiat estimation unless both values are filled in
+  const input = areBothValuesPresent ? INPUT : undefined
+  const output = areBothValuesPresent ? OUTPUT : undefined
+
+  const { value: fiatValueInput, isLoading: inputIsLoading } = useHigherUSDValue(input)
+  const { value: fiatValueOutput, isLoading: outputIsLoading } = useHigherUSDValue(output)
   const priceImpact = computeFiatValuePriceImpact(fiatValueInput, fiatValueOutput)
 
-  return { priceImpact, isLoading: inputIsLoading || outputIsLoading }
+  const isLoading = areBothValuesPresent && (inputIsLoading || outputIsLoading)
+
+  return { priceImpact, isLoading }
 }
