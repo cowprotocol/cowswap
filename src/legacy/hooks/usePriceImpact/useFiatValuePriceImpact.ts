77cec32b8e85920a4c8a354b5b376a458a230248
Alfetopito <alfetopito@users.noreply.github.com>
2023-08-08 11:07:16 +0100

    fix: only calculate price impact when both prices finish loading

--- src/legacy/hooks/usePriceImpact/useFiatValuePriceImpact.ts
+++ src/legacy/hooks/usePriceImpact/useFiatValuePriceImpact.ts
@@ -11,7 +11,11 @@ export default function useFiatValuePriceImpact({ INPUT, OUTPUT }: ParsedAmounts
 
   const { value: fiatValueInput, isLoading: inputIsLoading } = useHigherUSDValue(input)
   const { value: fiatValueOutput, isLoading: outputIsLoading } = useHigherUSDValue(output)
-  const priceImpact = computeFiatValuePriceImpact(fiatValueInput, fiatValueOutput)
+
+  // Only compute price impact after BOTH finished loading
+  // This prevents the impact look like it's ready but still loading
+  const priceImpact =
+    !inputIsLoading && !outputIsLoading && computeFiatValuePriceImpact(fiatValueInput, fiatValueOutput)
 
   const isLoading = areBothValuesPresent && (inputIsLoading || outputIsLoading)
 
