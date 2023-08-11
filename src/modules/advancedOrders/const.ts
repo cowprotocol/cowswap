6306bd26ee652be0dde6bb0bf2b9ed1f0ba8ff06
Leandro <alfetopito@users.noreply.github.com>
2023-08-11 02:48:09 -0700

    feat(twap): update contract addresses (#3031)
    * feat(twap): update contract addresses
    
    * test: update unittests with new addresses
    
    * fix: fix update TWAP contract to the right one
    
    ---------
    
    Co-authored-by: Anxo Rodriguez <anxolin@gmail.com>

--- src/modules/advancedOrders/const.ts
+++ src/modules/advancedOrders/const.ts
@@ -1,20 +1,20 @@
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
-const composableCowAddress = '0xF487887DA5a4b4e3eC114FDAd97dc0F785d72738'
+const composableCowAddress = '0xfdaFc9d1902f4e0b84f65F49f244b32b31013b74'
 export const COMPOSABLE_COW_ADDRESS: Record<SupportedChainId, string> = {
   1: composableCowAddress,
   100: composableCowAddress,
   5: composableCowAddress,
 }
 
-const extensibleHandlerAddress = '0x4e305935b14627eA57CBDbCfF57e81fd9F240403'
+const extensibleHandlerAddress = '0x2f55e8b20D0B9FEFA187AA7d00B6Cbe563605bF5'
 export const SAFE_EXTENSIBLE_HANDLER_ADDRESS: Record<SupportedChainId, string> = {
   1: extensibleHandlerAddress,
   100: extensibleHandlerAddress,
   5: extensibleHandlerAddress,
 }
 
-const currentBlockFactoryAddress = '0x0899c7DC280363d263Cc954506134F249CceC4a5'
+const currentBlockFactoryAddress = '0x52eD56Da04309Aca4c3FECC595298d80C2f16BAc'
 export const CURRENT_BLOCK_FACTORY_ADDRESS: Record<SupportedChainId, string> = {
   1: currentBlockFactoryAddress,
   100: currentBlockFactoryAddress,
