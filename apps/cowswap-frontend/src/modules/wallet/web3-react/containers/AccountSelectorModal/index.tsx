98683624424a38bbabe19bfb2ee8495209306703
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-17 13:47:34 +0600

    refactor: refactor AccountSelectorModal

--- apps/cowswap-frontend/src/modules/wallet/web3-react/containers/AccountSelectorModal/index.tsx
+++ apps/cowswap-frontend/src/modules/wallet/web3-react/containers/AccountSelectorModal/index.tsx
@@ -1,74 +1,19 @@
-import { atom, useAtomValue, useSetAtom, useAtom } from 'jotai'
+import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 
 import { useWeb3React } from '@web3-react/core'
-
-import styled from 'styled-components/macro'
-
-import { ReactComponent as Close } from 'legacy/assets/images/x.svg'
 
 import { useNativeCurrencyBalances } from 'modules/tokens/hooks/useCurrencyBalance'
 
 import { CowModal } from 'common/pure/Modal'
 
+import { accountsLoaders } from './accountsLoaders'
+import { accountSelectorModalAtom, toggleAccountSelectorModalAtom } from './state'
+import * as styledEl from './styled'
+
 import { hwAccountIndexAtom } from '../../../api/state'
-import { ConnectionType } from '../../../api/types'
 import { getWeb3ReactConnection, HardWareWallet } from '../../connection'
-import { trezorConnection } from '../../connection/trezor'
 import { AccountIndexSelect } from '../../pure/AccountIndexSelect'
-
-const CloseIcon = styled(Close)`
-  opacity: 0.6;
-  transition: opacity 0.3s ease-in-out;
-  stroke: ${({ theme }) => theme.text1};
-  width: 24px;
-  height: 24px;
-  cursor: pointer;
-
-  &:hover {
-    opacity: 1;
-  }
-`
-
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
-
-  h3 {
-    margin: 0;
-  }
-`
-
-const Wrapper = styled.div`
-  padding: 20px;
-`
-
-export interface AccountSelectorModalState {
-  isOpen: boolean
-}
-
-export const accountSelectorModalAtom = atom<AccountSelectorModalState>({ isOpen: false })
-
-export const toggleAccountSelectorModalAtom = atom(null, (get, set) => {
-  set(accountSelectorModalAtom, { isOpen: !get(accountSelectorModalAtom).isOpen })
-})
-
-interface WalletAccountsLoader {
-  getAccounts(): string[] | null
-  loadMoreAccounts(): Promise<void>
-}
-
-const accountsLoaders: Record<HardWareWallet, WalletAccountsLoader> = {
-  [ConnectionType.TREZOR]: {
-    getAccounts() {
-      return trezorConnection.connector.getAccounts()
-    },
-    loadMoreAccounts() {
-      return trezorConnection.connector.loadMoreAccounts()
-    },
-  },
-}
 
 export function AccountSelectorModal() {
   const { isOpen } = useAtomValue(accountSelectorModalAtom)
@@ -101,11 +46,11 @@ export function AccountSelectorModal() {
 
   return (
     <CowModal maxWidth={600} isOpen={isOpen} onDismiss={closeModal} minHeight={false} maxHeight={90}>
-      <Wrapper>
-        <Header>
+      <styledEl.Wrapper>
+        <styledEl.Header>
           <h3>Select account</h3>
-          <CloseIcon onClick={closeModal} />
-        </Header>
+          <styledEl.CloseIcon onClick={closeModal} />
+        </styledEl.Header>
         <AccountIndexSelect
           balances={balances}
           currentIndex={hwAccountIndex}
@@ -113,7 +58,7 @@ export function AccountSelectorModal() {
           accountsList={accountsList}
           loadMoreAccounts={loadMoreAccounts}
         />
-      </Wrapper>
+      </styledEl.Wrapper>
     </CowModal>
   )
 }
---
+++ apps/cowswap-frontend/src/modules/wallet/web3-react/containers/AccountSelectorModal/styled.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components/macro'
+
+import { ReactComponent as Close } from 'legacy/assets/images/x.svg'
+
+export const CloseIcon = styled(Close)`
+  opacity: 0.6;
+  transition: opacity 0.3s ease-in-out;
+  stroke: ${({ theme }) => theme.text1};
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1;
+  }
+`
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+
+  h3 {
+    margin: 0;
+  }
+`
+
+export const Wrapper = styled.div`
+  padding: 20px;
+`
