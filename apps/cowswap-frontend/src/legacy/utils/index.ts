8365242936104e23bba14d970ff6256c0ea55544
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-23 19:46:22 +0600

    refactor: remove unused uniswap code (#3076)

--- apps/cowswap-frontend/src/legacy/utils/index.ts
+++ apps/cowswap-frontend/src/legacy/utils/index.ts
@@ -3,15 +3,9 @@ import { getAddress } from '@ethersproject/address'
 import { AddressZero } from '@ethersproject/constants'
 import { Contract } from '@ethersproject/contracts'
 import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers'
-import { Percent, Token } from '@uniswap/sdk-core'
-import { FeeAmount } from '@uniswap/v3-sdk'
-
-import JSBI from 'jsbi'
 
 import { ORDER_ID_SHORT_LENGTH } from 'legacy/constants'
 import { getExplorerOrderLink } from 'legacy/utils/explorer'
-
-import { ChainTokenMap } from 'lib/hooks/useTokenList/utils'
 
 // returns the checksummed address if the address is valid, otherwise returns false
 export function isAddress(value: any): string | false {
@@ -52,14 +46,6 @@ export function getContract(address: string, ABI: any, provider: JsonRpcProvider
 
 export function escapeRegExp(string: string): string {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
-}
-
-export function isTokenOnList(chainTokenMap: ChainTokenMap, token?: Token): boolean {
-  return Boolean(token?.isToken && chainTokenMap[token.chainId]?.[token.address])
-}
-
-export function formattedFeeAmount(feeAmount: FeeAmount): number {
-  return feeAmount / 10000
 }
 
 const COW_ORDER_ID_LENGTH = 114 // 112 (56 bytes in hex) + 2 (it's prefixed with "0x")
@@ -148,13 +134,4 @@ export function formatOrderId(orderId: string): string {
 
   // 0x is at index 0 of orderId, shorten. Else return id as is
   return has0x?.index === 0 ? shortenOrderId(orderId, 2, orderId.length) : orderId
-}
-
-// converts a basis points value to a sdk percent
-export function basisPointsToPercent(num: number): Percent {
-  return new Percent(JSBI.BigInt(num), JSBI.BigInt(10000))
-}
-
-export function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
-  return value !== null && value !== undefined
 }
