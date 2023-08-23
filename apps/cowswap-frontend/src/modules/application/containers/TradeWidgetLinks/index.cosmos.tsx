8282850ccb78a61822fde9cda4b006057fb04d3a
fairlight <31534717+fairlighteth@users.noreply.github.com>
2023-08-23 11:35:44 +0100

    feat: add badges color functionality (#3072)
    * feat: add badges color functionality
    
    * feat: check if type badge passed
    
    * feat: check if type badge passed
    
    * feat: styles definition and Cosmos fixture
    
    * feat: general cleanup

---
+++ apps/cowswap-frontend/src/modules/application/containers/TradeWidgetLinks/index.cosmos.tsx
@@ -0,0 +1,36 @@
+import { Widget } from 'modules/application/pure/Widget'
+
+import { TradeWidgetLinks, BadgeType } from '.';
+
+type BadgeInfo = {
+  text: string;
+  type: BadgeType;
+}
+
+const BADGES: BadgeInfo[] = [
+  { text: 'BETA', type: 'default' },
+  { text: 'NEW!', type: 'success' },
+  { text: 'ALPHA', type: 'alert' },
+  { text: 'NEW!', type: 'alert2' },
+  { text: 'RELEASE', type: 'information' },
+];
+
+type Fixtures = {
+  [key: string]: React.FunctionComponent;
+};
+
+const BadgeFixtures = BADGES.reduce<Fixtures>((fixtures, badge) => {
+  const Fixture = () => (
+    <Widget>
+      <TradeWidgetLinks
+        highlightedBadgeText={badge.text}
+        highlightedBadgeType={badge.type}
+      />
+    </Widget>
+  );
+
+  fixtures[`Badge - ${badge.text} (${badge.type})`] = Fixture;
+  return fixtures;
+}, {});
+
+export default BadgeFixtures;
