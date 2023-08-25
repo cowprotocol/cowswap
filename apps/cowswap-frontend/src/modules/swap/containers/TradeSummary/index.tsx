6e6eba5097757fcf46ee0f91badebdf4358540a8
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-25 17:32:55 +0600

    chore: fix import

--- apps/cowswap-frontend/src/modules/swap/containers/TradeSummary/index.tsx
+++ apps/cowswap-frontend/src/modules/swap/containers/TradeSummary/index.tsx
@@ -2,10 +2,9 @@ import { Percent } from '@uniswap/sdk-core'
 
 import TradeGp from 'legacy/state/swap/TradeGp'
 
+import { useHigherUSDValue } from 'modules/fiatAmount'
 import { TradeSummaryContent } from 'modules/swap/pure/TradeSummary'
 import { useWalletDetails } from 'modules/wallet'
-
-import { useHigherUSDValue } from '../../../fiatAmount/hooks/useHigherUSDValue'
 
 // Sub-components
 
