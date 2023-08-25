e8ab94599167da9624c7abd34551dc1723ee522d
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-25 17:35:35 +0600

    chore: rename

--- apps/cowswap-frontend/src/modules/trade/updaters/PriceImpactUpdater.tsx
+++ apps/cowswap-frontend/src/modules/trade/updaters/PriceImpactUpdater.tsx
@@ -4,12 +4,12 @@ import { useFiatValuePriceImpact } from 'legacy/hooks/usePriceImpact'
 
 import { useSafeEffect } from 'common/hooks/useSafeMemo'
 
-import { useTradePriceImpactParams } from '../hooks/useTradePriceImpactParams'
+import { useTradeParsedAmounts } from '../hooks/useTradeParsedAmounts'
 import { priceImpactAtom } from '../state/priceImpactAtom'
 
 export function PriceImpactUpdater() {
   const updatePriceImpact = useSetAtom(priceImpactAtom)
-  const params = useTradePriceImpactParams()
+  const params = useTradeParsedAmounts()
   const { isLoading, priceImpact } = useFiatValuePriceImpact(params)
 
   useSafeEffect(() => {
