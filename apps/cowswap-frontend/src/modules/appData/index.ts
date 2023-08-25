d20e7f163bf577caf7e76e13916ad3f2f2617e8a
Alfetopito <alfetopito@users.noreply.github.com>
2023-08-25 16:12:23 +0100

    chore: export more stuff from appData module

--- apps/cowswap-frontend/src/modules/appData/index.ts
+++ apps/cowswap-frontend/src/modules/appData/index.ts
@@ -1,4 +1,6 @@
 export { getAppData } from './utils/fullAppData'
 export { AppDataUpdater } from './updater/AppDataInfoUpdater'
-export { useAppData, useUploadAppData } from './hooks'
+export { useAppData, useUploadAppData, useUpdateAppDataHooks } from './hooks'
+export { addHooksToAppData, buildAppData } from './utils/buildAppData'
+export { buildAppDataHooks } from './utils/buildAppDataHooks'
 export type { AppDataInfo, UploadAppDataParams } from './types'
