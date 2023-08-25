ae0e2c12fbf242f696097ab7d413aaf8cc68c015
Alfetopito <alfetopito@users.noreply.github.com>
2023-08-25 16:12:08 +0100

    chore: update hooks inside appData updater

--- apps/cowswap-frontend/src/modules/appData/updater/AppDataInfoUpdater.tsx
+++ apps/cowswap-frontend/src/modules/appData/updater/AppDataInfoUpdater.tsx
@@ -9,6 +9,7 @@ import { useWalletInfo } from 'modules/wallet'
 
 import { UseAppDataParams, useAppDataUpdater } from './useAppDataUpdater'
 
+import { useAppDataHooks } from '../hooks'
 import { AppDataOrderClass } from '../types'
 
 export interface AppDataUpdaterProps {
@@ -21,10 +22,13 @@ export const AppDataUpdater = React.memo(({ slippage, orderClass }: AppDataUpdat
 
   const slippageBips = percentToBips(slippage)
   const utm = useUtm()
+  const hooks = useAppDataHooks()
 
   if (!chainId) return null
 
-  return <AppDataUpdaterMemo chainId={chainId} slippageBips={slippageBips} orderClass={orderClass} utm={utm} />
+  return (
+    <AppDataUpdaterMemo chainId={chainId} slippageBips={slippageBips} orderClass={orderClass} utm={utm} hooks={hooks} />
+  )
 })
 
 const AppDataUpdaterMemo = React.memo((params: UseAppDataParams) => {
