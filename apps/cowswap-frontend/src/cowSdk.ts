2b09259fbff7f4b233cae39811ce51c537a5b33a
Alexandr Kazachenko <shoom3301@gmail.com>
2023-08-28 18:20:43 +0600

    chore: example of order-book API urls replacement

--- apps/cowswap-frontend/src/cowSdk.ts
+++ apps/cowswap-frontend/src/cowSdk.ts
@@ -1,9 +1,14 @@
 import { MetadataApi } from '@cowprotocol/app-data'
-import { OrderBookApi } from '@cowprotocol/cow-sdk'
+import { OrderBookApi, SupportedChainId } from '@cowprotocol/cow-sdk'
 
 import { isBarnBackendEnv } from 'legacy/utils/environments'
 
 export const metadataApiSDK = new MetadataApi()
 export const orderBookApi = new OrderBookApi({
   env: isBarnBackendEnv ? 'staging' : 'prod',
+  baseUrls: {
+    [SupportedChainId.MAINNET]: 'https://YOUR_HOST/mainnet',
+    [SupportedChainId.GNOSIS_CHAIN]: 'https://api.cow.fi/xdai',
+    [SupportedChainId.GOERLI]: 'https://api.cow.fi/goerli',
+  },
 })
