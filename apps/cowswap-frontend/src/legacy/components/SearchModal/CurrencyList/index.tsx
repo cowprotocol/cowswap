64c480b7f1ef3bd6ec460bf6b2c824f1009fc9de
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2023-09-01 13:09:55 +0100

    feat: optimize

--- apps/cowswap-frontend/src/legacy/components/SearchModal/CurrencyList/index.tsx
+++ apps/cowswap-frontend/src/legacy/components/SearchModal/CurrencyList/index.tsx
@@ -6,111 +6,36 @@ import ICON_GAS_FREE from 'assets/icon/gas-free.svg'
 import { HashLink } from 'react-router-hash-link'
 import styled from 'styled-components/macro'
 
-import { LightGreyCard } from 'legacy/components/Card'
-import Column from 'legacy/components/Column'
-import { RowFixed } from 'legacy/components/Row'
+
 import { MenuItem as MenuItemMod } from 'legacy/components/SearchModal/styleds'
 import { MouseoverTooltip } from 'legacy/components/Tooltip'
 import { UNSUPPORTED_TOKENS_FAQ_URL } from 'legacy/constants'
 import { TagInfo } from 'legacy/state/lists/wrappedTokenInfo'
 
-import { StyledLogo } from 'common/pure/CurrencyLogo'
 import { TokenAmount } from 'common/pure/TokenAmount'
 
 import CurrencyListMod, { StyledBalanceText, TagContainer } from './CurrencyListMod'
-import { Tag as TagMod } from './styled'
+import { Wrapper, Tag, TagLink } from './styled'
 
+enum Tags {
+  UNSUPPORTED = '0',
+  GAS_FREE = '1',
+}
 
-const TOKEN_TAGS: TagInfo[] = [
-  {
+const TOKEN_TAGS: Record<Tags, TagInfo> = {
+  [Tags.UNSUPPORTED]: {
     name: 'Unsupported',
     description:
       'This token is unsupported as it does not operate optimally with CoW Protocol. Please refer to the FAQ for more information.',
     id: '0',
   },
-  {
+  [Tags.GAS_FREE]: {
     name: 'Gas-free approval',
     icon: ICON_GAS_FREE,
     description: 'This token can be approved without spending gas, using the token Permit.',
     id: '1',
   }
-]
-
-const Tag = styled(TagMod)<{ tag?: TagInfo }>`
-  display: flex;
-  align-items: center;
-  background: ${({ tag }) => (tag?.id === '0' ? 'var(--cow-color-danger-bg)' : tag?.id === '1' ? 'var(--cow-color-success-bg)' : 'var(--cow-color-grey)')};
-  color: ${({ tag }) => (tag?.id === '0' ? 'var(--cow-color-danger-text)' : tag?.id === '1' ? 'var(--cow-color-success-text)' : 'var(--cow-color-text1)')};
-  font-size: 12px;
-  font-weight: var(--cow-font-weight-medium);
-  border-radius: 4px;
-  padding: 4px 6px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  justify-self: flex-end;
-  margin: 0 4px 0 0;
-
-  > img {
-    --size: 14px;
-    display: inline-block;
-    margin: 0 5px 0 0;
-    width: var(--size);
-    height: var(--size);
-  }
-`
-
-const TagLink = styled(Tag)`
-  a {
-    color: inherit;
-    font-weight: inherit;
-  }
-`
-
-const Wrapper = styled.div`
-  ${Column} {
-    > div {
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      max-width: 220px;
-      width: 100%;
-
-      // Token symbol name
-      &:first-of-type {
-        color: ${({ theme }) => theme.text1};
-      }
-
-      // Token full name
-      &:last-of-type {
-        color: ${({ theme }) => theme.text2};
-        font-weight: 400;
-      }
-
-      ${({ theme }) => theme.mediaWidth.upToSmall`
-        max-width: 140px;
-      `};
-    }
-  }
-
-  ${StyledLogo} {
-    height: 36px;
-    width: 36px;
-    border-radius: 36px;
-  }
-
-  ${TagLink} {
-    color: ${({ theme }) => theme.text1};
-  }
-
-  ${LightGreyCard} {
-    background: ${({ theme }) => theme.bg1};
-  }
-
-  ${LightGreyCard} ${RowFixed} > div {
-    color: ${({ theme }) => theme.text1};
-  }
-`
+};
 
 export const MenuItem = styled(MenuItemMod)`
   &:hover {
@@ -125,16 +50,12 @@ function TokenTags({
   isUnsupported: boolean; 
   isPermitCompatible?: boolean; 
 }) {
-
   const tagsToShow: TagInfo[] = [];
 
-  // If the token is unsupported, add the related tag to the tagsToShow array.
   if (isUnsupported) {
-    tagsToShow.push(TOKEN_TAGS[0]);
-  } 
-  // If the token has gas-free approval (permit compatible), add the related tag.
-  else if (isPermitCompatible) {
-    tagsToShow.push(TOKEN_TAGS[1]);
+    tagsToShow.push(TOKEN_TAGS[Tags.UNSUPPORTED]);
+  } else if (isPermitCompatible) {
+    tagsToShow.push(TOKEN_TAGS[Tags.GAS_FREE]);
   }
 
   if (tagsToShow.length === 0) {
--- apps/cowswap-frontend/src/legacy/components/SearchModal/CurrencyList/styled.ts
+++ apps/cowswap-frontend/src/legacy/components/SearchModal/CurrencyList/styled.ts
@@ -1,8 +1,18 @@
 import styled from 'styled-components/macro'
 
-export const Tag = styled.div`
-  /* background-color: ${({ theme }) => theme.bg3};
-  color: ${({ theme }) => theme.text2};
+
+import { LightGreyCard } from 'legacy/components/Card'
+import Column from 'legacy/components/Column'
+import { RowFixed } from 'legacy/components/Row'
+import { TagInfo } from 'legacy/state/lists/wrappedTokenInfo'
+
+import { StyledLogo } from 'common/pure/CurrencyLogo'
+
+export const Tag = styled.div<{ tag?: TagInfo }>`
+  display: flex;
+  align-items: center;
+  background: ${({ tag }) => (tag?.id === '0' ? 'var(--cow-color-danger-bg)' : tag?.id === '1' ? 'var(--cow-color-success-bg)' : 'var(--cow-color-grey)')};
+  color: ${({ tag }) => (tag?.id === '0' ? 'var(--cow-color-danger-text)' : tag?.id === '1' ? 'var(--cow-color-success-text)' : 'var(--cow-color-text1)')};
   font-size: 12px;
   font-weight: var(--cow-font-weight-medium);
   border-radius: 4px;
@@ -11,5 +21,65 @@ export const Tag = styled.div`
   text-overflow: ellipsis;
   white-space: nowrap;
   justify-self: flex-end;
-  margin: 0 4px 0 0; */
+  margin: 0 4px 0 0;
+
+  > img {
+    --size: 14px;
+    display: inline-block;
+    margin: 0 5px 0 0;
+    width: var(--size);
+    height: var(--size);
+  }
+`
+
+export const TagLink = styled(Tag)`
+  a {
+    color: inherit;
+    font-weight: inherit;
+  }
+`
+
+export const Wrapper = styled.div`
+  ${Column} {
+    > div {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      max-width: 220px;
+      width: 100%;
+
+      // Token symbol name
+      &:first-of-type {
+        color: ${({ theme }) => theme.text1};
+      }
+
+      // Token full name
+      &:last-of-type {
+        color: ${({ theme }) => theme.text2};
+        font-weight: 400;
+      }
+
+      ${({ theme }) => theme.mediaWidth.upToSmall`
+        max-width: 140px;
+      `};
+    }
+  }
+
+  ${StyledLogo} {
+    height: 36px;
+    width: 36px;
+    border-radius: 36px;
+  }
+
+  ${TagLink} {
+    color: ${({ theme }) => theme.text1};
+  }
+
+  ${LightGreyCard} {
+    background: ${({ theme }) => theme.bg1};
+  }
+
+  ${LightGreyCard} ${RowFixed} > div {
+    color: ${({ theme }) => theme.text1};
+  }
 `
