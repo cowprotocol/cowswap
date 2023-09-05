82be057b432783231f21e88da9455141b5d6fea9
Alfetopito <alfetopito@users.noreply.github.com>
2023-09-05 18:40:34 +0100

    chore: do not try to calculate prices when it's < 1 USDC atom

--- apps/cowswap-frontend/src/modules/usdAmount/state/usdTokenPricesAtom.ts
+++ apps/cowswap-frontend/src/modules/usdAmount/state/usdTokenPricesAtom.ts
@@ -23,18 +23,27 @@ export const usdTokenPricesAtom = atom((get) => {
 
     acc[tokenAddress] = {
       ...usdPrice,
-      price: usdPrice.price === null ? null : calculatePrice(usdPrice.currency, usdPrice.price),
+      price: calculatePrice(usdPrice.currency, usdPrice.price),
     }
 
     return acc
   }, {})
 })
 
-function calculatePrice(currency: Token, price: number): Price<Token, Token> {
+function calculatePrice(currency: Token, price: string | null): Price<Token, Token> | null {
+  if (!price) {
+    return null
+  }
+
   const usdcToken = USDC[currency.chainId as SupportedChainId]
 
+  if (price.includes('e') || new RegExp('\\.' + '0'.repeat(usdcToken.decimals)).test(price)) {
+    // Less than 1 atom of USDC, cannot create a Price instance
+    return null
+  }
+
   const baseAmount = tryParseCurrencyAmount('1', currency)
-  const quoteAmount = tryParseCurrencyAmount(price.toString(), usdcToken)
+  const quoteAmount = tryParseCurrencyAmount(price, usdcToken)
 
   return new Price({
     baseAmount,
