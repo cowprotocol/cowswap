a3e8bf5d456c0a6ecd1ff5d6cffe65ae6278b86e
Alexandr Kazachenko <shoom3301@gmail.com>
2023-09-08 20:23:04 +0600

    fix: fix mapping network name from URL query to chainId (#3125)

---
+++ apps/cowswap-frontend/src/utils/getCurrentChainIdFromUrl.test.ts
@@ -0,0 +1,56 @@
+import { SupportedChainId } from '@cowprotocol/cow-sdk'
+
+import { getCurrentChainIdFromUrl } from './getCurrentChainIdFromUrl'
+
+describe('getCurrentChainIdFromUrl()', () => {
+  it('When chainId presents in the url path, then it should taken as chainId', () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        hash: '#/5/WETH',
+      },
+      writable: true,
+    })
+    const chainId = getCurrentChainIdFromUrl()
+
+    expect(chainId).toBe(SupportedChainId.GOERLI)
+  })
+
+  it('When chainId from the url path is not valid, then should take mainnet as default', () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        hash: '#/999/WETH',
+      },
+      writable: true,
+    })
+
+    const chainId = getCurrentChainIdFromUrl()
+
+    expect(chainId).toBe(SupportedChainId.MAINNET)
+  })
+
+  it('When network name presents in the url query, then it should taken as chainId', () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        hash: '#/WETH?chain=gnosis_chain',
+      },
+      writable: true,
+    })
+
+    const chainId = getCurrentChainIdFromUrl()
+
+    expect(chainId).toBe(SupportedChainId.GNOSIS_CHAIN)
+  })
+
+  it('When network name from the url query is not valid, then should take mainnet as default', () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        hash: '#/WETH?chain=blabla',
+      },
+      writable: true,
+    })
+
+    const chainId = getCurrentChainIdFromUrl()
+
+    expect(chainId).toBe(SupportedChainId.MAINNET)
+  })
+})
