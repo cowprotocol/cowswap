8380218b348b97e78c23daa2e2e4b9cd9172840c
Alexandr Kazachenko <shoom3301@gmail.com>
2023-09-08 20:10:37 +0600

    fix: fix mapping network name from URL query to chainId

--- apps/cowswap-frontend/src/utils/getCurrentChainIdFromUrl.ts
+++ apps/cowswap-frontend/src/utils/getCurrentChainIdFromUrl.ts
@@ -1,13 +1,20 @@
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
+const chainNameToIdMap: { [key: string]: SupportedChainId } = {
+  mainnet: SupportedChainId.MAINNET,
+  gnosis_chain: SupportedChainId.GNOSIS_CHAIN,
+  goerli: SupportedChainId.GOERLI,
+}
+
 export function getCurrentChainIdFromUrl(): SupportedChainId {
   // Trying to get chainId from URL (#/100/swap)
   // eslint-disable-next-line no-restricted-globals
   const { location } = window
   const urlChainIdMatch = location.hash.match(/^#\/(\d{1,9})\D/)
-  const chainId = +(
-    (urlChainIdMatch ? urlChainIdMatch[1] : new URLSearchParams(location.hash.split('?')[1]).get('chain')) || ''
-  )
+  const searchParams = new URLSearchParams(location.hash.split('?')[1])
+  const chainQueryParam = searchParams.get('chain')
+
+  const chainId = +(urlChainIdMatch?.[1] || chainNameToIdMap[chainQueryParam || ''] || '')
 
   if (chainId && chainId in SupportedChainId) return chainId
 
