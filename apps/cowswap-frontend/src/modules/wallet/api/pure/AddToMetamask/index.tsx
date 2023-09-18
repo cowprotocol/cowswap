522a1ad39e73db47f6f320b1ebbe7509cdd3d128
Alexandr Kazachenko <shoom3301@gmail.com>
2023-09-18 11:36:26 +0300

    chore: fix dev merge

--- apps/cowswap-frontend/src/modules/wallet/api/pure/AddToMetamask/index.tsx
+++ apps/cowswap-frontend/src/modules/wallet/api/pure/AddToMetamask/index.tsx
@@ -20,7 +20,7 @@ export const ButtonCustom = styled.button`
   border-radius: 16px;
   min-height: 52px;
   border: 1px solid ${({ theme }) => theme.border2};
-  color: var(${UI.COLOR_TEXT1});
+  color: var(--cow-color-text1);
   background: transparent;
   outline: 0;
   padding: 8px 16px;
