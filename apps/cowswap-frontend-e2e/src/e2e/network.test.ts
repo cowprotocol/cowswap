7800864d402c15528a90f9aa4feaa478cddeb852
Alexandr Kazachenko <shoom3301@gmail.com>
2023-09-19 13:39:58 +0300

    chore: fix test

--- apps/cowswap-frontend-e2e/src/e2e/network.test.ts
+++ apps/cowswap-frontend-e2e/src/e2e/network.test.ts
@@ -1,18 +1,18 @@
 describe('Network', () => {
   it('When network is changed, then should reset swap form state', () => {
     // GIVEN: current network is Goerli, wallet is not connected
+    cy.viewport(1080, 1200)
     cy.visit('/#/5/swap/GNO/COW')
     cy.get('#web3-status-connected').click()
 
-    cy.get('#account-orders-panel')
-      .then((el) => {
-        /**
-         * Disable css smooth scroll, that doesn't plays nice with cypress.
-         * See https://github.com/cypress-io/cypress/issues/3200
-         */
-        el.css('scroll-behavior', 'auto')
-      })
-      .scrollTo('top')
+    cy.get('#account-orders-panel').then((el) => {
+      /**
+       * Disable css smooth scroll, that doesn't plays nice with cypress.
+       * See https://github.com/cypress-io/cypress/issues/3200
+       */
+      el.css('scroll-behavior', 'auto')
+      el.scrollTop(0)
+    })
 
     cy.get('#disconnect-btn').click()
 
