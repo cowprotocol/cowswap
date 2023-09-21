18dc278a7b4b10450996288692a1fb9d0ba81f93
Alfetopito <alfetopito@users.noreply.github.com>
2023-09-21 13:58:59 +0100

    chore: use a new empty private key for estimating the permits

--- apps/cowswap-frontend/src/modules/permit/const.ts
+++ apps/cowswap-frontend/src/modules/permit/const.ts
@@ -6,7 +6,7 @@ import ms from 'ms.macro'
 
 // PK used only for signing permit requests for quoting and identifying token 'permittability'
 // Do not use or try to send funds to it. Or do. It'll be your funds 🤷
-const PERMIT_PK = '0xd0683148c0c6116a3f47340cf088de2fd791304b57d723e46a7f54504edf2cf2'
+const PERMIT_PK = '0xc58a2a421ca71ca57ae698f1c32feeb0b0ccb434da0b8089d88d80fb918f3f9d' // address: 0xFf65D1DfCF256cf4A8D5F2fb8e70F936606B7474
 
 export const PERMIT_SIGNER = new Wallet(PERMIT_PK)
 
