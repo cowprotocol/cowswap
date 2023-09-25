693cc25a9331baf67645183cd52a9e1119e4b2a8
Alfetopito <alfetopito@users.noreply.github.com>
2023-09-25 12:21:00 +0100

    chore: do one permit utils instance cache per type

--- apps/cowswap-frontend/src/modules/permit/utils/getPermitUtilsInstance.ts
+++ apps/cowswap-frontend/src/modules/permit/utils/getPermitUtilsInstance.ts
@@ -7,25 +7,38 @@ import { PermitProviderConnector } from 'modules/wallet/utils/PermitProviderConn
 
 import { PERMIT_SIGNER } from '../const'
 
-const PERMIT_UTILS_CACHE: Record<number, Eip2612PermitUtils> = {}
+/**
+ * Cache by network. Here we don't care about the provider as a static account will be used for the signature
+ */
+const CHAIN_UTILS_CACHE = new Map<number, Eip2612PermitUtils>()
+/**
+ * Cache by provider. Here we cache per provider as each account should have its own instance
+ */
+const PROVIDER_UTILS_CACHE = new Map<Web3Provider, Eip2612PermitUtils>()
 
 export function getPermitUtilsInstance(
   chainId: SupportedChainId,
   provider: Web3Provider,
   account?: string | undefined
 ): Eip2612PermitUtils {
-  const cached = PERMIT_UTILS_CACHE[chainId]
+  const chainCache = CHAIN_UTILS_CACHE.get(chainId)
 
-  // Only cache if there's no account to prevent stale signers
-  if (!account && cached) {
-    return cached
+  if (!account && chainCache) {
+    return chainCache
+  }
+  const providerCache = PROVIDER_UTILS_CACHE.get(provider)
+
+  if (providerCache) {
+    return providerCache
   }
 
   const web3ProviderConnector = new PermitProviderConnector(provider, account ? undefined : PERMIT_SIGNER)
   const eip2612PermitUtils = new Eip2612PermitUtils(web3ProviderConnector)
 
   if (!account) {
-    PERMIT_UTILS_CACHE[chainId] = eip2612PermitUtils
+    CHAIN_UTILS_CACHE.set(chainId, eip2612PermitUtils)
+  } else {
+    PROVIDER_UTILS_CACHE.set(provider, eip2612PermitUtils)
   }
 
   return eip2612PermitUtils
