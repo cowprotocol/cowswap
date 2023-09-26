79ae98d6466288a0c4399116849b054e9014a7bc
Alfetopito <alfetopito@users.noreply.github.com>
2023-09-26 17:51:47 +0100

    refactor: use setter to load current state rather than relying on stale one

--- apps/cowswap-frontend/src/modules/swap/hooks/useSwapConfirmManager.ts
+++ apps/cowswap-frontend/src/modules/swap/hooks/useSwapConfirmManager.ts
@@ -1,10 +1,10 @@
-import { useAtom } from 'jotai'
+import { useSetAtom } from 'jotai'
 import { useMemo } from 'react'
 
 import TradeGp from 'legacy/state/swap/TradeGp'
 import { useExpertModeManager } from 'legacy/state/user/hooks'
 
-import { swapConfirmAtom } from 'modules/swap/state/swapConfirmAtom'
+import { swapConfirmAtom, SwapConfirmState } from 'modules/swap/state/swapConfirmAtom'
 
 export interface SwapConfirmManager {
   setSwapError(swapErrorMessage: string): void
@@ -16,15 +16,17 @@ export interface SwapConfirmManager {
 }
 
 export function useSwapConfirmManager(): SwapConfirmManager {
-  const [swapConfirmState, setSwapConfirmState] = useAtom(swapConfirmAtom)
+  const setSwapConfirmState = useSetAtom(swapConfirmAtom)
   const [isExpertMode] = useExpertModeManager()
 
   return useMemo(
     () => ({
       setSwapError(swapErrorMessage: string) {
-        const state = { ...swapConfirmState, swapErrorMessage }
-        console.debug('[Swap confirm state] setSwapError: ', state)
-        setSwapConfirmState(state)
+        setSwapConfirmState((prev) => {
+          const state = { ...prev, swapErrorMessage, attemptingTxn: false }
+          console.debug('[Swap confirm state] setSwapError: ', state)
+          return state
+        })
       },
       openSwapConfirmModal(tradeToConfirm: TradeGp) {
         const state = {
@@ -38,38 +40,48 @@ export function useSwapConfirmManager(): SwapConfirmManager {
         setSwapConfirmState(state)
       },
       acceptRateUpdates(tradeToConfirm: TradeGp) {
-        const state = { ...swapConfirmState, tradeToConfirm }
-        console.debug('[Swap confirm state] acceptRateUpdates: ', state)
-        setSwapConfirmState(state)
+        setSwapConfirmState((prev) => {
+          const state = { ...prev, tradeToConfirm }
+          console.debug('[Swap confirm state] acceptRateUpdates: ', state)
+          return state
+        })
+      },
       },
       closeSwapConfirm() {
-        const state = { ...swapConfirmState, showConfirm: false }
-        console.debug('[Swap confirm state] closeSwapConfirm: ', state)
-        setSwapConfirmState(state)
+        setSwapConfirmState((prev) => {
+          const state = { ...prev, showConfirm: false }
+          console.debug('[Swap confirm state] closeSwapConfirm: ', state)
+          return state
+        })
       },
       sendTransaction(tradeToConfirm: TradeGp) {
-        const state = {
-          tradeToConfirm,
-          attemptingTxn: true,
-          swapErrorMessage: undefined,
-          showConfirm: true,
-          txHash: undefined,
-        }
-        console.debug('[Swap confirm state] sendTransaction: ', state)
-        setSwapConfirmState(state)
+        setSwapConfirmState((prev) => {
+          const state = {
+            ...prev,
+            tradeToConfirm,
+            attemptingTxn: true,
+            swapErrorMessage: undefined,
+            showConfirm: true,
+            txHash: undefined,
+          }
+          console.debug('[Swap confirm state] sendTransaction: ', state)
+          return state
+        })
       },
       transactionSent(txHash: string) {
-        const state = {
-          ...swapConfirmState,
-          attemptingTxn: false,
-          swapErrorMessage: undefined,
-          showConfirm: !isExpertMode,
-          txHash,
-        }
-        console.debug('[Swap confirm state] transactionSent: ', state)
-        setSwapConfirmState(state)
+        setSwapConfirmState((prev) => {
+          const state = {
+            ...prev,
+            attemptingTxn: false,
+            swapErrorMessage: undefined,
+            showConfirm: !isExpertMode,
+            txHash,
+          }
+          console.debug('[Swap confirm state] transactionSent: ', state)
+          return state
+        })
       },
     }),
-    [swapConfirmState, setSwapConfirmState, isExpertMode]
+    [setSwapConfirmState, isExpertMode]
   )
 }
