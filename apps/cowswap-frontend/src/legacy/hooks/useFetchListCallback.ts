88f154d305d7bcf19ed19a88e4fadceac2780752
Alexandr Kazachenko <shoom3301@gmail.com>
2023-09-26 13:41:07 +0600

    fix: don't fetch tokens list ens hashes

--- apps/cowswap-frontend/src/legacy/hooks/useFetchListCallback.ts
+++ apps/cowswap-frontend/src/legacy/hooks/useFetchListCallback.ts
@@ -12,6 +12,15 @@ import { fetchTokenList } from 'legacy/state/lists/actions'
 
 import getTokenList from 'lib/hooks/useTokenList/fetchTokenList'
 
+const TOKENS_LISTS_ENS_HASHES: { [key: string]: string } = {
+  'tokenlist.aave.eth': '0xe301017012204aeb1d95ad5624a5299aa40dab01016b8a9c8401515ec063e9663d80f3776366',
+  'defi.cmc.eth': '0xe301017012201acd1250f6110108aaa17ed1ae463334655067138bc3270bbb44c71235cf6ffa',
+  'stablecoin.cmc.eth': '0xe30101701220f4b10385da995ae2d0925d328c7d82f21279000d10b4dcd8d409c0a9ef0c6cc1',
+  'synths.snx.eth': '0xe30101701220134dda0aa35ea0289bc5688fe82b3e2d153b8ffbfb150984666a80b7b0b8412e',
+  'wrapped.tokensoft.eth': '0xe30101701220a77c3f3999021e5a6e2551e36424e5270450ae259a9b1edde1ade5cd7c70ffa3',
+  't2crtokens.eth': '0xe3010170122062dd98ddd1b57a4e34c602b0fe40746cf71d427d24ab5689082480f9e8b97a12',
+}
+
 export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean) => Promise<TokenList> {
   const dispatch = useAppDispatch()
   const { chainId } = useWalletInfo()
@@ -21,7 +30,11 @@ export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean
     async (listUrl: string, sendDispatch = true) => {
       const requestId = nanoid()
       sendDispatch && dispatch(fetchTokenList.pending({ requestId, url: listUrl, chainId }))
-      return getTokenList(listUrl, (ensName: string) => resolveENSContentHash(ensName, MAINNET_PROVIDER))
+      return getTokenList(listUrl, (ensName: string) => {
+        const cachedHash = TOKENS_LISTS_ENS_HASHES[ensName]
+
+        return cachedHash ? Promise.resolve(cachedHash) : resolveENSContentHash(ensName, MAINNET_PROVIDER)
+      })
         .then((tokenList) => {
           // Mod: add chainId
           sendDispatch && dispatch(fetchTokenList.fulfilled({ url: listUrl, tokenList, requestId, chainId }))
