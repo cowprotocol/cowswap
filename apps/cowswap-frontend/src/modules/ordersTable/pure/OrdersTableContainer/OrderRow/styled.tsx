aa943bf06ab2ba3bb3040bae96106b4463f1283a
Alexandr Kazachenko <shoom3301@gmail.com>
2023-09-26 13:14:48 +0600

    feat(orders-table): approve order token from allowance warn tooltip (#3144)
    * chore: remove unused code
    
    * fix(limit-orders): fix tooltips displaying in settings
    
    * refactor(limit-orders): replace allowance warn tooltip by a new one
    
    * feat(orders-table): approve order token from allowance warn tooltip
    
    * chore: test commit

--- apps/cowswap-frontend/src/modules/ordersTable/pure/OrdersTableContainer/OrderRow/styled.tsx
+++ apps/cowswap-frontend/src/modules/ordersTable/pure/OrdersTableContainer/OrderRow/styled.tsx
@@ -1,6 +1,8 @@
 import { transparentize } from 'polished'
 import styled from 'styled-components/macro'
 
+import QuestionHelper from 'legacy/components/QuestionHelper'
+
 import { RateWrapper } from 'common/pure/RateInfo'
 
 export const WarningIndicator = styled.button<{ hasBackground?: boolean }>`
@@ -22,6 +24,7 @@ export const WarningIndicator = styled.button<{ hasBackground?: boolean }>`
 
   svg > path {
     fill: ${({ theme }) => theme.alert};
+    stroke: none;
   }
 `
 
@@ -29,6 +32,7 @@ export const WarningContent = styled.div`
   max-width: 270px;
   padding: 10px;
   color: ${({ theme }) => theme.black};
+  background: ${({ theme }) => theme.alert};
 
   h3,
   p {
@@ -41,12 +45,20 @@ export const WarningContent = styled.div`
   }
 `
 
+export const StyledQuestionHelper = styled(QuestionHelper)`
+  margin: 0;
+`
+
 export const WarningParagraph = styled.div`
   margin-bottom: 20px;
 
   :last-child {
     margin-bottom: 0;
   }
+`
+
+export const WarningActionBox = styled.div`
+  margin-top: 15px;
 `
 
 export const RateValue = styled.span``
