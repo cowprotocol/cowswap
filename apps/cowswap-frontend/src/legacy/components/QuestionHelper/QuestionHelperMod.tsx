009a9d75635d50ac8cb610279699c4a278754dc4
Alexandr Kazachenko <shoom3301@gmail.com>
2023-09-26 12:41:45 +0600

    fix(limit-orders): fix tooltips displaying in settings (#3143)
    * chore: remove unused code
    
    * fix(limit-orders): fix tooltips displaying in settings

--- apps/cowswap-frontend/src/legacy/components/QuestionHelper/QuestionHelperMod.tsx
+++ apps/cowswap-frontend/src/legacy/components/QuestionHelper/QuestionHelperMod.tsx
@@ -6,32 +6,6 @@ import styled from 'styled-components/macro'
 
 import { QuestionWrapper } from './index'
 
-/* const QuestionWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0px;
-  width: 18px;
-  height: 18px;
-  border: none;
-  background: none;
-  outline: none;
-  cursor: default;
-  border-radius: 36px;
-  font-size: 12px;
-  background-color: ${({ theme }) => theme.bg2};
-  color: var(${UI.COLOR_TEXT2});
-
-  :hover,
-  :focus {
-    opacity: 0.7;
-  }
-` */
-
-/* const QuestionMark = styled.span`
-  font-size: 14px;
-` */
-
 const QuestionHelperContainer = styled.span`
   margin-left: 4px;
   display: flex;
@@ -73,7 +47,6 @@ export default function QuestionHelper({ text, className, QuestionMark, ...toolt
     <QuestionHelperContainer className={className}>
       <Tooltip {...tooltipProps} show={show} text={content}>
         <QuestionWrapper onClick={open} onMouseEnter={open} onMouseLeave={close}>
-          {/* <QuestionMark>?</QuestionMark> */}
           <QuestionMark />
         </QuestionWrapper>
       </Tooltip>
