ceae847ca8184d2e537bb331ea27cd97afd190ce
Anxo Rodriguez <anxolin@gmail.com>
2023-09-27 13:36:21 +0100

    feat: allow to change rpc nodes

--- apps/cowswap-frontend/src/legacy/constants/networks.ts
+++ apps/cowswap-frontend/src/legacy/constants/networks.ts
@@ -1,18 +1,40 @@
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 import { JsonRpcProvider } from '@ethersproject/providers'
 
+function initRpcUrls(): Record<SupportedChainId, string> {
+  const { REACT_APP_INFURA_KEY, REACT_APP_NETWORK_URL_1, REACT_APP_NETWORK_URL_5, REACT_APP_NETWORK_URL_100 } =
+    process.env
+
+  console.log('process.env', process.env)
+
+  if (REACT_APP_NETWORK_URL_1 && REACT_APP_NETWORK_URL_5 && REACT_APP_NETWORK_URL_100) {
+    return {
+      [SupportedChainId.MAINNET]: REACT_APP_NETWORK_URL_1,
+      [SupportedChainId.GOERLI]: REACT_APP_NETWORK_URL_5,
+      [SupportedChainId.GNOSIS_CHAIN]: REACT_APP_NETWORK_URL_100,
+    }
+  }
+
+  if (REACT_APP_INFURA_KEY) {
+    return {
+      [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+      [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+      [SupportedChainId.GNOSIS_CHAIN]: `https://rpc.gnosis.gateway.fm`,
+    }
+  }
+
+  throw new Error(
+    `Either REACT_APP_NETWORK_URL_{1,5,100} or REACT_APP_INFURA_KEY environment variables must be defined`
+  )
+}
+
 const INFURA_KEY = process.env.REACT_APP_INFURA_KEY
 if (typeof INFURA_KEY === 'undefined') {
   throw new Error(`REACT_APP_INFURA_KEY must be a defined environment variable`)
 }
 
-export const MAINNET_PROVIDER = new JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_KEY}`)
-
 /**
  * These are the network URLs used by the interface when there is not another available source of chain data
  */
-export const RPC_URLS: Record<SupportedChainId, string> = {
-  [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.GNOSIS_CHAIN]: `https://rpc.gnosis.gateway.fm`,
-}
+export const RPC_URLS = initRpcUrls()
+export const MAINNET_PROVIDER = new JsonRpcProvider(RPC_URLS[SupportedChainId.MAINNET])
