78d1230323ff252ad3f76a39851ed74d940dc0db
Alfetopito <alfetopito@users.noreply.github.com>
2023-09-28 11:28:38 +0100

    refactor: sort imports

--- apps/cowswap-frontend/src/modules/swap/pure/SwapButtons/index.tsx
+++ apps/cowswap-frontend/src/modules/swap/pure/SwapButtons/index.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react'
 import { GpEther } from '@cowprotocol/common-const'
 import { genericPropsChecker } from '@cowprotocol/common-utils'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
-import { ButtonSize, TokenSymbol, ButtonError, ButtonPrimary, AutoRow } from '@cowprotocol/ui'
+import { AutoRow, ButtonError, ButtonPrimary, ButtonSize, TokenSymbol } from '@cowprotocol/ui'
 import { Currency, CurrencyAmount, Token } from '@uniswap/sdk-core'
 
 import { Trans } from '@lingui/macro'
