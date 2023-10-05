a546169ee92253e890bf49dfc0e009d4d04e0181
Alfetopito <alfetopito@users.noreply.github.com>
2023-10-05 10:22:49 +0100

    chore: remove comment about backend PR, which has been merged

--- apps/cowswap-frontend/src/modules/permit/const.ts
+++ apps/cowswap-frontend/src/modules/permit/const.ts
@@ -26,6 +26,6 @@ export const DEFAULT_PERMIT_DURATION = ms`5 years`
 
 export const ORDER_TYPE_SUPPORTS_PERMIT: Record<TradeType, boolean> = {
   [TradeType.SWAP]: true,
-  [TradeType.LIMIT_ORDER]: true, // TODO: temporary, for the modals. Should be turn on for real only when this is deployed https://github.com/cowprotocol/services/pull/1892
+  [TradeType.LIMIT_ORDER]: true,
   [TradeType.ADVANCED_ORDERS]: false,
 }
