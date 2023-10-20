dc1f1de42d99d8db17992e77e7dda4019e79ada5
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2023-10-20 12:41:36 +0100

    feat: fix import

--- apps/cowswap-frontend/src/cow-react/index.tsx
+++ apps/cowswap-frontend/src/cow-react/index.tsx
@@ -9,7 +9,6 @@ import { StrictMode } from 'react'
 import { BlockNumberProvider } from '@cowprotocol/common-hooks'
 import { isInjectedWidget } from '@cowprotocol/common-utils'
 import { nodeRemoveChildFix } from '@cowprotocol/common-utils'
-import { IframeResizer } from '@cowprotocol/common-utils'
 import { jotaiStore } from '@cowprotocol/core'
 import { SnackbarsWidget } from '@cowprotocol/snackbars'
 
@@ -34,7 +33,6 @@ import { FeatureGuard } from 'common/containers/FeatureGuard'
 import { IframeResizer } from 'utils/iframeResizer'
 
 import { WalletUnsupportedNetworkBanner } from '../common/containers/WalletUnsupportedNetworkBanner'
-
 
 // Node removeChild hackaround
 // based on: https://github.com/facebook/react/issues/11538#issuecomment-417504600
