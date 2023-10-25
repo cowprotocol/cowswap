66804a649c6a53854d22edc735b8697241f9682a
Alfetopito <alfetopito@users.noreply.github.com>
2023-10-25 12:13:31 +0100

    chore: expose GetTokenPermitIntoResult

--- libs/permit-utils/src/index.ts
+++ libs/permit-utils/src/index.ts
@@ -3,4 +3,11 @@ export { generatePermitHook } from './lib/generatePermitHook'
 export { getPermitUtilsInstance } from './lib/getPermitUtilsInstance'
 export { getTokenPermitInfo } from './lib/getTokenPermitInfo'
 
-export type { PermitHookData, PermitHookParams, PermitInfo, PermitType, SupportedPermitInfo } from './types'
+export type {
+  PermitHookData,
+  PermitHookParams,
+  PermitInfo,
+  PermitType,
+  SupportedPermitInfo,
+  GetTokenPermitIntoResult,
+} from './types'
