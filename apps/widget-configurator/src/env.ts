eb3ab7bef9e1568f3c0c3632e08f1807a2fb17f7
Alexandr Kazachenko <shoom3301@gmail.com>
2023-10-25 11:23:51 +0600

    chore: fix env

--- apps/widget-configurator/src/env.ts
+++ apps/widget-configurator/src/env.ts
@@ -1,3 +1,3 @@
-export const isLocalHost = window.location.host.includes('localhost')
+export const isLocalHost = ['localhost', '127.0.0.1'].includes(window.location.hostname)
 
-export const isVercel = window.location.host.includes('vercel.app')
+export const isVercel = window.location.hostname.includes('vercel.app')
