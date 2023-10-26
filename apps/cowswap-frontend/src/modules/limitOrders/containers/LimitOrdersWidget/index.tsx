39e615a35b0865a7611fecc1a908f1fee00226ce
Anxo Rodriguez <anxolin@gmail.com>
2023-10-26 16:35:50 +0100

    Halloween: Integrate layer3 (#3270)
    * feat: halloween theme
    
    * chore: fix build
    
    * chore: fix test
    
    * feat: refactor halloween theme (#3266)
    
    * feat: refactor halloween theme
    
    * feat: refactor halloween theme
    
    * feat: refactor halloween theme
    
    * feat: refactor halloween theme
    
    * feat: integrate layer3
    
    * feat: create banner component
    
    * feat: add style
    
    * test: test adding fortunes
    
    * fix: fix marging
    
    * feat: add feature flag
    
    * chore: revert change. Point to configuration branch
    
    * chore: remove empty spaces
    
    ---------
    
    Co-authored-by: Alexandr Kazachenko <shoom3301@gmail.com>
    Co-authored-by: fairlight <31534717+fairlighteth@users.noreply.github.com>

--- apps/cowswap-frontend/src/modules/limitOrders/containers/LimitOrdersWidget/index.tsx
+++ apps/cowswap-frontend/src/modules/limitOrders/containers/LimitOrdersWidget/index.tsx
@@ -2,6 +2,7 @@ import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 import React, { useMemo } from 'react'
 
 import { OrderKind } from '@cowprotocol/cow-sdk'
+import { Layer3Banner } from '@cowprotocol/ui'
 
 import { Field } from 'legacy/state/types'
 
@@ -15,6 +16,7 @@ import { BulletListItem, UnlockWidgetScreen } from 'modules/trade/pure/UnlockWid
 import { TradeFormValidation, useGetTradeFormValidation } from 'modules/tradeFormValidation'
 import { useSetTradeQuoteParams, useTradeQuote } from 'modules/tradeQuote'
 
+import { FeatureGuard } from 'common/containers/FeatureGuard'
 import { useRateInfoParams } from 'common/hooks/useRateInfoParams'
 import { CurrencyInfo } from 'common/pure/CurrencyInputPanel/types'
 
@@ -256,6 +258,9 @@ const LimitOrders = React.memo((props: LimitOrdersProps) => {
         inputCurrencyInfo={inputCurrencyInfo}
         outputCurrencyInfo={outputCurrencyInfo}
       />
+      <FeatureGuard featureFlag="layer3BannerEnabled">
+        <Layer3Banner />
+      </FeatureGuard>
       {tradeContext && (
         <LimitOrdersConfirmModal
           tradeContext={tradeContext}
