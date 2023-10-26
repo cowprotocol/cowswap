31d63709a120a311a18df9dbfa4d11880b69bbb5
Anxo Rodriguez <anxolin@gmail.com>
2023-10-26 18:23:22 +0100

    feat: fix issue with sounds

--- apps/cowswap-frontend/src/legacy/state/cowToken/middleware.ts
+++ apps/cowswap-frontend/src/legacy/state/cowToken/middleware.ts
@@ -19,8 +19,9 @@ export const cowTokenMiddleware: Middleware<Record<string, unknown>, AppState> =
     const { chainId, hash } = action.payload
     const transaction = store.getState().transactions[chainId][hash]
 
-    const { userDarkMode, matchesDarkMode } = store.getState().user
-    const isDarkMode = userDarkMode === null ? matchesDarkMode : userDarkMode
+    // const { userDarkMode, matchesDarkMode } = store.getState().user
+    // const isDarkMode = userDarkMode === null ? matchesDarkMode : userDarkMode
+    const isDarkMode = true // TODO: revert after halloween
 
     if (transaction.swapVCow || transaction.swapLockedGNOvCow) {
       const status = transaction.receipt?.status
