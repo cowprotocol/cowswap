c742343c057a106b1ce02db9fb860d485995d431
Anxo Rodriguez <anxolin@gmail.com>
2023-10-26 13:34:52 +0100

    feat: simplify envs, cleanup them and improve readme

--- apps/cowswap-frontend/src/legacy/state/application/initialState.ts
+++ apps/cowswap-frontend/src/legacy/state/application/initialState.ts
@@ -1,17 +1,17 @@
-import { localWarning } from './localWarning'
+// import { localWarning } from './localWarning'
 import { ApplicationState } from './reducer'
 
 const popupList: ApplicationState['popupList'] = []
 
-if (localWarning) {
-  popupList.push({
-    key: 'localWarning',
-    show: true,
-    removeAfterMs: null,
-    content: {
-      warning: localWarning,
-    },
-  })
-}
+// if (localWarning) {
+//   popupList.push({
+//     key: 'localWarning',
+//     show: true,
+//     removeAfterMs: null,
+//     content: {
+//       warning: localWarning,
+//     },
+//   })
+// }
 
 export const initialState: ApplicationState = { chainId: null, openModal: null, popupList }
