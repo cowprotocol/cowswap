b29c6009cff244112bb28f2be75d9dd418647b23
Anxo Rodriguez <anxolin@gmail.com>
2023-10-30 13:19:31 +0000

    feat: reuse .env for all projects and simplify envs further

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,9 @@
+import { config } from 'dotenv'
 import 'jest-styled-components' // include style rules in snapshots
 import fetchMock from 'jest-fetch-mock' // Mocks `fetch` calls in unittests
+
+// For simplicity, we will use CowSwap default .env for all projects and libs
+config({ path: __dirname + '/apps/cowswap-frontend/.env' })
 
 import { Readable } from 'stream'
 import { TextDecoder, TextEncoder } from 'util'
