121ea6d00c62f3f6c0bf9c2228a7eb9945515651
Alfetopito <alfetopito@users.noreply.github.com>
2023-11-01 17:55:33 +0000

    feat: add usePreGeneratedPermitInfoForToken

---
+++ apps/cowswap-frontend/src/modules/permit/hooks/usePreGeneratedPermitInfoForToken.ts
@@ -0,0 +1,25 @@
+import { Currency } from '@uniswap/sdk-core'
+
+import { Nullish } from 'types'
+
+import { usePreGeneratedPermitInfo } from './usePreGeneratedPermitInfo'
+
+import { IsTokenPermittableResult } from '../types'
+
+/**
+ * Fetch pre-generated permit info for a single token
+ */
+export function usePreGeneratedPermitInfoForToken(token: Nullish<Currency>): {
+  permitInfo: IsTokenPermittableResult
+  isLoading: boolean
+} {
+  const { allPermitInfo, isLoading } = usePreGeneratedPermitInfo()
+
+  const permitInfo =
+    allPermitInfo && token && 'address' in token ? allPermitInfo[token.address.toLowerCase()] : undefined
+
+  return {
+    permitInfo,
+    isLoading,
+  }
+}
