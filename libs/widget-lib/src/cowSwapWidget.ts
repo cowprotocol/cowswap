4cf6a2d52ed3f9b31d8e22adb1b412ce14c04ec8
Alexandr Kazachenko <shoom3301@gmail.com>
2023-11-02 15:21:23 +0600

    docs(widget): typedoc for widget lib

--- libs/widget-lib/src/cowSwapWidget.ts
+++ libs/widget-lib/src/cowSwapWidget.ts
@@ -2,10 +2,22 @@ import { JsonRpcManager } from './JsonRpcManager'
 import { CowSwapWidgetMetaData, CowSwapWidgetParams, CowSwapWidgetSettings } from './types'
 import { buildTradeAmountsQuery, buildWidgetPath, buildWidgetUrl } from './urlUtils'
 
+/**
+ * Key for identifying the event associated with the CoW Swap Widget.
+ */
 const COW_SWAP_WIDGET_EVENT_KEY = 'cowSwapWidget'
 
+/**
+ * Callback function signature for updating the CoW Swap Widget.
+ */
 export type UpdateWidgetCallback = (params: CowSwapWidgetSettings) => void
 
+/**
+ * Generates and injects a CoW Swap Widget into the provided container.
+ * @param params - Parameters for configuring the widget.
+ * @param settings - Settings for the CoW Swap Widget.
+ * @returns A callback function to update the widget with new settings.
+ */
 export function cowSwapWidget(params: CowSwapWidgetParams, settings: CowSwapWidgetSettings): UpdateWidgetCallback {
   const { container, provider } = params
   const iframe = createIframe(params, settings)
@@ -34,6 +46,12 @@ export function cowSwapWidget(params: CowSwapWidgetParams, settings: CowSwapWidg
   return (newSettings: CowSwapWidgetSettings) => updateWidget(newSettings, contentWindow, iframe)
 }
 
+/**
+ * Creates an iframe element for the CoW Swap Widget based on provided parameters and settings.
+ * @param params - Parameters for the widget.
+ * @param settings - Settings for the widget.
+ * @returns The generated HTMLIFrameElement.
+ */
 function createIframe(params: CowSwapWidgetParams, settings: CowSwapWidgetSettings): HTMLIFrameElement {
   const { width, height } = params
 
@@ -47,6 +65,12 @@ function createIframe(params: CowSwapWidgetParams, settings: CowSwapWidgetSettin
   return iframe
 }
 
+/**
+ * Updates the CoW Swap Widget based on the new settings provided.
+ * @param settings - New settings for the widget.
+ * @param contentWindow - Window object of the widget's iframe.
+ * @param iframe - The HTMLIFrameElement of the widget.
+ */
 function updateWidget(settings: CowSwapWidgetSettings, contentWindow: Window, iframe: HTMLIFrameElement) {
   const pathname = buildWidgetPath(settings)
   const search = buildTradeAmountsQuery(settings).toString()
@@ -70,6 +94,11 @@ function updateWidget(settings: CowSwapWidgetSettings, contentWindow: Window, if
   )
 }
 
+/**
+ * Sends metadata to the contentWindow of the widget.
+ * @param contentWindow - Window object of the widget's iframe.
+ * @param metaData - Metadata for the widget.
+ */
 function sendMetaData(contentWindow: Window, metaData: CowSwapWidgetMetaData) {
   window.addEventListener('message', (event) => {
     if (event.data.key !== COW_SWAP_WIDGET_EVENT_KEY || event.data.method !== 'activate') {
@@ -87,6 +116,11 @@ function sendMetaData(contentWindow: Window, metaData: CowSwapWidgetMetaData) {
   })
 }
 
+/**
+ * Applies dynamic height adjustments to the widget's iframe.
+ * @param iframe - The HTMLIFrameElement of the widget.
+ * @param defaultHeight - Default height for the widget.
+ */
 function applyDynamicHeight(iframe: HTMLIFrameElement, defaultHeight: number) {
   window.addEventListener('message', (event) => {
     if (event.data.key !== COW_SWAP_WIDGET_EVENT_KEY || event.data.method !== 'iframeHeight') {
--- libs/widget-lib/src/types.ts
+++ libs/widget-lib/src/types.ts
@@ -1,3 +1,5 @@
+import type { SupportedChainId } from '@cowprotocol/cow-sdk'
+
 export interface JsonRpcRequest {
   id: number
   method: string
@@ -6,8 +8,24 @@ export interface JsonRpcRequest {
 
 // https://eips.ethereum.org/EIPS/eip-1193
 export interface EthereumProvider {
+  /**
+   * Subscribes to Ethereum-related events.
+   * @param event - The event to subscribe to.
+   * @param args - Arguments for the event.
+   */
   on(event: string, args: unknown): void
+
+  /**
+   * Sends a JSON-RPC request to the Ethereum provider and returns the response.
+   * @param params - JSON-RPC request parameters.
+   * @returns A promise that resolves with the response.
+   */
   request<T>(params: JsonRpcRequest): Promise<T>
+
+  /**
+   * Requests permission to connect to the Ethereum provider.
+   * @returns A promise that resolves once permission is granted.
+   */
   enable(): Promise<void>
 }
 
@@ -15,8 +33,19 @@ export type CowSwapWidgetEnv = 'local' | 'prod' | 'dev' | 'pr'
 
 export type CowSwapTheme = 'dark' | 'light'
 
+/**
+ *Trade asset parameters, for example:
+ * { asset: 'WBTC', amount: 12 }
+ * or
+ * { asset: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' } // USDC
+ */
 interface TradeAsset {
+  /** The asset symbol or identifier. */
   asset: string
+  /**
+   * The amount of the asset (optional).
+   * If specified, represents the quantity or value of the asset.
+   */
   amount?: string
 }
 
@@ -28,11 +57,15 @@ export interface TradeAssets {
 export enum TradeType {
   SWAP = 'swap',
   LIMIT = 'limit',
+  /**
+   * Currently it means only TWAP orders.
+   * But in the future it can be extended to support other order types.
+   */
   ADVANCED = 'advanced',
 }
 
 export interface CowSwapWidgetUrlParams {
-  chainId?: number
+  chainId?: SupportedChainId
   tradeType?: TradeType
   env?: CowSwapWidgetEnv
   tradeAssets?: TradeAssets
@@ -47,18 +80,43 @@ export interface CowSwapWidgetPalette {
 }
 
 export interface CowSwapWidgetAppParams {
+  /**
+   * Allows to set a custom logo for the widget.
+   */
   logoUrl?: string
+  /**
+   * Option to hide the logo in the widget.
+   */
   hideLogo?: boolean
+  /**
+   * Option to hide the network selector in the widget.
+   */
   hideNetworkSelector?: boolean
+  /**
+   * Enables dynamic height for the widget.
+   * The widget will resize itself to fit the content.
+   */
   dynamicHeightEnabled?: boolean
+  /**
+   * Enables the ability to switch between trade types in the widget.
+   */
   enabledTradeTypes?: TradeType[]
+  /**
+   * Colors palette to customize the widget UI.
+   */
   palette?: CowSwapWidgetPalette
 }
 
 export type CowSwapWidgetSettings = CowSwapWidgetUrlParams & CowSwapWidgetAppParams
 
 export interface CowSwapWidgetMetaData {
+  /**
+   * The unique identifier of the widget consumer.
+   */
   appKey: string
+  /**
+   * The URL of the website that embeds the widget.
+   */
   url: string
 }
 
