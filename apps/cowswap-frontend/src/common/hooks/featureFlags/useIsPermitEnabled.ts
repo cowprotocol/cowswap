aed609707f6932ce3b696b6a9df2c779d14cbd41
Alfetopito <alfetopito@users.noreply.github.com>
2023-11-03 15:05:32 +0000

    refactor: remove unecessary variable

--- apps/cowswap-frontend/src/common/hooks/featureFlags/useIsPermitEnabled.ts
+++ apps/cowswap-frontend/src/common/hooks/featureFlags/useIsPermitEnabled.ts
@@ -1,8 +1,6 @@
 import { useIsSmartContractWallet } from '@cowprotocol/wallet'
 
 export function useIsPermitEnabled(): boolean {
-  const isSmartContractWallet = useIsSmartContractWallet()
-
   // Permit is only available for EOAs
-  return !isSmartContractWallet
+  return !useIsSmartContractWallet()
 }
