ddb3d433f4c2f0f49733bafa1e31c0da289a3a7b
Alexandr Kazachenko <shoom3301@gmail.com>
2023-11-03 14:35:34 +0600

    fix(widget-configurator): bind network selector to wallet provider (#3315)

--- apps/widget-configurator/src/app/configurator/controls/NetworkControl.tsx
+++ apps/widget-configurator/src/app/configurator/controls/NetworkControl.tsx
@@ -1,9 +1,11 @@
-import { ChangeEvent, Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction } from 'react'
 
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
-import Autocomplete from '@mui/material/Autocomplete'
-import TextField from '@mui/material/TextField'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
 
 export type NetworkOption = {
   chainId: SupportedChainId
@@ -16,20 +18,41 @@ export const NetworkOptions: NetworkOption[] = [
   { chainId: SupportedChainId.GOERLI, label: 'Goerli' },
 ]
 
+const DEFAULT_CHAIN_ID = NetworkOptions[0].chainId
+
+const LABEL = 'Network'
+
+export const getNetworkOption = (chainId: SupportedChainId) => NetworkOptions.find((item) => item.chainId === chainId)
+
 export function NetworkControl({ state }: { state: [NetworkOption, Dispatch<SetStateAction<NetworkOption>>] }) {
   const [network, setNetwork] = state
 
+  const switchNetwork = (chainId: number) => {
+    const targetChainId = chainId || DEFAULT_CHAIN_ID
+    const targetNetwork = getNetworkOption(targetChainId)
+
+    if (targetNetwork) {
+      setNetwork(targetNetwork)
+    }
+  }
+
   return (
-    <Autocomplete
-      value={network || NetworkOptions[0]}
-      onChange={(event: ChangeEvent<unknown>, newValue: NetworkOption | null) => {
-        setNetwork(newValue || NetworkOptions[0])
-      }}
-      getOptionLabel={(option: NetworkOption) => option.label}
-      id="controllable-states-network"
-      options={NetworkOptions}
-      size="small"
-      renderInput={(params) => <TextField {...params} label="Network" />}
-    />
+    <FormControl sx={{ width: '100%' }}>
+      <InputLabel>{LABEL}</InputLabel>
+      <Select
+        id="controllable-states-network"
+        value={network?.chainId || DEFAULT_CHAIN_ID}
+        onChange={(event) => switchNetwork(event.target.value as number)}
+        autoWidth
+        label={LABEL}
+        size="small"
+      >
+        {NetworkOptions.map((option) => (
+          <MenuItem key={option.chainId} value={option.chainId}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   )
 }
