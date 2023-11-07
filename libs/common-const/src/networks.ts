ede338d29279654873f0f8677d6e09ca2e41686f
Anxo Rodriguez <anxolin@gmail.com>
2023-11-07 14:58:15 +0000

    fix: fix issue with RPC env (#3353)

--- libs/common-const/src/networks.ts
+++ libs/common-const/src/networks.ts
@@ -3,6 +3,12 @@ import { JsonRpcProvider } from '@ethersproject/providers'
 
 const INFURA_KEY = process.env.REACT_APP_INFURA_KEY || '2af29cd5ac554ae3b8d991afe1ba4b7d' // Default rate-limited infura key (should be overridden, not reliable to use)
 
+const RPC_URL_ENVS: Record<SupportedChainId, string | undefined> = {
+  [SupportedChainId.MAINNET]: process.env.REACT_APP_NETWORK_URL_1 || undefined,
+  [SupportedChainId.GNOSIS_CHAIN]: process.env.REACT_APP_NETWORK_URL_100 || undefined,
+  [SupportedChainId.GOERLI]: process.env.REACT_APP_NETWORK_URL_5 || undefined,
+}
+
 const DEFAULT_RPC_URL: Record<SupportedChainId, { url: string; usesInfura: boolean }> = {
   [SupportedChainId.MAINNET]: { url: `https://mainnet.infura.io/v3/${INFURA_KEY}`, usesInfura: true },
   [SupportedChainId.GNOSIS_CHAIN]: { url: `https://rpc.gnosis.gateway.fm`, usesInfura: false },
@@ -22,7 +28,7 @@ export const MAINNET_PROVIDER = new JsonRpcProvider(RPC_URLS[SupportedChainId.MA
 
 function getRpcUrl(chainId: SupportedChainId): string {
   const envKey = `REACT_APP_NETWORK_URL_${chainId}`
-  const rpcUrl = process.env[envKey]
+  const rpcUrl = RPC_URL_ENVS[chainId]
 
   if (rpcUrl) {
     return rpcUrl
