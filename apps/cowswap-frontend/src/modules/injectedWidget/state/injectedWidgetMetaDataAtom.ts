6e0cd8dc9a50aeba7c7d40cdc4c16175153cb2ab
Anxo Rodriguez <anxolin@gmail.com>
2023-11-08 17:52:47 +0000

    docs: add fixme

--- apps/cowswap-frontend/src/modules/injectedWidget/state/injectedWidgetMetaDataAtom.ts
+++ apps/cowswap-frontend/src/modules/injectedWidget/state/injectedWidgetMetaDataAtom.ts
@@ -5,7 +5,7 @@ export interface CowSwapWidgetMetaData {
 }
 
 const DEFAULT_INJECTED_WIDGET_META_DATA: CowSwapWidgetMetaData = {
-  appCode: 'DEFAULT_INJECTED_WIDGET',
+  appCode: 'DEFAULT_INJECTED_WIDGET', // FIXME: there shouldn't be a default. If it's not set, we should have a banner or something
 }
 
 export const injectedWidgetMetaDataAtom = atom<CowSwapWidgetMetaData>(DEFAULT_INJECTED_WIDGET_META_DATA)
