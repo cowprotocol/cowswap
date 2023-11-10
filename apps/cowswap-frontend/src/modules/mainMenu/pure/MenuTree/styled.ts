a84e24bce9f38b69936572f56b38f2644a058317
Alexandr Kazachenko <shoom3301@gmail.com>
2023-11-11 00:38:21 +0600

    feat(widget): add widget link to main menu (#3381)

---
+++ apps/cowswap-frontend/src/modules/mainMenu/pure/MenuTree/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/macro'
+
+import { UI } from 'common/constants/theme'
+
+export const MenuBadge = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 5px;
+  margin-left: 5px;
+  background: var(${UI.COLOR_ALERT2_BG});
+  color: var(${UI.COLOR_ALERT2_TEXT});
+  border: 0;
+  cursor: pointer;
+  border-radius: 16px;
+  font-size: 9px;
+  text-transform: uppercase;
+  letter-spacing: 0.2px;
+  font-weight: 600;
+  transition: color 0.15s ease-in-out;
+`
