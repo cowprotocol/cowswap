24fbe1563e302419ab759240175a4c15ed34edf8
Alexandr Kazachenko <shoom3301@gmail.com>
2023-11-22 13:02:36 +0600

    chore: merge multicall

--- libs/multicall/src/multicall.ts
+++ libs/multicall/src/multicall.ts
@@ -4,7 +4,7 @@ import type { Web3Provider } from '@ethersproject/providers'
 import { DEFAULT_BATCH_SIZE } from './const'
 import { getMulticallContract } from './utils/getMulticallContract'
 
-export interface MulticallOptions {
+export interface MultiCallOptions {
   batchSize?: number
 }
 
@@ -13,27 +13,32 @@ export interface MulticallOptions {
  * TODO: add fallback for failed calls
  * TODO: add providers fallback
  */
-export async function multicall(
+export async function multiCall(
   provider: Web3Provider,
   calls: Multicall3.CallStruct[],
-  options: MulticallOptions = {}
+  options: MultiCallOptions = {}
 ): Promise<Multicall3.ResultStructOutput[]> {
   const { batchSize = DEFAULT_BATCH_SIZE } = options
 
   const multicall = getMulticallContract(provider)
 
-  const batches = calls.reduce<Multicall3.CallStruct[][]>((acc, call, index) => {
-    const batchIndex = Math.floor(index / batchSize)
-
-    if (!acc[batchIndex]) acc[batchIndex] = []
-
-    acc[batchIndex].push(call)
-    return acc
-  }, [])
+  const batches = splitIntoBatches(calls, batchSize)
 
   const requests = batches.map((batch) => {
     return multicall.callStatic.tryAggregate(false, batch)
   })
 
   return (await Promise.all(requests)).flat()
+}
+
+function splitIntoBatches(calls: Multicall3.CallStruct[], batchSize: number): Multicall3.CallStruct[][] {
+  const results: Multicall3.CallStruct[][] = []
+
+  for (let i = 0; i < calls.length; i += batchSize) {
+    const batch = calls.slice(i, i + batchSize)
+
+    results.push(batch)
+  }
+
+  return results
 }
