1428596de28027ea36edbb8a912e7d27750b70d3
Leandro <alfetopito@users.noreply.github.com>
2024-01-03 03:06:32 -0800

    feat(limit): max limit order deadline (#3567)
    * feat(limit): update max limit order deadline to match backend
    
    * feat(limit): add deadline option for `Max` value
    
    * chore: give 1 more pixel to deadline container to fit a longer label
    
    * feat: update label to `6 Months (max)`

--- apps/cowswap-frontend/src/modules/limitOrders/pure/DeadlineSelector/deadlines.ts
+++ apps/cowswap-frontend/src/modules/limitOrders/pure/DeadlineSelector/deadlines.ts
@@ -6,7 +6,7 @@ export interface LimitOrderDeadline {
 }
 
 export const MIN_CUSTOM_DEADLINE = ms`30min`
-export const MAX_CUSTOM_DEADLINE = Math.round(ms`1y` / 2) // 6 months
+export const MAX_CUSTOM_DEADLINE = ms`182d` + ms`12h` // 6 months, matching backend's https://github.com/cowprotocol/infrastructure/blob/901ed8e2fe3ea57956585f107bdd7539c2e7d3d1/services/Pulumi.yaml#L15
 
 export const defaultLimitOrderDeadline: LimitOrderDeadline = { title: '7 Days', value: ms`7d` }
 
@@ -18,4 +18,5 @@ export const limitOrdersDeadlines: LimitOrderDeadline[] = [
   { title: '3 Days', value: ms`3d` },
   defaultLimitOrderDeadline,
   { title: '1 Month', value: ms`30d` },
+  { title: '6 Months (max)', value: MAX_CUSTOM_DEADLINE },
 ]
