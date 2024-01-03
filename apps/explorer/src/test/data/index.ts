4cbf98abddf0d4af14578a94e8606438956a4bee
Alexandr Kazachenko <shoom3301@gmail.com>
2024-01-03 13:50:58 +0600

    chore: copy-paste test files

---
+++ apps/explorer/src/test/data/index.ts
@@ -0,0 +1,7 @@
+export * from './basic'
+
+export { default as erc20Allowances } from './erc20Allowances'
+export { default as erc20Balances } from './erc20Balances'
+export { default as unregisteredTokens } from './unregisteredTokens'
+export * from './operator'
+export * from './erc20s'
---
+++ apps/explorer/src/test/globalSetup.ts
@@ -0,0 +1,11 @@
+module.exports = (): void => {
+  process.env.MOCK_WALLET =
+    process.env.MOCK_TOKEN_LIST =
+    process.env.MOCK_ERC20 =
+    process.env.MOCK_WETH =
+    process.env.MOCK_DEPOSIT =
+    process.env.MOCK_EXCHANGE =
+    process.env.MOCK_WEB3 =
+    process.env.MOCK_OPERATOR =
+      process.env.MOCK
+}
