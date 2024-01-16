d9807f6dba15b8cd2a8303f01ed999a2d09fc8da
Alfetopito <alfetopito@users.noreply.github.com>
2024-01-16 17:02:39 +0000

    chore: remove deprecated appzi stuff

--- libs/common-utils/src/appzi.ts
+++ libs/common-utils/src/appzi.ts
@@ -1,7 +1,5 @@
 import ms from 'ms.macro'
 import ReactAppzi from 'react-appzi'
-
-import EventEmitter from 'events'
 import { isImTokenBrowser, majorBrowserVersion, userAgent } from './userAgent'
 import { environmentName, isProdLike } from './environments'
 
@@ -24,11 +22,8 @@ export const FEEDBACK_KEY =
 export const NPS_KEY = process.env.REACT_APP_APPZI_NPS_KEY || isProdLike ? PROD_NPS_KEY : TEST_NPS_KEY
 
 const APPZI_TOKEN = process.env.REACT_APP_APPZI_TOKEN || '5ju0G'
-const EVENT = 'message'
 
 const PENDING_TOO_LONG_TIME = ms`5 min`
-
-type EventCallback = (event: any) => void
 
 declare global {
   interface Window {
@@ -38,7 +33,6 @@ declare global {
     appziSettings: {
       userId: string
       data: AppziCustomSettings
-      onEvent: EventCallback
     }
   }
 }
@@ -63,35 +57,15 @@ type AppziSettings = {
   data?: Partial<AppziCustomSettings>
 }
 
-const eventEmitter = new EventEmitter()
-
-export function onAppziEvent(callback: EventCallback) {
-  eventEmitter.on(EVENT, callback)
-}
-
-export function onceAppziEvent(callback: EventCallback) {
-  eventEmitter.once(EVENT, callback)
-}
-
-export function offAppziEvent(callback: EventCallback) {
-  eventEmitter.off(EVENT, callback)
-}
-
-function triggerAppziEvent(payload: any) {
-  // console.log('[appzi] Event', payload)
-  eventEmitter.emit(EVENT, payload)
-}
-
 function initialize() {
   if (isAppziEnabled) {
     ReactAppzi.initialize(APPZI_TOKEN)
 
     window.appziSettings = window.appziSettings || {}
-    window.appziSettings.onEvent = triggerAppziEvent
   }
 }
 
-export function updateAppziSettings({ data = {}, userId = '' }: AppziSettings) {
+function updateAppziSettings({ data = {}, userId = '' }: AppziSettings) {
   window.appziSettings = { ...(window.appziSettings || {}), data, userId }
 }
 
@@ -99,28 +73,7 @@ export function openFeedbackAppzi() {
   window.appzi?.openWidget(FEEDBACK_KEY)
 }
 
-let appziRestyleApplied = false
-function applyOnceRestyleAppziNps() {
-  if (!appziRestyleApplied) {
-    appziRestyleApplied = true
-    // Make sure we apply the re-style once the appzi NPS is loaded
-    onceAppziEvent(restyleAppziNps)
-  }
-}
-
-function restyleAppziNps(event: any) {
-  if (event && event.type === 'open-survey') {
-    // Add a unique class based on NPS_KEY
-    const appziRoot = document.querySelector("div[id^='appzi-wfo-']")
-    if (appziRoot) {
-      appziRoot.classList.add(`appzi-nps-${NPS_KEY}`)
-      appziRestyleApplied = true
-    }
-  }
-}
-
 export function openNpsAppzi() {
-  applyOnceRestyleAppziNps()
   window.appzi?.openWidget(NPS_KEY)
 }
 
@@ -141,10 +94,8 @@ const NPS_DATA = isProdLike ? PROD_NPS_DATA : TEST_NPS_DATA
 /**
  * Opening of the modal is delegated to Appzi
  * It'll display only if the trigger rules are met
- * Check https://portal.appzi.com/portals/5ju0G/configs/55872789-593b-4c6c-9e49-9b5c7693e90a/trigger
  */
 export function openNpsAppziSometimes(data?: Omit<AppziCustomSettings, 'userTradedOrWaitedForLong' | 'isTestNps'>) {
-  applyOnceRestyleAppziNps()
   updateAppziSettings({ data: { env: environmentName, ...data, ...NPS_DATA } })
 }
 
