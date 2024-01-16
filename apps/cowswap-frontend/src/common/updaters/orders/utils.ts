9a0619cc72e9cc312edee82faadd423c2efa35ae
Alfetopito <alfetopito@users.noreply.github.com>
2024-01-16 13:23:29 +0000

    Revert "fix: use sellAmountBefore fee for notification summary on all order types"
    This reverts commit 0e2c9d0e8b49bb06fa1e03807b71f6c6b240e317.

--- apps/cowswap-frontend/src/common/updaters/orders/utils.ts
+++ apps/cowswap-frontend/src/common/updaters/orders/utils.ts
@@ -1,5 +1,6 @@
 import { formatSymbol, formatTokenAmount, shortenAddress } from '@cowprotocol/common-utils'
-import { EnrichedOrder, OrderKind, SupportedChainId as ChainId } from '@cowprotocol/cow-sdk'
+import { SupportedChainId as ChainId } from '@cowprotocol/cow-sdk'
+import { EnrichedOrder, OrderKind } from '@cowprotocol/cow-sdk'
 
 import { Order, OrderFulfillmentData, OrderStatus } from 'legacy/state/orders/actions'
 import { classifyOrder, OrderTransitionStatus } from 'legacy/state/orders/utils'
@@ -25,10 +26,20 @@ export function computeOrderSummary({
   // if we can find the order from the API
   // and our specific order exists in our state, let's use that
   if (orderFromApi) {
-    const { buyToken, sellToken, buyAmount, executedBuyAmount, executedSellAmount, owner, receiver } = orderFromApi
+    const {
+      buyToken,
+      sellToken,
+      sellAmount,
+      feeAmount,
+      buyAmount,
+      executedBuyAmount,
+      executedSellAmount,
+      owner,
+      receiver,
+    } = orderFromApi
 
     if (orderFromStore) {
-      const { inputToken, outputToken, status, kind, sellAmountBeforeFee } = orderFromStore
+      const { inputToken, outputToken, status, kind } = orderFromStore
       const isFulfilled = status === OrderStatus.FULFILLED
 
       if (!inputToken || !outputToken) return undefined
@@ -36,7 +47,8 @@ export function computeOrderSummary({
       // don't show amounts in atoms
       const inputAmount = isFulfilled
         ? stringToCurrency(executedSellAmount, inputToken)
-        : stringToCurrency(sellAmountBeforeFee as string, inputToken)
+        : // sellAmount doesn't include the fee, so we add it back to not show a different value when the order is traded
+          stringToCurrency(sellAmount, inputToken).add(stringToCurrency(feeAmount, inputToken))
       const outputAmount = stringToCurrency(isFulfilled ? executedBuyAmount : buyAmount, outputToken)
 
       const inputPrefix = !isFulfilled && kind === OrderKind.BUY ? 'at most ' : ''
