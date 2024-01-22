1cca5e004c83a2b397664f3c4d9beb24bb068bbe
Alfetopito <alfetopito@users.noreply.github.com>
2024-01-22 16:03:54 +0000

    refactor: remove migration code

--- apps/cowswap-frontend/src/modules/permit/hooks/usePreGeneratedPermitInfo.ts
+++ apps/cowswap-frontend/src/modules/permit/hooks/usePreGeneratedPermitInfo.ts
@@ -12,7 +12,7 @@ import { PRE_GENERATED_PERMIT_URL } from '../const'
  * Caches result with SWR until a page refresh
  */
 export function usePreGeneratedPermitInfo(): {
-  allPermitInfo: Record<string, PermitInfo | undefined>
+  allPermitInfo: Record<string, PermitInfo>
   isLoading: boolean
 } {
   const { chainId } = useWalletInfo()
@@ -21,33 +21,9 @@ export function usePreGeneratedPermitInfo(): {
 
   const { data, isLoading } = useSWR(
     url,
-    (url: string): Promise<Record<string, PermitInfo | undefined>> =>
-      fetch(url)
-        .then((r) => r.json())
-        .then(migrateData),
+    (url: string): Promise<Record<string, PermitInfo | undefined>> => fetch(url).then((r) => r.json()),
     { ...SWR_NO_REFRESH_OPTIONS, fallbackData: {} }
   )
 
   return { allPermitInfo: data, isLoading }
-}
-
-type OldPermitInfo = PermitInfo | false
-
-const UNSUPPORTED: PermitInfo = { type: 'unsupported' }
-
-/**
- * Handles data migration from former way of storing unsupported tokens to the new one
- */
-function migrateData(data: Record<string, OldPermitInfo>): Record<string, PermitInfo> {
-  const migrated: Record<string, PermitInfo> = {}
-
-  for (const [k, v] of Object.entries(data)) {
-    if (v === false) {
-      migrated[k] = UNSUPPORTED
-    } else {
-      migrated[k] = v
-    }
-  }
-
-  return migrated
 }
