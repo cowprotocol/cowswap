89fe6e1ab6ab157332d4b121749bfb63a4899e8a
Alexandr Kazachenko <shoom3301@gmail.com>
2024-01-30 17:40:43 +0600

    chore: fix PendingOrderNotification cosmos

--- apps/cowswap-frontend/src/common/containers/PendingOrderNotification/index.cosmos.tsx
+++ apps/cowswap-frontend/src/common/containers/PendingOrderNotification/index.cosmos.tsx
@@ -2,33 +2,55 @@ import { USDC_MAINNET, WBTC } from '@cowprotocol/common-const'
 import { OrderKind } from '@cowprotocol/cow-sdk'
 import { CurrencyAmount } from '@uniswap/sdk-core'
 
+import { useSelect, useValue } from 'react-cosmos/client'
 import styled from 'styled-components/macro'
 
 import { UiOrderType } from 'utils/orderUtils/getUiOrderType'
 
 import { PendingOrderNotification } from './index'
 
-const defaultProps = {
-  orderId:
-    '0x62baf4be8adec4766d26a2169999cc170c3ead90ae11a28d658e6d75edc05b185b0abe214ab7875562adee331deff0fe1912fe42644d2bb7',
-  kind: OrderKind.SELL,
-  orderType: UiOrderType.SWAP,
-  inputAmount: CurrencyAmount.fromRawAmount(USDC_MAINNET, 500_000 * 10 ** USDC_MAINNET.decimals),
-  outputAmount: CurrencyAmount.fromRawAmount(WBTC, 1.2 * 10 ** WBTC.decimals),
-  recipient: '0xfb3c7eb936cAA12B5A884d612393969A557d4307',
-}
+const orderId =
+  '0x62baf4be8adec4766d26a2169999cc170c3ead90ae11a28d658e6d75edc05b185b0abe214ab7875562adee331deff0fe1912fe42644d2bb7'
 
 const Wrapper = styled.div`
   width: 560px;
   margin: 0 auto;
 `
 
-const Fixtures = {
-  default: (
+function Custom({ orderType }: { orderType: UiOrderType }) {
+  const [kind] = useSelect('kind', {
+    options: [OrderKind.SELL, OrderKind.BUY],
+    defaultValue: OrderKind.SELL,
+  })
+
+  const [receiver] = useValue('receiver', { defaultValue: '0xfb3c7eb936cAA12B5A884d612393969A557d4307' })
+  const [inputAmountRaw] = useValue('inputAmount', { defaultValue: '500000' })
+  const [outputAmountRaw] = useValue('outputAmount', { defaultValue: '1.2' })
+
+  const inputAmount = CurrencyAmount.fromRawAmount(
+    USDC_MAINNET,
+    parseFloat(inputAmountRaw) * 10 ** USDC_MAINNET.decimals
+  )
+  const outputAmount = CurrencyAmount.fromRawAmount(WBTC, parseFloat(outputAmountRaw) * 10 ** WBTC.decimals)
+
+  return (
     <Wrapper>
-      <PendingOrderNotification {...defaultProps} />
+      <PendingOrderNotification
+        orderId={orderId}
+        kind={kind}
+        receiver={receiver}
+        orderType={orderType}
+        inputAmount={inputAmount}
+        outputAmount={outputAmount}
+      />
     </Wrapper>
-  ),
+  )
+}
+
+const Fixtures = {
+  swap: <Custom orderType={UiOrderType.SWAP} />,
+  limit: <Custom orderType={UiOrderType.LIMIT} />,
+  twap: <Custom orderType={UiOrderType.TWAP} />,
 }
 
 export default Fixtures
