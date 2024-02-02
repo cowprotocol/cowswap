2faf42eff2d33a1c78a023a43afaf8bb4e70a3ea
Alexandr Kazachenko <shoom3301@gmail.com>
2024-02-02 14:14:12 +0600

    fix(notification): display order type accordingly to the order (#3731)

---
+++ apps/cowswap-frontend/src/common/containers/PendingOrderNotification/index.cosmos.tsx
@@ -0,0 +1,85 @@
+import { useSetAtom } from 'jotai'
+import { useEffect } from 'react'
+
+import { USDC_MAINNET, WBTC } from '@cowprotocol/common-const'
+import { OrderKind } from '@cowprotocol/cow-sdk'
+import { SnackbarPopup } from '@cowprotocol/snackbars'
+import { GnosisSafeInfo, gnosisSafeInfoAtom } from '@cowprotocol/wallet'
+import { CurrencyAmount } from '@uniswap/sdk-core'
+
+import { useSelect, useValue } from 'react-cosmos/client'
+import { CheckCircle } from 'react-feather'
+import styled from 'styled-components/macro'
+
+import { UiOrderType } from 'utils/orderUtils/getUiOrderType'
+
+import { PendingOrderNotification } from './index'
+
+const SuccessIcon = styled(CheckCircle)`
+  color: ${({ theme }) => theme.green1};
+`
+
+const defaultOrderId =
+  '0x62baf4be8adec4766d26a2169999cc170c3ead90ae11a28d658e6d75edc05b185b0abe214ab7875562adee331deff0fe1912fe42644d2bb7'
+
+const Wrapper = styled.div`
+  width: 560px;
+  margin: 0 auto;
+`
+
+function Custom({ orderType, orderId }: { orderType: UiOrderType; orderId: string }) {
+  const setGnosisSafeInfo = useSetAtom(gnosisSafeInfoAtom)
+
+  const [kind] = useSelect('kind', {
+    options: [OrderKind.SELL, OrderKind.BUY],
+    defaultValue: OrderKind.SELL,
+  })
+
+  const [receiver] = useValue('receiver', { defaultValue: '0xfb3c7eb936cAA12B5A884d612393969A557d4307' })
+  const [inputAmountRaw] = useValue('inputAmount', { defaultValue: '500000' })
+  const [outputAmountRaw] = useValue('outputAmount', { defaultValue: '1.2' })
+
+  const inputAmount = CurrencyAmount.fromRawAmount(
+    USDC_MAINNET,
+    parseFloat(inputAmountRaw) * 10 ** USDC_MAINNET.decimals
+  )
+  const outputAmount = CurrencyAmount.fromRawAmount(WBTC, parseFloat(outputAmountRaw) * 10 ** WBTC.decimals)
+
+  useEffect(() => {
+    if (orderType === UiOrderType.TWAP) {
+      setGnosisSafeInfo({} as unknown as GnosisSafeInfo)
+    } else {
+      setGnosisSafeInfo(undefined)
+    }
+  }, [orderType, setGnosisSafeInfo])
+
+  return (
+    <Wrapper>
+      <SnackbarPopup
+        id="PendingOrderNotification"
+        icon={<SuccessIcon size={24} />}
+        duration={100_000}
+        onExpire={() => console.log('expire')}
+      >
+        <PendingOrderNotification
+          orderId={orderId}
+          kind={kind}
+          receiver={receiver}
+          orderType={orderType}
+          inputAmount={inputAmount}
+          outputAmount={outputAmount}
+        />
+      </SnackbarPopup>
+    </Wrapper>
+  )
+}
+
+const Fixtures = {
+  swap: <Custom orderType={UiOrderType.SWAP} orderId={defaultOrderId} />,
+  limit: <Custom orderType={UiOrderType.LIMIT} orderId={defaultOrderId} />,
+  twap: (
+    <Custom orderType={UiOrderType.TWAP} orderId="0xc554e6c5612af4796c3c5cd817cea13f012f0807c9ce5e18cdf51e911701eeff" />
+  ),
+}
+
+export default Fixtures
