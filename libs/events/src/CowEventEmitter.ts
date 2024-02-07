edadfbc5ff6dedbb976c2eecfdd7c688969bedf1
Anxo Rodriguez <anxolin@gmail.com>
2024-02-07 19:07:54 +0000

    feat: post event messages to the iFrame

--- libs/events/src/CowEventEmitter.ts
+++ libs/events/src/CowEventEmitter.ts
@@ -10,8 +10,8 @@ export type CowEventListeners = CowEventListener<CowEvents>[]
 
 export interface CowEventEmitter {
   on<T extends CowEvents>(listener: CowEventListener<T>): void
+  off<T extends CowEvents>(listener: CowEventListener<T>): void
   emit<T extends CowEvents>(event: T, payload: CowEventPayloads[T]): void
-  off<T extends CowEvents>(event: T, listenerToRemove: CowEventHandler<T>): void
 }
 
 export class CowEventEmitterImpl implements CowEventEmitter {
@@ -31,17 +31,18 @@ export class CowEventEmitterImpl implements CowEventEmitter {
     this.events[event].push(handler)
   }
 
+  off<T extends CowEvents>(listener: CowEventListener<T>): void {
+    const { event, handler } = listener
+    if (this.events[event]) {
+      this.events[event] = this.events[event].filter((listener) => listener !== handler)
+    }
+  }
+
   emit<T extends keyof CowEventPayloads>(event: T, payload: CowEventPayloads[T]): void {
     if (this.events[event]) {
       this.events[event].forEach((handler) => {
         handler(payload)
       })
-    }
-  }
-
-  off<T extends CowEvents>(event: T, listenerToRemove: CowEventHandler<T>): void {
-    if (this.events[event]) {
-      this.events[event] = this.events[event].filter((listener) => listener !== listenerToRemove)
     }
   }
 }
