f01ac4945dabcfaf7d919e46169812575f843b16
Anxo Rodriguez <anxolin@gmail.com>
2024-02-07 15:54:32 +0000

    feat: add cowEventEmitter provider into cowswap

---
+++ apps/cowswap-frontend/src/common/hooks/useCowEventEmitter.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+
+import { CowEventEmitter, CowEventEmitterContext } from '@cowprotocol/events'
+
+export function useCowEventEmitter(): CowEventEmitter {
+  const eventEmitter = useContext(CowEventEmitterContext)
+
+  if (!eventEmitter) {
+    throw new Error('useCowEventEmitter is not in the context. Please use CowEventEmitterProvider')
+  }
+
+  return eventEmitter
+}
