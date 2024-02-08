10ed0df99c695df88249d4b82decf98f8e3fb844
Alfetopito <alfetopito@users.noreply.github.com>
2024-02-08 11:34:06 +0000

    chore: temporarily disable ab testing for fee=0

--- apps/cowswap-frontend/src/common/hooks/featureFlags/useSwapZeroFee.ts
+++ apps/cowswap-frontend/src/common/hooks/featureFlags/useSwapZeroFee.ts
@@ -1,21 +1,7 @@
-import { useWalletInfo } from '@cowprotocol/wallet'
-
 import { useFeatureFlags } from './useFeatureFlags'
 
-// Expose the feature to XX% of users
-const PERCENTAGE_OF_USERS_WITH_FLAG = 30 // XX% of users
-
 export function useSwapZeroFee(): boolean {
-  const { account } = useWalletInfo()
   const { swapZeroFee } = useFeatureFlags()
 
-  return enabledForUser(account) && swapZeroFee
-}
-
-function enabledForUser(account: string | undefined): boolean {
-  if (!account) {
-    return false
-  }
-
-  return parseInt(account.toLowerCase(), 0) % 100 < PERCENTAGE_OF_USERS_WITH_FLAG
+  return swapZeroFee
 }
