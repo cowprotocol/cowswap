32a431e6c6072817ae22ee4fb1ad3cb981f2922a
Alfetopito <alfetopito@users.noreply.github.com>
2024-02-08 18:05:03 +0000

    feat: allow to set both sellAmount and buyAmount at the same time

--- apps/cowswap-frontend/src/modules/trade/utils/parameterizeTradeSearch.ts
+++ apps/cowswap-frontend/src/modules/trade/utils/parameterizeTradeSearch.ts
@@ -3,8 +3,10 @@ import { OrderKind } from '@cowprotocol/cow-sdk'
 import { TRADE_URL_BUY_AMOUNT_KEY, TRADE_URL_SELL_AMOUNT_KEY } from '../const/tradeUrl'
 
 export type TradeSearchParams = {
-  amount: string | undefined
-  kind: OrderKind
+  amount?: string | undefined
+  kind?: OrderKind
+  sellAmount?: string | undefined
+  buyAmount?: string | undefined
 }
 
 /**
@@ -15,13 +17,20 @@ export type TradeSearchParams = {
 export function parameterizeTradeSearch(search: string, searchParamsToAdd?: TradeSearchParams): string {
   const searchParams = new URLSearchParams(search)
 
-  const amountQueryKey = searchParamsToAdd
-    ? searchParamsToAdd.kind === OrderKind.SELL
-      ? TRADE_URL_SELL_AMOUNT_KEY
-      : TRADE_URL_BUY_AMOUNT_KEY
-    : undefined
+  // TODO: think about how to deal with buy limit orders
 
-  searchParamsToAdd?.amount && amountQueryKey && searchParams.set(amountQueryKey, searchParamsToAdd.amount)
+  if (searchParamsToAdd?.amount) {
+    const amountQueryKey =
+      searchParamsToAdd.kind === OrderKind.SELL ? TRADE_URL_SELL_AMOUNT_KEY : TRADE_URL_BUY_AMOUNT_KEY
+    searchParams.set(amountQueryKey, searchParamsToAdd.amount)
+  } else {
+    if (searchParamsToAdd?.sellAmount) {
+      searchParams.set(TRADE_URL_SELL_AMOUNT_KEY, searchParamsToAdd.sellAmount)
+    }
+    if (searchParamsToAdd?.buyAmount) {
+      searchParams.set(TRADE_URL_BUY_AMOUNT_KEY, searchParamsToAdd.buyAmount)
+    }
+  }
 
   return searchParams.toString()
 }
