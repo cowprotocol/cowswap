d9d1f23c1bf90a277a0d559c019b9794e43d8162
Anxo Rodriguez <anxolin@gmail.com>
2024-02-13 14:25:46 +0000

    feat(widget): create an iframe event emitter (#3821)

---
+++ libs/widget-lib/src/IframeCowEventEmitter.ts
@@ -0,0 +1,42 @@
+import { CowEventEmitterImpl, CowEventListener, CowEventListeners, CowEvents } from '@cowprotocol/events'
+const COW_SWAP_WIDGET_EVENT_KEY = 'cowSwapWidget'
+
+export class IframeCowEventEmitter {
+  private listeners: CowEventListeners
+  private eventEmitter: CowEventEmitterImpl
+
+  constructor(listeners?: CowEventListeners) {
+    this.eventEmitter = new CowEventEmitterImpl()
+    this.listeners = listeners ?? []
+
+    // Subscribe to events
+    this.updateListeners(listeners)
+
+    // Forward messages to the event emitter
+    window.addEventListener('message', this.forwardEvents)
+  }
+
+  public updateListeners(listeners?: CowEventListeners): void {
+    // Unsubscribe from previous listeners
+    for (const listener of this.listeners) {
+      this.eventEmitter.off(listener as CowEventListener<CowEvents>)
+    }
+
+    // Subscribe to events
+    this.listeners = listeners ?? []
+    for (const listener of this.listeners) {
+      this.eventEmitter.on(listener as CowEventListener<CowEvents>)
+    }
+  }
+
+  private forwardEvents = (event: MessageEvent): void => {
+    if (event.data.key !== COW_SWAP_WIDGET_EVENT_KEY || event.data.method !== 'event') {
+      return
+    }
+    console.debug(
+      `[TODO:remove] Received message client side - Forward to eventEmitter ${event.data.eventName}`,
+      event.data.payload
+    )
+    this.eventEmitter.emit(event.data.eventName, event.data.payload)
+  }
+}
