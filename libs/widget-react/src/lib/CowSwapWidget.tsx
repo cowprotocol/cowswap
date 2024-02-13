2f81380d3e2a49a63c9da4e5fedb11e378fb3c8c
Anxo Rodriguez <anxolin@gmail.com>
2024-02-13 14:59:50 +0000

    fix(widget): solve issues arising when changing the provider (#3825)

--- libs/widget-react/src/lib/CowSwapWidget.tsx
+++ libs/widget-react/src/lib/CowSwapWidget.tsx
@@ -74,8 +74,19 @@ export function CowSwapWidget({ params, provider, listeners }: CowSwapWidgetProp
       return
     }
 
-    const handler = widgetHandlerRef.current
-    tryOrHandleError('Updating the provider', () => handler.updateProvider(provider))
+    // TODO: Fix this https://github.com/cowprotocol/cowswap/issues/3810#issue-2127257473 (in meantime forcing full refresh as before)
+    // const handler = widgetHandlerRef.current
+    // tryOrHandleError('Updating the provider', () => handler.updateProvider(provider))
+    const container = containerRef.current
+    if (container) {
+      tryOrHandleError('Updating the provider', () => {
+        widgetHandlerRef.current = createCowSwapWidget(
+          container,
+          { ...params, provider: providerRef.current ?? undefined }, // Override params to add the provider
+          listeners
+        )
+      })
+    }
   }, [provider])
 
   useEffect(() => {
