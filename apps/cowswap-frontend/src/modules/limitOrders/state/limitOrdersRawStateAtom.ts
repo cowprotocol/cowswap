55ec35f91acd7116f84c59738e9e5dbd0d11a9ce
Alfetopito <alfetopito@users.noreply.github.com>
2024-02-14 16:41:52 +0000

    feat: use alternative limit order atoms when flag is set

--- apps/cowswap-frontend/src/modules/limitOrders/state/limitOrdersRawStateAtom.ts
+++ apps/cowswap-frontend/src/modules/limitOrders/state/limitOrdersRawStateAtom.ts
@@ -1,4 +1,4 @@
-import { atom } from 'jotai'
+import { atom, Getter, PrimitiveAtom, Setter, WritableAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
 import { getJotaiIsolatedStorage } from '@cowprotocol/core'
@@ -6,6 +6,8 @@ import { OrderKind, SupportedChainId } from '@cowprotocol/cow-sdk'
 
 import { DEFAULT_TRADE_DERIVED_STATE, TradeDerivedState } from 'modules/trade/types/TradeDerivedState'
 import { ExtendedTradeRawState, getDefaultTradeRawState } from 'modules/trade/types/TradeRawState'
+
+import { isAlternativeOrderModalVisibleAtom } from 'common/state/alternativeOrder'
 
 export interface LimitOrdersDerivedState extends TradeDerivedState {
   readonly isUnlocked: boolean
@@ -25,21 +27,107 @@ export function getDefaultLimitOrdersState(chainId: SupportedChainId | null): Li
   }
 }
 
-export const limitOrdersRawStateAtom = atomWithStorage<LimitOrdersRawState>(
+// Regular form state
+
+const regularLimitOrdersRawStateAtom = atomWithStorage<LimitOrdersRawState>(
   'limit-orders-atom:v4',
   getDefaultLimitOrdersState(null),
   getJotaiIsolatedStorage()
 )
 
-export const updateLimitOrdersRawStateAtom = atom(null, (get, set, nextState: Partial<LimitOrdersRawState>) => {
-  set(limitOrdersRawStateAtom, () => {
-    const prevState = get(limitOrdersRawStateAtom)
+const regularUpdateLimitOrdersRawStateAtom = atom(
+  null,
+  updaterAtomFactory<typeof regularLimitOrdersRawStateAtom, LimitOrdersRawState>(regularLimitOrdersRawStateAtom)
+)
 
-    return { ...prevState, ...nextState }
-  })
-})
-
-export const limitOrdersDerivedStateAtom = atom<LimitOrdersDerivedState>({
+const regularLimitOrdersDerivedStateAtom = atom<LimitOrdersDerivedState>({
   ...DEFAULT_TRADE_DERIVED_STATE,
   isUnlocked: true,
 })
+
+// Alternative state for recreating/editing existing orders
+
+const alternativeLimitOrdersRawStateAtom = atom<LimitOrdersRawState>(getDefaultLimitOrdersState(null))
+
+const alternativeUpdateLimitOrdersRawStateAtom = atom(
+  null,
+  updaterAtomFactory<typeof alternativeLimitOrdersRawStateAtom, LimitOrdersRawState>(alternativeLimitOrdersRawStateAtom)
+)
+
+const alternativeLimitOrdersDerivedStateAtom = atom<LimitOrdersDerivedState>({
+  ...DEFAULT_TRADE_DERIVED_STATE,
+  isUnlocked: true,
+})
+
+// Pick atom according to type of form user
+
+export const limitOrdersRawStateAtom = atom(
+  readFactory<LimitOrdersRawState, typeof regularLimitOrdersRawStateAtom, typeof alternativeLimitOrdersRawStateAtom>(
+    regularLimitOrdersRawStateAtom,
+    alternativeLimitOrdersRawStateAtom
+  ),
+  writeFactory<
+    LimitOrdersRawState,
+    LimitOrdersRawState,
+    typeof regularLimitOrdersRawStateAtom,
+    typeof alternativeLimitOrdersRawStateAtom
+  >(regularLimitOrdersRawStateAtom, alternativeLimitOrdersRawStateAtom)
+)
+
+export const updateLimitOrdersRawStateAtom = atom(
+  null,
+  writeFactory<
+    null,
+    LimitOrdersRawState,
+    typeof regularUpdateLimitOrdersRawStateAtom,
+    typeof alternativeUpdateLimitOrdersRawStateAtom
+  >(regularUpdateLimitOrdersRawStateAtom, alternativeUpdateLimitOrdersRawStateAtom)
+)
+
+export const limitOrdersDerivedStateAtom = atom(
+  readFactory<
+    LimitOrdersDerivedState,
+    typeof regularLimitOrdersDerivedStateAtom,
+    typeof alternativeLimitOrdersDerivedStateAtom
+  >(regularLimitOrdersDerivedStateAtom, alternativeLimitOrdersDerivedStateAtom),
+  writeFactory<
+    LimitOrdersDerivedState,
+    LimitOrdersDerivedState,
+    typeof regularLimitOrdersDerivedStateAtom,
+    typeof alternativeLimitOrdersDerivedStateAtom
+  >(regularLimitOrdersDerivedStateAtom, alternativeLimitOrdersDerivedStateAtom)
+)
+
+// utils
+function updaterAtomFactory<
+  AtomType extends WritableAtom<AtomWriterParamType, [value: AtomWriterParamType], void>,
+  AtomWriterParamType extends object
+>(primitiveAtom: AtomType) {
+  return (get: Getter, set: Setter, nextState: Partial<AtomWriterParamType>) => {
+    const prevState = get(primitiveAtom)
+    set(primitiveAtom, { ...prevState, ...nextState })
+  }
+}
+
+function readFactory<
+  AtomType,
+  RegularAtom extends PrimitiveAtom<AtomType>,
+  AlternativeAtom extends PrimitiveAtom<AtomType>
+>(regular: RegularAtom, alternative: AlternativeAtom) {
+  return (get: Getter) => get(get(isAlternativeOrderModalVisibleAtom) ? alternative : regular)
+}
+
+function writeFactory<
+  AtomGetterType,
+  AtomWriterParamType,
+  RegularAtom extends WritableAtom<AtomGetterType, [value: AtomWriterParamType], void>,
+  AlternativeAtom extends WritableAtom<AtomGetterType, [value: AtomWriterParamType], void>
+>(regular: RegularAtom, alternative: AlternativeAtom) {
+  return (get: Getter, set: Setter, value: AtomWriterParamType) => {
+    if (get(isAlternativeOrderModalVisibleAtom)) {
+      set(alternative, value)
+    } else {
+      set(regular, value)
+    }
+  }
+}
