65e26ee4173b474db91ab4213ad0a7bc2ee9a0a3
Anxo Rodriguez <anxolin@gmail.com>
2024-02-16 20:33:17 +0000

    chore: bring code from provider

--- libs/wallet/src/web3-react/connectors/Injected/IFrameEthereumProvider.ts
+++ libs/wallet/src/web3-react/connectors/Injected/IFrameEthereumProvider.ts
@@ -1,8 +1,10 @@
 // eslint-disable-next-line no-restricted-imports
-import { IFrameEthereumProvider as OriginalIFrameEthereumProvider } from '@ethvault/iframe-provider/dist/index'
+
+import { IFrameEthereumProvider as OriginalIFrameEthereumProvider } from './OriginalIframeEthereumProvider'
 
 export class IFrameEthereumProvider extends OriginalIFrameEthereumProvider {
   request<T>({ method, params }: { method: string; params?: any[] }) {
+    // console.log('[TEST:IFrameEthereumProvider] IFrameEthereumProvider - rpc request', { method, params })
     return this.send<typeof params, T>(method, params)
   }
 
