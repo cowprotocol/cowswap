0decc07b41a261947ebae1f105ca81ef06db9e27
fairlight <31534717+fairlighteth@users.noreply.github.com>
2024-02-21 11:31:22 +0000

    General style fixes (#3883)

--- apps/cowswap-frontend/src/legacy/theme/baseTheme.tsx
+++ apps/cowswap-frontend/src/legacy/theme/baseTheme.tsx
@@ -459,6 +459,10 @@ export const UniThemedGlobalStyle = css`
     ${UI.COLOR_SECONDARY_TEXT}: ${({ theme }) =>
       theme.darkMode ? transparentize(theme.text, 0.6) : transparentize(theme.text, 0.5)};
 
+    ${UI.COLOR_DARK_IMAGE_PAPER}: ${({ theme }) => getContrastText('#000000', theme.paper)};
+    ${UI.COLOR_DARK_IMAGE_PAPER_TEXT}: ${({ theme }) =>
+      getContrastText(getContrastText('#000000', theme.paper), theme.text)};
+
     ${UI.COLOR_BUTTON_TEXT}: ${({ theme }) => getContrastText(theme.primary, theme.text)};
     ${UI.COLOR_BUTTON_TEXT_DISABLED}: ${({ theme }) =>
       getContrastText(theme.darkMode ? darken(theme.paper, 0.07) : darken(theme.paper, 0.05), theme.text)};
