97270db22de2b62796622560d18da480e2cf8332
Alexandr Kazachenko <shoom3301@gmail.com>
2024-02-21 19:08:18 +0600

    fix: fix order cancellation summary

--- apps/cowswap-frontend/src/legacy/state/orders/middleware/batchCancelOrdersPopup.ts
+++ apps/cowswap-frontend/src/legacy/state/orders/middleware/batchCancelOrdersPopup.ts
@@ -2,25 +2,33 @@ import { orderAnalytics } from '@cowprotocol/analytics'
 
 import { Dispatch, MiddlewareAPI } from 'redux'
 
+import { computeOrderSummary } from 'common/updaters/orders/utils'
+
 import { addPopup, AddPopupPayload } from '../../application/reducer'
 import { AppState } from '../../index'
-import { BaseOrder } from '../actions'
 import { buildCancellationPopupSummary } from '../buildCancellationPopupSummary'
 import { OrderTxTypes, setPopupData } from '../helpers'
+import { OrderObject } from '../reducer'
+import { deserializeOrder } from '../utils/deserializeOrder'
 
-export function batchCancelOrdersPopup(store: MiddlewareAPI<Dispatch, AppState>, orders: BaseOrder[]) {
+export function batchCancelOrdersPopup(store: MiddlewareAPI<Dispatch, AppState>, orders: OrderObject[]) {
   // construct Cancelled Order Popups for each Order
   orders.forEach((order) => {
     const popup = _buildCancellationPopup(order)
-    const orderType = order.composableCowInfo ? 'TWAP' : order.class
+
+    const orderType = order.order.composableCowInfo ? 'TWAP' : order.order.class
     orderAnalytics('Canceled', orderType)
 
     store.dispatch(addPopup(popup))
   })
 }
 
-function _buildCancellationPopup(order: BaseOrder) {
-  const { cancellationHash, apiAdditionalInfo, id, summary } = order
+function _buildCancellationPopup(order: OrderObject) {
+  const { cancellationHash, apiAdditionalInfo, id } = order.order
+  const summary = computeOrderSummary({
+    orderFromStore: deserializeOrder(order),
+    orderFromApi: null,
+  })
 
   if (cancellationHash && !apiAdditionalInfo) {
     // EthFlow order which has been cancelled and does not exist on the backend
