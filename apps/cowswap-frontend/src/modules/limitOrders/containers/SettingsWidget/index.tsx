6908a94dc6ce1651bff29d0e2ed7fcf582be4ed4
Alfetopito <alfetopito@users.noreply.github.com>
2024-02-23 11:34:25 +0000

    refactor: remove unnecessary useCallback

--- apps/cowswap-frontend/src/modules/limitOrders/containers/SettingsWidget/index.tsx
+++ apps/cowswap-frontend/src/modules/limitOrders/containers/SettingsWidget/index.tsx
@@ -1,27 +1,16 @@
 import { useAtomValue, useSetAtom } from 'jotai'
-import React, { useCallback } from 'react'
+import React from 'react'
 
 import { Menu, MenuItem } from '@reach/menu-button'
 
 import { MenuContent, SettingsButton, SettingsIcon } from 'modules/trade/pure/Settings'
 
 import { Settings } from '../../pure/Settings'
-import {
-  limitOrdersSettingsAtom,
-  LimitOrdersSettingsState,
-  updateLimitOrdersSettingsAtom,
-} from '../../state/limitOrdersSettingsAtom'
+import { limitOrdersSettingsAtom, updateLimitOrdersSettingsAtom } from '../../state/limitOrdersSettingsAtom'
 
 export function SettingsWidget() {
   const settingsState = useAtomValue(limitOrdersSettingsAtom)
   const updateSettingsState = useSetAtom(updateLimitOrdersSettingsAtom)
-
-  const onStateChanged = useCallback(
-    (state: Partial<LimitOrdersSettingsState>) => {
-      updateSettingsState(state)
-    },
-    [updateSettingsState]
-  )
 
   return (
     <>
@@ -31,7 +20,7 @@ export function SettingsWidget() {
         </SettingsButton>
         <MenuContent>
           <MenuItem disabled={true} onSelect={() => void 0}>
-            <Settings state={settingsState} onStateChanged={onStateChanged} />
+            <Settings state={settingsState} onStateChanged={updateSettingsState} />
           </MenuItem>
         </MenuContent>
       </Menu>
