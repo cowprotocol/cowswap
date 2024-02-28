60fdcbf36937b86edf1bf77bf9f9523e0530b207
Alexandr Kazachenko <shoom3301@gmail.com>
2024-02-28 17:28:45 +0600

    chore: remove partner fee mock

--- apps/cowswap-frontend/src/modules/injectedWidget/hooks/useInjectedWidgetParams.ts
+++ apps/cowswap-frontend/src/modules/injectedWidget/hooks/useInjectedWidgetParams.ts
@@ -5,14 +5,5 @@ import type { CowSwapWidgetParams } from '@cowprotocol/widget-lib'
 import { injectedWidgetParamsAtom } from '../state/injectedWidgetParamsAtom'
 
 export function useInjectedWidgetParams(): Partial<CowSwapWidgetParams> {
-  const value = useAtomValue(injectedWidgetParamsAtom)
-
-  return {
-    ...value,
-    // TODO: Remove the default partner fee after testing
-    partnerFee: {
-      bps: 2500,
-      recipient: '0x79063d9173C09887d536924E2F6eADbaBAc099f5',
-    },
-  }
+  return useAtomValue(injectedWidgetParamsAtom)
 }
