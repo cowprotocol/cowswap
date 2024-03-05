ce7d57b12a476726ec8191c0bf3da8c148027296
Alfetopito <alfetopito@users.noreply.github.com>
2024-03-05 17:50:32 +0000

    feat: update alternative order atom type to include isEdit flag

--- apps/cowswap-frontend/src/modules/trade/state/alternativeOrder/atoms.ts
+++ apps/cowswap-frontend/src/modules/trade/state/alternativeOrder/atoms.ts
@@ -4,11 +4,16 @@ import { Order } from 'legacy/state/orders/actions'
 
 import { ParsedOrder } from 'utils/orderUtils/parseOrder'
 
+export type AlternativeOrder = {
+  order: Order | ParsedOrder
+  isEdit: boolean
+} | null
+
 /**
  * Main atom to control the alternative order modal/form
  * When it's set, the alternative flow should be displayed
  */
-export const alternativeOrderAtom = atom<Order | ParsedOrder | null>(null)
+export const alternativeOrderAtom = atom<AlternativeOrder>(null)
 
 /**
  * Derived atom that controls the alternative modal visibility
