0606c6ff109759cb9d1d2128328755e91b880cc4
Alfetopito <alfetopito@users.noreply.github.com>
2024-03-08 17:13:19 +0000

    feat: add useTokenList to explorer

---
+++ apps/explorer/src/hooks/useTokenList.ts
@@ -0,0 +1,54 @@
+import { SupportedChainId } from '@cowprotocol/cow-sdk'
+import useSWR from 'swr'
+import { SWR_NO_REFRESH_OPTIONS } from '@cowprotocol/common-const'
+import type { TokenInfo, TokenList } from '@uniswap/token-lists'
+
+type TokenListByAddress = Record<string, TokenInfo>
+type TokenListPerNetwork = Record<SupportedChainId, TokenListByAddress>
+
+const INITIAL_TOKEN_LIST_PER_NETWORK: TokenListPerNetwork = {
+  [SupportedChainId.MAINNET]: {},
+  [SupportedChainId.GNOSIS_CHAIN]: {},
+  [SupportedChainId.SEPOLIA]: {},
+}
+
+export function useTokenList(chainId: SupportedChainId | undefined): { data: TokenListByAddress; isLoading: boolean } {
+  const { data: cowSwapList, isLoading: isCowListLoading } = useTokenListByUrl(
+    'https://files.cow.fi/tokens/CowSwap.json'
+  )
+  const { data: coingeckoList, isLoading: isCoingeckoListLoading } = useTokenListByUrl(
+    'https://tokens.coingecko.com/uniswap/all.json'
+  )
+  const { data: honeyswapList, isLoading: isHoneyswapListLoading } = useTokenListByUrl(
+    chainId === SupportedChainId.GNOSIS_CHAIN ? 'https://tokens.honeyswap.org' : ''
+  )
+
+  const data = chainId ? { ...coingeckoList, ...honeyswapList, ...cowSwapList }[chainId] : {}
+  const isLoading = chainId ? isCowListLoading || isHoneyswapListLoading || isCoingeckoListLoading : false
+
+  return { data, isLoading }
+}
+
+function useTokenListByUrl(tokenListUrl: string) {
+  return useSWR(tokenListUrl, fetcher, {
+    fallbackData: INITIAL_TOKEN_LIST_PER_NETWORK,
+    ...SWR_NO_REFRESH_OPTIONS,
+  })
+}
+
+const SUPPORTED_CHAIN_IDS_SET = new Set(Object.values(SupportedChainId))
+
+function fetcher(tokenListUrl: string): Promise<TokenListPerNetwork> {
+  return fetch(tokenListUrl)
+    .then<TokenList>((res) => res.json())
+    .then(({ tokens }) =>
+      // Create an object with token addresses as keys
+      tokens.reduce((acc, token) => {
+        // Pick only supported chains
+        if (SUPPORTED_CHAIN_IDS_SET.has(token.chainId)) {
+          acc[token.chainId][token.address.toLowerCase()] = token
+        }
+        return acc
+      }, INITIAL_TOKEN_LIST_PER_NETWORK)
+    )
+}
