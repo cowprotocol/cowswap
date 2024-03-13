f726f931c96b402a3a194ea7ac2eefbe47ebc9de
Anxo Rodriguez <anxolin@gmail.com>
2024-03-13 21:12:16 +0000

    feat: style hooks

--- apps/cowswap-frontend/src/modules/hooks/smart/hookButtons.tsx
+++ apps/cowswap-frontend/src/modules/hooks/smart/hookButtons.tsx
@@ -1,14 +1,14 @@
 import { useAtomValue } from 'jotai'
 import { useState } from 'react'
 
-import { ButtonSecondaryAlt } from '@cowprotocol/ui'
+import { ButtonSecondaryAlt, UI } from '@cowprotocol/ui'
 
 import styled from 'styled-components/macro'
 
 import { Link } from 'legacy/components/Link'
 import QuestionHelper from 'legacy/components/QuestionHelper'
 
-import { CloseIcon } from 'common/pure/CloseIcon'
+import { CloseIcon as CloseIconOriginal } from 'common/pure/CloseIcon'
 
 import { HookStoreModal } from './HookStoreModal'
 
@@ -39,8 +39,81 @@ const List = styled.ul`
   font-size: 0.8rem;
 `
 
-const HookList = styled.ul``
-const HookItemWrapper = styled.li``
+const SeeDetailsButton = styled.a``
+
+const HookList = styled.ul`
+  padding-right: 10px;
+  max-width: 100%;
+  display: flex;
+
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  font-size: 0.8rem;
+`
+const HookItemWrapper = styled.li`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  dl {
+    background-color: var(${UI.COLOR_BACKGROUND});
+    padding: 20px;
+  }
+
+  dd {
+    color: var(${UI.COLOR_TEXT2});
+    word-break: break-all;
+  }
+
+  img {
+    width: 35px;
+    height: 35px;
+  }
+`
+
+const CloseIcon = styled(CloseIconOriginal)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`
+
+const HookItemInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0.5em;
+
+  dl {
+    margin: 0;
+    padding: 0 1em;
+    background-color: inherit;
+  }
+
+  dt {
+    width: 6em;
+  }
+
+  dd {
+    img {
+      width: 20px;
+    }
+  }
+
+  dt,
+  dd {
+    float: left;
+    margin: 5px;
+  }
+
+  dt {
+    font-weight: bold;
+    clear: left;
+    margin-right: 5px;
+  }
+`
 
 export function PreHookButton() {
   const [open, setOpen] = useState(false)
@@ -117,13 +190,52 @@ interface HookItemProp {
 }
 
 function HookItem({ hookDetails, isPreHook, removeHook }: HookItemProp) {
-  const { callData, gasLimit, target } = hookDetails.hook
+  const { uuid, hook, dapp } = hookDetails
+  const { callData, gasLimit, target } = hook
+
+  const [showDetails, setShowDetails] = useState(false)
   return (
     <HookItemWrapper>
-      <div>UUID: {hookDetails.uuid}</div>
-      <div>target: {target}</div>
-      <div>gasLimit: {gasLimit}</div>
-      <div>callData: {callData} </div>
+      <HookItemInfo>
+        <img src={dapp.image} alt={dapp.name} />
+        <dl>
+          <dt>Action</dt>
+          <dd>{dapp.name}</dd>
+
+          <dt>Token</dt>
+          <dd>
+            <img src={dapp.image} alt={dapp.name} /> <span>GNO</span>
+          </dd>
+
+          <dt>Amount</dt>
+          <dd>52.30216 GNO</dd>
+        </dl>
+      </HookItemInfo>
+      <SeeDetailsButton
+        onClick={(e) => {
+          e.preventDefault()
+          setShowDetails((details) => !details)
+        }}
+        href="#"
+      >
+        See hook details
+      </SeeDetailsButton>
+
+      {showDetails && (
+        <dl>
+          <dt>UUID</dt>
+          <dd>{uuid}</dd>
+
+          <dt>target</dt>
+          <dd>{target}</dd>
+
+          <dt>gasLimit</dt>
+          <dd>{gasLimit}</dd>
+
+          <dt>callData</dt>
+          <dd>{callData}</dd>
+        </dl>
+      )}
       <CloseIcon onClick={() => removeHook(hookDetails, isPreHook)} />
     </HookItemWrapper>
   )
