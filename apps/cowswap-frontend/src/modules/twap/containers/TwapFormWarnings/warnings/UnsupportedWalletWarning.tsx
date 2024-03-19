2042ea2d0eb5cc66410da04d86a801b100eae4d3
Alfetopito <alfetopito@users.noreply.github.com>
2024-03-19 15:17:02 +0000

    fix: add on TWAP wallet requirements note about Safe not deployed

--- apps/cowswap-frontend/src/modules/twap/containers/TwapFormWarnings/warnings/UnsupportedWalletWarning.tsx
+++ apps/cowswap-frontend/src/modules/twap/containers/TwapFormWarnings/warnings/UnsupportedWalletWarning.tsx
@@ -1,4 +1,4 @@
-import { ExternalLink, InlineBanner, CowSwapSafeAppLink } from '@cowprotocol/ui'
+import { CowSwapSafeAppLink, ExternalLink, InlineBanner } from '@cowprotocol/ui'
 
 import { UNSUPPORTED_WALLET_LINK } from 'modules/twap/const'
 
@@ -22,6 +22,10 @@ export function UnsupportedWalletWarning({ isSafeViaWc }: { isSafeViaWc: boolean
         TWAP orders currently require a Safe with a special fallback handler. Have one? Switch to it! Need setup?{' '}
         <ExternalLink href={UNSUPPORTED_WALLET_LINK}>Click here</ExternalLink>. Future updates may extend wallet
         support!
+      </p>
+      <p>
+        <strong>Note:</strong> If you are using the Safe App already but still see this, make sure your Safe is
+        deployed!
       </p>
     </InlineBanner>
   )
