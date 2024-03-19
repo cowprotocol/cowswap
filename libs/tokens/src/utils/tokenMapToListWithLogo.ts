ddae13dd33380285dd1317faa199d855ce607c70
Alfetopito <alfetopito@users.noreply.github.com>
2024-03-19 11:44:12 +0000

    fix: filter out tokens that don't belong to current network

--- libs/tokens/src/utils/tokenMapToListWithLogo.ts
+++ libs/tokens/src/utils/tokenMapToListWithLogo.ts
@@ -5,8 +5,9 @@ import { TokensMap } from '../types'
 /**
  * Convert a tokens map to a list of tokens and sort them alphabetically
  */
-export function tokenMapToListWithLogo(tokenMap: TokensMap): TokenWithLogo[] {
+export function tokenMapToListWithLogo(tokenMap: TokensMap, chainId: number): TokenWithLogo[] {
   return Object.values(tokenMap)
+    .filter((token) => token.chainId === chainId)
     .sort((a, b) => a.symbol.localeCompare(b.symbol))
     .map((token) => TokenWithLogo.fromToken(token, token.logoURI))
 }
