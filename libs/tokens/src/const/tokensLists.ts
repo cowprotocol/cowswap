64b5eaeeb1040502a1bd82f1d1d06b00d76ccf46
Alexandr Kazachenko <shoom3301@gmail.com>
2024-03-28 16:34:28 +0500

    feat: add curated list to Gnosis chain

--- libs/tokens/src/const/tokensLists.ts
+++ libs/tokens/src/const/tokensLists.ts
@@ -4,3 +4,6 @@ import tokensList from './tokensList.json'
 export const DEFAULT_TOKENS_LISTS: ListsSourcesByNetwork = tokensList
 
 export const UNISWAP_TOKENS_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
+
+export const GNOSIS_UNISWAP_TOKENS_LIST =
+  'https://raw.githubusercontent.com/cowprotocol/token-lists/main/src/public/GnosisUniswapTokensList.json'
