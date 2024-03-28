c891c3007ed25c53bce6ae93a6567e2c191245ba
Alfetopito <alfetopito@users.noreply.github.com>
2024-03-28 18:18:59 +0000

    Revert "fix: force release-please trigger cowswap release (#4113)"
    This reverts commit 7d6be88e0e1b23b495f386637e0a156b46241bc1.

--- apps/cowswap-frontend/src/common/hooks/featureFlags/useIsAprilFoolsEnabled.ts
+++ apps/cowswap-frontend/src/common/hooks/featureFlags/useIsAprilFoolsEnabled.ts
@@ -3,7 +3,6 @@ import { isInjectedWidget } from '@cowprotocol/common-utils'
 
 export function useIsAprilFoolsEnabled(): boolean {
   const { isAprilFoolsEnabled } = useFeatureFlags()
-  // useless comment to trigger release please...
 
   return isAprilFoolsEnabled && !isInjectedWidget()
 }
