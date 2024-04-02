a7e400e5d9b4fa639854743785eb837b045bfecc
Alexandr Kazachenko <shoom3301@gmail.com>
2024-04-02 13:10:47 +0500

    fix(widget): update snippets corresponding to the code (#4136)

--- apps/widget-configurator/src/app/embedDialog/utils/jsExample.ts
+++ apps/widget-configurator/src/app/embedDialog/utils/jsExample.ts
@@ -3,7 +3,7 @@ import { CowSwapWidgetParams } from '@cowprotocol/widget-lib'
 import { formatParameters } from './formatParameters'
 
 import { ColorPalette } from '../../configurator/types'
-import { COMMENTS_BEFORE_PARAMS } from '../const'
+import { COMMENTS_BEFORE_PARAMS, PROVIDER_PARAM_COMMENT } from '../const'
 
 export function jsExample(params: CowSwapWidgetParams, defaultPalette: ColorPalette): string {
   return `
@@ -14,6 +14,9 @@ const container = document.getElementById('<YOUR_CONTAINER>')
 // ${COMMENTS_BEFORE_PARAMS}
 const params = ${formatParameters(params, 0, false, defaultPalette)}
 
-const updateWidget = cowSwapWidget(container, params)
+// ${PROVIDER_PARAM_COMMENT}
+const provider = window.ethereum
+
+const { updateParams } = cowSwapWidget(container, { params, provider })
   `
 }
