8d516b53d50fc659b556a9b77504441842112122
Alexandr Kazachenko <shoom3301@gmail.com>
2024-04-08 18:55:23 +0500

    fix: dark theme for skeleton

--- apps/widget-configurator/src/theme/hooks/useColorMode.ts
+++ apps/widget-configurator/src/theme/hooks/useColorMode.ts
@@ -1,13 +1,21 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import { PaletteMode } from '@mui/material'
 import useMediaQuery from '@mui/material/useMediaQuery'
 
 import { ColorModeParams } from '../ColorModeContext'
 
+const THEME_STORAGE_KEY = 'widget-cfg-theme'
+
+const getThemeFromCache = () => localStorage.getItem(THEME_STORAGE_KEY)
+
 export function useColorMode(): ColorModeParams {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  const [mode, setMode] = useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light')
+  const [mode, setMode] = useState<PaletteMode>(getThemeFromCache() || prefersDarkMode ? 'dark' : 'light')
+
+  useEffect(() => {
+    localStorage.setItem(THEME_STORAGE_KEY, mode)
+  }, [mode])
 
   return useMemo(
     () => ({
