aea31d73382c6760092095229b0339c868534f90
Anxo Rodriguez <anxolin@gmail.com>
2024-04-09 09:14:11 +0200

    chore: delete the debug messages

--- libs/widget-lib/src/IframeSafeSdkBridge.ts
+++ libs/widget-lib/src/IframeSafeSdkBridge.ts
@@ -8,10 +8,8 @@ export class IframeSafeSdkBridge {
       }
 
       if (isSafeMessageRequest(event.data)) {
-        console.debug('[IframeSafeSdkBridge] Safe SDK request: ' + event.data.method, event.data.id, event.data)
         this.appWindow.parent.postMessage(event.data, '*')
       } else if (isSafeMessageResponse(event.data)) {
-        console.debug('[IframeSafeSdkBridge] Safe SDK response', event.data.id, event.data)
         this.iframeWidow.postMessage(event.data, '*')
       }
     }
@@ -20,12 +18,10 @@ export class IframeSafeSdkBridge {
   }
 
   private startListening() {
-    console.debug('[IframeSafeSdkBridge] Start listening')
     this.appWindow.addEventListener('message', this.forwardSdkMessage)
   }
 
   public stopListening() {
-    console.debug('[IframeSafeSdkBridge] Stop listening')
     this.appWindow.removeEventListener('message', this.forwardSdkMessage)
   }
 }
