a47715ab1cdc10874c96c4cf6449b3df0c4caafd
Alexandr Kazachenko <shoom3301@gmail.com>
2024-04-16 15:58:35 +0500

    test: test getOrderValidTo

--- apps/cowswap-frontend/src/modules/tradeQuote/utils/quoteDeadline.test.ts
+++ apps/cowswap-frontend/src/modules/tradeQuote/utils/quoteDeadline.test.ts
@@ -1,4 +1,8 @@
-import { getQuoteTimeOffset } from './quoteDeadline'
+import { MAX_VALID_TO_EPOCH } from '@cowprotocol/common-utils'
+
+import { getOrderValidTo, getQuoteTimeOffset } from './quoteDeadline'
+
+const NOW_TIME = 1713264841334
 
 describe('Quote deadline utils', () => {
   describe('getQuoteTimeOffset()', () => {
@@ -41,6 +45,47 @@ describe('Quote deadline utils', () => {
           quoteValidTo: localQuoteTimestamp + validFor,
         })
       ).toEqual(-7200)
+    })
+  })
+
+  describe('getOrderValidTo()', () => {
+    beforeEach(() => {
+      // Freeze time
+      Date.now = jest.fn(() => NOW_TIME)
+    })
+
+    it('When time offset is not defined, then should be zero', () => {
+      const deadline = 10
+      const quoteDeadlineParams = {
+        validFor: undefined,
+        quoteValidTo: undefined,
+        localQuoteTimestamp: undefined,
+      }
+
+      expect(getOrderValidTo(deadline, quoteDeadlineParams)).toEqual(0)
+    })
+
+    it('ValidTo should be now + deadline + timeOffset', () => {
+      const deadline = 5400 // 1.5 hours
+      const offset = 3600 // 1 hour
+      const quoteDeadlineParams = {
+        validFor: deadline,
+        quoteValidTo: NOW_TIME + deadline + offset,
+        localQuoteTimestamp: NOW_TIME,
+      }
+
+      expect(getOrderValidTo(deadline, quoteDeadlineParams)).toEqual(Math.floor(NOW_TIME / 1000 + deadline + offset))
+    })
+
+    it('When the result is too big, then it should be capped by MAX_VALID_TO_EPOCH', () => {
+      const deadline = 54000000000000000
+      const quoteDeadlineParams = {
+        validFor: deadline,
+        quoteValidTo: NOW_TIME + deadline,
+        localQuoteTimestamp: NOW_TIME,
+      }
+
+      expect(getOrderValidTo(deadline, quoteDeadlineParams)).toEqual(MAX_VALID_TO_EPOCH)
     })
   })
 })
