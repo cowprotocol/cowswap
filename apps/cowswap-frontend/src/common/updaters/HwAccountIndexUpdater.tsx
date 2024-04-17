464fe3443fd896f4a3fd3f2ac39089f7c01a52d2
Alexandr Kazachenko <shoom3301@gmail.com>
2024-04-17 12:39:42 +0500

    chore: comment out HwAccountIndexUpdater

--- apps/cowswap-frontend/src/common/updaters/HwAccountIndexUpdater.tsx
+++ apps/cowswap-frontend/src/common/updaters/HwAccountIndexUpdater.tsx
@@ -1,46 +1,47 @@
-import { useAtom } from 'jotai'
-import { useEffect, useMemo, useRef } from 'react'
+// TODO: FIXME web3modal doesn't support hardware wallets
+// import { useAtom } from 'jotai'
+// import { useEffect, useMemo, useRef } from 'react'
+//
+// import { getIsHardWareWallet, getWeb3ReactConnection, hwAccountIndexAtom, useWalletInfo } from '@cowprotocol/wallet'
+// import { useWeb3React } from '@web3-react/core'
 
-import { getIsHardWareWallet, getWeb3ReactConnection, hwAccountIndexAtom, useWalletInfo } from '@cowprotocol/wallet'
-import { useWeb3React } from '@web3-react/core'
-
-const indexChanged = true
+// const indexChanged = true
 
 export function HwAccountIndexUpdater() {
-  const [hwAccountIndex, setHwAccountIndex] = useAtom(hwAccountIndexAtom)
-  const { chainId, account, active } = useWalletInfo()
-  const { connector } = useWeb3React()
-  const connectorRef = useRef(connector)
-
-  connectorRef.current = connector
-
-  const connectionType = useMemo(() => {
-    const connection = getWeb3ReactConnection(connector)
-
-    return connection.type
-  }, [connector])
+  // const [hwAccountIndex, setHwAccountIndex] = useAtom(hwAccountIndexAtom)
+  // const { chainId, account, active } = useWalletInfo()
+  // const { connector } = useWeb3React()
+  // const connectorRef = useRef(connector)
+  //
+  // connectorRef.current = connector
+  //
+  // const connectionType = useMemo(() => {
+  //   const connection = getWeb3ReactConnection(connector)
+  //
+  //   return connection.type
+  // }, [connector])
 
   /**
    * Reactivate connector each time when account index is changed from HwAccountIndexSelector
    * A hardware wallet connector should take into account the second parameter (indexChanged = true) for activate() method
    */
-  useEffect(() => {
-    if (!active) return
-
-    const isHardWare = getIsHardWareWallet(connectionType)
-
-    if (!isHardWare) return
-
-    console.debug('[Hardware wallet] account index changed', hwAccountIndex)
-    connectorRef.current?.activate(chainId, indexChanged)
-  }, [active, hwAccountIndex, connectionType, chainId])
-
-  useEffect(() => {
-    if (account) return
-
-    console.debug('[Hardware wallet] reset account index to 0')
-    setHwAccountIndex(0)
-  }, [setHwAccountIndex, account])
+  // useEffect(() => {
+  //   if (!active) return
+  //
+  //   const isHardWare = getIsHardWareWallet(connectionType)
+  //
+  //   if (!isHardWare) return
+  //
+  //   console.debug('[Hardware wallet] account index changed', hwAccountIndex)
+  //   connectorRef.current?.activate(chainId, indexChanged)
+  // }, [active, hwAccountIndex, connectionType, chainId])
+  //
+  // useEffect(() => {
+  //   if (account) return
+  //
+  //   console.debug('[Hardware wallet] reset account index to 0')
+  //   setHwAccountIndex(0)
+  // }, [setHwAccountIndex, account])
 
   return null
 }
