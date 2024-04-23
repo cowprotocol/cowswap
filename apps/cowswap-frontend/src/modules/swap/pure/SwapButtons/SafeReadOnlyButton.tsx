132ccde990a7ab84633bff02fa52df0dfc1020c8
Alexandr Kazachenko <shoom3301@gmail.com>
2024-04-23 14:44:02 +0500

    chore: fix text

--- apps/cowswap-frontend/src/modules/swap/pure/SwapButtons/SafeReadOnlyButton.tsx
+++ apps/cowswap-frontend/src/modules/swap/pure/SwapButtons/SafeReadOnlyButton.tsx
@@ -3,7 +3,6 @@ import React, { useCallback, useState } from 'react'
 import { ButtonPrimary, ButtonSize, Tooltip } from '@cowprotocol/ui'
 
 import { Trans } from '@lingui/macro'
-import { AlertCircle } from 'react-feather'
 
 export function SafeReadOnlyButton() {
   const [show, setShow] = useState(false)
@@ -30,22 +29,11 @@ export function SafeReadOnlyButton() {
         <Tooltip
           show={show}
           text={
-            <>
-              <p>
-                Your Safe is not connected with a signer, hence you are in a read-only mode and won't be able to place
-                an order.
-              </p>
-              <p>Please, make sure the connected wallet is one of the signers of this Safe.</p>
-              <div style={{ display: 'flex', gap: '10px' }}>
-                <div>
-                  <AlertCircle size={16} />
-                </div>
-                <div>
-                  After solving the issue above, <strong>you will need to refresh the website</strong> to exit read-only
-                  mode.
-                </div>
-              </div>
-            </>
+            <p>
+              Your Safe is not connected with a signer.
+              <br />
+              To place an order, you must connect using a signer of the Safe and refresh the page.
+            </p>
           }
         >
           <Trans>Connect signer</Trans>
