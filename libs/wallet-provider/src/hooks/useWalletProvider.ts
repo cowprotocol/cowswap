ce0a711b4cdb7c0480aa3dc02c90d91fb6b3dbcd
Alexandr Kazachenko <shoom3301@gmail.com>
2024-04-25 13:54:38 +0500

    fix: fix useWalletProvider caching

--- libs/wallet-provider/src/hooks/useWalletProvider.ts
+++ libs/wallet-provider/src/hooks/useWalletProvider.ts
@@ -1,13 +1,9 @@
 import { Web3Provider } from '@ethersproject/providers'
 import { useWeb3ModalProvider } from '@web3modal/ethers5/react'
-import { useMemo } from 'react'
+import useSWR from 'swr'
 
 export function useWalletProvider() {
   const { walletProvider } = useWeb3ModalProvider()
 
-  return useMemo(() => {
-    if (!walletProvider) return undefined
-
-    return new Web3Provider(walletProvider)
-  }, [walletProvider])
+  return useSWR([walletProvider], () => (walletProvider ? new Web3Provider(walletProvider) : undefined)).data
 }
