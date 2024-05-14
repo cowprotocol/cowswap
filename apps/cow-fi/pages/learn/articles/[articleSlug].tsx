5cfde16ca25b442cb88abbd957339a9c4a56643a
Anxo Rodriguez <anxolin@gmail.com>
2024-05-14 13:59:21 +0100

    feat(cms): add blog page with an article (#4421)
    * feat(blog): add blog page with an article
    
    * feat: style article page

--- apps/cow-fi/pages/learn/articles/[articleSlug].tsx
+++ apps/cow-fi/pages/learn/articles/[articleSlug].tsx
@@ -3,7 +3,10 @@ import React from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { getArticleBySlug, getAllArticleSlugs, Article } from 'services/cms'
 
-import { ArticleContent } from '@/components/Article'
+import { ArticleContent, GetInTouchSection } from '@/components/Article'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import Link from 'next/link'
 
 const DATA_CACHE_TIME_SECONDS = 10 * 60 // 10 minutes
 
@@ -12,7 +15,38 @@ export interface BlogPostProps {
 }
 
 export default function BlogPostPage({ article }: BlogPostProps) {
-  return <ArticleContent article={article} />
+  const { id } = article
+  const { title, description, slug, seo } =
+    article?.attributes || {}
+  const { metaTitle, shareImage, metaDescription } = seo || {}
+  const shareImageUrl = shareImage?.data?.attributes?.url
+
+  // TODO: Add SEO information
+
+  return (
+    <Layout fullWidthGradientVariant={true} data-article-id={id} data-slug={slug}>
+      <Head>
+        <title>{title}</title>
+
+        <meta name="description" content={metaDescription || description} key="description" />
+        <meta property="og:description" content={metaDescription || description} key="og-description" />
+        <meta property="og:title" content={metaTitle || title} key="og-title" />
+        <meta name="twitter:title" content={title} key="twitter-title" />
+        {shareImageUrl && (
+          <>
+            <meta key="ogImage" property="og:image" content={shareImageUrl} />
+            <meta key="twitterImage" name="twitter:image" content={shareImageUrl} />
+          </>
+        )}
+      </Head>
+
+      <ArticleContent article={article} />
+
+
+      <GetInTouchSection />
+    </Layout>
+  )
+  
 }
 
 type ArticleQuery = { articleSlug: string }
