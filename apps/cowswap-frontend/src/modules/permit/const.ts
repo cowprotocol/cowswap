061ff441ca33400c64e2d8468e33648141a0e3b3
Alfetopito <alfetopito@users.noreply.github.com>
2024-05-15 11:27:43 +0100

    chore: change permitinfo temporarily to pending token-lists PR
    https://github.com/cowprotocol/token-lists/pull/389

--- apps/cowswap-frontend/src/modules/permit/const.ts
+++ apps/cowswap-frontend/src/modules/permit/const.ts
@@ -2,5 +2,6 @@ import ms from 'ms.macro'
 
 export const PENDING_ORDER_PERMIT_CHECK_INTERVAL = ms`60s`
 
+// TODO: revert to previous one, only for temporary testing
 export const PRE_GENERATED_PERMIT_URL =
-  'https://raw.githubusercontent.com/cowprotocol/token-lists/main/src/public/PermitInfo'
+  'https://raw.githubusercontent.com/cowprotocol/token-lists/2a83e3299264b31b99e3bc760223cb75b14fe7fa/src/public/PermitInfo'
