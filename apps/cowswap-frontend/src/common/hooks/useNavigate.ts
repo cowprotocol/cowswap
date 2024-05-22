ad9987f62f4902a46f2f9c81ffd8bc30e8e747ab
Anxo Rodriguez <anxolin@gmail.com>
2024-05-22 19:18:11 +0100

    feat: implement a widget-aware navigate function

---
+++ apps/cowswap-frontend/src/common/hooks/useNavigate.ts
@@ -0,0 +1,17 @@
+import { isInjectedWidget } from '@cowprotocol/common-utils'
+
+// eslint-disable-next-line no-restricted-imports
+import { NavigateOptions, To, useNavigate as useNavigateOriginal } from 'react-router-dom'
+
+export type NavigateFunction = (to: To, options?: NavigateOptions) => void
+
+export function useNavigate(): NavigateFunction {
+  const isWidget = isInjectedWidget()
+  const navigate = useNavigateOriginal()
+
+  return (to, options) =>
+    navigate(to, {
+      replace: isWidget,
+      ...options,
+    })
+}
