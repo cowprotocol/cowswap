a1da67960c6e105ce08c1dd483313b45a95f0332
Anxo Rodriguez <anxolin@gmail.com>
2024-05-22 19:19:12 +0100

    feat: replace the use of navigate hook so it doesnt polute history

--- apps/cow-fi/modules/utm/utils.ts
+++ apps/cow-fi/modules/utm/utils.ts
@@ -6,7 +6,6 @@ const UTM_TERM_PARAM = 'utm_term'
 
 const ALL_UTM_PARAMS = [UTM_SOURCE_PARAM, UTM_MEDIUM_PARAM, UTM_CAMPAIGN_PARAM, UTM_CONTENT_PARAM, UTM_TERM_PARAM]
 
-// import { useLocation, useNavigate } from 'react-router-dom'
 import { NextRouter } from 'next/router'
 import { UtmParams } from './types'
 import { ParsedUrlQuery } from 'querystring'
@@ -50,40 +49,38 @@ export function hasUtmCodes(utm: UtmParams | undefined): boolean {
 }
 
 export function addUtmToUrl(href: string, utm: UtmParams): string {
-  const origin = typeof window !== 'undefined' ? window.location.origin : CONFIG.url.root;
-  const url = new URL(href, origin);
+  const origin = typeof window !== 'undefined' ? window.location.origin : CONFIG.url.root
+  const url = new URL(href, origin)
 
   // Extract the hash and its associated query parameters
-  const [hashPath, hashQuery] = url.hash.split('?');
+  const [hashPath, hashQuery] = url.hash.split('?')
 
   // Create a new URLSearchParams object for the hash's query parameters
-  const hashParams = new URLSearchParams(hashQuery);
+  const hashParams = new URLSearchParams(hashQuery)
 
   // Add UTM parameters to the hash's query parameters
   if (utm.utmCampaign) {
-    hashParams.set(UTM_CAMPAIGN_PARAM, utm.utmCampaign);
+    hashParams.set(UTM_CAMPAIGN_PARAM, utm.utmCampaign)
   }
 
   if (utm.utmContent) {
-    hashParams.set(UTM_CONTENT_PARAM, utm.utmContent);
+    hashParams.set(UTM_CONTENT_PARAM, utm.utmContent)
   }
 
   if (utm.utmMedium) {
-    hashParams.set(UTM_MEDIUM_PARAM, utm.utmMedium);
+    hashParams.set(UTM_MEDIUM_PARAM, utm.utmMedium)
   }
 
   if (utm.utmSource) {
-    hashParams.set(UTM_SOURCE_PARAM, utm.utmSource);
+    hashParams.set(UTM_SOURCE_PARAM, utm.utmSource)
   }
 
   if (utm.utmTerm) {
-    hashParams.set(UTM_TERM_PARAM, utm.utmTerm);
+    hashParams.set(UTM_TERM_PARAM, utm.utmTerm)
   }
 
   // Construct the final URL
-  const baseUrl = url.origin + url.pathname + url.search;
-  const finalHash = `${hashPath}?${hashParams.toString()}`;
-  return baseUrl + finalHash;
+  const baseUrl = url.origin + url.pathname + url.search
+  const finalHash = `${hashPath}?${hashParams.toString()}`
+  return baseUrl + finalHash
 }
-
-
