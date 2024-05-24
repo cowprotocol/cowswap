1e90fb1a28f5eaa844493aacff0a8f2a09bb4e60
Alexandr Kazachenko <shoom3301@gmail.com>
2024-05-24 16:02:12 +0500

    chore: remove mocks

--- apps/cowswap-frontend/src/api/gnosisProtocol/hooks.ts
+++ apps/cowswap-frontend/src/api/gnosisProtocol/hooks.ts
@@ -1,5 +1,5 @@
 import { GP_ORDER_UPDATE_INTERVAL } from '@cowprotocol/common-const'
-import { EnrichedOrder, OrderStatus } from '@cowprotocol/cow-sdk'
+import { EnrichedOrder } from '@cowprotocol/cow-sdk'
 import { useWalletInfo } from '@cowprotocol/wallet'
 
 import useSWR from 'swr'
@@ -19,21 +19,7 @@ export function useGpOrders(): EnrichedOrder[] {
     () => {
       if (!chainId || !requestParams) return []
 
-      return getOrders(requestParams, { chainId }).then((orders) => {
-        // TODO: remove after test
-        const my = orders.find(
-          (order) =>
-            order.uid ===
-            '0xfbc02116477e1d722ce60e5306fcbbd8c9738ca4541ccfbef72fd86db9dd4c98fb3c7eb936caa12b5a884d612393969a557d430766583061'
-        )
-
-        if (my) {
-          my.executedSellAmount = '499989000000000000'
-          my.executedSellAmountBeforeFees = '499989000000000000'
-          my.status = OrderStatus.OPEN
-        }
-        return orders
-      })
+      return getOrders(requestParams, { chainId })
     },
     { refreshInterval: GP_ORDER_UPDATE_INTERVAL }
   )
