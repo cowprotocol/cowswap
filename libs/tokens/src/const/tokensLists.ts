a306b8e27cf98a1d9c75064678110c301ec0de56
Alfetopito <alfetopito@users.noreply.github.com>
2024-05-27 16:01:55 +0100

    chore: point to brach rather than individual commit

--- libs/tokens/src/const/tokensLists.ts
+++ libs/tokens/src/const/tokensLists.ts
@@ -11,4 +11,4 @@ export const GNOSIS_UNISWAP_TOKENS_LIST =
 
 // TODO: update address once https://github.com/cowprotocol/token-lists/pull/393 is merged
 export const ARBITRUM_ONE_TOKENS_LIST =
-  'https://raw.githubusercontent.com/cowprotocol/token-lists/ca0fac81d0318234cf16b9f661d05a5c24c2ccb6/src/public/ArbitrumOneUniswapTokensList.json'
+  'https://github.com/cowprotocol/token-lists/blob/feat/arb1-uniswap-mapped-list/src/public/ArbitrumOneUniswapTokensList.json'
