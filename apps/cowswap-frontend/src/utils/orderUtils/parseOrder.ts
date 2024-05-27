d89a0d4ba76836c05526091c9ab6bee8a5048616
Alexandr Kazachenko <shoom3301@gmail.com>
2024-05-27 16:13:49 +0500

    fix(limit-orders): adjust almost fully filled orders displaying (#4477)

--- apps/cowswap-frontend/src/utils/orderUtils/parseOrder.ts
+++ apps/cowswap-frontend/src/utils/orderUtils/parseOrder.ts
@@ -1,3 +1,4 @@
+import { LONG_PRECISION } from '@cowprotocol/common-const'
 import { OrderClass, OrderKind, SigningScheme } from '@cowprotocol/cow-sdk'
 import { Currency, CurrencyAmount, Price, Token } from '@uniswap/sdk-core'
 
@@ -64,7 +65,8 @@ export const parseOrder = (order: Order): ParsedOrder => {
   const creationTime = new Date(order.creationTime)
   const fullyFilled = isOrderFilled(order)
   const partiallyFilled = isPartiallyFilled(order)
-  const filledPercentDisplay = filledPercentage.times(100).decimalPlaces(2).toNumber()
+  const filledPercentDisplay = +filledPercentage.toFixed(LONG_PRECISION) * 100
+
   const executedPrice = JSBI.greaterThan(executedBuyAmount, JSBI.BigInt(0))
     ? new Price({
         baseAmount: CurrencyAmount.fromRawAmount(order.inputToken, executedSellAmount),
