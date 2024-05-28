5564848b1fa6ba63edb3005c8ae45ad7cb98e59a
Alfetopito <alfetopito@users.noreply.github.com>
2024-05-28 11:11:26 +0100

    feat: use correct address for token list

--- libs/tokens/src/const/tokensLists.ts
+++ libs/tokens/src/const/tokensLists.ts
@@ -9,6 +9,5 @@ export const UNISWAP_TOKENS_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.
 export const GNOSIS_UNISWAP_TOKENS_LIST =
   'https://raw.githubusercontent.com/cowprotocol/token-lists/main/src/public/GnosisUniswapTokensList.json'
 
-// TODO: update address once https://github.com/cowprotocol/token-lists/pull/393 is merged
 export const ARBITRUM_ONE_TOKENS_LIST =
-  'https://github.com/cowprotocol/token-lists/blob/feat/arb1-uniswap-mapped-list/src/public/ArbitrumOneUniswapTokensList.json'
+  'https://raw.githubusercontent.com/cowprotocol/token-lists/main/src/public/ArbitrumOneUniswapTokensList.json'
