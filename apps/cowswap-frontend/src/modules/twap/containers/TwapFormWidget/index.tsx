977e376bd0ecf4f88177be15a6ff91701d6af3d5
Alexandr Kazachenko <shoom3301@gmail.com>
2024-06-12 13:27:10 +0500

    chore: fix import

--- apps/cowswap-frontend/src/modules/twap/containers/TwapFormWidget/index.tsx
+++ apps/cowswap-frontend/src/modules/twap/containers/TwapFormWidget/index.tsx
@@ -15,7 +15,6 @@ import { useGetTradeFormValidation } from 'modules/tradeFormValidation'
 import { TwapFormState } from 'modules/twap/pure/PrimaryActionButton/getTwapFormState'
 
 import { useRateInfoParams } from 'common/hooks/useRateInfoParams'
-import { ExecutionPrice } from 'common/pure/ExecutionPrice'
 
 import * as styledEl from './styled'
 import { LABELS_TOOLTIPS } from './tooltips'
