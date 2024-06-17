d6dd53dba7feffe422cd3d2fd9382305679dd2d9
Alexandr Kazachenko <shoom3301@gmail.com>
2024-06-17 16:06:15 +0500

    refactor: make cowswap volume fee config safer

--- apps/cowswap-frontend/src/modules/volumeFee/state/cowswapFeeAtom.ts
+++ apps/cowswap-frontend/src/modules/volumeFee/state/cowswapFeeAtom.ts
@@ -9,9 +9,15 @@ import { derivedTradeStateAtom } from 'modules/trade'
 
 import { VolumeFee } from '../types'
 
-const COWSWAP_VOLUME_FEE: VolumeFee = {
-  bps: 10, // 0.1%
-  recipient: '0x6b3214fD11dc91De14718DeE98Ef59bCbFcfB432', // Gnosis Chain Protocol fee safe
+const COWSWAP_VOLUME_FEES: Record<SupportedChainId, VolumeFee | null> = {
+  [SupportedChainId.MAINNET]: null,
+  [SupportedChainId.SEPOLIA]: null,
+  [SupportedChainId.ARBITRUM_ONE]: null,
+  // Only Gnosis chain
+  [SupportedChainId.GNOSIS_CHAIN]: {
+    bps: 10, // 0.1%
+    recipient: '0x6b3214fD11dc91De14718DeE98Ef59bCbFcfB432', // Gnosis Chain Protocol fee safe
+  },
 }
 
 export const cowSwapFeeAtom = atom((get) => {
@@ -22,9 +28,6 @@ export const cowSwapFeeAtom = atom((get) => {
 
   // No widget mode
   if (isInjectedWidget()) return null
-
-  // Only Gnosis chain
-  if (chainId !== SupportedChainId.GNOSIS_CHAIN) return null
 
   if (!inputCurrency || !outputCurrency) return null
 
@@ -38,5 +41,5 @@ export const cowSwapFeeAtom = atom((get) => {
   // No stable-stable trades
   if (isInputTokenStable && isOutputTokenStable) return null
 
-  return COWSWAP_VOLUME_FEE
+  return COWSWAP_VOLUME_FEES[chainId]
 })
