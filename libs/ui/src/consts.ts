7be18ddc8ba564bacb3c66312a7cc29ef6a00670
Alexandr Kazachenko <shoom3301@gmail.com>
2024-06-21 14:40:35 +0500

    refactor: remove outdated feature-flag

--- libs/ui/src/consts.ts
+++ libs/ui/src/consts.ts
@@ -5,7 +5,6 @@ import { UI } from './enum'
 import { colors, themeVariables as baseThemeVariables } from './theme'
 import { CowSwapTheme } from './types'
 
-export const AMOUNTS_FORMATTING_FEATURE_FLAG = 'highlight-amounts-formatting'
 export const SAFE_COW_APP_LINK = 'https://app.safe.global/share/safe-app?appUrl=https%3A%2F%2Fswap.cow.fi&chain=eth'
 export const LINK_GUIDE_ADD_CUSTOM_TOKEN = 'https://blog.cow.fi/how-to-add-custom-tokens-on-cow-swap-a72d677c78c0'
 export const MY_ORDERS_ID = 'my-orders'
