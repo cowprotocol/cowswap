2c0c7220fce13e2d2c06375e299e821a95037b0e
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2024-06-25 17:54:44 +0100

    feat: update legal pages

--- apps/cow-fi/styles/styled.ts
+++ apps/cow-fi/styles/styled.ts
@@ -1498,7 +1498,11 @@ export const BodyContent = styled.div<{ color?: string }>`
 
   > p,
   > ul,
-  > ol {
+  > ul > li ul,
+  > ol,
+  > ol > li ul,
+  > em,
+  > p em {
     margin-bottom: 16px;
     font-size: 21px;
     line-height: 1.5;
@@ -1508,13 +1512,23 @@ export const BodyContent = styled.div<{ color?: string }>`
     }
   }
 
-  > ul,
-  > ol {
-    padding-left: 20px;
+  > p em {
+    display: block;
+    font-weight: ${Font.weight.bold};
+  }
 
-    > li {
-      margin: 0 0 24px;
+  > ul,
+  > ul > li ul,
+  > ol,
+  > ol > li ul {
+    padding: 8px 0 0 20px;
+
+    > li,
+    > li > p,
+    > li > h3 {
+      margin: 0 0 12px;
       font-size: inherit;
+      line-height: inherit;
     }
   }
 
@@ -1529,6 +1543,34 @@ export const BodyContent = styled.div<{ color?: string }>`
 
     > p {
       line-height: 1.6;
+    }
+  }
+
+  // Add general styles for <table> with odd and even rows bg color
+  > table {
+    width: 100%;
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin: 24px 0;
+    font-size: 15px;
+    line-height: 1.6;
+
+    > thead {
+      background: ${Color.neutral90};
+      color: ${Color.neutral20};
+    }
+
+    > tbody {
+      > tr {
+        &:nth-child(odd) {
+          background: ${Color.neutral98};
+        }
+
+        > td {
+          padding: 8px 12px;
+          border: 1px solid ${Color.neutral90};
+        }
+      }
     }
   }
 
