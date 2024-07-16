6e5fa81f343cf8670ea45233b1d8050ab90a0d8b
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2024-07-16 16:29:19 +0100

    fix: keep accordion open on price refresh

--- apps/cowswap-frontend/src/common/pure/TradeDetailsAccordion/index.tsx
+++ apps/cowswap-frontend/src/common/pure/TradeDetailsAccordion/index.tsx
@@ -1,100 +1,20 @@
-import { useState } from 'react'
+import React from 'react'
 
 import CarretIcon from '@cowprotocol/assets/cow-swap/carret-down.svg'
-import { FiatAmount, TokenAmount, UI } from '@cowprotocol/ui'
+import { FiatAmount, TokenAmount } from '@cowprotocol/ui'
 import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 
 import SVG from 'react-inlinesvg'
-import styled from 'styled-components/macro'
 
-const Wrapper = styled.div<{ isOpen: boolean }>`
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-  width: 100%;
-  border: 1px solid var(${UI.COLOR_PAPER_DARKER});
-  border-radius: 16px;
-  padding: 10px;
-  height: auto;
-  font-size: 13px;
-  font-weight: var(${UI.FONT_WEIGHT_MEDIUM});
-`
-
-const Details = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-  width: 100%;
-  margin: 6px 0 0;
-`
-
-const Summary = styled.div`
-  display: grid;
-  grid-template-columns: auto 1fr;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  gap: 8px;
-  font-size: inherit;
-  font-weight: inherit;
-
-  span {
-    font-size: inherit;
-    font-weight: inherit;
-    white-space: nowrap;
-  }
-`
-
-const SummaryClickable = styled.div<{ isOpen: boolean }>`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  gap: 7px;
-  opacity: ${({ isOpen }) => (isOpen ? 1 : 0.7)};
-  transition: opacity ${UI.ANIMATION_DURATION_SLOW} ease-in-out;
-  outline: none;
-  font-size: inherit;
-  font-weight: inherit;
-
-  &:hover {
-    opacity: 1;
-  }
-
-  // If it's the only child, make it take all the space
-  &:only-child {
-    grid-column: 1 / -1;
-  }
-`
-
-const ToggleIcon = styled.div<{ isOpen: boolean }>`
-  --size: var(${UI.ICON_SIZE_SMALL});
-  transform: ${({ isOpen }) => (isOpen ? 'rotate(180deg)' : 'rotate(0deg)')};
-  transition: transform var(${UI.ANIMATION_DURATION_SLOW}) ease-in-out;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: var(--size);
-  height: var(--size);
-
-  > svg {
-    --size: var(${UI.ICON_SIZE_TINY});
-    width: var(--size);
-    height: var(--size);
-    object-fit: contain;
-
-    path {
-      fill: var(${UI.COLOR_TEXT});
-    }
-  }
-`
+import { Wrapper, Summary, SummaryClickable, ToggleIcon, Details } from './styled'
 
 interface TradeDetailsAccordionProps {
   rateInfo: React.ReactNode
   feeTotalAmount: CurrencyAmount<Currency> | null
   feeUsdTotalAmount: CurrencyAmount<Currency> | null
   children?: React.ReactNode
-  open?: boolean
+  open: boolean
+  onToggle: () => void
 }
 
 export const TradeDetailsAccordion = ({
@@ -102,34 +22,36 @@ export const TradeDetailsAccordion = ({
   feeTotalAmount,
   feeUsdTotalAmount,
   children,
-  open = false,
+  open,
+  onToggle,
 }: TradeDetailsAccordionProps) => {
-  const [isOpen, setIsOpen] = useState(open)
-
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen)
+  const handleToggle = () => {
+    if (typeof onToggle === 'function') {
+      onToggle()
+    } else {
+      console.error('onToggle is not a function:', onToggle)
+    }
   }
 
-  // Accessibility enhancement: manage focus and aria-expanded attribute
   const handleKeyDown = (e: { key: string; preventDefault: () => void }) => {
     if (['Enter', ' ', 'ArrowUp', 'ArrowDown'].includes(e.key)) {
       e.preventDefault()
-      toggleAccordion()
+      handleToggle()
     }
   }
 
   return (
-    <Wrapper isOpen={isOpen}>
+    <Wrapper isOpen={open}>
       <Summary>
         {rateInfo}
         <SummaryClickable
-          onClick={toggleAccordion}
+          onClick={handleToggle}
           onKeyDown={handleKeyDown}
-          aria-expanded={isOpen}
+          aria-expanded={open}
           tabIndex={0}
-          isOpen={isOpen}
+          isOpen={open}
         >
-          {!isOpen && (
+          {!open && (
             <>
               {feeUsdTotalAmount?.greaterThan(0) ? (
                 <>
@@ -148,12 +70,12 @@ export const TradeDetailsAccordion = ({
               )}
             </>
           )}
-          <ToggleIcon isOpen={isOpen}>
-            <SVG src={CarretIcon} title={isOpen ? 'Close' : 'Open'} />
+          <ToggleIcon isOpen={open}>
+            <SVG src={CarretIcon} title={open ? 'Close' : 'Open'} />
           </ToggleIcon>
         </SummaryClickable>
       </Summary>
-      {isOpen && <Details>{children}</Details>}
+      {open && <Details>{children}</Details>}
     </Wrapper>
   )
 }
