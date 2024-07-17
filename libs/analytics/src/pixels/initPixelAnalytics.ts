b29e80534590f47de37e903ce2bcfb34a3c34c12
Anxo Rodriguez <anxolin@gmail.com>
2024-07-17 15:32:54 +0100

    fix: dont initiate pixels

--- libs/analytics/src/pixels/initPixelAnalytics.ts
+++ libs/analytics/src/pixels/initPixelAnalytics.ts
@@ -8,9 +8,9 @@ import { PavedPixel } from './providers/paved'
 import { RedditPixel } from './providers/reddit'
 import { TwitterPixel } from './providers/twitter'
 
+const isPixelEnabled = false // For now this is disabled for all environments. We can enable it later by adding "isProd || isEns" instead of the hardcoded "false"
 
 export function initPixelAnalytics(): PixelAnalytics | undefined {
-  const isPixelEnabled = isProd || isEns
   if (!isPixelEnabled) {
     return undefined
   }
