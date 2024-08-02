54d97448e4e3f92bdc66f1cd30b89af85c161d0f
Anxo Rodriguez <anxolin@gmail.com>
2024-08-02 14:18:17 +0100

    fix: don't catch unsupported tokens

--- libs/tokens/src/updaters/UnsupportedTokensUpdater/index.ts
+++ libs/tokens/src/updaters/UnsupportedTokensUpdater/index.ts
@@ -8,7 +8,7 @@ import ms from 'ms.macro'
 import { useUnsupportedTokens } from '../../hooks/tokens/unsupported/useUnsupportedTokens'
 import { removeUnsupportedTokensAtom } from '../../state/tokens/unsupportedTokensAtom'
 
-const UNSUPPORTED_TOKEN_TTL = ms`1h`
+const UNSUPPORTED_TOKEN_TTL = 0 // TODO: Set to 1h after fixing race condition (see https://github.com/cowprotocol/cowswap/issues/4759)
 
 /**
  * Since an unsupported token might become supported in the future, we should periodically reset the list of unsupported tokens.
