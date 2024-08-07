2bd92ded45896608c63cce1865f967a20e3e495d
Alfetopito <alfetopito@users.noreply.github.com>
2024-08-07 17:30:00 +0100

    feat: accept children props on CancelButton component

--- apps/cowswap-frontend/src/common/pure/CancelButton/index.tsx
+++ apps/cowswap-frontend/src/common/pure/CancelButton/index.tsx
@@ -1,11 +1,13 @@
+import { PropsWithChildren } from 'react'
+
 import { Command } from '@cowprotocol/types'
 
 import { LinkStyledButton } from 'theme'
 
 export type CancelButtonProps = {
   onClick: Command
-}
+} & PropsWithChildren
 
-export function CancelButton({ onClick }: CancelButtonProps) {
-  return <LinkStyledButton onClick={onClick}>Cancel order</LinkStyledButton>
+export function CancelButton({ onClick, children }: CancelButtonProps) {
+  return <LinkStyledButton onClick={onClick}>{children || 'Cancel order'}</LinkStyledButton>
 }
