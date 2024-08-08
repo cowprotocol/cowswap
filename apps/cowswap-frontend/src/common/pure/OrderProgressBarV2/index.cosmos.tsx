b48c7f2e5a3d3ce970fdc113dea0d953f52223a2
Alfetopito <alfetopito@users.noreply.github.com>
2024-08-08 17:40:56 +0100

    chore: add missing steps to cosmos

--- apps/cowswap-frontend/src/common/pure/OrderProgressBarV2/index.cosmos.tsx
+++ apps/cowswap-frontend/src/common/pure/OrderProgressBarV2/index.cosmos.tsx
@@ -1,12 +1,21 @@
+import { USDC_GNOSIS_CHAIN } from '@cowprotocol/common-const'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
+import { CurrencyAmount } from '@uniswap/sdk-core'
 
 import { getOrderMock } from 'mocks/orderMock'
 import styled from 'styled-components/macro'
 
+import { Order } from 'legacy/state/orders/actions'
+
 import { OrderProgressBarV2, OrderProgressBarV2Props } from './index'
 
+const order = {
+  ...getOrderMock(SupportedChainId.MAINNET),
+  apiAdditionalInfo: { executedBuyAmount: '1000000000000000000000', executedSellAmount: '5000000000000000000' },
+} as Order
+
 const defaultProps: OrderProgressBarV2Props = {
-  order: getOrderMock(SupportedChainId.MAINNET),
+  order,
   stepName: 'initial',
   showCancellationModal: () => {
     alert('cancellation triggered o/')
@@ -22,6 +31,14 @@ const defaultProps: OrderProgressBarV2Props = {
     { solver: 'uniswap', executedAmounts: { sell: '5000000000000000000', buy: '500000000000000000000' } },
     { solver: 'oneinch', executedAmounts: { sell: '5000000000000000000', buy: '400000000000000000000' } },
   ],
+  surplusData: {
+    surplusFiatValue: CurrencyAmount.fromRawAmount(USDC_GNOSIS_CHAIN, '10000000'),
+    surplusAmount: CurrencyAmount.fromRawAmount(order.outputToken, '1000000000000000000'),
+    surplusToken: order.outputToken,
+    surplusPercent: '10',
+    showFiatValue: true,
+    showSurplus: true,
+  },
 }
 
 const Wrapper = styled.div`
@@ -50,6 +67,16 @@ const Fixtures = {
       <OrderProgressBarV2 {...defaultProps} stepName="unfillable" />
     </Wrapper>
   ),
+  '2c-solved': (
+    <Wrapper>
+      <OrderProgressBarV2 {...defaultProps} stepName="solved" />
+    </Wrapper>
+  ),
+  '2d-nextBatch': (
+    <Wrapper>
+      <OrderProgressBarV2 {...defaultProps} stepName="nextBatch" />
+    </Wrapper>
+  ),
   '3-executing': (
     <Wrapper>
       <OrderProgressBarV2 {...defaultProps} stepName="executing" />
@@ -63,6 +90,26 @@ const Fixtures = {
   '4-finished': (
     <Wrapper>
       <OrderProgressBarV2 {...defaultProps} stepName="finished" />
+    </Wrapper>
+  ),
+  '4a-cancellationFailed': (
+    <Wrapper>
+      <OrderProgressBarV2 {...defaultProps} stepName="cancellationFailed" />
+    </Wrapper>
+  ),
+  cancelling: (
+    <Wrapper>
+      <OrderProgressBarV2 {...defaultProps} stepName="cancelling" />
+    </Wrapper>
+  ),
+  cancelled: (
+    <Wrapper>
+      <OrderProgressBarV2 {...defaultProps} stepName="cancelled" />
+    </Wrapper>
+  ),
+  expired: (
+    <Wrapper>
+      <OrderProgressBarV2 {...defaultProps} stepName="expired" />
     </Wrapper>
   ),
 }
