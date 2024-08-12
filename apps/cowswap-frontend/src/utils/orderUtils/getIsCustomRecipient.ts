8513980924e532db4a230e123bb6c78a3fd8b427
Leandro <alfetopito@users.noreply.github.com>
2024-08-12 09:51:16 +0100

    feat(progress-bar-v2): custom recipient (#4779)
    * refactor: add getIsCustomRecipient helper fn
    
    * feat: show custom recipient (and ens address) if any

---
+++ apps/cowswap-frontend/src/utils/orderUtils/getIsCustomRecipient.ts
@@ -0,0 +1,5 @@
+import { Order } from 'legacy/state/orders/actions'
+
+export function getIsCustomRecipient({ owner, receiver }: Pick<Order, 'owner' | 'receiver'>): boolean {
+  return Boolean(receiver && owner !== receiver)
+}
