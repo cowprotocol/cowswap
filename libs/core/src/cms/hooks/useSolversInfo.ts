858da376cc7d4b507170365c21ada960d8f5266d
Alfetopito <alfetopito@users.noreply.github.com>
2024-08-19 11:51:00 +0100

    feat: update to latest CMS data model

--- libs/core/src/cms/hooks/useSolversInfo.ts
+++ libs/core/src/cms/hooks/useSolversInfo.ts
@@ -1,6 +1,7 @@
 import { useAtomValue } from 'jotai'
 import { useMemo } from 'react'
 
+import { isProdLike } from '@cowprotocol/common-utils'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
 import { solversInfoAtom } from '../state'
@@ -9,15 +10,20 @@ import { SolverInfo } from '../types'
 export function useSolversInfo(chainId: SupportedChainId): Record<string, SolverInfo> {
   const allSolversInfo = useAtomValue(solversInfoAtom)
 
-  return useMemo(
-    () =>
-      allSolversInfo.reduce<Record<string, SolverInfo>>((acc, info) => {
-        if (info.chainIds.includes(chainId)) {
-          acc[info.solverId] = info
-        }
+  return useMemo(() => {
+    const env = isProdLike ? 'prod' : 'barn' // Should match what's set on CMS!
 
-        return acc
-      }, {}),
-    [chainId, allSolversInfo]
-  )
+    return allSolversInfo.reduce<Record<string, SolverInfo>>((acc, info) => {
+      if (
+        info.solverNetworks.some(
+          ({ env: solverEnv, chainId: solverChainId, active }) =>
+            solverEnv === env && solverChainId === chainId && active
+        )
+      ) {
+        acc[info.solverId] = info
+      }
+
+      return acc
+    }, {})
+  }, [chainId, allSolversInfo])
 }
