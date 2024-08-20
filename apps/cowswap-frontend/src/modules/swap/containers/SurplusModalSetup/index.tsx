9fa11ff9a84e072b3447b65099767ae4a10f20ae
Alfetopito <alfetopito@users.noreply.github.com>
2024-08-20 15:17:09 +0100

    feat: reuse TransactionSubmittedContent inside SurplusModal

--- apps/cowswap-frontend/src/modules/swap/containers/SurplusModalSetup/index.tsx
+++ apps/cowswap-frontend/src/modules/swap/containers/SurplusModalSetup/index.tsx
@@ -5,8 +5,7 @@ import { useWalletInfo } from '@cowprotocol/wallet'
 import { useOrder } from 'legacy/state/orders/hooks'
 
 import { CowModal } from 'common/pure/Modal'
-import { OrderProgressBarV2 } from 'common/pure/OrderProgressBarV2'
-import * as styledEl from 'common/pure/TransactionSubmittedContent/styled'
+import { TransactionSubmittedContent } from 'common/pure/TransactionSubmittedContent'
 
 import { useTradeConfirmState } from '../../../trade'
 import { useOrderIdForSurplusModal, useRemoveOrderFromSurplusQueue } from '../../state/surplusModal'
@@ -21,7 +20,7 @@ export function SurplusModalSetup() {
   const order = useOrder({ id: orderId, chainId })
 
   const progressBarV2Props = useSetupAdditionalProgressBarProps(chainId, order)
-  const { surplusData, isProgressBarSetup } = progressBarV2Props
+  const { surplusData, isProgressBarSetup, activityDerivedState } = progressBarV2Props
   const { showSurplus } = surplusData
 
   const { isOpen: isConfirmationModalOpen } = useTradeConfirmState()
@@ -43,16 +42,14 @@ export function SurplusModalSetup() {
 
   return (
     <CowModal isOpen={isOpen} onDismiss={onDismiss} maxHeight={90} maxWidth={470}>
-      <styledEl.Wrapper>
-        <styledEl.Section>
-          <styledEl.Header>
-            <styledEl.CloseIconWrapper onClick={onDismiss} />
-          </styledEl.Header>
-          {isProgressBarSetup && (
-            <OrderProgressBarV2 {...progressBarV2Props} order={order} navigateToNewOrder={navigateToNewOrderCallback(chainId, order, onDismiss)} />
-          )}
-        </styledEl.Section>
-      </styledEl.Wrapper>
+      <TransactionSubmittedContent
+        onDismiss={onDismiss}
+        chainId={chainId}
+        hash={orderId}
+        activityDerivedState={activityDerivedState}
+        orderProgressBarV2Props={progressBarV2Props}
+        navigateToNewOrderCallback={navigateToNewOrderCallback}
+      />
     </CowModal>
   )
 }
