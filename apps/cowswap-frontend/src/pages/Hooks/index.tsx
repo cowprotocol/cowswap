7c7a7a37693928d8408c4f009b5663c9d995f3d5
Alexandr Kazachenko <shoom3301@gmail.com>
2024-09-03 17:07:25 +0500

    refactor: fix dependency inversion (hooksStore -> swap)

--- apps/cowswap-frontend/src/pages/Hooks/index.tsx
+++ apps/cowswap-frontend/src/pages/Hooks/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-import { SwapPage } from 'pages/Swap'
+import { PostHookButton, PreHookButton } from 'modules/hooksStore'
+import { SwapUpdaters } from 'modules/swap'
+import { SwapWidget } from 'modules/swap'
 
 export function HooksPage() {
-  return <SwapPage hooksEnabled={true} />
+  return (
+    <>
+      <SwapUpdaters />
+      <SwapWidget topContent={<PreHookButton />} bottomContent={<PostHookButton />} />
+    </>
+  )
 }
