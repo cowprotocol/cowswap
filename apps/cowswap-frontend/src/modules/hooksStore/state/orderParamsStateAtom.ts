4d48c7731b71bda206551c9fc10542bc7310f6e8
Alexandr Kazachenko <shoom3301@gmail.com>
2024-09-11 14:34:59 +0500

    chore: fix build

--- apps/cowswap-frontend/src/modules/hooksStore/state/orderParamsStateAtom.ts
+++ apps/cowswap-frontend/src/modules/hooksStore/state/orderParamsStateAtom.ts
@@ -1,5 +1,5 @@
 import { atom } from 'jotai'
 
-import { HookDappOrderParams } from '@cowprotocol/types'
+import { HookDappOrderParams } from '../types/hooks'
 
 export const orderParamsStateAtom = atom<HookDappOrderParams | null>(null)
