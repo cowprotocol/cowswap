d00a09083d51a4f2f34b89790318e0e2f4f4f7fe
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-17 14:09:09 -0300

    refactor: use TokenWithLogo.fromToken in airdrop hook constants

--- apps/cowswap-frontend/src/modules/hooksStore/dapps/AirdropHookApp/constants.ts
+++ apps/cowswap-frontend/src/modules/hooksStore/dapps/AirdropHookApp/constants.ts
@@ -1,27 +1,11 @@
 import { COW } from '@cowprotocol/common-const'
 import { TokenWithLogo } from '@cowprotocol/common-const'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
+import { Token } from '@uniswap/sdk-core'
 
 import { AirdropOption } from './types'
 
-function buildToken(tokenObj: {
-  logoURI: string | undefined
-  chainId: number
-  address: string
-  decimals: number
-  symbol?: string
-  name?: string
-  bypassChecksum?: boolean
-}) {
-  return new TokenWithLogo(
-    tokenObj.logoURI,
-    tokenObj.chainId,
-    tokenObj.address,
-    tokenObj.decimals,
-    tokenObj.symbol,
-    tokenObj.name,
-  )
-}
+const cowSepolia = COW[SupportedChainId.SEPOLIA]
 
 // Update this list to add a new airdrop into the UI
 // For more information: https://github.com/bleu/cow-airdrop-contract-deployer
@@ -31,10 +15,10 @@ export const AIRDROP_OPTIONS = [
     dataBaseUrl: 'https://raw.githubusercontent.com/bleu/cow-airdrop-contract-deployer/example/mock-airdrop-data/',
     chainId: SupportedChainId.SEPOLIA,
     address: '0xD1fB81659c434DDebC8468713E482134be0D85C0',
-    token: buildToken({
-      ...COW[SupportedChainId.SEPOLIA],
-      address: '0x5fe27bf718937ca1c4a7818d246cd4e755c7470c',
-    }),
+    token: TokenWithLogo.fromToken(
+      new Token(cowSepolia.chainId, cowSepolia.address, cowSepolia.decimals, cowSepolia.symbol, cowSepolia.name),
+      cowSepolia.logoURI,
+    ),
   },
   // {
   //   name: 'YYY',
