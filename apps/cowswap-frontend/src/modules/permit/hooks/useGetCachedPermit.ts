e4794db69065508b8d98d0551870e1dd95857076
Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
2024-09-20 10:59:09 -0300

    refactor(hook-store): support permit with custom spender (#4891)

--- apps/cowswap-frontend/src/modules/permit/hooks/useGetCachedPermit.ts
+++ apps/cowswap-frontend/src/modules/permit/hooks/useGetCachedPermit.ts
@@ -1,6 +1,7 @@
 import { useSetAtom } from 'jotai/index'
 import { useCallback } from 'react'
 
+import { COW_PROTOCOL_SETTLEMENT_CONTRACT_ADDRESS } from '@cowprotocol/cow-sdk'
 import { getPermitUtilsInstance, PermitHookData } from '@cowprotocol/permit-utils'
 import { useWalletInfo } from '@cowprotocol/wallet'
 import { useWalletProvider } from '@cowprotocol/wallet-provider'
@@ -9,16 +10,20 @@ import { Nullish } from 'types'
 
 import { getPermitCacheAtom } from '../state/permitCacheAtom'
 
-export function useGetCachedPermit(): (tokenAddress: Nullish<string>) => Promise<PermitHookData | undefined> {
+export function useGetCachedPermit(): (
+  tokenAddress: Nullish<string>,
+  customSpender?: string,
+) => Promise<PermitHookData | undefined> {
   const { chainId, account } = useWalletInfo()
   const provider = useWalletProvider()
   const getCachedPermit = useSetAtom(getPermitCacheAtom)
 
   return useCallback(
-    async (tokenAddress: Nullish<string>) => {
+    async (tokenAddress: Nullish<string>, customSpender?: string) => {
       if (!provider || !account || !tokenAddress) {
         return
       }
+      const spender = customSpender || COW_PROTOCOL_SETTLEMENT_CONTRACT_ADDRESS[chainId]
 
       try {
         const eip2162Utils = getPermitUtilsInstance(chainId, provider, account)
@@ -27,7 +32,7 @@ export function useGetCachedPermit(): (tokenAddress: Nullish<string>) => Promise
         // Static account should never need to pre-check the nonce as it'll never change once cached
         const nonce = account ? await eip2162Utils.getTokenNonce(tokenAddress, account) : undefined
 
-        const permitParams = { chainId, tokenAddress, account, nonce }
+        const permitParams = { chainId, tokenAddress, account, nonce, spender }
 
         return getCachedPermit(permitParams)
       } catch (e) {
@@ -36,6 +41,6 @@ export function useGetCachedPermit(): (tokenAddress: Nullish<string>) => Promise
         return undefined
       }
     },
-    [getCachedPermit, chainId, account, provider]
+    [getCachedPermit, chainId, account, provider],
   )
 }
