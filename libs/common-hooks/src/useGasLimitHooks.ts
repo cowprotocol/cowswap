cab240778128dfbdd29f0873062a39b23695d577
Jean Neiverth <79885562+JeanNeiverth@users.noreply.github.com>
2024-09-23 06:57:02 -0300

    feat(cow-hooks): add claim airdrop hook app  (#4897)
    * feat: add Airdrop PreHook on Sepolia Network
    
    * fix: change name platform -> airdrop
    
    * fix: review issues
    
    * Jean/cow 351 add preview of claim tokens on airdrop hook (#4)
    
    * fix: aidropHookApp -> move UI components to a separate folder
    
    * feat: hook to get user claimable tokens
    
    * feat: add COW database url in airdrop hook constants
    
    Co-authored-by: Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
    
    * fix: switch name 'link' to 'dataBaseUrl'
    
    * feat: (wip) add test function for findIntervalKey
    
    * docs: comment findIntervalKeys function in Airdrop hook
    
    * fix: typescript issues
    
    * feat: add message to the user about claimable tokens status
    
    * wip: call isClaimed verification on contract
    
    * wip: use preview token output and add airdrop contract hook
    
    * feat: add preview of claimable tokens in Airdrop hook
    
    * style: add empty line in the end of component files
    
    * fix: change token formatting to cow standard
    
    * refactor: move dropdownMenu from  AidropHookApp/index to components
    
    * fix: no more claimable tokens messages when user connects without airdrop selected
    
    * wip: review
    
    * refactor: add claimed call on the usePreviewTokens
    
    * chore: remove duplicated virtual token abi
    
    * refactor: transform test script in ts test
    
    * refactor: rename preview tokens error variable
    
    ---------
    
    Co-authored-by: Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
    Co-authored-by: Pedro Yves Fracari <yves.fracari@gmail.com>
    
    * refactor: pass dropdow states to dropdown component
    
    * chore: add cow-sdk from bleu repo
    
    * Add airdrop claim hook encoding (#5)
    
    * wip: hook encoding
    
    * chore: add airdrop claim hook
    
    * wip: add cowshed hooks
    
    * refactor: rename components folder to styles
    
    * refactor: simplife hooks and fix cow shed init code
    
    * refactor: components hook messages and hooks
    
    * chore: replace airdrop contract for uniswap interface
    
    * fix: airdrop conctract merkle proof
    
    ---------
    
    Co-authored-by: Jean Neiverth <jeanneiverth@gmail.com>
    
    * Fix airdrop hook contract address (#6)
    
    * wip: fixing airdrop address
    
    * chore: add airdrop abi
    
    * fix: use new airdrop contract on useClaimData
    
    * chore: remove todo comment
    
    ---------
    
    Co-authored-by: Jean Neiverth <jeanneiverth@gmail.com>
    
    * feat: add basic info and structure of new Aidrop Hook UI
    
    * style: match Airdrop PreHook UI with figma
    
    * fix: mobile bad rendering
    
    * refactor: add styled label for 'Select Airdrop' text
    
    * fix: label 'Select Airdrop'
    
    * docs: update comment in airdrop hook constants
    
    Co-authored-by: Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
    
    * fix: now filtering airdrops by chain in airdrop hook selector
    
    * fix: remove files unused for airdrop hook
    
    * fix: remove yarn.lock changes
    
    * refactor: use TokenWithLogo.fromToken in airdrop hook constants
    
    * feat: include airdrop hook as a post hook
    
    * refactor: send styles to single file in airdrop hook app
    
    * refactor: add useSWR to useGasLimitHooks
    
    * refactor: move useGasLimitHooks to cowprotocol/common-hooks
    
    * fix: fetchPreviewClaimableTokens is now a useCallback
    
    * fix: remove airdrop selector
    
    * fix: build issue involving import @cowprotocol/wallet-provider in useGasLimitHooks
    
    * refactor: change claim airdrop hook UI based on feedbacks
    
    * refactor: change contract address to new with more addresses to claim
    
    * fix: cow airdrop full description - change 'right before swap' to 'before or after'
    
    * fix: use airdrop hook image with transparent background
    
    ---------
    
    Co-authored-by: Pedro Yves Fracari <55461956+yvesfracari@users.noreply.github.com>
    Co-authored-by: Pedro Yves Fracari <yves.fracari@gmail.com>
    Co-authored-by: José Ribeiro <me@joseribeiro.dev>

---
+++ libs/common-hooks/src/useGasLimitHooks.ts
@@ -0,0 +1,35 @@
+import { calculateGasMargin } from '@cowprotocol/common-utils'
+import type { TransactionRequest } from '@ethersproject/abstract-provider'
+import type { Deferrable } from '@ethersproject/properties'
+import type { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+
+import useSWR from 'swr'
+
+import type { SWRConfiguration } from 'swr'
+
+type ITransactionData = Deferrable<TransactionRequest>
+
+type IHookGasCalculator = (transactionData: ITransactionData) => Promise<string>
+
+export function useWalletProvider(): Web3Provider | undefined {
+  const { provider } = useWeb3React()
+
+  return provider
+}
+
+export const useHookGasLimitCalculator = (): IHookGasCalculator => {
+  const provider = useWalletProvider()
+
+  return async (transactionData) => {
+    if (!provider) throw new Error('Provider is not defined')
+    const gasEstimation = await provider.estimateGas(transactionData)
+    return calculateGasMargin(gasEstimation).toString()
+  }
+}
+
+export const useGasLimit = (transactionData: ITransactionData, swrConfig?: SWRConfiguration) => {
+  const gasCalculator = useHookGasLimitCalculator()
+
+  return useSWR<string | undefined>(transactionData, gasCalculator, swrConfig)
+}
