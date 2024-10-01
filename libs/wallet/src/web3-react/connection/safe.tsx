7f215619ecfd1c9f1fcc08ac7ced8c383012ca92
Alexandr Kazachenko <shoom3301@gmail.com>
2024-10-01 12:49:20 +0500

    feat(wallets): add coinshift to allowed domains

--- libs/wallet/src/web3-react/connection/safe.tsx
+++ libs/wallet/src/web3-react/connection/safe.tsx
@@ -7,7 +7,19 @@ import { Web3ReactConnection } from '../types'
 
 const [web3GnosisSafe, web3GnosisSafeHooks] = initializeConnector<AsyncConnector>(
   (actions) =>
-    new AsyncConnector(() => import('@web3-react/gnosis-safe').then((m) => new m.GnosisSafe({ actions })), actions)
+    new AsyncConnector(
+      () =>
+        import('@web3-react/gnosis-safe').then(
+          (m) =>
+            new m.GnosisSafe({
+              actions,
+              options: {
+                allowedDomains: [/^app\.safe\.global$/, /^(.+\.)?coinshift\.global$/, /^localhost:5173$/],
+              },
+            }),
+        ),
+      actions,
+    ),
 )
 export const gnosisSafeConnection: Web3ReactConnection = {
   connector: web3GnosisSafe,
