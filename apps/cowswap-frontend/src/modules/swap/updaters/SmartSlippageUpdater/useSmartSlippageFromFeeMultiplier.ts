7738a4051a7072a73e3553430261086ca6cf919d
Alfetopito <alfetopito@users.noreply.github.com>
2024-10-04 16:27:00 +0100

    refactor: extract calculateBpsFromFeeMultiplier and added unittests

--- apps/cowswap-frontend/src/modules/swap/updaters/SmartSlippageUpdater/useSmartSlippageFromFeeMultiplier.ts
+++ apps/cowswap-frontend/src/modules/swap/updaters/SmartSlippageUpdater/useSmartSlippageFromFeeMultiplier.ts
@@ -1,12 +1,10 @@
 import { useMemo } from 'react'
 
 import { useFeatureFlags } from '@cowprotocol/common-hooks'
-import { Fraction, TradeType } from '@uniswap/sdk-core'
 
-import { useDerivedSwapInfo } from '../../hooks/useSwapState'
+import { calculateBpsFromFeeMultiplier } from './calculateBpsFromFeeMultiplier'
 
-const ONE = new Fraction(1)
-const MAX_BPS = 5000 // 50%
+import { useReceiveAmountInfo } from '../../../trade'
 
 /**
  * Calculates smart slippage in bps, based on quoted fee
@@ -16,57 +14,15 @@ const MAX_BPS = 5000 // 50%
  * More relevant for small orders in relation to fee amount, negligent for larger orders.
  */
 export function useSmartSlippageFromFeeMultiplier(): number | undefined {
-  const { trade } = useDerivedSwapInfo() || {}
-  const { fee, inputAmountWithFee, inputAmountWithoutFee, tradeType } = trade || {}
-  const { feeAsCurrency } = fee || {}
-  const { smartSlippageFeeMultiplierPercentage = 0 } = useFeatureFlags()
-  const feeMultiplierFactor = new Fraction(100 + smartSlippageFeeMultiplierPercentage, 100) // 50% more fee, applied to the whole value => 150% => 15/10 in fraction
+  const { beforeNetworkCosts, costs, isSell } = useReceiveAmountInfo() || {}
+  const { sellAmount: sellAmount } = beforeNetworkCosts || {}
+  const { networkFee } = costs || {}
+  const { amountInSellCurrency: feeAmount } = networkFee || {}
 
-  const percentage = useMemo(() => {
-    if (
-      !inputAmountWithFee ||
-      !inputAmountWithoutFee ||
-      !feeAsCurrency ||
-      tradeType === undefined ||
-      !smartSlippageFeeMultiplierPercentage
-    ) {
-      return undefined
-    }
+  const { smartSlippageFeeMultiplierPercentage = 50 } = useFeatureFlags()
 
-    if (tradeType === TradeType.EXACT_INPUT) {
-      // sell
-      // 1 - (sellAmount - feeAmount * 1.5) / (sellAmount - feeAmount)
-      // 1 - (inputAmountWithoutFee - feeAsCurrency * feeMultiplierFactor) / inputAmountWithFee
-      return ONE.subtract(
-        inputAmountWithoutFee
-          .subtract(feeAsCurrency.multiply(feeMultiplierFactor))
-          // !!! Need to convert to fraction before division to not lose precision
-          .asFraction.divide(inputAmountWithFee.asFraction),
-      )
-    } else {
-      // buy
-      // (sellAmount + feeAmount * 1.5) / (sellAmount + feeAmount) - 1
-      // (inputAmountWithFee + feeAsCurrency * feeMultiplierFactor) / inputAmountWithFee - 1
-      return (
-        inputAmountWithFee
-          .add(feeAsCurrency.multiply(feeMultiplierFactor))
-          // !!! Need to convert to fraction before division to not lose precision
-          .asFraction.divide(inputAmountWithFee.asFraction)
-          .subtract(ONE)
-      )
-    }
-  }, [tradeType, inputAmountWithFee, inputAmountWithoutFee, feeAsCurrency, smartSlippageFeeMultiplierPercentage])
-
-  // Stable reference
-  // convert % to BPS. E.g.: 1% => 0.01 => 100 BPS
-  const bps = percentage?.multiply(10_000).toFixed(0)
-
-  return useMemo(() => {
-    if (bps) {
-      // Cap it at MAX_BPS
-      return Math.min(+bps, MAX_BPS)
-    } else {
-      return undefined
-    }
-  }, [bps])
+  return useMemo(
+    () => calculateBpsFromFeeMultiplier(sellAmount, feeAmount, isSell, smartSlippageFeeMultiplierPercentage),
+    [isSell, sellAmount, feeAmount, smartSlippageFeeMultiplierPercentage],
+  )
 }
