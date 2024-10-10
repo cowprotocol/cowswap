462dbcba389c7e1aa55b00503faa75b335ff08a5
Alfetopito <alfetopito@users.noreply.github.com>
2024-10-10 10:32:39 +0100

    feat: do not show warning when not connected

--- apps/cowswap-frontend/src/modules/swap/containers/SwapWidget/index.tsx
+++ apps/cowswap-frontend/src/modules/swap/containers/SwapWidget/index.tsx
@@ -224,7 +224,7 @@ export function SwapWidget({ topContent, bottomContent }: SwapWidgetProps) {
   const nativeCurrencySymbol = useNativeCurrency().symbol || 'ETH'
   const wrappedCurrencySymbol = useWrappedToken().symbol || 'WETH'
 
-  const isSuggestedSlippage = useIsSmartSlippageApplied() && !isTradePriceUpdating
+  const isSuggestedSlippage = useIsSmartSlippageApplied() && !isTradePriceUpdating && !!account
 
   const swapWarningsTopProps: SwapWarningsTopProps = {
     chainId,
