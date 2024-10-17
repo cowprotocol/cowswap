2bce57a93819a0acb2934185f27320f66ac1fd71
Alfetopito <alfetopito@users.noreply.github.com>
2024-10-17 11:08:54 +0100

    chore: fix typo

--- apps/cowswap-frontend/src/common/containers/InvalidLocalTimeWarning/index.tsx
+++ apps/cowswap-frontend/src/common/containers/InvalidLocalTimeWarning/index.tsx
@@ -16,8 +16,7 @@ export function InvalidLocalTimeWarning() {
 
   return (
     <GlobalWarning>
-      Local device time does is not accurate, CoW Swap most likely will not work correctly. Please adjust your device's
-      time.
+      Local device time is not accurate, CoW Swap most likely will not work correctly. Please adjust your device's time.
     </GlobalWarning>
   )
 }
