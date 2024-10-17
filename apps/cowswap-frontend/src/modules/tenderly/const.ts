0d3254c71f6352f8a91882b6e51dda0e5f03bcdb
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-10-17 16:45:55 -0300

    chore: remove unused tenderly consts

--- apps/cowswap-frontend/src/modules/tenderly/const.ts
+++
@@ -1,6 +0,0 @@
-const TENDERLY_ORG_NAME = process.env.REACT_APP_TENDERLY_ORG_NAME
-const TENDERLY_PROJECT_NAME = process.env.REACT_APP_TENDERLY_PROJECT_NAME
-
-export const getSimulationLink = (simulationId: string): string => {
-  return `https://dashboard.tenderly.co/${TENDERLY_ORG_NAME}/${TENDERLY_PROJECT_NAME}/simulator/${simulationId}`
-}
