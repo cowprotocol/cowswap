98597c2b6b360d0519bfc26adce6895bee5a3c0a
fairlight <31534717+fairlighteth@users.noreply.github.com>
2024-10-24 15:26:24 +0100

    feat: modify pool token list layout (#5014)
    * feat: modify pool token list layout
    
    * feat: apply mobile widgetlinks menu
    
    * feat: add padding to pool token balance
    
    ---------
    
    Co-authored-by: Alexandr Kazachenko <shoom3301@gmail.com>

--- apps/cowswap-frontend/src/modules/tokensList/pure/LpTokenLists/styled.ts
+++ apps/cowswap-frontend/src/modules/tokensList/pure/LpTokenLists/styled.ts
@@ -1,13 +1,16 @@
-import { UI } from '@cowprotocol/ui'
+import { TokenLogoWrapper } from '@cowprotocol/tokens'
+import { Media, UI } from '@cowprotocol/ui'
 
 import { ArrowRight } from 'react-feather'
 import styled from 'styled-components/macro'
 
 export const Wrapper = styled.div`
+  --grid-columns: 1fr 100px 50px 20px;
   display: flex;
   flex-direction: column;
   overflow: auto;
-  margin-bottom: 15px;
+  margin: 0 0 20px;
+  height: 100%;
 `
 
 export const LpTokenWrapper = styled.div`
@@ -19,16 +22,16 @@ export const LpTokenWrapper = styled.div`
 
 export const ListHeader = styled.div`
   display: grid;
-  grid-template-columns: 1fr 100px 50px 30px;
+  grid-template-columns: var(--grid-columns);
   font-size: 12px;
   font-weight: 500;
   color: var(${UI.COLOR_TEXT_OPACITY_70});
-  margin: 0 20px 15px 20px;
+  margin: 0 25px 15px 20px;
 `
 
 export const ListItem = styled.div`
   display: grid;
-  grid-template-columns: 1fr 100px 50px 20px;
+  grid-template-columns: var(--grid-columns);
   padding: 10px 20px;
   cursor: pointer;
 
@@ -39,48 +42,102 @@ export const ListItem = styled.div`
 
 export const LpTokenLogo = styled.div`
   --size: 36px;
-  --halfSize: 18px;
-
+  position: relative;
   width: var(--size);
   height: var(--size);
-  position: relative;
+  object-fit: contain;
 
-  > div {
-    position: absolute;
-    width: var(--halfSize);
-    overflow: hidden;
+  ${Media.upToSmall()} {
+    --size: 32px;
   }
 
-  > div:last-child {
-    right: -1px;
-  }
-
-  > div:last-child > div {
-    right: 100%;
+  ${TokenLogoWrapper} {
     position: relative;
+    z-index: 1;
+    border-radius: var(--size);
+    width: var(--size);
+    height: var(--size);
+    min-width: var(--size);
+    min-height: var(--size);
+    font-size: var(--size);
+  }
+
+  ${TokenLogoWrapper}:nth-child(2) {
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 2;
+    clip-path: inset(0 0 0 50%);
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 2px;
+    height: 100%;
+    background-color: var(${UI.COLOR_PAPER});
+    transform: translateX(-50%);
+    z-index: 3;
   }
 `
 
 export const LpTokenInfo = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 2px;
+
+  > strong {
+    font-weight: 600;
+  }
 
   > p {
     margin: 0;
-    font-size: 13px;
-    color: var(${UI.COLOR_TEXT_OPACITY_70});
+    font-size: 12px;
+    color: var(${UI.COLOR_TEXT_OPACITY_60});
+    letter-spacing: -0.02rem;
   }
+`
+
+export const LpTokenYieldPercentage = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 600;
+`
+
+export const LpTokenBalance = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  letter-spacing: -0.02rem;
+  color: var(${UI.COLOR_TEXT_OPACITY_70});
+  padding: 0 8px 0 0;
+`
+
+export const LpTokenTooltip = styled.div`
+  display: flex;
+  align-items: center;
+  margin: auto;
 `
 
 export const NoPoolWrapper = styled.div`
   border-top: 1px solid var(${UI.COLOR_BORDER});
   color: var(${UI.COLOR_TEXT_OPACITY_70});
-  padding: 20px 0 10px 0;
+  padding: 20px 20px 0;
   display: flex;
-  flex-direction: column;
-  gap: 16px;
+  flex-flow: row wrap;
+  width: 100%;
+  gap: 10px;
   align-items: center;
+  justify-content: space-between;
   font-size: 13px;
+  margin: auto 0 0;
+
+  > div {
+    flex: 1;
+  }
 `
 
 export const ArrowUpRight = styled(ArrowRight)`
@@ -102,4 +159,43 @@ export const CreatePoolLink = styled.a`
   &:hover {
     opacity: 0.8;
   }
+`
+
+export const EmptyList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 200px;
+  font-size: 16px;
+  color: var(${UI.COLOR_TEXT_OPACITY_70});
+`
+
+export const MobileCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: var(${UI.COLOR_PAPER});
+  padding: 20px;
+  border-bottom: 1px solid var(${UI.COLOR_PAPER_DARKER});
+`
+
+export const MobileCardRow = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 8px;
+  gap: 8px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
+
+export const MobileCardLabel = styled.span`
+  font-size: 14px;
+  color: var(${UI.COLOR_TEXT_OPACITY_70});
+`
+
+export const MobileCardValue = styled.span`
+  font-size: 16px;
+  font-weight: 600;
 `
