1feda355d32e3d8037ced3886214f65766a89edf
Anxo Rodriguez <anxolin@gmail.com>
2024-11-14 16:53:58 +0700

    fix: reorder imports

--- apps/cowswap-frontend/src/pages/Hooks/cowShed.tsx
+++ apps/cowswap-frontend/src/pages/Hooks/cowShed.tsx
@@ -1,6 +1,7 @@
+import { RescueFundsFromProxy } from 'modules/hooksStore/containers/RescueFundsFromProxy'
+
 import { useNavigateBack } from 'common/hooks/useNavigate'
 
-import { RescueFundsFromProxy } from 'modules/hooksStore/containers/RescueFundsFromProxy'
 
 export function CowShed() {
   const navigateBack = useNavigateBack()
