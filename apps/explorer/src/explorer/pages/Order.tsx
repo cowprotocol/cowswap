44130f1047b80084566757352c37e923248a8092
tienshaoku <tienshaoku@gmail.com>
2024-11-18 12:59:46 +0700

    feat(explorer): add search bar in order view

--- apps/explorer/src/explorer/pages/Order.tsx
+++ apps/explorer/src/explorer/pages/Order.tsx
@@ -6,15 +6,22 @@ import { Wrapper as WrapperMod } from './styled'
 import RedirectToSearch from '../../components/RedirectToSearch'
 import { useOrderIdParam } from '../../hooks/useSanitizeOrderIdAndUpdateUrl'
 import { isAnOrderId } from '../../utils'
+import { Search } from '../components/common/Search'
 import { OrderWidget } from '../components/OrderWidget'
 import { APP_TITLE } from '../const'
 
 const Wrapper = styled(WrapperMod)`
   max-width: 140rem;
+  padding: 1.6rem;
 
   > h1 {
     padding: 2.4rem 0 0.75rem;
   }
+`
+
+const StyledSearch = styled(Search)`
+  width: 100%;
+  max-width: 60rem;
 `
 
 const Order = () => {
@@ -29,6 +36,7 @@ const Order = () => {
       <Helmet>
         <title>Order Details - {APP_TITLE}</title>
       </Helmet>
+      <StyledSearch />
       <OrderWidget />
     </Wrapper>
   )
