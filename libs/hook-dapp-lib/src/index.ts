8a8484c58acd8a327d6749b2180c504e873a135a
Alexandr Kazachenko <shoom3301@gmail.com>
2024-11-19 11:22:52 +0500

    chore: fix hooks store registry import (#5109)

--- libs/hook-dapp-lib/src/index.ts
+++ libs/hook-dapp-lib/src/index.ts
@@ -3,5 +3,5 @@ export * from './hookDappIframeTransport'
 export * from './types'
 export * from './consts'
 export * from './utils'
-import * as hookDappsRegistry from './hookDappsRegistry.json'
+import hookDappsRegistry from './hookDappsRegistry.json'
 export { hookDappsRegistry }
