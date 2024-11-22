e18bd5d37c05649c2a0d41ffacafae2291d14124
tienshaoku <tienshaoku@gmail.com>
2024-11-22 18:12:47 +0800

    feat: search bar added to all views

--- apps/explorer/src/explorer/pages/Order.tsx
+++ apps/explorer/src/explorer/pages/Order.tsx
@@ -1,27 +1,20 @@
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components/macro'
 
-import { Wrapper as WrapperMod } from './styled'
+import { Wrapper as WrapperMod, StyledSearch } from './styled'
 
 import RedirectToSearch from '../../components/RedirectToSearch'
 import { useOrderIdParam } from '../../hooks/useSanitizeOrderIdAndUpdateUrl'
 import { isAnOrderId } from '../../utils'
-import { Search } from '../components/common/Search'
 import { OrderWidget } from '../components/OrderWidget'
 import { APP_TITLE } from '../const'
 
 const Wrapper = styled(WrapperMod)`
   max-width: 140rem;
-  padding: 1.6rem;
 
   > h1 {
     padding: 2.4rem 0 0.75rem;
   }
-`
-
-const StyledSearch = styled(Search)`
-  width: 100%;
-  max-width: 60rem;
 `
 
 const Order = () => {
