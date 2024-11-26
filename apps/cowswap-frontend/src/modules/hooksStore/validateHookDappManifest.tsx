0f2e2db13ed64be94d7de5db60cd70bd2019435a
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2024-11-26 13:45:19 +0000

    feat: refactor manifest validation

--- apps/cowswap-frontend/src/modules/hooksStore/validateHookDappManifest.tsx
+++ apps/cowswap-frontend/src/modules/hooksStore/validateHookDappManifest.tsx
@@ -20,34 +20,41 @@ export function validateHookDappManifest(
 ): ReactElement | string | null {
   const { conditions = {}, ...dapp } = data
 
-  if (dapp) {
-    const emptyFields = MANDATORY_DAPP_FIELDS.filter((field) => typeof dapp[field] === 'undefined')
-
-    if (emptyFields.length > 0) {
-      return ERROR_MESSAGES.MISSING_REQUIRED_FIELDS(emptyFields)
-    } else {
-      if (
-        isSmartContractWallet === true &&
-        typeof conditions.walletCompatibility !== 'undefined' &&
-        !conditions.walletCompatibility.includes(HookDappWalletCompatibility.SMART_CONTRACT)
-      ) {
-        return ERROR_MESSAGES.SMART_CONTRACT_INCOMPATIBLE
-      } else if (!isHex(dapp.id) || dapp.id.length !== HOOK_DAPP_ID_LENGTH) {
-        return ERROR_MESSAGES.INVALID_HOOK_ID
-      } else if (chainId && conditions.supportedNetworks && !conditions.supportedNetworks.includes(chainId)) {
-        return ERROR_MESSAGES.NETWORK_COMPATIBILITY_ERROR(
-          chainId,
-          getChainInfo(chainId).label,
-          conditions.supportedNetworks.map((id) => ({ id, label: getChainInfo(id).label })),
-        )
-      } else if (conditions.position === 'post' && isPreHook === true) {
-        return ERROR_MESSAGES.HOOK_POSITION_MISMATCH('post')
-      } else if (conditions.position === 'pre' && isPreHook === false) {
-        return ERROR_MESSAGES.HOOK_POSITION_MISMATCH('pre')
-      }
-    }
-  } else {
+  if (!dapp) {
     return ERROR_MESSAGES.INVALID_MANIFEST
+  }
+
+  const emptyFields = MANDATORY_DAPP_FIELDS.filter((field) => typeof dapp[field] === 'undefined')
+  if (emptyFields.length > 0) {
+    return ERROR_MESSAGES.MISSING_REQUIRED_FIELDS(emptyFields)
+  }
+
+  if (
+    isSmartContractWallet === true &&
+    typeof conditions.walletCompatibility !== 'undefined' &&
+    !conditions.walletCompatibility.includes(HookDappWalletCompatibility.SMART_CONTRACT)
+  ) {
+    return ERROR_MESSAGES.SMART_CONTRACT_INCOMPATIBLE
+  }
+
+  if (!isHex(dapp.id) || dapp.id.length !== HOOK_DAPP_ID_LENGTH) {
+    return ERROR_MESSAGES.INVALID_HOOK_ID
+  }
+
+  if (chainId && conditions.supportedNetworks && !conditions.supportedNetworks.includes(chainId)) {
+    return ERROR_MESSAGES.NETWORK_COMPATIBILITY_ERROR(
+      chainId,
+      getChainInfo(chainId).label,
+      conditions.supportedNetworks.map((id) => ({ id, label: getChainInfo(id).label })),
+    )
+  }
+
+  if (conditions.position === 'post' && isPreHook === true) {
+    return ERROR_MESSAGES.HOOK_POSITION_MISMATCH('post')
+  }
+
+  if (conditions.position === 'pre' && isPreHook === false) {
+    return ERROR_MESSAGES.HOOK_POSITION_MISMATCH('pre')
   }
 
   return null
