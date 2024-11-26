049a5b076bb9ba2fb64d4d051bf4a386a20685f9
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2024-11-26 14:57:32 +0000

    feat: cleanup util function

--- libs/common-utils/src/fetch/index.ts
+++ libs/common-utils/src/fetch/index.ts
@@ -1,19 +1,21 @@
 import { getTimeoutAbortController } from '../request'
 
+export const TIMEOUT_ERROR_MESSAGE = 'Request timed out. Please try again.'
+
 interface FetchTimeoutOptions extends RequestInit {
-  timeout: number
+  timeout?: number
   timeoutMessage?: string
 }
 
-export async function fetchWithTimeout(url: string, options: FetchTimeoutOptions) {
-  const { timeout, timeoutMessage, ...fetchOptions } = options
+export async function fetchWithTimeout(url: string, options: FetchTimeoutOptions = {}) {
+  const { timeout = 30000, timeoutMessage = TIMEOUT_ERROR_MESSAGE, ...fetchOptions } = options
 
   try {
     const response = await fetch(url, { signal: getTimeoutAbortController(timeout).signal, ...fetchOptions })
     return response
   } catch (error) {
-    if (error instanceof Error && error.name === 'AbortError') {
-      throw new Error(timeoutMessage || 'Request timeout')
+    if (error?.name === 'AbortError') {
+      throw new Error(timeoutMessage)
     }
     throw error
   }
