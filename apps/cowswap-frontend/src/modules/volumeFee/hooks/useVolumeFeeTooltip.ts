f058ea049bec158968831e14242647b2f6bf049a
Alexandr Kazachenko <shoom3301@gmail.com>
2024-11-29 17:19:39 +0500

    chore: fix tooltip

--- apps/cowswap-frontend/src/modules/volumeFee/hooks/useVolumeFeeTooltip.ts
+++ apps/cowswap-frontend/src/modules/volumeFee/hooks/useVolumeFeeTooltip.ts
@@ -4,7 +4,7 @@ import { useInjectedWidgetParams } from '../../injectedWidget'
 import { safeAppFeeAtom } from '../state/safeAppFeeAtom'
 
 const SAFE_FEE_TOOLTIP =
-  'The [tiered widget fee](https://help.safe.global/en/articles/178530-how-does-the-widget-fee-work-for-native-swaps) incurred here is charged by CoW Protocol for the operation of this widget. The fee is automatically calculated into this quote. Part of the fee will contribute to a license fee that supports the Safe Community. Neither the Safe Ecosystem Foundation nor Safe{Wallet} operate the CoW Swap Widget and/or CoW Swap'
+  'The [Safe App Fee](https://help.safe.global/en/articles/178530-how-does-the-widget-fee-work-for-native-swaps) incurred here is charged by the Safe Foundation for the display of the app within their Safe Store. The fee is automatically calculated in this quote. Part of the fees will contribute to the CoW DAO treasury that supports the CoW Community.'
 
 export function useVolumeFeeTooltip() {
   const safeAppFee = useAtomValue(safeAppFeeAtom)
