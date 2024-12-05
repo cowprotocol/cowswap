52f299864b7a6e3fbe01d3ad5aad4761410058dd
Alexandr Kazachenko <shoom3301@gmail.com>
2024-12-05 20:15:55 +0500

    feat(cowfi): upgrade 404 page

--- apps/cow-fi/app/not-found.tsx
+++ apps/cow-fi/app/not-found.tsx
@@ -1,4 +1,5 @@
-import { GetStaticProps } from 'next'
+'use client'
+
 import { Color } from '@cowprotocol/ui'
 
 import Layout from '@/components/Layout'
@@ -6,27 +7,21 @@ import { Link } from '@/components/Link'
 
 import { PageWrapper, ContainerCard, ArticleContent, ArticleMainTitle, BodyContent } from '@/styles/styled'
 
-import { CONFIG, DATA_CACHE_TIME_SECONDS } from '@/const/meta'
-import { clickOnError404GoHome } from 'modules/analytics'
+import { clickOnError404GoHome } from '../modules/analytics'
 
-interface PageProps {
-  siteConfigData: typeof CONFIG
-}
-
-export default function Page({ siteConfigData }: PageProps) {
-  const { title } = siteConfigData
-
+export default function Page() {
   return (
     <Layout
       bgColor={Color.neutral90}
       metaTitle="404 - Page Not Found"
       metaDescription="This page could not be found. Please go back to the homepage or use the navigation menu to find what you are looking for."
     >
+      <title>404 - Page Not Found</title>
       <PageWrapper>
         <ContainerCard bgColor={'transparent'} minHeight="70vh" gap={62} gapMobile={42} centerContent touchFooter>
           <ArticleContent maxWidth="90rem">
             <ArticleMainTitle margin={'0 0 62px'} fontSize={52}>
-              {title}
+              404 - Page Not Found
             </ArticleMainTitle>
 
             <BodyContent>
@@ -43,17 +38,4 @@ export default function Page({ siteConfigData }: PageProps) {
       </PageWrapper>
     </Layout>
   )
-}
-
-export const getStaticProps: GetStaticProps<PageProps> = async () => {
-  return {
-    props: {
-      siteConfigData: {
-        ...CONFIG,
-        title: '404 - Page Not Found',
-        descriptionShort: '404 - Page Not Found',
-      },
-    },
-    revalidate: DATA_CACHE_TIME_SECONDS,
-  }
 }
