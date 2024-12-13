cb7d0e17beb817d6b795c08829e8fc6195c70f58
Alexandr Kazachenko <shoom3301@gmail.com>
2024-12-13 19:29:15 +0500

    chore: fix pendingOrders

--- apps/cowswap-frontend/src/pages/AdvancedOrders/index.tsx
+++ apps/cowswap-frontend/src/pages/AdvancedOrders/index.tsx
@@ -1,5 +1,7 @@
 import { useAtomValue } from 'jotai'
 
+import { PENDING_STATES } from 'legacy/state/orders/actions'
+
 import {
   advancedOrdersAtom,
   AdvancedOrdersWidget,
@@ -31,12 +33,11 @@ export default function AdvancedOrdersPage() {
   const twapFormValidation = useTwapFormState()
   const twapSlippage = useTwapSlippage()
   const mapTwapCurrencyInfo = useMapTwapCurrencyInfo()
+  const { hideOrdersTable } = useInjectedWidgetParams()
 
   const disablePriceImpact = twapFormValidation === TwapFormState.SELL_AMOUNT_TOO_SMALL
-
   const advancedWidgetParams = { disablePriceImpact }
-
-  const { hideOrdersTable } = useInjectedWidgetParams()
+  const pendingOrders = allEmulatedOrders.filter((order) => PENDING_STATES.includes(order.status))
 
   return (
     <>
@@ -66,7 +67,7 @@ export default function AdvancedOrdersPage() {
               orderType={TabOrderTypes.ADVANCED}
               orders={allEmulatedOrders}
             >
-              {isFallbackHandlerRequired && allEmulatedOrders.length > 0 && <SetupFallbackHandlerWarning />}
+              {isFallbackHandlerRequired && pendingOrders.length > 0 && <SetupFallbackHandlerWarning />}
             </OrdersTableWidget>
           )}
         </styledEl.SecondaryWrapper>
