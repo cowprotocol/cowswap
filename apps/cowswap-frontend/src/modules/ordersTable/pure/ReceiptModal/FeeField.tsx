4c289d51dd553320f58fe2afbe4a447f102d00f6
Alfetopito <alfetopito@users.noreply.github.com>
2024-12-31 09:59:39 +0000

    refactor: moved getFeeToken out and improved logic as suggested

--- apps/cowswap-frontend/src/modules/ordersTable/pure/ReceiptModal/FeeField.tsx
+++ apps/cowswap-frontend/src/modules/ordersTable/pure/ReceiptModal/FeeField.tsx
@@ -1,31 +1,13 @@
 import { TokenAmount } from '@cowprotocol/ui'
 import { CurrencyAmount } from '@uniswap/sdk-core'
 
+import { getFeeToken } from 'modules/ordersTable/utils/getFeeToken'
+
 import { ParsedOrder } from 'utils/orderUtils/parseOrder'
 
 import * as styledEl from './styled'
 
 export type Props = { order: ParsedOrder }
-
-// TODO: get rid of this once https://github.com/cowprotocol/services/pull/3184 is complete
-const HAS_BACKEND_MIGRATED = false
-
-function getFeeToken(order: ParsedOrder) {
-  if (!HAS_BACKEND_MIGRATED) {
-    return order.inputToken
-  }
-
-  const { inputToken, outputToken } = order
-  const { executedFeeToken } = order.executionData
-
-  if (inputToken?.address.toLowerCase() === executedFeeToken?.toLowerCase()) {
-    return inputToken
-  }
-  if (outputToken?.address.toLowerCase() === executedFeeToken?.toLowerCase()) {
-    return outputToken
-  }
-  return undefined
-}
 
 export function FeeField({ order }: Props): JSX.Element | null {
   const { totalFee } = order.executionData
