698f5783771ce066c4d711bf5724be0e80369071
Alfetopito <alfetopito@users.noreply.github.com>
2025-01-08 15:31:00 +0000

    feat: simplify text

--- apps/cowswap-frontend/src/modules/swap/pure/banners/PartialApprovalBanner.tsx
+++ apps/cowswap-frontend/src/modules/swap/pure/banners/PartialApprovalBanner.tsx
@@ -3,11 +3,8 @@ import { Command } from '@cowprotocol/types'
 import { BannerOrientation, ClosableBanner, InlineBanner, UnderlinedLinkStyledButton } from '@cowprotocol/ui'
 
 import styled from 'styled-components/macro'
-import * as timeago from 'timeago.js'
 
 const BANNER_STORAGE_KEY = 'partialPermitBannerKey:v0'
-
-const YEARS_SINCE_DEPLOYMENT = timeago.format(new Date('2021-06-08')).replace(/ ago/, '') // mainnet contract deployment date https://etherscan.io/tx/0xf49f90aa5a268c40001d1227b76bb4dd8247f18361fcad9fffd4a7a44f1320d3
 
 type PartialApprovalBannerProps = {
   isApprovalNeeded?: boolean
@@ -28,9 +25,7 @@ export function PartialApprovalBanner({ isApprovalNeeded, openSettings }: Partia
       onClose={onClose}
     >
       <p>
-        <b>NEW: </b>You can now choose to do only minimal approvals in the <Link onClick={openSettings}>settings</Link>.
-        When enabled, every order placed that needs approval will request only the minimum necessary to trade. When
-        disabled, you can enjoy the same trusted experience CoW Swap has provided for the past {YEARS_SINCE_DEPLOYMENT}.
+        <b>NEW: </b>You can now choose to do minimal token approvals in the <Link onClick={openSettings}>settings</Link>.
       </p>
     </InlineBanner>
   ))
