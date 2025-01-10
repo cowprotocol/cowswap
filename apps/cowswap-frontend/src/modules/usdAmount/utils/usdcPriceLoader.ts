90c400dc65df1155e7f491d5f1c14f6a9bd08e82
Alfetopito <alfetopito@users.noreply.github.com>
2025-01-10 18:05:02 +0000

    refactor: move usdcPriceLoader to its own file

---
+++ apps/cowswap-frontend/src/modules/usdAmount/utils/usdcPriceLoader.ts
@@ -0,0 +1,18 @@
+import { USDC } from '@cowprotocol/common-const'
+import { SupportedChainId } from '@cowprotocol/cow-sdk'
+import { Fraction } from '@uniswap/sdk-core'
+
+import { getCowProtocolNativePrice } from '../apis/getCowProtocolNativePrice'
+
+export function usdcPriceLoader(chainId: SupportedChainId): () => Promise<Fraction | null> {
+  let usdcPricePromise: Promise<Fraction | null> | null = null
+
+  return () => {
+    // Cache the result to avoid fetching it multiple times
+    if (!usdcPricePromise) {
+      usdcPricePromise = getCowProtocolNativePrice(USDC[chainId])
+    }
+
+    return usdcPricePromise
+  }
+}
