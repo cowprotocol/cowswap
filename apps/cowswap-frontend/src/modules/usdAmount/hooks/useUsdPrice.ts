ab45749dd427e58d11a5fe42abd981e3fcc44f05
Alfetopito <alfetopito@users.noreply.github.com>
2025-01-15 17:02:30 +0000

    feat: further refactor SpotPrice updater
    Use UsdPriceUpdater instead of triggering new requests from here

--- apps/cowswap-frontend/src/modules/usdAmount/hooks/useUsdPrice.ts
+++ apps/cowswap-frontend/src/modules/usdAmount/hooks/useUsdPrice.ts
@@ -6,9 +6,13 @@ import { Nullish } from 'types'
 
 import { useSafeEffect } from 'common/hooks/useSafeMemo'
 
+import { useEffect, useMemo } from 'react'
 import { addCurrencyToUsdPriceQueue, removeCurrencyToUsdPriceFromQueue } from '../state/usdRawPricesAtom'
-import { usdTokenPricesAtom, UsdPriceState } from '../state/usdTokenPricesAtom'
+import { UsdPriceState, usdTokenPricesAtom } from '../state/usdTokenPricesAtom'
 
+/**
+ * Subscribe to USD price for a single currency and returns the USD price state
+ */
 export function useUsdPrice(currency: Nullish<Token>): UsdPriceState | null {
   const currencyAddress = currency?.address?.toLowerCase()
 
@@ -35,4 +39,55 @@ export function useUsdPrice(currency: Nullish<Token>): UsdPriceState | null {
   if (!price || price.currency.chainId !== currency?.chainId) return null
 
   return price
+}
+
+/**
+ * Subscribe to USD prices for multiple currencies, returns void
+ */
+function useSubscribeUsdPrices(currencies: Token[]): void {
+  const addCurrencyToUsdPrice = useSetAtom(addCurrencyToUsdPriceQueue)
+  const removeCurrencyToUsdPrice = useSetAtom(removeCurrencyToUsdPriceFromQueue)
+
+  useEffect(() => {
+    // Avoid subscribing to the same currency multiple times
+    const seenCurrencies = new Set<string>()
+
+    currencies.forEach((currency) => {
+      if (seenCurrencies.has(currency?.address?.toLowerCase())) {
+        return
+      }
+
+      addCurrencyToUsdPrice(currency)
+      seenCurrencies.add(currency?.address?.toLowerCase())
+    })
+
+    return () => {
+      currencies.forEach((currency) => {
+        if (seenCurrencies.has(currency?.address?.toLowerCase())) {
+          removeCurrencyToUsdPrice(currency)
+          seenCurrencies.delete(currency?.address?.toLowerCase())
+        }
+      })
+    }
+  }, [currencies])
+}
+
+/**
+ * Subscribe to USD prices for multiple currencies and returns the USD prices state
+ */
+export function useUsdPrices(currencies: Token[]): Record<string, UsdPriceState | null> {
+  useSubscribeUsdPrices(currencies)
+  const usdPrices = useAtomValue(usdTokenPricesAtom)
+
+  return useMemo(
+    () =>
+      currencies.reduce<Record<string, UsdPriceState | null>>((acc, currency) => {
+        const currencyAddress = currency.address.toLowerCase()
+
+        acc[currencyAddress] = usdPrices[currencyAddress] || null
+
+        return acc
+      }, {}),
+    [currencies, usdPrices],
+  )
 }
