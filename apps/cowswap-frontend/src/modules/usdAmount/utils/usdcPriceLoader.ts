af309d65002d27a1a6b7698cbc71000b7ee00b31
Alfetopito <alfetopito@users.noreply.github.com>
2025-01-15 14:51:15 +0000

    fix: remove promise caching cache is done at api level
    Caching added on https://github.com/cowprotocol/cowswap/pull/5266/files

--- apps/cowswap-frontend/src/modules/usdAmount/utils/usdcPriceLoader.ts
+++ apps/cowswap-frontend/src/modules/usdAmount/utils/usdcPriceLoader.ts
@@ -5,14 +5,5 @@ import { Fraction } from '@uniswap/sdk-core'
 import { getCowProtocolNativePrice } from '../apis/getCowProtocolNativePrice'
 
 export function usdcPriceLoader(chainId: SupportedChainId): () => Promise<Fraction | null> {
-  let usdcPricePromise: Promise<Fraction | null> | null = null
-
-  return async () => {
-    // Cache the result to avoid fetching it multiple times
-    if (!usdcPricePromise) {
-      usdcPricePromise = getCowProtocolNativePrice(USDC[chainId])
-    }
-
-    return usdcPricePromise
-  }
+  return () => getCowProtocolNativePrice(USDC[chainId])
 }
