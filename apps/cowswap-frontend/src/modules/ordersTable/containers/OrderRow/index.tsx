64171c06fadcf596269c6714365754ac263fc79f
Alexandr Kazachenko <shoom3301@gmail.com>
2025-01-24 19:09:34 +0500

    fix(orders-table): fix limit price inverting (#5343)
    * fix(orders-table): fix limit price inverting
    
    * chore: fix lint

--- apps/cowswap-frontend/src/modules/ordersTable/containers/OrderRow/index.tsx
+++ apps/cowswap-frontend/src/modules/ordersTable/containers/OrderRow/index.tsx
@@ -122,12 +122,13 @@ export function OrderRow({
   )
   const activityUrl = chainId ? getActivityUrl(chainId, order) : undefined
 
-  const [isInverted, setIsInverted] = useState(() => {
+  const isInvertedState = useState(() => {
     // On mount, apply smart quote selection
     const quoteCurrency = getQuoteCurrency(chainId, inputCurrencyAmount, outputCurrencyAmount)
     return getAddress(quoteCurrency) === getAddress(inputCurrencyAmount?.currency)
   })
-  const toggleIsInverted = useCallback(() => setIsInverted((curr) => !curr), [])
+  const [isInverted, setIsInverted] = isInvertedState
+  const toggleIsInverted = useCallback(() => setIsInverted((curr) => !curr), [setIsInverted])
 
   // Toggle isInverted whenever isGloballyInverted changes
   useEffect(() => {
@@ -223,10 +224,10 @@ export function OrderRow({
       </styledEl.CurrencyCell>
 
       {/* Limit price */}
-      <styledEl.PriceElement onClick={toggleIsInverted}>
+      <styledEl.PriceElement>
         <RateInfo
           prependSymbol={false}
-          isInvertedState={[isInverted, setIsInverted]}
+          isInvertedState={isInvertedState}
           noLabel={true}
           doNotUseSmartQuote
           isInverted={isInverted}
