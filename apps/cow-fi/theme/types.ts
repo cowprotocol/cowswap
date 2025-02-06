7380731136523a471e5eb4f03e3079ea640b0b87
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-02-06 12:47:46 +0000

    refactor: simplify theme type declarations for styled-components

--- apps/cow-fi/theme/types.ts
+++ apps/cow-fi/theme/types.ts
@@ -1,7 +1,5 @@
 import type { CowProtocolTheme } from '@cowprotocol/ui'
 
 declare module 'styled-components' {
-  export interface CowFiDefaultTheme extends CowProtocolTheme {}
-
-  export interface DefaultTheme extends CowFiDefaultTheme {}
+  export interface DefaultTheme extends CowProtocolTheme {}
 }
