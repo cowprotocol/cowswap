969de0188fdef08cfcc7b2e45104af43f178ec59
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-02-13 12:24:03 +0000

    fix: update delegate URL with correct path

--- apps/cowswap-frontend/src/pages/Account/constants.ts
+++ apps/cowswap-frontend/src/pages/Account/constants.ts
@@ -1,5 +1,5 @@
 export const DELEGATE_URL = (): string => {
-  const url = new URL('https://cow.fi/learn/cow-dao-launches-delegate-program-for-token-holders')
+  const url = new URL('https://cow.fi/learn/cowdao-launches-delegate-program-for-token-holders')
   url.searchParams.set('utm_source', 'cowswap')
   url.searchParams.set('utm_medium', 'account_page')
   url.searchParams.set('utm_campaign', 'delegate')
