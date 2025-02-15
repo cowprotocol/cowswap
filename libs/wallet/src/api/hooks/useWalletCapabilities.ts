037aba97ee58d1c23cb540425754e085a75bdcfd
Alexandr Kazachenko <shoom3301@gmail.com>
2025-02-15 15:14:11 +0500

    fix: do not check capabilities in mobile wc

--- libs/wallet/src/api/hooks/useWalletCapabilities.ts
+++ libs/wallet/src/api/hooks/useWalletCapabilities.ts
@@ -1,9 +1,11 @@
 import { SWR_NO_REFRESH_OPTIONS } from '@cowprotocol/common-const'
+import { isMobile } from '@cowprotocol/common-utils'
 import { useWalletProvider } from '@cowprotocol/wallet-provider'
 
 import ms from 'ms.macro'
 import useSWR, { SWRResponse } from 'swr'
 
+import { useIsWalletConnect } from '../../web3-react/hooks/useIsWalletConnect'
 import { useWalletInfo } from '../hooks'
 
 export type WalletCapabilities = {
@@ -14,10 +16,16 @@ const requestTimeout = ms`10s`
 
 export function useWalletCapabilities(): SWRResponse<WalletCapabilities | undefined> {
   const provider = useWalletProvider()
+  const isWalletConnect = useIsWalletConnect()
   const { chainId, account } = useWalletInfo()
+  /**
+   * Walletconnect in mobile browsers initiates a request with confirmation to the wallet
+   * to get the capabilities. It breaks the flow with perpetual reuqests.
+   */
+  const shouldCheckCapabilities = !(isWalletConnect && isMobile)
 
   return useSWR(
-    provider && account && chainId ? [provider, account, chainId] : null,
+    shouldCheckCapabilities && provider && account && chainId ? [provider, account, chainId] : null,
     ([provider, account, chainId]) => {
       return new Promise((resolve) => {
         const timeout = setTimeout(() => {
