3d8e7f608a3b738145ca85cbf6ab375a2eb3fc4f
Alexandr Kazachenko <shoom3301@gmail.com>
2025-02-19 17:49:54 +0500

    feat(swap): migrate MetamaskTransactionWarning banner from legacy

--- apps/cowswap-frontend/src/modules/swap2/containers/Warnings/index.tsx
+++ apps/cowswap-frontend/src/modules/swap2/containers/Warnings/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-import { HighFeeWarning } from 'modules/tradeWidgetAddons'
+import { HighFeeWarning, MetamaskTransactionWarning } from 'modules/tradeWidgetAddons'
 import { SellNativeWarningBanner } from 'modules/tradeWidgetAddons'
 
+import { useSwapDerivedState } from '../../hooks/useSwapDerivedState'
 import { SwapFormState, useSwapFormState } from '../../hooks/useSwapFormState'
 
 export function Warnings() {
+  const { inputCurrency } = useSwapDerivedState()
   const localFormValidation = useSwapFormState()
   const isNativeSellInHooksStore = localFormValidation === SwapFormState.SellNativeInHooks
 
   return (
     <>
+      {inputCurrency && !isNativeSellInHooksStore && <MetamaskTransactionWarning sellToken={inputCurrency} />}
       {isNativeSellInHooksStore && <SellNativeWarningBanner />}
       <HighFeeWarning />
     </>
