19f2f781fce6a2dec152d37412f7849775699759
Alexandr Kazachenko <shoom3301@gmail.com>
2025-02-19 17:44:29 +0500

    feat(swap): migrate SellNativeInHooks state from legacy

--- apps/cowswap-frontend/src/modules/swap2/containers/Warnings/index.tsx
+++ apps/cowswap-frontend/src/modules/swap2/containers/Warnings/index.tsx
@@ -1,8 +1,17 @@
+import React from 'react'
+
 import { HighFeeWarning } from 'modules/tradeWidgetAddons'
+import { SellNativeWarningBanner } from 'modules/tradeWidgetAddons'
+
+import { SwapFormState, useSwapFormState } from '../../hooks/useSwapFormState'
 
 export function Warnings() {
+  const localFormValidation = useSwapFormState()
+  const isNativeSellInHooksStore = localFormValidation === SwapFormState.SellNativeInHooks
+
   return (
     <>
+      {isNativeSellInHooksStore && <SellNativeWarningBanner />}
       <HighFeeWarning />
     </>
   )
