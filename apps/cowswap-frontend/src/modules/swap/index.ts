20ccbc9d5111130e3e93dfcda561d54de910fadd
Alexandr Kazachenko <shoom3301@gmail.com>
2025-02-20 18:01:26 +0500

    refactor: remove dead code

--- apps/cowswap-frontend/src/modules/swap/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './containers/SwapWidget'
-export * from './containers/SwapUpdaters'
-export * from './updaters/SwapDerivedStateUpdater'
-export * from './updaters/SwapAmountsFromUrlUpdater'
-export * from '../tradeSlippage/updaters/SmartSlippageUpdater'
-export * from './state/swapDerivedStateAtom'
