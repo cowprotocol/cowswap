0a80686957560e64dff7f61ae019f3a5e6bba49b
Alexandr Kazachenko <shoom3301@gmail.com>
2025-02-24 19:29:56 +0500

    fix(swap): display modified deadline for eth-flow

--- apps/cowswap-frontend/src/modules/swap/containers/SwapConfirmModal/index.tsx
+++ apps/cowswap-frontend/src/modules/swap/containers/SwapConfirmModal/index.tsx
@@ -15,7 +15,7 @@ import {
   useReceiveAmountInfo,
   useTradeConfirmActions,
 } from 'modules/trade'
-import { HighFeeWarning } from 'modules/tradeWidgetAddons'
+import { HighFeeWarning, RowDeadline } from 'modules/tradeWidgetAddons'
 
 import { useRateInfoParams } from 'common/hooks/useRateInfoParams'
 import { CurrencyPreviewInfo } from 'common/pure/CurrencyAmountPreview'
@@ -23,6 +23,7 @@ import { CurrencyPreviewInfo } from 'common/pure/CurrencyAmountPreview'
 import { useLabelsAndTooltips } from './useLabelsAndTooltips'
 
 import { useSwapDerivedState } from '../../hooks/useSwapDerivedState'
+import { useSwapDeadlineState } from '../../hooks/useSwapSettings'
 
 const CONFIRM_TITLE = 'Swap'
 const PRICE_UPDATE_INTERVAL = ms`30s`
@@ -53,6 +54,7 @@ export function SwapConfirmModal(props: SwapConfirmModalProps) {
   const receiveAmountInfo = useReceiveAmountInfo()
   const tradeConfirmActions = useTradeConfirmActions()
   const { slippage } = useSwapDerivedState()
+  const [deadline] = useSwapDeadlineState()
 
   const rateInfoParams = useRateInfoParams(inputCurrencyInfo.amount, outputCurrencyInfo.amount)
   const submittedContent = useOrderSubmittedContent(chainId)
@@ -88,7 +90,9 @@ export function SwapConfirmModal(props: SwapConfirmModalProps) {
                 hideLimitPrice
                 hideUsdValues
                 withTimelineDot={false}
-              ></TradeBasicConfirmDetails>
+              >
+                <RowDeadline deadline={deadline} />
+              </TradeBasicConfirmDetails>
             )}
             {restContent}
             <HighFeeWarning readonlyMode />
