6349be6212473f6ce73057b6aa946742adcef1b4
Alexandr Kazachenko <shoom3301@gmail.com>
2025-02-28 11:20:44 +0500

    chore: fix style

--- apps/cowswap-frontend/src/modules/hooksStore/containers/HooksStoreWidget/styled.tsx
+++ apps/cowswap-frontend/src/modules/hooksStore/containers/HooksStoreWidget/styled.tsx
@@ -4,4 +4,5 @@ export const TradeWidgetWrapper = styled.div<{ visible$: boolean }>`
   visibility: ${({ visible$ }) => (visible$ ? 'visible' : 'hidden')};
   height: ${({ visible$ }) => (visible$ ? '' : '0px')};
   width: ${({ visible$ }) => (visible$ ? '100%' : '0px')};
+  overflow: ${({ visible$ }) => (visible$ ? '' : 'hidden')};
 `
