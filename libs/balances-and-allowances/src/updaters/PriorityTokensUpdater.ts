9d991823997ef557e31e50b3ec03d64e32babdd6
Alexandr Kazachenko <shoom3301@gmail.com>
2025-03-14 19:36:23 +0500

    fix: throttle priority tokens fetching

--- libs/balances-and-allowances/src/updaters/PriorityTokensUpdater.ts
+++ libs/balances-and-allowances/src/updaters/PriorityTokensUpdater.ts
@@ -3,10 +3,12 @@ import type { SupportedChainId } from '@cowprotocol/cow-sdk'
 import ms from 'ms.macro'
 
 import { usePersistBalancesAndAllowances } from '../hooks/usePersistBalancesAndAllowances'
+import { useSwrConfigWithPause } from '../hooks/useSwrConfigWithPause'
 
 // A small gap between balances and allowances refresh intervals is needed to avoid high load to the node at the same time
 const BALANCES_SWR_CONFIG = { refreshInterval: ms`8s` }
 const ALLOWANCES_SWR_CONFIG = { refreshInterval: ms`11s` }
+const BALANCE_VALIDITY_PERIOD = ms`5s`
 
 export interface PriorityTokensUpdaterProps {
   account: string | undefined
@@ -15,10 +17,13 @@ export interface PriorityTokensUpdaterProps {
 }
 
 export function PriorityTokensUpdater(props: PriorityTokensUpdaterProps) {
+  const balancesSwrConfig = useSwrConfigWithPause(props.chainId, BALANCES_SWR_CONFIG, BALANCE_VALIDITY_PERIOD)
+  const allowancesSwrConfig = useSwrConfigWithPause(props.chainId, ALLOWANCES_SWR_CONFIG, BALANCE_VALIDITY_PERIOD)
+
   usePersistBalancesAndAllowances({
     ...props,
-    balancesSwrConfig: BALANCES_SWR_CONFIG,
-    allowancesSwrConfig: ALLOWANCES_SWR_CONFIG,
+    balancesSwrConfig,
+    allowancesSwrConfig,
   })
 
   return null
