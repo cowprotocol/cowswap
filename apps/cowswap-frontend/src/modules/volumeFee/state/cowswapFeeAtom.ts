4aa518e2bf640b0acac6201f773856fa9701554f
Leandro <alfetopito@users.noreply.github.com>
2025-03-18 13:19:23 +0000

    feat: bring arb1 feature flag back "Revert (#5500)" (#5519)
    This reverts commit 937686e5a3770f4d99ebc95e032966a8795b86dc.

--- apps/cowswap-frontend/src/modules/volumeFee/state/cowswapFeeAtom.ts
+++ apps/cowswap-frontend/src/modules/volumeFee/state/cowswapFeeAtom.ts
@@ -7,6 +7,8 @@ import { walletInfoAtom } from '@cowprotocol/wallet'
 
 import { derivedTradeStateAtom, TradeType, tradeTypeAtom } from 'modules/trade'
 
+import { featureFlagsAtom } from 'common/state/featureFlagsState'
+
 import { VolumeFee } from '../types'
 
 const COWSWAP_VOLUME_FEES: Record<SupportedChainId, VolumeFee | null> = {
@@ -24,7 +26,7 @@ const COWSWAP_VOLUME_FEES: Record<SupportedChainId, VolumeFee | null> = {
 }
 
 export const cowSwapFeeAtom = atom((get) => {
-  const { chainId } = get(walletInfoAtom)
+  const { chainId, account } = get(walletInfoAtom)
   const volumeFee = COWSWAP_VOLUME_FEES[chainId]
 
   // Early exit if fee is not set for this network
@@ -35,6 +37,7 @@ export const cowSwapFeeAtom = atom((get) => {
   const tradeState = get(derivedTradeStateAtom)
   const tradeTypeState = get(tradeTypeAtom)
   const isYieldWidget = tradeTypeState?.tradeType === TradeType.YIELD
+  const featureFlags = get(featureFlagsAtom)
 
   const { inputCurrency, outputCurrency } = tradeState || {}
 
@@ -47,6 +50,10 @@ export const cowSwapFeeAtom = atom((get) => {
   // No fee for Yield widget and LP tokens
   if (isYieldWidget || inputCurrency instanceof LpToken || outputCurrency instanceof LpToken) return null
 
+  // Don't use it when on arb1 and shouldn't apply fee based on percentage
+  if (chainId === SupportedChainId.ARBITRUM_ONE && !shouldApplyFee(account, featureFlags.arb1CowSwapFeePercentage))
+    return null
+
   const isInputTokenStable = STABLECOINS[chainId].has(getCurrencyAddress(inputCurrency).toLowerCase())
   const isOutputTokenStable = STABLECOINS[chainId].has(getCurrencyAddress(outputCurrency).toLowerCase())
 
@@ -55,3 +62,17 @@ export const cowSwapFeeAtom = atom((get) => {
 
   return volumeFee
 })
+
+function shouldApplyFee(account: string | undefined, percentage: number | boolean | undefined): boolean {
+  // Early exit for 100%, meaning should be enabled for everyone
+  if (percentage === 100) {
+    return true
+  }
+
+  // Falsy conditions
+  if (typeof percentage !== 'number' || !account || percentage < 0 || percentage > 100) {
+    return false
+  }
+
+  return BigInt(account) % 100n < percentage
+}
