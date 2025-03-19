50c65fc3b4b651534178f887c25657669ffd5f21
Leandro <alfetopito@users.noreply.github.com>
2025-03-19 10:31:11 +0000

    fix(wallets): show MM warning only to affected/unknown versions (#5510)
    * fix(wallets): show MM warning only to affected/unknown versions
    
    * chore: update msg to include fix version
    
    * fix: exclude mobile based MM wallets
    
    * chore: fix linting issues
    
    * chore: yet another linter fix
    
    * fix: start mm check with false state and only display banner if needed
    
    * fix: use proper type instead of any for provider
    
    * feat: display wallet version if available
    
    * fix: do not show the warning when connected to mobile via sdk

--- apps/cowswap-frontend/src/modules/tradeWidgetAddons/containers/MetamaskTransactionWarning/index.tsx
+++ apps/cowswap-frontend/src/modules/tradeWidgetAddons/containers/MetamaskTransactionWarning/index.tsx
@@ -5,19 +5,22 @@ import { getIsNativeToken } from '@cowprotocol/common-utils'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 import { ProviderMetaInfoPayload, WidgetEthereumProvider } from '@cowprotocol/iframe-transport'
 import { InlineBanner } from '@cowprotocol/ui'
-import { METAMASK_RDNS, useIsMetamaskBrowserExtensionWallet, useWalletDetails } from '@cowprotocol/wallet'
+import { METAMASK_RDNS, useIsMetamaskBrowserExtensionWallet } from '@cowprotocol/wallet'
 import { useWalletProvider } from '@cowprotocol/wallet-provider'
+import { ExternalProvider } from '@ethersproject/providers'
 import { Currency } from '@uniswap/sdk-core'
 
 import SVG from 'react-inlinesvg'
 import styled from 'styled-components/macro'
 
-const METAMASK_WALLET_NAME_REGEX = /metamask/i
-
 const Banner = styled(InlineBanner)`
   font-size: 14px;
   text-align: center;
   width: 100%;
+
+  b {
+    display: contents;
+  }
 `
 
 const NetworkInfo = styled.div`
@@ -26,36 +29,28 @@ const NetworkInfo = styled.div`
   gap: 8px;
 `
 
+const VERSION_WHERE_BUG_WAS_FIXED = '12.10.4' // Anything smaller than this version is potentially affected by the bug
+
 export function MetamaskTransactionWarning({ sellToken }: { sellToken: Currency }) {
-  const walletDetails = useWalletDetails()
-  const isMetamaskBrowserExtension = useIsMetamaskBrowserExtensionWallet()
-
-  const widgetProviderMetaInfo = useWidgetProviderMetaInfo()
-  const isMetamaskMobileInjectedBrowser = useIsMetamaskMobileInjectedWallet()
-
-  const isMetamaskViaWalletConnect = METAMASK_WALLET_NAME_REGEX.test(walletDetails.walletName || '')
-
-  const isWidgetMetamaskBrowserExtension = widgetProviderMetaInfo?.providerEip6963Info?.rdns === METAMASK_RDNS
-  const isWidgetMetamaskViaWalletConnect = METAMASK_WALLET_NAME_REGEX.test(
-    widgetProviderMetaInfo?.providerWcMetadata?.name || '',
-  )
-
-  const isMetamask =
-    isMetamaskBrowserExtension ||
-    isMetamaskViaWalletConnect ||
-    isMetamaskMobileInjectedBrowser ||
-    isWidgetMetamaskBrowserExtension ||
-    isWidgetMetamaskViaWalletConnect
   const isNativeSellToken = getIsNativeToken(sellToken)
 
-  if (!isMetamask || !isNativeSellToken) return null
+  const { shouldDisplayMetamaskWarning, currentVersion } = useShouldDisplayMetamaskWarning()
+
+  if (!shouldDisplayMetamaskWarning || !isNativeSellToken) return null
 
   const chainInfo = CHAIN_INFO[sellToken.chainId as SupportedChainId]
 
   return (
     <Banner bannerType="danger" iconSize={32}>
-      Issues have been reported with Metamask sending transactions to the wrong chain. Before you sign, please check in
-      your wallet that the transaction is being sent to the network:{' '}
+      {currentVersion && (
+        <>
+          Your Metamask extension (<b>v{currentVersion}</b>) is out of date.{' '}
+        </>
+      )}
+      <br />
+      Issues have been reported with Metamask sending transactions to the wrong chain on versions prior to{' '}
+      <b>v{VERSION_WHERE_BUG_WAS_FIXED}</b>. Before you sign, please check in your wallet that the transaction is being
+      sent to the network:{' '}
       <NetworkInfo>
         <SVG src={chainInfo.logo.light} height={24} width={24} /> <span>{chainInfo.label}</span>
       </NetworkInfo>
@@ -81,12 +76,109 @@ function useWidgetProviderMetaInfo() {
 }
 
 /**
- * This is hacky way to detect if the wallet is metamask mobile injected wallet
- * Many injected wallet browsers emulate isMetaMask, but only metamask mobile has _metamask
+ * Fetch the Metamask version using the method defined in https://docs.metamask.io/wallet/reference/json-rpc-methods/web3_clientversion
+ * Returns null if the version could not be fetched
  */
-function useIsMetamaskMobileInjectedWallet(): boolean {
-  const walletProvider = useWalletProvider()
-  const rawProvider = walletProvider?.provider as any
+async function getMetamaskVersion(provider: ExternalProvider): Promise<string | null> {
+  if (!provider.request) return null
 
-  return Boolean(rawProvider?.isMetaMask && rawProvider._metamask && !rawProvider.isRabby)
+  try {
+    return await provider.request({
+      method: 'web3_clientVersion',
+      params: [],
+    })
+  } catch (error) {
+    console.error('Failed to get Metamask version:', error)
+    return null
+  }
+}
+
+const SEMVER_REGEX = /\d+\.\d+\.\d+/
+const EXTRACT_SEMVER_REGEX = new RegExp(`metamask/v(${SEMVER_REGEX.source})`, 'i')
+const METAMASK_EXTENSION_REGEX = /metamask/i
+
+function extractMetamaskSemver(version: string): string | null {
+  const match = version.match(EXTRACT_SEMVER_REGEX)
+  return match ? match[1] : null
+}
+
+/**
+ * Check whether the Metamask version is smaller than the target version
+ * Returns undefined if the version is not a valid semantic version
+ */
+function isMetamaskSemverSmallerThanTarget(version: string, target: string): boolean | undefined {
+  // Check whether the version is smaller than the target
+  // We are comparing semantic versions, so the format of both should already be: major.minor.patch
+  if (!SEMVER_REGEX.test(version) || !SEMVER_REGEX.test(target)) return undefined
+
+  const [major, minor, patch] = version.split('.').map(Number)
+  const [targetMajor, targetMinor, targetPatch] = target.split('.').map(Number)
+
+  if (major < targetMajor) return true
+  if (major === targetMajor && minor < targetMinor) return true
+  return major === targetMajor && minor === targetMinor && patch < targetPatch
+}
+
+/**
+ * Hook to check if the wallet is affected by the Metamask bug where transactions are sent to the wrong chain
+ * Returns true if the wallet is affected, false if it is not
+ */
+function useShouldDisplayMetamaskWarning(): { shouldDisplayMetamaskWarning: boolean; currentVersion: string } {
+  const [isAffected, setIsAffected] = useState<boolean | undefined>(false)
+  const [currentVersion, setCurrentVersion] = useState<string>('')
+
+  const isMetamaskBrowserExtension = useIsMetamaskBrowserExtensionWallet()
+
+  const widgetProviderMetaInfo = useWidgetProviderMetaInfo()
+
+  const isWidgetMetamaskBrowserExtension = widgetProviderMetaInfo?.providerEip6963Info?.rdns === METAMASK_RDNS
+
+  const isMetamask = isMetamaskBrowserExtension || isWidgetMetamaskBrowserExtension
+
+  const provider = useWalletProvider()
+
+  useEffect(() => {
+    if (!isMetamask || !provider?.provider) {
+      setIsAffected(false)
+      return
+    }
+
+    // Here we know we are connected to a form of Metamask
+    // Fetch the version
+    getMetamaskVersion(provider.provider).then((version) => {
+      if (!version) {
+        // No version found, assume the wallet is affected
+        setIsAffected(undefined)
+        setCurrentVersion('')
+        return
+      }
+      if (!METAMASK_EXTENSION_REGEX.test(version)) {
+        // Not the browser extension, assume the wallet is not affected
+        // MM via SDK on mobile returns this for example: "Geth/v1.14.13-stable-eb00f169/linux-arm64/go1.23.5"
+        setIsAffected(false)
+        setCurrentVersion('')
+        return
+      }
+
+      const semver = extractMetamaskSemver(version)
+
+      if (!semver) {
+        // Invalid version, assume the wallet is affected
+        setIsAffected(undefined)
+        setCurrentVersion('')
+        return
+      }
+      setCurrentVersion(semver)
+
+      // Check if the version is smaller than the target version where the bug was fixed
+      // If the version is smaller, the wallet is still affected by the bug
+      const isAffected = isMetamaskSemverSmallerThanTarget(semver, VERSION_WHERE_BUG_WAS_FIXED)
+      setIsAffected(isAffected)
+    })
+  }, [isMetamask, provider])
+
+  // If we don't know, show it according to the isMetamask flag
+  const shouldDisplayMetamaskWarning = isAffected === undefined ? isMetamask : isAffected
+
+  return { shouldDisplayMetamaskWarning, currentVersion }
 }
