a86718f4f0274c73fae1266a8264118e3e627042
Alexandr Kazachenko <shoom3301@gmail.com>
2025-03-19 11:13:23 +0500

    chore: fix network change issue

--- libs/wallet/src/web3-react/hooks/useSwitchNetwork.ts
+++ libs/wallet/src/web3-react/hooks/useSwitchNetwork.ts
@@ -3,6 +3,7 @@ import { useCallback } from 'react'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 import { useWeb3React } from '@web3-react/core'
 
+import { networkConnection } from '../connection/network'
 import { switchChain } from '../utils/switchChain'
 
 export function useSwitchNetwork() {
@@ -10,7 +11,7 @@ export function useSwitchNetwork() {
 
   return useCallback(
     async (targetChain: SupportedChainId) => {
-      if (!account) return
+      if (connector !== networkConnection.connector && !account) return
 
       return switchChain(connector, targetChain)
     },
