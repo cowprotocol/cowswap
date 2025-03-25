270c8807871a136b4500d3216e6b9a1e5eb4801d
Alexandr Kazachenko <shoom3301@gmail.com>
2025-03-25 16:03:30 +0500

    chore: get rid of jotai-cache

--- apps/cowswap-frontend/src/tradingSdk/tradingSdkAtom.ts
+++ apps/cowswap-frontend/src/tradingSdk/tradingSdkAtom.ts
@@ -2,11 +2,10 @@ import { atom } from 'jotai'
 
 import { TradingSdk, TraderParameters } from '@cowprotocol/cow-sdk'
 
-import { atomWithCache } from 'jotai-cache'
-
 export const tradingSdkParamsAtom = atom<TraderParameters | null>(null)
 
-export const tradingSdkAtom = atomWithCache((get) => {
+// TODO: use single instance of the SDK
+export const tradingSdkAtom = atom((get) => {
   const params = get(tradingSdkParamsAtom)
 
   if (!params) return null
