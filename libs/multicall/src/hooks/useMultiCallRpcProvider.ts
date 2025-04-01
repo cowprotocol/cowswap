02de18638875f80fe43e1758ed129df2b217fed0
Alexandr Kazachenko <shoom3301@gmail.com>
2025-04-01 15:53:14 +0500

    chore: update walletChainId

--- libs/multicall/src/hooks/useMultiCallRpcProvider.ts
+++ libs/multicall/src/hooks/useMultiCallRpcProvider.ts
@@ -2,26 +2,25 @@ import { useAtomValue } from 'jotai/index'
 import { useMemo } from 'react'
 
 import { getRpcProvider } from '@cowprotocol/common-const'
-import { useWalletProvider } from '@cowprotocol/wallet-provider'
+import { useWalletChainId, useWalletProvider } from '@cowprotocol/wallet-provider'
 import { JsonRpcProvider } from '@ethersproject/providers'
 
 import { multiCallContextAtom } from '../state/multiCallContextAtom'
 
 export function useMultiCallRpcProvider(): JsonRpcProvider | null {
+  const walletChainId = useWalletChainId()
   const provider = useWalletProvider()
   const context = useAtomValue(multiCallContextAtom)
 
   return useMemo(() => {
-    const providerChainId = provider?.network?.chainId
-
-    if (!context || !providerChainId) return null
+    if (!context || !provider || !walletChainId) return null
 
     // Use wallet provider if current network matches the wallet network
-    if (providerChainId === context.chainId) {
+    if (walletChainId === context.chainId) {
       return provider
     }
 
     // Otherwise use RPC node
     return getRpcProvider(context.chainId)
-  }, [context, provider])
+  }, [context, provider, walletChainId])
 }
