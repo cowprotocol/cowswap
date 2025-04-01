77ef0e356a0b8f6cb3864142a44369010700522b
daniele <daniele@cow.fi>
2025-04-01 15:49:41 +0100

    chore: breakdown `RenderProgressTopSection` component

---
+++ apps/cowswap-frontend/src/common/pure/OrderProgressBarV2/container/ProgressImageWrapper.tsx
@@ -0,0 +1,85 @@
+import { Color } from '@cowprotocol/ui'
+
+import * as styledEl from './styled'
+
+import { OrderProgressBarV2Props } from '../types'
+
+interface ProgressImageWrapperProps {
+  children: React.ReactNode
+  stepName: OrderProgressBarV2Props['stepName']
+}
+
+function getProgressImageWrapperBgColor(stepName: OrderProgressBarV2Props['stepName']) {
+  switch (stepName) {
+    case 'initial':
+      return Color.cowfi_blue_lighter
+    case 'unfillable':
+      return '#FFDB9C'
+    case 'delayed':
+    case 'submissionFailed':
+    case 'solved':
+      return '#FFB3B3'
+    case 'solving':
+      return Color.cowfi_blue_lighter
+    case 'finished':
+    case 'cancellationFailed':
+      return Color.cowfi_blue_lighter
+    default:
+      return Color.cowfi_blue_lighter
+  }
+}
+
+function getProgressImageWrapperPadding(stepName: OrderProgressBarV2Props['stepName']) {
+  switch (stepName) {
+    case 'initial':
+      return '24px'
+    case 'unfillable':
+      return '20px 0 0'
+    case 'delayed':
+    case 'submissionFailed':
+    case 'solved':
+      return '0'
+    case 'solving':
+      return '16px'
+    case 'finished':
+    case 'cancellationFailed':
+      return '10px'
+    default:
+      return '0'
+  }
+}
+
+function getProgressImageWrapperGap(stepName: OrderProgressBarV2Props['stepName']) {
+  switch (stepName) {
+    case 'finished':
+    case 'cancellationFailed':
+      return '10px'
+    default:
+      return '0'
+  }
+}
+
+function getProgressImageWrapperHeight(stepName: OrderProgressBarV2Props['stepName']) {
+  switch (stepName) {
+    case 'delayed':
+    case 'submissionFailed':
+    case 'solved':
+      return '229px'
+    default:
+      return 'auto'
+  }
+}
+
+export function ProgressImageWrapper({ children, stepName, ...props }: ProgressImageWrapperProps) {
+  return (
+    <styledEl.ProgressImageWrapper
+      bgColor={getProgressImageWrapperBgColor(stepName)}
+      padding={getProgressImageWrapperPadding(stepName)}
+      height={getProgressImageWrapperHeight(stepName)}
+      gap={getProgressImageWrapperGap(stepName)}
+      {...props}
+    >
+      {children}
+    </styledEl.ProgressImageWrapper>
+  )
+}
--- apps/cowswap-frontend/src/common/pure/OrderProgressBarV2/container/RenderProgressTopSection.tsx
+++ apps/cowswap-frontend/src/common/pure/OrderProgressBarV2/container/RenderProgressTopSection.tsx
@@ -1,26 +1,23 @@
 import { useMemo } from 'react'
 
-import STEP_IMAGE_CANCELLED from '@cowprotocol/assets/cow-swap/progressbar-step-cancelled.svg'
-import STEP_IMAGE_EXPIRED from '@cowprotocol/assets/cow-swap/progressbar-step-expired.svg'
-import STEP_IMAGE_SOLVING from '@cowprotocol/assets/cow-swap/progressbar-step-solving.svg'
-import STEP_IMAGE_UNFILLABLE from '@cowprotocol/assets/cow-swap/progressbar-step-unfillable.svg'
-import STEP_LOTTIE_EXECUTING from '@cowprotocol/assets/lottie/progressbar-step-executing.json'
-import STEP_LOTTIE_NEXTBATCH from '@cowprotocol/assets/lottie/progressbar-step-nextbatch.json'
-import LOTTIE_TIME_EXPIRED_DARK from '@cowprotocol/assets/lottie/time-expired-dark.json'
 import { getRandomInt } from '@cowprotocol/common-utils'
-import { ProductLogo, ProductVariant, UI } from '@cowprotocol/ui'
 
 import { AnimatePresence, motion } from 'framer-motion'
-import Lottie from 'lottie-react'
-import SVG from 'react-inlinesvg'
-import { Textfit } from 'react-textfit'
 
+import { ProgressImageWrapper } from './ProgressImageWrapper'
 import * as styledEl from './styled'
+import {
+  CancelledCancellingTopSection,
+  DelayedSolvedSubmissionFailedTopSection,
+  ExecutingTopSection,
+  ExpiredTopSection,
+  FinishedCancellationFailedTopSection,
+  InitialTopSection,
+  SolvingTopSection,
+  UnfillableTopSection,
+} from './TopSections'
 
 import { CHAIN_SPECIFIC_BENEFITS, SURPLUS_IMAGES } from '../constants'
-import { truncateWithEllipsis } from '../helpers'
-import AnimatedTokens from '../steps/AnimatedToken'
-import CircularCountdown from '../steps/CircularCountdown'
 import OrderIntent from '../steps/OrderIntent'
 import { OrderProgressBarV2Props } from '../types'
 
@@ -49,156 +46,47 @@ function RenderProgressTopSection({
   const shouldShowSurplus = debugForceShowSurplus || showSurplus
   const surplusPercentValue = surplusPercent ? parseFloat(surplusPercent).toFixed(2) : 'N/A'
 
-  const content = useMemo(() => {
+  const renderProgressTopSectionContent = useMemo(() => {
     switch (stepName) {
       case 'initial':
-        return (
-          <>
-            <styledEl.ProgressImageWrapper bgColor={'#65D9FF'} padding={'24px'}>
-              <AnimatedTokens sellToken={order?.inputToken} buyToken={order?.outputToken} />
-            </styledEl.ProgressImageWrapper>
-          </>
-        )
+        return <InitialTopSection stepName={stepName} order={order} />
       case 'solving':
       case 'solved':
       case 'unfillable':
       case 'delayed':
       case 'submissionFailed':
         return (
-          <styledEl.ProgressImageWrapper
-            bgColor={
-              stepName === 'unfillable'
-                ? '#FFDB9C'
-                : stepName === 'delayed' || stepName === 'submissionFailed' || stepName === 'solved'
-                  ? '#FFB3B3'
-                  : '#65D9FF'
+          <ProgressImageWrapper stepName={stepName}>
+            {stepName === 'unfillable' && <UnfillableTopSection />}
+
+            {
+              (stepName === 'delayed' ||
+                stepName === 'submissionFailed' ||
+                stepName === 'solved') && <DelayedSolvedSubmissionFailedTopSection />
             }
-            padding={stepName === 'unfillable' ? '20px 0 0' : stepName === 'solving' ? '16px' : '0'}
-            height={
-              stepName === 'delayed' || stepName === 'submissionFailed' || stepName === 'solved' ? '229px' : 'auto'
-            }
-          >
-            {stepName === 'unfillable' ? (
-              <img src={STEP_IMAGE_UNFILLABLE} alt="Order out of market" />
-            ) : stepName === 'delayed' || stepName === 'submissionFailed' || stepName === 'solved' ? (
-              <Lottie
-                animationData={STEP_LOTTIE_NEXTBATCH}
-                loop={true}
-                autoplay={true}
-                style={{ width: '100%', height: '100%' }}
-              />
-            ) : (
-              <>
-                <SVG src={STEP_IMAGE_SOLVING} />
-                {stepName === 'solving' && <CircularCountdown countdown={countdown || 0} />}
-              </>
-            )}
-          </styledEl.ProgressImageWrapper>
+
+            {stepName === 'solving' && <SolvingTopSection stepName={stepName} countdown={countdown || 0} />}
+          </ProgressImageWrapper>
         )
       case 'executing':
-        return (
-          <styledEl.ProgressImageWrapper height={'auto'}>
-            <Lottie
-              animationData={STEP_LOTTIE_EXECUTING}
-              loop={true}
-              autoplay={true}
-              style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-            />
-          </styledEl.ProgressImageWrapper>
-        )
+        return <ExecutingTopSection stepName={stepName} />
       case 'finished':
       case 'cancellationFailed':
         return (
-          <styledEl.ProgressTopSection>
-            <styledEl.ProgressImageWrapper bgColor={'#65D9FF'} padding={'10px'} gap={'10px'}>
-              <styledEl.CowImage>
-                <SVG src={randomImage} />
-              </styledEl.CowImage>
-              <styledEl.FinishedImageContent>
-                <styledEl.FinishedTagLine>
-                  {shouldShowSurplus ? (
-                    <styledEl.BenefitSurplusContainer>
-                      I just received surplus on
-                      <styledEl.TokenPairTitle title={`${order?.inputToken.symbol} / ${order?.outputToken.symbol}`}>
-                        {truncateWithEllipsis(`${order?.inputToken.symbol} / ${order?.outputToken.symbol}`, 30)}
-                      </styledEl.TokenPairTitle>{' '}
-                      <styledEl.Surplus>
-                        <Textfit
-                          mode="multi"
-                          min={14}
-                          max={60}
-                          style={{
-                            width: '100%',
-                            height: '100%',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            lineHeight: 1.2,
-                          }}
-                        >
-                          {shouldShowSurplus && surplusPercentValue !== 'N/A' ? `+${surplusPercentValue}%` : 'N/A'}
-                        </Textfit>
-                      </styledEl.Surplus>
-                    </styledEl.BenefitSurplusContainer>
-                  ) : (
-                    <styledEl.BenefitSurplusContainer>
-                      <styledEl.BenefitTagLine>Did you know?</styledEl.BenefitTagLine>
-                      <styledEl.BenefitText>
-                        <Textfit
-                          mode="multi"
-                          min={12}
-                          max={50}
-                          style={{
-                            width: '100%',
-                            height: '100%',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            lineHeight: 1.2,
-                          }}
-                        >
-                          {randomBenefit}
-                        </Textfit>
-                      </styledEl.BenefitText>
-                    </styledEl.BenefitSurplusContainer>
-                  )}
-                </styledEl.FinishedTagLine>
-
-                <styledEl.FinishedLogo>
-                  <ProductLogo
-                    variant={ProductVariant.CowSwap}
-                    theme="light"
-                    overrideColor={UI.COLOR_PRIMARY_DARKER}
-                    height={19}
-                    logoIconOnly
-                  />
-                  <b>CoW Swap</b>
-                </styledEl.FinishedLogo>
-              </styledEl.FinishedImageContent>
-            </styledEl.ProgressImageWrapper>
-          </styledEl.ProgressTopSection>
+          <FinishedCancellationFailedTopSection
+            stepName={stepName}
+            order={order}
+            randomImage={randomImage}
+            shouldShowSurplus={shouldShowSurplus}
+            surplusPercentValue={surplusPercentValue}
+            randomBenefit={randomBenefit}
+          />
         )
       case 'cancelling':
       case 'cancelled':
-        return (
-          <styledEl.ProgressImageWrapper height={'auto'}>
-            <img src={STEP_IMAGE_CANCELLED} alt="Cancelling order" />
-          </styledEl.ProgressImageWrapper>
-        )
+        return <CancelledCancellingTopSection stepName={stepName} />
       case 'expired':
-        return (
-          <styledEl.ProgressImageWrapper height={'auto'}>
-            <styledEl.ClockAnimation>
-              <Lottie
-                animationData={LOTTIE_TIME_EXPIRED_DARK}
-                loop={false}
-                autoplay
-                style={{ width: '100%', height: '100%' }}
-              />
-            </styledEl.ClockAnimation>
-            <img src={STEP_IMAGE_EXPIRED} alt="Order expired" />
-          </styledEl.ProgressImageWrapper>
-        )
+        return <ExpiredTopSection stepName={stepName} />
       default:
         return null
     }
@@ -221,7 +109,7 @@ function RenderProgressTopSection({
           exit={{ opacity: 0, width: '100%' }}
           style={{ width: '100%' }}
         >
-          {content}
+          {renderProgressTopSectionContent}
         </motion.div>
       </AnimatePresence>
       {!hideIntent && <OrderIntent order={order} />}
