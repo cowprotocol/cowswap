d51fe5394f6c13ed28ac1725331bfb505b8275a5
Alexandr Kazachenko <shoom3301@gmail.com>
2025-04-01 17:40:58 +0500

    fix: do not use RPC node when wallet is connected (#5585)
    * fix: do not use RPC node when wallet is connected
    
    * chore: update walletChainId

--- libs/multicall/src/hooks/useMultiCallRpcProvider.ts
+++ libs/multicall/src/hooks/useMultiCallRpcProvider.ts
@@ -2,24 +2,25 @@ import { useAtomValue } from 'jotai/index'
 import { useMemo } from 'react'
 
 import { getRpcProvider } from '@cowprotocol/common-const'
-import { useWalletProvider } from '@cowprotocol/wallet-provider'
+import { useWalletChainId, useWalletProvider } from '@cowprotocol/wallet-provider'
 import { JsonRpcProvider } from '@ethersproject/providers'
 
 import { multiCallContextAtom } from '../state/multiCallContextAtom'
 
 export function useMultiCallRpcProvider(): JsonRpcProvider | null {
+  const walletChainId = useWalletChainId()
   const provider = useWalletProvider()
   const context = useAtomValue(multiCallContextAtom)
 
   return useMemo(() => {
-    if (!context) return null
+    if (!context || !provider || !walletChainId) return null
 
     // Use wallet provider if current network matches the wallet network
-    if (provider?.network?.chainId === context.chainId) {
+    if (walletChainId === context.chainId) {
       return provider
     }
 
     // Otherwise use RPC node
     return getRpcProvider(context.chainId)
-  }, [context, provider])
+  }, [context, provider, walletChainId])
 }
