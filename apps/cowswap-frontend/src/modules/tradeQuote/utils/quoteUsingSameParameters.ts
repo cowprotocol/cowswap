a534d125febea35895ffe2d0e1cf0993bc2e90dc
Alexandr Kazachenko <shoom3301@gmail.com>
2025-04-21 16:20:26 +0100

    fix: throttle quote requests for bridge

--- apps/cowswap-frontend/src/modules/tradeQuote/utils/quoteUsingSameParameters.ts
+++ apps/cowswap-frontend/src/modules/tradeQuote/utils/quoteUsingSameParameters.ts
@@ -1,10 +1,12 @@
 import { WRAPPED_NATIVE_CURRENCIES } from '@cowprotocol/common-const'
 import { getIsNativeToken } from '@cowprotocol/common-utils'
-import { SupportedChainId, QuoteBridgeRequest, TradeParameters } from '@cowprotocol/cow-sdk'
+import { SupportedChainId, QuoteBridgeRequest } from '@cowprotocol/cow-sdk'
 
 import { Nullish } from 'types'
 
-import { AppDataInfo } from 'modules/appData'
+import type { AppDataInfo, CowHook } from 'modules/appData'
+
+import type { TradeQuoteState } from '../state/tradeQuoteAtom'
 
 /**
  * Checks if the parameters for the current quote are correct
@@ -13,20 +15,14 @@ import { AppDataInfo } from 'modules/appData'
  */
 export function quoteUsingSameParameters(
   chainId: SupportedChainId,
-  currentParams: Nullish<TradeParameters>,
+  currentQuote: TradeQuoteState,
   nextParams: Nullish<QuoteBridgeRequest>,
   currentAppData: AppDataInfo['doc'] | undefined,
   appData: AppDataInfo['doc'] | undefined,
 ): boolean {
+  const currentParams = currentQuote.quote?.quoteResults.tradeParameters
   if (!currentParams || !nextParams) return false
 
-  /**
-   * TODO: in bridging mode buy token changes to intermediate token
-   * TODO: in bridging mode, appData contains only hooks without other information
-   * TODO: in bridging mode, receiver became a CoW Shed address
-   */
-  const isBridge = true
-  const hasSameAppData = compareAppDataWithoutQuoteData(currentAppData, appData)
   const isNativeToken = getIsNativeToken(chainId, nextParams.sellTokenAddress)
   const wrappedToken = WRAPPED_NATIVE_CURRENCIES[chainId]
   /**
@@ -34,16 +30,35 @@ export function quoteUsingSameParameters(
    */
   const nextSellToken = isNativeToken ? wrappedToken.address.toLowerCase() : nextParams.sellTokenAddress.toLowerCase()
 
-  // TODO: compare tokens chainIds
+  if (currentQuote.bridgeQuote) {
+    const bridgeTradeParams = currentQuote.bridgeQuote.tradeParameters
+    const bridgePostHook = currentQuote.bridgeQuote.bridgeCallDetails.preAuthorizedBridgingHook.postHook
+
+    return (
+      compareAppDataWithoutQuoteData(
+        removeBridgePostHook(currentAppData, bridgePostHook),
+        removeBridgePostHook(appData, bridgePostHook),
+      ) &&
+      currentParams.owner === nextParams.owner &&
+      currentParams.kind === nextParams.kind &&
+      currentParams.amount === nextParams.amount.toString() &&
+      bridgeTradeParams.validFor === nextParams.validFor &&
+      bridgeTradeParams.receiver === nextParams.receiver &&
+      currentParams.sellToken.toLowerCase() === nextSellToken &&
+      bridgeTradeParams.sellTokenChainId === nextParams.sellTokenChainId &&
+      bridgeTradeParams.buyTokenAddress.toLowerCase() === nextParams.buyTokenAddress.toLowerCase()
+    )
+  }
+
   return (
-    (isBridge ? true : hasSameAppData) &&
+    compareAppDataWithoutQuoteData(currentAppData, appData) &&
     currentParams.owner === nextParams.owner &&
     currentParams.kind === nextParams.kind &&
-    currentParams.sellToken.toLowerCase() === nextSellToken &&
-    (isBridge ? true : currentParams.buyToken === nextParams.buyTokenAddress) &&
     currentParams.amount === nextParams.amount.toString() &&
-    (isBridge ? true : currentParams.receiver === nextParams.receiver) &&
-    currentParams.validFor === nextParams.validFor
+    currentParams.validFor === nextParams.validFor &&
+    currentParams.receiver === nextParams.receiver &&
+    currentParams.sellToken.toLowerCase() === nextSellToken &&
+    currentParams.buyToken.toLowerCase() === nextParams.buyTokenAddress.toLowerCase()
   )
 }
 
@@ -64,7 +79,38 @@ function compareAppDataWithoutQuoteData(a: AppDataInfo['doc'] | undefined, b: Ap
  * If appData is set and is valid, remove `quote` metadata from it
  */
 function removeQuoteMetadata(appData: AppDataInfo['doc']): string {
-  const { metadata: fullMetadata, ...rest } = appData
-  const { quote: _, ...metadata } = fullMetadata
+  const { metadata: fullMetadata, environment: _e, ...rest } = appData
+  const { quote: _, referrer: _1, replacedOrder: _2, utm: _3, widget: _4, ...metadata } = fullMetadata
   return JSON.stringify({ ...rest, metadata })
+}
+
+function removeBridgePostHook(
+  appData: AppDataInfo['doc'] | undefined,
+  postHook: CowHook,
+): AppDataInfo['doc'] | undefined {
+  if (!appData) return appData
+
+  const copy = { ...appData }
+
+  if (copy.metadata.hooks?.post) {
+    copy.metadata.hooks.post = copy.metadata.hooks.post.filter((hook) => {
+      return (
+        hook.target !== postHook.target && hook.callData !== postHook.callData && hook.gasLimit !== postHook.gasLimit
+      )
+    })
+
+    if (!copy.metadata.hooks.post.length) {
+      copy.metadata.hooks.post = undefined
+    }
+
+    if (!copy.metadata.hooks.pre?.length) {
+      copy.metadata.hooks.pre = undefined
+    }
+  }
+
+  if (copy.metadata.hooks && !copy.metadata.hooks.post && !copy.metadata.hooks.pre) {
+    copy.metadata.hooks = undefined
+  }
+
+  return copy
 }
