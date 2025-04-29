998568828264ae1243ec0c55d14837cc041b0c11
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-04-29 20:12:21 +0100

    refactor(hooks): enhance debouncing logic

--- apps/cowswap-frontend/src/modules/trade/hooks/setupTradeState/useSetupTradeState.ts
+++ apps/cowswap-frontend/src/modules/trade/hooks/setupTradeState/useSetupTradeState.ts
@@ -64,19 +64,22 @@ export function useSetupTradeState(): void {
         // Because it's a normal situation when we are not in Gnosis safe App
         if (error.name === 'NoSafeContext') return
 
-        console.error('[TRADE STATE]', 'Failed to switch network in wallet', error)
+        console.error('Network switching error: ', error)
       })
     },
     [switchNetwork],
   )
 
-  const debouncedSwitchNetworkInWallet = useCallback(debounce(switchNetworkInWallet, 200), [switchNetworkInWallet])
+  const debouncedSwitchNetworkInWallet = debounce(([targetChainId]: [SupportedChainId]) => {
+    switchNetworkInWallet(targetChainId)
+  }, 800)
 
   const onProviderNetworkChanges = useCallback(() => {
     const rememberedUrlState = rememberedUrlStateRef.current
 
     if (rememberedUrlState) {
       rememberedUrlStateRef.current = null
+
       tradeNavigate(rememberedUrlState.chainId, rememberedUrlState)
     } else {
       // When app loaded with connected wallet
@@ -98,71 +101,9 @@ export function useSetupTradeState(): void {
         updateState(defaultState)
       }
     }
-  }, [
-    isFirstLoad,
-    isWalletConnected,
-    urlChainId,
-    providerChainId,
-    tradeNavigate,
-    switchNetworkInWallet,
-    isTwapMode,
-    updateState,
-  ])
-
-  /**
-   * On:
-   *  - chainId in URL changes
-   *  - provider changes
-   *
-   * Note: useEagerlyConnect() changes connectors several times at the beginning
-   *
-   * 1. When chainId in URL is changed, then set it to the provider
-   * 2. If provider's chainId is the same with chainId in URL, then do nothing
-   * 3. When the URL state is remembered, then set it's chainId to the provider
-   */
-  useEffect(() => {
-    // When wallet provider is loaded and chainId matches to the URL chainId
-    const isProviderChainIdMatchesUrl = providerChainId === urlChainId
-
-    if (isProviderChainIdMatchesUrl) {
-      setIsFirstLoad(false)
-    }
-
-    if (!providerChainId || providerChainId === currentChainId) return
-
-    const targetChainId = rememberedUrlStateRef.current?.chainId || currentChainId
-
-    // Debouncing switching multiple time in a quick span of time to avoid running into infinity loop of updating provider and url state.
-    // issue GH : https://github.com/cowprotocol/cowswap/issues/4734
-    debouncedSwitchNetworkInWallet(targetChainId)
-
-    console.debug('[TRADE STATE]', 'Set chainId to provider', { provider, urlChainId })
-    // Triggering only when chainId in URL is changes, provider is changed or rememberedUrlState is changed
+    // Triggering only when chainId was changed in the provider
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [provider, urlChainId])
-
-  /**
-   * On chainId in provider changes
-   *
-   * 1. Do nothing, when provider's chainId matches to the chainId from URL, or it's not supported
-   * 2. If the URL state was remembered, then put it into URL
-   * 3. If it's the first load and wallet is connected, then switch network in the wallet to the chainId from URL
-   * 4. Otherwise, navigate to the new chainId with default tokens
-   */
-  useEffect(() => {
-    // When wallet provider is not loaded yet, or chainId has not changed
-    const shouldSkip = !providerChainId || providerChainId === urlChainId || providerChainId === prevProviderChainId
-
-    if (shouldSkip) return
-
-    onProviderNetworkChanges()
-
-    console.debug('[TRADE STATE]', 'Provider changed chainId', {
-      providerChainId,
-      urlChanges: rememberedUrlStateRef.current,
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [onProviderNetworkChanges])
+  }, [providerChainId, prevProviderChainId, isTwapMode, updateState])
 
   /**
    * On URL parameter changes
@@ -260,7 +201,62 @@ export function useSetupTradeState(): void {
 
     // Triggering only on changes from URL
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tradeStateFromUrl, pendingNetworkChange, providerChainId, isTwapMode])
+  }, [tradeStateFromUrl, pendingNetworkChange])
+
+  /**
+   * On:
+   *  - chainId in URL changes
+   *  - provider changes
+   *
+   * Note: useEagerlyConnect() changes connectors several times at the beginning
+   *
+   * 1. When chainId in URL is changed, then set it to the provider
+   * 2. If provider's chainId is the same with chainId in URL, then do nothing
+   * 3. When the URL state is remembered, then set it's chainId to the provider
+   */
+  useEffect(() => {
+    // When wallet provider is loaded and chainId matches to the URL chainId
+    const isProviderChainIdMatchesUrl = providerChainId === urlChainId
+
+    if (isProviderChainIdMatchesUrl) {
+      setIsFirstLoad(false)
+    }
+
+    if (!providerChainId || providerChainId === currentChainId) return
+
+    const targetChainId = rememberedUrlStateRef.current?.chainId || currentChainId
+
+    // Debouncing switching multiple time in a quick span of time to avoid running into infinity loop of updating provider and url state.
+    // issue GH : https://github.com/cowprotocol/cowswap/issues/4734
+    debouncedSwitchNetworkInWallet(targetChainId)
+
+    console.debug('[TRADE STATE]', 'Set chainId to provider', { provider, urlChainId })
+    // Triggering only when chainId in URL is changes, provider is changed or rememberedUrlState is changed
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [provider, urlChainId])
+
+  /**
+   * On chainId in provider changes
+   *
+   * 1. Do nothing, when provider's chainId matches to the chainId from URL, or it's not supported
+   * 2. If the URL state was remembered, then put it into URL
+   * 3. If it's the first load and wallet is connected, then switch network in the wallet to the chainId from URL
+   * 4. Otherwise, navigate to the new chainId with default tokens
+   */
+  useEffect(() => {
+    // When wallet provider is not loaded yet, or chainId has not changed
+    const shouldSkip = !providerChainId || providerChainId === urlChainId || providerChainId === prevProviderChainId
+
+    if (shouldSkip) return
+
+    onProviderNetworkChanges()
+
+    console.debug('[TRADE STATE]', 'Provider changed chainId', {
+      providerChainId,
+      urlChanges: rememberedUrlStateRef.current,
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [onProviderNetworkChanges])
 
   /**
    * If user opened a link with some token symbol, and we have more than one token with the same symbol in the listing
