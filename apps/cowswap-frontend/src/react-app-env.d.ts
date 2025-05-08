54339cfbabe652b3770a5e37a554b874931e36ef
Alfetopito <alfetopito@users.noreply.github.com>
2025-05-08 15:23:56 +0100

    chore: formatting changes just to trigger the build

--- apps/cowswap-frontend/src/react-app-env.d.ts
+++ apps/cowswap-frontend/src/react-app-env.d.ts
@@ -3,7 +3,7 @@
 declare module '*.md'
 
 declare module '@metamask/jazzicon' {
-  export default function (diameter: number, seed: number): HTMLElement
+  export default function(diameter: number, seed: number): HTMLElement
 }
 
 interface TelegramData {
@@ -34,7 +34,11 @@ interface Window {
   web3?: Record<string, unknown>
   Telegram?: {
     Login: {
-      auth(options: {bot_id: number, lang?: string, request_access?: string}, callback: (data: TelegramData | false) => void): void
+      auth(options: {
+        bot_id: number,
+        lang?: string,
+        request_access?: string
+      }, callback: (data: TelegramData | false) => void): void
     }
   }
 }
@@ -45,10 +49,12 @@ interface Console extends Node.Console {
 
 declare module 'content-hash' {
   declare function decode(x: string): string
+
   declare function getCodec(x: string): string
 }
 
 declare module 'multihashes' {
   declare function decode(buff: Uint8Array): { code: number; name: string; length: number; digest: Uint8Array }
+
   declare function toB58String(hash: Uint8Array): string
 }
