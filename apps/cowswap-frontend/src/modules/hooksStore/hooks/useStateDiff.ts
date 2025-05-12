5811e551a87c85fa2abaf56df6086d4334d2bc71
Jean Neiverth <jeanneiverth@gmail.com>
2025-05-12 08:16:14 -0300

    chore: add generic function to handle pre and post hooks state diff

--- apps/cowswap-frontend/src/modules/hooksStore/hooks/useStateDiff.ts
+++ apps/cowswap-frontend/src/modules/hooksStore/hooks/useStateDiff.ts
@@ -1,41 +1,37 @@
 import { useMemo } from 'react'
 
-import { StateDiff } from '@cowprotocol/hook-dapp-lib'
+import { CowHookDetails, StateDiff } from '@cowprotocol/hook-dapp-lib'
 
 import { useTenderlyBundleSimulation } from 'modules/tenderly/hooks/useTenderlyBundleSimulation'
+import { SimulationData } from 'modules/tenderly/types'
 
 import { useHooks } from './useHooks'
 
 const EMPTY_STATE_DIFF: StateDiff[] = []
 
+function getHookStateDiff(data: Record<string, SimulationData> | undefined, hooks: CowHookDetails[]) {
+  if (!data || !hooks) return null
+
+  const lastHook = hooks[hooks.length - 1]
+  return data[lastHook?.uuid]?.stateDiff || null
+}
+
 export function useHookStateDiff(isPreHook: boolean, hookToEditUid?: string): StateDiff[] {
   const { data } = useTenderlyBundleSimulation()
   const { preHooks, postHooks } = useHooks()
 
-  const preHookStateDiff = useMemo(() => {
-    if (!data || !preHooks.length) return EMPTY_STATE_DIFF
-
-    const lastPreHook = preHooks[preHooks.length - 1]
-    return data[lastPreHook?.uuid]?.stateDiff || EMPTY_STATE_DIFF
-  }, [data, preHooks])
-
-  const postHookStateDiff = useMemo(() => {
-    // is adding the first post hook or simulation not available
-    if (!data || !postHooks) return preHookStateDiff
-
-    const lastPostHook = postHooks[postHooks.length - 1]
-    return data[lastPostHook?.uuid]?.stateDiff || preHookStateDiff
-  }, [data, preHookStateDiff, postHooks])
+  const preHookStateDiff = useMemo(() => getHookStateDiff(data, preHooks), [data, preHooks])
+  const postHookStateDiff = useMemo(() => getHookStateDiff(data, postHooks), [data, postHooks])
 
   const hookToEditStateDiff = useMemo(() => {
-    if (!data || !hookToEditUid) return EMPTY_STATE_DIFF
+    if (!data || !hookToEditUid) return null
 
     const otherHooks = isPreHook ? preHooks : postHooks
 
     const hookToEditIndex = otherHooks.findIndex((hook) => hook.uuid === hookToEditUid)
 
     // is editing first preHook -> return empty state
-    if (!hookToEditIndex && isPreHook) return EMPTY_STATE_DIFF
+    if (!hookToEditIndex && isPreHook) return null
 
     // is editing first postHook -> return
     if (!hookToEditIndex && !isPreHook) return preHookStateDiff
@@ -43,12 +39,12 @@ export function useHookStateDiff(isPreHook: boolean, hookToEditUid?: string): St
     // is editing a non first hook, return the latest available hook state
     const previousHookIndex = hookToEditIndex - 1
 
-    return data[otherHooks[previousHookIndex]?.uuid]?.stateDiff || EMPTY_STATE_DIFF
+    return data[otherHooks[previousHookIndex]?.uuid]?.stateDiff || null
   }, [data, hookToEditUid, isPreHook, preHooks, postHooks, preHookStateDiff])
 
   return useMemo(() => {
-    if (hookToEditUid) return hookToEditStateDiff
-    if (isPreHook) return preHookStateDiff
-    return postHookStateDiff
+    if (hookToEditUid) return hookToEditStateDiff || EMPTY_STATE_DIFF
+    if (isPreHook) return preHookStateDiff || EMPTY_STATE_DIFF
+    return postHookStateDiff || preHookStateDiff || EMPTY_STATE_DIFF
   }, [hookToEditStateDiff, hookToEditUid, isPreHook, postHookStateDiff, preHookStateDiff])
 }
