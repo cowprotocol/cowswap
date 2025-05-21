d8e30b9c46bd2a32a418e9da6ac6496367bd1f2d
Alexandr Kazachenko <shoom3301@gmail.com>
2025-05-21 11:12:47 +0500

    refactor: remove dead code

--- apps/cowswap-frontend/src/modules/bridge/hooks/index.ts
+++ apps/cowswap-frontend/src/modules/bridge/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useBridgeProvider } from './useBridgeProvider'
 export { useBridgeSupportedTokens } from './useBridgeSupportedTokens'
 export { useBridgeSupportedNetworks } from './useBridgeSupportedNetworks'
-export { useBridgeProviderDetails } from './useBridgeProviderDetails'
