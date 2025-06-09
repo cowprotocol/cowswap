aa4e3fbc2a6e3e2f7343ccd402ddc986f66b8f19
Alexandr Kazachenko <shoom3301@gmail.com>
2025-06-09 19:34:39 +0500

    chore: refactor FilledProgress

--- apps/explorer/src/components/orders/FilledProgress/PriceWithTitle.tsx
+++ apps/explorer/src/components/orders/FilledProgress/PriceWithTitle.tsx
@@ -1,9 +1,16 @@
 import React, { ReactNode } from 'react'
 
+import { TokenErc20 } from '@gnosis.pm/dex-js'
+
+import { TableHeadingContent } from './styled'
+
+import { Nullish } from '../../../types'
 import { OrderPriceDisplay, OrderPriceDisplayProps } from '../OrderPriceDisplay'
 
-interface PriceWithTitleProps extends OrderPriceDisplayProps {
+interface PriceWithTitleProps extends Omit<OrderPriceDisplayProps, 'buyToken' | 'sellToken'> {
   title: string
+  buyToken: Nullish<TokenErc20>
+  sellToken: Nullish<TokenErc20>
 }
 
 export function PriceWithTitle(props: PriceWithTitleProps): ReactNode {
@@ -12,7 +19,9 @@ export function PriceWithTitle(props: PriceWithTitleProps): ReactNode {
     <TableHeadingContent>
       <p className="title">{title}</p>
       <p className="priceNumber">
-        {priceProps.buyToken && priceProps.sellToken && <OrderPriceDisplay {...priceProps} />}
+        {priceProps.buyToken && priceProps.sellToken && (
+          <OrderPriceDisplay {...(priceProps as OrderPriceDisplayProps)} />
+        )}
       </p>
     </TableHeadingContent>
   )
