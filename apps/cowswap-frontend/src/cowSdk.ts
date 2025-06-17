599d6f0067f386060047c536f8679ccaa69964f9
Alfetopito <alfetopito@users.noreply.github.com>
2025-06-17 11:56:39 +0100

    chore: more changes to make sure ethflow orders point to backend's prod

--- apps/cowswap-frontend/src/cowSdk.ts
+++ apps/cowswap-frontend/src/cowSdk.ts
@@ -1,13 +1,8 @@
 import { MetadataApi } from '@cowprotocol/app-data'
 import { DEFAULT_BACKOFF_OPTIONS, OrderBookApi } from '@cowprotocol/cow-sdk'
 
-const prodBaseUrls = process.env.REACT_APP_ORDER_BOOK_URLS
-  ? JSON.parse(process.env.REACT_APP_ORDER_BOOK_URLS)
-  : undefined
-
 export const metadataApiSDK = new MetadataApi()
 export const orderBookApi = new OrderBookApi({
   env: 'prod', // TODO: this is a temporary hack to force prod env
-  ...(prodBaseUrls ? { baseUrls: prodBaseUrls } : undefined),
   backoffOpts: DEFAULT_BACKOFF_OPTIONS,
 })
