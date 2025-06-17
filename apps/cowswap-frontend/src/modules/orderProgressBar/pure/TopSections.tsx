a6532912109e048b0fe8567f2760585bf6390c80
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-06-17 13:02:51 +0100

    refactor: extract ShowSurplus and NoSurplus components into BenefitComponents

--- apps/cowswap-frontend/src/modules/orderProgressBar/pure/TopSections.tsx
+++ apps/cowswap-frontend/src/modules/orderProgressBar/pure/TopSections.tsx
@@ -10,77 +10,15 @@ import LOTTIE_TIME_EXPIRED_DARK from '@cowprotocol/assets/lottie/time-expired-da
 import { ProductLogo, ProductVariant, UI } from '@cowprotocol/ui'
 
 import SVG from 'react-inlinesvg'
-import { Textfit } from 'react-textfit'
 
+import { NoSurplus, ShowSurplus } from './BenefitComponents'
 import { FullSizeLottie } from './LottieContainer'
 import { ProgressImageWrapper } from './ProgressImageWrapper'
 import { AnimatedTokens } from './steps/AnimatedToken'
 import { CircularCountdown } from './steps/CircularCountdown'
 import * as styledEl from './styled'
 
-import { truncateWithEllipsis } from '../helpers'
 import { OrderProgressBarProps } from '../types'
-
-function ShowSurplus({
-  order,
-  shouldShowSurplus,
-  surplusPercentValue,
-}: {
-  order: OrderProgressBarProps['order']
-  shouldShowSurplus: boolean | undefined | null
-  surplusPercentValue: string
-}): ReactNode {
-  return (
-    <styledEl.BenefitSurplusContainer>
-      I just received surplus on
-      <styledEl.TokenPairTitle title={`${order?.inputToken.symbol} / ${order?.outputToken.symbol}`}>
-        {truncateWithEllipsis(`${order?.inputToken.symbol} / ${order?.outputToken.symbol}`, 30)}
-      </styledEl.TokenPairTitle>{' '}
-      <styledEl.Surplus>
-        <Textfit
-          mode="multi"
-          min={14}
-          max={60}
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            lineHeight: 1.2,
-          }}
-        >
-          {shouldShowSurplus && surplusPercentValue !== 'N/A' ? `+${surplusPercentValue}%` : 'N/A'}
-        </Textfit>
-      </styledEl.Surplus>
-    </styledEl.BenefitSurplusContainer>
-  )
-}
-
-function NoSurplus({ randomBenefit }: { randomBenefit: string }): ReactNode {
-  return (
-    <styledEl.BenefitSurplusContainer>
-      <styledEl.BenefitTagLine>Did you know?</styledEl.BenefitTagLine>
-      <styledEl.BenefitText>
-        <Textfit
-          mode="multi"
-          min={12}
-          max={50}
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            lineHeight: 1.2,
-          }}
-        >
-          {randomBenefit}
-        </Textfit>
-      </styledEl.BenefitText>
-    </styledEl.BenefitSurplusContainer>
-  )
-}
 
 interface BaseTopSectionProps {
   stepName: OrderProgressBarProps['stepName']
