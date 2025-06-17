7ac653c404ab707f93bfe375cdf5c8eea774468b
Alexandr Kazachenko <shoom3301@gmail.com>
2025-06-17 14:17:50 +0500

    chore: merge CustomGlobalStyles in ThemedGlobalStyle

--- apps/cowswap-frontend/src/theme/ThemedGlobalStyle.tsx
+++ apps/cowswap-frontend/src/theme/ThemedGlobalStyle.tsx
@@ -98,4 +98,26 @@ export const ThemedGlobalStyle = createGlobalStyle`
       }
     }
   }
+
+  body {
+    font-family: 'studiofeixen', Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background: transparent;
+    color: var(${UI.COLOR_TEXT});
+    scroll-behavior: smooth;
+    font-variant: none;
+    font-variant-ligatures: none;
+    text-rendering: optimizeLegibility;
+    font-feature-settings:
+      'liga' off,
+      'kern' on;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+
+  a {
+    color: inherit;
+  }
 `
