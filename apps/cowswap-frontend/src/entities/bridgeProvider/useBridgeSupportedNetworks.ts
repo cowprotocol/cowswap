6d77019043d43a0a3dbde8b4baddfd9b7b5a8a39
Alexandr Kazachenko <shoom3301@gmail.com>
2025-06-19 12:46:42 +0500

    fix(bridge): enable bridging only in Swap (#5837)
    * fix(bridge): enable bridging only in Swap
    
    * chore: improve useBridgeSupportedNetworks
    
    * chore: fix code style
    
    * chore: fix lint
    
    ---------
    
    Co-authored-by: fairlight <31534717+fairlighteth@users.noreply.github.com>

--- apps/cowswap-frontend/src/entities/bridgeProvider/useBridgeSupportedNetworks.ts
+++ apps/cowswap-frontend/src/entities/bridgeProvider/useBridgeSupportedNetworks.ts
@@ -1,13 +1,18 @@
-import { ChainInfo } from '@cowprotocol/cow-sdk'
+import { useIsBridgingEnabled } from '@cowprotocol/common-hooks'
+import type { ChainInfo } from '@cowprotocol/cow-sdk'
 
 import useSWR, { SWRResponse } from 'swr'
 
 import { useBridgeProvider } from './useBridgeProvider'
 
 export function useBridgeSupportedNetworks(): SWRResponse<ChainInfo[]> {
+  const isBridgingEnabled = useIsBridgingEnabled()
   const bridgeProvider = useBridgeProvider()
 
-  return useSWR([bridgeProvider, 'useBridgeSupportedNetworks'], ([bridgeProvider]) => {
-    return bridgeProvider.getNetworks()
-  })
+  return useSWR(
+    isBridgingEnabled ? [bridgeProvider, bridgeProvider.info.dappId, 'useBridgeSupportedNetworks'] : null,
+    ([bridgeProvider]) => {
+      return bridgeProvider.getNetworks()
+    },
+  )
 }
