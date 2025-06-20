cf836bc9613a71e3fea4fd98d8797e4fcb917935
Denis Makarov <limitofzero@gmail.com>
2025-06-20 14:16:55 +0400

    feat(auto-slippage): apply auto slippage for ETH flow (#5836)
    * fix(slippage): take smart slippage from the active quote
    
    * chore: renaming and types
    
    * chore: remove unused hook
    
    * fix: handle ethflow
    
    * chore: remove unused hook
    
    * chore: fix review remarks
    
    * fix: use enableSmartSlippage from the params to set value
    
    * chore: remove unused
    
    * chore: revert enableSlippage changes
    
    * fix: review remarks
    
    * fix: build
    
    * feat: support auto slippage for eth flow
    
    * feat: move deadline code to new component
    
    * feat: move some code from tx settings
    
    * chore: move warning component
    
    * chore: move some logic from tx settings
    
    * chore: move tx slippage input to another component
    
    * fix: styles for slippage input
    
    * chore: move styles for custom deadline
    
    * chore: remove unused
    
    * chore: move tx settings to pure dir
    
    * fix: lints problems
    
    * fix: lint
    
    * fix: review remarks
    
    * fix: adjust min slippage for warning for eth flow
    
    * chore: update tooltip text
    
    * chore: update imports
    
    * feat: add modified for eth flow when slippage is set by defualt
    
    * fix: build
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Alexandr Kazachenko <shoom3301@gmail.com>

---
+++ apps/cowswap-frontend/src/modules/tradeWidgetAddons/containers/DeadlineTransactionSettings/index.tsx
@@ -0,0 +1,67 @@
+import { JSX } from 'react'
+
+import { DEFAULT_DEADLINE_FROM_NOW } from '@cowprotocol/common-const'
+import { StatefulValue } from '@cowprotocol/types'
+import { HelpTooltip, RowFixed } from '@cowprotocol/ui'
+
+
+import { Trans } from '@lingui/macro'
+import { ThemedText } from 'theme'
+
+import { useIsEoaEthFlow } from 'modules/trade'
+
+import { getNativeOrderDeadlineTooltip, getNonNativeOrderDeadlineTooltip } from 'common/utils/tradeSettingsTooltips'
+import useNativeCurrency from 'lib/hooks/useNativeCurrency'
+
+import { useCustomDeadline } from './hooks/useCustomDeadline'
+import * as styledEl from './styled'
+
+interface DeadlineSettingsProps {
+  deadlineState: StatefulValue<number>
+}
+
+export function DeadlineTransactionSettings({ deadlineState }: DeadlineSettingsProps): JSX.Element {
+  const {
+    viewValue,
+    parseCustomDeadline,
+    error,
+    isDisabled,
+    onBlur,
+  } = useCustomDeadline(deadlineState)
+  const nativeCurrency = useNativeCurrency()
+  const isEoaEthFlow = useIsEoaEthFlow()
+
+  return (
+    <>
+      <RowFixed>
+        <ThemedText.Black fontSize={14} fontWeight={400}>
+          <Trans>Swap deadline</Trans>
+        </ThemedText.Black>
+        <HelpTooltip
+          text={
+            <Trans>
+              {isEoaEthFlow
+                ? getNativeOrderDeadlineTooltip([nativeCurrency.symbol])
+                : getNonNativeOrderDeadlineTooltip()}
+            </Trans>
+          }
+        />
+      </RowFixed>
+      <RowFixed>
+        <styledEl.OptionCustom style={{ width: '80px' }} warning={!!error} tabIndex={-1}>
+          <styledEl.Input
+            placeholder={(DEFAULT_DEADLINE_FROM_NOW / 60).toString()}
+            value={viewValue}
+            onChange={(e) => parseCustomDeadline(e.target.value)}
+            onBlur={onBlur}
+            color={error ? 'red' : ''}
+            disabled={isDisabled}
+          />
+        </styledEl.OptionCustom>
+        <ThemedText.Body style={{ paddingLeft: '8px' }} fontSize={14}>
+          <Trans>minutes</Trans>
+        </ThemedText.Body>
+      </RowFixed>
+    </>
+  )
+}
---
+++ libs/common-utils/src/validation/isValidIntegerFactory.ts
@@ -0,0 +1,7 @@
+export function isValidIntegerFactory(min: number, max: number) {
+  return (value: number): boolean => {
+    return  Number.isInteger(value) &&
+      value >= min &&
+      value <= max
+  };
+}
