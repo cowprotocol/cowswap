21df25eec568db56ac836e1236b210ade7be4e8f
Alexandr Kazachenko <shoom3301@gmail.com>
2025-06-25 20:16:57 +0500

    chore: fix build

--- apps/explorer/src/sdk/cowSdk.ts
+++ apps/explorer/src/sdk/cowSdk.ts
@@ -1,3 +1,4 @@
+import { getRpcProvider } from '@cowprotocol/common-const'
 import { AcrossBridgeProvider, BungeeBridgeProvider, OrderBookApi } from '@cowprotocol/cow-sdk'
 
 export const orderBookApi = new OrderBookApi()
@@ -6,8 +7,9 @@ export const bungeeBridgeProvider = new BungeeBridgeProvider({
   apiOptions: {
     includeBridges: ['across', 'cctp'],
   },
+  getRpcProvider,
 })
 
-export const acrossBridgeProvider = new AcrossBridgeProvider()
+export const acrossBridgeProvider = new AcrossBridgeProvider({ getRpcProvider })
 
 export const knownBridgeProviders = [bungeeBridgeProvider, acrossBridgeProvider]
