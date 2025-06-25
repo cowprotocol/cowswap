e7d544a5dea402963912362158d6f9a5378c6815
Alexandr Kazachenko <shoom3301@gmail.com>
2025-06-25 18:06:48 +0500

    chore: remove surplus duplication

--- apps/cowswap-frontend/src/modules/bridge/pure/BridgeActivitySummary/BridgeMetaDetails.tsx
+++ apps/cowswap-frontend/src/modules/bridge/pure/BridgeActivitySummary/BridgeMetaDetails.tsx
@@ -1,10 +1,6 @@
 import { ReactNode } from 'react'
 
-import { FiatAmount, TokenAmount } from '@cowprotocol/ui'
-
 import { ShimmerWrapper, SummaryRow } from 'common/pure/OrderSummaryRow'
-
-import { FiatWrapper } from './styled'
 
 import { SwapAndBridgeContext } from '../../types'
 import { RecipientDisplay } from '../RecipientDisplay'
@@ -15,29 +11,12 @@ interface BridgeMetaDetailsProps {
 
 export function BridgeMetaDetails({ context }: BridgeMetaDetailsProps): ReactNode {
   const {
-    swapResultContext: { surplusAmount, surplusAmountUsd },
-    overview: { sourceAmounts, targetCurrency },
+    overview: { targetCurrency },
     quoteBridgeContext,
   } = context
 
-  const sourceToken = sourceAmounts.buyAmount.currency // intermediate token after swap
-
   return (
     <>
-      {surplusAmount?.greaterThan(0) && (
-        <SummaryRow>
-          <b>Surplus</b>
-          <i>
-            <TokenAmount amount={surplusAmount} tokenSymbol={sourceToken} />
-            {surplusAmountUsd && (
-              <FiatWrapper>
-                (<FiatAmount amount={surplusAmountUsd} />)
-              </FiatWrapper>
-            )}
-          </i>
-        </SummaryRow>
-      )}
-
       {/*TODO: add bridging execution time, we don't such a value for now*/}
 
       <SummaryRow>
