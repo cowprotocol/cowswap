ec0733a6773738a74ec991d260c1b4537c8382b2
limitofzero <limitofzero@gmail.com>
2025-06-27 01:37:54 +0400

    feat: use balance context for priority tokens

--- apps/cowswap-frontend/src/modules/balancesAndAllowances/updaters/CommonPriorityBalancesAndAllowancesUpdater.tsx
+++ apps/cowswap-frontend/src/modules/balancesAndAllowances/updaters/CommonPriorityBalancesAndAllowancesUpdater.tsx
@@ -6,10 +6,14 @@ import { useWalletInfo } from '@cowprotocol/wallet'
 import { useSourceChainId } from 'modules/tokensList'
 import { usePriorityTokenAddresses } from 'modules/trade'
 
+import { useBalancesContext } from '../../../entities/balancesContext/useBalancesContext'
+
 export function CommonPriorityBalancesAndAllowancesUpdater(): ReactNode {
   const sourceChainId = useSourceChainId()
   const { account } = useWalletInfo()
+  const balancesContext = useBalancesContext()
+  const balancesAccount = balancesContext.account || account
   const excludedTokens = usePriorityTokenAddresses()
 
-  return <BalancesAndAllowancesUpdater account={account} chainId={sourceChainId} excludedTokens={excludedTokens}/>
+  return <BalancesAndAllowancesUpdater account={balancesAccount} chainId={sourceChainId} excludedTokens={excludedTokens}/>
 }
