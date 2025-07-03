5c2ba0ad70e5bdde818e960910dac54db71d831b
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-07-03 15:28:23 +0100

    refactor(bridge): update styling for BridgingStatusHeader to improve responsiveness

--- apps/cowswap-frontend/src/modules/orderProgressBar/pure/BridgingStatusHeader/styled.ts
+++ apps/cowswap-frontend/src/modules/orderProgressBar/pure/BridgingStatusHeader/styled.ts
@@ -1,5 +1,5 @@
 import { TokenLogo as TokenLogoOriginal } from '@cowprotocol/tokens'
-import { UI } from '@cowprotocol/ui'
+import { Media, UI } from '@cowprotocol/ui'
 
 import styled, { DefaultTheme } from 'styled-components/macro'
 
@@ -88,27 +88,48 @@ export const TokenLogo = styled(TokenLogoOriginal)<{ $step: BridgingFlowStep; $t
     height: ${CONNECTOR_HEIGHT};
     background: var(${({ $step, theme }) => getStepColors(theme)[$step].iconBackground});
     position: absolute;
+
+    ${Media.upToSmall()} {
+      width: calc(var(--size) * 0.7);
+    }
   }
 
   &:first-child {
     margin-right: var(--margin);
 
+    ${Media.upToSmall()} {
+      margin-right: calc(var(--margin) * 0.7);
+    }
+
     &:before {
       right: var(--connector-offset);
+
+      ${Media.upToSmall()} {
+        right: calc(var(--connector-offset) * 0.7);
+      }
     }
   }
 
   &:last-child {
     margin-left: var(--margin);
 
+    ${Media.upToSmall()} {
+      margin-left: calc(var(--margin) * 0.7);
+    }
+
     &:before {
       left: var(--connector-offset);
+
+      ${Media.upToSmall()} {
+        left: calc(var(--connector-offset) * 0.7);
+      }
     }
   }
 `
 
 export const StatusIcon = styled.div<{ $step: BridgingFlowStep }>`
   --iconSize: 80px;
+  --iconSizeMobile: 48px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -116,7 +137,15 @@ export const StatusIcon = styled.div<{ $step: BridgingFlowStep }>`
   color: var(${({ $step, theme }) => getStepColors(theme)[$step].icon});
   border-radius: var(--iconSize);
   width: var(--iconSize);
-  height: var(--iconSize);
+  height: auto;
+  max-height: var(--iconSize);
+  max-width: var(--iconSize);
+  min-width: var(--iconSize);
+  min-height: var(--iconSize);
+
+  ${Media.upToSmall()} {
+    --iconSize: var(--iconSizeMobile);
+  }
 
   > svg {
     width: 100%;
