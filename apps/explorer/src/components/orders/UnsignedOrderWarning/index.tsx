0206f8a4056348c863827f4f54f0d7c1a59ca60e
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-07-09 12:42:44 +0100

    fix: update InlineBanner width for UnsignedOrderWarning component

--- apps/explorer/src/components/orders/UnsignedOrderWarning/index.tsx
+++ apps/explorer/src/components/orders/UnsignedOrderWarning/index.tsx
@@ -2,7 +2,7 @@ import { BannerOrientation, InlineBanner, StatusColorVariant } from '@cowprotoco
 
 export const UnsignedOrderWarning: React.FC = () => {
   return (
-    <InlineBanner orientation={BannerOrientation.Horizontal} bannerType={StatusColorVariant.Alert} padding="0">
+    <InlineBanner orientation={BannerOrientation.Horizontal} bannerType={StatusColorVariant.Alert} width="fit-content">
       An unsigned order is not necessarily placed by the owner's account. Please be cautious.
     </InlineBanner>
   )
