016d2066b37e6710a5f0456c87c758889b4346e4
Alexandr Kazachenko <shoom3301@gmail.com>
2025-07-16 18:17:42 +0500

    chore: fix useNeedsApproval

--- libs/common-utils/src/isEnoughAmount.ts
+++ libs/common-utils/src/isEnoughAmount.ts
@@ -3,11 +3,16 @@ import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 
 export function isEnoughAmount(
   sellAmount: CurrencyAmount<Currency>,
-  _targetAmount: CurrencyAmount<Currency> | BigNumber | undefined
+  _targetAmount: CurrencyAmount<Currency> | BigNumber | bigint | undefined,
 ): boolean | undefined {
   if (!_targetAmount) return undefined
 
-  const targetAmount = _targetAmount instanceof BigNumber ? _targetAmount.toHexString() : _targetAmount
+  const targetAmount =
+    _targetAmount instanceof BigNumber
+      ? _targetAmount.toHexString()
+      : typeof _targetAmount === 'bigint'
+        ? _targetAmount.toString()
+        : _targetAmount
 
   return sellAmount.equalTo(targetAmount) || sellAmount.lessThan(targetAmount)
 }
