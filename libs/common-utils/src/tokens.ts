419f837dd935e605d3e2e2323cd5246cc8d563e3
Alfetopito <alfetopito@users.noreply.github.com>
2025-07-17 12:50:34 +0100

    refactor: remove explorer tokens dead code

--- libs/common-utils/src/tokens.ts
+++ libs/common-utils/src/tokens.ts
@@ -1,12 +1,7 @@
-import {
-  NATIVE_CURRENCIES,
-  NATIVE_CURRENCY_ADDRESS,
-  WRAPPED_NATIVE_CURRENCIES as WETH,
-} from '@cowprotocol/common-const'
+import { NATIVE_CURRENCIES } from '@cowprotocol/common-const'
 import { SupportedChainId as ChainId } from '@cowprotocol/cow-sdk'
 
 import { doesTokenMatchSymbolOrAddress } from './doesTokenMatchSymbolOrAddress'
-
 
 export function isNativeAddress(tokenAddress: string, chainId: ChainId): boolean {
   if (!tokenAddress || !chainId) return false
@@ -18,22 +13,4 @@ export function isNativeAddress(tokenAddress: string, chainId: ChainId): boolean
   const native = NATIVE_CURRENCIES[chainId]
 
   return native && doesTokenMatchSymbolOrAddress(native, tokenAddressLower)
-}
-
-export function toErc20Address(tokenAddress: string, chainId: ChainId): string {
-  let checkedAddress = tokenAddress
-  if (isNativeAddress(tokenAddress, chainId)) {
-    checkedAddress = WETH[chainId].address
-  }
-
-  return checkedAddress
-}
-
-export function toNativeBuyAddress(tokenAddress: string, chainId: ChainId): string {
-  let checkedAddress = tokenAddress
-  if (isNativeAddress(tokenAddress, chainId)) {
-    checkedAddress = NATIVE_CURRENCY_ADDRESS
-  }
-
-  return checkedAddress
 }
