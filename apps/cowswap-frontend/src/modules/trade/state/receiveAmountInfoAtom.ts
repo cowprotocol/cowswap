c986a3900e8503e2f46b1e06c66adf964d445423
limitofzero <limitofzero@gmail.com>
2025-07-17 21:04:03 +0400

    fix: revert comment

--- apps/cowswap-frontend/src/modules/trade/state/receiveAmountInfoAtom.ts
+++ apps/cowswap-frontend/src/modules/trade/state/receiveAmountInfoAtom.ts
@@ -9,13 +9,8 @@ import { derivedTradeStateAtom } from './derivedTradeStateAtom'
 
 import { getReceiveAmountInfo } from '../utils/getReceiveAmountInfo'
 
-// TODO: Reduce function complexity by extracting logic
-
-/**
- * @deprecated
- * use `useGetReceiveAmountInfo` instead
- * this will be removed
- * */
+// TODO should use intermediate token from atom (after migration tokens to entities)
+// eslint-disable-next-line complexity
 export const receiveAmountInfoAtom = atom((get) => {
   const tradeQuotes = get(tradeQuotesAtom)
   const volumeFee = get(volumeFeeAtom)
