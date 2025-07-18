b14219963baeb2d2fa4a24ded50c6dd74bc58afe
Alexandr Kazachenko <shoom3301@gmail.com>
2025-07-18 22:44:51 +0500

    fix: do not multicall allowances globally (#5984)
    * chore: indicate allowances problem
    
    * chore: add new useTokenAllowance
    
    * chore: fix useApproveState
    
    * chore: fix useNeedsApproval
    
    * chore: fix useEnoughBalanceAndAllowance
    
    * fix: get rid of global allowances multicall
    
    * fix: update allowance when approve tx is mined
    
    * refactor: get rid of tokens module
    
    * fix: use multicall allowances only in tables
    
    * chore: fix bigint check
    
    * chore: fix rabby
    
    * chore: adjust swr configs
    
    * chore: adjust swr configs
    
    * fix: fetch allowances only for required tokens
    
    * chore: fix allowance falsy check
    
    * chore: fix isEnoughAmount
    
    * chore: revert useMultipleContractSingleData swr deps
    
    * chore: fix allowances multicall config
    
    * chore: rename useEnoughAllowance
    
    * feat: check lastApproveBlockNumber per token

--- apps/cowswap-frontend/src/modules/trade/hooks/useHasTradeEnoughAllowance.ts
+++ apps/cowswap-frontend/src/modules/trade/hooks/useHasTradeEnoughAllowance.ts
@@ -1,24 +1,11 @@
-import { COW_PROTOCOL_VAULT_RELAYER_ADDRESS } from '@cowprotocol/cow-sdk'
-import { useWalletInfo } from '@cowprotocol/wallet'
-
-import { useEnoughBalanceAndAllowance } from 'modules/tokens'
+import { useEnoughAllowance } from 'common/hooks/useEnoughAllowance'
 
 import { useReceiveAmountInfo } from './useReceiveAmountInfo'
 
-// TODO: Add proper return type annotation
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function useHasTradeEnoughAllowance() {
+export function useHasTradeEnoughAllowance(): boolean | undefined {
   const receiveAmountInfo = useReceiveAmountInfo()
-  const { chainId, account } = useWalletInfo()
 
   const amount = receiveAmountInfo?.afterSlippage.sellAmount
-  const checkAllowanceAddress = COW_PROTOCOL_VAULT_RELAYER_ADDRESS[chainId]
 
-  const { enoughAllowance } = useEnoughBalanceAndAllowance({
-    account,
-    amount,
-    checkAllowanceAddress,
-  })
-
-  return enoughAllowance
+  return useEnoughAllowance(amount)
 }
