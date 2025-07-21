6680055578f8db5f950f02270003a42bae16f171
limitofzero <limitofzero@gmail.com>
2025-07-21 21:07:11 +0400

    fix: lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  '{apps,libs,tools}/**/*.{ts,tsx,js,jsx}': files => [
-    `nx affected:lint --files=${files.join(',')}`,   // no --fix → read‑only
-    `nx format:write --files=${files.join(',')}`,    // prettier auto‑fix
-  ],
+  '*.{ts,tsx,js,jsx}': (filesArray) => {
+    const files = filesArray.join();
+    return [
+      `nx affected:lint --fix --files=${files}`,
+    ];
+  },
 };
