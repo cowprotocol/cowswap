c72b9409b318cf75f9b89a9dc4332bcacfd6fe7b
limitofzero <limitofzero@gmail.com>
2025-07-21 22:55:53 +0400

    feat: test commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   '*.{ts,tsx,js,jsx}': (filesArray) => {
     const files = filesArray.join();
+    console.log('affected files:', files);
     return [
       `nx affected:lint --fix --files=${files}`,
     ];
