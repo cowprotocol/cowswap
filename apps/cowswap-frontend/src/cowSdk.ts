48b53d9346b4f659b183a977d7853c8a746439ea
Alfetopito <alfetopito@users.noreply.github.com>
2025-07-23 16:46:48 +0100

    Revert "chore: point to prod REVERT ME!!! (#5917)"
    This reverts commit 1176bdff70151c3ace9b41b74be2ec077d476578.

--- apps/cowswap-frontend/src/cowSdk.ts
+++ apps/cowswap-frontend/src/cowSdk.ts
@@ -1,8 +1,14 @@
 import { MetadataApi } from '@cowprotocol/app-data'
+import { isBarnBackendEnv } from '@cowprotocol/common-utils'
 import { DEFAULT_BACKOFF_OPTIONS, OrderBookApi } from '@cowprotocol/cow-sdk'
+
+const prodBaseUrls = process.env.REACT_APP_ORDER_BOOK_URLS
+  ? JSON.parse(process.env.REACT_APP_ORDER_BOOK_URLS)
+  : undefined
 
 export const metadataApiSDK = new MetadataApi()
 export const orderBookApi = new OrderBookApi({
-  env: 'prod', // TODO: this is a temporary hack to force prod env
+  env: isBarnBackendEnv ? 'staging' : 'prod',
+  ...(prodBaseUrls ? { baseUrls: prodBaseUrls } : undefined),
   backoffOpts: DEFAULT_BACKOFF_OPTIONS,
 })
