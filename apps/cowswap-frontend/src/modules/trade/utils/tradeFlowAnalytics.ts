52fcc654073fb2518bffeb9c593016d228bce811
Alexandr Kazachenko <shoom3301@gmail.com>
2025-07-28 15:29:13 +0500

    refactor: memo useTradeFlowAnalytics

--- apps/cowswap-frontend/src/modules/trade/utils/tradeFlowAnalytics.ts
+++ apps/cowswap-frontend/src/modules/trade/utils/tradeFlowAnalytics.ts
@@ -1,9 +1,10 @@
+import { useMemo } from 'react'
+
 import { useCowAnalytics } from '@cowprotocol/analytics'
 import { UiOrderType } from '@cowprotocol/types'
 
 import { CowSwapAnalyticsCategory } from 'common/analytics/types'
-
-import { USER_SWAP_REJECTED_ERROR } from '../../../common/utils/getSwapErrorMessage'
+import { USER_SWAP_REJECTED_ERROR } from 'common/utils/getSwapErrorMessage'
 
 export interface TradeFlowAnalyticsContext {
   account: string | null
@@ -25,45 +26,45 @@ export interface TradeFlowAnalytics {
 export function useTradeFlowAnalytics(): TradeFlowAnalytics {
   const analytics = useCowAnalytics()
 
-  // TODO: Add proper return type annotation
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const sendTradeAnalytics = (action: string, orderType: UiOrderType, marketLabel?: string, value?: number) => {
-    analytics.sendEvent({
-      category: CowSwapAnalyticsCategory.TRADE,
-      action,
-      label: `${orderType}|${marketLabel}`,
-      ...(value !== undefined && { value }),
-    })
-  }
+  return useMemo(() => {
+    const sendTradeAnalytics = (action: string, orderType: UiOrderType, marketLabel?: string, value?: number): void => {
+      analytics.sendEvent({
+        category: CowSwapAnalyticsCategory.TRADE,
+        action,
+        label: `${orderType}|${marketLabel}`,
+        ...(value !== undefined && { value }),
+      })
+    }
 
-  return {
-    trade(context: TradeFlowAnalyticsContext) {
-      sendTradeAnalytics('Send', context.orderType, context.marketLabel)
-    },
-    sign(context: TradeFlowAnalyticsContext) {
-      const { marketLabel, orderType } = context
-      sendTradeAnalytics('Sign', orderType, marketLabel)
-    },
-    approveAndPresign(context: TradeFlowAnalyticsContext) {
-      const { marketLabel, orderType } = context
-      sendTradeAnalytics('Bundle Approve and Swap', orderType, marketLabel)
-    },
-    placeAdvancedOrder(context: TradeFlowAnalyticsContext) {
-      const { marketLabel, orderType } = context
-      sendTradeAnalytics('Place Advanced Order', orderType, marketLabel)
-    },
-    wrapApproveAndPresign(context: TradeFlowAnalyticsContext) {
-      const { marketLabel, orderType } = context
-      sendTradeAnalytics('Bundled Eth Flow', orderType, marketLabel)
-    },
-    error(error: Error & { code?: number }, errorMessage: string, context: TradeFlowAnalyticsContext) {
-      const { marketLabel, orderType } = context
+    return {
+      trade(context: TradeFlowAnalyticsContext) {
+        sendTradeAnalytics('Send', context.orderType, context.marketLabel)
+      },
+      sign(context: TradeFlowAnalyticsContext) {
+        const { marketLabel, orderType } = context
+        sendTradeAnalytics('Sign', orderType, marketLabel)
+      },
+      approveAndPresign(context: TradeFlowAnalyticsContext) {
+        const { marketLabel, orderType } = context
+        sendTradeAnalytics('Bundle Approve and Swap', orderType, marketLabel)
+      },
+      placeAdvancedOrder(context: TradeFlowAnalyticsContext) {
+        const { marketLabel, orderType } = context
+        sendTradeAnalytics('Place Advanced Order', orderType, marketLabel)
+      },
+      wrapApproveAndPresign(context: TradeFlowAnalyticsContext) {
+        const { marketLabel, orderType } = context
+        sendTradeAnalytics('Bundled Eth Flow', orderType, marketLabel)
+      },
+      error(error: Error & { code?: number }, errorMessage: string, context: TradeFlowAnalyticsContext) {
+        const { marketLabel, orderType } = context
 
-      if (errorMessage === USER_SWAP_REJECTED_ERROR) {
-        sendTradeAnalytics('Reject', orderType, marketLabel)
-      } else {
-        sendTradeAnalytics('Error', orderType, marketLabel, error.code)
-      }
-    },
-  }
+        if (errorMessage === USER_SWAP_REJECTED_ERROR) {
+          sendTradeAnalytics('Reject', orderType, marketLabel)
+        } else {
+          sendTradeAnalytics('Error', orderType, marketLabel, error.code)
+        }
+      },
+    }
+  }, [analytics])
 }
