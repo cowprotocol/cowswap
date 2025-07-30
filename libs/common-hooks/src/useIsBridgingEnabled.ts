3980b9e1319f737ea1caaa5371a6d7ef7388a127
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-07-30 18:01:40 +0100

    revert: accidentally committed debugging changes to useIsBridgingEnabled

--- libs/common-hooks/src/useIsBridgingEnabled.ts
+++ libs/common-hooks/src/useIsBridgingEnabled.ts
@@ -1,11 +1,9 @@
-import { atom, /* useAtomValue, */ useSetAtom } from 'jotai'
+import { atom, useAtomValue, useSetAtom } from 'jotai'
 
 const isBridgingEnabledAtom = atom(false)
 
 export function useIsBridgingEnabled(): boolean {
-  // Force bridging to be enabled for testing
-  return true
-  // return useAtomValue(isBridgingEnabledAtom)
+  return useAtomValue(isBridgingEnabledAtom)
 }
 
 export function useSetIsBridgingEnabled(): (value: boolean) => void {
