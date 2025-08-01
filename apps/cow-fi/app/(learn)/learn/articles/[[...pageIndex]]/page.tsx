a117209a5dfdfd83aea83c606114211d9d5383c7
fairlight <31534717+fairlighteth@users.noreply.github.com>
2025-08-01 10:09:05 +0100

    fix(cow-fi): update articles page with caching strategy and type annotations … (#6066)
    * fix: update articles page with caching strategy and type annotations for performance
    
    * chore: remove outdated comment about caching strategy in learn pages config
    
    * chore: update revalidate constant to match CMS_CACHE_TIME

--- apps/cow-fi/app/(learn)/learn/articles/[[...pageIndex]]/page.tsx
+++ apps/cow-fi/app/(learn)/learn/articles/[[...pageIndex]]/page.tsx
@@ -1,6 +1,8 @@
-import { ArticlesPageComponents } from '@/components/ArticlesPageComponents'
 import { redirect } from 'next/navigation'
-import { Article, getArticles, getCategories } from '../../../../../services/cms'
+
+import { Article, Category, getArticles, getCategories } from '../../../../../services/cms'
+
+import { ArticlesPageComponents } from '@/components/ArticlesPageComponents'
 import { ARTICLES_PER_PAGE } from '@/const/pagination'
 import { calculateTotalPages } from '@/util/paginationUtils'
 
@@ -17,6 +19,8 @@ export type ArticlesResponse = {
   }
 }
 
+// TODO: Add proper return type annotation
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function generateStaticParams() {
   const articlesResponse = await getArticles({ page: 0, pageSize: ARTICLES_PER_PAGE })
   const totalArticles = articlesResponse.meta?.pagination?.total || 0
@@ -25,6 +29,13 @@ export async function generateStaticParams() {
   return Array.from({ length: totalPages }, (_, i) => ({ pageIndex: [(i + 1).toString()] }))
 }
 
+// Next.js requires revalidate to be a literal number for static analysis
+// This value (3600 seconds = 1 hour) should match CMS_CACHE_TIME in services/cms/index.ts
+export const revalidate = 3600
+
+// TODO: Reduce function complexity by extracting logic
+// TODO: Add proper return type annotation
+// eslint-disable-next-line complexity, @typescript-eslint/explicit-function-return-type
 export default async function Page({ params }: Props) {
   const pageParam = (await params)?.pageIndex?.[0]
   const paramsAreSet = Boolean(pageParam)
@@ -52,6 +63,8 @@ export default async function Page({ params }: Props) {
   const allArticles = allArticlesResponse.data
 
   const articles =
+    // TODO: Reduce function complexity by extracting logic
+    // eslint-disable-next-line complexity
     articlesResponse.data?.map((article: Article) => ({
       ...article,
       id: article.id || 0,
@@ -69,7 +82,7 @@ export default async function Page({ params }: Props) {
 
   const categoriesResponse = await getCategories()
   const allCategories =
-    categoriesResponse?.map((category: any) => ({
+    categoriesResponse?.map((category: Category) => ({
       name: category?.attributes?.name || '',
       slug: category?.attributes?.slug || '',
     })) || []
