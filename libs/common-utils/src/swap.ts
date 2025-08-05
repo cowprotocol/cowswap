f7a5e75f73b2824948de06072559d34180ad0388
daniele <daniele@cow.fi>
2025-08-05 11:23:32 +0100

    fix: pr env points to dev

--- libs/common-utils/src/swap.ts
+++ libs/common-utils/src/swap.ts
@@ -8,14 +8,13 @@ function _getSwapUrlByEnvironment(): Record<EnvironmentName, string> {
     barn: process.env.REACT_APP_DOMAIN_REGEX_BARN || 'https://barn.swap.cow.fi',
     ens: process.env.REACT_APP_DOMAIN_REGEX_ENS || 'https://ens.swap.cow.fi',
     staging: process.env.REACT_APP_DOMAIN_REGEX_STAGING || 'https://staging.swap.cow.fi',
-    pr: process.env.REACT_APP_DOMAIN_REGEX_PR || 'https://pr.swap.cow.fi',
+    pr: process.env.REACT_APP_DOMAIN_REGEX_PR || 'https://dev.swap.cow.fi',
     development: process.env.REACT_APP_DOMAIN_REGEX_DEV || 'https://dev.swap.cow.fi',
     local: process.env.REACT_APP_DOMAIN_REGEX_LOCAL || 'https://localhost:3000',
   }
 }
 
 export function getSwapBaseUrl(): string {
-  console.log('environmentName ==>', environmentName)
   if (!environmentName) {
     console.error('Missing environment name!')
     return DEFAULT_SWAP_URL
