2c27c51b08a297eec5e4b77b8be2ccc4066be4d7
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-08-05 15:02:43 +0100

    refactor(cowfi): clean up CMS config by removing outdated comment and fixing newline at end of file

--- apps/cow-fi/services/cms/config.ts
+++ apps/cow-fi/services/cms/config.ts
@@ -2,7 +2,6 @@ export const DEFAULT_PAGE_SIZE = 100
 export const CMS_CACHE_TIME = 60 * 60 // 60 minutes
 
 export const clientAddons = {
-  // https://github.com/openapi-ts/openapi-typescript/issues/1569#issuecomment-1982247959
   fetch: (request: unknown) =>
     fetch(request as Request, {
       next: {
