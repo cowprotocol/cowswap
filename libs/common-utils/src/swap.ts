070464255705782b623473682bc33bce470fa43b
daniele <daniele@cow.fi>
2025-08-05 11:47:58 +0100

    chore: address CR comments

--- libs/common-utils/src/swap.ts
+++ libs/common-utils/src/swap.ts
@@ -2,16 +2,14 @@ import { EnvironmentName, environmentName } from './environments'
 
 const DEFAULT_SWAP_URL = 'https://swap.cow.fi'
 
-function _getSwapUrlByEnvironment(): Record<EnvironmentName, string> {
-  return {
-    production: process.env.REACT_APP_DOMAIN_REGEX_PROD || 'https://swap.cow.fi',
-    barn: process.env.REACT_APP_DOMAIN_REGEX_BARN || 'https://barn.swap.cow.fi',
-    ens: process.env.REACT_APP_DOMAIN_REGEX_ENS || 'https://ens.swap.cow.fi',
-    staging: process.env.REACT_APP_DOMAIN_REGEX_STAGING || 'https://staging.swap.cow.fi',
-    pr: process.env.REACT_APP_DOMAIN_REGEX_PR || 'https://dev.swap.cow.fi',
-    development: process.env.REACT_APP_DOMAIN_REGEX_DEV || 'https://dev.swap.cow.fi',
-    local: process.env.REACT_APP_DOMAIN_REGEX_LOCAL || 'https://localhost:3000',
-  }
+const swapUrlByEnvironment: Record<EnvironmentName, string> = {
+  production: 'https://swap.cow.fi',
+  barn: 'https://barn.swap.cow.fi',
+  ens: 'https://ens.swap.cow.fi',
+  staging: 'https://staging.swap.cow.fi',
+  pr: 'https://dev.swap.cow.fi',
+  development: 'https://dev.swap.cow.fi',
+  local: 'https://localhost:3000',
 }
 
 export function getSwapBaseUrl(): string {
@@ -20,5 +18,5 @@ export function getSwapBaseUrl(): string {
     return DEFAULT_SWAP_URL
   }
 
-  return _getSwapUrlByEnvironment()[environmentName]
+  return swapUrlByEnvironment[environmentName]
 }
