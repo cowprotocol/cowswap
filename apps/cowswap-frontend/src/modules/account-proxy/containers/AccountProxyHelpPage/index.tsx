17d1c960aab09ff3c09ee871c37858f78e8b92b4
Alexandr Kazachenko <shoom3301@gmail.com>
2025-08-05 20:41:57 +0500

    feat(proxy-account): add help page

--- apps/cowswap-frontend/src/modules/account-proxy/containers/AccountProxyHelpPage/index.tsx
+++ apps/cowswap-frontend/src/modules/account-proxy/containers/AccountProxyHelpPage/index.tsx
@@ -1,5 +1,18 @@
 import { ReactNode } from 'react'
 
+import { useWalletInfo } from '@cowprotocol/wallet'
+
+import { Routes } from 'common/constants/routes'
+
+import { FAQContent } from '../../pure/FAQContent'
+import { parameterizeRoute } from '../../utils/parameterizeRoute'
+
 export function AccountProxyHelpPage(): ReactNode {
-  return <div>AccountProxyHelpPage</div>
+  const { chainId } = useWalletInfo()
+
+  return (
+    <div>
+      <FAQContent recoverRouteLink={parameterizeRoute(Routes.ACCOUNT_PROXIES, { chainId })} />
+    </div>
+  )
 }
