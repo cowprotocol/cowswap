0872120db3d342a80bb1d741e22ade129faa8314
Alexandr Kazachenko <shoom3301@gmail.com>
2025-08-06 15:26:31 +0500

    refactor: extract useTokenBalanceAndUsdValue

--- apps/cowswap-frontend/src/modules/account-proxy/containers/AccountProxyRecoverPage/index.tsx
+++ apps/cowswap-frontend/src/modules/account-proxy/containers/AccountProxyRecoverPage/index.tsx
@@ -1,53 +1,48 @@
 import { ReactNode } from 'react'
 
-import { useTokensBalances } from '@cowprotocol/balances-and-allowances'
-import { TokenLogo, useTokensByAddressMap } from '@cowprotocol/tokens'
+import { TokenLogo } from '@cowprotocol/tokens'
 import { ButtonPrimary, ButtonSize, FiatAmount, Loader, TokenSymbol } from '@cowprotocol/ui'
-import { CurrencyAmount } from '@uniswap/sdk-core'
 
 import { useParams } from 'react-router'
 
-import { useUsdAmount } from 'modules/usdAmount'
-
 import { BalanceWrapper, TokenAmountStyled, TokenLogoWrapper, TokenWrapper } from './styled'
+
+import { useTokenBalanceAndUsdValue } from '../../hooks/useTokenBalanceAndUsdValue'
 
 export function AccountProxyRecoverPage(): ReactNode {
   const { tokenAddress } = useParams()
-  const tokensByAddress = useTokensByAddressMap()
-  const { values: balances } = useTokensBalances()
 
-  const tokenKey = tokenAddress?.toLowerCase() || undefined
-
-  const token = !!tokenKey && tokensByAddress[tokenKey]
-  const balanceRaw = !!tokenKey && balances[tokenKey]
-
-  const balance = (token && balanceRaw && CurrencyAmount.fromRawAmount(token, balanceRaw.toHexString())) || undefined
-
-  const { value: usdValue } = useUsdAmount(balance)
+  const { balance, usdValue } = useTokenBalanceAndUsdValue(tokenAddress)
 
   const onRecover = (): void => {
     // TODO: add a listener
     console.log('onRecover')
   }
 
-  if (!token || !balanceRaw) return <Loader />
-
   return (
     <div>
       <TokenWrapper>
         <p>Recoverable balance</p>
         <BalanceWrapper>
-          <TokenAmountStyled amount={balance} />
-          <TokenLogoWrapper>
-            <TokenLogo token={token} size={24} />
-            <TokenSymbol token={token} />
-          </TokenLogoWrapper>
+          {balance ? (
+            <>
+              <TokenAmountStyled amount={balance} />
+              <TokenLogoWrapper>
+                <TokenLogo token={balance.currency} size={24} />
+                <TokenSymbol token={balance.currency} />
+              </TokenLogoWrapper>
+            </>
+          ) : (
+            <div>
+              <Loader />
+            </div>
+          )}
         </BalanceWrapper>
         <div>
           <FiatAmount amount={usdValue} />
         </div>
       </TokenWrapper>
-      <ButtonPrimary buttonSize={ButtonSize.BIG} onClick={onRecover}>
+      <ButtonPrimary disabled={!balance} buttonSize={ButtonSize.BIG} onClick={onRecover}>
         Recover funds
       </ButtonPrimary>
     </div>
