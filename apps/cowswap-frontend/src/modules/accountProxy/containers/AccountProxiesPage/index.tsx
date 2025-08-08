0e644481bce172fe89173aa1385aa03e677243bd
Alexandr Kazachenko <shoom3301@gmail.com>
2025-08-08 13:07:38 +0500

    chore: add network label to refund title

--- apps/cowswap-frontend/src/modules/accountProxy/containers/AccountProxiesPage/index.tsx
+++ apps/cowswap-frontend/src/modules/accountProxy/containers/AccountProxiesPage/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 
 import { ACCOUNT_PROXY_LABEL } from '@cowprotocol/common-const'
+import { getChainInfo } from '@cowprotocol/cow-sdk'
 import { useWalletInfo } from '@cowprotocol/wallet'
 
 import { Title } from './styled'
@@ -13,9 +14,14 @@ export function AccountProxiesPage(): ReactNode {
 
   const proxies = useAccountProxies()
 
+  const chainIfo = getChainInfo(chainId)
+  const chainLabel = chainIfo?.label
+
   return (
     <div>
-      <Title>Select an {ACCOUNT_PROXY_LABEL} to see the amounts available for refund</Title>
+      <Title>
+        Select an {ACCOUNT_PROXY_LABEL} to see the amounts available for refund {chainLabel ? `on ${chainLabel}` : ''}
+      </Title>
       <div>
         {proxies?.map(({ account, version }) => {
           return <AccountItem key={account} chainId={chainId} account={account} version={version} />
