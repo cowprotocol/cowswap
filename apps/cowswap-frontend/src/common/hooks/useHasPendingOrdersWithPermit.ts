787f0b811464f40e0bdd684a963ac355b5f5fc5b
limitofzero <limitofzero@gmail.com>
2025-08-13 18:30:45 +0400

    refactor: move permit checking

--- apps/cowswap-frontend/src/common/hooks/useHasPendingOrdersWithPermit.ts
+++ apps/cowswap-frontend/src/common/hooks/useHasPendingOrdersWithPermit.ts
@@ -5,11 +5,9 @@ import { Nullish } from '@cowprotocol/types'
 import { useWalletInfo } from '@cowprotocol/wallet'
 import { Currency } from '@uniswap/sdk-core'
 
-import { useOnlyPendingOrders } from '../../legacy/state/orders/hooks'
-import { getAppDataHooks } from '../../modules/appData'
+import { useOnlyPendingOrders } from 'legacy/state/orders/hooks'
 
-const DAI_LIKE_SELECTOR = '8fcbaf0c'
-const EIP2612_SELECTOR = 'd505accf'
+import { doesOrderHavePermit } from '../utils/doesOrderHavePermit'
 
 export function useHasPendingOrdersWithPermitForInputToken(
   currency: Nullish<Currency>,
@@ -25,19 +23,8 @@ export function useHasPendingOrdersWithPermitForInputToken(
     return pendingOrders
       .map((order) => {
         if (!areAddressesEqual(currency?.address, order.sellToken)) return false
-
-        const appData = order.fullAppData
-        const hooks = getAppDataHooks(appData)
-        if (!hooks?.pre) return false
-
-        return hooks.pre.filter((hook) => hasPermitSelector(hook.callData)).length > 0
+        return doesOrderHavePermit(order)
       })
       .some(Boolean)
   }, [pendingOrders, currency, isPermitSupported])
-}
-
-function hasPermitSelector(calldata: string): boolean {
-  calldata = calldata.toLowerCase()
-  if (calldata.includes(EIP2612_SELECTOR)) return true
-  return calldata.includes(DAI_LIKE_SELECTOR)
 }
