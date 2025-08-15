25ed87167e5d4eecc8664edf42758ca93e52f10a
Elena <70885163+elena-zh@users.noreply.github.com>
2025-08-15 13:05:20 +0300

    update tooltip text

--- apps/cowswap-frontend/src/modules/tradeFormValidation/pure/TradeFormButtons/tradeButtonsMap.tsx
+++ apps/cowswap-frontend/src/modules/tradeFormValidation/pure/TradeFormButtons/tradeButtonsMap.tsx
@@ -115,7 +115,7 @@ export const tradeButtonsMap: Record<TradeFormValidation, ButtonErrorConfig | Bu
           {isBridging && recipient && (
             <HelpTooltip
               placement="top"
-              text="Swap&Bridge doesn't support ENS name as a recipient value. Use address instead"
+              text="ENS recipient not supported for Swap and Bridge — use address instead."
             />
           )}
         </>
