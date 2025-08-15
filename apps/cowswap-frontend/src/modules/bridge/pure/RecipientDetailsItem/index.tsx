9c1f68f42ebc0db6624b80dcb730abd7148a263c
Alexandr Kazachenko <shoom3301@gmail.com>
2025-08-15 09:12:21 +0500

    chore: remove excessive RecipientDisplay component

--- apps/cowswap-frontend/src/modules/bridge/pure/RecipientDetailsItem/index.tsx
+++ apps/cowswap-frontend/src/modules/bridge/pure/RecipientDetailsItem/index.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from 'react'
 
-import { InfoTooltip } from '@cowprotocol/ui'
+import { InfoTooltip, NetworkLogo } from '@cowprotocol/ui'
 
 import { ConfirmDetailsItem } from 'modules/trade'
 import { BRIDGE_QUOTE_ACCOUNT } from 'modules/tradeQuote'
 
-import { RecipientDisplay } from '../RecipientDisplay'
+import { AddressLink } from '../../../../common/pure/AddressLink'
+import { RecipientWrapper } from '../../styles'
 
 interface RecipientDetailsItemProps {
   recipient: string
@@ -25,7 +26,10 @@ export function RecipientDetailsItem({ recipient, chainId }: RecipientDetailsIte
             </>
           }
         >
-          <RecipientDisplay recipient={recipient} chainId={chainId} logoSize={16} />
+          <RecipientWrapper>
+            <NetworkLogo chainId={chainId} size={16} />
+            <AddressLink address={recipient} chainId={chainId} />
+          </RecipientWrapper>
         </ConfirmDetailsItem>
       )}
     </>
