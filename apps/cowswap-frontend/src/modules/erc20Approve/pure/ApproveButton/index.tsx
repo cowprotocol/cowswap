6384f2b511cb33aad897181979e11fa371e3938b
limitofzero <limitofzero@gmail.com>
2025-08-18 19:36:34 +0400

    refactor: use styled instead of theme hook

--- apps/cowswap-frontend/src/modules/erc20Approve/pure/ApproveButton/index.tsx
+++ apps/cowswap-frontend/src/modules/erc20Approve/pure/ApproveButton/index.tsx
@@ -1,12 +1,11 @@
-import { ReactNode, useContext } from 'react'
+import { ReactNode } from 'react'
 
 import { TokenLogo } from '@cowprotocol/tokens'
 import { Command } from '@cowprotocol/types'
-import { ButtonPrimary, Loader, TokenSymbol } from '@cowprotocol/ui'
+import { ButtonPrimary, TokenSymbol } from '@cowprotocol/ui'
 import { Currency } from '@uniswap/sdk-core'
 
 import { Trans } from '@lingui/macro'
-import { ThemeContext } from 'styled-components/macro'
 
 import * as styledEl from './styledEl'
 
@@ -23,7 +22,6 @@ export interface ApproveButtonProps {
 export function ApproveButton(props: ApproveButtonProps): ReactNode {
   const { currency, state, onClick, isDisabled, children } = props
 
-  const theme = useContext(ThemeContext)
   const isPending = state === ApprovalState.PENDING
   const disabled = isDisabled || state !== ApprovalState.NOT_APPROVED
 
@@ -39,7 +37,7 @@ export function ApproveButton(props: ApproveButtonProps): ReactNode {
                 Allow CoW Swap to use your <TokenSymbol token={currency} />
               </Trans>
             </span>
-            <span>{isPending && <Loader stroke={theme.text1} />}</span>
+            <span>{isPending && <styledEl.StyledLoader />}</span>
           </>
         </styledEl.ApproveButtonWrapper>
       </ButtonPrimary>
