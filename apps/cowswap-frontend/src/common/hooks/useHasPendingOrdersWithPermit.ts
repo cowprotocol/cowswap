9c5070e623bfacde8f61f44077da6846fd4a70ee
limitofzero <limitofzero@gmail.com>
2025-08-18 16:33:05 +0400

    fix: copmarison

--- apps/cowswap-frontend/src/common/hooks/useHasPendingOrdersWithPermit.ts
+++ apps/cowswap-frontend/src/common/hooks/useHasPendingOrdersWithPermit.ts
@@ -17,7 +17,7 @@ export function useHasPendingOrdersWithPermitForInputToken(
   const pendingOrders = useOnlyPendingOrders(chainId, account)
 
   return useMemo(() => {
-    if (typeof isPermitSupported === 'boolean' && !isPermitSupported) return false
+    if (isPermitSupported === false) return false
     if (currency && getIsNativeToken(currency)) return false
 
     return pendingOrders
