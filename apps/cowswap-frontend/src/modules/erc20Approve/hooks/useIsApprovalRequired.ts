63c2551663032b3a16807e4d36733de73f69d3bb
limitofzero <limitofzero@gmail.com>
2025-08-20 15:39:58 +0400

    fix: don't push approve if permit exists

--- apps/cowswap-frontend/src/modules/erc20Approve/hooks/useIsApprovalRequired.ts
+++ apps/cowswap-frontend/src/modules/erc20Approve/hooks/useIsApprovalRequired.ts
@@ -1,8 +1,6 @@
 import { useTokenSupportsPermit } from 'modules/permit'
 import { useAmountsToSign, useDerivedTradeState } from 'modules/trade'
 
-import { useHasPendingOrdersWithPermitForInputToken } from 'common/hooks/useHasPendingOrdersWithPermit'
-
 import { useApproveState } from './useApproveState'
 
 import { ApprovalState } from '../types'
@@ -15,12 +13,6 @@ export function useIsApprovalRequired(): boolean {
   const { inputCurrency, tradeType } = derivedTradeState || {}
 
   const isPermitSupported = useTokenSupportsPermit(inputCurrency, tradeType)
-  const hasActiveOrderWithTheSamePermit = useHasPendingOrdersWithPermitForInputToken(inputCurrency, isPermitSupported)
 
-  // if the user has order with permit for current token that wasn't executed yet
-  const allowPermitSigning = isPermitSupported && !hasActiveOrderWithTheSamePermit
-
-  return (
-    !allowPermitSigning && (approvalState === ApprovalState.NOT_APPROVED || approvalState === ApprovalState.PENDING)
-  )
+  return !isPermitSupported && (approvalState === ApprovalState.NOT_APPROVED || approvalState === ApprovalState.PENDING)
 }
