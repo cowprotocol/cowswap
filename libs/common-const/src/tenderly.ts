829a4df186895fb346c6937f4d9a17aa0fc2f7cb
Alfetopito <alfetopito@users.noreply.github.com>
2025-08-22 16:47:35 +0100

    chore: temporarily enable tenderly for lens

--- libs/common-const/src/tenderly.ts
+++ libs/common-const/src/tenderly.ts
@@ -2,5 +2,5 @@ import { mapSupportedNetworks, SupportedChainId } from '@cowprotocol/cow-sdk'
 
 export const TENDERLY_AVAILABLE: Record<SupportedChainId, boolean> = {
   ...mapSupportedNetworks(true),
-  [SupportedChainId.LENS]: false, // Lens is not supported by Tenderly ATM (2025-08)
+  // [SupportedChainId.LENS]: false, // Lens is not supported by Tenderly ATM (2025-08)
 }
