23b136d5f19f791aacee034a5470ce633dcdef93
Alfetopito <alfetopito@users.noreply.github.com>
2025-08-25 18:27:58 +0100

    fix: remove unused settings for NPS

--- apps/cowswap-frontend/src/appzi.ts
+++ apps/cowswap-frontend/src/appzi.ts
@@ -24,11 +24,8 @@ export const isAppziEnabled =
 
 const PROD_FEEDBACK_KEY = 'f7591eca-72f7-4888-b15f-e7ff5fcd60cd'
 const TEST_FEEDBACK_KEY = '6da8bf10-4904-4952-9a34-12db70e9194e'
-const PROD_NPS_KEY = '55872789-593b-4c6c-9e49-9b5c7693e90a'
-const TEST_NPS_KEY = '5b794318-f81c-4dac-83ba-15a6e4c9353d'
 
 const FEEDBACK_KEY = process.env.REACT_APP_APPZI_FEEDBACK_KEY || isProdLike ? PROD_FEEDBACK_KEY : TEST_FEEDBACK_KEY
-const NPS_KEY = process.env.REACT_APP_APPZI_NPS_KEY || isProdLike ? PROD_NPS_KEY : TEST_NPS_KEY
 
 const APPZI_TOKEN = process.env.REACT_APP_APPZI_TOKEN || '5ju0G'
 
@@ -102,14 +99,6 @@ export function openFeedbackAppzi(params: { account?: string; walletName?: strin
   if (typeof window !== 'undefined') {
     updateAppziSettings({ data: { account, chainId, walletName } })
     window.appzi?.openWidget(FEEDBACK_KEY)
-  }
-}
-
-// TODO: Add proper return type annotation
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function openNpsAppzi() {
-  if (typeof window !== 'undefined') {
-    window.appzi?.openWidget(NPS_KEY)
   }
 }
 
