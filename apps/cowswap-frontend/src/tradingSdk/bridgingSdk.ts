9c236200b5f635f1f465e5b05181f44ff0472c97
daniele <daniele@cow.fi>
2025-08-28 18:14:30 +0100

    chore: remove affiliate code from header

--- apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
+++ apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
@@ -1,18 +1,13 @@
 import { getRpcProvider } from '@cowprotocol/common-const'
-import { isLocal } from '@cowprotocol/common-utils'
 import { BridgingSdk, BungeeBridgeProvider } from '@cowprotocol/cow-sdk'
 
 import { orderBookApi } from 'cowSdk'
 
 import { tradingSdk } from './tradingSdk'
 
-const bungeeApiBase = getBungeeApiBase()
-
 export const bungeeBridgeProvider = new BungeeBridgeProvider({
   apiOptions: {
     includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
-    apiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee` : undefined,
-    manualApiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee-manual` : undefined,
   },
   getRpcProvider,
 })
@@ -23,8 +18,3 @@ export const bridgingSdk = new BridgingSdk({
   tradingSdk,
   orderBookApi,
 })
-
-// returns private endpoint if not local, otherwise undefined
-function getBungeeApiBase(): string | undefined {
-  return isLocal ? undefined : 'https://backend.bungee.exchange'
-}
--- apps/explorer/src/sdk/cowSdk.ts
+++ apps/explorer/src/sdk/cowSdk.ts
@@ -1,16 +1,11 @@
 import { getRpcProvider } from '@cowprotocol/common-const'
-import { isLocal } from '@cowprotocol/common-utils'
 import { AcrossBridgeProvider, BungeeBridgeProvider, OrderBookApi } from '@cowprotocol/cow-sdk'
 
 export const orderBookApi = new OrderBookApi()
 
-const bungeeAffiliateCode =
-  '609913096e1a3d62cecd0ffff47aa3e459eaedceb5fef75aad43e6cbff367039708902197e0b2b78b1d76cb0837ad0b318baedceb5fef75aad43e6cb'
-
 export const bungeeBridgeProvider = new BungeeBridgeProvider({
   apiOptions: {
     includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
-    affiliate: isLocal ? undefined : bungeeAffiliateCode,
   },
   getRpcProvider,
 })
