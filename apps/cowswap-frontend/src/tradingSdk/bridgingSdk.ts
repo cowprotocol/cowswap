5b1313a86d3bceaa299e4616b1e56b2906cbaffe
daniele <daniele@cow.fi>
2025-08-28 11:01:17 +0100

    feat: add Bungee affiliate code

--- apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
+++ apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
@@ -8,11 +8,15 @@ import { tradingSdk } from './tradingSdk'
 
 const bungeeApiBase = getBungeeApiBase()
 
+const bungeeAffiliateCode =
+  '609913096e1a3d62cecd0ffff47aa3e459eaedceb5fef75aad43e6cbff367039708902197e0b2b78b1d76cb0837ad0b318baedceb5fef75aad43e6cb'
+
 export const bungeeBridgeProvider = new BungeeBridgeProvider({
   apiOptions: {
     includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
     apiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee` : undefined,
     manualApiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee-manual` : undefined,
+    affiliate: bungeeApiBase ? bungeeAffiliateCode : undefined,
   },
   getRpcProvider,
 })
