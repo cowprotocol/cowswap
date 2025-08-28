55b38cc3803eeebebd7f346578466ac5c83a741e
daniele <daniele@cow.fi>
2025-08-28 17:37:45 +0100

    fix: call bungee private endpoint from swap and public from explorer

--- apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
+++ apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
@@ -8,17 +8,11 @@ import { tradingSdk } from './tradingSdk'
 
 const bungeeApiBase = getBungeeApiBase()
 
-const bungeeAffiliateCode =
-  '609913096e1a3d62cecd0ffff47aa3e459eaedceb5fef75aad43e6cbff367039708902197e0b2b78b1d76cb0837ad0b318baedceb5fef75aad43e6cb'
-
 export const bungeeBridgeProvider = new BungeeBridgeProvider({
   apiOptions: {
     includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
-    // if undefined, the sdk will default to the public api that allows custom headers
     apiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee` : undefined,
-    // if undefined, the sdk will default to the public api that allows custom headers
     manualApiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee-manual` : undefined,
-    affiliate: bungeeApiBase ? undefined : bungeeAffiliateCode,
   },
   getRpcProvider,
 })
@@ -30,6 +24,7 @@ export const bridgingSdk = new BridgingSdk({
   orderBookApi,
 })
 
+// returns private endpoint if not local, otherwise undefined
 function getBungeeApiBase(): string | undefined {
-  return isLocal ? 'https://backend.bungee.exchange' : undefined
+  return isLocal ? undefined : 'https://backend.bungee.exchange'
 }
--- apps/explorer/src/sdk/cowSdk.ts
+++ apps/explorer/src/sdk/cowSdk.ts
@@ -3,7 +3,6 @@ import { isLocal } from '@cowprotocol/common-utils'
 import { AcrossBridgeProvider, BungeeBridgeProvider, OrderBookApi } from '@cowprotocol/cow-sdk'
 
 export const orderBookApi = new OrderBookApi()
-const bungeeApiBase = getBungeeApiBase()
 
 const bungeeAffiliateCode =
   '609913096e1a3d62cecd0ffff47aa3e459eaedceb5fef75aad43e6cbff367039708902197e0b2b78b1d76cb0837ad0b318baedceb5fef75aad43e6cb'
@@ -11,11 +10,7 @@ const bungeeAffiliateCode =
 export const bungeeBridgeProvider = new BungeeBridgeProvider({
   apiOptions: {
     includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
-    // if undefined, the sdk will default to the public api that allows custom headers
-    apiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee` : undefined,
-    // if undefined, the sdk will default to the public api that allows custom headers
-    manualApiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee-manual` : undefined,
-    affiliate: bungeeApiBase ? undefined : bungeeAffiliateCode,
+    affiliate: isLocal ? undefined : bungeeAffiliateCode,
   },
   getRpcProvider,
 })
@@ -23,7 +18,3 @@ export const bungeeBridgeProvider = new BungeeBridgeProvider({
 export const acrossBridgeProvider = new AcrossBridgeProvider({ getRpcProvider })
 
 export const knownBridgeProviders = [bungeeBridgeProvider, acrossBridgeProvider]
-
-function getBungeeApiBase(): string | undefined {
-  return isLocal ? 'https://backend.bungee.exchange' : undefined
-}
