22d08941a67032712e6d206ea8579786ed78715f
cowdan <daniele@cow.fi>
2025-09-11 11:22:01 +0100

    feat: restore Gnosis Affiliate header (#6228)
    * feat: restore Gnosis Affiliate header
    
    * chore: remove hardcoded value
    
    * fix: missing dependency
    
    * chore: address CR comments

--- apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
+++ apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
@@ -1,12 +1,19 @@
+import { bungeeAffiliateCode } from '@cowprotocol/common-const'
+import { isBarn, isDev, isProd, isStaging } from '@cowprotocol/common-utils'
 import { BridgingSdk, BungeeBridgeProvider } from '@cowprotocol/sdk-bridging'
 
 import { orderBookApi } from 'cowSdk'
 
 import { tradingSdk } from './tradingSdk'
 
+const bungeeApiBase = getBungeeApiBase()
+
 export const bungeeBridgeProvider = new BungeeBridgeProvider({
   apiOptions: {
     includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
+    apiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee` : undefined,
+    manualApiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee-manual` : undefined,
+    affiliate: bungeeApiBase ? bungeeAffiliateCode : undefined,
   },
 })
 
@@ -16,3 +23,11 @@ export const bridgingSdk = new BridgingSdk({
   tradingSdk,
   orderBookApi,
 })
+
+function getBungeeApiBase(): string | undefined {
+  if (isProd || isDev || isStaging || isBarn) {
+    return 'https://backend.bungee.exchange'
+  }
+
+  return undefined
+}
