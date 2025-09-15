e652d2b87945f43889d54d924c021a002993e974
limitofzero <limitofzero@gmail.com>
2025-09-15 21:02:54 +0400

    feat: add container for order

--- apps/cowswap-frontend/src/modules/erc20Approve/pure/AffectedOrdersWithPermit/AffectedOrdersWithPermit.tsx
+++ apps/cowswap-frontend/src/modules/erc20Approve/pure/AffectedOrdersWithPermit/AffectedOrdersWithPermit.tsx
@@ -1,52 +1,20 @@
 import { ReactNode } from 'react'
 
-import { TokenLogo } from '@cowprotocol/tokens'
-import { TokenAmount } from '@cowprotocol/ui'
-import { CurrencyAmount } from '@uniswap/sdk-core'
-
 import { Order } from 'legacy/state/orders/actions'
 
 import * as styledEl from './styled'
 
-import { parseOrder } from '../../../../utils/orderUtils/parseOrder'
-import { useOrdersTableState } from '../../../ordersTable/hooks/useOrdersTableState'
-import { OrderStatusBox } from '../../../ordersTable/pure/OrderStatusBox'
+import { AffectedPermitOrder } from '../../containers/AffectedPermitOrder'
 
 export type AffectedOrdersWithPermitProps = {
   orders: Order[]
 }
 
 export function AffectedOrdersWithPermit({ orders }: AffectedOrdersWithPermitProps): ReactNode {
-  const { orderActions } = useOrdersTableState() ?? {}
-
-  console.log('orderActions', orderActions)
-
   return orders.map((order) => {
-    const inputAmount = CurrencyAmount.fromRawAmount(order.inputToken, order.sellAmount.toString())
-    const outputAmount = CurrencyAmount.fromRawAmount(order.outputToken, order.buyAmount.toString())
-    const parsedOrder = parseOrder(order)
-
     return (
       <styledEl.OrderWrapper key={order.id}>
-        <styledEl.SwapInfo>
-          <styledEl.TokenLogos>
-            <TokenLogo size={28} token={order.inputToken} hideNetworkBadge />
-            <TokenLogo size={28} token={order.outputToken} hideNetworkBadge />
-          </styledEl.TokenLogos>
-          <styledEl.AmountInfo>
-            <TokenAmount className={'token-amount'} amount={inputAmount} tokenSymbol={order.inputToken} />
-            <TokenAmount className={'token-amount'} amount={outputAmount} tokenSymbol={order.outputToken} />
-          </styledEl.AmountInfo>
-        </styledEl.SwapInfo>
-
-        <styledEl.OrderActionsWrapper>
-          <OrderStatusBox
-            order={parsedOrder}
-            onClick={() => {
-              orderActions?.selectReceiptOrder(parsedOrder)
-            }}
-          />
-        </styledEl.OrderActionsWrapper>
+        <AffectedPermitOrder order={order} />
       </styledEl.OrderWrapper>
     )
   })
--- apps/cowswap-frontend/src/modules/erc20Approve/pure/AffectedOrdersWithPermit/styled.ts
+++ apps/cowswap-frontend/src/modules/erc20Approve/pure/AffectedOrdersWithPermit/styled.ts
@@ -11,30 +11,3 @@ export const OrderWrapper = styled.div`
   border-radius: 4px;
   margin-bottom: 1px;
 `
-
-export const SwapInfo = styled.div`
-  display: flex;
-`
-
-export const AmountInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 6px;
-
-  .token-amount {
-    font-size: 12px;
-  }
-`
-
-export const TokenLogos = styled.div`
-  display: flex;
-  align-items: center;
-
-  > *:not(:first-child) {
-    margin-left: -14px;
-  }
-`
-
-export const OrderActionsWrapper = styled.div`
-  display: flex;
-`
