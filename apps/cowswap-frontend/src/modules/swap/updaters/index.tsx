301fe16f128ab7314d7e9f1d97ed7fe969dc1812
daniele <daniele@cow.fi>
2025-09-23 17:51:06 +0100

    chore: address CR comments

--- apps/cowswap-frontend/src/modules/swap/updaters/index.tsx
+++ apps/cowswap-frontend/src/modules/swap/updaters/index.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react'
+
 import { useIsWindowVisible } from '@cowprotocol/common-hooks'
 import { isSellOrder, percentToBps } from '@cowprotocol/common-utils'
 
@@ -5,7 +7,7 @@ import { useLocation } from 'react-router'
 
 import { AppDataUpdater } from 'modules/appData'
 import { EthFlowDeadlineUpdater } from 'modules/ethFlow'
-import { ORDERS_TABLE_PAGE_SIZE, OrderTabId } from 'modules/ordersTable'
+import { OrderTabId } from 'modules/ordersTable'
 import { parseOrdersTableUrl } from 'modules/ordersTable/utils/parseOrdersTableUrl'
 import { useIsHooksTradeType } from 'modules/trade'
 import { useSetTradeQuoteParams } from 'modules/tradeQuote'
@@ -19,9 +21,10 @@ import { SetupSwapAmountsFromUrlUpdater } from './SetupSwapAmountsFromUrlUpdater
 import { useFillSwapDerivedState, useSwapDerivedState } from '../hooks/useSwapDerivedState'
 import { useSwapDeadlineState } from '../hooks/useSwapSettings'
 
-// TODO: Add proper return type annotation
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function SwapUpdaters() {
+// update last 10 pending swap orders
+const PENDING_ORDERS_PAGE_SIZE = 10
+
+export function SwapUpdaters(): ReactNode {
   const { orderKind, inputCurrencyAmount, outputCurrencyAmount, slippage } = useSwapDerivedState()
   const isSmartSlippageApplied = useIsSmartSlippageApplied()
   const swapDeadlineState = useSwapDeadlineState()
@@ -31,7 +34,7 @@ export function SwapUpdaters() {
   const location = useLocation()
   const isWindowVisible = useIsWindowVisible()
 
-  const { pageNumber, tabId } = parseOrdersTableUrl(location.search)
+  const { tabId } = parseOrdersTableUrl(location.search)
   const isTabWithPending = tabId === OrderTabId.open || tabId === OrderTabId.all
 
   useFillSwapDerivedState()
@@ -44,8 +47,7 @@ export function SwapUpdaters() {
   return (
     <>
       <UnfillableOrdersUpdater
-        pageSize={ORDERS_TABLE_PAGE_SIZE}
-        pageNumber={pageNumber}
+        pageSize={PENDING_ORDERS_PAGE_SIZE}
         isTabWithPending={isTabWithPending}
         isWindowVisible={isWindowVisible}
       />
