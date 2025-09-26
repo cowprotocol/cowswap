b8bacac2c60736ea6c2066bf6dc57f9e151ba6ef
Elena <70885163+elena-zh@users.noreply.github.com>
2025-09-26 16:41:33 +0300

    update whales and pros stats

--- apps/cow-fi/app/(main)/cow-swap/page.tsx
+++ apps/cow-fi/app/(main)/cow-swap/page.tsx
@@ -303,10 +303,10 @@ export default function Page() {
             >
               <TopicCardInner contentAlign="left">
                 <TopicTitle color={`var(${UI.COLOR_NEUTRAL_100})`} fontSize={51}>
-                  $2,500
+                  $67k
                 </TopicTitle>
                 <TopicDescription fontSize={21} color={`var(${UI.COLOR_BLUE_300_PRIMARY})`}>
-                  Average trade size (more than 2x Uniswap&apos;s)
+                  Average trade size 
                 </TopicDescription>
               </TopicCardInner>
             </TopicCard>
@@ -320,7 +320,7 @@ export default function Page() {
             >
               <TopicCardInner contentAlign="left">
                 <TopicTitle color={`var(${UI.COLOR_NEUTRAL_100})`} fontSize={51}>
-                  50%
+                  67%
                 </TopicTitle>
                 <TopicDescription fontSize={21} color={`var(${UI.COLOR_BLUE_300_PRIMARY})`}>
                   Market share among smart contract wallets
@@ -337,7 +337,7 @@ export default function Page() {
             >
               <TopicCardInner contentAlign="left">
                 <TopicTitle color={`var(${UI.COLOR_NEUTRAL_100})`} fontSize={51}>
-                  42%
+                  70%
                 </TopicTitle>
                 <TopicDescription fontSize={21} color={`var(${UI.COLOR_BLUE_300_PRIMARY})`}>
                   Monthly user retention rate – the highest in DeFi
