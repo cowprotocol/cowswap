c0af76b0bb76b607d552bf109a11e424fcc24b0a
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-10-02 18:10:11 +0100

    feat: add CowLoadingIcon component

--- apps/cowswap-frontend/src/legacy/components/FlashingLoading/index.tsx
+++ apps/cowswap-frontend/src/legacy/components/FlashingLoading/index.tsx
@@ -1,7 +1,7 @@
-import { ProductLogo, ProductVariant } from '@cowprotocol/ui'
-
 import { transparentize } from 'color2k'
 import styled from 'styled-components/macro'
+
+import { CowLoadingIcon } from '../../../common/pure/CowLoadingIcon'
 
 export const LoadingWrapper = styled.div`
   display: flex;
@@ -17,52 +17,21 @@ export const LoadingWrapper = styled.div`
   height: 100vh;
   backdrop-filter: blur(3px);
 
-  > svg {
-    animation: pulse 1s infinite ease-in-out;
-    transform-style: preserve-3d;
-    backface-visibility: visible;
-  }
-
-  > svg > g {
-    fill: currentColor;
-  }
-
   > span {
     display: block;
     text-transform: uppercase;
     font-size: 10px;
     font-weight: 500;
     letter-spacing: 2px;
-    margin: 5px auto 0;
+    margin: 20px auto 0;
     color: inherit;
-  }
-
-  @keyframes pulse {
-    0% {
-      transform: scale(1);
-    }
-    20% {
-      transform: scale(1.05);
-    }
-    30% {
-      transform: scale(1);
-    }
-    40% {
-      transform: scale(1.05);
-    }
-    50% {
-      transform: scale(1);
-    }
-    100% {
-      transform: scale(1);
-    }
   }
 `
 
 export const Loading: React.FC = () => {
   return (
     <LoadingWrapper>
-      <ProductLogo variant={ProductVariant.CowSwap} height={100} logoIconOnly />
+      <CowLoadingIcon size={120} />
       <span>Loading...</span>
     </LoadingWrapper>
   )
