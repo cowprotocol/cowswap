e1301bd0549136d0726df74e517127bd94a3616d
Alexandr Kazachenko <shoom3301@gmail.com>
2025-10-03 17:35:32 +0500

    fix: add loader to slippage field

--- apps/cowswap-frontend/src/modules/tradeWidgetAddons/containers/TransactionSlippageInput/index.tsx
+++ apps/cowswap-frontend/src/modules/tradeWidgetAddons/containers/TransactionSlippageInput/index.tsx
@@ -5,19 +5,21 @@ import { HelpTooltip, RowBetween, RowFixed } from '@cowprotocol/ui'
 import { Trans } from '@lingui/macro'
 import { ThemeContext } from 'styled-components/macro'
 
-import { useSmartSlippageFromQuote } from 'modules/tradeQuote'
+import { useSmartSlippageFromQuote, useTradeQuote } from 'modules/tradeQuote'
 import { useIsSlippageModified, useIsSmartSlippageApplied, useTradeSlippage } from 'modules/tradeSlippage'
 import { SlippageWarningMessage } from 'modules/tradeWidgetAddons/pure/SlippageWarning'
 
 import { useSlippageInput } from './hooks/useSlippageInput'
 import { useSlippageWarningParams } from './hooks/useSlippageWarningParams'
 import * as styledEl from './styled'
+import { SlippageLoader } from './styled'
 
 import { SlippageTooltip } from '../SlippageTooltip'
 
 export function TransactionSlippageInput(): JSX.Element {
   const theme = useContext(ThemeContext)
 
+  const { isLoading: isQuoteLoading } = useTradeQuote()
   const swapSlippage = useTradeSlippage()
   const isSmartSlippageApplied = useIsSmartSlippageApplied()
   const smartSlippageFromQuote = useSmartSlippageFromQuote()
@@ -45,28 +47,32 @@ export function TransactionSlippageInput(): JSX.Element {
           <Trans>Auto</Trans>
         </styledEl.Option>
         <styledEl.OptionCustom active={isSlippageModified} warning={!!slippageError} tabIndex={-1}>
-          <RowBetween>
-            {slippageWarningParams && (slippageWarningParams.tooLow || slippageWarningParams?.tooHigh) ? (
-              <styledEl.SlippageEmojiContainer>
-                <span role="img" aria-label="warning">
-                  ⚠️
-                </span>
-              </styledEl.SlippageEmojiContainer>
-            ) : null}
-            <styledEl.Input
-              placeholder={placeholderSlippage.toFixed(2)}
-              value={slippageViewValue}
-              onChange={(e) => parseSlippageInput(e.target.value)}
-              onBlur={onSlippageInputBlur}
-            />
-            %
-          </RowBetween>
+          {!isSlippageModified && isQuoteLoading ? (
+            <SlippageLoader size="16px" />
+          ) : (
+            <RowBetween>
+              {slippageWarningParams && (slippageWarningParams.tooLow || slippageWarningParams?.tooHigh) ? (
+                <styledEl.SlippageEmojiContainer>
+                  <span role="img" aria-label="warning">
+                    ⚠️
+                  </span>
+                </styledEl.SlippageEmojiContainer>
+              ) : null}
+              <styledEl.Input
+                placeholder={placeholderSlippage.toFixed(2)}
+                value={slippageViewValue}
+                onChange={(e) => parseSlippageInput(e.target.value)}
+                onBlur={onSlippageInputBlur}
+              />
+              %
+            </RowBetween>
+          )}
         </styledEl.OptionCustom>
       </RowBetween>
-      {slippageWarningParams && (
+      {slippageWarningParams && !isQuoteLoading && (
         <SlippageWarningMessage error={!!slippageError} theme={theme} slippageWarningParams={slippageWarningParams} />
       )}
-      {isSmartSlippageApplied && (
+      {isSmartSlippageApplied && !isQuoteLoading && (
         <RowBetween>
           <styledEl.SmartSlippageInfo>
             <HelpTooltip
