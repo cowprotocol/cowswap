b090a2ea3c59e8051f53e11939543be7c84837d9
Alexandr Kazachenko <shoom3301@gmail.com>
2025-10-07 18:42:17 +0500

    chore: rename atom

--- apps/cowswap-frontend/src/common/updaters/orders/UnfillableOrdersUpdater/state/orderLastTimePriceUpdateAtom.ts
+++ apps/cowswap-frontend/src/common/updaters/orders/UnfillableOrdersUpdater/state/orderLastTimePriceUpdateAtom.ts
@@ -3,7 +3,7 @@ import { atomWithStorage } from 'jotai/utils'
 import { getJotaiMergerStorage } from '@cowprotocol/core'
 
 export const orderLastTimePriceUpdateAtom = atomWithStorage<Record<string, number>>(
-  'order-last-time-price-update-atom:v1',
+  'orderLastTimePriceUpdateAtom:v1',
   {},
   getJotaiMergerStorage(),
   {
