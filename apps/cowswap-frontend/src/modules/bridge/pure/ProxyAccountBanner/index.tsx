c5cfc7f235eb98ddf9d09a57dc3c2a05e5cbe6fe
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-10-17 18:07:48 -0300

    fix: message descriptor missing i18n function

--- apps/cowswap-frontend/src/modules/bridge/pure/ProxyAccountBanner/index.tsx
+++ apps/cowswap-frontend/src/modules/bridge/pure/ProxyAccountBanner/index.tsx
@@ -16,7 +16,6 @@ interface ProxyAccountBannerProps {
   bridgeReceiverOverride: string | null
 }
 
-
 export function ProxyAccountBanner({ recipient, bridgeReceiverOverride, chainId }: ProxyAccountBannerProps): ReactNode {
   const isRecipientOverridden = recipient === bridgeReceiverOverride
   const { i18n, t } = useLingui()
@@ -29,7 +28,9 @@ export function ProxyAccountBanner({ recipient, bridgeReceiverOverride, chainId 
       fontSize={13}
       collapsedContent={
         <div>
-          {isRecipientOverridden ? t`Modified recipient address to` : t`Swap bridged via your` + ` ` + ACCOUNT_PROXY_LABEL}
+          {isRecipientOverridden
+            ? t`Modified recipient address to`
+            : t`Swap bridged via your` + ` ` + accountProxyLabelString}
           : <AddressLink address={recipient} chainId={chainId} />
         </div>
       }
@@ -37,26 +38,24 @@ export function ProxyAccountBanner({ recipient, bridgeReceiverOverride, chainId 
         isRecipientOverridden ? (
           <div>
             <Trans>
-              The bridge provider modified the recipient address to <AddressLink address={recipient} chainId={chainId} />.
-              This ensure smooooth bridging.
+              The bridge provider modified the recipient address to{' '}
+              <AddressLink address={recipient} chainId={chainId} />. This ensure smooooth bridging.
             </Trans>
             <br />
             <br />
-            <Trans>
-              Only proceed if you trust this provider.
-            </Trans>
+            <Trans>Only proceed if you trust this provider.</Trans>
           </div>
         ) : (
           <div>
             <Trans>
-              CoW Swap uses a dedicated {ACCOUNT_PROXY_LABEL}, controlled only by you, to ensure smooooth bridging.
+              CoW Swap uses a dedicated {accountProxyLabelString}, controlled only by you, to ensure smooooth bridging.
               Confirm the recipient address above is <AddressLink address={recipient} chainId={chainId} />
             </Trans>
             <br />
             <br />
             <Link to={getProxyAccountUrl(chainId)} target="_blank">
               <b style={{ cursor: 'pointer', textDecoration: 'underline' }}>
-                <Trans>View your private {accountProxyLabelString}</Trans> + {' '}
+                <Trans>View your private {accountProxyLabelString}</Trans> +{' '}
               </b>
             </Link>
           </div>
