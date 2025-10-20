80b43bc6e7821947011f24729ff233d1e1be608e
Alexandr Kazachenko <shoom3301@gmail.com>
2025-10-20 16:56:25 +0200

    chore: fix confirm button text

--- apps/cowswap-frontend/src/modules/swap/hooks/useGetConfirmButtonLabel.ts
+++ apps/cowswap-frontend/src/modules/swap/hooks/useGetConfirmButtonLabel.ts
@@ -1,24 +1,22 @@
-import { useFeatureFlags } from '@cowprotocol/common-hooks'
-
-import { useIsApprovalOrPermitRequired } from 'modules/erc20Approve'
+import { ApproveRequiredReason, useIsApprovalOrPermitRequired } from 'modules/erc20Approve'
 import { useIsCurrentTradeBridging } from 'modules/trade'
 
-function getSwapLabel(isBridging: boolean): string {
-  return isBridging ? 'Swap and Bridge' : 'Swap'
+enum ConfirmButtonLabel {
+  SWAP = 'Swap',
+  SWAP_BRIDGE = 'Swap and Bridge',
+  APPROVE_SWAP = 'Approve and Swap',
+  APPROVE_SWAP_BRIDGE = 'Approve, Swap & Bridge',
 }
 
 export function useGetConfirmButtonLabel(): string {
   const isCurrentTradeBridging = useIsCurrentTradeBridging()
-  const isPermitOrApproveRequired = useIsApprovalOrPermitRequired({ isBundlingSupportedOrEnabledForContext: false })
-  const { isPartialApproveEnabled } = useFeatureFlags()
+  const { reason: approveRequiredReason } = useIsApprovalOrPermitRequired({
+    isBundlingSupportedOrEnabledForContext: false,
+  })
 
-  if (!isPartialApproveEnabled) {
-    return getSwapLabel(isCurrentTradeBridging)
+  if (approveRequiredReason === ApproveRequiredReason.Required) {
+    return isCurrentTradeBridging ? ConfirmButtonLabel.APPROVE_SWAP_BRIDGE : ConfirmButtonLabel.APPROVE_SWAP
   }
 
-  if (isPermitOrApproveRequired) {
-    return isCurrentTradeBridging ? 'Approve, Swap & Bridge' : 'Approve and Swap'
-  }
-
-  return getSwapLabel(isCurrentTradeBridging)
+  return isCurrentTradeBridging ? ConfirmButtonLabel.SWAP_BRIDGE : ConfirmButtonLabel.SWAP
 }
