b372725a4ddef6d7e5bf5956270510799979bf3f
Alfetopito <alfetopito@users.noreply.github.com>
2025-10-21 18:41:18 +0100

    feat: add linea/plasma/bnb bridge banner

--- apps/cowswap-frontend/src/modules/swap/containers/NetworkBridgeBanner/NetworkBridgeBanner.tsx
+++ apps/cowswap-frontend/src/modules/swap/containers/NetworkBridgeBanner/NetworkBridgeBanner.tsx
@@ -51,9 +51,13 @@ const RootWrapper = styled.div`
   }
 `
 
+// TODO: remove once network is support for cross-chain swaps
 const SHOULD_SHOW_ALERT = {
   [SupportedChainId.GNOSIS_CHAIN]: true,
   [SupportedChainId.LENS]: true,
+  [SupportedChainId.BNB]: true,
+  [SupportedChainId.LINEA]: true,
+  [SupportedChainId.PLASMA]: true,
 }
 
 type NetworkAlertChains = keyof typeof SHOULD_SHOW_ALERT
