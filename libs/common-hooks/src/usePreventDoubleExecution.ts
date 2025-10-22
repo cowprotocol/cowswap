4bf55fcda7818cfbcea55ab0c1b1e10eae1d15ac
Alexandr Kazachenko <shoom3301@gmail.com>
2025-10-22 15:59:15 +0200

    fix(approve): add loading state when approve request is sent

--- libs/common-hooks/src/usePreventDoubleExecution.ts
+++ libs/common-hooks/src/usePreventDoubleExecution.ts
@@ -1,17 +1,23 @@
 import { useCallback, useState } from 'react'
 
-export function usePreventDoubleExecution<R>(fn: () => Promise<R>): () => Promise<R | undefined> {
+export function usePreventDoubleExecution<R>(fn: () => Promise<R>): {
+  callback: () => Promise<R | undefined>
+  isExecuting: boolean
+} {
   const [isExecuting, setIsExecuting] = useState(false)
 
-  return useCallback(async (): Promise<R | undefined> => {
-    if (isExecuting) return
+  return {
+    callback: useCallback(async (): Promise<R | undefined> => {
+      if (isExecuting) return
 
-    setIsExecuting(true)
-    try {
-      const result = await fn()
-      return result
-    } finally {
-      setIsExecuting(false)
-    }
-  }, [fn, isExecuting])
+      setIsExecuting(true)
+      try {
+        const result = await fn()
+        return result
+      } finally {
+        setIsExecuting(false)
+      }
+    }, [fn, isExecuting]),
+    isExecuting,
+  }
 }
