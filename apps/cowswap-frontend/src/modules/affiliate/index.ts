508f1ccaed1599d4f89f3ff63727bdfee4ba8171
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-10-24 18:05:07 +0100

    feat(affiliate): add referral modal state hook

--- apps/cowswap-frontend/src/modules/affiliate/index.ts
+++ apps/cowswap-frontend/src/modules/affiliate/index.ts
@@ -3,6 +3,7 @@ export * from './constants'
 export { ReferralProvider, useReferralContext } from './state/ReferralContext'
 export { useReferral } from './hooks/useReferral'
 export { useReferralActions } from './hooks/useReferralActions'
+export { useReferralModalState } from './hooks/useReferralModalState'
 export {
   verifyReferralCode,
   getWalletReferralStatus,
