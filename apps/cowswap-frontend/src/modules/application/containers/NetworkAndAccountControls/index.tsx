1cd9b934dbb4dc85f18463116b005d77a069daf2
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-10-24 17:38:48 +0100

    feat(affiliate): wire referral ui entrypoints

--- apps/cowswap-frontend/src/modules/application/containers/NetworkAndAccountControls/index.tsx
+++ apps/cowswap-frontend/src/modules/application/containers/NetworkAndAccountControls/index.tsx
@@ -1,17 +1,36 @@
 import { ReactNode } from 'react'
 
+import { useFeatureFlags } from '@cowprotocol/common-hooks'
+import { ButtonSecondary, ButtonSize } from '@cowprotocol/ui'
+
+import { Trans } from '@lingui/macro'
+
 import { AccountElement } from 'legacy/components/Header/AccountElement'
 import { NetworkSelector } from 'legacy/components/Header/NetworkSelector'
 import { HeaderControls, HeaderElement } from 'legacy/components/Header/styled'
 
+import { useReferralActions } from 'modules/affiliate'
 import { useInjectedWidgetParams } from 'modules/injectedWidget'
 
 export function NetworkAndAccountControls(): ReactNode {
   const { hideNetworkSelector } = useInjectedWidgetParams()
+  const { isAffiliateRewardsEnabled } = useFeatureFlags()
+  const referralActions = useReferralActions()
+
+  const handleOpenReferral = (): void => {
+    referralActions.openModal('header')
+  }
 
   return (
     <HeaderControls>
       {!hideNetworkSelector && <NetworkSelector />}
+      {isAffiliateRewardsEnabled && (
+        <HeaderElement>
+          <ButtonSecondary buttonSize={ButtonSize.SMALL} width="auto" onClick={handleOpenReferral}>
+            <Trans>Enter referral code</Trans>
+          </ButtonSecondary>
+        </HeaderElement>
+      )}
       <HeaderElement>
         <AccountElement />
       </HeaderElement>
