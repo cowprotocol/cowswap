fc8fe40c327019e0b635e20b259d9eab31fe0646
Anxo Rodriguez <anxolin@gmail.com>
2025-11-04 01:22:23 +0000

    feat: add near provider

--- apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
+++ apps/cowswap-frontend/src/tradingSdk/bridgingSdk.ts
@@ -1,33 +1,35 @@
-import { bungeeAffiliateCode } from '@cowprotocol/common-const'
-import { isBarn, isDev, isProd, isProdLike, isStaging } from '@cowprotocol/common-utils'
 import {
-  AcrossBridgeProvider,
-  BridgeProvider,
-  BridgeQuoteResult,
+  NearIntentsBridgeProvider,
+  // AcrossBridgeProvider,
+  // BridgeProvider,
+  // BridgeQuoteResult,
   BridgingSdk,
-  BungeeBridgeProvider,
+  // BungeeBridgeProvider,
 } from '@cowprotocol/sdk-bridging'
 
 import { orderBookApi } from 'cowSdk'
 
 import { tradingSdk } from './tradingSdk'
 
-const bungeeApiBase = getBungeeApiBase()
+// const bungeeApiBase = getBungeeApiBase()
 
-const bungeeBridgeProvider = new BungeeBridgeProvider({
-  apiOptions: {
-    includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
-    apiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee` : undefined,
-    manualApiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee-manual` : undefined,
-    affiliate: bungeeApiBase ? bungeeAffiliateCode : undefined,
-  },
-})
+// const bungeeBridgeProvider = new BungeeBridgeProvider({
+//   apiOptions: {
+//     includeBridges: ['across', 'cctp', 'gnosis-native-bridge'],
+//     apiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee` : undefined,
+//     manualApiBaseUrl: bungeeApiBase ? `${bungeeApiBase}/api/v1/bungee-manual` : undefined,
+//     affiliate: bungeeApiBase ? bungeeAffiliateCode : undefined,
+//   },
+// })
 
-const acrossBridgeProvider = new AcrossBridgeProvider()
-export const bridgeProviders: BridgeProvider<BridgeQuoteResult>[] = [bungeeBridgeProvider]
+const nearIntentsBridgeProvider = new NearIntentsBridgeProvider()
+export const bridgeProviders = [nearIntentsBridgeProvider]
+
+// const acrossBridgeProvider = new AcrossBridgeProvider()
+// export const bridgeProviders: BridgeProvider<BridgeQuoteResult>[] = [nearIntentsBridgeProvider]
 
 // TODO: Should not enable Across on Prod until it's finalized
-!isProdLike && bridgeProviders.push(acrossBridgeProvider)
+// !isProdLike && bridgeProviders.push(acrossBridgeProvider)
 
 export const bridgingSdk = new BridgingSdk({
   providers: bridgeProviders,
@@ -36,10 +38,10 @@ export const bridgingSdk = new BridgingSdk({
   orderBookApi,
 })
 
-function getBungeeApiBase(): string | undefined {
-  if (isProd || isDev || isStaging || isBarn) {
-    return 'https://backend.bungee.exchange'
-  }
+// function getBungeeApiBase(): string | undefined {
+//   if (isProd || isDev || isStaging || isBarn) {
+//     return 'https://backend.bungee.exchange'
+//   }
 
-  return 'https://bff.barn.cow.fi/proxies/socket'
-}
+//   return 'https://bff.barn.cow.fi/proxies/socket'
+// }
