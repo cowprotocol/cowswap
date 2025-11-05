1d72a00146eebb026a1cc927f8a9c21a5fe5441e
Elena <70885163+elena-zh@users.noreply.github.com>
2025-11-05 11:30:43 +0300

    fix comments

--- apps/cowswap-frontend/src/common/hooks/useUnsupportedNetworksText.tsx
+++ apps/cowswap-frontend/src/common/hooks/useUnsupportedNetworksText.tsx
@@ -1,9 +1,9 @@
 import { ReactElement } from 'react'
 
-import { getChainInfo } from '@cowprotocol/common-const'
 import { Trans } from '@lingui/macro'
 
 export function useUnsupportedNetworksText(): ReactElement {
+  // TODO: turn into a pure component now that it doesn't need additional info
   return (
     <Trans>
       Please connect your wallet to one of our supported networks.
