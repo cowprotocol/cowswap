0e8e6f5c8174681f830ac8975f529dcc6bccc184
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-11-07 15:20:12 -0300

    fix: revert localized string to original state

--- apps/cowswap-frontend/src/modules/trade/utils/tradeFlowAnalytics.ts
+++ apps/cowswap-frontend/src/modules/trade/utils/tradeFlowAnalytics.ts
@@ -3,8 +3,6 @@ import { useMemo } from 'react'
 import { GtmEvent, useCowAnalytics } from '@cowprotocol/analytics'
 import { UiOrderType } from '@cowprotocol/types'
 
-import { useLingui } from '@lingui/react/macro'
-
 import { CowSwapAnalyticsCategory } from 'common/analytics/types'
 import { USER_SWAP_REJECTED_ERROR } from 'common/utils/getSwapErrorMessage'
 
@@ -28,7 +26,6 @@ export interface TradeFlowAnalytics {
 
 export function useTradeFlowAnalytics(): TradeFlowAnalytics {
   const analytics = useCowAnalytics()
-  const { i18n } = useLingui()
 
   return useMemo(() => {
     const sendTradeAnalytics = (
@@ -70,12 +67,12 @@ export function useTradeFlowAnalytics(): TradeFlowAnalytics {
       error(error: Error & { code?: number }, errorMessage: string, context: TradeFlowAnalyticsContext) {
         const { marketLabel, orderType } = context
 
-        if (errorMessage === i18n._(USER_SWAP_REJECTED_ERROR)) {
+        if (errorMessage === USER_SWAP_REJECTED_ERROR) {
           sendTradeAnalytics('Reject', orderType, marketLabel)
         } else {
           sendTradeAnalytics('Error', orderType, marketLabel, error.code)
         }
       },
     }
-  }, [analytics, i18n])
+  }, [analytics])
 }
