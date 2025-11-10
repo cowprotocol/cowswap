115c35dce3280003fd4399e18dd1a68346d50976
limitofzero <limitofzero@gmail.com>
2025-11-10 15:57:34 +0400

    fix: check only pending orders

--- apps/cowswap-frontend/src/pages/LimitOrders/RegularLimitOrders.tsx
+++ apps/cowswap-frontend/src/pages/LimitOrders/RegularLimitOrders.tsx
@@ -1,8 +1,10 @@
 import { useAtomValue } from 'jotai'
+import { ReactNode, useMemo } from 'react'
 
 import { UiOrderType } from '@cowprotocol/types'
 import { useWalletInfo } from '@cowprotocol/wallet'
 
+import { OrderStatus } from 'legacy/state/orders/actions'
 import { useOrders } from 'legacy/state/orders/hooks'
 
 import { useInjectedWidgetParams } from 'modules/injectedWidget'
@@ -12,12 +14,14 @@ import * as styledEl from 'modules/trade/pure/TradePageLayout'
 
 const LIMIT_ORDERS_MAX_WIDTH = '1800px'
 
-// TODO: Add proper return type annotation
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function RegularLimitOrders() {
+export function RegularLimitOrders(): ReactNode {
   const isUnlocked = useIsWidgetUnlocked()
   const { chainId, account } = useWalletInfo()
   const allLimitOrders = useOrders(chainId, account, UiOrderType.LIMIT)
+  const pendingLimitOrders = useMemo(
+    () => allLimitOrders.filter((order) => order.status === OrderStatus.PENDING),
+    [allLimitOrders],
+  )
   const { hideOrdersTable } = useInjectedWidgetParams()
   const { ordersTableOnLeft } = useAtomValue(limitOrdersSettingsAtom)
 
@@ -34,7 +38,7 @@ export function RegularLimitOrders() {
 
       {!hideOrdersTable && (
         <styledEl.SecondaryWrapper>
-          {allLimitOrders.length > 0 && <LimitOrdersPermitUpdater orders={allLimitOrders} />}
+          {pendingLimitOrders.length > 0 && <LimitOrdersPermitUpdater orders={pendingLimitOrders} />}
           <OrdersTableWidget orderType={TabOrderTypes.LIMIT} orders={allLimitOrders} />
         </styledEl.SecondaryWrapper>
       )}
