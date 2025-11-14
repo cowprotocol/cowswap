071ced1a4194fe552fb702cec749c3df9521ed78
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-11-14 10:09:01 +0000

    feat(tokenselector): add chain selection logic and enhance token widget interactions

--- apps/cowswap-frontend/src/modules/tokensList/containers/SelectTokenWidget/index.tsx
+++ apps/cowswap-frontend/src/modules/tokensList/containers/SelectTokenWidget/index.tsx
@@ -146,7 +146,7 @@ export function SelectTokenWidget({ displayLpTokenLists, standalone }: SelectTok
 
   const onDismiss = useCallback(() => {
     setIsManageWidgetOpen(false)
-    closeTokenSelectWidget()
+    closeTokenSelectWidget({ overrideForceLock: true })
   }, [closeTokenSelectWidget])
 
   const importTokenAndClose = (tokens: TokenWithLogo[]): void => {
