b3c7e14767a4f839d146f3de5db0ad4ebd4e633e
limitofzero <limitofzero@gmail.com>
2025-11-18 00:56:44 +0400

    fix: 2 ways to calculate fee for a tooltip

--- apps/cowswap-frontend/src/modules/trade/utils/getReceiveAmountInfo.ts
+++ apps/cowswap-frontend/src/modules/trade/utils/getReceiveAmountInfo.ts
@@ -4,24 +4,50 @@ import { Currency, CurrencyAmount, Percent, Price } from '@uniswap/sdk-core'
 
 import { OrderTypeReceiveAmounts, ReceiveAmountInfo } from '../types'
 
+// eslint-disable-next-line complexity
 export function getOrderTypeReceiveAmounts(info: ReceiveAmountInfo): OrderTypeReceiveAmounts {
   const {
     isSell,
-    costs: { networkFee, bridgeFee },
+    costs: { networkFee, bridgeFee, protocolFee, partnerFee },
+    beforeNetworkCosts,
     afterPartnerFees,
     afterSlippage,
-    beforeNetworkCosts,
   } = info
 
+  const hasProtocolFee = (protocolFee?.bps ?? 0) > 0
+
+  const networkFeeAmount = isSell ? networkFee.amountInBuyCurrency : networkFee.amountInSellCurrency
+
+  if (!hasProtocolFee) {
+    return {
+      amountBeforeFees: isSell ? beforeNetworkCosts.buyAmount : beforeNetworkCosts.sellAmount,
+      amountAfterFees: isSell
+        ? bridgeFee
+          ? afterPartnerFees.buyAmount.subtract(bridgeFee.amountInIntermediateCurrency)
+          : afterPartnerFees.buyAmount
+        : afterPartnerFees.sellAmount,
+
+      amountAfterSlippage: isSell ? afterSlippage.buyAmount : afterSlippage.sellAmount,
+      networkFeeAmount,
+    }
+  }
+
+  const amountAfterFees = isSell
+    ? bridgeFee
+      ? afterPartnerFees.buyAmount.subtract(bridgeFee.amountInIntermediateCurrency)
+      : afterPartnerFees.buyAmount
+    : afterPartnerFees.sellAmount
+
+  const protocolFeeAmount = protocolFee?.amount ?? amountAfterFees.subtract(amountAfterFees) // 0
+  const partnerFeeAmount = partnerFee?.amount ?? amountAfterFees.subtract(amountAfterFees) // 0
+
+  const amountBeforeFees = amountAfterFees.add(protocolFeeAmount).add(partnerFeeAmount).add(networkFeeAmount)
+
   return {
-    amountBeforeFees: isSell ? beforeNetworkCosts.buyAmount : beforeNetworkCosts.sellAmount,
-    amountAfterFees: isSell
-      ? bridgeFee
-        ? afterPartnerFees.buyAmount.subtract(bridgeFee.amountInIntermediateCurrency)
-        : afterPartnerFees.buyAmount
-      : afterPartnerFees.sellAmount,
+    amountBeforeFees,
+    amountAfterFees,
     amountAfterSlippage: isSell ? afterSlippage.buyAmount : afterSlippage.sellAmount,
-    networkFeeAmount: isSell ? networkFee.amountInBuyCurrency : networkFee.amountInSellCurrency,
+    networkFeeAmount,
   }
 }
 
