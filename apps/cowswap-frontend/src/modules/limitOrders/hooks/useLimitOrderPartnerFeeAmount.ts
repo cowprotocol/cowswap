3896f0ad04e5ad1b49635ab772c147436d09457b
limitofzero <limitofzero@gmail.com>
2025-11-17 23:00:20 +0400

    fix: partner fee calculation for limit orders

--- apps/cowswap-frontend/src/modules/limitOrders/hooks/useLimitOrderPartnerFeeAmount.ts
+++ apps/cowswap-frontend/src/modules/limitOrders/hooks/useLimitOrderPartnerFeeAmount.ts
@@ -6,25 +6,16 @@ import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 import { useDerivedTradeState } from 'modules/trade'
 import { useVolumeFee } from 'modules/volumeFee'
 
-import { useLimitOrderProtocolFeeAmount } from './useLimitOrderProtocolFeeAmount'
-
 export function useLimitOrderPartnerFeeAmount(): CurrencyAmount<Currency> | null {
   const state = useDerivedTradeState()
   const { volumeBps } = useVolumeFee() || {}
   const outputCurrencyAmount = state?.outputCurrencyAmount
-  const protocolFeeAmount = useLimitOrderProtocolFeeAmount()
 
   return useMemo(() => {
     if (!outputCurrencyAmount) return null
 
-    // Partner fee is applied after a protocol fee (outputCurrencyAmount - protocolFee)
-    const amountAfterProtocolFee =
-      protocolFeeAmount && !protocolFeeAmount.equalTo(0)
-        ? outputCurrencyAmount.subtract(protocolFeeAmount)
-        : outputCurrencyAmount
-
     return !!volumeBps && volumeBps > 0
-      ? amountAfterProtocolFee.multiply(bpsToPercent(volumeBps))
+      ? outputCurrencyAmount.multiply(bpsToPercent(volumeBps))
       : CurrencyAmount.fromRawAmount(outputCurrencyAmount.currency, 0)
-  }, [outputCurrencyAmount, volumeBps, protocolFeeAmount])
+  }, [outputCurrencyAmount, volumeBps])
 }
