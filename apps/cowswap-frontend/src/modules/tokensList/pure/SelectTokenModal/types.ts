8c81595163195bc0f46a3b3e822f4bf374d57582
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-11-27 10:26:06 +0000

    refactor: add TODO for prop count reduction in TokenListContentProps

--- apps/cowswap-frontend/src/modules/tokensList/pure/SelectTokenModal/types.ts
+++ apps/cowswap-frontend/src/modules/tokensList/pure/SelectTokenModal/types.ts
@@ -10,6 +10,7 @@ import { PermitCompatibleTokens } from 'modules/permit'
 
 import { ChainsToSelectState, TokenSelectionHandler } from '../../types'
 
+// TODO: Refactor to reduce prop count
 export interface TokenListContentProps<T = TokenListCategory[] | null> {
   allTokens: TokenWithLogo[]
   favoriteTokens: TokenWithLogo[]
