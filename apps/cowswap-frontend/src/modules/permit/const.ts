9edfaee4e9a77c75b57f37ca386515bc56e1d5ae
Daniel Constantin <hello@danielconstantin.net>
2025-11-28 15:54:52 +0200

    feat: get permit info lists from aws

--- apps/cowswap-frontend/src/modules/permit/const.ts
+++ apps/cowswap-frontend/src/modules/permit/const.ts
@@ -3,4 +3,4 @@ import ms from 'ms.macro'
 export const PENDING_ORDER_PERMIT_CHECK_INTERVAL = ms`60s`
 
 export const PRE_GENERATED_PERMIT_URL =
-  'https://raw.githubusercontent.com/cowprotocol/token-lists/main/src/public/PermitInfo'
+  'https://files.cow.fi/token-lists/PermitInfo'
