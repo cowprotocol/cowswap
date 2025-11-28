53e810ce684cc388d6ac7e683b34d50e1f85fd7a
Daniel Constantin <hello@danielconstantin.net>
2025-11-28 15:50:17 +0200

    feat: use aws for token images

--- libs/common-const/src/cowprotocolTokenLogoUrl.ts
+++ libs/common-const/src/cowprotocolTokenLogoUrl.ts
@@ -1,6 +1,6 @@
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
-const tokenUrlRoot = 'https://raw.githubusercontent.com/cowprotocol/token-lists/main/src/public/images'
+const tokenUrlRoot = 'https://files.cow.fi/token-lists/images'
 
 export const cowprotocolTokenLogoUrl = (address: string, chainId: SupportedChainId): string =>
   `${tokenUrlRoot}/${chainId}/${address}/logo.png`
