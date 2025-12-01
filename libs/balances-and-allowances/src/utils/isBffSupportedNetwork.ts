4a3f1d54770c2883ab1e357c990499a4cb609253
limitofzero <limitofzero@gmail.com>
2025-12-01 20:19:31 +0400

    feat: update supported networks

--- libs/balances-and-allowances/src/utils/isBffSupportedNetwork.ts
+++ libs/balances-and-allowances/src/utils/isBffSupportedNetwork.ts
@@ -5,7 +5,7 @@ import { SupportedChainId } from '@cowprotocol/cow-sdk'
 import { bffUnsupportedChainsAtom } from '../state/isBffFailedAtom'
 
 // TODO: check before Plasma launch. Currently unsupported on 2025/10/20
-const UNSUPPORTED_BFF_NETWORKS = [SupportedChainId.PLASMA]
+const UNSUPPORTED_BFF_NETWORKS = [SupportedChainId.PLASMA, SupportedChainId.SEPOLIA]
 
 export function isBffSupportedNetwork(chainId: SupportedChainId): boolean {
   return !UNSUPPORTED_BFF_NETWORKS.includes(chainId)
