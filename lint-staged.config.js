2bc110987dfdd939b43055f57acbac74612b0a5b
Konstantin Barabanov <criitch@yandex.ru>
2025-12-02 11:58:49 +0300

    chore(prettier): run prettier on lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx}': files => {
-    const list = files.map(f => `"${f}"`).join(' ');
+  '*.{ts,tsx,js,jsx,mjs,mts,md,json,css,html,yaml,yml}': (files) => {
+    const prettierList = files.map((f) => `"${f}"`).join(' ')
+    const eslintList = files
+      .filter((f) => f.match(/\.(ts|tsx|js|jsx)$/))
+      .map((f) => `"${f}"`)
+      .join(' ')
     return [
-      `eslint --fix --max-warnings=0 ${list}`,
-      `git add ${list}`
-    ];
-  }
-};
+      `prettier --write ${prettierList}`,
+      eslintList.length > 0 && `eslint --fix --max-warnings=0 ${eslintList}`,
+    ].filter(Boolean)
+  },
+  // lint-staged runs `git add` automatically
+}
