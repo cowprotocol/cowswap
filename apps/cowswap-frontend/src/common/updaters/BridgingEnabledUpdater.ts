53a04faef47b26402f213facd853db42f45f3da5
Alexandr Kazachenko <shoom3301@gmail.com>
2025-12-04 16:00:59 +0500

    chore: remove bridging disabling for smart accounts

--- apps/cowswap-frontend/src/common/updaters/BridgingEnabledUpdater.ts
+++ apps/cowswap-frontend/src/common/updaters/BridgingEnabledUpdater.ts
@@ -1,23 +1,16 @@
 import { useEffect } from 'react'
 
 import { useSetIsBridgingEnabled } from '@cowprotocol/common-hooks'
-import { AccountType } from '@cowprotocol/types'
-import { useAccountType, useWalletInfo } from '@cowprotocol/wallet'
 
 import { useTradeTypeInfo } from 'modules/trade'
 
 import { Routes } from '../constants/routes'
 
 export function BridgingEnabledUpdater(): null {
-  const { account } = useWalletInfo()
   const tradeTypeInfo = useTradeTypeInfo()
-  const accountType = useAccountType()
   const setIsBridgingEnabled = useSetIsBridgingEnabled()
 
-  const isSwapRoute = tradeTypeInfo?.route === Routes.SWAP
-
-  const isWalletCompatible = Boolean(account ? accountType !== AccountType.SMART_CONTRACT : true)
-  const shouldEnableBridging = isWalletCompatible && isSwapRoute
+  const shouldEnableBridging = tradeTypeInfo?.route === Routes.SWAP
 
   useEffect(() => {
     setIsBridgingEnabled(shouldEnableBridging)
