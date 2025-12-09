5d60b03f9c3475726c4091332630ee0dfb83c05b
Elena <70885163+elena-zh@users.noreply.github.com>
2025-12-09 18:06:24 +0300

    remove more

--- apps/cowswap-frontend/src/modules/ethFlow/pure/EthFlowModalContent/EthFlowModalTopContent.tsx
+++ apps/cowswap-frontend/src/modules/ethFlow/pure/EthFlowModalContent/EthFlowModalTopContent.tsx
@@ -1,6 +1,5 @@
 import { Media, UI } from '@cowprotocol/ui'
 
-import { Trans } from '@lingui/react/macro'
 import { darken, transparentize } from 'color2k'
 import styled from 'styled-components/macro'
 
@@ -23,19 +22,3 @@ const ModalMessage = styled.div`
     margin: 0 0 8px;
   }
 `
-
-const LowBalanceMessage = styled(ModalMessage)`
-  margin: 0 0 10px;
-  background: ${({ theme }) => (theme.darkMode ? transparentize(theme.alert, 0.9) : transparentize(theme.alert, 0.85))};
-  color: ${({ theme }) => (theme.darkMode ? `var(${UI.COLOR_ALERT})` : darken(theme.alert, 0.2))};
-  padding: 16px;
-  border-radius: 16px;
-  width: 100%;
-  box-sizing: border-box;
-  line-height: 1.5;
-
-  > strong {
-    display: contents;
-  }
-`
-
