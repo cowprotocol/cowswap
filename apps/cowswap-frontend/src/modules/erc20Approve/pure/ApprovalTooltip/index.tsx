8a4a6d938f1f75977a526fee29d29c3c9e30ddb8
dinesh11515 <dineshaitham2@gmail.com>
2025-12-10 17:51:32 +0530

    feat(ApprovalTooltip): enhance tooltip to include legacy approval message

--- apps/cowswap-frontend/src/modules/erc20Approve/pure/ApprovalTooltip/index.tsx
+++ apps/cowswap-frontend/src/modules/erc20Approve/pure/ApprovalTooltip/index.tsx
@@ -5,15 +5,29 @@ import { Currency } from '@uniswap/sdk-core'
 
 import { Trans } from '@lingui/react/macro'
 
+import { useIsPartialApproveSelectedByUser } from '../../state'
+
 interface ApprovalTooltipProps {
   currency: Currency
+  isLegacyApproval?: boolean
 }
 
-export function ApprovalTooltip({ currency }: ApprovalTooltipProps): ReactNode {
-  return (
+export function ApprovalTooltip({ currency, isLegacyApproval = false }: ApprovalTooltipProps): ReactNode {
+  const isPartialApproveSelectedByUser = useIsPartialApproveSelectedByUser()
+
+  const baseText = (
     <Trans>
-      You must give the CoW Protocol smart contracts permission to use your <TokenSymbol token={currency} />. If you approve the default amount, you will only have to
-      do this once per token.
+      You must give the CoW Protocol smart contracts permission to use your <TokenSymbol token={currency} />.
     </Trans>
   )
+
+  if (isPartialApproveSelectedByUser || isLegacyApproval) {
+    return (
+      <>
+        {baseText} <Trans>If you approve an unlimited amount, you will only have to do this once per token.</Trans>
+      </>
+    )
+  }
+
+  return baseText
 }
