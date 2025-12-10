4807075d0c2337d0fa27bf1b641764be0e63fe8f
dinesh11515 <dineshaitham2@gmail.com>
2025-12-10 10:28:31 +0530

    fix(ApprovalTooltip): simplify tooltip to show same msg

--- apps/cowswap-frontend/src/modules/erc20Approve/pure/ApprovalTooltip/index.tsx
+++ apps/cowswap-frontend/src/modules/erc20Approve/pure/ApprovalTooltip/index.tsx
@@ -5,28 +5,15 @@ import { Currency } from '@uniswap/sdk-core'
 
 import { Trans } from '@lingui/react/macro'
 
-import { useIsPartialApproveSelectedByUser } from '../../state'
-
 interface ApprovalTooltipProps {
   currency: Currency
 }
 
 export function ApprovalTooltip({ currency }: ApprovalTooltipProps): ReactNode {
-  const isPartialApproveSelectedByUser = useIsPartialApproveSelectedByUser()
-
-  const baseText = (
+  return (
     <Trans>
-      You must give the CoW Protocol smart contracts permission to use your <TokenSymbol token={currency} />.
+      You must give the CoW Protocol smart contracts permission to use your <TokenSymbol token={currency} />. If you approve the default amount, you will only have to
+      do this once per token.
     </Trans>
   )
-
-  if (isPartialApproveSelectedByUser) {
-    return (
-      <>
-        {baseText} <Trans>If you approve the full amount, you will only have to do this once per token.</Trans>
-      </>
-    )
-  }
-
-  return baseText
 }
