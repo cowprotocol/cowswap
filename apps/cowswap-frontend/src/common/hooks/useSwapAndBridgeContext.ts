623e0e5b09c33132debdfd06493c357c0af8aa80
Alexandr Kazachenko <shoom3301@gmail.com>
2025-12-15 18:00:58 +0500

    fix(bridge): fix displayed bridge quote amounts (#6670)
    * fix(bridge): fix displayed bridge quote amounts
    
    * chore: fix build
    
    * chore: fix build
    
    * chore: fix minToReceiveAmount
    
    * chore: fix FulfilledOrderInfo tokens
    
    * fix: define bridgeMinReceiveAmount relatively to quote response
    
    * fix: use quote based min amount only for QuoteBridgeContext
    
    * fix: get rid of estimateWalletMinReceived
    
    * fix: fix FulfilledOrderInfo

--- apps/cowswap-frontend/src/common/hooks/useSwapAndBridgeContext.ts
+++ apps/cowswap-frontend/src/common/hooks/useSwapAndBridgeContext.ts
@@ -29,8 +29,6 @@ import { useBridgeOrderOutputToken } from './useBridgeOrderOutputToken'
 import { useSwapAndBridgeOverview } from './useSwapAndBridgeOverview'
 import { useSwapResultsContext } from './useSwapResultsContext'
 
-import { calculateTargetAmountsBeforeBridging } from '../utils/calculateTargetAmountsBeforeBridging'
-
 const bridgeStatusMap: Record<BridgeStatus, SwapAndBridgeStatus> = {
   [BridgeStatus.IN_PROGRESS]: SwapAndBridgeStatus.PENDING,
   [BridgeStatus.EXECUTED]: SwapAndBridgeStatus.DONE,
@@ -126,16 +124,18 @@ export function useSwapAndBridgeContext(
 
     if (crossChainOrder && bridgeReceiveAmount) {
       return {
-        sellAmount: CurrencyAmount.fromRawAmount(
-          intermediateToken,
-          crossChainOrder.bridgingParams.inputAmount.toString(),
-        ),
+        sellAmount:
+          receivedAmount ??
+          CurrencyAmount.fromRawAmount(intermediateToken, crossChainOrder.bridgingParams.inputAmount.toString()),
         buyAmount: bridgeReceiveAmount,
       }
     }
 
     if (bridgeQuoteAmounts && !bridgeOutputAmount) {
-      return calculateTargetAmountsBeforeBridging(bridgeQuoteAmounts, receivedAmount)
+      return {
+        sellAmount: receivedAmount ?? bridgeQuoteAmounts.swapMinReceiveAmount,
+        buyAmount: bridgeQuoteAmounts.bridgeMinReceiveAmount,
+      }
     }
 
     return undefined
@@ -201,6 +201,8 @@ export function useSwapAndBridgeContext(
       bridgeMinReceiveAmount,
       bridgeMinDepositAmount: null,
       bridgeMinDepositAmountUsd: null,
+      expectedToReceive: null,
+      expectedToReceiveUsd: null,
     }
 
     return {
