b9c8cb72b11ced6c04c8a2d6c90a553d575785d6
Daniel Constantin <hello@danielconstantin.net>
2025-12-17 17:28:37 +0200

    chore: warn about allTokenListsInfoAtom changes needing a reset in tokens:lastUpdateTimeAtom

--- libs/tokens/src/state/tokenLists/tokenListsStateAtom.ts
+++ libs/tokens/src/state/tokenLists/tokenListsStateAtom.ts
@@ -65,8 +65,12 @@ export const allListsSourcesAtom = atom((get) => {
 // Migrating from localStorage to indexedDB
 localStorage.removeItem('allTokenListsInfoAtom:v5')
 
-// Lists states
-// Note: v6 -> v7 migration is handled by migrateTokenListsFromGithubToCdn()
+/**
+ * Lists states (user preferences)
+ * Note: v6 -> v7 migration is handled by migrateTokenListsFromGithubToCdn()
+ *
+ * @warning any migration or changes to this atom should be accompanied by a reset in tokens:lastUpdateTimeAtom:v6
+ */
 export const listsStatesByChainAtom = atomWithIdbStorage<TokenListsByChainState>(
   'allTokenListsInfoAtom:v7',
   mapSupportedNetworks({}),
