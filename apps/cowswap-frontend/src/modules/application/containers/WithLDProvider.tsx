fa02aa4cf7f951464049cb4f3a92544de457e760
Leandro <alfetopito@users.noreply.github.com>
2025-12-19 09:14:19 +0000

    fix: add user context on launch darkly initialization (#6726)

--- apps/cowswap-frontend/src/modules/application/containers/WithLDProvider.tsx
+++ apps/cowswap-frontend/src/modules/application/containers/WithLDProvider.tsx
@@ -1,17 +1,20 @@
-import { JSX, PropsWithChildren } from 'react'
+import { JSX, PropsWithChildren, ReactNode } from 'react'
 
 import { LAUNCH_DARKLY_CLIENT_KEY } from '@cowprotocol/common-const'
 
 import { withLDProvider } from 'launchdarkly-react-client-sdk'
 
-// TODO: Add proper return type annotation
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function InnerWithLDProvider({ children }: PropsWithChildren) {
+function InnerWithLDProvider({ children }: PropsWithChildren): ReactNode {
   return children
 }
 
 export const WithLDProvider = withLDProvider<PropsWithChildren & JSX.IntrinsicAttributes>({
   clientSideID: LAUNCH_DARKLY_CLIENT_KEY,
+  context: {
+    kind: 'user',
+    key: 'cowswap',
+    name: 'cowswap',
+  },
   options: {
     bootstrap: 'localStorage',
   },
