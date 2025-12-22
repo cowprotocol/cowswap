309229dfed75d800735a646e68fabdb81db4f109
Konstantin Barabanov <criitch@yandex.ru>
2025-12-22 17:33:32 +0300

    test: enable lists tests

--- apps/cowswap-frontend-e2e/src/e2e/lists.test.ts
+++ apps/cowswap-frontend-e2e/src/e2e/lists.test.ts
@@ -1,10 +1,12 @@
+import { unlockCrossChainSwap } from '../support/swap'
+
 describe('Lists', () => {
   beforeEach(() => {
     cy.visit('/#/11155111/swap/')
   })
 
-  // TODO: disable this test because it's not working - needs to be fixed
-  it.skip('change list', () => {
+  it('change list', () => {
+    unlockCrossChainSwap()
     cy.get('#output-currency-input .open-currency-select-button', { timeout: 20_000 }).should('be.enabled').click()
     cy.get('#list-token-manage-button').click()
     cy.get('#tokens-lists-table > div').should('have.length.greaterThan', 0)
