45ab62aa86e74f6c0393930f573b20f5be5b0304
Konstantin Barabanov <criitch@yandex.ru>
2025-12-23 13:53:51 +0300

    chore: tests - allow @cowprotocol imports

--- apps/cowswap-frontend-e2e/cypress.config.ts
+++ apps/cowswap-frontend-e2e/cypress.config.ts
@@ -1,10 +1,29 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
 import { defineConfig } from 'cypress'
+import macrosPlugin from 'vite-plugin-babel-macros'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__dirname, {
       bundler: 'vite',
+      viteConfigOverrides: {
+        plugins: [
+          nodePolyfills({
+            globals: {
+              Buffer: true,
+              global: true,
+              process: true,
+            },
+            protocolImports: true,
+          }),
+          viteTsConfigPaths({
+            root: '../../',
+          }),
+          macrosPlugin(),
+        ],
+      },
     }),
     specPattern: ['src/**/*.test.{js,jsx,ts,tsx}'],
     baseUrl: 'http://localhost:3000',
