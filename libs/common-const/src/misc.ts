9f5f85be18b07f8ef0078d20c1cf29933fa4d80f
Alexandr Kazachenko <shoom3301@gmail.com>
2025-12-25 17:16:22 +0500

    feat(trade): adjust price impact warnings for bridging (#6754)
    * chore: fix type
    
    * refactor: move price impact consts to cowswap
    
    * chore: fix useConfirmPriceImpactWithoutFee return type
    
    * refactor: move warningSeverity to PriceImpactIndicator
    
    * chore: cosmos PriceImpactIndicator
    
    * refactor: simplify PriceImpactIndicator
    
    * refactor: extract FiatValueProps
    
    * refactor: unwire useTradeUsdAmounts from modules/trade
    
    * chore: pass isBridging to PriceImpactIndicator
    
    * feat(trade): adjust price impact warnings for bridging
    
    * chore: update tooltip text
    
    * chore: remove excessive tests

--- libs/common-const/src/misc.ts
+++ libs/common-const/src/misc.ts
@@ -1,24 +1,8 @@
-import { Fraction, Percent } from '@uniswap/sdk-core'
-
-import JSBI from 'jsbi'
+import { Percent } from '@uniswap/sdk-core'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 // 30 minutes, denominated in seconds
 export const DEFAULT_DEADLINE_FROM_NOW = 60 * 30
 
-// one basis JSBI.BigInt
-const BPS_BASE = JSBI.BigInt(10000)
-
-// used for warning states
-export const ALLOWED_PRICE_IMPACT_LOW: Percent = new Percent(JSBI.BigInt(100), BPS_BASE) // 1%
-export const ALLOWED_PRICE_IMPACT_MEDIUM: Percent = new Percent(JSBI.BigInt(300), BPS_BASE) // 3%
-export const ALLOWED_PRICE_IMPACT_HIGH: Percent = new Percent(JSBI.BigInt(500), BPS_BASE) // 5%
-// if the price slippage exceeds this number, force the user to type 'confirm' to execute
-export const PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN: Percent = new Percent(JSBI.BigInt(1000), BPS_BASE) // 10%
-// for non expert mode disable swaps above this
-export const BLOCKED_PRICE_IMPACT_NON_EXPERT: Percent = new Percent(JSBI.BigInt(1500), BPS_BASE) // 15%
-
 export const ONE_HUNDRED_PERCENT = new Percent('1')
-
-export const ONE_FRACTION = new Fraction(1, 1)
