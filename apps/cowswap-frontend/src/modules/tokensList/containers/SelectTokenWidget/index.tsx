963d812fa59b2eef0b3296766a2b030cf7b08e5b
limitofzero <limitofzero@gmail.com>
2026-01-06 02:11:55 +0400

    refactor: migrate to new token selector

--- apps/cowswap-frontend/src/modules/tokensList/containers/SelectTokenWidget/index.tsx
+++ apps/cowswap-frontend/src/modules/tokensList/containers/SelectTokenWidget/index.tsx
@@ -1,10 +1,4 @@
-import { MouseEvent, ReactNode, useEffect } from 'react'
-
-import { useMediaQuery } from '@cowprotocol/common-hooks'
-import { addBodyClass, removeBodyClass } from '@cowprotocol/common-utils'
-import { Media } from '@cowprotocol/ui'
-
-import { createPortal } from 'react-dom'
+import { ReactNode } from 'react'
 
 import {
   Header,
@@ -15,82 +9,24 @@ import {
   BlockingView,
   useHasBlockingView,
 } from './components'
-import { WidgetCallbacksProvider, WidgetConfigProvider } from './context'
-import { useSelectTokenWidgetController, type SelectTokenWidgetProps } from './controller'
-import { useWidgetSetup } from './hooks'
-import { InnerWrapper, ModalContainer, WidgetCard, WidgetOverlay, Wrapper } from './styled'
+import { SelectTokenWidgetV2 } from './v2'
 
-import { useCloseTokenSelectWidget } from '../../hooks/useCloseTokenSelectWidget'
 import * as styledEl from '../../pure/SelectTokenModal/styled'
 
+import type { SelectTokenWidgetProps } from './controller'
+
+/**
+ * SelectTokenWidget - Token selector modal
+ *
+ * This is the main entry point. It uses V2 internally with slot-based composition.
+ * The compound component API (SelectTokenWidget.Header, etc.) is preserved for backwards compatibility.
+ */
 export function SelectTokenWidget(props: SelectTokenWidgetProps): ReactNode {
-  const { shouldRender, hasChainPanel, viewProps } = useSelectTokenWidgetController(props)
-  const isCompactLayout = useMediaQuery(Media.upToMedium(false))
-  const isChainPanelVisible = hasChainPanel && !isCompactLayout
-  const closeTokenSelectWidget = useCloseTokenSelectWidget()
-
-  // Setup atoms and get provider values
-  const { callbacks, config } = useWidgetSetup({
-    viewProps,
-    isChainPanelVisible,
-    isCompactLayout,
-    isChainPanelEnabled: hasChainPanel,
-  })
-
-  // Cleanup: reset widget state on unmount
-  useEffect(() => {
-    return () => {
-      closeTokenSelectWidget({ overrideForceLock: true })
-    }
-  }, [closeTokenSelectWidget])
-
-  useEffect(() => {
-    if (!shouldRender) {
-      removeBodyClass('noScroll')
-      return undefined
-    }
-
-    addBodyClass('noScroll')
-    return () => removeBodyClass('noScroll')
-  }, [shouldRender])
-
-  if (!shouldRender) {
-    return null
-  }
-
-  const handleOverlayClick = (event: MouseEvent<HTMLDivElement>): void => {
-    if (event.target !== event.currentTarget) {
-      return
-    }
-
-    viewProps.onDismiss()
-  }
-
-  const widgetContent = (
-    <WidgetCallbacksProvider value={callbacks}>
-      <WidgetConfigProvider value={config}>
-        <Wrapper>
-          <InnerWrapper $hasSidebar={isChainPanelVisible} $isMobileOverlay={isCompactLayout}>
-            <SelectTokenWidgetContent />
-          </InnerWrapper>
-        </Wrapper>
-      </WidgetConfigProvider>
-    </WidgetCallbacksProvider>
+  return (
+    <SelectTokenWidgetV2 {...props}>
+      <SelectTokenWidgetContent />
+    </SelectTokenWidgetV2>
   )
-
-  const overlay = (
-    <WidgetOverlay onClick={handleOverlayClick}>
-      <WidgetCard $isCompactLayout={isCompactLayout} $hasChainPanel={hasChainPanel}>
-        {widgetContent}
-      </WidgetCard>
-    </WidgetOverlay>
-  )
-
-  if (typeof document === 'undefined') {
-    return overlay
-  }
-
-  return createPortal(overlay, document.body)
 }
 
 function SelectTokenWidgetContent(): ReactNode {
@@ -98,28 +34,22 @@ function SelectTokenWidgetContent(): ReactNode {
 
   // Blocking views take over the entire modal
   if (hasBlockingView) {
-    return (
-      <ModalContainer>
-        <BlockingView />
-      </ModalContainer>
-    )
+    return <BlockingView />
   }
 
   // Normal token selection view
   return (
     <>
-      <ModalContainer>
-        <SelectTokenWidget.Modal>
-          <SelectTokenWidget.Header />
-          <SelectTokenWidget.SearchInput />
-          <SelectTokenWidget.ChainSelector />
-          <styledEl.Body>
-            <styledEl.TokenColumn>
-              <SelectTokenWidget.TokenList />
-            </styledEl.TokenColumn>
-          </styledEl.Body>
-        </SelectTokenWidget.Modal>
-      </ModalContainer>
+      <SelectTokenWidget.Modal>
+        <SelectTokenWidget.Header />
+        <SelectTokenWidget.SearchInput />
+        <SelectTokenWidget.ChainSelector />
+        <styledEl.Body>
+          <styledEl.TokenColumn>
+            <SelectTokenWidget.TokenList />
+          </styledEl.TokenColumn>
+        </styledEl.Body>
+      </SelectTokenWidget.Modal>
       <SelectTokenWidget.DesktopChainPanel />
     </>
   )
