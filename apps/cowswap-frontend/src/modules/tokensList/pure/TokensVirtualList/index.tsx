7ef562eb7728ed93d45b414c37fd60fd2ef37170
limitofzero <limitofzero@gmail.com>
2026-01-13 22:06:22 +0400

    refactor: token virtual list

--- apps/cowswap-frontend/src/modules/tokensList/pure/TokensVirtualList/index.tsx
+++ apps/cowswap-frontend/src/modules/tokensList/pure/TokensVirtualList/index.tsx
@@ -2,32 +2,24 @@ import { ReactNode, useCallback, useMemo } from 'react'
 
 import { TokenWithLogo } from '@cowprotocol/common-const'
 import { useFeatureFlags } from '@cowprotocol/common-hooks'
-import { getIsNativeToken } from '@cowprotocol/common-utils'
 
-import { t } from '@lingui/core/macro'
 import { VirtualItem } from '@tanstack/react-virtual'
 
 import { CoWAmmBanner } from 'common/containers/CoWAmmBanner'
 import { VirtualList } from 'common/pure/VirtualList'
 
+import { buildVirtualRows, sortTokensByBalance } from './tokensVirtualListUtils'
+import { TokensVirtualRowRenderer } from './TokensVirtualRowRenderer'
+import { TokensVirtualRow } from './types'
+
 import { useTokenListData } from '../../hooks/useTokenListData'
-import { SelectTokenContext } from '../../types'
-import { tokensListSorter } from '../../utils/tokensListSorter'
-import { FavoriteTokensList } from '../FavoriteTokensList'
-import * as modalStyled from '../SelectTokenModal/styled'
-import { TokenListItemContainer } from '../TokenListItemContainer'
 
 export interface TokensVirtualListProps {
-  tokensToDisplay: TokenWithLogo[] // Pre-filtered by parent
+  tokensToDisplay: TokenWithLogo[]
   favoriteTokens?: TokenWithLogo[]
   recentTokens?: TokenWithLogo[]
   onClearRecentTokens: () => void
 }
-
-type TokensVirtualRow =
-  | { type: 'favorite-section'; tokens: TokenWithLogo[]; hideTooltip?: boolean }
-  | { type: 'title'; label: string; actionLabel?: string; onAction?: () => void }
-  | { type: 'token'; token: TokenWithLogo }
 
 export function TokensVirtualList({
   tokensToDisplay,
@@ -36,63 +28,22 @@ export function TokensVirtualList({
   onClearRecentTokens,
 }: TokensVirtualListProps): ReactNode {
   const { selectTokenContext, hideFavoriteTokensTooltip, selectedTargetChainId } = useTokenListData()
-
   const { values: balances } = selectTokenContext.balancesState
-
   const { isYieldEnabled } = useFeatureFlags()
 
-  const sortedTokens = useMemo(() => {
-    if (!balances) {
-      return tokensToDisplay
-    }
+  const sortedTokens = useMemo(() => sortTokensByBalance(tokensToDisplay, balances), [tokensToDisplay, balances])
 
-    const prioritized: TokenWithLogo[] = []
-    const remainder: TokenWithLogo[] = []
-
-    for (const token of tokensToDisplay) {
-      const hasBalance = Boolean(balances[token.address.toLowerCase()])
-      if (hasBalance || getIsNativeToken(token)) {
-        prioritized.push(token)
-      } else {
-        remainder.push(token)
-      }
-    }
-
-    const sortedPrioritized = prioritized.length > 1 ? [...prioritized].sort(tokensListSorter(balances)) : prioritized
-
-    return [...sortedPrioritized, ...remainder]
-  }, [tokensToDisplay, balances])
-
-  const rows = useMemo<TokensVirtualRow[]>(() => {
-    const tokenRows = sortedTokens.map<TokensVirtualRow>((token) => ({ type: 'token', token }))
-    const composedRows: TokensVirtualRow[] = []
-
-    if (favoriteTokens?.length) {
-      composedRows.push({
-        type: 'favorite-section',
-        tokens: favoriteTokens,
-        hideTooltip: hideFavoriteTokensTooltip,
-      })
-    }
-
-    if (recentTokens?.length) {
-      composedRows.push({
-        type: 'title',
-        label: t`Recent`,
-        actionLabel: t`Clear`,
-        onAction: onClearRecentTokens,
-      })
-      recentTokens.forEach((token) => composedRows.push({ type: 'token', token }))
-    }
-
-    if (favoriteTokens?.length || recentTokens?.length) {
-      composedRows.push({ type: 'title', label: t`All tokens` })
-    }
-
-    return [...composedRows, ...tokenRows]
-  }, [favoriteTokens, hideFavoriteTokensTooltip, onClearRecentTokens, recentTokens, sortedTokens])
-
-  const virtualListKey = selectedTargetChainId ?? 'tokens-list'
+  const rows = useMemo<TokensVirtualRow[]>(
+    () =>
+      buildVirtualRows({
+        sortedTokens,
+        favoriteTokens,
+        recentTokens,
+        hideFavoriteTokensTooltip,
+        onClearRecentTokens,
+      }),
+    [favoriteTokens, hideFavoriteTokensTooltip, onClearRecentTokens, recentTokens, sortedTokens],
+  )
 
   const getItemView = useCallback(
     (virtualRows: TokensVirtualRow[], virtualItem: VirtualItem) => (
@@ -101,9 +52,7 @@ export function TokensVirtualList({
     [selectTokenContext],
   )
 
-  // Check if LP token lists are displayed (no CoWAmmBanner in that case)
-  // This was previously read from atom, now we don't have it - default to not showing banner for LP
-  const showCoWAmmBanner = isYieldEnabled
+  const virtualListKey = selectedTargetChainId ?? 'tokens-list'
 
   return (
     <VirtualList
@@ -113,34 +62,7 @@ export function TokensVirtualList({
       getItemView={getItemView}
       scrollResetKey={selectedTargetChainId}
     >
-      {showCoWAmmBanner ? <CoWAmmBanner isTokenSelectorView /> : null}
+      {isYieldEnabled ? <CoWAmmBanner isTokenSelectorView /> : null}
     </VirtualList>
   )
-}
-
-interface TokensVirtualRowRendererProps {
-  row: TokensVirtualRow
-  selectTokenContext: SelectTokenContext
-}
-
-function TokensVirtualRowRenderer({ row, selectTokenContext }: TokensVirtualRowRendererProps): ReactNode {
-  switch (row.type) {
-    case 'favorite-section':
-      return (
-        <FavoriteTokensList tokens={row.tokens} selectTokenContext={selectTokenContext} hideTooltip={row.hideTooltip} />
-      )
-    case 'title':
-      return (
-        <modalStyled.ListTitle>
-          <span>{row.label}</span>
-          {row.actionLabel && row.onAction ? (
-            <modalStyled.ListTitleActionButton type="button" onClick={row.onAction}>
-              {row.actionLabel}
-            </modalStyled.ListTitleActionButton>
-          ) : null}
-        </modalStyled.ListTitle>
-      )
-    default:
-      return <TokenListItemContainer token={row.token} context={selectTokenContext} />
-  }
 }
---
+++ apps/cowswap-frontend/src/modules/tokensList/pure/TokensVirtualList/tokensVirtualListUtils.ts
@@ -0,0 +1,71 @@
+import { TokenWithLogo } from '@cowprotocol/common-const'
+import { getIsNativeToken } from '@cowprotocol/common-utils'
+
+import { t } from '@lingui/core/macro'
+
+import { TokensVirtualRow } from './types'
+
+import { tokensListSorter } from '../../utils/tokensListSorter'
+
+type BalancesMap = Record<string, unknown> | undefined
+
+export function sortTokensByBalance(tokens: TokenWithLogo[], balances: BalancesMap): TokenWithLogo[] {
+  if (!balances) {
+    return tokens
+  }
+
+  const prioritized: TokenWithLogo[] = []
+  const remainder: TokenWithLogo[] = []
+
+  for (const token of tokens) {
+    const hasBalance = Boolean(balances[token.address.toLowerCase()])
+    if (hasBalance || getIsNativeToken(token)) {
+      prioritized.push(token)
+    } else {
+      remainder.push(token)
+    }
+  }
+
+  const sortedPrioritized = prioritized.length > 1 ? [...prioritized].sort(tokensListSorter(balances)) : prioritized
+
+  return [...sortedPrioritized, ...remainder]
+}
+
+export interface BuildVirtualRowsParams {
+  sortedTokens: TokenWithLogo[]
+  favoriteTokens: TokenWithLogo[] | undefined
+  recentTokens: TokenWithLogo[] | undefined
+  hideFavoriteTokensTooltip: boolean
+  onClearRecentTokens: () => void
+}
+
+export function buildVirtualRows(params: BuildVirtualRowsParams): TokensVirtualRow[] {
+  const { sortedTokens, favoriteTokens, recentTokens, hideFavoriteTokensTooltip, onClearRecentTokens } = params
+
+  const tokenRows = sortedTokens.map<TokensVirtualRow>((token) => ({ type: 'token', token }))
+  const composedRows: TokensVirtualRow[] = []
+
+  if (favoriteTokens?.length) {
+    composedRows.push({
+      type: 'favorite-section',
+      tokens: favoriteTokens,
+      hideTooltip: hideFavoriteTokensTooltip,
+    })
+  }
+
+  if (recentTokens?.length) {
+    composedRows.push({
+      type: 'title',
+      label: t`Recent`,
+      actionLabel: t`Clear`,
+      onAction: onClearRecentTokens,
+    })
+    recentTokens.forEach((token) => composedRows.push({ type: 'token', token }))
+  }
+
+  if (favoriteTokens?.length || recentTokens?.length) {
+    composedRows.push({ type: 'title', label: t`All tokens` })
+  }
+
+  return [...composedRows, ...tokenRows]
+}
