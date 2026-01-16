2d38b2447119805860f4c82c3d5f8e3a398946cc
limitofzero <limitofzero@gmail.com>
2026-01-16 15:26:02 +0400

    fix: helper for keys

--- libs/common-utils/src/tokens.ts
+++ libs/common-utils/src/tokens.ts
@@ -15,10 +15,12 @@ export function isNativeAddress(tokenAddress: string, chainId: ChainId): boolean
   return native && doesTokenMatchSymbolOrAddress(native, tokenAddressLower)
 }
 
+// further it will be a good point to have EvmAddressKey type and Solana/Bitcoin etc
+// so this type it's kind of part of unition
 export type AddressKey = `0x${string}`
 
 export function getTokenAddressKey(address: string): AddressKey {
-  return `0x${address.toLowerCase()}` as AddressKey
+  return `${address.toLowerCase()}` as AddressKey
 }
 
 export interface TokenIdentifier {
@@ -26,6 +28,7 @@ export interface TokenIdentifier {
   chainId: number
 }
 
+// the same as for AddressKey - will be more complex when integrate solana/bitcoin
 export type TokenId = `${number}:${AddressKey}`
 
 export function getTokenId(token: TokenIdentifier): TokenId {
