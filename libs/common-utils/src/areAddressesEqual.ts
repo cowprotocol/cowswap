d867a943b4f6a293e69792f417e8b92814556a75
limitofzero <limitofzero@gmail.com>
2026-01-16 15:52:46 +0400

    fix: address comparison logic + tests

--- libs/common-utils/src/areAddressesEqual.ts
+++ libs/common-utils/src/areAddressesEqual.ts
@@ -3,7 +3,7 @@ import { Nullish } from '@cowprotocol/types'
 import { getTokenAddressKey } from './tokens'
 
 export function areAddressesEqual(a: Nullish<string>, b: Nullish<string>): boolean {
-  if (!a && !b) return true
+  if (!a && !b) return false
   if (!a || !b) return false
 
   return getTokenAddressKey(a) === getTokenAddressKey(b)
