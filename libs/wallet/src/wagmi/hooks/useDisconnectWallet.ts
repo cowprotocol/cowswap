0427ba4323be4a57f417f8fe7158d659212e339a
Bernardo Valverde <bsvalverde@gmail.com>
2026-01-21 16:29:30 -0300

    refactor: uses wagmi hooks

--- libs/wallet/src/wagmi/hooks/useDisconnectWallet.ts
+++ libs/wallet/src/wagmi/hooks/useDisconnectWallet.ts
@@ -2,11 +2,11 @@ import { useCallback } from 'react'
 
 import { Command } from '@cowprotocol/types'
 
-import { useDisconnect } from '@reown/appkit/react'
+import { useDisconnect } from 'wagmi'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function useDisconnectWallet(onDisconnect?: Command) {
-  const { disconnect } = useDisconnect()
+  const { mutateAsync: disconnect } = useDisconnect()
 
   return useCallback(async () => {
     await disconnect()
