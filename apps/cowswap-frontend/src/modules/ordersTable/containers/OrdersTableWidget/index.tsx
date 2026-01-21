05b6787195affcdf9bc1771d96231d844fbff7a2
Dani Gámez Franco <gmzcodes@outlook.com>
2026-01-21 11:05:16 +0100

    fix: rename EXECUTED to FILLED

--- apps/cowswap-frontend/src/modules/ordersTable/containers/OrdersTableWidget/index.tsx
+++ apps/cowswap-frontend/src/modules/ordersTable/containers/OrdersTableWidget/index.tsx
@@ -36,7 +36,7 @@ export function OrdersTableWidget(ordersTableParams: OrdersTableParams): ReactNo
   const navigate = useNavigate()
 
   const [searchTerm, setSearchTerm] = useState('')
-  const [historyStatusFilter, setHistoryStatusFilter] = useState<HistoryStatusFilter>(HistoryStatusFilter.EXECUTED)
+  const [historyStatusFilter, setHistoryStatusFilter] = useState<HistoryStatusFilter>(HistoryStatusFilter.FILLED)
 
   const resetPagination = (): void => {
     if (!currentPageNumber || currentPageNumber === 1 || !filteredOrders) return
@@ -65,7 +65,7 @@ export function OrdersTableWidget(ordersTableParams: OrdersTableParams): ReactNo
 
   useEffect(() => {
     // When moving away from the history tab, reset the showOnlyFilled filter, as the UI for it won't be shown in other tabs:
-    if (currentTabId !== OrderTabId.history) setHistoryStatusFilter(HistoryStatusFilter.EXECUTED)
+    if (currentTabId !== OrderTabId.history) setHistoryStatusFilter(HistoryStatusFilter.FILLED)
   }, [currentTabId])
 
   const pendingOrders = useMemo(() => {
@@ -106,7 +106,7 @@ export function OrdersTableWidget(ordersTableParams: OrdersTableParams): ReactNo
             {currentTabId === OrderTabId.history && (
               <SelectContainer>
                 <Select name="historyStatusFilter" value={historyStatusFilter} onChange={handleSelectChange}>
-                  <option value="executed">{i18n._('Executed orders')}</option>
+                  <option value="filled">{i18n._('Filled orders')}</option>
                   <option value="cancelled">{i18n._('Cancelled orders')}</option>
                   <option value="expired">{i18n._('Expired orders')}</option>
                   <option value="all">{i18n._('All orders')}</option>
