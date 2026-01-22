d5582509973346929d4bc22cf47020c288ea2219
Bernardo Valverde <bsvalverde@gmail.com>
2026-01-22 17:43:39 -0300

    fix: updates flag to use local storage

--- libs/wallet/src/versionResolver.ts
+++ libs/wallet/src/versionResolver.ts
@@ -1,20 +1,13 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { LAUNCH_DARKLY_VIEM_MIGRATION } from '@cowprotocol/common-const'
+import { Connector as LegacyConnector } from '@web3-react/types'
 
-import * as useIsSmartContractWalletFile from './wagmi/hooks/useIsSmartContractWallet'
-import * as useIsWalletConnectFile from './wagmi/hooks/useIsWalletConnect'
-import * as useWalletMetadataFile from './wagmi/hooks/useWalletMetadata'
-import * as legacyUseIsSmartContractWalletFile from './web3-react/hooks/useIsSmartContractWallet'
-import * as legacyUseIsWalletConnectFile from './web3-react/hooks/useIsWalletConnect'
-import * as legacyUseWalletMetadataFile from './web3-react/hooks/useWalletMetadata'
-
-let useIsSmartContractWalletImplementation = legacyUseIsSmartContractWalletFile
-let useIsWalletConnectImplementation = legacyUseIsWalletConnectFile
-let useWalletMetaDataImplementation = legacyUseWalletMetadataFile
-if (LAUNCH_DARKLY_VIEM_MIGRATION) {
-  useIsSmartContractWalletImplementation = useIsSmartContractWalletFile
-  useIsWalletConnectImplementation = useIsWalletConnectFile
-  useWalletMetaDataImplementation = useWalletMetadataFile
-}
+import * as newUseIsSmartContract from './wagmi/hooks/useIsSmartContractWallet'
+import * as newUseIsWalletConnect from './wagmi/hooks/useIsWalletConnect'
+import * as newUseWalletMetadata from './wagmi/hooks/useWalletMetadata'
+import * as legacyUseIsSmartContractWallet from './web3-react/hooks/useIsSmartContractWallet'
+import * as legacyUseIsWalletConnect from './web3-react/hooks/useIsWalletConnect'
+import * as legacyUseWalletMetadata from './web3-react/hooks/useWalletMetadata'
 
 export * from './web3-react/hooks/useActivateConnector'
 
@@ -22,18 +15,51 @@ export * from './web3-react/hooks/useConnectionType'
 
 export * from './web3-react/hooks/useDisconnectWallet'
 
-export const useAccountType = useIsSmartContractWalletImplementation.useAccountType
-export const useIsSmartContractWallet = useIsSmartContractWalletImplementation.useIsSmartContractWallet
+export function useAccountType() {
+  const newUseAccountType = newUseIsSmartContract.useAccountType()
+  const legacyUseAccountType = legacyUseIsSmartContractWallet.useAccountType()
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newUseAccountType : legacyUseAccountType
+}
+export function useIsSmartContractWallet() {
+  const newIsSmartContractWallet = newUseIsSmartContract.useIsSmartContractWallet()
+  const legacyIsSmartContractWallet = legacyUseIsSmartContractWallet.useIsSmartContractWallet()
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newIsSmartContractWallet : legacyIsSmartContractWallet
+}
 
-export const getIsWalletConnect = useIsWalletConnectImplementation.getIsWalletConnect
-export const useIsWalletConnect = useIsWalletConnectImplementation.useIsWalletConnect
+export function getIsWalletConnect(connector: LegacyConnector) {
+  const newIsWalletConnect = newUseIsWalletConnect.getIsWalletConnect(connector)
+  const legacyIsWalletConnect = legacyUseIsWalletConnect.getIsWalletConnect(connector)
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newIsWalletConnect : legacyIsWalletConnect
+}
+export function useIsWalletConnect() {
+  const newIsWalletConnect = newUseIsWalletConnect.useIsWalletConnect()
+  const legacyIsWalletConnect = legacyUseIsWalletConnect.useIsWalletConnect
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newIsWalletConnect : legacyIsWalletConnect
+}
 
 export * from './web3-react/hooks/useSafeAppsSdk'
 
 export * from './web3-react/hooks/useSwitchNetwork'
 
-export const useIsSafeApp = useWalletMetaDataImplementation.useIsSafeApp
-export const useIsSafeViaWc = useWalletMetaDataImplementation.useIsSafeViaWc
-export const useIsSafeWallet = useWalletMetaDataImplementation.useIsSafeWallet
-export const useWalletMetaData = useWalletMetaDataImplementation.useWalletMetaData
+export function useIsSafeApp() {
+  const newIsSafeApp = newUseWalletMetadata.useIsSafeApp()
+  const legacyIsSafeApp = legacyUseWalletMetadata.useIsSafeApp()
+  console.log('LAUNCH_DARKLY_VIEM_MIGRATION', LAUNCH_DARKLY_VIEM_MIGRATION)
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newIsSafeApp : legacyIsSafeApp
+}
+export function useIsSafeViaWc() {
+  const newIsSafeViaWc = newUseWalletMetadata.useIsSafeViaWc()
+  const legacyIsSafeViaWc = legacyUseWalletMetadata.useIsSafeViaWc()
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newIsSafeViaWc : legacyIsSafeViaWc
+}
+export function useIsSafeWallet() {
+  const newIsSafeWallet = newUseWalletMetadata.useIsSafeWallet()
+  const legacyIsSafeWallet = legacyUseWalletMetadata.useIsSafeWallet()
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newIsSafeWallet : legacyIsSafeWallet
+}
+export function useWalletMetaData() {
+  const newWalletMetadata = newUseWalletMetadata.useWalletMetaData()
+  const legacyWalletMetadata = legacyUseWalletMetadata.useWalletMetaData()
+  return LAUNCH_DARKLY_VIEM_MIGRATION ? newWalletMetadata : legacyWalletMetadata
+}
 export type { WalletMetaData } from './wagmi/hooks/useWalletMetadata'
