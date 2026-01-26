937faff898cb2a2d1d477f61af959236f931993c
Bernardo Valverde <bsvalverde@gmail.com>
2026-01-26 18:03:47 -0300

    chore: removes TODO from hook that will no longer be used

--- libs/wallet/src/web3-react/hooks/useIsActiveConnection.ts
+++ libs/wallet/src/web3-react/hooks/useIsActiveConnection.ts
@@ -5,8 +5,6 @@ import { Web3ReactConnection } from '../types'
 // TODO: Add proper return type annotation
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const useIsActiveConnection = (selectedWallet: string | undefined, connection: Web3ReactConnection) => {
-  // TODO M-2 COW-568
-  // Wallet connection (and warnings) through wagmi will be handled in a future task
   const { account } = useWeb3React()
 
   const isActive = connection.hooks.useIsActive()
