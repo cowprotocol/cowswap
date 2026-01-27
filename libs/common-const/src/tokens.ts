4a1a60275e710ad1001896ca20ce7f5c3de3bf32
Dani Gámez Franco <gmzcodes@outlook.com>
2026-01-27 12:25:25 +0100

    fix: revert changed comment

--- libs/common-const/src/tokens.ts
+++ libs/common-const/src/tokens.ts
@@ -572,7 +572,7 @@ export const WETH_INK = new TokenWithLogo(
 export const USDC_OPTIMISM = new TokenWithLogo(
   USDC_MAINNET.logoURI,
   AdditionalTargetChainId.OPTIMISM,
-  // https://explorer.inkonchain.com/token/0x4200000000000000000000000000000000000006
+  // https://optimistic.etherscan.io/address/0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85
   '0x0b2c639c533813f4aa9d7837caf62653d097ff85',
   6,
   'USDC',
