6f11822d060c5c4152e99f2afa5ce4417c921137
Dani Gámez Franco <gmzcodes@outlook.com>
2026-01-29 23:58:04 +0100

    fix: add TODO for missing i18n value

--- libs/common-const/src/getAvailableChainsText.ts
+++ libs/common-const/src/getAvailableChainsText.ts
@@ -3,13 +3,16 @@ import { ALL_SUPPORTED_CHAINS_MAP } from '@cowprotocol/cow-sdk'
 import { SORTED_CHAIN_IDS } from './chainInfo'
 
 export function getAvailableChainsText(): string {
-  return SORTED_CHAIN_IDS.reduce((acc, chainId) => {
-    const { label, isTestnet, isUnderDevelopment } = ALL_SUPPORTED_CHAINS_MAP[chainId]
-    if (!isUnderDevelopment) {
-      acc.push(`${label}${isTestnet ? ' (testnet)' : ''}`)
-    }
-    return acc
-  }, [] as string[])
-    .join(', ')
-    .replace(/, ([^,]*)$/, ' and $1')
+  return (
+    SORTED_CHAIN_IDS.reduce((acc, chainId) => {
+      const { label, isTestnet, isUnderDevelopment } = ALL_SUPPORTED_CHAINS_MAP[chainId]
+      if (!isUnderDevelopment) {
+        acc.push(`${label}${isTestnet ? ' (testnet)' : ''}`)
+      }
+      return acc
+    }, [] as string[])
+      .join(', ')
+      // TODO: i18n missing here
+      .replace(/, ([^,]*)$/, ' and $1')
+  )
 }
