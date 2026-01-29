9631390957280d75b7429927d3f9f86ded430c51
Alexandr Kazachenko <shoom3301@gmail.com>
2026-01-29 17:14:08 +0500

    chore: test commit

--- apps/cowswap-frontend/src/common/containers/CancellationModal/index.tsx
+++ apps/cowswap-frontend/src/common/containers/CancellationModal/index.tsx
@@ -28,6 +28,7 @@ export function CancellationModal(props: CancellationModalProps): ReactNode {
     if (!ultimateOrder) return undefined
 
     const { inputAmount, outputAmount } = getUltimateOrderTradeAmounts(ultimateOrder)
+
     const receiver = ultimateOrder.bridgeOrderFromStore?.recipient ?? ultimateOrder.orderFromStore.receiver
     const owner = ultimateOrder.orderFromStore.owner
 
