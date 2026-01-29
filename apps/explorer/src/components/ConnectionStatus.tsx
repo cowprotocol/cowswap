6a1473cc35bc2da33edbf2fd26e938e9e0f766d2
Alexandr Kazachenko <shoom3301@gmail.com>
2026-01-29 19:24:39 +0500

    chore: fix cache

--- apps/explorer/src/components/ConnectionStatus.tsx
+++ apps/explorer/src/components/ConnectionStatus.tsx
@@ -4,7 +4,6 @@ import { Notification } from './Notification'
 
 export const ConnectionStatus: React.FC = () => {
   const [online, setOnline] = useState(true)
-
   const handleConnectionChange = useCallback((e: Event) => {
     setOnline(e.type === 'online')
   }, [])
