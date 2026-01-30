5a4695258b26e694dcd641c68593273f8250e1eb
Alexandr Kazachenko <shoom3301@gmail.com>
2026-01-30 11:32:40 +0500

    chore: test cache

--- apps/widget-configurator/src/main.tsx
+++ apps/widget-configurator/src/main.tsx
@@ -68,6 +68,7 @@ function Root(): ReactNode {
       <ColorModeContext.Provider value={colorMode}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+
           <GlobalStyles styles={globalStyles(theme, colorMode.mode)} />
           <Box sx={WrapperStyled}>
             <WithLDProvider>
