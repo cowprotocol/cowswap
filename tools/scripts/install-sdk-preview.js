a97c17e48dcc31fa189fcd0d9930559b1a5f3b8b
Alexandr Kazachenko <shoom3301@gmail.com>
2026-02-02 17:23:58 +0500

    chore: add a script to upgrade sdk packages from sdk PR

--- tools/scripts/install-sdk-preview.js
+++ tools/scripts/install-sdk-preview.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const packageJson = require('../../package.json')
+const packageJson = require('../../apps/cowswap-frontend/package.json')
 
 const sdkPrVersionRegex = /pr-\d+/
 
