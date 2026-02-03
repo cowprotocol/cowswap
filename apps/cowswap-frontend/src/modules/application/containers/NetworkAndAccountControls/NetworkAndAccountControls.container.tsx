94ece9913de2b8effd7978d3e4e386fd1c7abd49
Dani Gámez Franco <gmzcodes@outlook.com>
2026-02-03 14:27:07 +0100

    fix: move NetworkSelector outside of legacy directory

--- apps/cowswap-frontend/src/modules/application/containers/NetworkAndAccountControls/NetworkAndAccountControls.container.tsx
+++ apps/cowswap-frontend/src/modules/application/containers/NetworkAndAccountControls/NetworkAndAccountControls.container.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
 
 import { AccountElement } from 'legacy/components/Header/AccountElement'
-import { NetworkSelector } from 'legacy/components/Header/NetworkSelector/NetworkSelector.container'
 import { HeaderControls, HeaderElement } from 'legacy/components/Header/styled'
 
+import { NetworkSelector } from 'modules/application/containers/NetworkSelector/NetworkSelector.container'
 import { useInjectedWidgetParams } from 'modules/injectedWidget'
 
 export function NetworkAndAccountControls(): ReactNode {
