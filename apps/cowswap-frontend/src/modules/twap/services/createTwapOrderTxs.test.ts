8b037ead5246be30aedc7b8953fe52b301c3114a
Bernardo Valverde <bsvalverde@gmail.com>
2026-02-13 17:53:54 -0300

    fix: fixes tests

--- apps/cowswap-frontend/src/modules/twap/services/createTwapOrderTxs.test.ts
+++ apps/cowswap-frontend/src/modules/twap/services/createTwapOrderTxs.test.ts
@@ -1,8 +1,9 @@
 import { COW_TOKEN_TO_CHAIN, WETH_SEPOLIA } from '@cowprotocol/common-const'
-import { SupportedChainId } from '@cowprotocol/cow-sdk'
+import { SupportedChainId, ZERO_ADDRESS } from '@cowprotocol/cow-sdk'
+import { ComposableCoWAbi } from '@cowprotocol/cowswap-abis'
 import { CurrencyAmount } from '@uniswap/sdk-core'
 
-import { COMPOSABLE_COW_ADDRESS, CURRENT_BLOCK_FACTORY_ADDRESS } from 'modules/advancedOrders'
+import { CURRENT_BLOCK_FACTORY_ADDRESS } from 'modules/advancedOrders'
 import { getAppData } from 'modules/appData'
 
 import { createTwapOrderTxs } from './createTwapOrderTxs'
@@ -12,6 +13,12 @@ import { TWAPOrder } from '../types'
 import { buildTwapOrderParamsStruct } from '../utils/buildTwapOrderParamsStruct'
 
 jest.mock('modules/permit')
+jest.mock('viem', () => {
+  return {
+    ...jest.requireActual('viem'),
+    encodeFunctionData: jest.fn(),
+  }
+})
 
 const APP_DATA_HASH = getAppData().appDataKeccak256
 
@@ -32,43 +39,28 @@ const order: TWAPOrder = {
   appData: APP_DATA_HASH,
 }
 
-const CREATE_COW_TX_DATA = '0xCREATE_COW_TX_DATA'
-const APPROVE_TX_DATA = '0xAPPROVE_TX_DATA'
-
 describe('Create TWAP order', () => {
   let context: TwapOrderCreationContext
-  let createCowFn: jest.Mock
-  let approveFn: jest.Mock
 
   beforeEach(() => {
     jest.spyOn(Date, 'now').mockImplementation(() => 1497076708000)
 
-    createCowFn = jest.fn().mockReturnValue(CREATE_COW_TX_DATA)
-    approveFn = jest.fn().mockReturnValue(APPROVE_TX_DATA)
-
     context = {
       chainId,
       composableCowContract: {
-        interface: { encodeFunctionData: createCowFn },
-        address: COMPOSABLE_COW_ADDRESS[chainId],
-        // TODO: Replace any with proper type definitions
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any,
+        abi: ComposableCoWAbi,
+        address: ZERO_ADDRESS,
+      },
       needsApproval: false,
       needsZeroApproval: false,
       spender: '0xB4FBF271143F4FBf7B91A5ded31805e42b222222',
       currentBlockFactoryAddress: CURRENT_BLOCK_FACTORY_ADDRESS[chainId],
-      // TODO: Replace any with proper type definitions
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      erc20Contract: { interface: { encodeFunctionData: approveFn } } as any,
     }
   })
 
   it('When sell token is approved, then should generate only creation transaction', () => {
     const paramsStruct = buildTwapOrderParamsStruct(chainId, order)
     const result = createTwapOrderTxs(order, paramsStruct, { ...context, needsApproval: false })
-
-    expect(createCowFn).toHaveBeenCalledTimes(1)
 
     expect(result.length).toBe(1)
   })
@@ -77,17 +69,12 @@ describe('Create TWAP order', () => {
     const paramsStruct = buildTwapOrderParamsStruct(chainId, order)
     const result = createTwapOrderTxs(order, paramsStruct, { ...context, needsApproval: true })
 
-    expect(createCowFn).toHaveBeenCalledTimes(1)
-    expect(approveFn).toHaveBeenCalledTimes(1)
     expect(result.length).toBe(2)
   })
 
   it('When sell token is NOT approved AND token needs zero approval, then should generate 2 approvals and creation transactions', () => {
     const paramsStruct = buildTwapOrderParamsStruct(chainId, order)
     const result = createTwapOrderTxs(order, paramsStruct, { ...context, needsApproval: true, needsZeroApproval: true })
-
-    expect(createCowFn).toHaveBeenCalledTimes(1)
-    expect(approveFn).toHaveBeenCalledTimes(2)
 
     expect(result.length).toBe(3)
   })
