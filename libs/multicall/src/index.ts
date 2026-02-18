213f4d9165c480d4617c8a9c778b4f79d549fb9d
Bernardo Valverde <bsvalverde@gmail.com>
2026-02-18 15:22:47 -0300

    chore: minimizes multicall lib

--- libs/multicall/src/index.ts
+++ libs/multicall/src/index.ts
@@ -1,8 +1,1 @@
-export { useSingleContractMultipleData } from './hooks/useSingleContractMultipleData'
-export { useMultipleContractSingleData } from './hooks/useMultipleContractSingleData'
 export { getMulticallContract } from './utils/getMulticallContract'
-export { useMultiCallRpcProvider } from './hooks/useMultiCallRpcProvider'
-export { multiCall } from './multicall'
-export type { MultiCallOptions } from './multicall'
-
-export { MultiCallUpdater } from './updaters/MultiCallUpdater'
