4cd89b55b96dfadf443fa6f2c340f966b027aeba
Bernardo Valverde <bsvalverde@gmail.com>
2026-02-19 17:38:34 -0300

    chore: removes ethers from sdk-tools app

--- apps/sdk-tools/src/main.tsx
+++ apps/sdk-tools/src/main.tsx
@@ -1,26 +1,20 @@
 import { StrictMode } from 'react'
 
 import 'inter-ui'
-import { getRpcProvider } from '@cowprotocol/common-const'
-import { setGlobalAdapter, SupportedChainId } from '@cowprotocol/cow-sdk'
-import { EthersV5Adapter } from '@cowprotocol/sdk-ethers-v5-adapter'
+import { Web3Provider } from '@cowprotocol/wallet'
 
 import { createRoot } from 'react-dom/client'
 
 import { SdkTools } from './SdkTools'
-
-// Initialize the global adapter for the CoW SDK
-const adapter = new EthersV5Adapter({
-  provider: getRpcProvider(SupportedChainId.MAINNET)!,
-})
-setGlobalAdapter(adapter)
 
 // TODO: Add proper return type annotation
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function Root() {
   return (
     <StrictMode>
-      <SdkTools />
+      <Web3Provider>
+        <SdkTools />
+      </Web3Provider>
     </StrictMode>
   )
 }
