87100bb08bd11773507f9323ab7160b5db9e7843
Bernardo Valverde <bsvalverde@gmail.com>
2026-02-19 11:45:04 -0300

    chore: removes old packages from types lib

--- libs/types/src/eip6963-types.ts
+++ libs/types/src/eip6963-types.ts
@@ -1,6 +1,4 @@
-import type { RequestArguments } from '@web3-react/types'
-
-import type EventEmitter from 'eventemitter3'
+import type { EIP1193Provider } from 'viem'
 
 export interface EIP6963ProviderInfo {
   uuid: string
@@ -13,12 +11,6 @@ export interface EIP6963ProviderInfo {
 export interface EIP6963ProviderDetail {
   info: EIP6963ProviderInfo
   provider: EIP1193Provider
-}
-
-// https://eips.ethereum.org/EIPS/eip-1193
-export type EIP1193Provider = EventEmitter & {
-  isConnected?: () => boolean
-  request<T>(args: RequestArguments): Promise<T>
 }
 
 export interface EIP6963AnnounceProviderEvent extends CustomEvent {
