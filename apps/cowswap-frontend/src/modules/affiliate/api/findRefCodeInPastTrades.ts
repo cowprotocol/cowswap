e95e301c90039c037eb20f3f00d157582ca7a092
Daniel Constantin <hello@danielconstantin.net>
2026-03-03 14:12:13 +0200

    feat: check both environments for trade eligibility & ref codes

--- apps/cowswap-frontend/src/modules/affiliate/api/findRefCodeInPastTrades.ts
+++ apps/cowswap-frontend/src/modules/affiliate/api/findRefCodeInPastTrades.ts
@@ -1,4 +1,4 @@
-import { Address } from '@cowprotocol/cow-sdk'
+import { Address, CowEnv } from '@cowprotocol/cow-sdk'
 
 import { getTrades } from 'api/cowProtocol'
 import { safeShortenAddress } from 'utils/address'
@@ -7,19 +7,32 @@ import { AFFILIATE_SUPPORTED_CHAIN_IDS } from '../config/affiliateProgram.const'
 import { AppDataOrder, extractFullAppDataFromOrder, getRefCodeFromAppData } from '../lib/affiliateProgramUtils'
 import { logAffiliate } from '../utils/logger'
 
-export async function findRefCodeInPastTrades(account: Address): Promise<string | undefined> {
-  const codesByChain = await Promise.all(
-    AFFILIATE_SUPPORTED_CHAIN_IDS.map(async (chainId) => {
-      try {
-        const trades = (await getTrades({ owner: account, limit: 1 }, { chainId })) as unknown as AppDataOrder[]
+const TRADE_ENVS_TO_CHECK: CowEnv[] = ['prod', 'staging']
 
-        return trades.map((trade) => getRefCodeFromAppData(extractFullAppDataFromOrder(trade)))
-      } catch (error) {
-        logAffiliate(safeShortenAddress(account), 'Failed to fetch trades for referral recovery', error, { chainId })
-        return []
-      }
-    }),
+export async function findRefCodeInPastTrades(owner: Address): Promise<string | undefined> {
+  logAffiliate(safeShortenAddress(owner), 'Calling cow api for each supported chain/env to recover ref code')
+
+  const codesByChainAndEnv = await Promise.all(
+    AFFILIATE_SUPPORTED_CHAIN_IDS.flatMap((chainId) =>
+      TRADE_ENVS_TO_CHECK.map(async (env) => {
+        try {
+          const trades = (await getTrades({ owner, limit: 1 }, { chainId, env })) as unknown as AppDataOrder[]
+
+          return trades.map((trade) => getRefCodeFromAppData(extractFullAppDataFromOrder(trade)))
+        } catch (error) {
+          logAffiliate(safeShortenAddress(owner), 'Failed to fetch trades for ref code recovery', error, {
+            chainId,
+            env,
+          })
+
+          return []
+        }
+      }),
+    ),
   )
 
-  return codesByChain.flat().find((code) => !!code)
+  const code = codesByChainAndEnv.flat().find((code) => !!code)
+
+  logAffiliate(safeShortenAddress(owner), `Called cow api, ref code ${code ? 'found' : 'not found'}`)
+  return code
 }
