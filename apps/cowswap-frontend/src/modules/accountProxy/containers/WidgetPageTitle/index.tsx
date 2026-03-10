c17d7ca903e00f27a02d573f4f4402163c00da14
Daniel Constantin <hello@danielconstantin.net>
2026-03-10 14:11:33 +0200

    chore: bring back Recover funds title

--- apps/cowswap-frontend/src/modules/accountProxy/containers/WidgetPageTitle/index.tsx
+++ apps/cowswap-frontend/src/modules/accountProxy/containers/WidgetPageTitle/index.tsx
@@ -5,9 +5,7 @@ import { useTokensByAddressMap } from '@cowprotocol/tokens'
 import { useWalletInfo } from '@cowprotocol/wallet'
 
 import { useLingui } from '@lingui/react/macro'
-import { matchPath, useLocation, useParams } from 'react-router'
-
-import { Routes } from 'common/constants/routes'
+import { useLocation, useParams } from 'react-router'
 
 import { NEED_HELP_LABEL } from '../../consts'
 
@@ -21,15 +19,10 @@ export function WidgetPageTitle(): ReactNode {
   const isWalletConnected = !!account
   const isHelpPage = location.pathname.endsWith('/help')
   const isRecoverPage = !!tokenAddress
-  const isRootProxyPage = !!matchPath(Routes.ACCOUNT_PROXIES, location.pathname)
 
   // Check for help page first, regardless of wallet connection
   if (isHelpPage) {
     return i18n._(NEED_HELP_LABEL)
-  }
-
-  if (isRootProxyPage) {
-    return null
   }
 
   if (!isWalletConnected) {
@@ -41,5 +34,5 @@ export function WidgetPageTitle(): ReactNode {
     return t`Recover ${target}`
   }
 
-  return t`All accounts`
+  return t`Recover funds`
 }
