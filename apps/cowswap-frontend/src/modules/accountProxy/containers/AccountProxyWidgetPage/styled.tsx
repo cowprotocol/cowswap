0a5c92b326ce10f8e6c73bb9b6630cf190a947ae
Daniel Constantin <hello@danielconstantin.net>
2026-03-10 15:44:42 +0200

    refactor(account): remove modal mode in account proxy page

--- apps/cowswap-frontend/src/modules/accountProxy/containers/AccountProxyWidgetPage/styled.tsx
+++ apps/cowswap-frontend/src/modules/accountProxy/containers/AccountProxyWidgetPage/styled.tsx
@@ -1,29 +1,9 @@
-import { Media } from '@cowprotocol/ui'
-
-import { transparentize } from 'color2k'
 import { Link } from 'react-router'
 import styled from 'styled-components/macro'
 import { WIDGET_MAX_WIDTH } from 'theme'
 
-export const EmptyWrapper = styled.div<{ $modalMode: boolean }>`
+export const EmptyWrapper = styled.div`
   width: 100%;
-`
-
-export const ModalWrapper = styled.div<{ $modalMode: boolean }>`
-  position: ${({ $modalMode }) => ($modalMode ? 'fixed' : 'static')};
-  width: 100%;
-  height: 100%;
-  z-index: 100;
-  left: 0;
-  top: 0;
-  overflow-y: scroll;
-  padding: 50px 0;
-  background: ${({ theme }) => (theme.isWidget ? 'transparent' : transparentize(theme.black, 0.5))};
-  backdrop-filter: blur(3px);
-
-  ${Media.upToSmall()} {
-    padding: 0;
-  }
 `
 
 export const WidgetWrapper = styled.div`
