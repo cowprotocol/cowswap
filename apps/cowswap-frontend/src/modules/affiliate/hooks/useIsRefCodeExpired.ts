34d0b82df5ef9f452bd6ea6b8ee1b25bb517d2db
Daniel Constantin <hello@danielconstantin.net>
2026-03-11 10:45:11 +0200

    feat(affiliate): enable conditional fetching of trader stats based on referral code status

--- apps/cowswap-frontend/src/modules/affiliate/hooks/useIsRefCodeExpired.ts
+++ apps/cowswap-frontend/src/modules/affiliate/hooks/useIsRefCodeExpired.ts
@@ -1,3 +1,4 @@
+import { useAtomValue } from 'jotai'
 import { useEffect } from 'react'
 
 import { useMachineTimeMs } from '@cowprotocol/common-hooks'
@@ -9,11 +10,13 @@ import { useAffiliateTraderStats } from './useAffiliateTraderStats'
 
 import { AFFILIATE_EXPIRY_CHECK_INTERVAL_MS } from '../config/affiliateProgram.const'
 import { toValidDate } from '../lib/affiliateProgramUtils'
+import { affiliateTraderSavedCodeAtom } from '../state/affiliateTraderSavedCodeAtom'
 import { logAffiliate } from '../utils/logger'
 
 export function useIsRefCodeExpired(): boolean {
   const { account } = useWalletInfo()
-  const { data: stats } = useAffiliateTraderStats(account)
+  const { savedCode } = useAtomValue(affiliateTraderSavedCodeAtom)
+  const { data: stats } = useAffiliateTraderStats(account, !!savedCode)
 
   const now = useMachineTimeMs(AFFILIATE_EXPIRY_CHECK_INTERVAL_MS)
   const rewardsEndTimestamp = toValidDate(stats?.rewards_end)?.getTime()
