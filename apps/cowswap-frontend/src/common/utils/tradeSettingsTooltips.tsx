edde5c62be487310e06646ab8a8abef542e76377
Dani Gámez Franco <gmzcodes@outlook.com>
2026-03-16 17:01:19 +0100

    fix: remove import from a different module

--- apps/cowswap-frontend/src/common/utils/tradeSettingsTooltips.tsx
+++ apps/cowswap-frontend/src/common/utils/tradeSettingsTooltips.tsx
@@ -6,12 +6,11 @@ import {
   MINIMUM_ETH_FLOW_SLIPPAGE,
   PERCENTAGE_PRECISION,
 } from '@cowprotocol/common-const'
+import { SimpleStyledText } from '@cowprotocol/ui'
 
 import { i18n } from '@lingui/core'
 import { t } from '@lingui/core/macro'
 import { Trans } from '@lingui/react/macro'
-import { SlippageWarningParams } from 'modules/tradeWidgetAddons'
-import { SimpleStyledText } from '@cowprotocol/ui'
 
 export function getNativeOrderDeadlineTooltip(symbols: (string | undefined)[] | undefined): ReactNode {
   const symbolName = symbols?.[0] || t`Native currency (e.g ETH)`
@@ -35,7 +34,9 @@ export function getNativeOrderDeadlineTooltip(symbols: (string | undefined)[] | 
 export function getNonNativeOrderDeadlineTooltip(): ReactNode {
   return (
     <SimpleStyledText>
-      <p><Trans>Your swap expires and will not execute if it is pending for longer than the selected duration.</Trans></p>
+      <p>
+        <Trans>Your swap expires and will not execute if it is pending for longer than the selected duration.</Trans>
+      </p>
       <p>{i18n._(INPUT_OUTPUT_EXPLANATION)}</p>
     </SimpleStyledText>
   )
@@ -49,14 +50,18 @@ export function getNativeSlippageTooltip(symbols: (string | undefined)[] | undef
 
   return (
     <SimpleStyledText>
-      <p><Trans>
-        When selling {aNativeCurrency}, the minimum slippage tolerance is set to {minimumETHFlowSlippage}% or higher to
-        ensure a high likelihood of order matching, even in volatile market conditions.
-      </Trans></p>
-      <p><Trans>
-        {currencyNameCapitalized} orders can, in rare cases, be frontrun due to their on-chain component. For more
-        robust MEV protection, consider wrapping your {currencyName} before trading.
-      </Trans></p>
+      <p>
+        <Trans>
+          When selling {aNativeCurrency}, the minimum slippage tolerance is set to {minimumETHFlowSlippage}% or higher
+          to ensure a high likelihood of order matching, even in volatile market conditions.
+        </Trans>
+      </p>
+      <p>
+        <Trans>
+          {currencyNameCapitalized} orders can, in rare cases, be frontrun due to their on-chain component. For more
+          robust MEV protection, consider wrapping your {currencyName} before trading.
+        </Trans>
+      </p>
     </SimpleStyledText>
   )
 }
@@ -64,7 +69,10 @@ export function getNativeSlippageTooltip(symbols: (string | undefined)[] | undef
 interface GetNonNativeSlippageTooltipParams {
   isDynamic?: boolean
   isSettingsModal?: boolean
-  slippageWarningParams?: SlippageWarningParams | null
+  slippageWarningParams?: {
+    tooLow?: boolean
+    tooHigh?: boolean
+  }
 }
 
 export function getNonNativeSlippageTooltip({
@@ -75,22 +83,29 @@ export function getNonNativeSlippageTooltip({
   if (isDynamic) {
     return (
       <SimpleStyledText>
-        <p><Trans>
-          CoW Swap has dynamically selected this slippage tolerance, accounting for current gas prices and trade size, to ensure your trade executes quickly while still getting the best price.
-        </Trans></p>
+        <p>
+          <Trans>
+            CoW Swap has dynamically selected this slippage tolerance, accounting for current gas prices and trade size,
+            to ensure your trade executes quickly while still getting the best price.
+          </Trans>
+        </p>
 
-        {isSettingsModal ? (<>
-          <p><Trans>
-            To override this, enter your desired slippage amount, but this may result in slower execution.
-          </Trans></p>
-          <p><Trans>
-            Either way, trades are protected from MEV, so your slippage can't be exploited!
-          </Trans></p>
-        </>) : (
-          <p><Trans>
-            Trades are protected from MEV, so your slippage can't be exploited!
-          </Trans></p>
-        ) }
+        {isSettingsModal ? (
+          <>
+            <p>
+              <Trans>
+                To override this, enter your desired slippage amount, but this may result in slower execution.
+              </Trans>
+            </p>
+            <p>
+              <Trans>Either way, trades are protected from MEV, so your slippage can't be exploited!</Trans>
+            </p>
+          </>
+        ) : (
+          <p>
+            <Trans>Trades are protected from MEV, so your slippage can't be exploited!</Trans>
+          </p>
+        )}
       </SimpleStyledText>
     )
   }
@@ -105,13 +120,21 @@ export function getNonNativeSlippageTooltip({
 
   return (
     <SimpleStyledText>
-      <p>{ manualSlippageMessage }</p>
-      { isSettingsModal ? (<>
-        <p><Trans>To let Cow Swap automatically select the slippage tolerance, toggle the "Dynamic" option.</Trans></p>
-        <p><Trans>Either way, trades are protected from MEV, so your slippage can't be exploited!</Trans></p>
-      </>) : (
-        <p><Trans>Trades are protected from MEV, so your slippage can't be exploited!</Trans></p>
-      ) }
+      <p>{manualSlippageMessage}</p>
+      {isSettingsModal ? (
+        <>
+          <p>
+            <Trans>To let Cow Swap automatically select the slippage tolerance, toggle the "Dynamic" option.</Trans>
+          </p>
+          <p>
+            <Trans>Either way, trades are protected from MEV, so your slippage can't be exploited!</Trans>
+          </p>
+        </>
+      ) : (
+        <p>
+          <Trans>Trades are protected from MEV, so your slippage can't be exploited!</Trans>
+        </p>
+      )}
     </SimpleStyledText>
   )
 }
