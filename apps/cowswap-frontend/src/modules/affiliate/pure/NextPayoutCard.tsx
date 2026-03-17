8df4f4ae46814cd5065c826d9940bfd3abb04372
Elena <70885163+elena-zh@users.noreply.github.com>
2026-03-17 12:53:15 +0300

    add up to

--- apps/cowswap-frontend/src/modules/affiliate/pure/NextPayoutCard.tsx
+++ apps/cowswap-frontend/src/modules/affiliate/pure/NextPayoutCard.tsx
@@ -37,7 +37,7 @@ export function NextPayoutCard({ payout, showLoader }: NextPayoutCardProps): Rea
       </PayoutValue>
       <BottomMetaRow>
         <InlineNote>
-          <Trans>Paid weekly on Fridays via airdrop</Trans>
+          <Trans>Paid weekly up to Fridays via airdrop</Trans>
         </InlineNote>
       </BottomMetaRow>
     </ColumnThreeCard>
