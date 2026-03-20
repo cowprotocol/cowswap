a8b24f854cffa29a5cf505b1a967119ec854b785
Dani Gámez Franco <gmzcodes@outlook.com>
2026-03-20 13:56:15 +0100

    feat: introduce a generic asyncAtomFamily and use it in allowancesAtom

--- libs/balances-and-allowances/src/state/allowancesAtom.ts
+++ libs/balances-and-allowances/src/state/allowancesAtom.ts
@@ -1,16 +1,13 @@
-import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
 import { getRpcProvider } from '@cowprotocol/common-const'
-import { COW_PROTOCOL_VAULT_RELAYER_ADDRESS } from '@cowprotocol/common-utils'
+import { asyncAtomFamily, COW_PROTOCOL_VAULT_RELAYER_ADDRESS } from '@cowprotocol/common-utils'
 import { getJotaiMergerStorage } from '@cowprotocol/core'
 import { mapSupportedNetworks, SupportedChainId } from '@cowprotocol/cow-sdk'
 import { ERC_20_INTERFACE } from '@cowprotocol/cowswap-abis'
 import { multiCall } from '@cowprotocol/multicall'
 import { PersistentStateByChain } from '@cowprotocol/types'
 import { BigNumber } from '@ethersproject/bignumber'
-
-import { atomFamily } from 'jotai-family'
 
 export type AllowancesState = Record<string, BigNumber | undefined>
 
@@ -71,53 +68,20 @@ export interface TokenAllowancesFamilyParams {
   tokenAddresses: string[]
 }
 
-export const tokenAllowancesFamily = atomFamily((params: TokenAllowancesFamilyParams) => {
-  const tokenAllowancesAtom = atom<AllowancesState | null>(null)
-
-  tokenAllowancesAtom.onMount = (set) => {
+export const tokenAllowancesFamily = asyncAtomFamily(
+  async (params: TokenAllowancesFamilyParams): Promise<AllowancesState | null> => {
     const { chainId, account, tokenAddresses } = params
     const spender = COW_PROTOCOL_VAULT_RELAYER_ADDRESS[chainId]
 
-    const totalFamilyMembers = tokenAllowancesFamily.length
-
-    if (totalFamilyMembers > 10) {
-      console.warn('Possible memory leak detected in tokenAllowancesFamily')
-    }
-
     if (!chainId || !account || !spender || !tokenAddresses.length) {
-      return () => {
-        tokenAllowancesFamily.remove(params)
-      }
+      return null
     }
 
-    let cancelled = false
-
-    fetchAllowances(chainId, account, spender, tokenAddresses)
-      .then((result) => {
-        if (cancelled) return
-
-        set(result)
-      })
-      .catch((err: unknown) => {
-        if (cancelled) return
-
-        console.error('[tokenAllowancesFamily] error:', err)
-
-        set({})
-      })
-
-    return () => {
-      cancelled = true
-      tokenAllowancesFamily.remove(params)
-    }
-  }
-
-  return tokenAllowancesAtom
-}, areTokenAllowancesParamsEqual)
-
-// Why are we doing the cleanup using `onMount` instead of `setShouldRemove`?
-// Well, `setShouldRemove` runs immediately and when you are about to get an
-// atom from the cache. Internally, atomFamily is just a Map whose key is a
-// param and whose value is an atom config. Unless you explicitly remove unused
-// params, this leads to memory leaks. This is crucial if you use infinite
-// number of params.
+    return fetchAllowances(chainId, account, spender, tokenAddresses)
+  },
+  {
+    areEqual: areTokenAllowancesParamsEqual,
+    familyLabel: 'tokenAllowancesFamily',
+    valueOnError: {} as AllowancesState,
+  },
+)
