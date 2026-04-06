b15e8ce11ae2dabf62130621872047627d692d5f
brunota20 <brunot.anjos@gmail.com>
2026-04-06 15:40:29 -0300

    fix: missing import

--- apps/cowswap-frontend/src/modules/tokensList/containers/SelectTokenWidget/hooks/useTokenSelectionHandler.ts
+++ apps/cowswap-frontend/src/modules/tokensList/containers/SelectTokenWidget/hooks/useTokenSelectionHandler.ts
@@ -9,7 +9,8 @@ import { Field } from 'legacy/state/types'
 
 import { TradeType } from 'modules/trade'
 
-import { useOnSelectNetwork } from '../../../../application/hooks/useOnSelectNetwork'
+import { useOnSelectNetwork } from 'common/hooks/useOnSelectNetwork'
+
 import { useSelectTokenWidgetState } from '../../../hooks/useSelectTokenWidgetState'
 import { TokenSelectionHandler } from '../../../types'
 
@@ -18,18 +19,6 @@ interface ShouldSwitchNetworkParams {
   tradeType: TradeType | undefined
   targetChainId: number | undefined
   walletChainId: SupportedChainId
-}
-
-function getNetworkToSwitch(params: ShouldSwitchNetworkParams): SupportedChainId | null {
-  const { field, tradeType, targetChainId, walletChainId } = params
-
-  const shouldSwitch =
-    field === Field.INPUT &&
-    (tradeType === TradeType.LIMIT_ORDER || tradeType === TradeType.ADVANCED_ORDERS) &&
-    isSupportedChainId(targetChainId) &&
-    targetChainId !== walletChainId
-
-  return shouldSwitch ? targetChainId : null
 }
 
 export function useTokenSelectionHandler(
@@ -68,4 +57,16 @@ export function useTokenSelectionHandler(
       onSelectNetwork,
     ],
   )
+}
+
+function getNetworkToSwitch(params: ShouldSwitchNetworkParams): SupportedChainId | null {
+  const { field, tradeType, targetChainId, walletChainId } = params
+
+  const shouldSwitch =
+    field === Field.INPUT &&
+    (tradeType === TradeType.LIMIT_ORDER || tradeType === TradeType.ADVANCED_ORDERS) &&
+    isSupportedChainId(targetChainId) &&
+    targetChainId !== walletChainId
+
+  return shouldSwitch ? targetChainId : null
 }
