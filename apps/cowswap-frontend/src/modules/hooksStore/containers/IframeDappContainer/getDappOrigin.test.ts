de2c88a164624944b1317275521ad3d1e226c034
Alexandr Kazachenko <shoom3301@gmail.com>
2026-04-14 17:14:47 +0500

    chore: fix getDappOrigin test

--- apps/cowswap-frontend/src/modules/hooksStore/containers/IframeDappContainer/getDappOrigin.test.ts
+++ apps/cowswap-frontend/src/modules/hooksStore/containers/IframeDappContainer/getDappOrigin.test.ts
@@ -1,13 +1,15 @@
+import { getDappOrigin } from './getDappOrigin'
+
 describe('getDappOrigin', () => {
   it('Should return "null" when starts with javascript:', () => {
-    expect(new URL('javascript:alert(1)').origin).toBe('null')
+    expect(getDappOrigin('javascript:alert(1)')).toBe(null)
   })
 
   it('Should return the URL when it is valid', () => {
-    expect(new URL('https://google.com').origin).toBe('https://google.com')
+    expect(getDappOrigin('https://google.com')).toBe('https://google.com')
   })
 
-  it('Should throw an error when URL is not valid', () => {
-    expect(() => new URL('vreer43').origin).toThrow('Invalid URL')
+  it('Should return null when URL is not valid', () => {
+    expect(getDappOrigin('vreer43')).toBe(null)
   })
 })
