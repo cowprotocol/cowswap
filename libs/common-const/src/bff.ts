8ac512e98f60713c153940842665a724f3117c6a
Alexandr Kazachenko <shoom3301@gmail.com>
2026-04-14 23:17:14 +0500

    chore: hardcode bff and cms

--- libs/common-const/src/bff.ts
+++ libs/common-const/src/bff.ts
@@ -1,1 +1,3 @@
-export const BFF_BASE_URL = process.env['REACT_APP_BFF_BASE_URL'] || 'https://bff.barn.cow.finance'
+// export const BFF_BASE_URL = process.env['REACT_APP_BFF_BASE_URL'] || 'https://bff.barn.cow.finance'
+// TODO: COW.FINANCE
+export const BFF_BASE_URL = 'https://bff.cow.finance'
