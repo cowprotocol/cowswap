44fb968a2a4c1bc4b738e44fb59623799cc8cdc0
Alexandr Kazachenko <shoom3301@gmail.com>
2026-04-16 13:49:02 +0500

    feat(widget)!: bring cow.fi back (#7361)

--- libs/widget-lib/src/index.ts
+++ libs/widget-lib/src/index.ts
@@ -1,9 +1,9 @@
 export { createCowSwapWidget } from './cowSwapWidget'
 export { WIDGET_IFRAME_ALLOW, WIDGET_IFRAME_REFERRER_POLICY, WIDGET_IFRAME_SANDBOX } from './cowSwapWidget.constants'
-export { widgetIframeTransport } from './widgetIframeTransport'
 export type { CowSwapWidgetHandler } from './cowSwapWidget'
 export { resolveFlexibleConfigValues } from './resolveFlexibleConfigValues'
 export { sanitizeWidgetBaseUrl, SHOULD_THROW_IF_INVALID_URL } from './urlUtils'
+export { widgetIframeTransport } from './widgetIframeTransport'
 
 export * from './types'
 export * from './themeUtils'
