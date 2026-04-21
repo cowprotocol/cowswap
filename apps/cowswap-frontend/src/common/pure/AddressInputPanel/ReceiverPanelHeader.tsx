170ba8278145209ba351c57f7cc12f5b2585460b
Denis Makarov <limitofzero@gmail.com>
2026-04-21 17:44:50 +0400

    fix: icon

--- apps/cowswap-frontend/src/common/pure/AddressInputPanel/ReceiverPanelHeader.tsx
+++ apps/cowswap-frontend/src/common/pure/AddressInputPanel/ReceiverPanelHeader.tsx
@@ -4,7 +4,6 @@ import { BaseChainInfo } from '@cowprotocol/common-const'
 import { TargetChainId } from '@cowprotocol/cow-sdk'
 
 import { Trans, useLingui } from '@lingui/react/macro'
-import QR_CODE_ICON from 'assets/icon/qr-code.svg'
 
 import { useReceiverActions } from './hooks/useReceiverActions'
 import { useReceiverChainInfo } from './hooks/useReceiverChainInfo'
@@ -61,7 +60,9 @@ export function ReceiverPanelHeader({ onChange, value, targetChainId, label }: R
           {showScanPaste && (
             <ActionBtn onClick={() => setShowQrModal(true)}>
               <QrIconWrapper>
-                <img src={QR_CODE_ICON} alt="" aria-hidden="true" width="14" height="14" />
+                <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                  <path d="M3 3h7v7H3V3zm2 2v3h3V5H5zm9-2h7v7h-7V3zm2 2v3h3V5h-3zM3 14h7v7H3v-7zm2 2v3h3v-3H5zm11 0h2v2h-2v-2zm2-2h2v2h-2v-2zm-4 4h2v2h-2v-2zm2 0h2v2h-2v-2zm0 2h2v2h-2v-2zm2-4h2v2h-2v-2z" />
+                </svg>
               </QrIconWrapper>
               <Trans>Scan</Trans>
             </ActionBtn>
