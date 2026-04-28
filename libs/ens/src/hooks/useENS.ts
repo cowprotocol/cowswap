221697cd8a800ed2c2e4a4a34ed3778ae5fb63a9
Daniel Constantin <hello@danielconstantin.net>
2026-04-28 10:33:22 +0300

    refactor(ens): update useENS to accept string or null for nameOrAddress

--- libs/ens/src/hooks/useENS.ts
+++ libs/ens/src/hooks/useENS.ts
@@ -11,21 +11,22 @@ import type { Address } from 'viem'
  * Given a name or address, does a lookup to resolve to an address and name
  * @param nameOrAddress ENS name or address
  */
-export function useENS(nameOrAddress?: Address | null): {
+export function useENS(nameOrAddress?: string | null): {
   loading: boolean
   address: string | null
   name: string | null
 } {
-  const validated = isAddress(nameOrAddress) as Address
-  const reverseLookup = useENSName(validated ? validated : undefined)
+  const validatedAddress = isAddress(nameOrAddress)
+  const address = validatedAddress ? (validatedAddress as Address) : undefined
+  const reverseLookup = useENSName(address)
   const lookup = useENSAddress(nameOrAddress)
 
   return useMemo(
     () => ({
       loading: reverseLookup.loading || lookup.loading,
-      address: validated ? validated : lookup.address,
-      name: reverseLookup.ENSName ? reverseLookup.ENSName : !validated && lookup.address ? nameOrAddress || null : null,
+      address: address || lookup.address,
+      name: reverseLookup.ENSName ? reverseLookup.ENSName : !address && lookup.address ? nameOrAddress || null : null,
     }),
-    [lookup.address, lookup.loading, nameOrAddress, reverseLookup.ENSName, reverseLookup.loading, validated],
+    [address, lookup.address, lookup.loading, nameOrAddress, reverseLookup.ENSName, reverseLookup.loading],
   )
 }
