62109a83eacf4050f2c2452789aec07e0ca7ea0a
Kaze <230549489+kaze-cow@users.noreply.github.com>
2026-05-06 21:18:40 +0900

    use jotai for state management, and remove unnecessary orderflow file (should be separate PR)

--- apps/explorer/src/hooks/euler/client.ts
+++ apps/explorer/src/hooks/euler/client.ts
@@ -1,17 +1,23 @@
 import { RPC_URLS, VIEM_CHAINS } from '@cowprotocol/common-const'
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
+import { atom, getDefaultStore } from 'jotai'
+import { atomFamily } from 'jotai/utils'
 import { createPublicClient, http } from 'viem'
 
-const clientCache = new Map<number, ReturnType<typeof createPublicClient>>()
+type PublicClient = ReturnType<typeof createPublicClient>
 
-export function getPublicClient(chainId: SupportedChainId): ReturnType<typeof createPublicClient> {
-  const cached = clientCache.get(chainId)
+const publicClientAtomFamily = atomFamily((_chainId: SupportedChainId) => atom<PublicClient | null>(null))
+
+export function getPublicClient(chainId: SupportedChainId): PublicClient {
+  const store = getDefaultStore()
+  const clientAtom = publicClientAtomFamily(chainId)
+  const cached = store.get(clientAtom)
   if (cached) return cached
   const client = createPublicClient({
     chain: VIEM_CHAINS[chainId],
     transport: http(RPC_URLS[chainId]),
   })
-  clientCache.set(chainId, client)
+  store.set(clientAtom, client)
   return client
 }
