1c4e3ab2f1b467e6a0a540bf3caee265db54cf43
Daniel Constantin <hello@danielconstantin.net>
2026-05-19 17:09:22 +0300

    fix(affiliate): don't notify of linked code in unsupported networks

--- apps/cowswap-frontend/src/modules/affiliate/hooks/useNotifyAffiliateLinkedCode.tsx
+++ apps/cowswap-frontend/src/modules/affiliate/hooks/useNotifyAffiliateLinkedCode.tsx
@@ -8,15 +8,17 @@ import type { Order } from 'legacy/state/orders/actions'
 import { useAffiliateTraderInfo } from './useAffiliateTraderInfo'
 
 import { PROGRAM_DEFAULTS } from '../config/affiliateProgram.const'
+import { isSupportedTradingNetwork } from '../lib/affiliateProgramUtils'
 import { AffiliateLinkedCodeNotification } from '../pure/AffiliateLinkedCodeNotification'
 import { AffiliateNotificationIcon } from '../pure/AffiliateNotificationIcon'
 import { affiliateTraderSavedCodeAtom, setAffiliateTraderSavedCodeAtom } from '../state/affiliateTraderSavedCodeAtom'
 
 interface NotifyAffiliateLinkedCodeParams {
   order: Order | undefined
+  chainId: number
 }
 
-export function useNotifyAffiliateLinkedCode({ order }: NotifyAffiliateLinkedCodeParams): void {
+export function useNotifyAffiliateLinkedCode({ order, chainId }: NotifyAffiliateLinkedCodeParams): void {
   const { savedCode: refCode, isLinked } = useAtomValue(affiliateTraderSavedCodeAtom)
   const setSavedCode = useSetAtom(setAffiliateTraderSavedCodeAtom)
   const addSnackbar = useAddSnackbar()
@@ -25,7 +27,7 @@ export function useNotifyAffiliateLinkedCode({ order }: NotifyAffiliateLinkedCod
   const timeCapDays = codeInfo?.timeCapDays ?? PROGRAM_DEFAULTS.AFFILIATE_TIME_CAP_DAYS
 
   useEffect(() => {
-    if (!order || !refCode || isLinked) return
+    if (!order || !refCode || isLinked || !isSupportedTradingNetwork(chainId)) return
 
     setSavedCode({ savedCode: refCode, isLinked: true })
     addSnackbar({
@@ -35,5 +37,5 @@ export function useNotifyAffiliateLinkedCode({ order }: NotifyAffiliateLinkedCod
       duration: 0,
       content: <AffiliateLinkedCodeNotification code={refCode} timeCapDays={timeCapDays} />,
     })
-  }, [addSnackbar, isLinked, order, refCode, setSavedCode, timeCapDays])
+  }, [addSnackbar, chainId, isLinked, order, refCode, setSavedCode, timeCapDays])
 }
