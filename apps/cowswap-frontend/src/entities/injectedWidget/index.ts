0230132a9691f1b9fbc2bf35654ac3d98f6515c6
Alexandr Kazachenko <shoom3301@gmail.com>
2026-05-26 16:29:43 +0500

    chore: fix build

--- apps/cowswap-frontend/src/entities/injectedWidget/index.ts
+++ apps/cowswap-frontend/src/entities/injectedWidget/index.ts
@@ -1,3 +1,3 @@
 export { useInjectedWidgetParams, useWidgetPartnerFee } from './hooks/useInjectedWidgetParams'
 export type { WidgetParamsErrors } from './state/injectedWidgetParamsAtom'
-export { injectedWidgetParamsAtom } from './state/injectedWidgetParamsAtom'
+export { injectedWidgetParamsAtom, injectedWidgetPartnerFeeAtom } from './state/injectedWidgetParamsAtom'
