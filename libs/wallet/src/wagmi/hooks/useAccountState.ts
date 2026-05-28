fe27195b2be91640630e8d447bb7e9bda3cb02ff
Alexandr Kazachenko <shoom3301@gmail.com>
2026-05-28 16:27:59 +0500

    feat: add basic solana integration

---
+++ libs/wallet/src/wagmi/hooks/useAccountState.ts
@@ -0,0 +1,58 @@
+import { useEffect, useMemo, useState } from 'react'
+
+import { getCurrentChainIdFromUrl } from '@cowprotocol/common-utils'
+import { SupportedChainId } from '@cowprotocol/cow-sdk'
+
+import { UseAppKitAccountReturn } from '@reown/appkit'
+import { Address } from 'viem'
+import { useConnection } from 'wagmi'
+
+import { CAIP_TO_SUPPORTED_CHAIN_ID } from '../../constants'
+import { reownAppKit } from '../config'
+
+export interface AccountState {
+  chainId: SupportedChainId
+  address: Address | undefined
+  isConnected: boolean
+  status: 'reconnecting' | 'connected' | 'disconnected' | 'connecting' | undefined
+}
+
+export function useAccountState(): AccountState {
+  const [chainId, setChainId] = useState<SupportedChainId>(getCurrentChainIdFromUrl())
+  const [solanaAccountState, setSolanaAccountState] = useState<UseAppKitAccountReturn | null>(null)
+
+  const evmState = useConnection()
+
+  useEffect(() => {
+    if (!reownAppKit) return
+
+    const subscriptions: Array<() => void> = []
+
+    subscriptions.push(reownAppKit.subscribeAccount(setSolanaAccountState, 'solana'))
+
+    subscriptions.push(
+      reownAppKit.subscribeState((state) => {
+        if (state.selectedNetworkId) {
+          setChainId(CAIP_TO_SUPPORTED_CHAIN_ID[state.selectedNetworkId])
+        }
+      }),
+    )
+
+    return () => {
+      subscriptions.forEach((s) => s())
+    }
+  }, [])
+
+  return useMemo(() => {
+    if (chainId === SupportedChainId.SOLANA && solanaAccountState) {
+      return {
+        ...solanaAccountState,
+        chainId,
+      }
+    }
+    return {
+      ...evmState,
+      chainId,
+    }
+  }, [chainId, evmState, solanaAccountState])
+}
