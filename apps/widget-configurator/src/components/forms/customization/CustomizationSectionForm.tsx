07983e90bd07c2e2336b219b34bab6b7d2a0c922
Dani Gámez Franco <gmzcodes@outlook.com>
2026-06-04 20:28:04 +0200

    chore: simplify CustomizationSectionForm input change handler

--- apps/widget-configurator/src/components/forms/customization/CustomizationSectionForm.tsx
+++ apps/widget-configurator/src/components/forms/customization/CustomizationSectionForm.tsx
@@ -1,52 +1,64 @@
-import type { ReactNode } from 'react'
-
-import type { CowSwapWidgetParams } from '@cowprotocol/widget-lib'
+import { useCallback, type ReactNode } from 'react'
 
 import { TextInput } from '../../ui/inputs/TextInput/TextInput.component'
 
 import type { SidebarSectionFormProps } from '../forms.types'
 
-type WidgetSounds = keyof NonNullable<CowSwapWidgetParams['sounds']>
+const valueNullAsString = (value: string | undefined | null): string => (value === null ? 'null' : value || '')
+
+const customizationFields = ['customImages', 'customSounds'] as const
+
+type CustomizationField = (typeof customizationFields)[number]
 
 export function CustomizationSectionForm({ values, onChange }: SidebarSectionFormProps): ReactNode {
-  const customImages = values.customImages || {}
-  const customSounds = values.customSounds || {}
+  const { customImages, customSounds } = values
 
-  const valueNullAsString = (value: string | undefined | null): string => (value === null ? 'null' : value || '')
-  const handleSoundChange =
-    (type: WidgetSounds) =>
+  const handleChange = useCallback(
     (name: string, value: string | null): void => {
-      const nextValue = value === 'null' ? null : value || ''
-      onChange('customSounds', { ...customSounds, [type]: nextValue })
-    }
+      const [fieldName, subFieldName] = name.split('.') as [CustomizationField, string]
+
+      if (!fieldName || !subFieldName || !customizationFields.includes(fieldName)) {
+        console.warn('[COW][CONFIGURATOR] Missing field name in change event:', name)
+        return
+      }
+
+      const prevValues = {
+        customImages,
+        customSounds,
+      }[fieldName]
+
+      onChange(fieldName, { ...prevValues, [subFieldName]: value === 'null' ? null : value || '' })
+    },
+    [customImages, customSounds, onChange],
+  )
 
   return (
     <>
       <TextInput
-        name="customImages-emptyOrders"
+        name="customImages.emptyOrders"
         label="Empty orders image URL"
-        value={customImages.emptyOrders || ''}
-        onChange={(_, value) => onChange('customImages', { ...customImages, emptyOrders: value || '' })}
+        value={customImages?.emptyOrders || ''}
+        onChange={handleChange}
       />
       <TextInput
-        name="customSounds-postOrder"
+        name="customSounds.postOrder"
         label="Submitted order sound URL"
-        value={valueNullAsString(customSounds.postOrder)}
-        onChange={handleSoundChange('postOrder')}
+        value={valueNullAsString(customSounds?.postOrder)}
+        onChange={handleChange}
         helperText='Set literal "null" to disable this sound.'
       />
       <TextInput
-        name="customSounds-orderExecuted"
+        name="customSounds.orderExecuted"
         label="Executed order sound URL"
-        value={valueNullAsString(customSounds.orderExecuted)}
-        onChange={handleSoundChange('orderExecuted')}
+        value={valueNullAsString(customSounds?.orderExecuted)}
+        onChange={handleChange}
         helperText='Set literal "null" to disable this sound.'
       />
       <TextInput
-        name="customSounds-orderError"
+        name="customSounds.orderError"
         label="Failed order sound URL"
-        value={valueNullAsString(customSounds.orderError)}
-        onChange={handleSoundChange('orderError')}
+        value={valueNullAsString(customSounds?.orderError)}
+        onChange={handleChange}
         helperText='Set literal "null" to disable this sound.'
       />
     </>
