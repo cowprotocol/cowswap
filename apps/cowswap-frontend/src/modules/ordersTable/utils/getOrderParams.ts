a7fe66146e53411d881d646ba8b83eb3be26b3ba
Dani Gámez Franco <gmzcodes@outlook.com>
2026-06-05 18:23:24 +0200

    fix: fix core issue with _hasEnoughBalanceAndAllowance

--- apps/cowswap-frontend/src/modules/ordersTable/utils/getOrderParams.ts
+++ apps/cowswap-frontend/src/modules/ordersTable/utils/getOrderParams.ts
@@ -29,6 +29,7 @@ export function getOrderParams(
   const isOrderAtLeastOnceFilled = order.executionData.filledAmount.gt(0)
   const sellAmount = CurrencyAmount.fromRawAmount(order.inputToken, order.sellAmount)
   const buyAmount = CurrencyAmount.fromRawAmount(order.outputToken, order.buyAmount)
+
   const isPermitInvalid = pendingOrdersPermitValidityState
     ? pendingOrdersPermitValidityState[order.id] === false
     : false
@@ -46,12 +47,15 @@ export function getOrderParams(
   const balance = balances[getAddressKey(order.inputToken.address)]
   const allowance = allowances?.[getAddressKey(order.inputToken.address)]
 
+  // After the first fill, the permit is spent, so we only use the on-chain allowance from then on.
+  // Before the first fill, we use the bigger of the on-chain approve allowance, or permit amount (once validated on-chain).
+  const effectiveAllowance = isOrderAtLeastOnceFilled ? allowance : getBiggerAmount(allowance, permitAmount)
+
   const { hasEnoughBalance, hasEnoughAllowance } = _hasEnoughBalanceAndAllowance({
     partiallyFillable: order.partiallyFillable,
     sellAmount,
     balance,
-    // If the order has been filled at least once, we should not consider the permit amount
-    allowance: !isOrderAtLeastOnceFilled ? getBiggerAmount(allowance, permitAmount) : allowance,
+    allowance: effectiveAllowance,
   })
 
   return {
@@ -74,10 +78,15 @@ function _hasEnoughBalanceAndAllowance(params: {
   hasEnoughAllowance: boolean | undefined
 } {
   const { allowance, balance, partiallyFillable, sellAmount } = params
-  // Check there's at least PERCENTAGE_FOR_PARTIAL_FILLS of balance/allowance to consider it as enough
-  const amount = partiallyFillable ? sellAmount.multiply(PERCENTAGE_FOR_PARTIAL_FILLS) : sellAmount
-  const hasEnoughBalance = isEnoughAmount(amount, balance)
-  const hasEnoughAllowance = isEnoughAmount(amount, allowance)
+
+  // For partially fillable orders, we want to check there's sat least `PERCENTAGE_FOR_PARTIAL_FILLS` of balance instead
+  // of the full amount. However, for allowance, we probably want to check if the allowance covers the full sell amount.
+  //
+  // Otherwise, a fillable order with full balance available but just some dust allowance would be considered fillable,
+  // and the user would falsely expect it to be fully fillable, which would not be the case.
+  const balanceAmount = partiallyFillable ? sellAmount.multiply(PERCENTAGE_FOR_PARTIAL_FILLS) : sellAmount
+  const hasEnoughBalance = isEnoughAmount(balanceAmount, balance)
+  const hasEnoughAllowance = isEnoughAmount(sellAmount, allowance)
 
   return { hasEnoughBalance, hasEnoughAllowance }
 }
