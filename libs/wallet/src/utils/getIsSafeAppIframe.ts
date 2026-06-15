1c6f760f55c6aa5512d79711e3a29b1f44e08b24
Alexandr Kazachenko <shoom3301@gmail.com>
2026-06-15 16:33:23 +0500

    chore: extend getIsSafeAppIframe

--- libs/wallet/src/utils/getIsSafeAppIframe.ts
+++ libs/wallet/src/utils/getIsSafeAppIframe.ts
@@ -2,6 +2,13 @@ import { getParentOrigin } from '@cowprotocol/iframe-transport'
 
 import { SAFE_APP_ORIGIN } from '../constants'
 
+const SAFE_APP_PREVIEW_URL = 'https://safe-wallet-monorepo-cowswap-web.vercel.app'
+const SAFE_APP_LOCAL_URL = 'http://localhost:4003'
+const SAFE_SUPPORTED_ORIGINS = [SAFE_APP_ORIGIN, SAFE_APP_PREVIEW_URL, SAFE_APP_LOCAL_URL]
+
 export function getIsSafeAppIframe(): boolean {
-  return getParentOrigin() === SAFE_APP_ORIGIN
+  const origin = getParentOrigin()
+
+  if (!origin) return false
+  return SAFE_SUPPORTED_ORIGINS.includes(origin)
 }
