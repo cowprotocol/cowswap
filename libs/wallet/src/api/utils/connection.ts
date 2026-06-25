88fee3262b0e4c474f5e7b3e21e4d44ff13cd04c
Alexandr Kazachenko <shoom3301@gmail.com>
2026-06-25 13:43:14 +0500

    chore: revert 7691

--- libs/wallet/src/api/utils/connection.ts
+++ libs/wallet/src/api/utils/connection.ts
@@ -1,23 +1,7 @@
 import { isMobile } from '@cowprotocol/common-utils'
 
-import type { EIP1193Provider } from 'viem'
-
-type WindowWithInjectedProvider = {
-  ethereum?: unknown
-}
-
-export function getInjectedProvider(targetWindow?: WindowWithInjectedProvider): EIP1193Provider | undefined {
-  try {
-    const ethereumWindow = targetWindow ?? (typeof window === 'undefined' ? undefined : window)
-
-    return ethereumWindow?.ethereum as EIP1193Provider | undefined
-  } catch {
-    return undefined
-  }
-}
-
 export function getIsInjected(): boolean {
-  return Boolean(getInjectedProvider())
+  return Boolean(window.ethereum)
 }
 
 export function getIsInjectedMobileBrowser(): boolean {
