25eb49b7b61b96a694f539ca1cd3e955777323ff
Alfetopito <alfetopito@users.noreply.github.com>
2026-07-01 11:54:56 +0100

    fix: add Safe preview/test urls to iframe allowlist

--- libs/wallet/src/utils/getIsSafeAppIframe.ts
+++ libs/wallet/src/utils/getIsSafeAppIframe.ts
@@ -2,13 +2,26 @@ import { getParentOrigin } from '@cowprotocol/iframe-transport'
 
 import { SAFE_APP_ORIGIN } from '../constants'
 
-const SAFE_APP_PREVIEW_URL = 'https://safe-wallet-monorepo-cowswap-web.vercel.app'
-const SAFE_APP_LOCAL_URL = 'http://localhost:4003'
-const SAFE_SUPPORTED_ORIGINS = [SAFE_APP_ORIGIN, SAFE_APP_PREVIEW_URL, SAFE_APP_LOCAL_URL]
+const SAFE_APP_PREVIEW_URLS = [
+  'https://safe-wallet-monorepo-cowswap-web.vercel.app', // Our internal Safe app for testing
+  'https://safe-wallet-web.dev.5afe.dev', // Safe's dev env
+  'https://safe-wallet-web.staging.5afe.dev', // Safe's staging env
+]
+const SAFE_APP_PREVIEW_HOST_SUFFIX = '.review.5afe.dev' // Safe's preview env
+const SAFE_APP_LOCAL_URLS = ['http://localhost:4003', 'http://localhost:3000']
+const SAFE_SUPPORTED_ORIGINS = [SAFE_APP_ORIGIN, ...SAFE_APP_PREVIEW_URLS, ...SAFE_APP_LOCAL_URLS].map(
+  (origin) => new URL(origin).origin,
+)
 
 export function getIsSafeAppIframe(): boolean {
   const origin = getParentOrigin()
 
   if (!origin) return false
-  return SAFE_SUPPORTED_ORIGINS.includes(origin)
+
+  const originUrl = new URL(origin)
+
+  return (
+    SAFE_SUPPORTED_ORIGINS.includes(originUrl.origin) ||
+    (originUrl.protocol === 'https:' && !originUrl.port && originUrl.hostname.endsWith(SAFE_APP_PREVIEW_HOST_SUFFIX))
+  )
 }
