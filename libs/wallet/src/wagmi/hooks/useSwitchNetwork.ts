ef14f70ff340dd59902f629eca0d057413c5952b
shoom3301 <shoom3301@gmail.com>
2026-07-13 14:41:19 +0200

    fix: switch network through reown

--- libs/wallet/src/wagmi/hooks/useSwitchNetwork.ts
+++ libs/wallet/src/wagmi/hooks/useSwitchNetwork.ts
@@ -1,25 +1,34 @@
 import { useSetAtom } from 'jotai'
 import { useCallback } from 'react'
 
-import { useConnection, useSwitchChain } from 'wagmi'
+import { useConnection } from 'wagmi'
 
 import { SupportedChainId } from '@cowprotocol/cow-sdk'
 
+import { useAppKitNetwork } from '@reown/appkit/react'
+
 import { walletInfoAtom } from '../../api/state'
+import { SUPPORTED_REOWN_NETWORKS } from '../../reown/networks'
 
 export function useSwitchNetwork(): (chainId: SupportedChainId) => Promise<void> {
-  const { mutateAsync: switchChain } = useSwitchChain()
   const { isConnected } = useConnection()
   const setWalletInfo = useSetAtom(walletInfoAtom)
+  const { switchNetwork } = useAppKitNetwork()
 
   return useCallback(
     async (chainId: SupportedChainId) => {
       if (isConnected) {
-        await switchChain({ chainId })
+        const network = SUPPORTED_REOWN_NETWORKS.find(({ id }) => id === chainId)
+
+        if (!network) {
+          console.error('Unknown network to switch on', chainId)
+          return
+        }
+        await switchNetwork(network)
       } else {
         setWalletInfo((prev) => ({ ...prev, chainId }))
       }
     },
-    [switchChain, isConnected, setWalletInfo],
+    [switchNetwork, isConnected, setWalletInfo],
   )
 }
