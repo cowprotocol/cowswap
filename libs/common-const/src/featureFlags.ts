2306273c1516cee6f0ec6809e1bf01755ea89c02
shoom3301 <shoom3301@gmail.com>
2026-07-14 18:14:53 +0200

    chore: ff IS_SOLANA_ENABLED in URL

--- libs/common-const/src/featureFlags.ts
+++ libs/common-const/src/featureFlags.ts
@@ -2,4 +2,10 @@ export type FeatureFlags = Record<string, FeatureFlagValue>
 
 export type FeatureFlagValue = boolean | number | undefined
 
-export const IS_SOLANA_ENABLED = typeof localStorage !== 'undefined' && !!localStorage.getItem('IS_SOLANA_ENABLED')
+export let IS_SOLANA_ENABLED = typeof localStorage !== 'undefined' && !!localStorage.getItem('IS_SOLANA_ENABLED')
+
+if (typeof location !== 'undefined') {
+  const value = new URLSearchParams(location.hash.split('?')[1]).get('IS_SOLANA_ENABLED')
+
+  IS_SOLANA_ENABLED = value === 'true'
+}
