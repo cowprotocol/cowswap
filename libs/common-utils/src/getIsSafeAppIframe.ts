a6b6d0e04d81d230933ead361fc9403210e723de
Dani Gámez Franco <gmzcodes@outlook.com>
2026-07-22 13:23:31 +0200

    feat: change sort-modules rule from warn to error and run lint:fix

--- libs/common-utils/src/getIsSafeAppIframe.ts
+++ libs/common-utils/src/getIsSafeAppIframe.ts
@@ -10,9 +10,12 @@ const SAFE_APP_PREVIEW_URLS = [
 const SAFE_APP_PREVIEW_HOST_SUFFIX = '.review.5afe.dev' // Safe's per-PR review env
 const SAFE_APP_LOCAL_URL1 = 'http://localhost:4003'
 const SAFE_APP_LOCAL_URL2 = 'http://localhost:8080'
-const SAFE_SUPPORTED_ORIGINS = [SAFE_APP_ORIGIN, ...SAFE_APP_PREVIEW_URLS, SAFE_APP_LOCAL_URL1, SAFE_APP_LOCAL_URL2].map(
-  (origin) => new URL(origin).origin,
-)
+const SAFE_SUPPORTED_ORIGINS = [
+  SAFE_APP_ORIGIN,
+  ...SAFE_APP_PREVIEW_URLS,
+  SAFE_APP_LOCAL_URL1,
+  SAFE_APP_LOCAL_URL2,
+].map((origin) => new URL(origin).origin)
 
 export function getIsSafeAppIframe(): boolean {
   const origin = getParentOrigin()
