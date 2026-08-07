66a989d0acdd197b343e0495c15ce45584cddf63
Denis Makarov <limitofzero@gmail.com>
2026-08-07 13:23:18 +0400

    feat(solana): approve flow (#7946)

--- apps/cowswap-frontend/src/common/hooks/useTokenAllowance.ts
+++ apps/cowswap-frontend/src/common/hooks/useTokenAllowance.ts
@@ -11,6 +11,7 @@ import ms from 'ms.macro'
 import useSWR, { SWRConfiguration, SWRResponse } from 'swr'
 
 import { useTokenContract } from 'common/hooks/useContract'
+import { useSolanaDelegationAllowance } from 'common/hooks/useSolanaDelegationAllowance'
 
 import { getOptimisticAllowanceKey } from '../../entities/optimisticAllowance/getOptimisticAllowanceKey'
 
@@ -33,6 +34,7 @@ export function useTokenAllowance(
   const { contract: erc20Contract } = useTokenContract(tokenAddress)
   const tradeSpender = useTradeSpenderAddress()
   const [optimisticAllowances, setOptimisticAllowances] = useAtom(optimisticAllowancesAtom)
+  const solanaAllowance = useSolanaDelegationAllowance(tokenAddress)
 
   const targetOwner = owner ?? account
   const targetSpender = spender ?? tradeSpender
@@ -81,8 +83,8 @@ export function useTokenAllowance(
   return useMemo(
     () => ({
       ...swrResponse,
-      data: optimisticAllowance?.amount ?? swrResponse.data,
+      data: solanaAllowance ?? optimisticAllowance?.amount ?? swrResponse.data,
     }),
-    [optimisticAllowance?.amount, swrResponse],
+    [solanaAllowance, optimisticAllowance?.amount, swrResponse],
   )
 }
