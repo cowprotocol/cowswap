93f0fd65dc23e47002840569e0552c7f78edc55a
Dani Gámez Franco <gmzcodes@outlook.com>
2026-08-19 17:21:42 +0200

    fix: close DrawerOrInline of width forces it to change mode

--- libs/ui/src/pure/BottomDrawer/DrawerOrInline.pure.tsx
+++ libs/ui/src/pure/BottomDrawer/DrawerOrInline.pure.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 
-import { useMediaQuery } from '@cowprotocol/common-hooks'
+import { useLatestRef, useMediaQuery } from '@cowprotocol/common-hooks'
 
 import { BottomDrawer } from './BottomDrawer.pure'
 
@@ -26,6 +26,22 @@ export function DrawerOrInline({
   footer,
 }: DrawerOrInlineProps): ReactNode {
   const isUpToLarge = useMediaQuery(Media.upToLarge(false))
+
+  const onOpenChangeRef = useLatestRef(onOpenChange)
+
+  useEffect(() => {
+    const closeDrawer = onOpenChangeRef.current
+
+    // If we got from "drawer" to "inline" (we make the window wider),
+    // we close it, so that if we resize the window back down, the drawer is not already opened:
+    if (!isUpToLarge) {
+      closeDrawer(false)
+    }
+
+    return () => {
+      closeDrawer(false)
+    }
+  }, [onOpenChangeRef, isUpToLarge])
 
   if (!isUpToLarge) {
     return (
