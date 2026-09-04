b972180298c2ad98c9e00adf3bda5f61187b1181
Alexandr Kazachenko <shoom3301@gmail.com>
2026-09-04 15:48:09 +0200

    fix: support solana in useAreThereTokensWithSameSymbol

--- libs/tokens/src/hooks/tokens/useAreThereTokensWithSameSymbol.ts
+++ libs/tokens/src/hooks/tokens/useAreThereTokensWithSameSymbol.ts
@@ -2,22 +2,26 @@ import { useAtomValue } from 'jotai'
 import { useCallback } from 'react'
 
 import { isAddress } from '@cowprotocol/common-utils'
-import { SupportedChainId } from '@cowprotocol/cow-sdk'
+import { getAddressKey, Nullish, SupportedChainId } from '@cowprotocol/cow-sdk'
 
 import { tokensBySymbolAtom } from '../../state/tokens/allTokensAtom'
 
-// TODO: Add proper return type annotation
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function useAreThereTokensWithSameSymbol() {
+export function useAreThereTokensWithSameSymbol(): (
+  tokenAddressOrSymbol: Nullish<string>,
+  chainId: SupportedChainId,
+) => boolean {
   const tokensBySymbol = useAtomValue(tokensBySymbolAtom)
 
   return useCallback(
-    (tokenAddressOrSymbol: string | null | undefined, chainId: SupportedChainId) => {
+    (tokenAddressOrSymbol: Nullish<string>, chainId: SupportedChainId) => {
       if (!tokenAddressOrSymbol || isAddress(tokenAddressOrSymbol)) return false
 
       if (tokensBySymbol.chainId !== chainId) return false
 
-      const tokens = tokensBySymbol.tokens[tokenAddressOrSymbol.toLowerCase()]
+      const key = isAddress(tokenAddressOrSymbol)
+        ? getAddressKey(tokenAddressOrSymbol)
+        : tokenAddressOrSymbol.toLowerCase()
+      const tokens = tokensBySymbol.tokens[key]
       const hasDuplications = tokens?.length > 1
 
       if (hasDuplications) {
