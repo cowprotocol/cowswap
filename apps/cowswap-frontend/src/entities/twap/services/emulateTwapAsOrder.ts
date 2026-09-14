6f0e797f51a81c4db63a4ac20658e1c7d4b6fc84
Daniel Constantin <hello@danielconstantin.net>
2026-09-14 16:02:19 +0000

    fix: change status mapping

--- apps/cowswap-frontend/src/entities/twap/services/emulateTwapAsOrder.ts
+++ apps/cowswap-frontend/src/entities/twap/services/emulateTwapAsOrder.ts
@@ -8,7 +8,7 @@ const statusMap: Record<TwapOrderStatus, OrderStatus> = {
   [TwapOrderStatus.WaitSigning]: OrderStatus.PRESIGNATURE_PENDING,
   [TwapOrderStatus.Cancelled]: OrderStatus.CANCELLED,
   [TwapOrderStatus.Expired]: OrderStatus.EXPIRED,
-  [TwapOrderStatus.PartiallyFilled]: OrderStatus.EXPIRED,
+  [TwapOrderStatus.PartiallyFilled]: OrderStatus.FULFILLED,
   [TwapOrderStatus.Fulfilled]: OrderStatus.FULFILLED,
 }
 
